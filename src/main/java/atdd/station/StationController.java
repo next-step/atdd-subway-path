a20074bbdab64ee2370621b64a0725375c144482
jen6 <fjen6b@gmail.com>
2020-02-07 22:20:11 +0900

    feat(addStation) : addStation

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -1,8 +1,17 @@
 package atdd.station;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class StationController {
+    @PostMapping("/stations")
+    public ResponseEntity addStation(Map<String, Object> model){
+        return ResponseEntity.ok("{\"name\":\"강남역\"}");
+    }
+
 
 }
