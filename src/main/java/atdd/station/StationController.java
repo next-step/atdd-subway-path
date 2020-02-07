072499acc06da4def2291df85d3ba750334d3c6a
mouthgaebal <56108657+mouthgaebal@users.noreply.github.com>
2020-02-07 22:25:23 +0900

    step0 (#7)

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -1,8 +1,19 @@
 package atdd.station;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
+@RequestMapping("/station")
 public class StationController {
+
+    @PostMapping
+    public ResponseEntity<String> create() {
+        return ResponseEntity.created(URI.create("/station/1")).build();
+    }
 
 }
--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -1,8 +1,32 @@
 package atdd.station;
 
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StationAcceptanceTest {
+
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void test() throws Exception {
+        final String stationName = "강남역";
+        final String inputJson = "{\"name\":\"" + stationName + "\"}";
+
+        webTestClient.post().uri("/station")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(inputJson), String.class)
+                .exchange()
+                .expectStatus().isCreated();
+    }
+
 }
