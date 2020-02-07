24eb9c9691a2e250b04f35447369258b3e1539de
sanggu <tkdrn8578@naver.com>
2020-02-08 01:24:26 +0900

    [ADD] list 를 조회하면 강남역을 리턴해주는 mock 객체 추가
     * /station 경로를 RequestMapping 으로 추출
     * GET method 를 허용하기 위해 method 설정 추가
     * application/json 을 허용하기 위해 produces 옵션 추가

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -1,20 +1,30 @@
 package atdd.station;
 
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
 @RestController
+@RequestMapping(value = "/stations",
+        produces = "application/json",
+        method = {RequestMethod.GET, RequestMethod.GET})
 public class StationController {
 
-    @PostMapping("/stations")
-    public ResponseEntity createStation() {
+    @PostMapping("/create")
+    public ResponseEntity create() {
         return ResponseEntity
                 .created(URI.create("/station/1"))
-                .contentType(MediaType.APPLICATION_JSON)
+                .body("{\"name\":\"강남역\"}");
+    }
+
+    @PostMapping("/list")
+    public ResponseEntity list() {
+        return ResponseEntity
+                .ok()
                 .body("{\"name\":\"강남역\"}");
     }
 }
