7b0a7cad1811c29b9cd1b7c119321db302d942d6
developerKDK <60494631+developerKDK@users.noreply.github.com>
2020-02-07 22:32:41 +0900

    test (#18)
    Co-authored-by: DKKang <akwdmfxu@hanmail.net>

--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -1,8 +1,33 @@
 package atdd.station;
 
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebClient
 public class StationAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void test() {
+        String stationName = "강남역";
+        String inputJson = "{\"name\":\""+stationName+"\"}";
+
+        webTestClient.post().uri("/stations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(inputJson), String.class)
+                .exchange()
+                .expectStatus().isCreated();
+    }
+
 }
