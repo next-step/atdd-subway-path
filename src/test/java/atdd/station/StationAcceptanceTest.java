32ec2b0cd444e84e85703c7832abed744160b6ca
unknown <dud0701@naver.com>
2020-02-07 22:17:31 +0900

    create station

--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -1,8 +1,36 @@
 package atdd.station;
 
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD) // 각 테스트 마다 새로 컨텍스트를 띄운다.(초기화)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebClient //Spring boot test에 기본적으로 포함되어 있음
 public class StationAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
+
+    @Autowired
+    private WebTestClient webTestClient; //webflux기반의 테스트 객체 : 비동기콜 가능
+
+    @Test
+    public void createStation(){
+        String STATION_NAME = "강남역";
+        String inputJson = "{\"name\":\"" + STATION_NAME + "\"}";
+
+        webTestClient.post().uri("/stations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(inputJson), String.class) //webflux 환경?에서 webhandler가 요청 할 수 있는 걸만들어준다 ?
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectHeader().exists("Location");
+    }
 }
