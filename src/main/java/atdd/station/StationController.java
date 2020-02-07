2b2e91d18b2a460677da7bcf3dbb171260692dd5
DKKang <akwdmfxu@hanmail.net>
2020-02-07 22:16:31 +0900

    test

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -1,8 +1,22 @@
 package atdd.station;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 public class StationController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StationController.class);
+
+    @PostMapping(value = "/stations")
+    public ResponseEntity createStation() {
+        return ResponseEntity.created(URI.create("/station/1")).build();
+    }
 
 }
