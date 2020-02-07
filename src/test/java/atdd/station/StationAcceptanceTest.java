f71525ff2c96bf9df731da7ca1908c9aa9e78393
sanggu <tkdrn8578@naver.com>
2020-02-08 01:22:40 +0900

    [ADD] 지하철역를 조회하여 강남역을 리턴하는 테스트코드 추가
     * "강남역" 이 중복으로 사용되여 상수로 추출

--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -17,23 +17,34 @@ import reactor.core.publisher.Mono;
 public class StationAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
 
+    private static final String KANGNAM_STATION = "강남역";
+
     @Autowired
     private WebTestClient webTestClient;
 
     @Test
-    public void createStation_test() {
+    public void 강남역_지하철_등록을_요청이_성공하는지() {
         //given
-        String stationName = "강남역";
-        String inputJson = "{\"name\":\""+stationName+"\"}";
+        String inputJson = "{\"name\":\"" + KANGNAM_STATION + "\"}";
 
         //when
-        webTestClient.post().uri("stations")
+        webTestClient.post().uri("/stations/create")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(inputJson), String.class)
                 .exchange()
                 .expectStatus().isCreated()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectHeader().exists("Location")
-                .expectBody().jsonPath("$.name").isEqualTo(stationName);
+                .expectBody().jsonPath("$.name").isEqualTo(KANGNAM_STATION);
+    }
+
+    @Test
+    public void 강남역_지하철이_조회가_되는지() {
+        //when
+        webTestClient.get().uri("/stations/list")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody().jsonPath("$.name").isEqualTo(KANGNAM_STATION);
     }
 }
