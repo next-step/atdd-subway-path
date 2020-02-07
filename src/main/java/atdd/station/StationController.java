51181be889b2bcd84d49fc574ba8d13d480190dc
yhs <mvmaniaz@gmail.com>
2020-02-07 22:28:31 +0900

    feat(stations) (#11)

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -1,8 +1,18 @@
 package atdd.station;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 public class StationController {
+
+    @PostMapping("/stations")
+    public ResponseEntity createStation() {
+        return ResponseEntity.created(URI.create("/stations/1")).build();
+    }
+
 
 }
--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -1,8 +1,49 @@
 package atdd.station;
 
+import com.sun.jndi.toolkit.url.Uri;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StationAcceptanceTest {
+
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @DisplayName("createStation")
+    @Test
+    void createStation() {
+        String stationName = "강남역";
+        String inputJson = "{\"name\":\""+stationName+"\"}";
+
+        webTestClient.post().uri("/stations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(inputJson), String.class)
+                .exchange()
+                .expectStatus().isCreated();
+    }
+
+    @DisplayName("findStations")
+    @Test
+    void findStations() {
+        String stationName = "강남역";
+
+        webTestClient.get().uri("/stations")
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectHeader().exists("Location")
+                .expectBody().jsonPath("$.name").isEqualTo(stationName)
+        ;
+    }
+
 }
