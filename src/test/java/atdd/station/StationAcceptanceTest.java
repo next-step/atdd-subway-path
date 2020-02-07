f3564b1ba39bbd7814023bd90f90568a5146a39f
SonGeon <fjen6b@gmail.com>
2020-02-07 22:28:58 +0900

    feat(addStation) : addStation (#8)

--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -1,8 +1,37 @@
 package atdd.station;
 
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 public class StationAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void createStation() {
+        String stationName = "강남역";
+        String inputJSON = "{\"name\":\""+stationName+"\"}";
+
+        webTestClient.post().uri("/stations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(inputJSON), String.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectHeader().exists("Location")
+                .expectBody().jsonPath("$.name").isEqualTo(stationName);
+    }
 }
