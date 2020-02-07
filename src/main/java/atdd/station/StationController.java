1c7641c5b5f6ce10d10e285c245f9558bd22c980
yechaechae <dud0701@naver.com>
2020-02-07 22:24:40 +0900

    create station (#4)

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -1,8 +1,17 @@
 package atdd.station;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 public class StationController {
+
+    @PostMapping("/stations")
+    public ResponseEntity createStation(){
+        return ResponseEntity.created(URI.create("/stations/1")).build();
+    }
 
 }
