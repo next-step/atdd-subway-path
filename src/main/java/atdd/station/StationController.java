91df5a98fdabcfe52249ee94f3c4c20020f4a07d
강선도(Sundo Kang)/Server개발팀/SKTECH <sundo.kang@sk.com>
2020-02-07 22:17:09 +0900

    Step0 - Mock Test

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -1,8 +1,23 @@
 package atdd.station;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class StationController {
 
+    private static final Logger logger = LoggerFactory.getLogger(StationController.class);
+
+    @PostMapping(value = "/stations")
+    @ResponseBody
+    public ResponseEntity<String> createStation(Map<String, String> param) {
+        String stationName = param.get("name");
+        return ResponseEntity.ok().body("{\"name\":\""+stationName+"\"}");
+    }
 }
