48cabb7ae0259461c9c820d8f7a2ec7219cdb8ed
이재석 <34270397+seogi89@users.noreply.github.com>
2020-02-07 22:29:43 +0900

    <test> 인수 테스트 작성 (#2)

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -1,8 +1,19 @@
 package atdd.station;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
+@RequestMapping("stations")
 public class StationController {
+
+    @PostMapping
+    public ResponseEntity crate() {
+        return ResponseEntity.created(URI.create("stations/1")).build();
+    }
 
 }
