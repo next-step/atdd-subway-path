e166392e629844700e0694381639de00fb03add3
hongjongwan <serverwizard@woowahan.com>
2020-02-07 22:16:09 +0900

    test: git commit 테스트 완료

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -1,8 +1,16 @@
 package atdd.station;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 public class StationController {
 
+    @PostMapping("/stations")
+    public ResponseEntity create() {
+        return ResponseEntity.created(URI.create("/stations/1")).build();
+    }
 }
