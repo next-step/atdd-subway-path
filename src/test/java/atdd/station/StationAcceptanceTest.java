af0941fe145cacdc4e577779eddd3fac46f71f02
yu-keun, OH <37852701+eottabom@users.noreply.github.com>
2020-02-07 22:21:21 +0900

    temp (#1)

--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -3,6 +3,27 @@ package atdd.station;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 public class StationAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void test(){
+        String stationName = "강남역";
+        String inputJson = "{\"name\":\""+stationName+"\"}";
+
+        webTestClient.post().uri("/stations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(inputJson), String.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectHeader().exists("Location")
+                .expectBody().jsonPath("$.name").isEqualTo(stationName);
+    }
 }
