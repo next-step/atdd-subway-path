7805a633a6c929632760cf1f2eb8fcd5a18383fe
heesunjeong <jhswit@naver.com>
2020-02-07 22:17:14 +0900

    feat(stations): create station acceptance test

--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -1,8 +1,30 @@
 package atdd.station;
 
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
+@DirtiesContext()
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StationAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void createStation() {
+        String stationName = "강남역";
+        String inputJson = "{\"name\":\""+stationName+"\"}";
+
+        webTestClient.post().uri("/stations")
+                .body(Mono.just(inputJson), String.class)
+                .exchange()
+                .expectStatus().isCreated();
+    }
 }
