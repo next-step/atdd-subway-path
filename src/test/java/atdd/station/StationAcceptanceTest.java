02af4f721541159e738cfeac84c1744c09fb5d25
Hyeonseok AHN <hyeonseok@pplink.net>
2020-02-08 18:04:32 +0900

    Update comments for each classes

--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -1,3 +1,24 @@
+/*
+ *
+ * StationAcceptanceTest
+ *
+ * 0.0.1
+ *
+ * Copyright 2020 irrationnelle <drakkarverenis@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * */
 package atdd.station;
 
 import org.junit.jupiter.api.Test;
@@ -16,15 +37,21 @@ import reactor.core.publisher.Mono;
 @AutoConfigureWebTestClient
 public class StationAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
+    private final Station targetStation = new Station("강남역");
 
     @Autowired
     private WebTestClient webTestClient;
 
     @Test
     public void test() {
-        String stationName = "강남역";
-        String inputJson = "{\"name\":\"" + stationName + "\"}";
+        String inputJson = String.format("{name: %s}", targetStation.getName());
 
-        webTestClient.post().uri("/stations").contentType(MediaType.APPLICATION_JSON).body(Mono.just(inputJson), String.class).exchange().expectStatus().isCreated();
+        webTestClient.post()
+                     .uri("/stations")
+                     .contentType(MediaType.APPLICATION_JSON)
+                     .body(Mono.just(inputJson), String.class)
+                     .exchange()
+                     .expectStatus()
+                     .isCreated();
     }
 }
