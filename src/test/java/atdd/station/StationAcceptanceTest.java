16c24f1e054d4857b6d4e7092d2be833fe85eeb7
LeeSeungHee <palmseung@gmail.com>
2020-02-08 12:05:43 +0900

    Modify branch name step0 to step1(2)

--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -24,7 +24,7 @@ public class StationAcceptanceTest {
 
     @Test
     public void createStation() {
-        String stationName = "강남역(2)";
+        String stationName = "강남역(3)";
         String inputJson = "{\"name\":\""+stationName+"\"}";
 
 
