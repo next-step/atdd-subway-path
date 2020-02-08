d23dfcbdb76e1cbc7d560e27a36b32323ee05329
sanggu <tkdrn8578@naver.com>
2020-02-08 21:27:28 +0900

    [MODIFY] 불필요한 간격 제거

--- src/main/java/atdd/AtddApplication.java
+++ src/main/java/atdd/AtddApplication.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AtddApplication {
     public static void main(String[] args) {
         SpringApplication.run(AtddApplication.class, args);
-
     }
-
 }
