5a40af13bc126a8ccd15cd128a4d0b6a6eb95e2f
sanggu <tkdrn8578@naver.com>
2020-02-09 21:19:54 +0900

    [ADD] 지하철 노선들 list 를 조회하는 acceptanceTest 추가

--- src/test/java/atdd/station/controller/SubwayLineAcceptanceTest.java
+++ src/test/java/atdd/station/controller/SubwayLineAcceptanceTest.java
@@ -2,6 +2,8 @@ package atdd.station.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,8 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
+
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
@@ -19,20 +23,21 @@ public class SubwayLineAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(SubwayLineAcceptanceTest.class);
 
     private static final String NAME_JSON_PARSE_EXPRESSION = "$.name";
-    private static final String STATION_API_BASE_URL = "/subway-lines/";
+    private static final String SUBWAY_LINE_API_BASE_URL = "/subway-lines/";
     private static final String LOCATION_HEADER = "Location";
 
     @Autowired
     private WebTestClient webTestClient;
 
     @DisplayName("2호선_지하철노선_생성이_성공하는지")
-    @Test
-    void createSubwayLineSuccessTest() {
+    @ParameterizedTest
+    @ValueSource(strings = {"2호선"})
+    void createSubwayLineSuccessTest(String subwayLineName) {
         //when
         //then
-        webTestClient.post().uri(STATION_API_BASE_URL)
+        webTestClient.post().uri(SUBWAY_LINE_API_BASE_URL)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just("{\"name\": \"2호선\"}"), String.class)
+                .body(Mono.just("{\"name\": \"" + subwayLineName + "\"}"), String.class)
                 .exchange()
                 .expectStatus().isCreated()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
@@ -43,6 +48,37 @@ public class SubwayLineAcceptanceTest {
                 .jsonPath("$.interval").isEqualTo("10")
                 .jsonPath(getStationNameJsonParseExpressionByIndex("0")).isEqualTo("교대역");
     }
+
+    @DisplayName("지하철노선_조회가_성공하는지")
+    @Test
+    void listSubwayLineSuccessTest() {
+        creatSubwayLine("2호선");
+        creatSubwayLine("2호선");
+
+        //when
+        //then
+        webTestClient.get().uri(SUBWAY_LINE_API_BASE_URL)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(String.class).hasSize(2);
+    }
+
+
+    private String creatSubwayLine(String subwayLineName) {
+        return Objects.requireNonNull(webTestClient.post().uri(SUBWAY_LINE_API_BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just("{\"name\": \"" + subwayLineName + "\"}"), String.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectHeader().exists(LOCATION_HEADER)
+                .expectBody().jsonPath(NAME_JSON_PARSE_EXPRESSION).isEqualTo(subwayLineName)
+                .returnResult()
+                .getResponseHeaders()
+                .getLocation()).getPath();
+    }
+
 
     private String getStationNameJsonParseExpressionByIndex(String index) {
         return "$.stations[" + index + "].name";
