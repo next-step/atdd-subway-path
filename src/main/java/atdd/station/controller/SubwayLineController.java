4dfc778581cf8ee92b81800275510053145121e4
sanggu <tkdrn8578@naver.com>
2020-02-10 00:09:33 +0900

    [MODIFY] create 의 mock 로직을 제거하고, 실제 service 에서 가져오는 로직 추가

--- src/main/java/atdd/station/controller/SubwayLineController.java
+++ src/main/java/atdd/station/controller/SubwayLineController.java
@@ -1,8 +1,14 @@
 package atdd.station.controller;
 
+import atdd.station.domain.SubwayLine;
+import atdd.station.service.StationService;
+import atdd.station.service.SubwayLineService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.net.URI;
 
 @RestController
@@ -10,24 +16,16 @@ import java.net.URI;
         produces = "application/json")
 public class SubwayLineController {
 
+    @Resource(name = "subwayLineService")
+    private SubwayLineService subwayLineService;
+
     @PostMapping("/")
-    public ResponseEntity<String> create(@RequestBody String subwayLine) {
-        return ResponseEntity
-                .created(URI.create("/subway-lines/1"))
-                .body("{" +
-                "    \"id\": 1," +
-                "    \"name\": \"2호선\"," +
-                "    \"startTime\": \"05:00\"," +
-                "    \"endTime\": \"23:50\"," +
-                "    \"interval\": \"10\"," +
-                "    \"stations\": [" +
-                "        {\"id\":4,\"name\":\"교대역\"}," +
-                "        {\"id\":5,\"name\":\"강남역\"}," +
-                "        {\"id\":2,\"name\":\"역삼역\"}," +
-                "        {\"id\":3,\"name\":\"선릉역\"}," +
-                "        {\"id\":1,\"name\":\"삼성역\"}" +
-                "    ]" +
-                " }");
+    public ResponseEntity<SubwayLine> create(@RequestBody SubwayLine subwayLine) {
+        SubwayLine createdSubwayLine = subwayLineService.create(subwayLine);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/subway-lines/" + createdSubwayLine.getId()));
+        return new ResponseEntity<>(createdSubwayLine, headers, HttpStatus.CREATED);
     }
 
     @GetMapping("/")
