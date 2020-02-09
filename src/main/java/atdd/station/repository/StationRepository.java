781e1ceafb0573f0dcf9ca9b39d9ca614b5bf2e9
yukeun <snoopy12oyk@gmail.com>
2020-02-09 23:03:01 +0900

    Add: Repository 추가

--- src/main/java/atdd/station/repository/StationRepository.java
+++ src/main/java/atdd/station/repository/StationRepository.java
@@ -1,4 +1,8 @@
 package atdd.station.repository;
 
-public interface StationRepository {
-}
+import atdd.station.domain.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StationRepository extends JpaRepository<Station, Long> {}
