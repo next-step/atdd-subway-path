388e1ac01b28d0a744bd366291fdfd878d644bb6
sanggu <tkdrn8578@naver.com>
2020-02-10 00:10:15 +0900

    [MODIFY] SubwayLinesService -> SubwayLineService 로 변경

--- src/main/java/atdd/station/service/SubwayLineService.java
+++ src/main/java/atdd/station/service/SubwayLineService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
-@Service("subwayLinesService")
+@Service("subwayLineService")
 public class SubwayLineService {
     @Resource(name = "subwayLineRepository")
     private SubwayLineRepository subwayLineRepository;
