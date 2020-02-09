f41a35fd4f5693f98b1a763bebe505cc12c2e8ad
sanggu <tkdrn8578@naver.com>
2020-02-09 21:20:23 +0900

    [ADD] 지하철 노선들을 조회하는 mock API 추가

--- src/main/java/atdd/station/controller/SubwayLineController.java
+++ src/main/java/atdd/station/controller/SubwayLineController.java
@@ -1,10 +1,7 @@
 package atdd.station.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -17,19 +14,55 @@ public class SubwayLineController {
     public ResponseEntity<String> create(@RequestBody String subwayLine) {
         return ResponseEntity
                 .created(URI.create("/subway-lines/1"))
-                .body("{\n" +
-                "    \"id\": 1,\n" +
-                "    \"name\": \"2호선\",\n" +
-                "    \"startTime\": \"05:00\",\n" +
-                "    \"endTime\": \"23:50\",\n" +
-                "    \"interval\": \"10\",\n" +
-                "    \"stations\": [\n" +
-                "        {\"id\":4,\"name\":\"교대역\"},\n" +
-                "        {\"id\":5,\"name\":\"강남역\"},\n" +
-                "        {\"id\":2,\"name\":\"역삼역\"},\n" +
-                "        {\"id\":3,\"name\":\"선릉역\"},\n" +
-                "        {\"id\":1,\"name\":\"삼성역\"}\n" +
-                "    ]\n" +
+                .body("{" +
+                "    \"id\": 1," +
+                "    \"name\": \"2호선\"," +
+                "    \"startTime\": \"05:00\"," +
+                "    \"endTime\": \"23:50\"," +
+                "    \"interval\": \"10\"," +
+                "    \"stations\": [" +
+                "        {\"id\":4,\"name\":\"교대역\"}," +
+                "        {\"id\":5,\"name\":\"강남역\"}," +
+                "        {\"id\":2,\"name\":\"역삼역\"}," +
+                "        {\"id\":3,\"name\":\"선릉역\"}," +
+                "        {\"id\":1,\"name\":\"삼성역\"}" +
+                "    ]" +
                 " }");
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> list() {
+        return ResponseEntity
+                .ok()
+                .body("[" +
+                        "{" +
+                        "    \"id\": 1," +
+                        "    \"name\": \"5호선\"," +
+                        "    \"startTime\": \"05:00\"," +
+                        "    \"endTime\": \"23:50\"," +
+                        "    \"interval\": \"10\"," +
+                        "    \"stations\": [" +
+                        "        {\"id\":4,\"name\":\"교대역\"}," +
+                        "        {\"id\":5,\"name\":\"강남역\"}," +
+                        "        {\"id\":2,\"name\":\"역삼역\"}," +
+                        "        {\"id\":3,\"name\":\"선릉역\"}," +
+                        "        {\"id\":1,\"name\":\"삼성역\"}" +
+                        "    ]" +
+                        " }" +
+                        "{" +
+                        "    \"id\": 2," +
+                        "    \"name\": \"2호선\"," +
+                        "    \"startTime\": \"05:00\"," +
+                        "    \"endTime\": \"23:50\"," +
+                        "    \"interval\": \"10\"," +
+                        "    \"stations\": [" +
+                        "        {\"id\":4,\"name\":\"교대역\"}," +
+                        "        {\"id\":5,\"name\":\"강남역\"}," +
+                        "        {\"id\":2,\"name\":\"역삼역\"}," +
+                        "        {\"id\":3,\"name\":\"선릉역\"}," +
+                        "        {\"id\":1,\"name\":\"삼성역\"}" +
+                        "    ]" +
+                        " }" +
+                        "]");
     }
 }
