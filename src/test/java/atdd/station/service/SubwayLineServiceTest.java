025fbd19e6e660e529ade8aff318a14393d7a476
sanggu <tkdrn8578@naver.com>
2020-02-09 22:33:06 +0900

    [ADD] subwayLineService 의 create 에 대한 Test 코드 추가

---
+++ src/test/java/atdd/station/service/SubwayLineServiceTest.java
@@ -0,0 +1,42 @@
+package atdd.station.service;
+
+import atdd.station.domain.SubwayLine;
+import atdd.station.domain.SubwayLineRepository;
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SoftAssertionsExtension.class)
+public class SubwayLineServiceTest {
+
+    @Mock
+    SubwayLineRepository subwayLineRepository;
+
+    @InjectMocks
+    SubwayLineService subwayLineService;
+
+    @Test
+    public void 지하철노선_생성시_성공하는지(SoftAssertions softly) {
+        //given
+        SubwayLine subwayLine = SubwayLine.builder()
+                .name("2호선")
+                .build();
+
+        //when
+        when(subwayLineRepository.save(any())).thenReturn(subwayLine);
+
+        SubwayLine createSubwayLine = subwayLineService.create(subwayLine);
+
+        //then
+        softly.assertThat(createSubwayLine.getId()).isEqualTo(0L);
+        softly.assertThat(createSubwayLine.getName()).isEqualTo("2호선");
+    }
+}
