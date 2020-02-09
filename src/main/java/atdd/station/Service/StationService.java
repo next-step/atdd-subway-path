24830d19ed663909b3fd4b96dd00828501d06b8a
yukeun <snoopy12oyk@gmail.com>
2020-02-09 23:01:29 +0900

    Add: Service 추가

--- src/main/java/atdd/station/Service/StationService.java
+++ src/main/java/atdd/station/Service/StationService.java
@@ -1,4 +1,38 @@
 package atdd.station.Service;
 
-public class StationService {
+import atdd.station.domain.Station;
+import atdd.station.domain.dto.StationDto;
+import atdd.station.repository.StationRepository;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
+
+@Service("stationService")
+public class StationService
+{
+    @Resource(name = "stationRepository")
+    private StationRepository stationRepository;
+
+    public Station create(StationDto stationDto)
+    {
+        return stationRepository.save(stationDto.toEntity());
+    }
+
+    public List<Station> list()
+    {
+        List<Station> returnList = stationRepository.findAll();
+        return returnList;
+    }
+
+    public Optional<Station> findById(long id)
+    {
+        return stationRepository.findById(id);
+    }
+
+    public void deleteStationById(long id)
+    {
+        stationRepository.deleteById(id);
+    }
 }
