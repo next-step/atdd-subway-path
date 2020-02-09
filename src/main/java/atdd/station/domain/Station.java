5836bedd05f2e55002ca1b17c170aeb8a22f47e4
yukeun <snoopy12oyk@gmail.com>
2020-02-09 23:02:24 +0900

    Add: Dto, Entity 추가

--- src/main/java/atdd/station/domain/Station.java
+++ src/main/java/atdd/station/domain/Station.java
@@ -1,4 +1,25 @@
 package atdd.station.domain;
 
-public class Station {
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Station
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Builder
+    public Station(String name)
+    {
+        this.name = name;
+    }
+
 }
--- src/main/java/atdd/station/domain/dto/StationDto.java
+++ src/main/java/atdd/station/domain/dto/StationDto.java
@@ -1,4 +1,19 @@
 package atdd.station.domain.dto;
 
-public class StationDto {
+import atdd.station.domain.Station;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class StationDto
+{
+    private String name;
+
+    public Station toEntity()
+    {
+       return Station.builder().name(name).build();
+    }
 }
