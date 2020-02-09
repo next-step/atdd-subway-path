f3ddab4c48b99854d6fc6ad1961de0b896983e81
hongjongwan <serverwizard@naver.com>
2020-02-10 00:16:35 +0900

    [feat] 지하철 노선 목록 조회, ATDD 코드 작성

--- src/test/java/atdd/station/SubwayLineAcceptanceTest.java
+++ src/test/java/atdd/station/SubwayLineAcceptanceTest.java
@@ -1,5 +1,7 @@
 package atdd.station;
 
+import atdd.station.domain.SubwayLine;
+import atdd.station.web.dto.SubwayLineResponseDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
 
 @AutoConfigureWebTestClient
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -37,6 +41,25 @@ public class SubwayLineAcceptanceTest {
                 .expectHeader().exists(HttpHeaders.LOCATION)
                 .expectBody(Void.class)
                 .returnResult();
+    }
+
+    @DisplayName("지하철 노선 목록을 조회한다")
+    @Test
+    public void retrieveSubwayLines() {
+        // given
+        createSubwayLine("2호선");
+        createSubwayLine("8호선");
+
+        // when, then
+        webTestClient.get()
+                .uri("/subway-lines")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(SubwayLineResponseDto.class)
+                .hasSize(2)
+                .isEqualTo(Arrays.asList(SubwayLineResponseDto.of(SubwayLine.of("2호선")), SubwayLineResponseDto.of(SubwayLine.of("8호선"))));
     }
 
 }
