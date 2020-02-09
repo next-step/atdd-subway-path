da1a1d0bea68506490e8e26f386be1d9de94857f
hyeoni90 <gusk743@gmail.com>
2020-02-09 21:46:46 +0900

    Add station repository

---
+++ src/main/java/atdd/station/repository/StationRepository.java
@@ -0,0 +1,9 @@
+package atdd.station.repository;
+
+import atdd.station.model.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StationRepository extends JpaRepository<Station, Long> {
+}
