48b7fa5ea4b909c1759b970b49e03d539a3dc32f
sanggu <tkdrn8578@naver.com>
2020-02-09 16:14:38 +0900

    [FIXED] Station 을 import 하여 경로에 대한 명시 제거

--- src/test/java/atdd/station/fixture/StationFixture.java
+++ src/test/java/atdd/station/fixture/StationFixture.java
@@ -1,9 +1,11 @@
 package atdd.station.fixture;
 
+import atdd.station.domain.Station;
+
 public class StationFixture {
     public static final String KANGNAM_STATION_NAME = "강남역";
-    public static final String SINSA = "신사역";
+    public static final String SINSA_STATION_NAME = "신사역";
 
-    public static final atdd.station.domain.Station KANNAM_STATON = new atdd.station.domain.Station(KANGNAM_STATION_NAME);
-    public static final atdd.station.domain.Station SINSA_STATION = new atdd.station.domain.Station(SINSA);
+    public static final Station KANNAM_STATON = new Station(KANGNAM_STATION_NAME);
+    public static final Station SINSA_STATION = new Station(SINSA_STATION_NAME);
 }
