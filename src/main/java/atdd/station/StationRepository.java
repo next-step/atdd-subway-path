ac38f73c855f923163e1316edc4f314bd1f399b0
Gyumin Kim <starkying@gmail.com>
2020-02-09 14:35:18 +0900

    Delete deleteByName method (StationRepository)

--- src/main/java/atdd/station/StationRepository.java
+++ src/main/java/atdd/station/StationRepository.java
@@ -1,11 +1,9 @@
 package atdd.station;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,8 +12,4 @@ public interface StationRepository extends JpaRepository<Station, Long> {
 
 	@Query("select s from Station s where s.name = :name")
 	Optional<Station> findByName(@Param("name") String name);
-
-	@Transactional
-	@Modifying
-	void deleteByName(String name);
 }
