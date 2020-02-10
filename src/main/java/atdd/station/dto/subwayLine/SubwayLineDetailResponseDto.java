1e071981a19fcec31423dfb12c6e4e981e693a30
sanggu <tkdrn8578@naver.com>
2020-02-10 22:39:32 +0900

    [MODIFY] SubwayLineDetailResponseDto 의 toDtoEntity 생서

--- src/main/java/atdd/station/dto/subwayLine/SubwayLineDetailResponseDto.java
+++ src/main/java/atdd/station/dto/subwayLine/SubwayLineDetailResponseDto.java
@@ -1,31 +1,40 @@
 package atdd.station.dto.subwayLine;
 
 import atdd.station.domain.Station;
+import atdd.station.domain.SubwayLine;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class SubwayLineDetailResponseDto {
     private String name;
+    private String startTime;
+    private String endTime;
+    private String intervalTime;
+    private List<Station> stations;
 
     @Builder
-    public SubwayLineDetailResponseDto(String name) {
+    public SubwayLineDetailResponseDto(String name, String startTime, String endTime, String intervalTime, List<Station> stations) {
         this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.intervalTime = intervalTime;
+        this.stations = stations;
     }
 
-    public Station toEntity() {
-        return Station.builder()
-                .name(this.name)
-                .build();
-    }
-
-    public static SubwayLineDetailResponseDto toDtoEntity(Station station) {
+    public static SubwayLineDetailResponseDto toDtoEntity(SubwayLine subwayLine) {
         return SubwayLineDetailResponseDto.builder()
-                .name(station.getName())
+                .name(subwayLine.getName())
+                .startTime(subwayLine.getStartTime())
+                .endTime(subwayLine.getEndTime())
+                .intervalTime(subwayLine.getIntervalTime())
+                .stations(subwayLine.getStations())
                 .build();
     }
 }
