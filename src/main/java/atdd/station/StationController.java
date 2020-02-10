67c3ac9dcb70bdad54d376852199e991c7f2f519
DKKang <akwdmfxu@hanmail.net>
2020-02-11 01:34:33 +0900

    테스트케이스 작성

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -2,21 +2,40 @@ package atdd.station;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Map;
 
 @RestController
+@RequestMapping(value = "/stations", produces = "application/json")
 public class StationController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StationController.class);
 
-    @PostMapping(value = "/stations")
-    public ResponseEntity createStation() {
-        return ResponseEntity.created(URI.create("/station/1")).build();
+    @PostMapping("")
+    public ResponseEntity<String> createStation(@RequestBody Map<String, String> param) {
+
+        String stationName = param.get("name");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Location", stationName);
+
+        return ResponseEntity.created(URI.create("/stations/" + stationName)).headers(httpHeaders).body("{\"name\":\""+stationName+"\"}");
+    }
+
+    @GetMapping("/{stationName}")
+    public ResponseEntity<String> getStation(@PathVariable String stationName) {
+
+        try {
+            return ResponseEntity.ok().body("{\"name\":\""+stationName+"\"}");
+        } catch (EmptyResultDataAccessException e) {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 
 }
--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -5,29 +5,108 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebClient
+@AutoConfigureWebTestClient
 public class StationAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
 
     @Autowired
     private WebTestClient webTestClient;
 
+    /**
+     * 지하철 정보
+     * {
+     *     "id":1,
+     *     "name":"강남역"
+     * }
+     */
+
+    /**
+     * Scenario: 지하철역 등록
+     * When 관리자는 "강남역" 지하철역 등록을 요청한다.
+     * Then "강남역" 지하철역이 등록된다.
+     */
     @Test
-    public void test() {
-        String stationName = "강남역";
+    public void testCreateStation() {
+
+        createStation("강남역");
+
+    }
+
+    /**
+     * Scenario: 지하철역 목록 조회
+     * Given "강남역" 지하철역이 등록되어 있다.
+     * When 사용자는 지하철역 목록조회를 요청한다.
+     * Then 사용자는 "강남역" 지하철역의 정보를 응답받는다.
+     */
+    @Test
+    public void testGetStation() {
+
+        createStation("강남역");
+
+        webTestClient.get().uri("/stations/강남역")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody().jsonPath("$.name").isEqualTo("강남역");
+    }
+
+    /**
+     * Scenario: 지하철역 정보 조회
+     * Given "강남역" 지하철역이 등록되어 있다.
+     * When 사용자는 "강남역" 지하철역의 정보 조회를 요청한다.
+     * Then 사용자는 "강남역" 지하철역의 정보를 응답받는다.
+     */
+
+    @Test
+    public void testGetStationInfo() {
+
+        createStation("강남역");
+
+        webTestClient.get().uri("/stations/info/강남역")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody().jsonPath("$.name").isEqualTo("강남역");
+    }
+
+    /**
+     * Scenario: 지하철역 삭제
+     * Given "강남역" 지하철역이 등록되어 있다.
+     * When 관리자는 "강남역" 지하철역 삭제를 요청한다.
+     * Then "강남역" 지하철역이 삭제되었다.
+     */
+    @Test
+    public void testDeleteStaion() {
+
+        createStation("강남역");
+
+        webTestClient.delete().uri("/stations/강남역")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    /**
+     * 공통) 지하철역 등록
+     */
+    public void createStation(String stationName) {
+
         String inputJson = "{\"name\":\""+stationName+"\"}";
 
         webTestClient.post().uri("/stations")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(inputJson), String.class)
                 .exchange()
-                .expectStatus().isCreated();
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON);
     }
-
 }
+
