ca91606a76065c6bcd72fe67e9bcd228982df20c
sanggu <tkdrn8578@naver.com>
2020-02-10 22:40:47 +0900

    [MODIFY] findById 하여 optional 을 체크하여 SubwayLine 을 리턴하는 findById 추가

--- src/main/java/atdd/station/service/SubwayLineService.java
+++ src/main/java/atdd/station/service/SubwayLineService.java
@@ -3,6 +3,7 @@ package atdd.station.service;
 import atdd.station.domain.SubwayLine;
 import atdd.station.domain.SubwayLineRepository;
 import atdd.station.dto.subwayLine.SubwayLineCreateRequestDto;
+import atdd.station.dto.subwayLine.SubwayLineDetailResponseDto;
 import atdd.station.dto.subwayLine.SubwayLineListResponseDto;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,11 @@ public class SubwayLineService {
         return SubwayLineListResponseDto.toDtoEntity(subwayLineRepository.findAll());
     }
 
-    public SubwayLine detail(long id) {
+    public SubwayLine findById(long id) {
         return subwayLineRepository.findById(id).orElseThrow(IllegalAccessError::new);
+    }
+
+    public SubwayLineDetailResponseDto detail(long id) {
+        return SubwayLineDetailResponseDto.toDtoEntity(findById(id));
     }
 }
