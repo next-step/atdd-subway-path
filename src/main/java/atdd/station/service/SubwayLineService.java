df00938b8d8075dcf5db789c88cbbced45d1f562
sanggu <tkdrn8578@naver.com>
2020-02-10 22:34:45 +0900

    [MODIFY] subwayLine 의 list 응답값을 SubwayLineResponseDto 로 변겨

--- src/main/java/atdd/station/service/SubwayLineService.java
+++ src/main/java/atdd/station/service/SubwayLineService.java
@@ -3,10 +3,10 @@ package atdd.station.service;
 import atdd.station.domain.SubwayLine;
 import atdd.station.domain.SubwayLineRepository;
 import atdd.station.dto.subwayLine.SubwayLineCreateRequestDto;
+import atdd.station.dto.subwayLine.SubwayLineListResponseDto;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @Service("subwayLineService")
 public class SubwayLineService {
@@ -17,8 +17,8 @@ public class SubwayLineService {
         return subwayLineRepository.save(subwayLine.toEntity());
     }
 
-    public List<SubwayLine> list() {
-        return subwayLineRepository.findAll();
+    public SubwayLineListResponseDto list() {
+        return SubwayLineListResponseDto.toDtoEntity(subwayLineRepository.findAll());
     }
 
     public SubwayLine detail(long id) {
