180ce810873859419aeca8df4a53490ae43044c1
Gyumin Kim <starkying@gmail.com>
2020-02-10 22:06:16 +0900

    Remove JPQL from StationRepository

--- src/main/java/atdd/station/StationRepository.java
+++ src/main/java/atdd/station/StationRepository.java
@@ -1,8 +1,6 @@
 package atdd.station;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +8,5 @@ import java.util.Optional;
 @Repository
 public interface StationRepository extends JpaRepository<Station, Long> {
 
-	@Query("select s from Station s where s.name = :name")
-	Optional<Station> findByName(@Param("name") String name);
+	Optional<Station> findByName(String name);
 }
