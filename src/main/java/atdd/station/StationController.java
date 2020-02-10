190463faf6c84e059817c3ea998ee8e8fba17643
chunbong <cdy0266@gmail.com>
2020-02-10 21:11:58 +0900

    feat(stations): create station registration request

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -1,8 +1,21 @@
 package atdd.station;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/stations")
 public class StationController {
 
+    @PostMapping("")
+    public ResponseEntity<String> create() {
+
+        return ResponseEntity.created(URI.create("/stations")).build();
+    }
 }
