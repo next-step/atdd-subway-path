e0158c6216c117f8382475b46cb77d3330b77eaf
sanggu <tkdrn8578@naver.com>
2020-02-10 22:11:50 +0900

    [ADD] subwayLine list service 에 대한 테스트코드 추가

--- src/test/java/atdd/station/service/SubwayLineServiceTest.java
+++ src/test/java/atdd/station/service/SubwayLineServiceTest.java
@@ -11,6 +11,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -40,4 +43,33 @@ public class SubwayLineServiceTest {
         softly.assertThat(createSubwayLine.getId()).isEqualTo(0L);
         softly.assertThat(createSubwayLine.getName()).isEqualTo("2호선");
     }
+
+    @Test
+    public void 지하철노선_list_조회가_성공하는지(SoftAssertions softly) {
+        //given
+        List<SubwayLine> subwayLines = new ArrayList<>();
+
+        SubwayLine secondSubwayLine = SubwayLine.builder()
+                .name("2호선")
+                .build();
+
+        SubwayLine firstSubwayLine = SubwayLine.builder()
+                .name("1호선")
+                .build();
+
+        subwayLines.add(secondSubwayLine);
+        subwayLines.add(firstSubwayLine);
+
+        //when
+        when(subwayLineRepository.findAll()).thenReturn(subwayLines);
+
+        List<SubwayLine> listedSubwayLines = subwayLineService.list();
+
+        //then
+        softly.assertThat(listedSubwayLines).isNotNull();
+        softly.assertThat(listedSubwayLines.size()).isGreaterThan(1);
+        softly.assertThat(listedSubwayLines.toString()).contains("2호선");
+        softly.assertThat(listedSubwayLines.toString()).contains("1호선");
+    }
+
 }
