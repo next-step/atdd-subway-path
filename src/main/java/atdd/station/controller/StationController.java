f1841c7334f33386f1c387a0d86a8c470c5600fe
yhs <mvmaniaz@gmail.com>
2020-02-11 21:27:33 +0900

    feat(station): refactor controller, add global exception

--- src/main/java/atdd/station/controller/StationController.java
+++ src/main/java/atdd/station/controller/StationController.java
@@ -1,5 +1,6 @@
 package atdd.station.controller;
 
+import atdd.global.exception.ServiceNotFoundException;
 import atdd.station.api.request.CreateStationRequestView;
 import atdd.station.api.response.StationListResponseView;
 import atdd.station.api.response.StationResponseView;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.stream.Collectors.toList;
 
@@ -28,7 +30,7 @@ public class StationController {
         final Station persistStation = stationRepository.save(station);
 
         return ResponseEntity
-                .created(URI.create(request.getServletPath() +"/"+ persistStation.getId()))
+                .created(URI.create(request.getServletPath() + "/" + persistStation.getId()))
                 .body(new StationResponseView(persistStation));
     }
 
@@ -41,21 +43,22 @@ public class StationController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<StationResponseView> getStationById(@PathVariable("id") Long id) {
-        return stationRepository.findById(id)
-                .map(station -> ResponseEntity.ok(new StationResponseView(station)))
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<StationResponseView> getStation(@PathVariable("id") Long id) {
+        final Station station = getStationById(id);
+        return ResponseEntity.ok(new StationResponseView(station));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteStation(@PathVariable("id") Long id) {
+        final Station station = getStationById(id);
+        stationRepository.deleteById(station.getId());
+
+        return ResponseEntity.noContent().build();
+    }
+
+    private Station getStationById(Long id) {
         return stationRepository.findById(id)
-                .map(
-                    station -> {
-                        stationRepository.deleteById(station.getId());
-                        return ResponseEntity.noContent().build();
-                })
-                .orElseGet(() -> ResponseEntity.badRequest().build());
+                .orElseThrow(() -> new ServiceNotFoundException("지하철 역이 존재하지 않습니다.", Map.of("id", id)));
     }
 
 }
