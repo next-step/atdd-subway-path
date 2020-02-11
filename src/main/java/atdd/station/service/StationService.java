d5aaa2ddaf9b421cadd8d29a06f8ddb1f61038ee
sanggu <tkdrn8578@naver.com>
2020-02-11 22:26:36 +0900

    [MODIFY] Station 의 service response 값을 StationCreateResponseDto 로 변경

--- src/main/java/atdd/station/service/StationService.java
+++ src/main/java/atdd/station/service/StationService.java
@@ -3,6 +3,7 @@ package atdd.station.service;
 import atdd.station.domain.Station;
 import atdd.station.domain.StationRepository;
 import atdd.station.dto.station.StationCreateRequestDto;
+import atdd.station.dto.station.StationCreateResponseDto;
 import atdd.station.dto.station.StationDetailResponseDto;
 import atdd.station.dto.station.StationListResponseDto;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,8 @@ public class StationService {
     @Resource(name = "stationRepository")
     private StationRepository stationRepository;
 
-    public Station create(StationCreateRequestDto station) {
-        return stationRepository.save(station.toEntity());
+    public StationCreateResponseDto create(StationCreateRequestDto station) {
+        return StationCreateResponseDto.toDtoEntity(stationRepository.save(station.toEntity()));
     }
 
     public StationListResponseDto list() {
