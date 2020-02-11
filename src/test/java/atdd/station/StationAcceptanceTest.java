2d3310a2cb91c3af57a1c597aa595c34954b8b32
Hyunjin Choi <hyeonisism@gmail.com>
2020-02-12 06:41:08 +0900

    Test createStation() with @Nested

--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -1,8 +1,8 @@
 package atdd.station;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,29 +13,48 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
+
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
+@DisplayName("Station Controller 를 테스트한다")
 public class StationAcceptanceTest {
 
     @Autowired
     private WebTestClient webTestClient;
 
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
-    
-    @Test
-    public void createStation() {
-        String stationName = "강남역";
-        String inputJson = "{\"name\":\"" + stationName + "\"}";
 
-        webTestClient.post().uri("/stations")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(inputJson), String.class)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectHeader().exists("Location")
-                .expectBody().jsonPath("$.name").isEqualTo(stationName);
+
+    @Nested
+    @DisplayName("역을 생성하는 API")
+    class CreateStation{
+
+        @Nested
+        @DisplayName("역 이름이 주어진다면")
+        class GivenStationName{
+
+
+            final String stationName = "강남역";
+            final String inputJson = "{\"name\":\"" + stationName + "\"}";
+
+            @Test
+            @DisplayName("생성된 역을 리턴한다")
+            void expectCreateStation(){
+                webTestClient.post().uri("/stations")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(Mono.just(inputJson), String.class)
+                        .exchange()
+                        .expectStatus().isCreated()
+                        .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                        .expectHeader().exists("Location")
+                        .expectBody().jsonPath("$.name").isEqualTo(stationName);
+            }
+
+        }
+
     }
 
     @Test
@@ -44,6 +63,7 @@ public class StationAcceptanceTest {
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody().jsonPath("$.name").isEqualTo("강남역");
+                .expectBody(List.class)
+                .returnResult();
     }
 }
