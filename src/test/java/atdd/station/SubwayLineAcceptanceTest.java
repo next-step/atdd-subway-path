ac09508b1edfdb3523506fc902c96f051e572ea1
hongjongwan <serverwizard@naver.com>
2020-02-12 00:10:03 +0900

    [feat] 지하철 노선 관리, ATDD 코드 작성

--- src/test/java/atdd/station/SubwayLineAcceptanceTest.java
+++ src/test/java/atdd/station/SubwayLineAcceptanceTest.java
@@ -1,58 +1,29 @@
 package atdd.station;
 
+import atdd.station.application.dto.SubwayLineResponseDto;
 import atdd.station.domain.SubwayLine;
-import atdd.station.domain.SubwayLineRepository;
-import atdd.station.web.dto.SubwayLineResponseDto;
-import org.junit.jupiter.api.AfterEach;
+import atdd.support.SubwayAcceptanceTestSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 
-@AutoConfigureWebTestClient
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SubwayLineAcceptanceTest {
-    @Autowired
-    private WebTestClient webTestClient;
+import static org.assertj.core.api.Assertions.assertThat;
 
-    @Autowired
-    private SubwayLineRepository subwayLineRepository;
+public class SubwayLineAcceptanceTest extends SubwayAcceptanceTestSupport {
 
-    @DisplayName("지하철역 노선을 등록한다")
+    @DisplayName("지하철 노선을 등록한다")
     @Test
     public void create() {
         // expect
         createSubwayLine("2호선");
     }
 
-    private EntityExchangeResult<Void> createSubwayLine(String subwayLineName) {
-        // given
-        String inputJson = "{\"name\":\"" + subwayLineName + "\"}";
-
-        return webTestClient.post()
-                .uri("/subway-lines")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(inputJson), String.class)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectHeader().exists(HttpHeaders.LOCATION)
-                .expectBody(Void.class)
-                .returnResult();
-    }
-
-    @DisplayName("지하철 노선 목록을 조회한다")
+    @DisplayName("지하철 노선 목록을 조회한다.")
     @Test
-    public void retrieveSubwayLines() {
+    public void getSubwayLines() {
         // given
         createSubwayLine("2호선");
         createSubwayLine("8호선");
@@ -69,55 +40,41 @@ public class SubwayLineAcceptanceTest {
                 .isEqualTo(Arrays.asList(SubwayLineResponseDto.of(SubwayLine.of("2호선")), SubwayLineResponseDto.of(SubwayLine.of("8호선"))));
     }
 
-    @DisplayName("지하철역 노선 정보를 조회한다")
-    @ParameterizedTest
-    @ValueSource(strings = {"2호선", "8호선", "5호선"})
-    public void retrieveFromSubwayLineName(String subwayLineName) {
+    @DisplayName("지하철 노선 정보를 조회한다.")
+    @Test
+    public void retrieveFromSubwayLineName() {
         // given
+        String subwayLineName = "2호선";
         EntityExchangeResult<Void> createdResult = createSubwayLine(subwayLineName);
+        String locationPath = createdResult.getResponseHeaders().getLocation().getPath();
 
-        // when, then
-        getFromSubwayLineName(subwayLineName, createdResult);
+        // when
+        EntityExchangeResult<SubwayLineResponseDto> result = getSubwayLineFromLocationPath(locationPath);
+
+        // then
+        assertThat(result.getResponseBody().getName()).isEqualTo(subwayLineName);
     }
 
-    private void getFromSubwayLineName(String subwayLineName,
-                                       EntityExchangeResult<Void> createdResult) {
-
-        webTestClient.get()
-                .uri(createdResult.getResponseHeaders().getLocation().getPath())
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody(SubwayLineResponseDto.class)
-                .isEqualTo(SubwayLineResponseDto.of(SubwayLine.of(subwayLineName)));
-    }
 
     @DisplayName("지하철 노선을 삭제한다")
     @Test
     public void deleteFromSubwayLineName() {
         // given
-        String subwayLineName = "2호선";
-        EntityExchangeResult<Void> createdResult = createSubwayLine(subwayLineName);
+        EntityExchangeResult<Void> createdResult = createSubwayLine("2호선");
+        String locationPath = createdResult.getResponseHeaders().getLocation().getPath();
 
         // when
         webTestClient.delete()
-                .uri(createdResult.getResponseHeaders().getLocation().getPath())
-                .accept(MediaType.APPLICATION_JSON)
+                .uri(locationPath)
                 .exchange()
                 .expectStatus().isOk();
 
         // then
         webTestClient.get()
-                .uri(createdResult.getResponseHeaders().getLocation().getPath())
+                .uri(locationPath)
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
-                .expectStatus().isBadRequest();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        subwayLineRepository.deleteAll();
+                .expectStatus().isNoContent();
     }
 
 }
