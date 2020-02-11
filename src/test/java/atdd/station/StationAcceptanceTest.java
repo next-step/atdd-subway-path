a1996e6ebfd858b56a9c5125568f0ec26644703f
seul <eeseul.p@gmail.com>
2020-02-12 04:00:40 +0900

    Step1 (#40)
    * feat : add StationRepository
    
    * feat : add createStation api
    
    * feat : add create station test
    
    * feat : add find all stations
    
    * feat : add find all stations test
    
    * fix : find all stations api add HttpHeaders
    
    * feat : add find station by id
    
    * feat : add find station by id test
    
    * fix : createStation api add response headers
    
    * feat : add delete station
    
    * feat : add delete station test
    
    * fix : delete query modify
    
    * fix : delete API noContent return
    
    * fix : @Builder modify
    
    * fix : Station findById add EmptyResultDataAccessException
    
    * fix : add junit
    
    * fix : StationAcceptanceTest modify

--- src/test/java/atdd/station/StationAcceptanceTest.java
+++ src/test/java/atdd/station/StationAcceptanceTest.java
@@ -1,5 +1,10 @@
 package atdd.station;
 
+import atdd.station.model.Station;
+import atdd.station.repository.StationRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,8 +13,11 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,21 +25,134 @@ import reactor.core.publisher.Mono;
 public class StationAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
 
+    private static final ObjectMapper mapper = new ObjectMapper();
+
     @Autowired
     private WebTestClient webTestClient;
 
+    @Autowired
+    private StationRepository repository;
+
     @Test
     public void createStation(){
+        //when
         String stationName = "강남역";
-        String inputJson = "\"{\"name\":\"}\"" + stationName + "\"}";
+        String inputJson = "{\"name\":\"" + stationName + "\"}";
 
-        webTestClient.post().uri("/stations")
+        EntityExchangeResult result = webTestClient.post().uri("/stations")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(inputJson), String.class)
                 .exchange()
                 .expectStatus().isCreated()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectHeader().exists("Location")
-                .expectBody().jsonPath("$.name").isEqualTo(stationName);
+                .expectBody(Station.class).returnResult();
+
+        // then
+        String location = result.getResponseHeaders().getLocation().getPath();
+
+        Station station = (Station) result.getResponseBody();
+
+        String expected = writeValueAsString(station);
+        String actual = writeValueAsString(repository.findById(station.getId()));
+
+        Assert.assertEquals(expected, actual);
+
+        logger.info("createStation location = {}", location);
+    }
+
+    @Test
+    public void findAllStations(){
+        // given
+        createStations();
+
+        // when
+        EntityExchangeResult result = webTestClient.get().uri("/stations")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(List.class).returnResult();
+
+        //then
+        String expected = writeValueAsString(result.getResponseBody());
+        String actual = writeValueAsString(repository.findAll());
+
+        Assert.assertEquals(expected, actual);
+
+        logger.info("findAllStations = {}", expected);
+    }
+
+    @Test
+    public void findStation(){
+        // given
+        createStations();
+
+        // when
+        long stationId = 1;
+
+        EntityExchangeResult result = webTestClient.get().uri("/stations/" + stationId)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(Station.class).returnResult();
+
+        // then
+        String expected = writeValueAsString(result.getResponseBody());
+        String actual = writeValueAsString(repository.findById(stationId));
+
+        Assert.assertEquals(expected, actual);
+
+        logger.info("findStation = {}", expected);
+    }
+
+    @Test
+    public void deleteStation(){
+        // given
+        createStations();
+
+        // when
+        long stationId = 1;
+
+        EntityExchangeResult result = webTestClient.delete().uri("/stations/" + stationId)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNoContent()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody().returnResult();
+
+        // then
+        Station station = repository.findById(stationId);
+
+        Assert.assertEquals(null, station);
+
+        logger.info("deleteStation = {}", station);
+    }
+
+    private void createStations() {
+        createStation("강남역");
+        createStation("삼성역");
+    }
+
+    private void createStation(String name) {
+
+        String inputJson = "{\"name\":\"" + name + "\"}";
+
+        webTestClient.post().uri("/stations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(inputJson), String.class)
+                .exchange();
+    }
+
+    private String writeValueAsString(Object object) {
+        String result = null;
+        try {
+            result = mapper.writeValueAsString(object);
+        }catch (JsonProcessingException e){
+            logger.error("JsonProcessingException", e);
+        }
+
+        return result;
     }
 }
