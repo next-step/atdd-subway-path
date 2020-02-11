cc5c73277e58b3cbcbb7ae48afd70543635544ad
sanggu <tkdrn8578@naver.com>
2020-02-11 22:25:30 +0900

    [MODIFY] Station 내의 List<Subway> 의 이름을 Subway  로 통일

--- src/main/java/atdd/station/dto/station/StationCreateRequestDto.java
+++ src/main/java/atdd/station/dto/station/StationCreateRequestDto.java
@@ -1,32 +1,38 @@
 package atdd.station.dto.station;
 
 import atdd.station.domain.Station;
+import atdd.station.domain.Subway;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class StationCreateRequestDto {
     private String name;
+    private List<Subway> subways;
 
     @Builder
-    public StationCreateRequestDto(String name) {
+    public StationCreateRequestDto(String name, List<Subway> subways) {
         this.name = name;
+        this.subways = subways;
     }
 
     public Station toEntity() {
         return Station.builder()
                 .name(this.name)
+                .subways(this.subways)
                 .build();
     }
 
-    public static StationCreateRequestDto toDtoEntity(String name) {
+    public static StationCreateRequestDto toDtoEntity(String name, List<Subway> subways) {
         return StationCreateRequestDto.builder()
                 .name(name)
+                .subways(subways)
                 .build();
-
     }
 }
