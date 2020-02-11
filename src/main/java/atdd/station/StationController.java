2289185d9ce3f25e92463b90748e0455b92a45ac
Hyunjin Choi <hyeonisism@gmail.com>
2020-02-12 08:04:41 +0900

    Add findStation controller

--- src/main/java/atdd/station/StationController.java
+++ src/main/java/atdd/station/StationController.java
@@ -3,6 +3,8 @@ package atdd.station;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,4 +26,9 @@ public class StationController {
 
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity get(@PathVariable("id") Station station) {
+
+        return ResponseEntity.ok().body(station);
+    }
 }
