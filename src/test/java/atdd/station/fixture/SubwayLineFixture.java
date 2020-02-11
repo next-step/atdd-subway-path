7983813547f05883305dcbec2bb2945de6b90d8c
sanggu <tkdrn8578@naver.com>
2020-02-11 23:44:11 +0900

    [MODIFY] SubwayLine Test fixture 에 detault 값 추가 및 subays 추가

--- src/test/java/atdd/station/fixture/SubwayLineFixture.java
+++ src/test/java/atdd/station/fixture/SubwayLineFixture.java
@@ -1,43 +1,51 @@
 package atdd.station.fixture;
 
-import atdd.station.domain.Station;
 import atdd.station.domain.SubwayLine;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static atdd.station.fixture.StationFixture.*;
+import static atdd.station.fixture.SubwaysFixture.SUBWAYS;
+
 
 public class SubwayLineFixture {
-    public static final String SECOND_SUBWAY_LINE = "2호선";
+    public static final String SECOND_SUBWAY_LINE_NAME = "2호선";
     public static final String FIRST_SUBWAY_LINE = "1호선";
+    public static final String NEW_BUNDANG_LINE_NAME = "신분당선";
     private static final String DEFAULT_START_TIME = "05:00";
     private static final String DEFAULT_END_TIME = "23:50";
     private static final String DEFAULT_INTERVAL = "10";
 
-    public static SubwayLine getSubwayLine(String line) {
-        List<Station> stations = new ArrayList<>();
-        stations.add(KANNAM_STATON);
-        stations.add(SINSA_STATION);
+    public static final SubwayLine SECOND_SUBWAY_LINE = new SubwayLine(SECOND_SUBWAY_LINE_NAME);
+    public static final SubwayLine NEW_BUNDANG_SUBWAY_LINE = new SubwayLine(NEW_BUNDANG_LINE_NAME);
 
+    public static SubwayLine getSubwayLine(String line) {
         return SubwayLine.builder()
                 .name(line)
                 .startTime(DEFAULT_START_TIME)
                 .endTime(DEFAULT_END_TIME)
                 .intervalTime(DEFAULT_INTERVAL)
-                .stations(stations)
+                .subways(SUBWAYS)
                 .build();
     }
 
-    public static SubwayLine getSecondSubwayLine() {
+    public static SubwayLine getSecondSubwayLineName() {
         return SubwayLine.builder()
-                .name(SECOND_SUBWAY_LINE)
+                .name(SECOND_SUBWAY_LINE_NAME)
+                .startTime(DEFAULT_START_TIME)
+                .endTime(DEFAULT_END_TIME)
+                .intervalTime(DEFAULT_INTERVAL)
+                .subways(SUBWAYS)
                 .build();
     }
 
     public static SubwayLine getFirstSubwayLine() {
         return SubwayLine.builder()
                 .name(FIRST_SUBWAY_LINE)
+                .startTime(DEFAULT_START_TIME)
+                .endTime(DEFAULT_END_TIME)
+                .intervalTime(DEFAULT_INTERVAL)
+                .subways(SUBWAYS)
                 .build();
     }
 
@@ -45,7 +53,7 @@ public class SubwayLineFixture {
         List<SubwayLine> subwayLines = new ArrayList<>();
 
         subwayLines.add(getFirstSubwayLine());
-        subwayLines.add(getSecondSubwayLine());
+        subwayLines.add(getSecondSubwayLineName());
 
         return subwayLines;
     }
