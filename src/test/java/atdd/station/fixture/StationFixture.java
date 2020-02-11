6912b00a132c0fae108342a6191be4990b7d1c71
sanggu <tkdrn8578@naver.com>
2020-02-11 22:25:53 +0900

    [MODIFY] 테스트 fixture 의  getStation 메서드 추가

--- src/test/java/atdd/station/fixture/StationFixture.java
+++ src/test/java/atdd/station/fixture/StationFixture.java
@@ -2,10 +2,19 @@ package atdd.station.fixture;
 
 import atdd.station.domain.Station;
 
+import static atdd.station.fixture.SubwaysFixture.SUBWAYS;
+
 public class StationFixture {
     public static final String KANGNAM_STATION_NAME = "강남역";
-    public static final String SINSA_STATION_NAME = "신사역";
+    public static final String PANGYO_STATION_NAME = "판교역";
 
     public static final Station KANNAM_STATON = new Station(KANGNAM_STATION_NAME);
-    public static final Station SINSA_STATION = new Station(SINSA_STATION_NAME);
+    public static final Station PANGYO = new Station(PANGYO_STATION_NAME);
+
+    public static Station getStation(String name) {
+        return Station.builder()
+                .name(name)
+                .subways(SUBWAYS)
+                .build();
+    }
 }
