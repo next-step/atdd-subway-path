b3aa7988347085a536d90c3deefd25f0b2710af8
yukeun <snoopy12oyk@gmail.com>
2020-02-13 00:08:58 +0900

    feat: subway add

--- src/main/java/atdd/station/dto/StationDto.java
+++ src/main/java/atdd/station/dto/StationDto.java
@@ -1,9 +1,12 @@
 package atdd.station.dto;
 
 import atdd.station.domain.Station;
+import atdd.station.domain.Subway;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,9 +14,12 @@ import lombok.Setter;
 public class StationDto
 {
     private String name;
+    private List<Subway> subways;
 
     public Station toEntity()
     {
-       return Station.builder().name(name).build();
+       return Station.builder()
+               .name(name)
+               .build();
     }
 }
