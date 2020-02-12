43f23ef855ec6ff93cc338a55bfb397eb64b6184
yukeun <snoopy12oyk@gmail.com>
2020-02-13 00:03:16 +0900

    feat: 지하철 라인 dto 추가

--- src/main/java/atdd/station/dto/LineDto.java
+++ src/main/java/atdd/station/dto/LineDto.java
@@ -1,4 +1,37 @@
 package atdd.station.dto;
 
-public class LineDto {
+import atdd.station.domain.Line;
+import atdd.station.domain.Subway;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LineDto
+{
+    private String name;
+    private String startTime;
+    private String endTime;
+    private String intervalTime;
+    private List<Subway> subways;
+
+    public Line toEntity()
+    {
+        return Line.builder()
+                .name(name)
+                .subways(subways)
+                .build();
+    }
+
+    public Line toEntity(String name, List<Subway> subways)
+    {
+        return Line.builder()
+                .name(name)
+                .subways(subways)
+                .build();
+    }
 }
