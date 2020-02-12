bcaa99041c1ff264504b11eaf311f3f4ae7724a8
yukeun <snoopy12oyk@gmail.com>
2020-02-13 00:00:51 +0900

    style: 공백 제거

--- src/main/java/atdd/AtddApplication.java
+++ src/main/java/atdd/AtddApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AtddApplication {
+public class AtddApplication
+{
     public static void main(String[] args) {
         SpringApplication.run(AtddApplication.class, args);
-
     }
-
 }
