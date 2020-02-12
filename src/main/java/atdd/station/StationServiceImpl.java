61316975466b8408d9f63812ee03cdafc824e508
Hyunjin Choi <hyeonisism@gmail.com>
2020-02-12 10:45:16 +0900

    Add trasactional annotation to class

--- src/main/java/atdd/station/StationServiceImpl.java
+++ src/main/java/atdd/station/StationServiceImpl.java
@@ -7,25 +7,23 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class StationServiceImpl implements StationService {
 
     private final StationRepository stationRepository;
 
     @Override
-    @Transactional
     public Station create(Station station) {
         return stationRepository.save(station);
     }
 
     @Override
-    @Transactional
     public void delete(Station station) {
         stationRepository.delete(station);
     }
 
     @Override
-    @Transactional
     public List<Station> findAll() {
         return stationRepository.findAll();
     }
