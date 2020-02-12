740445c1bcc1c00967401c442106c2687dff4225
sanggu <tkdrn8578@naver.com>
2020-02-12 21:23:57 +0900

    [ADD] Stations 로 해당 SubwayLine 에 추가된 Stations 이 생성되는지에 대한 테스트코드 추가

--- src/test/java/atdd/station/domain/SubwayLineTest.java
+++ src/test/java/atdd/station/domain/SubwayLineTest.java
@@ -3,6 +3,8 @@ package atdd.station.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static atdd.station.fixture.StationFixture.KANGNAM_AND_YUCKSAM_STATIONS;
 import static atdd.station.fixture.SubwayLineFixture.SECOND_SUBWAY_LINE_NAME;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,16 +27,29 @@ public class SubwayLineTest {
 
     @DisplayName("Subway_에_Stations_를_추가가_성공하는지")
     @Test
-    public void addStationsInSubway() {
+    public void addStationsInSubwayTest() {
         //given
 
         SubwayLine subwayLine = new SubwayLine(SECOND_SUBWAY_LINE_NAME);
 
         //when
-        SubwayLine updatedSubwayLine = subwayLine.addStations(KANGNAM_AND_YUCKSAM_STATIONS);
+        SubwayLine updatedSubwayLine = subwayLine.updateStationsInSubwayLine(KANGNAM_AND_YUCKSAM_STATIONS);
 
         //then
-        assertThat(updatedSubwayLine.getStations().size()).isGreaterThan(2);
+        assertThat(updatedSubwayLine.getStations().size()).isEqualTo(2);
     }
 
+    @DisplayName("Stations_로_해당_subwayLine_에_추가된_것이_만들어지는지")
+    @Test
+    public void makeSubwayByStationSuccessTest() {
+        //given
+
+        SubwayLine subwayLine = new SubwayLine(SECOND_SUBWAY_LINE_NAME);
+
+        //when
+        List<Subway> madeSubways = subwayLine.makeSubwaysByStations(KANGNAM_AND_YUCKSAM_STATIONS);
+
+        //then
+        assertThat(madeSubways.size()).isEqualTo(2);
+    }
 }
