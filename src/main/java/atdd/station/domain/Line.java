f31114da0637019bc8c3c33b96bd67406c6c02a0
yukeun <snoopy12oyk@gmail.com>
2020-02-13 00:02:17 +0900

    feat: 지하철 노선, 지하철 domain 추가

--- src/main/java/atdd/station/domain/Line.java
+++ src/main/java/atdd/station/domain/Line.java
@@ -1,4 +1,50 @@
 package atdd.station.domain;
 
-public class Line {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Line
+{
+    private static final String START_TIME = "05:00";
+    private static final String END_TIME = "23:50";
+    private static final String INTERVAL_TIME = "10";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String startTime;
+
+    @Column(length = 20, nullable = false)
+    private String endTime;
+
+    @Column(length = 20, nullable = false)
+    private String intervalTime;
+
+    @OneToMany(mappedBy = "station")
+    @OrderBy("id ASC")
+    private List<Subway> subways = new ArrayList<>();
+
+    @Builder
+    public Line(String name, List<Subway> subways)
+    {
+        this.name = name;
+        this.startTime = START_TIME;
+        this.endTime = END_TIME;
+        this.intervalTime = INTERVAL_TIME;
+        this.subways = subways;
+    }
+
 }
--- src/main/java/atdd/station/domain/Subway.java
+++ src/main/java/atdd/station/domain/Subway.java
@@ -1,4 +1,29 @@
 package atdd.station.domain;
 
-public class Subway {
+import lombok.Builder;
+
+import javax.persistence.*;
+
+@Entity
+public class Subway
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "station_id")
+    private Station station;
+
+    @ManyToOne
+    @JoinColumn(name = "line_id")
+    private Line line;
+
+    @Builder
+    public Subway(Station station, Line line)
+    {
+        this.station = station;
+        this.line = line;
+    }
+
 }
