e20d0ad8e38d16cd27787a33897d7555a8c4bb9c
yukeun <snoopy12oyk@gmail.com>
2020-02-13 00:04:59 +0900

    feat: line service add

--- src/main/java/atdd/station/service/LineService.java
+++ src/main/java/atdd/station/service/LineService.java
@@ -1,4 +1,41 @@
 package atdd.station.service;
 
-public class LineService {
+import atdd.station.domain.Line;
+import atdd.station.domain.Subway;
+import atdd.station.dto.LineDto;
+import atdd.station.repository.LineRepository;
+import atdd.station.repository.StationRepository;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
+
+@Service("lineService")
+public class LineService
+{
+    @Resource(name = "lineRepository")
+    private LineRepository lineRepository;
+
+    public Line create(LineDto lineDto)
+    {
+        List<Subway> subways = lineDto.getSubways();
+        return lineRepository.save(lineDto.toEntity(lineDto.getName(), subways));
+    }
+
+    public List<Line> findLines()
+    {
+        List<Line> lines = lineRepository.findAll();
+        return lines;
+    }
+
+    public Optional<Line> findById(long id)
+    {
+        return lineRepository.findById(id);
+    }
+
+    public void deleteLineById(long id)
+    {
+        lineRepository.deleteById(id);
+    }
 }
