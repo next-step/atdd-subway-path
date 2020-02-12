3531743bd5b8cb747c2763976839b50ca08bf523
parkeeseul <eeseul.p@gmail.com>
2020-02-12 22:13:26 +0900

    fix : HttpHeaders delete

--- src/main/java/atdd/station/controller/StationController.java
+++ src/main/java/atdd/station/controller/StationController.java
@@ -6,13 +6,10 @@ import atdd.station.repository.StationRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -27,54 +24,35 @@ public class StationController {
     public ResponseEntity<Station> createStation(@RequestBody CreateStationRequestView view) {
         Station station = stationRepository.save(view.toStation());
 
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-
         return ResponseEntity.created(URI.create("/stations/" + station.getId()))
-                .headers(httpHeaders)
                 .body(station);
     }
 
     @GetMapping(value = "/stations")
     @ResponseBody
     public ResponseEntity<List<Station>> findAllStations() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 
         List stations = stationRepository.findAll();
 
         return ResponseEntity.ok()
-                .headers(httpHeaders)
                 .body(stations);
     }
 
     @GetMapping(value = "/stations/{id}")
     @ResponseBody
     public ResponseEntity<Station> findStation(@PathVariable long id) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-
         return ResponseEntity
                 .ok()
-                .headers(httpHeaders)
                 .body(stationRepository.findById(id));
     }
 
     @DeleteMapping(value = "/stations/{id}")
     @ResponseBody
     public ResponseEntity deleteStation(@PathVariable long id) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-
         stationRepository.deleteById(id);
 
         return ResponseEntity
                 .noContent()
-                .headers(httpHeaders)
                 .build();
     }
 }
