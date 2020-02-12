05e59df6bb7f691568197326629c52048154fdd7
parkeeseul <eeseul.p@gmail.com>
2020-02-13 01:38:06 +0900

    fix : modify StationRepository

--- src/main/java/atdd/station/repository/StationRepository.java
+++ src/main/java/atdd/station/repository/StationRepository.java
@@ -1,61 +1,46 @@
 package atdd.station.repository;
 
 import atdd.station.model.Station;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public class StationRepository {
     private final JdbcTemplate jdbcTemplate;
-    private SimpleJdbcInsert simpleJdbcInsert;
-
-    @Autowired
-    public void setDataSource(final DataSource dataSource) {
-        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
-                .withTableName("STATION")
-                .usingGeneratedKeyColumns("ID");
-    }
 
     public StationRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public Station save(Station station) {
-        final Map parameters = new HashMap<>();
-        parameters.put("NAME", station.getName());
-
-        final Long stationId = simpleJdbcInsert.executeAndReturnKey(parameters).longValue();
+    public Optional<Station> save(Station station) {
+        int stationId = jdbcTemplate.update("insert into STATION (name) values (?)", station.getName());
 
         return findById(stationId);
     }
 
-    public Station findById(long id) {
-        try {
-            return jdbcTemplate.queryForObject("select * from STATION where id = ?",
-                    new Object[]{id},
-                    ((rs, rowNum) -> new Station(
-                            rs.getLong("id"),
-                            rs.getString("name"))));
+    public Optional<Station> findById(long id) {
+        RowMapper<Station> rowMapper = ((rs, rowNum) -> new Station(rs.getLong("id"), rs.getString("name")));
 
-        }catch (EmptyResultDataAccessException e){
-            return null;
+        try {
+            return Optional.of(jdbcTemplate.queryForObject("select * from STATION where id = ?",
+                    new Object[]{id},
+                    rowMapper));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.of(null);
         }
     }
 
     public List findAll() {
+        RowMapper<Station> rowMapper = ((rs, rowNum) -> new Station(rs.getLong("id"), rs.getString("name")));
+
         return jdbcTemplate.query(
                 "select * from STATION",
-                (rs, rowNum) -> new Station(
-                        rs.getLong("id"),
-                        rs.getString("name")));
+                rowMapper);
     }
 
     public void deleteById(long id) {
