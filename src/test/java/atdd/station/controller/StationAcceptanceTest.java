7a29366215a9f59f05cf1a2d359691406ca5447d
yukeun <snoopy12oyk@gmail.com>
2020-02-13 00:07:50 +0900

    fix: typo

--- src/test/java/atdd/station/controller/StationAcceptanceTest.java
+++ src/test/java/atdd/station/controller/StationAcceptanceTest.java
@@ -19,10 +19,10 @@ public class StationAcceptanceTest
 {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
 
-    private final String STATIOIN_NAME = "강남역";
-    private final String BASE_URL = "/stations/";
-    private final int TEST_ID = 1;
-    private final String INPUT_JSON = "{\"name\":\""+ STATIOIN_NAME +"\"}";
+    private static final String STATION_NAME = "강남역";
+    private static final String BASE_STATION_URL = "/stations/";
+    private static final int TEST_ID = 1;
+    private static final String INPUT_JSON = "{\"name\":\""+ STATION_NAME +"\"}";
 
     @Autowired
     private WebTestClient webTestClient;
@@ -31,49 +31,49 @@ public class StationAcceptanceTest
     @DisplayName("지하철역 등록")
     public void createStation()
     {
-        createStation(STATIOIN_NAME);
+        createStation(STATION_NAME);
     }
 
     @Test
     @DisplayName("지하철역 목록 조회")
     public void findStations()
     {
-        createStation(STATIOIN_NAME);
+        createStation(STATION_NAME);
 
-        webTestClient.get().uri(BASE_URL)
+        webTestClient.get().uri(BASE_STATION_URL)
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody().jsonPath("$.[0].name").isEqualTo(STATIOIN_NAME);
+                .expectBody().jsonPath("$.[0].name").isEqualTo(STATION_NAME);
     }
 
     @Test
     @DisplayName("지하철역 정보 조회")
     public void detailById()
     {
-        createStation(STATIOIN_NAME);
+        createStation(STATION_NAME);
 
-        webTestClient.get().uri(BASE_URL + "/" + TEST_ID)
+        webTestClient.get().uri(BASE_STATION_URL + TEST_ID)
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody().jsonPath("$.name").isEqualTo(STATIOIN_NAME);
+                .expectBody().jsonPath("$.name").isEqualTo(STATION_NAME);
     }
 
     @Test
     @DisplayName("지하철역 삭제")
     public void deleteStation()
     {
-        createStation(STATIOIN_NAME);
+        createStation(STATION_NAME);
 
-        webTestClient.delete().uri(BASE_URL + "/" + TEST_ID)
+        webTestClient.delete().uri(BASE_STATION_URL + TEST_ID)
                 .exchange()
                 .expectStatus().isOk();
     }
 
     public void createStation(String stationName)
     {
-        webTestClient.post().uri(BASE_URL)
+        webTestClient.post().uri(BASE_STATION_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(INPUT_JSON), String.class)
                 .exchange()
