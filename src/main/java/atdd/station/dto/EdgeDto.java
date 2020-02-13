947e385dd6efbe51d0100ea69ab605774e43a686
yukeun <snoopy12oyk@gmail.com>
2020-02-14 01:59:47 +0900

    feat: Add EdgeDto

--- src/main/java/atdd/station/dto/EdgeDto.java
+++ src/main/java/atdd/station/dto/EdgeDto.java
@@ -1,4 +1,25 @@
 package atdd.station.dto;
 
-public class EdgeDto {
+import atdd.station.domain.Edge;
+import atdd.station.domain.Station;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EdgeDto
+{
+    private Station source;
+    private Station target;
+
+    public Edge toEntity()
+    {
+        return Edge.builder()
+                .source(source)
+                .target(target)
+                .build();
+    }
+
 }
