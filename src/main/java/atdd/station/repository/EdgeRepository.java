ba5ad7eb04d2fd03ceee197aaa78ae05df8fe58c
yukeun <snoopy12oyk@gmail.com>
2020-02-14 01:59:04 +0900

    feat: Add EdgeRepository

--- src/main/java/atdd/station/repository/EdgeRepository.java
+++ src/main/java/atdd/station/repository/EdgeRepository.java
@@ -1,4 +1,9 @@
 package atdd.station.repository;
 
-public class EdgeRepository {
-}
+import atdd.station.domain.Edge;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EdgeRepository extends JpaRepository<Edge, Long> {}
+
