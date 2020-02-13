0dbb32251d5d5eca7b5cdd940c9231406917d804
yukeun <snoopy12oyk@gmail.com>
2020-02-14 01:57:50 +0900

    feat: Add EdgeService

--- src/main/java/atdd/station/service/EdgeService.java
+++ src/main/java/atdd/station/service/EdgeService.java
@@ -1,4 +1,20 @@
 package atdd.station.service;
 
-public class EdgeService {
+import atdd.station.domain.Edge;
+import atdd.station.dto.EdgeDto;
+import atdd.station.repository.EdgeRepository;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service("edgeService")
+public class EdgeService
+{
+    @Resource(name = "edgeRepository")
+    private EdgeRepository edgeRepository;
+
+    public Edge create(EdgeDto edgeDto)
+    {
+        return edgeRepository.save(edgeDto.toEntity());
+    }
 }
