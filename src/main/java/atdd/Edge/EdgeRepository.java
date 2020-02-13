cf1a464455a039715c94ed6d4d909563f15c823a
unknown <dud0701@naver.com>
2020-02-14 00:24:15 +0900

    Add: 구간 repository에 findById 추가

--- src/main/java/atdd/Edge/EdgeRepository.java
+++ src/main/java/atdd/Edge/EdgeRepository.java
@@ -2,5 +2,8 @@ package atdd.Edge;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EdgeRepository extends JpaRepository<Edge, Long> {
+    List<Edge> findByLineId(Long lineId);
 }
