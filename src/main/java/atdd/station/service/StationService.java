7f9389bbdd033ac0741c83788fd013d70a7b36f9
ghchae <cvxgihyun@gmail.com>
2020-02-13 10:41:52 +0900

    @Transactional 애노테이션 클래스레벨로 수정

--- src/main/java/atdd/station/service/StationService.java
+++ src/main/java/atdd/station/service/StationService.java
@@ -10,26 +10,25 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class StationService {
     private final StationRepository stationRepository;
 
-    @Transactional
     public Long create(StationRequestDto stationRequestDto){
         return stationRepository.save(stationRequestDto.toEntity()).getId();
     }
 
-    @Transactional
     public List<Station> select(){
         return stationRepository.findAll();
     }
-    @Transactional
+
     public StationResponseDto findById(Long id){
         Station entity = stationRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
         return new StationResponseDto(entity);
     }
-    @Transactional
+
     public void delete(Long id) {
         Station station = stationRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
         stationRepository.delete(station);
