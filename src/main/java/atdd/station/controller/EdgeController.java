ec5df1fbd6f311fe5a7e9dd3b753726d5e13b7a6
yukeun <snoopy12oyk@gmail.com>
2020-02-14 01:58:33 +0900

    feat: Add EdgeController

--- src/main/java/atdd/station/controller/EdgeController.java
+++ src/main/java/atdd/station/controller/EdgeController.java
@@ -1,4 +1,32 @@
 package atdd.station.controller;
 
-public class EdgeController {
+import atdd.station.domain.Edge;
+import atdd.station.dto.EdgeDto;
+import atdd.station.service.EdgeService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/edges", produces = "application/json")
+public class EdgeController
+{
+    private static final String BASE_EDGE_URL = "/edges";
+
+    @Resource(name = "edgeService")
+    private EdgeService edgeService;
+
+    @RequestMapping("")
+    public ResponseEntity<Edge> createEdges(@RequestBody EdgeDto edgeDto)
+    {
+        Edge createEdge = edgeService.create(edgeDto);
+        return ResponseEntity.created(URI.create(BASE_EDGE_URL + "/" + createEdge.getId()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(createEdge);
+    }
 }
