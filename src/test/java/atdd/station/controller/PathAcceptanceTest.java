28b0cdeb9e8baa5d05b6f7b18572bbad44f40e1d
sanggu <tkdrn8578@naver.com>
2020-02-15 17:25:39 +0900

    [MODIFY] request params 의 형태 오류 수정 및 응답 값 검증 수정

---
+++ src/test/java/atdd/station/controller/PathAcceptanceTest.java
@@ -0,0 +1,40 @@
+package atdd.station.controller;
+
+import atdd.station.AbstractAcceptanceTest;
+import atdd.station.dto.path.PathFindResponseDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PathAcceptanceTest extends AbstractAcceptanceTest {
+    private static final String PATH_API_BASE_URL = "/paths";
+
+    private RestWebClientTest restWebClientTest;
+
+    @BeforeEach
+    void setUp() {
+        this.restWebClientTest = new RestWebClientTest(this.webTestClient);
+    }
+
+    @DisplayName("지하철_간의_최단거리를_구한다")
+    @Test
+    void findShortestPath() {
+        long startStationId = 1L;
+        long endStationId = 5L;
+
+        //when
+        EntityExchangeResult<PathFindResponseDto> expectResponse
+                = restWebClientTest.getMethodAcceptance
+                (PATH_API_BASE_URL + "?startId=" + startStationId + "&endId=" + endStationId, PathFindResponseDto.class);
+
+        PathFindResponseDto path = expectResponse.getResponseBody();
+
+        //then
+        assertThat(path.getStartStationId()).isEqualTo(1L);
+        assertThat(path.getEndStationId()).isEqualTo(5L);
+        assertThat(path.getStations().size()).isEqualTo(4);
+    }
+}
