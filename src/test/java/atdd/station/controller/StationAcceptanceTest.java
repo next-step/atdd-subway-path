3a2b679545d644e7be73e19c601cce4f2951b5e4
sanggu <tkdrn8578@naver.com>
2020-02-15 16:15:00 +0900

    [MODIFY] RestWebClineTest 추가에 따른 테스트코드 변경 및 deleteStation, subway update 경로 변경에 따른 수정

--- src/test/java/atdd/station/controller/StationAcceptanceTest.java
+++ src/test/java/atdd/station/controller/StationAcceptanceTest.java
@@ -1,117 +1,106 @@
 package atdd.station.controller;
 
+import atdd.station.AbstractAcceptanceTest;
 import atdd.station.domain.Station;
+import atdd.station.dto.station.StationDetailResponseDto;
+import atdd.station.dto.station.StationListResponseDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 
+import java.util.List;
 import java.util.Objects;
 
 import static atdd.station.fixture.StationFixture.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
-public class StationAcceptanceTest {
+public class StationAcceptanceTest extends AbstractAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
 
     public static final String KANGNAM_STATION_JSON = "{\"name\": \"" + KANGNAM_STATION_NAME + "\"}";
-    private static final String NAME_JSON_PARSE_EXPRESSION = "$.name";
-    private static final String ID_JSON_PARSE_EXPRESSION = "$.id";
     private static final String STATION_API_BASE_URL = "/stations/";
-    private static final String LOCATION_HEADER = "Location";
 
-    @Autowired
-    private WebTestClient webTestClient;
+    private RestWebClientTest restWebClientTest;
+
+    @BeforeEach
+    void setUp() {
+        this.restWebClientTest = new RestWebClientTest(this.webTestClient);
+    }
 
     @DisplayName("강남역_지하철_등록을_요청이_성공하는지")
     @ParameterizedTest
     @ValueSource(strings = {KANGNAM_STATION_NAME, PANGYO_STATION_NAME})
     void createStationSuccessTest(String stationName) {
         //when
+        EntityExchangeResult<Station> expectResponse
+                = restWebClientTest.postMethodAcceptance(STATION_API_BASE_URL, getStation(stationName), Station.class);
+
         //then
-        webTestClient.post().uri(STATION_API_BASE_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(getStation(stationName)), Station.class)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectHeader().exists(LOCATION_HEADER)
-                .expectBody().jsonPath(NAME_JSON_PARSE_EXPRESSION).isEqualTo(stationName)
-                .jsonPath(ID_JSON_PARSE_EXPRESSION).isEqualTo("1")
-                .jsonPath("$.subwayLines[0].name").isEqualTo("2호선");
+        HttpHeaders responseHeaders = expectResponse.getResponseHeaders();
+        assertThat(responseHeaders.getLocation()).isNotNull();
+        assertThat(expectResponse.getResponseBody().getName()).isEqualTo(stationName);
     }
 
     @DisplayName("강남역_지하철이_조회가_성공하는지")
     @Test
     void listStationSuccessTest() {
         //given
-        creatStation(KANGNAM_STATION_NAME);
-        creatStation(PANGYO_STATION_NAME);
+        getLocationPathToCreateStation(KANGNAM_STATION_NAME);
+        getLocationPathToCreateStation(PANGYO_STATION_NAME);
 
         //when
-        //then
-        webTestClient.get().uri(STATION_API_BASE_URL)
-                .exchange()
-                .expectStatus().isOk()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody().jsonPath(getStationNameJsonParseExpressionByIndex("0")).isEqualTo(KANGNAM_STATION_NAME)
-                .jsonPath(getStationNameJsonParseExpressionByIndex("1")).isEqualTo(PANGYO_STATION_NAME);
-    }
 
+        EntityExchangeResult<StationListResponseDto> expectResponse
+                = restWebClientTest.getMethodAcceptance(STATION_API_BASE_URL, StationListResponseDto.class);
+
+        StationListResponseDto responseBody = expectResponse.getResponseBody();
+        List<Station> stations = Objects.requireNonNull(responseBody).getStations();
+
+        //then
+        assertThat(expectResponse.getStatus()).isEqualTo(HttpStatus.OK);
+        assertThat(stations.get(0).getName()).isEqualTo(KANGNAM_STATION_NAME);
+        assertThat(stations.get(1).getName()).isEqualTo(PANGYO_STATION_NAME);
+    }
 
     @DisplayName("강남역_지하철_역_정보_상세조회_요청이_성공하는지")
     @Test
     void stationDetailSuccessTest() {
         //given
-        String location = creatStation(KANGNAM_STATION_NAME);
+        String location = getLocationPathToCreateStation(KANGNAM_STATION_NAME);
 
         //when
+        EntityExchangeResult<StationDetailResponseDto> expectResponse
+                = restWebClientTest.getMethodAcceptance(location, StationDetailResponseDto.class);
         //then
-        webTestClient.get().uri(location)
-                .exchange()
-                .expectStatus().isOk()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody().jsonPath(NAME_JSON_PARSE_EXPRESSION).isEqualTo(KANGNAM_STATION_NAME);
+        assertThat(expectResponse.getStatus()).isEqualTo(HttpStatus.OK);
+        assertThat(expectResponse.getResponseBody().getName()).isEqualTo(KANGNAM_STATION_NAME);
     }
 
 
     @DisplayName("강남역_지하철_역_정보_삭제_요청이_성공하는지")
     @Test
     void stationDeleteSuccessTest() {
-        String location = creatStation(KANGNAM_STATION_NAME);
+        String location = getLocationPathToCreateStation(KANGNAM_STATION_NAME);
 
         //when
-        webTestClient.delete().uri(location)
-                .exchange()
-                .expectStatus().isOk();
+        EntityExchangeResult expectResponse = restWebClientTest.deleteMethodAcceptance(location);
+
+        //then
+        assertThat(expectResponse.getStatus()).isEqualTo(HttpStatus.OK);
     }
 
-    private String creatStation(String name) {
-        return Objects.requireNonNull(webTestClient.post().uri(STATION_API_BASE_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(getStation(name)), Station.class)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectHeader().exists(LOCATION_HEADER)
-                .expectBody().jsonPath(NAME_JSON_PARSE_EXPRESSION).isEqualTo(name)
-                .returnResult()
+    private String getLocationPathToCreateStation(String name) {
+        return Objects.requireNonNull(
+                restWebClientTest.postMethodAcceptance(STATION_API_BASE_URL, getStation(name), Station.class)
                 .getResponseHeaders()
-                .getLocation()).getPath();
-    }
-
-    private String getStationNameJsonParseExpressionByIndex(String index) {
-        return "$.stations[" + index + "].name";
+                .getLocation())
+                .getPath();
     }
 }
