da2604fea5b79a212424a0173df31a9ee8824ee9
sanggu <tkdrn8578@naver.com>
2020-02-15 16:15:16 +0900

    [MODIFY] KANGNAME FIXTURE 에 대한 이름 오타 수정

--- src/test/java/atdd/station/fixture/StationFixture.java
+++ src/test/java/atdd/station/fixture/StationFixture.java
@@ -14,11 +14,11 @@ public class StationFixture {
     public static final String SUNLENG_STATION_NAME = "선릉역";
     public static final String PANGYO_STATION_NAME = "판교역";
 
-    public static final Station KANNAM_STATON = new Station(KANGNAM_STATION_NAME);
+    public static final Station KANGNAM_STATON = new Station(KANGNAM_STATION_NAME);
     public static final Station YUCKSAM_STATON = new Station(YUCKSAM_STATION_NAME);
     public static final Station SUNLENG_STATON = new Station(SUNLENG_STATION_NAME);
 
-    public static final List<Station> KANGNAM_AND_YUCKSAM_STATIONS = new ArrayList<>(Arrays.asList(KANNAM_STATON, YUCKSAM_STATON));
+    public static final List<Station> KANGNAM_AND_YUCKSAM_STATIONS = new ArrayList<>(Arrays.asList(KANGNAM_STATON, YUCKSAM_STATON));
 
     public static final Station PANGYO = new Station(PANGYO_STATION_NAME);
 
