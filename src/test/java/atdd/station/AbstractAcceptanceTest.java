cf86cc2eb363b60ef22478f57afb70e10b7a23f2
sanggu <tkdrn8578@naver.com>
2020-02-15 15:10:05 +0900

    [ADD] 여러개의 assertThat 을 동시에 검증하기 위한 SoftAssertionExtension 추가

--- src/test/java/atdd/station/AbstractAcceptanceTest.java
+++ src/test/java/atdd/station/AbstractAcceptanceTest.java
@@ -1,5 +1,7 @@
 package atdd.station;
 
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +11,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient(timeout = "100000")
+@ExtendWith(SoftAssertionsExtension.class)
 public class AbstractAcceptanceTest {
     @Autowired
     protected WebTestClient webTestClient;
