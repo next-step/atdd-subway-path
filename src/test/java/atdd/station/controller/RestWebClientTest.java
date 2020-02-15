5a918da2ec5c8cccd2803d440e53f2e663835e79
sanggu <tkdrn8578@naver.com>
2020-02-15 15:11:45 +0900

    [ADD] Acceptance 테스트에 대한 request 중복을 제거하기 위한 REST Test Class 생성
     * post, get, delete 에 대한 Acceptance Test 추가

---
+++ src/test/java/atdd/station/controller/RestWebClientTest.java
@@ -0,0 +1,41 @@
+package atdd.station.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+public class RestWebClientTest {
+    private WebTestClient webTestClient;
+
+    public RestWebClientTest(WebTestClient webTestClient) {
+        this.webTestClient = webTestClient;
+    }
+
+    <T> EntityExchangeResult<T> postMethodAcceptance(String uri, Object requestBody, Class<T> bodyClass) {
+        return webTestClient.post().uri(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(requestBody), requestBody.getClass())
+                .exchange()
+                .expectBody(bodyClass)
+                .returnResult();
+    }
+
+
+    <T> EntityExchangeResult<T> getMethodAcceptance(String uri, Class<T> bodyClass) {
+        return this.webTestClient.get().uri(uri)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(bodyClass)
+                .returnResult();
+    }
+
+    <T> EntityExchangeResult<T> deleteMethodAcceptance(String uri, Class<T> bodyClass) {
+        return this.webTestClient.delete().uri(uri)
+                .exchange()
+                .expectBody(bodyClass)
+                .returnResult();
+    }
+
+}
