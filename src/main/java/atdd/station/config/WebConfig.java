eae9a71bbd9d27006efb0d2fca71bafe9dfcbaa5
hongjongwan <serverwizard@naver.com>
2020-02-17 01:03:12 +0900

    [feat] ETag를 위한 필터 추가

---
+++ src/main/java/atdd/station/config/WebConfig.java
@@ -0,0 +1,17 @@
+package atdd.station.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<>( new ShallowEtagHeaderFilter());
+        filterRegistrationBean.addUrlPatterns("/stations", "/subway-lines");
+        filterRegistrationBean.setName("etagFilter");
+        return filterRegistrationBean;
+    }
+}
