e33ce2ac819989cd96467903c0f7581b09dd943a
sanggu <tkdrn8578@naver.com>
2020-02-17 22:32:19 +0900

    [MODIFY] 최단경로 구하는 테스트가 성공하기 위한 fixture 수정

--- src/test/java/atdd/station/domain/GraphTest.java
+++ src/test/java/atdd/station/domain/GraphTest.java
@@ -3,10 +3,11 @@ package atdd.station.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
-import static atdd.station.fixture.StationFixture.KANGNAM_STATION;
-import static atdd.station.fixture.StationFixture.YUCKSAM_STATON;
+import static atdd.station.fixture.StationFixture.*;
+import static atdd.station.fixture.SubwayLineFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GraphTest {
@@ -16,14 +17,14 @@ public class GraphTest {
     public void  createSubwayGraph() {
         //given
         long startId = 0L;
-        long endId = 5L;
-        Graph graph = new Graph;
+        long endId = 3L;
+        Graph graph = new Graph(Arrays.asList(getSecondSubwayLineName(), getFirstSubwayLineName()));
 
         //when
         List<Station> result = graph.getShortestDistancePath(startId, endId);
 
         //then
         assertThat(result.get(0)).isEqualTo(KANGNAM_STATION);
-        assertThat(result.get(1)).isEqualTo(YUCKSAM_STATON);
+        assertThat(result.get(1)).isEqualTo(SUNLENG_STATON);
     }
 }
