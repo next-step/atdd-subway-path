fd134bd745e797004189b275fa543cc6aaf3c6bf
parkeeseul <eeseul.p@gmail.com>
2020-03-01 16:51:27 +0900

    refactor : LineDtoAssembler 리팩토링

--- src/main/java/atdd/station/model/dto/LineDtoAssembler.java
+++ src/main/java/atdd/station/model/dto/LineDtoAssembler.java
@@ -6,23 +6,44 @@ import atdd.station.model.entity.Station;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LineDtoAssembler {
-    public static LineResponseDto assemble(final Line line, final List<Edge> newEdges, final List<Station> stations) {
+    private Map<Long, Station> stationMap;
+    private List<Edge> newEdges;
+
+    private LineDtoAssembler(Map<Long, Station> stationMap, List<Edge> newEdges) {
+        this.stationMap = stationMap;
+        this.newEdges = newEdges;
+    }
+
+    private List<StationSimpleDto> getStationSimpleDtoList() {
         List<StationSimpleDto> stationSimpleDtos = new ArrayList<>();
 
-        newEdges.forEach(edgeData -> {
+        for (Edge newEdge : newEdges) {
             if (stationSimpleDtos.isEmpty()) {
-                Station sourceStation = stations.stream().filter(stationData -> stationData.getId() == edgeData.getSourceStationId()).findAny().get();
-                stationSimpleDtos.add(sourceStation.toSimpleDto());
+                stationSimpleDtos.add(findStationSimpleDto(newEdge.getSourceStationId()));
+                stationSimpleDtos.add(findStationSimpleDto(newEdge.getTargetStationId()));
 
-                Station targetStation = stations.stream().filter(stationData -> stationData.getId() == edgeData.getTargetStationId()).findAny().get();
-                stationSimpleDtos.add(targetStation.toSimpleDto());
-            } else {
-                Station targetStation = stations.stream().filter(stationData -> stationData.getId() == edgeData.getTargetStationId()).findAny().get();
-                stationSimpleDtos.add(targetStation.toSimpleDto());
+                continue;
             }
-        });
+
+            stationSimpleDtos.add(findStationSimpleDto(newEdge.getTargetStationId()));
+        }
+
+        return stationSimpleDtos;
+    }
+
+    private StationSimpleDto findStationSimpleDto(long stationId) {
+        return stationMap.get(stationId).toSimpleDto();
+    }
+
+    public static LineResponseDto assemble(final Line line, final List<Edge> newEdges, final List<Station> stations) {
+        Map<Long, Station> stationMap = stations.stream().collect(Collectors.toMap(Station::getId, station -> station));
+        LineDtoAssembler assembler = new LineDtoAssembler(stationMap, newEdges);
+
+        List<StationSimpleDto> stationSimpleDtos = assembler.getStationSimpleDtoList();
 
         return LineResponseDto.builder()
                 .id(line.getId())
