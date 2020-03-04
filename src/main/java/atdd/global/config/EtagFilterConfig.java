51a8c3bcb42d191f0bb01e83103c9222d7210ed6
yhs <mvmaniaz@gmail.com>
2020-03-04 23:43:48 +0900

    feat(config): add etag filter

---
+++ src/main/java/atdd/global/config/EtagFilterConfig.java
@@ -0,0 +1,20 @@
+package atdd.global.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class EtagFilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean
+                = new FilterRegistrationBean<>(new ShallowEtagHeaderFilter());
+        filterRegistrationBean.addUrlPatterns("/stations/*", "/paths/*");
+        filterRegistrationBean.setName("etagFilter");
+        return filterRegistrationBean;
+    }
+
+}
