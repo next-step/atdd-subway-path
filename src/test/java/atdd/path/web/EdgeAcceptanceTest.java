94a93b4047d70f19303871d2f5c01aaaaa861c2e
LeeSeungHee <palmseung@gmail.com>
2020-03-09 00:18:26 +0900

    Test (line) add lineServiceTest to add edge

--- src/test/java/atdd/path/web/EdgeAcceptanceTest.java
+++ src/test/java/atdd/path/web/EdgeAcceptanceTest.java
@@ -1,7 +1,6 @@
 package atdd.path.web;
 
 import atdd.AbstractAcceptanceTest;
-import atdd.path.application.dto.EdgeResponseView;
 import atdd.path.application.dto.LineRequestView;
 import atdd.path.application.dto.LineResponseView;
 import atdd.path.application.dto.StationResponseView;
@@ -9,7 +8,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 
 import java.time.LocalTime;
 
@@ -47,41 +45,23 @@ public class EdgeAcceptanceTest extends AbstractAcceptanceTest {
     @Test
     void 지하철_노선에_구간_등록을_요청한다() throws Exception {
         //given
-        stationHttpTest.create(STATION_NAME);
-        stationHttpTest.create(STATION_NAME_2);
+        StationResponseView stationResponseView = stationHttpTest.create(STATION_NAME);
+        StationResponseView stationResponseView2 = stationHttpTest.create(STATION_NAME_2);
         String input = objectMapper.writeValueAsString(requestView);
-        lineHttpTest.create(input);
+        LineResponseView lineResponseView = lineHttpTest.create(input);
+        Long lineId = lineResponseView.getId();
+        Long startId = stationResponseView.getId();
+        Long endId = stationResponseView2.getId();
+
 
         //when
         EdgeResponseView edgeResponseView
-                = edgeHttpTest.createEdge(1L, 1L, 2L,
-                DISTANCE_KM, INTERVAL_TIME, objectMapper);
+                = edgeHttpTest.createEdge(lineId, startId, endId, DISTANCE_KM, INTERVAL_TIME, objectMapper);
 
         //then
         assertThat(edgeResponseView.getId()).isEqualTo(1L);
-        assertThat(edgeResponseView.getSource().getName()).isEqualTo(STATION_NAME);
-        assertThat(edgeResponseView.getTarget().getName()).isEqualTo(STATION_NAME_2);
-    }
-
-    @Test
-    void 지하철_노선에_구간_제외를_요청한다() throws Exception {
-        //given
-        StationResponseView stationResponseView = stationHttpTest.create(STATION_NAME);
-        StationResponseView stationResponseView2 = stationHttpTest.create(STATION_NAME_2);
-//        StationResponseView stationResponseView3 = stationHttpTest.create(STATION_NAME_3);
-        String input = objectMapper.writeValueAsString(requestView);
-        LineResponseView lineResponseView = lineHttpTest.create(input);
-        EdgeResponseView edge = edgeHttpTest.createEdge(lineResponseView.getId(), stationResponseView.getId(), stationResponseView2.getId(),
-                DISTANCE_KM, INTERVAL_TIME, objectMapper);
-//        EdgeResponseView edge2 = edgeHttpTest.createEdge(lineResponseView.getId(), stationResponseView2.getId(), stationResponseView3.getId(),
-//                DISTANCE_KM, INTERVAL_TIME, objectMapper);
-
-        //when, then
-        webTestClient.delete()
-                .uri("/edges/" + lineResponseView.getId()
-                        + "?stationId=" + stationResponseView2.getId())
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk();
+        assertThat(edgeResponseView.getLine().getId()).isEqualTo(lineId);
+        assertThat(edgeResponseView.getSource().getId()).isEqualTo(startId);
+        assertThat(edgeResponseView.getTarget().getId()).isEqualTo(endId);
     }
 }
