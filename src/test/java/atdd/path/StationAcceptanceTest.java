6cec7dbe504d419fd9eda5a8dc820143264ae142
LeeSeungHee <palmseung@gmail.com>
2020-03-13 23:52:21 +0900

    Refactor(station) : refactor StationAcceptanceTest
    - add StationHttpTest

--- src/test/java/atdd/path/StationAcceptanceTest.java
+++ src/test/java/atdd/path/StationAcceptanceTest.java
@@ -1,30 +1,28 @@
 package atdd.path;
 
 import atdd.BaseAcceptanceTest;
-import atdd.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
 
-import java.net.URI;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StationAcceptanceTest extends BaseAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
+    private StationHttpTest stationHttpTest;
+
+    @BeforeEach
+    void setUp() {
+        stationHttpTest = new StationHttpTest(webTestClient);
+    }
 
     @Test
     void createStation() {
-        //given
-        Station station = Station.builder()
-                .name("강남")
-                .build();
+        //when
+        Long stationId = stationHttpTest.createStation("강남");
 
-        webTestClient.post().uri("/stations")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .bodyValue(station)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectBody().jsonPath("$.name").isEqualTo("강남");
+        //then
+        assertThat(stationId).isEqualTo(1L);
     }
 }
---
+++ src/test/java/atdd/path/StationHttpTest.java
@@ -0,0 +1,35 @@
+package atdd.path;
+
+import atdd.domain.Station;
+import atdd.dto.StationResponseView;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.stream.Collectors;
+
+public class StationHttpTest {
+    private WebTestClient webTestClient;
+
+    public StationHttpTest(WebTestClient webTestClient) {
+        this.webTestClient = webTestClient;
+    }
+
+    public Long createStation(String name) {
+        Station station = Station.builder()
+                .name(name)
+                .build();
+
+        return webTestClient.post().uri("/stations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(station)
+                .exchange()
+                .expectStatus().isCreated()
+                .returnResult(StationResponseView.class)
+                .getResponseBody()
+                .toStream()
+                .map(StationResponseView::getId)
+                .collect(Collectors.toList())
+                .get(0);
+    }
+}
