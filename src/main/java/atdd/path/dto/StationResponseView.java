f44d412ebaf5de6bc5a2cf2184fe1308d9efa02c
LeeSeungHee <palmseung@gmail.com>
2020-03-21 19:29:10 +0900

    Test (path, edge) : delete duplicated files

--- src/main/java/atdd/path/dto/StationResponseView.java
+++ src/main/java/atdd/path/dto/StationResponseView.java
@@ -1,5 +1,6 @@
 package atdd.path.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.sound.sampled.Line;
@@ -14,6 +15,7 @@ public class StationResponseView {
     public StationResponseView() {
     }
 
+    @Builder
     public StationResponseView(Long id, String name, List<Line> lines) {
         this.id = id;
         this.name = name;
