404bf4d973b458e3bc1b0cd604c362ed516e9bfa
boorownie <probitanima11@gmail.com>
2020-06-26 15:36:18 +0900

    test: done the step 0

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -1,5 +1,9 @@
 package nextstep.subway.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +20,12 @@ public class Line {
     private List<Section> sections = new ArrayList<>();
 
     public Line() {
+    }
+
+    public Line(String name, String color, Station upStation, Station downStation, int distance) {
+        this.name = name;
+        this.color = color;
+        this.sections.add(new Section(this, upStation, downStation, distance));
     }
 
     public Line(String name, String color) {
@@ -49,5 +59,33 @@ public class Line {
 
     public List<Section> getSections() {
         return sections;
+    }
+
+    public void addSection(Station upStation, Station downStation, int distance) {
+        sections.add(new Section(this, upStation, downStation, distance));
+    }
+
+    public List<Station> getStations() {
+        final Set<Station> stationSet = new HashSet<>();
+        sections.forEach(section -> {
+            stationSet.add(section.getUpStation());
+            stationSet.add(section.getDownStation());
+        });
+
+        return List.copyOf(stationSet);
+    }
+
+    public void removeSection(Station upStation, Station downStation) {
+        final var isSameStation = generateSameStationChecker(upStation, downStation);
+
+        sections = sections.stream()
+            .filter(isSameStation)
+            .collect(Collectors.toList());
+    }
+
+    private static Predicate<Section> generateSameStationChecker(Station upStation, Station downStation) {
+        return section ->
+            section.getUpStation() != upStation
+                && section.getDownStation() != downStation;
     }
 }
--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,17 +1,70 @@
 package nextstep.subway.unit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 
 class LineTest {
+
+
+    /**
+     * GIVEN 지하철 노선이 생성면 구간이 1개이고
+     * WHEN 구간이 추가되면
+     * THEN 구간의 수는 2개가 된다.
+     */
     @Test
     void addSection() {
+        // given
+        Station 강남역 = new Station("강남역");
+        Station 역삼역 = new Station("역삼역");
+        Station 선릉역 = new Station("선릉역");
+
+        Line line = new Line("2호선", "green", 강남역, 역삼역, 10);
+
+        // when
+        line.addSection(역삼역, 선릉역, 10);
+
+        // then
+        assertThat(line.getSections()).hasSize(2);
     }
 
+    /**
+     * WHEN 노선을 생성했을 때
+     * THEN 노선에 등록된 역은 2개다.
+     */
     @Test
     void getStations() {
+        // when
+        Station 강남역 = new Station("강남역");
+        Station 역삼역 = new Station("역삼역");
+
+        Line line = new Line("2호선", "green", 강남역, 역삼역, 10);
+
+        // then
+        assertThat(line.getStations()).hasSize(2);
     }
 
+    /**
+     * GIVEN 노선에 구간이 2개일 때
+     * WHEN 노선에서 구간을 삭제하면
+     * THEN 노선의 구간은 1개이다.
+     */
     @Test
     void removeSection() {
+        // given
+        Station 강남역 = new Station("강남역");
+        Station 역삼역 = new Station("역삼역");
+        Station 선릉역 = new Station("선릉역");
+
+        Line line = new Line("2호선", "green", 강남역, 역삼역, 10);
+        line.addSection(역삼역, 선릉역, 10);
+
+        // when
+        line.removeSection(역삼역, 선릉역);
+
+        // then
+        assertThat(line.getSections()).hasSize(1);
     }
 }
