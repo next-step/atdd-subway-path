7f8d0ff0baf92a237b2dd1ded549bd36fc8faae5
koamania <koamania2@gmail.com>
2020-07-11 20:37:22 +0900

    refactoring(line-stations): 테스트코드 리팩토링

--- src/test/java/nextstep/subway/line/domain/LineStationsTest.java
+++ src/test/java/nextstep/subway/line/domain/LineStationsTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -17,6 +16,7 @@ public class LineStationsTest {
     private Station station1;
     private Station station2;
     private Station station3;
+    private Station newStation;
 
     @BeforeEach
     void setUp() {
@@ -25,6 +25,9 @@ public class LineStationsTest {
         station1 = new Station("서울역");
         station2 = new Station("홍대입구");
         station3 = new Station("검암역");
+
+        newStation = new Station("인천공함");
+
         lineStations.registerLineStation(new LineStation(station1, null, 10, 10));
         lineStations.registerLineStation(new LineStation(station2, station1, 10, 10));
         lineStations.registerLineStation(new LineStation(station3, station2, 10, 10));
@@ -34,26 +37,20 @@ public class LineStationsTest {
     @Test
     void add1() {
         // when
-        Station newStation = new Station("인천공항");
-        lineStations.registerLineStation(new LineStation(newStation, station3, 10, 10));
+        지하철_노선에_역을_등록한다(newStation, station3);
 
         // then
-        List<Station> stations = lineStations.getLineStationsInOrder().stream()
-                .map(LineStation::getStation)
-                .collect(Collectors.toList());
-        assertThat(stations).containsExactly(station1, station2, station3, newStation);
+        지하철노선에_등록된_지하철역이_정렬됨(lineStations.getLineStationsInOrder(), station1, station2, station3, newStation);
     }
 
     @DisplayName("지하철 노선에 역을 중간에 등록한다.")
     @Test
     void add2() {
         // when
-        Station newStation = new Station("인천공함");
-        LineStation newLineStation = new LineStation(newStation, station2, 10, 10);
-        lineStations.registerLineStation(newLineStation);
+        지하철_노선에_역을_등록한다(newStation, station2);
 
-        assertThat(lineStations.getLineStationsInOrder()).extracting(LineStation::getStation)
-                .containsExactly(station1, station2, newStation, station3);
+        // then
+        지하철노선에_등록된_지하철역이_정렬됨(lineStations.getLineStationsInOrder(), station1, station2, newStation, station3);
     }
 
     @DisplayName("이미 등록되어 있던 역을 등록한다.")
@@ -82,5 +79,15 @@ public class LineStationsTest {
     @DisplayName("지하철 노선에서 등록되지 않는 역을 제외한다.")
     @Test
     void removeLineStation4() {
+    }
+
+    private void 지하철_노선에_역을_등록한다(Station newStation, Station preStation) {
+        LineStation newLineStation = new LineStation(newStation, preStation, 10, 10);
+        lineStations.registerLineStation(newLineStation);
+    }
+
+    private void 지하철노선에_등록된_지하철역이_정렬됨(List<LineStation> lineStations, Station... stations) {
+        assertThat(lineStations).extracting(LineStation::getStation)
+                .containsExactly(stations);
     }
 }
