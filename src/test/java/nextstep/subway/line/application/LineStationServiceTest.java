bb1492b3841303cdb4a5d56e7fc3b93702893571
koamania <koamania2@gmail.com>
2020-07-12 14:28:34 +0900

    test(line-station): step0 - 존재하지 않는 역을 등록했을 때 실패 검증하는 테스트 추가

--- src/test/java/nextstep/subway/line/application/LineStationServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineStationServiceTest.java
@@ -1,27 +1,22 @@
 package nextstep.subway.line.application;
 
-import com.google.common.collect.Lists;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.domain.LineStation;
-import nextstep.subway.line.dto.LineStationCreateRequest;
 import nextstep.subway.line.dto.LineStationRequest;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -36,19 +31,20 @@ public class LineStationServiceTest {
     private StationRepository stationRepository;
 
     private LineStationService lineStationService;
+    private Line line;
 
     @BeforeEach
     void setUp() {
+        line = new Line();
         lineStationService = new LineStationService(lineRepository, stationRepository);
+        when(lineRepository.findById(anyLong())).thenReturn(Optional.of(line));
     }
 
     @DisplayName("지하철 노선에 역을 등록한다.")
     @Test
     void addLineStation1() {
         // given
-        Line line = new Line();
-        Station station = mock(Station.class);
-        when(lineRepository.findById(anyLong())).thenReturn(Optional.of(line));
+        Station station = new Station();
         when(stationRepository.findById(anyLong())).thenReturn(Optional.of(station));
 
         // when
@@ -62,6 +58,16 @@ public class LineStationServiceTest {
     @DisplayName("존재하지 않는 역을 등록한다.")
     @Test
     void addLineStation2() {
+        // given
+        when(lineRepository.findById(anyLong())).thenReturn(Optional.of(line));
+        when(stationRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        // when
+        LineStationRequest lineStationRequest = new LineStationRequest(1L, null, 10, 10);
+
+        // then
+        assertThatThrownBy(() -> lineStationService.addLineStation(1L, lineStationRequest))
+                .isInstanceOf(RuntimeException.class);
     }
 
     @DisplayName("지하철 노선에 역을 제외한다.")
