865cdbf2d3c067088dcc30d58f7318b0c9f308c5
koamania <koamania2@gmail.com>
2020-07-13 02:07:13 +0900

    feature(map): step1 ShallowEtagHeaderFilter 추가

--- src/main/java/nextstep/subway/config/ETagHeaderFilter.java
+++ src/main/java/nextstep/subway/config/ETagHeaderFilter.java
@@ -1,10 +1,19 @@
 package nextstep.subway.config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
+@Configuration
 public class ETagHeaderFilter {
+    @Bean
     public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-        return null;
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<>(new ShallowEtagHeaderFilter());
+        filterRegistrationBean.addUrlPatterns("/maps");
+        filterRegistrationBean.setName("eTagFilter");
+        return filterRegistrationBean;
     }
 }
