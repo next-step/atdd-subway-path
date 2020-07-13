d892317006b7744eacd165607854906f513a52ca
junwoochoi <cjw4690@gmail.com>
2020-07-14 08:23:44 +0900

    refactor : change to more readable variable names

--- src/test/java/nextstep/subway/map/ui/MapControllerTest.java
+++ src/test/java/nextstep/subway/map/ui/MapControllerTest.java
@@ -1,26 +1,30 @@
 package nextstep.subway.map.ui;
 
-import nextstep.subway.SubwayApplication;
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.MapResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("지하철 노선도 컨트롤러 테스트")
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = SubwayApplication.class)
+@WebMvcTest
 class MapControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
     private MapController mapController;
@@ -30,15 +34,16 @@ class MapControllerTest {
 
     @DisplayName("노선도 조회 도중 에러 발생시 에러 코드 응답")
     @Test
-    void getMapsWithError() {
+    void getMapsWithError() throws Exception {
         //given
         given(mapService.getMaps()).willThrow(RuntimeException.class);
 
         //when
-        ResponseEntity<MapResponse> response = mapController.getMaps();
+        ResultActions result = mockMvc.perform(get("/maps"))
+                .andDo(print());
 
         //then
-        assertThat(response.getStatusCode().isError()).isTrue();
+        result.andExpect(status().is4xxClientError());
     }
 
     @DisplayName("노선도 조회 요청시 200 코드 응답")
