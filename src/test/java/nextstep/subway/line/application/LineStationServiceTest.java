c0a44db8dfe210f4d8fe57b957eead231bafa335
etff <wish2rich@gmail.com>
2020-07-14 02:25:36 +0900

    Step0 단위 테스트 실습 (#70)
    * test(lineStations): add station between stations
    
    * test(lineStations): remove station
    
    * Test(lineStations): remove first station test
    
    * test(lineStations): remove unregistred station
    
    * style: remove space
    
    * test(lineStationService): add remove test
    
    * test(lineStationService): add unregistred station test
    
    * test(lineStationService): add not existed station test
    
    * refactor: change when to given

--- src/test/java/nextstep/subway/line/application/LineStationServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineStationServiceTest.java
@@ -13,40 +13,84 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 
+import java.time.LocalTime;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
 
 @DisplayName("지하철 노선 서비스 테스트")
+@ExtendWith(MockitoExtension.class)
 public class LineStationServiceTest {
+
+    @Mock
     private LineRepository lineRepository;
+
+    @Mock
     private StationRepository stationRepository;
 
     private LineStationService lineStationService;
 
+    private Line line;
+
+    @Mock
+    private Station station;
+
+    private LineStation lineStation;
+
+    private LineStationCreateRequest request;
+
     @BeforeEach
     void setUp() {
         lineStationService = new LineStationService(lineRepository, stationRepository);
+        line = new Line("2호선", "GREEN", LocalTime.of(5, 30), LocalTime.of(23, 30), 5);
+        lineStation = new LineStation(1L, null, 5, 5);
     }
 
     @DisplayName("지하철 노선에 역을 등록한다.")
     @Test
     void addLineStation1() {
+        given(lineRepository.findById(any())).willReturn(Optional.of(line));
+        given(station.getId()).willReturn(1L);
+        given(stationRepository.findAllById(anyList())).willReturn(Lists.newArrayList(station));
+        request = new LineStationCreateRequest(1L, null, 5, 5);
+
+        // when
+        lineStationService.addLineStation(1L, request);
+
+        // then
+        assertAll(
+                () -> assertThat(line.getLineStations()).isNotNull(),
+                () -> assertThat(line.getStationInOrder()).hasSize(1)
+        );
     }
 
     @DisplayName("존재하지 않는 역을 등록한다.")
     @Test
     void addLineStation2() {
+        given(stationRepository.findAllById(anyList())).willReturn(Lists.newArrayList());
+        request = new LineStationCreateRequest(1L, null, 5, 5);
+
+        //when
+        assertThatThrownBy(() -> lineStationService.addLineStation(1L, request))
+                .isInstanceOf(RuntimeException.class);
     }
 
     @DisplayName("지하철 노선에 역을 제외한다.")
     @Test
     void removeLineStation() {
+        // given
+        given(lineRepository.findById(anyLong())).willReturn(Optional.of(line));
+        line.addLineStation(lineStation);
+
+        // when
+        lineStationService.removeLineStation(1L, 1L);
+
+        // then
+        assertThat(line.getStationInOrder()).isEmpty();
     }
 }
