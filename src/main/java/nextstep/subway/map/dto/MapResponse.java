9274decd599b238086a949093344f37e23a847fd
junwoochoi <cjw4690@gmail.com>
2020-07-14 08:15:39 +0900

    refactor : chagnge to more readable variable name

--- src/main/java/nextstep/subway/map/dto/MapResponse.java
+++ src/main/java/nextstep/subway/map/dto/MapResponse.java
@@ -1,23 +1,22 @@
 package nextstep.subway.map.dto;
 
-import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.dto.LineResponse;
 
 import java.util.List;
 
 public class MapResponse {
-    private List<LineResponse> data;
+    private List<LineResponse> lines;
 
     public static MapResponse of(List<LineResponse> lines) {
         return new MapResponse(lines);
     }
 
-    public List<LineResponse> getData() {
-        return data;
+    public List<LineResponse> getLines() {
+        return lines;
     }
 
-    public MapResponse(List<LineResponse> data) {
-        this.data = data;
+    public MapResponse(List<LineResponse> lines) {
+        this.lines = lines;
     }
 
     public MapResponse() {
