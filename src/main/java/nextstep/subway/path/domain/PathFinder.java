c0d35db41998d604f0d5e6440e08688bca074074
koamania <koamania2@gmail.com>
2020-07-15 01:38:01 +0900

    refactoring(path): step2 PathFinder 서비스로 등록

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -1,22 +1,15 @@
 package nextstep.subway.path.domain;
 
 import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.dto.StationResponse;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component
+@Service
 public class PathFinder {
 
     public ShortestPathResult findShortestPath(List<LineResponse> lines, Long startStationId, Long endStationId, PathFindType type) {
         return ShortestPathResult.empty();
-    }
-
-    @Bean
-    public PathFinder pathFinder() {
-        return new PathFinder();
     }
 }
