271d8e4b51a99071c1d691755815039c2b864c40
koamania <koamania2@gmail.com>
2020-07-14 23:48:27 +0900

    feature(path): step2 PathController 기능 추가

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,7 +1,9 @@
 package nextstep.subway.path.application;
 
 import nextstep.subway.path.dto.PathResponse;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PathService {
     public PathResponse findShortestPath(long startStationId, long endStationId) {
         return null;
--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -3,7 +3,13 @@ package nextstep.subway.path.ui;
 import nextstep.subway.path.application.PathService;
 import nextstep.subway.path.dto.PathResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/paths")
 public class PathController {
     private final PathService pathService;
 
@@ -11,7 +17,9 @@ public class PathController {
         this.pathService = pathService;
     }
 
+    @GetMapping
     public ResponseEntity<PathResponse> findShortestPath(long startStationId, long endStationId) {
-        return null;
+        PathResponse pathResponse = pathService.findShortestPath(startStationId, endStationId);
+        return ResponseEntity.ok(pathResponse);
     }
 }
