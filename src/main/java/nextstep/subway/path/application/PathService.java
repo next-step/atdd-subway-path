343f2dd337e63183933a14db254576df9eef6110
junwoochoi <cjw4690@gmail.com>
2020-07-15 08:38:49 +0900

    feat : "최단 경로 탐색 요청 시, 출발역과 도착역이 같은 경우 에러가 발생한다."

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,8 +1,11 @@
 package nextstep.subway.path.application;
 
+import nextstep.subway.exception.NotValidRequestException;
 import nextstep.subway.path.dto.PathResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
 
 @Service
 @Transactional
@@ -10,6 +13,9 @@ public class PathService {
 
     @Transactional(readOnly = true)
     public PathResponse findShortestPath(Long startStationId, Long endStationId) {
+        if (Objects.equals(startStationId, endStationId)) {
+            throw new NotValidRequestException("출발역과 도착역은 같을 수 없습니다.");
+        }
         return null;
     }
 }
--- src/test/java/nextstep/subway/path/application/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -1,19 +1,32 @@
 package nextstep.subway.path.application;
 
-import nextstep.subway.station.domain.StationRepository;
+import nextstep.subway.exception.NotValidRequestException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("경로 탐색 서비스 테스트")
 @ExtendWith(MockitoExtension.class)
 class PathServiceTest {
 
+    private PathService pathService;
+
+    @BeforeEach
+    void setUp() {
+        pathService = new PathService();
+    }
+
     @DisplayName("최단 경로 탐색 요청 시, 출발역과 도착역이 같은 경우 에러가 발생한다.")
     @Test
     void findShortestPathWithSameStartAndEndStation() {
+        //when
+        assertThatThrownBy(() -> pathService.findShortestPath(1L, 1L))
+                //then
+                .isInstanceOf(NotValidRequestException.class);
     }
 
     @DisplayName("최단 경로 탐색 요청 시, 출발역과 도착역이 연결이 되어 있지 않은 경우 에러가 발생한다.")
