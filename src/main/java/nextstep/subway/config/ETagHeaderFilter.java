cce3d112cfb08932ae26055c25bd5383de20d9b1
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-15 12:50:22 +0900

    refactor: declare Bean annotation to ETagHeaderFilter

--- src/main/java/nextstep/subway/config/ETagHeaderFilter.java
+++ src/main/java/nextstep/subway/config/ETagHeaderFilter.java
@@ -1,10 +1,12 @@
 package nextstep.subway.config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 public class ETagHeaderFilter {
 
+	@Bean
 	public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
 		FilterRegistrationBean<ShallowEtagHeaderFilter> filter = new FilterRegistrationBean<>(
 			new ShallowEtagHeaderFilter());
