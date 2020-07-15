4f04491458b6b924ab2e9d3896c93617218e5382
junwoochoi <cjw4690@gmail.com>
2020-07-15 14:51:00 +0900

    feat: '최단 경로 탐색 요청 시, 존재하지 않은 출발역이나 도착역을 조회 할 경우 에러가 발생한다.'

--- src/test/java/nextstep/subway/path/application/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -29,10 +29,26 @@ class PathServiceTest {
     @Mock
     private LineService lineService;
     private PathService pathService;
+    private LineStations lineStations1;
+    private LineStations lineStations2;
+    private LineStation lineStation1;
+    private LineStation lineStation2;
+    private LineStation lineStation3;
+    private LineStation lineStation4;
+    private LineStation lineStation5;
 
     @BeforeEach
     void setUp() {
         pathService = new PathService(lineService);
+
+        lineStations1 = mock(LineStations.class);
+        lineStations2 = mock(LineStations.class);
+
+        lineStation1 = reflectionLineStation(1L, null, 5, 5);
+        lineStation2 = reflectionLineStation(2L, 1L, 5, 5);
+        lineStation3 = reflectionLineStation(3L, 2L, 5, 5);
+        lineStation4 = reflectionLineStation(3L, null, 5, 5);
+        lineStation5 = reflectionLineStation(4L, 3L, 5, 5);
     }
 
     @DisplayName("최단 경로 탐색 요청 시, 출발역과 도착역이 같은 경우 에러가 발생한다.")
@@ -48,13 +64,6 @@ class PathServiceTest {
     @Test
     void findShortestPathWithNotConnectedStations() {
         //given
-        LineStations lineStations1 = mock(LineStations.class);
-        LineStations lineStations2 = mock(LineStations.class);
-        LineStation lineStation1 = new LineStation(1L, null, 5, 5);
-        LineStation lineStation2 = new LineStation(2L, 1L, 5, 5);
-        LineStation lineStation4 = new LineStation(3L, null, 5, 5);
-        LineStation lineStation5 = new LineStation(4L, 3L, 5, 5);
-
         given(lineStations1.getStationsInOrder())
                 .willReturn(Lists.list(lineStation1, lineStation2));
         given(lineStations2.getStationsInOrder())
@@ -74,23 +83,23 @@ class PathServiceTest {
 
     }
 
-    private Line reflectionLine(long id, String name, String color, LineStations lineStations) {
-        Line line = new Line();
-        ReflectionTestUtils.setField(line, "id", id);
-        ReflectionTestUtils.setField(line, "name", name);
-        ReflectionTestUtils.setField(line, "color", color);
-        ReflectionTestUtils.setField(line, "startTime", LocalTime.now());
-        ReflectionTestUtils.setField(line, "endTime", LocalTime.now());
-        ReflectionTestUtils.setField(line, "intervalTime", 4);
-        ReflectionTestUtils.setField(line, "lineStations", lineStations);
-        return line;
-    }
-
     @DisplayName("최단 경로 탐색 요청 시, 존재하지 않은 출발역이나 도착역을 조회 할 경우 에러가 발생한다.")
     @Test
     void findShortestPathWithNotExistStations() {
+        //given
+        given(lineStations1.getStationsInOrder())
+                .willReturn(Lists.list(lineStation1, lineStation2, lineStation3));
+        given(lineStations2.getStationsInOrder())
+                .willReturn(Lists.list(lineStation4, lineStation5));
+
+        Line line1 = reflectionLine(1L, "2호선", "GREEN", lineStations1);
+        Line line2 = reflectionLine(2L, "신분당선", "RED", lineStations2);
+
+        given(lineService.findAllLineEntities())
+                .willReturn(Lists.list(line1, line2));
+
         //when
-        assertThatThrownBy(() -> pathService.findShortestPath(1L, 4L))
+        assertThatThrownBy(() -> pathService.findShortestPath(5L, 6L))
                 //then
                 .isInstanceOf(NotFoundException.class);
     }
@@ -103,5 +112,26 @@ class PathServiceTest {
     @DisplayName("최단 경로 탐색하여 여러개의 최단 경로가 나와도 하나의 최단 경로만 리턴한다.")
     @Test
     void findShortestPathWithMultipleAnswers() {
+    }
+
+    private LineStation reflectionLineStation(long stationId, Object preStationId, int distance, int duration) {
+        LineStation lineStation = new LineStation();
+        ReflectionTestUtils.setField(lineStation, "stationId", stationId);
+        ReflectionTestUtils.setField(lineStation, "preStationId", preStationId);
+        ReflectionTestUtils.setField(lineStation, "distance", distance);
+        ReflectionTestUtils.setField(lineStation, "duration", duration);
+        return lineStation;
+    }
+
+    private Line reflectionLine(long id, String name, String color, LineStations lineStations) {
+        Line line = new Line();
+        ReflectionTestUtils.setField(line, "id", id);
+        ReflectionTestUtils.setField(line, "name", name);
+        ReflectionTestUtils.setField(line, "color", color);
+        ReflectionTestUtils.setField(line, "startTime", LocalTime.now());
+        ReflectionTestUtils.setField(line, "endTime", LocalTime.now());
+        ReflectionTestUtils.setField(line, "intervalTime", 4);
+        ReflectionTestUtils.setField(line, "lineStations", lineStations);
+        return line;
     }
 }
