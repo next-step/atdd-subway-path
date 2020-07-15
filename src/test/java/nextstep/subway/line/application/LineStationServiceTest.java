e191fb57a6c3f423b001b500db5270b94ddc6793
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-15 13:28:41 +0900

    test: not existing station to be registered throws exception

--- src/test/java/nextstep/subway/line/application/LineStationServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineStationServiceTest.java
@@ -1,52 +1,58 @@
 package nextstep.subway.line.application;
 
-import com.google.common.collect.Lists;
-import nextstep.subway.line.domain.Line;
-import nextstep.subway.line.domain.LineRepository;
-import nextstep.subway.line.domain.LineStation;
-import nextstep.subway.line.dto.LineStationCreateRequest;
-import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.domain.StationRepository;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import com.google.common.collect.Lists;
+import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.dto.LineStationCreateRequest;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 
 @DisplayName("지하철 노선 서비스 테스트")
 public class LineStationServiceTest {
-    private LineRepository lineRepository;
-    private StationRepository stationRepository;
+	private LineRepository lineRepository;
+	private StationRepository stationRepository;
 
-    private LineStationService lineStationService;
+	private LineStationService lineStationService;
 
-    @BeforeEach
-    void setUp() {
-        lineStationService = new LineStationService(lineRepository, stationRepository);
-    }
+	@BeforeEach
+	void setUp() {
+		lineRepository = mock(LineRepository.class);
+		stationRepository = mock(StationRepository.class);
+		lineStationService = new LineStationService(lineRepository, stationRepository);
+	}
 
-    @DisplayName("지하철 노선에 역을 등록한다.")
-    @Test
-    void addLineStation1() {
-    }
+	@DisplayName("존재하지 않는 역을 등록한다.")
+	@Test
+	void 존재하지_않는_역을_등록한다() {
+		Long stationId = 1L;
+		when(stationRepository.findAllById(Lists.newArrayList(null, stationId))).thenReturn(Lists.newArrayList());
+        LineStationCreateRequest lineStationCreateRequest = new LineStationCreateRequest(stationId, null, 2, 2);
+        assertThatThrownBy(
+            () -> lineStationService.addLineStation(1L, lineStationCreateRequest)
+        ).isInstanceOf(RuntimeException.class);
+	}
 
-    @DisplayName("존재하지 않는 역을 등록한다.")
-    @Test
-    void addLineStation2() {
-    }
+	@DisplayName("존재하지 않는 역을 이전 역으로 등록한다.")
+	@Test
+	void 존재하지_않는_역을_이전_역으로_등록한다() {
+		Long preStationId = 1L;
+		Long stationId = 2L;
+		Station station = new Station("회기역");
+		when(stationRepository.findAllById(Lists.newArrayList(preStationId, stationId))).thenReturn(Lists.newArrayList(station));
+		LineStationCreateRequest lineStationCreateRequest = new LineStationCreateRequest(stationId, preStationId, 2, 2);
+		assertThatThrownBy(
+			() -> lineStationService.addLineStation(1L, lineStationCreateRequest)
+		).isInstanceOf(RuntimeException.class);
+	}
 
-    @DisplayName("지하철 노선에 역을 제외한다.")
-    @Test
-    void removeLineStation() {
-    }
+	@DisplayName("지하철 노선에 역을 제외한다.")
+	@Test
+	void removeLineStation() {
+	}
 }
