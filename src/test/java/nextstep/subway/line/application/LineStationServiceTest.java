0ed93c3ee0bdea64b4015a8f4ad25f5d25a18cd7
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-15 13:45:03 +0900

    test: verify whether adding station to line is possible

--- src/test/java/nextstep/subway/line/application/LineStationServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineStationServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalTime;
+import java.util.LinkedList;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -32,12 +33,31 @@ public class LineStationServiceTest {
 		lineStationService = new LineStationService(lineRepository, stationRepository);
 	}
 
+	@DisplayName("지하철 노선에 역을 등록한다.")
+	@Test
+	void 지하철_노선에_역을_등록한다() {
+		Line line = new Line("신분당선", "red", LocalTime.of(5, 30), LocalTime.of(0, 0), 5);
+		when(lineRepository.findById(anyLong())).thenReturn(Optional.of(line));
+
+		Long stationId = 1L;
+		Station station = new Station(stationId, "강남역");
+		when(stationRepository.findAllById(Lists.newArrayList(null, stationId))).thenReturn(Lists.newArrayList(station));
+
+		LineStationCreateRequest request = new LineStationCreateRequest(stationId, null, 2, 2);
+		lineStationService.addLineStation(anyLong(), request);
+
+		assertThat(line.getLineStations().getLineStations()).hasSize(1);
+		assertThat(line.getLineStations().getLineStations().get(0).getStationId()).isEqualTo(stationId);
+	}
+
 	@DisplayName("존재하지 않는 역을 등록한다.")
 	@Test
 	void 존재하지_않는_역을_등록한다() {
 		Long stationId = 1L;
 		when(stationRepository.findAllById(Lists.newArrayList(null, stationId))).thenReturn(Lists.newArrayList());
+
         LineStationCreateRequest lineStationCreateRequest = new LineStationCreateRequest(stationId, null, 2, 2);
+
         assertThatThrownBy(
             () -> lineStationService.addLineStation(1L, lineStationCreateRequest)
         ).isInstanceOf(RuntimeException.class);
@@ -50,7 +70,9 @@ public class LineStationServiceTest {
 		Long stationId = 2L;
 		Station station = new Station("회기역");
 		when(stationRepository.findAllById(Lists.newArrayList(preStationId, stationId))).thenReturn(Lists.newArrayList(station));
+
 		LineStationCreateRequest lineStationCreateRequest = new LineStationCreateRequest(stationId, preStationId, 2, 2);
+
 		assertThatThrownBy(
 			() -> lineStationService.addLineStation(1L, lineStationCreateRequest)
 		).isInstanceOf(RuntimeException.class);
@@ -62,6 +84,7 @@ public class LineStationServiceTest {
 		Line line = new Line("line2", "green", LocalTime.of(5, 30), LocalTime.of(0, 0), 5);
 		line.addLineStation(new LineStation(1L, null, 2, 2));
 		when(lineRepository.findById(anyLong())).thenReturn(Optional.of(line));
+
 		lineStationService.removeLineStation(1L, 1L);
 		assertThat(line.getLineStations().getLineStations()).isEmpty();
 	}
