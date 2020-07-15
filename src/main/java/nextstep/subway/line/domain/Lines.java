d23924e6c1d2494a28eeafa0e86cbbe45061f674
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-15 12:44:48 +0900

    feat: add Lines that is the first collection of Line

---
+++ src/main/java/nextstep/subway/line/domain/Lines.java
@@ -0,0 +1,30 @@
+package nextstep.subway.line.domain;
+
+import static java.util.stream.Collectors.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import nextstep.subway.line.dto.LineResponses;
+
+public class Lines {
+
+	private final List<Line> lines;
+
+	public Lines(List<Line> lines) {
+		this.lines = lines;
+	}
+
+	public List<Long> getAllStationIdsOfLines() {
+		return lines.stream()
+			.flatMap(Line::getStationIds)
+			.distinct()
+			.collect(Collectors.toList());
+	}
+
+	public LineResponses toLineResponses(AllStations stations) {
+		return lines.stream()
+			.map(line -> line.toLineResponse(stations))
+			.collect(collectingAndThen(Collectors.toList(), LineResponses::new));
+	}
+}
