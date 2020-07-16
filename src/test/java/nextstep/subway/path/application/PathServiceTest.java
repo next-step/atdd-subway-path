b552e1e2cd987bb90338c57bf171cb157760af73
hyeyoom <neoul_chw@icloud.com>
2020-07-16 22:11:44 +0900

    misc: 메인 노트북으로 옮기기 전에 세이브

---
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -0,0 +1,16 @@
+package nextstep.subway.path.application;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("경로 탐색 서비스 유닛 테스트")
+class PathServiceTest {
+
+    @DisplayName("경로를 잘 찾는지 검사한다.")
+    @Test
+    void find_path() {
+        
+    }
+}
