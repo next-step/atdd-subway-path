1fdf5b3e52d8c8f6dc3b4af0c2b60dfbe35a1c60
hyeyoom <neoul_chw@icloud.com>
2020-07-17 00:30:22 +0900

    test(path-service): 유닛 테스트 실패하는 친구 추가..

--- src/test/java/nextstep/subway/path/application/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -1,16 +1,40 @@
 package nextstep.subway.path.application;
 
+import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.path.dto.PathResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
 @DisplayName("경로 탐색 서비스 유닛 테스트")
+@ExtendWith(MockitoExtension.class)
 class PathServiceTest {
+
+    @Mock
+    private LineRepository lineRepository;
+
+    private PathService pathService;
+
+    @BeforeEach
+    void setUp() {
+        pathService = new PathService(lineRepository);
+    }
 
     @DisplayName("경로를 잘 찾는지 검사한다.")
     @Test
     void find_path() {
-        
+
+        // when
+        final PathResponse pathResponse = pathService.findShortestPath(3, 5);
+
+        // then
+        assertThat(pathResponse).isNotNull();
+        verify(lineRepository.findAll());
     }
 }
