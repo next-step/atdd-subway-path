126a5fe367c5268a71b10122d0f0557803d32ee7
koamania <koamania2@gmail.com>
2020-07-17 01:21:06 +0900

    test(path): step3 PathService에 최단 시간 경로 조회 테스트코드 추가

--- src/test/java/nextstep/subway/path/application/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -24,9 +24,9 @@ public class PathServiceTest {
      * - PathFinder 도메인에 startStation과 endStation을 인자로 넘기는 메소드 호출
      * - Response객체로 변환 후 응답
      */
-    @DisplayName("최단 경로를 조회하는 기능 테스트")
+    @DisplayName("최단 거리 경로를 조회하는 기능 테스트")
     @Test
-    void findShortestPath() {
+    void findShortestPathByDistance() {
         // given
         long startStationId = 1L;
         long endStationId = 2L;
@@ -43,6 +43,31 @@ public class PathServiceTest {
 
         // when
         PathResponse pathResponse = pathService.findShortestPath(startStationId, endStationId, PathFindType.DISTANCE);
+
+        // then
+        assertThat(pathResponse).isNotNull();
+        verify(pathFinder).findShortestPath(anyList(), anyLong(), anyLong(), any(PathFindType.class));
+    }
+
+    @DisplayName("최단 시간 경로를 조회하는 기능 테스트")
+    @Test
+    void findShortestPathByDuration() {
+        // given
+        long startStationId = 1L;
+        long endStationId = 2L;
+
+        PathFinder pathFinder = mock(PathFinder.class);
+        ShortestPathResult shortestPathResult = ShortestPathResult.empty();
+        when(pathFinder.findShortestPath(anyList(), anyLong(), anyLong(), any(PathFindType.class)))
+                .thenReturn(shortestPathResult);
+
+        LineService lineService = mock(LineService.class);
+        when(lineService.findAllLines()).thenReturn(Collections.emptyList());
+
+        PathService pathService = new PathService(pathFinder, lineService);
+
+        // when
+        PathResponse pathResponse = pathService.findShortestPath(startStationId, endStationId, PathFindType.DURATION);
 
         // then
         assertThat(pathResponse).isNotNull();
