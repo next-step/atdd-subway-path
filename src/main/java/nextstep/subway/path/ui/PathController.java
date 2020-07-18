d463b1a51198a09c3a454245fa836e5ac52c6b05
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-18 17:34:02 +0900

    feat: add type queryParam to determine path search options

--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -3,6 +3,7 @@ package nextstep.subway.path.ui;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import nextstep.subway.path.application.PathService;
@@ -20,7 +21,13 @@ public class PathController {
     }
 
     @GetMapping
-    public ResponseEntity<PathResponse> findShortestPath(Long startId, Long endId, ShortestPathSearchType type) {
+    public ResponseEntity<PathResponse> findShortestPath(@RequestParam("source") Long startId,
+        @RequestParam("target") Long endId, @RequestParam("type") String typeString) {
+        ShortestPathSearchType type = determineTypeByRequestParam(typeString);
         return ResponseEntity.ok().body(pathService.findShortestPath(startId, endId, type));
+    }
+
+    private ShortestPathSearchType determineTypeByRequestParam(String typeString) {
+        return ShortestPathSearchType.findTypeByTypeName(typeString);
     }
 }
