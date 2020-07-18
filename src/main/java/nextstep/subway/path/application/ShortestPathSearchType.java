f0f10fb10eaf9c60d3d3c0f23b563317f03edd8a
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-18 17:35:04 +0900

    feat: Path enum type to be searched with name String

--- src/main/java/nextstep/subway/path/application/ShortestPathSearchType.java
+++ src/main/java/nextstep/subway/path/application/ShortestPathSearchType.java
@@ -1,6 +1,25 @@
 package nextstep.subway.path.application;
 
+import java.util.Arrays;
+
 public enum ShortestPathSearchType {
-    DISTANCE,
-    DURATION;
+    DISTANCE("DISTANCE"),
+    DURATION("DURATION");
+
+    private final String typeName;
+
+    ShortestPathSearchType(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public static ShortestPathSearchType findTypeByTypeName(String typeString) {
+        return Arrays.stream(ShortestPathSearchType.values())
+            .filter(type -> type.getTypeName().equals(typeString))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("nothing type found or registered"));
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
 }
