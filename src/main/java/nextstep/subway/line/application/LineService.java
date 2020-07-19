bf4b485d55d9e01cfe10a7d651d7c77480656694
jong-dae <jdpark.sw@gmail.com>
2020-07-19 23:53:25 +0900

    test(TDD): MapControllerTest 추가

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -70,4 +70,7 @@ public class LineService {
                 .map(it -> LineStationResponse.of(it, StationResponse.of(stations.get(it.getStationId()))))
                 .collect(Collectors.toList());
     }
+
+	public void findAllLinesForMaps() {
+	}
 }
