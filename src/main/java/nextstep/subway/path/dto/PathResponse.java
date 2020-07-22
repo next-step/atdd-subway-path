2acece22c9d80bab6fcbeaffc1cba9bffbbf0c89
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-23 08:24:28 +0900

    refactor: remove Weight property in PathResponse

--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -8,20 +8,17 @@ import nextstep.subway.station.dto.StationResponse;
 
 public class PathResponse {
     private final List<StationResponse> stationResponses;
-    private final Double weight;
     private final Integer duration;
     private final Integer distance;
 
-    public PathResponse(List<StationResponse> stationResponses, Double weight, Integer duration, Integer distance) {
+    public PathResponse(List<StationResponse> stationResponses, Integer duration, Integer distance) {
         this.stationResponses = stationResponses;
-        this.weight = weight;
         this.duration = duration;
         this.distance = distance;
     }
 
     private PathResponse() {
         this.stationResponses = Collections.emptyList();
-        this.weight = 0.0;
         this.duration = 0;
         this.distance = 0;
     }
@@ -30,16 +27,12 @@ public class PathResponse {
         return new PathResponse();
     }
 
-    public static PathResponse of(List<StationResponse> stations, Double weight, int duration, int distance) {
-        return new PathResponse(stations, weight, duration, distance);
+    public static PathResponse of(List<StationResponse> stations, int duration, int distance) {
+        return new PathResponse(stations, duration, distance);
     }
 
     public List<StationResponse> getStationResponses() {
         return Optional.ofNullable(stationResponses).orElseThrow(() -> new RuntimeException("not found response"));
-    }
-
-    public Double getWeight() {
-        return Optional.ofNullable(weight).orElse(0.0);
     }
 
     public Integer getDuration() {
