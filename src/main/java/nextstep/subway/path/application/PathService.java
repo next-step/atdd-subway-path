8c980aefb693d62400fb81396ae1fe2c3b80e9f1
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-22 11:39:09 +0900

    refactor: findShortestPath to include duration and distance in the result

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Service;
 
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.dto.LineStationResponse;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.path.dto.ShortestPathResult;
+import nextstep.subway.station.dto.StationResponse;
 
 @Service
 public class PathService {
@@ -23,9 +25,29 @@ public class PathService {
 
     public PathResponse findShortestPath(Long startId, Long endId, ShortestPathSearchType type) {
         List<LineResponse> allLines = lineService.findAllLines();
-        ShortestPathResult pathResponse = Optional.ofNullable(
-            shortestPathFinder.findShortestPath(allLines, startId, endId, type))
-            .orElse(ShortestPathResult.empty());
-        return PathResponse.of(pathResponse);
+        ShortestPathResult result =
+            Optional.ofNullable(shortestPathFinder.findShortestPath(allLines, startId, endId, type))
+                .orElse(ShortestPathResult.empty());
+        List<StationResponse> stations = result.getStations();
+        int totalDuration = totalDurationCalculator(result, allLines);
+        int totalDistance = totalDistanceCalculator(result, allLines);
+
+        return PathResponse.of(stations, result.getWeight(), totalDuration, totalDistance);
+    }
+
+    private int totalDurationCalculator(ShortestPathResult result, List<LineResponse> allLines) {
+        List<LineStationResponse> lineStationResponses = result.toLineStationResponses(allLines);
+        return lineStationResponses.stream()
+            .skip(1) // 자기 자신은 제외
+            .mapToInt(LineStationResponse::getDuration)
+            .sum();
+    }
+
+    private int totalDistanceCalculator(ShortestPathResult result, List<LineResponse> allLines) {
+        List<LineStationResponse> lineStationResponses = result.toLineStationResponses(allLines);
+        return lineStationResponses.stream()
+            .skip(1) // 자기 자신은 제외
+            .mapToInt(LineStationResponse::getDuration)
+            .sum();
     }
 }
