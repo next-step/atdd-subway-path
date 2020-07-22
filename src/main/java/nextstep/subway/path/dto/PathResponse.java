e274f714090135239a21a68536764e61e115fa82
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-22 11:35:10 +0900

    refactor: PathResponse to hold duration and distance properties

--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -9,23 +9,29 @@ import nextstep.subway.station.dto.StationResponse;
 public class PathResponse {
     private final List<StationResponse> stationResponses;
     private final Double weight;
+    private final int duration;
+    private final int distance;
 
-    public PathResponse(List<StationResponse> stationResponses, Double weight) {
+    public PathResponse(List<StationResponse> stationResponses, Double weight, int duration, int distance) {
         this.stationResponses = stationResponses;
         this.weight = weight;
+        this.duration = duration;
+        this.distance = distance;
     }
 
     private PathResponse() {
         this.stationResponses = Collections.emptyList();
         this.weight = 0.0;
+        this.duration = 0;
+        this.distance = 0;
     }
 
     public static PathResponse empty() {
         return new PathResponse();
     }
 
-    public static PathResponse of(ShortestPathResult pathResponse) {
-        return new PathResponse(pathResponse.getStations(), pathResponse.getWeight());
+    public static PathResponse of(List<StationResponse> stations, Double weight, int duration, int distance) {
+        return new PathResponse(stations, weight, duration, distance);
     }
 
     public List<StationResponse> getStationResponses() {
@@ -34,5 +40,13 @@ public class PathResponse {
 
     public Double getWeight() {
         return Optional.ofNullable(weight).orElse(0.0);
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 }
