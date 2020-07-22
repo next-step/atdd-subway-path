c8f23c1d32556af57d6d896d843cddb0d4f50d16
etff <wish2rich@gmail.com>
2020-07-22 21:39:21 +0900

    Feature(path): add find path method

--- src/main/java/nextstep/subway/map/application/Graph.java
+++ src/main/java/nextstep/subway/map/application/Graph.java
@@ -15,7 +15,23 @@ import java.util.stream.Collectors;
 public class Graph {
 
     public PathResult findPath(List<LineResponse> lineResponses, Long start, Long target) {
-        return null;
+        WeightedMultigraph<Long, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+
+        lineResponses.stream()
+                .flatMap(it -> it.getStations().stream())
+                .map(it -> it.getStation())
+                .collect(Collectors.toList())
+                .forEach(it -> graph.addVertex(it.getId()));
+
+        lineResponses.stream()
+                .flatMap(it -> it.getStations().stream())
+                .filter(it -> it.getPreStationId() != null)
+                .forEach(it -> graph.setEdgeWeight(graph.addEdge(it.getPreStationId(), it.getStation().getId()), it.getDistance()));
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        GraphPath result = dijkstraShortestPath.getPath(start, target);
+
+        return new PathResult(result.getVertexList(), result.getWeight());
     }
 
 }
