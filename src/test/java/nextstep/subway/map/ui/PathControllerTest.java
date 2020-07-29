7d43619cd04546abe5974df0a0fccc1961ea48af
etff <wish2rich@gmail.com>
2020-07-29 22:13:26 +0900

    test(path) : add constant controller test

--- src/test/java/nextstep/subway/map/ui/PathControllerTest.java
+++ src/test/java/nextstep/subway/map/ui/PathControllerTest.java
@@ -12,6 +12,8 @@ import static org.mockito.Mockito.verify;
 
 class PathControllerTest {
 
+    public static final long SOURCE = 1L;
+    public static final long TARGET = 3L;
     private PathService pathService;
     private PathController pathController;
 
@@ -24,10 +26,10 @@ class PathControllerTest {
     @Test
     void findShortDistance() {
         // when
-        ResponseEntity entity = pathController.findShortPath(1L, 3L);
+        ResponseEntity entity = pathController.findShortPath(SOURCE, TARGET);
 
         // then
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        verify(pathService).findPath(1L, 3L);
+        verify(pathService).findPath(SOURCE, TARGET);
     }
 }
