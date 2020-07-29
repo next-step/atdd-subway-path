b0f403c1b035f4cad4f4a2ec65a9fccd9f856cc3
etff <wish2rich@gmail.com>
2020-07-29 21:50:49 +0900

    test(path): add acceptance test

--- src/test/java/nextstep/subway/map/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/map/acceptance/PathAcceptanceTest.java
@@ -1,30 +1,18 @@
 package nextstep.subway.map.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.station.dto.StationResponse;
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
-
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import static nextstep.subway.line.acceptance.step.LineAcceptanceStep.지하철_노선_등록되어_있음;
 import static nextstep.subway.line.acceptance.step.LineStationAcceptanceStep.지하철_노선에_지하철역_등록되어_있음;
-import static nextstep.subway.map.acceptance.step.MapAcceptanceStep.*;
-import static nextstep.subway.map.acceptance.step.MapAcceptanceStep.지하철_노선도에_노선별_지하철역_순서_정렬됨;
+import static nextstep.subway.map.acceptance.step.PathAcceptanceStep.출발역에서_도착역까지의_최단거리_경로_조회를_요청;
 import static nextstep.subway.station.acceptance.step.StationAcceptanceStep.지하철역_등록되어_있음;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,6 +21,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
 
     private Long lineId1;
     private Long lineId2;
+    private Long lineId3;
     private Long stationId1;
     private Long stationId2;
     private Long stationId3;
@@ -42,29 +31,42 @@ public class PathAcceptanceTest extends AcceptanceTest {
     public void setUp() {
         super.setUp();
 
-        // given
         ExtractableResponse<Response> createLineResponse1 = 지하철_노선_등록되어_있음("2호선", "GREEN");
-        ExtractableResponse<Response> createLineResponse2 = 지하철_노선_등록되어_있음("신분당성", "RED");
-        ExtractableResponse<Response> createdStationResponse1 = 지하철역_등록되어_있음("강남역");
-        ExtractableResponse<Response> createdStationResponse2 = 지하철역_등록되어_있음("역삼역");
-        ExtractableResponse<Response> createdStationResponse3 = 지하철역_등록되어_있음("선릉역");
-        ExtractableResponse<Response> createdStationResponse4 = 지하철역_등록되어_있음("양재역");
+        ExtractableResponse<Response> createLineResponse2 = 지하철_노선_등록되어_있음("3호선", "ORANGE");
+        ExtractableResponse<Response> createLineResponse3 = 지하철_노선_등록되어_있음("4호선", "BLUE");
+
+        ExtractableResponse<Response> createdStationResponse1 = 지하철역_등록되어_있음("을지로4가");
+        ExtractableResponse<Response> createdStationResponse2 = 지하철역_등록되어_있음("을지로3가");
+        ExtractableResponse<Response> createdStationResponse3 = 지하철역_등록되어_있음("충무로역");
+        ExtractableResponse<Response> createdStationResponse4 = 지하철역_등록되어_있음("동대문역");
 
         lineId1 = createLineResponse1.as(LineResponse.class).getId();
         lineId2 = createLineResponse2.as(LineResponse.class).getId();
+        lineId3 = createLineResponse3.as(LineResponse.class).getId();
+
         stationId1 = createdStationResponse1.as(StationResponse.class).getId();
         stationId2 = createdStationResponse2.as(StationResponse.class).getId();
         stationId3 = createdStationResponse3.as(StationResponse.class).getId();
         stationId4 = createdStationResponse4.as(StationResponse.class).getId();
 
-        지하철_노선에_지하철역_등록되어_있음(lineId1, null, stationId1);
-        지하철_노선에_지하철역_등록되어_있음(lineId1, stationId1, stationId2);
-        지하철_노선에_지하철역_등록되어_있음(lineId1, stationId2, stationId3);
-        지하철_노선에_지하철역_등록되어_있음(lineId2, null, stationId1);
-        지하철_노선에_지하철역_등록되어_있음(lineId2, stationId1, stationId4);
+        지하철_노선에_지하철역_등록되어_있음(lineId1, null, stationId1, 0, 0);
+        지하철_노선에_지하철역_등록되어_있음(lineId1, stationId1, stationId2, 2, 2);
+        지하철_노선에_지하철역_등록되어_있음(lineId1, stationId2, stationId3, 2, 4);
+
+        지하철_노선에_지하철역_등록되어_있음(lineId2, null, stationId2, 0, 0);
+        지하철_노선에_지하철역_등록되어_있음(lineId2, stationId2, stationId4, 2, 2);
+
+        지하철_노선에_지하철역_등록되어_있음(lineId3, null, stationId1, 0, 0);
+        지하철_노선에_지하철역_등록되어_있음(lineId3, stationId1, stationId4, 2, 2);
+        지하철_노선에_지하철역_등록되어_있음(lineId3, stationId4, stationId3, 3, 2);
     }
 
-    @DisplayName("두 역의 최단 거리 경로를 조회")
+    /**
+     * line1 : 을지로 3가 -> 충무로  2 + 2  = 4
+     * line3 : 동대문 -> 충무로 -> 3 : 2 + 3  = 5
+     */
+
+    @DisplayName("최단 거리 검색")
     @Test
     void shortestPath() {
         // when
@@ -72,20 +74,8 @@ public class PathAcceptanceTest extends AcceptanceTest {
 
         // then
         PathResponse pathResponse = response.as(PathResponse.class);
-
-        assertThat(pathResponse.getDistance()).isEqualTo(3);
+        assertThat(pathResponse.getDistance()).isEqualTo(4);
     }
 
-    private ExtractableResponse<Response> 출발역에서_도착역까지의_최단거리_경로_조회를_요청(Long sourceId, Long targetId) {
-        return RestAssured
-                .given()
-                    .log().all()
-                    .accept(MediaType.APPLICATION_JSON_VALUE).
-                when().
-                        get("/paths?source={sourceId}&target={targetId}", sourceId, targetId).
-                then().
-                        log().all().
-                        extract();
-    }
 
 }
