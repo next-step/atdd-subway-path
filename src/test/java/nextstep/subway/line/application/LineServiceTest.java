65279001f9343a14b6745750ae8c72934437cd03
catsbi <killfine1988@gmail.com>
2021-03-10 23:34:06 +0900

    [feat] LineServiceTest addSection 테스트 작성

--- src/test/java/nextstep/subway/line/application/LineServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineServiceTest.java
@@ -1,11 +1,19 @@
 package nextstep.subway.line.application;
 
 import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.dto.SectionRequest;
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -22,11 +30,23 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 금정역 = stationRepository.save(new Station("금정역"));
+        Station 범계역 = stationRepository.save(new Station("범계역"));
+        Station 평촌역 = stationRepository.save(new Station("평촌역"));
+        LineResponse lineResponse = lineService.saveLine(new LineRequest("사호선", "skyblue", 금정역.getId(), 범계역.getId(), 10));
 
         // when
         // lineService.addSection 호출
+        SectionRequest sectionRequest = SectionRequest.Builder()
+                .upStationId(범계역.getId())
+                .downStationId(평촌역.getId())
+                .distance(6)
+                .build();
+        lineService.addSection(lineResponse.getId(),sectionRequest);
+        LineResponse resultResponse = lineService.findLineResponseById(lineResponse.getId());
 
         // then
         // line.getSections 메서드를 통해 검증
+        assertThat(resultResponse.getStations()).hasSize(3);
     }
 }
