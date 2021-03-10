1cfb04534af4cefe6cc1db497c56836ee1c13b44
catsbi <killfine1988@gmail.com>
2021-03-10 23:09:52 +0900

    [feat] addSection 단위 테스트 완성

--- src/test/java/nextstep/subway/line/application/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/line/application/LineServiceMockTest.java
@@ -2,8 +2,11 @@ package nextstep.subway.line.application;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +14,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
@@ -44,11 +53,24 @@ public class LineServiceMockTest {
     void addSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        when(lineRepository.findById(이호선.getId())).thenReturn(Optional.of(이호선));
+        when(stationService.findStationById(역삼역.getId())).thenReturn(역삼역);
+        when(stationService.findStationById(삼성역.getId())).thenReturn(삼성역);
+        LineService lineService = new LineService(lineRepository, stationService);
 
         // when
         // lineService.addSection 호출
+        SectionRequest sectionRequest = SectionRequest.Builder()
+                .distance(6)
+                .upStationId(역삼역.getId())
+                .downStationId(삼성역.getId())
+                .build();
+
+        lineService.addSection(이호선.getId(), sectionRequest);
 
         // then
         // line.findLineById 메서드를 통해 검증
+        LineResponse response = lineService.findLineResponseById(이호선.getId());
+        assertThat(response.getStations().size()).isEqualTo(3);
     }
 }
