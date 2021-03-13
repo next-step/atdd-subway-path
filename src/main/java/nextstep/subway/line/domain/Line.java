edc782804b9fab8c47c7a1b9b7b66ac449ff7ae7
decaffeine <decaffeine12@gmail.com>
2021-03-13 18:05:25 +0900

    test: LineTest (도메인으로 로직 옮기기)

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -54,7 +54,77 @@ public class Line extends BaseEntity {
     }
 
     public void addSection(Station upStation, Station downStation, int distance) {
+        if (getStations().size() == 0) {
+            sections.add(new Section(this, upStation, downStation, distance));
+            return;
+        }
+        checkSectionAddValidty(upStation, downStation);
+        sections.add(new Section(this, upStation, downStation, distance));
+    }
 
+    private void checkSectionAddValidty(Station upStation, Station downStation) {
+        if (getStations().get(getStations().size() - 1) != upStation) {
+            throw new RuntimeException("상행역은 하행 종점역이어야 합니다.");
+        }
+
+        if (getStations().stream().anyMatch(it -> it == downStation)) {
+            throw new RuntimeException("하행역이 이미 등록되어 있습니다.");
+        }
+    }
+
+    public List<Station> getStations() {
+        List<Station> stations = new ArrayList<>();
+        if (getSections().isEmpty()) {
+            return stations;
+        }
+
+        Station downStation = findUpStation();
+        stations.add(downStation);
+
+        while (downStation != null) {
+            Station finalDownStation = downStation;
+            Optional<Section> nextLineStation = getSections().stream()
+                    .filter(it -> it.getUpStation() == finalDownStation)
+                    .findFirst();
+            if (!nextLineStation.isPresent()) {
+                break;
+            }
+            downStation = nextLineStation.get().getDownStation();
+            stations.add(downStation);
+        }
+        return stations;
+    }
+
+    private Station findUpStation() {
+        Station downStation = getSections().get(0).getUpStation();
+        while (downStation != null) {
+            Station finalDownStation = downStation;
+            Optional<Section> nextLineStation = getSections().stream()
+                    .filter(it -> it.getDownStation() == finalDownStation)
+                    .findFirst();
+            if (!nextLineStation.isPresent()) {
+                break;
+            }
+            downStation = nextLineStation.get().getUpStation();
+        }
+        return downStation;
+    }
+
+    public void removeSection(Long stationId) {
+        checkSectionRemoveValidity(stationId);
+        getSections().stream()
+                .filter(it -> it.getDownStation().getId() == stationId)
+                .findFirst()
+                .ifPresent(it -> getSections().remove(it));
+    }
+
+    private void checkSectionRemoveValidity(Long stationId) {
+        if (getSections().size() <= 1) {
+            throw new RuntimeException();
+        }
+        if (getStations().get(getStations().size() - 1).getId() != stationId) {
+            throw new RuntimeException("하행 종점역만 삭제가 가능합니다.");
+        }
     }
 
     @Override
--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -1,10 +1,12 @@
 package nextstep.subway.line.domain;
 
+import io.restassured.internal.assertion.Assertion;
 import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LineTest {
     @Test
@@ -23,19 +25,47 @@ public class LineTest {
     @DisplayName("목록 중간에 추가할 경우 에러 발생")
     @Test
     void addSectionInMiddle() {
+
     }
 
     @DisplayName("이미 존재하는 역 추가 시 에러 발생")
     @Test
     void addSectionAlreadyIncluded() {
+        Line line = new Line("2호선","green");
+        Station upStation = new Station("홍대입구역");
+        Station downStation = new Station("신촌역");
+        line.addSection(upStation, downStation, 5);
+        assertThatThrownBy(() -> line.addSection(upStation, downStation, 3)).isInstanceOf(RuntimeException.class);
     }
 
     @Test
     void removeSection() {
+        // given
+        Line line = new Line("2호선","green");
+        Station upStation = new Station("홍대입구역");
+        Station downStation = new Station("신촌역");
+        line.addSection(upStation, downStation, 5);
+        Station newDownStation = new Station("이대역");
+        line.addSection(downStation, newDownStation, 3);
+        assertThat(line.getSections().size()).isEqualTo(2);
+
+        // when
+        line.removeSection(newDownStation.getId());
+
+        // then
+        assertThat(line.getSections().size()).isEqualTo(1);
     }
 
     @DisplayName("구간이 하나인 노선에서 역 삭제 시 에러 발생")
     @Test
     void removeSectionNotEndOfList() {
+        // given
+        Line line = new Line("2호선","green");
+        Station upStation = new Station("홍대입구역");
+        Station downStation = new Station("신촌역");
+        line.addSection(upStation, downStation, 5);
+
+        // when, then
+        assertThatThrownBy(() -> line.removeSection(downStation.getId())).isInstanceOf(RuntimeException.class);
     }
 }
