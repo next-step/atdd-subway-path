85ca939d5136671e2b9a52dcb23c818f657b7f6f
임한결 <dlagksruf19@naver.com>
2021-03-14 21:00:08 +0900

    feat : LineTest 단위 테스트 기능 구현 (실습)

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -1,6 +1,6 @@
 package nextstep.subway.line.domain;
 
-import nextstep.subway.line.dto.SectionRequest;
+import nextstep.subway.line.exception.CannotRemoveSectionException;
 import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,67 +15,57 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 @DisplayName("노선 Domain 테스트")
 public class LineTest {
 
-    private Station 강남역;
-    private Station 역삼역;
-    private Station 삼성역;
-    private Line 이호선;
+    private Station savedStationGangnam;
+    private Station savedStationYeoksam;
+    private Station savedStationSamseong;
+    private Line line2;
 
     @BeforeEach
     void setUp() {
-        강남역 = new Station("강남역");
-        역삼역 = new Station("역삼역");
-        삼성역 = new Station("삼성역");
-        이호선 = new Line("2호선", "bg-green-600", 강남역, 역삼역, 10);
+        savedStationGangnam = new Station(1L, "강남역");
+        savedStationYeoksam = new Station(2L, "역삼역");
+        savedStationSamseong = new Station(3L, "삼성역");
+        line2 = new Line(1L, "2호선", "bg-green-600");
+        line2.addSection(savedStationGangnam, savedStationYeoksam, 10);
     }
 
     @Test
     @DisplayName("노선의 구간에 있는 역들을 가져오기")
     void getStations() {
-        List<Station> stations = 이호선.getStations();
+        List<Station> stations = line2.getStations();
         assertThat(stations).hasSize(2);
     }
 
     @Test
     @DisplayName("노선에 구간 추가하기")
     void addSection() {
-        SectionRequest sectionRequest = new SectionRequest(역삼역.getId(), 삼성역.getId(), 6);
-        이호선.addSection(sectionRequest);
-        assertThat(이호선.getStations()).containsExactlyElementsOf(Arrays.asList(강남역, 역삼역, 삼성역));
+        line2.addSection(savedStationYeoksam, savedStationSamseong, 6);
+        assertThat(line2.getStations()).containsExactlyElementsOf(Arrays.asList(savedStationGangnam, savedStationYeoksam, savedStationSamseong));
     }
 
     @Test
     @DisplayName("노선 중간에 구간을 추가할 경우 에러 발생")
     void addSectionInMiddle() {
-        assertThatExceptionOfType(CannotAddSectionException.class)
-                .isThrownBy(() -> {
-                    SectionRequest sectionRequest = new SectionRequest(강남역.getId(), 선릉역.getId(), 20);
-                    이호선.addSection(sectionRequest);
-                }).withMessage("노선 중간에 구간을 추가할 수 없습니다.");
+
     }
 
     @Test
     @DisplayName("노선에 존재하는 구간 추가 시 에러 발생")
     void addSectionAlreadyIncluded() {
-        assertThatExceptionOfType(CannotAddSectionException.class)
-                .isThrownBy(() -> {
-                    SectionRequest sectionRequest = new SectionRequest(강남역.getId(), 역삼역.getId(), 10);
-                    이호선.addSection(sectionRequest);
-                }).withMessage("이미 존재하는 구간입니다.");
     }
 
     @Test
     @DisplayName("노선에 있는 하행 종점역 구간 삭제")
     void removeSection() {
-        이호선.removeSection(1L);
-        assertThat(이호선.getSections()).isEmpty();
+        line2.addSection(savedStationYeoksam, savedStationSamseong, 6);
+        line2.deleteLastDownStation(savedStationSamseong.getId());
+        assertThat(line2.getSections()).hasSize(1);
     }
 
     @Test
-    @DisplayName("구간이 하나인 노선에서 역 삭제 시 에러 발생")
+    @DisplayName("구간이 하나인 노선에서 역(하행 종점역) 삭제 시 에러 발생")
     void removeSectionNotEndOfList() {
         assertThatExceptionOfType(CannotRemoveSectionException.class)
-                .isThrownBy(() -> {
-                    이호선.removeSection(1L);
-                });
+                .isThrownBy(() -> line2.deleteLastDownStation(savedStationYeoksam.getId()));
     }
 }
