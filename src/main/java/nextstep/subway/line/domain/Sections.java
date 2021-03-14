88dd86d74c1365acfaadf9e2ab34dd17730b1da0
catsbi <killfine1988@gmail.com>
2021-03-15 01:03:08 +0900

    [refactor] Sections forEach 메소드 추가 및 StationResponse equals비교를위한 hash&equal 메소드 생성

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -217,5 +218,9 @@ public class Sections {
         if (sectionList.size() == 1) {
             throw new OnlyOneSectionException();
         }
+    }
+
+    public void forEach(Consumer<Section> consumer) {
+        sectionList.forEach(consumer);
     }
 }
