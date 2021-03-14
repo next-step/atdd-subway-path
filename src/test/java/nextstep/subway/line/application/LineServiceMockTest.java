c225ad98fddbc8e6a1440910cf5527d4f293491d
HyunSung-Na <oceana57@naver.com>
2021-03-14 14:54:50 +0900

    step0 LineService에 있는 비즈니스 로직을 도메인 클래스로 리팩터링하기

--- src/test/java/nextstep/subway/line/application/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/line/application/LineServiceMockTest.java
@@ -1,9 +1,11 @@
 package nextstep.subway.line.application;
 
+import nextstep.subway.error.NotFoundException;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +13,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
@@ -44,11 +54,19 @@ public class LineServiceMockTest {
     void addSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
-
+        when(stationService.findByStation(역삼역.getId())).thenReturn(역삼역);
+        when(stationService.findByStation(삼성역.getId())).thenReturn(삼성역);
+        when(lineRepository.findById(anyLong())).thenReturn(Optional.of(이호선));
         // when
         // lineService.addSection 호출
+        lineService.addSection(이호선, 역삼역.getId(), 삼성역.getId(), 10);
 
         // then
         // line.findLineById 메서드를 통해 검증
+        Line responses = lineRepository.findById(이호선.getId())
+                .orElseThrow(() -> new NotFoundException(이호선.getId()));
+
+        assertThat(responses.getSections().getSections()).hasSize(2);
     }
+
 }
--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -1,33 +1,81 @@
 package nextstep.subway.line.domain;
 
+import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 public class LineTest {
+
     @Test
     void getStations() {
     }
 
     @Test
     void addSection() {
+        Line line = new Line("2호선","green");
+        Station upStation = new Station("강남역");
+        Station downStation = new Station("교대역");
+        line.addSection(upStation, downStation, 10);
+        assertThat(line.getSections().size()).isEqualTo(1);
     }
 
     @DisplayName("목록 중간에 추가할 경우 에러 발생")
     @Test
     void addSectionInMiddle() {
+        Line line = new Line("2호선","green");
+        Station upStation = new Station("강남역");
+        Station downStation = new Station("교대역");
+        line.addSection(upStation, downStation, 10);
+        Station middleStation = new Station("삼성역");
+        assertThatThrownBy(() -> line.addSection(upStation, downStation, 10))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("추가하는 구간의 상행역이 잘못되었습니다.");
+
     }
 
     @DisplayName("이미 존재하는 역 추가 시 에러 발생")
     @Test
     void addSectionAlreadyIncluded() {
+        Line line = new Line("2호선","green");
+        Station upStation = new Station("강남역");
+        Station downStation = new Station("교대역");
+        line.addSection(upStation, downStation, 10);
+        assertThatThrownBy(() -> line.addSection(upStation, downStation, 10))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("추가하는 구간의 상행역이 잘못되었습니다.");
     }
 
     @Test
     void removeSection() {
+        // given
+        Line line = new Line("2호선","green");
+        Station upStation = new Station("강남역");
+        Station downStation = new Station("교대역");
+        line.addSection(upStation, downStation, 10);
+        Station newDownStation = new Station("삼성역");
+        line.addSection(downStation, newDownStation, 10);
+        assertThat(line.getSections().size()).isEqualTo(2);
+
+        // when
+        line.removeSection(newDownStation);
+
+        // then
+        assertThat(line.getSections().size()).isEqualTo(1);
     }
 
     @DisplayName("구간이 하나인 노선에서 역 삭제 시 에러 발생")
     @Test
     void removeSectionNotEndOfList() {
+        // given
+        Line line = new Line("2호선","green");
+        Station upStation = new Station("강남역");
+        Station downStation = new Station("교대역");
+        line.addSection(upStation, downStation, 10);
+
+        // when, then
+        assertThatThrownBy(() -> line.removeSection(downStation)).isInstanceOf(RuntimeException.class);
     }
 }
