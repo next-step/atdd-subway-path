9e38e3bd55af766bb431ae8ff059dd37c23441ec
pa324 <ms66225@nate.com>
2021-03-14 19:34:41 +0900

    refactor : private method 상단으로 이동

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -25,30 +25,6 @@ public class Sections {
   public Sections() {
   }
 
-  private void validateSection(boolean isRegisteredUpStation, boolean isRegisteredDownStation) {
-    if (isRegisteredDownStation && isRegisteredUpStation) {
-      throw new InvalidSectionException("등록하려는 구간의 상행역/하행역 모두 이미 등록되어있습니다.");
-    }
-
-    if (!isRegisteredDownStation && !isRegisteredUpStation) {
-      throw new InvalidSectionException("등록하려는 구간의 상행역과 하행역중 하나는 노선에 포함되어있어야합니다.");
-    }
-  }
-
-  private int calculateNewDistance(int newDistance, int oldDistance) {
-    return oldDistance - newDistance;
-  }
-
-  private void validateDistance(int newDistance, int oldDistance) {
-    if (newDistance >= oldDistance) {
-      throw new InvalidSectionException("기존 구간의 길이보다 길거나 같습니다.");
-    }
-  }
-
-  private void insertLastSection(Line line, Station upStation, Station downStation, int distance) {
-    sections.add(new Section(line, upStation, downStation, distance));
-  }
-
   public void add(Line line, Station upStation, Station downStation, int distance) {
     if (sections.isEmpty()) {
       insertLastSection(line, upStation, downStation, distance);
@@ -67,6 +43,47 @@ public class Sections {
       append(line, upStation, downStation, distance);
     }
 
+  }
+
+  public void remove(long stationId) {
+    if (getSize() == EMPTY) {
+      throw new InvalidSectionException("삭제할 구간이 없습니다.");
+    }
+    if (getSize() == MIN) {
+      throw new InvalidSectionException("구간이 1개남은경우 삭제할 수 없습니다.");
+    }
+    if (!isLastStation(stationId)) {
+      throw new InvalidSectionException("노선의 종점이 아닌경우 삭제할 수 없습니다.");
+    }
+    sections.remove(lastIndex());
+  }
+
+  public int getSize() {
+    return this.sections.size();
+  }
+
+  public boolean isLastStation(long stationId) {
+    return sections.get(lastIndex())
+        .getDownStation().getId()
+        .equals(stationId);
+  }
+
+  public List<Station> getSortedStations() {
+    return sections.stream()
+        .sorted()
+        .flatMap(section -> Stream.of(section.getUpStation(), section.getDownStation()))
+        .distinct()
+        .collect(Collectors.toList());
+  }
+
+  public List<Section> getSortedSection() {
+    return sections.stream().sorted().collect(Collectors.toList());
+  }
+
+  public boolean isRegisteredStation(Station station) {
+    return sections.stream()
+        .anyMatch(section -> Objects.equals(section.getUpStation(), station)
+            || Objects.equals(section.getDownStation(), station));
   }
 
   private Optional<Section> findMatchedSectionByDownStation(Station downStation) {
@@ -108,50 +125,31 @@ public class Sections {
         .add(targetIndex + 1, new Section(line, downStation, target.getDownStation(), newDistance));
   }
 
-  public void remove(long stationId) {
-    if (getSize() == EMPTY) {
-      throw new InvalidSectionException("삭제할 구간이 없습니다.");
+  private void validateSection(boolean isRegisteredUpStation, boolean isRegisteredDownStation) {
+    if (isRegisteredDownStation && isRegisteredUpStation) {
+      throw new InvalidSectionException("등록하려는 구간의 상행역/하행역 모두 이미 등록되어있습니다.");
     }
-    if (getSize() == MIN) {
-      throw new InvalidSectionException("구간이 1개남은경우 삭제할 수 없습니다.");
+
+    if (!isRegisteredDownStation && !isRegisteredUpStation) {
+      throw new InvalidSectionException("등록하려는 구간의 상행역과 하행역중 하나는 노선에 포함되어있어야합니다.");
     }
-    if (!isLastStation(stationId)) {
-      throw new InvalidSectionException("노선의 종점이 아닌경우 삭제할 수 없습니다.");
-    }
-    sections.remove(lastIndex());
   }
 
-  public int getSize() {
-    return this.sections.size();
+  private int calculateNewDistance(int newDistance, int oldDistance) {
+    return oldDistance - newDistance;
+  }
+
+  private void validateDistance(int newDistance, int oldDistance) {
+    if (newDistance >= oldDistance) {
+      throw new InvalidSectionException("기존 구간의 길이보다 길거나 같습니다.");
+    }
+  }
+
+  private void insertLastSection(Line line, Station upStation, Station downStation, int distance) {
+    sections.add(new Section(line, upStation, downStation, distance));
   }
 
   private int lastIndex() {
     return getSize() - 1;
-  }
-
-
-  public boolean isLastStation(long stationId) {
-    return sections.get(lastIndex())
-        .getDownStation().getId()
-        .equals(stationId);
-  }
-
-
-  public List<Station> getSortedStations() {
-    return sections.stream()
-        .sorted()
-        .flatMap(section -> Stream.of(section.getUpStation(), section.getDownStation()))
-        .distinct()
-        .collect(Collectors.toList());
-  }
-
-  public List<Section> getSortedSection() {
-    return sections.stream().sorted().collect(Collectors.toList());
-  }
-
-  public boolean isRegisteredStation(Station station) {
-    return sections.stream()
-        .anyMatch(section -> Objects.equals(section.getUpStation(), station)
-            || Objects.equals(section.getDownStation(), station));
   }
 }
