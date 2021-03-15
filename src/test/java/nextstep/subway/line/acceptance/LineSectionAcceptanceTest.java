ed259fadf1b5ce563a69c45dc53a1df35c2127f2
Kaeum <oror1024@gmail.com>
2021-03-15 16:38:20 +0900

    test: 새로운 역을 하행 종점으로 등록하는 경우의 인수테스트 작성

--- src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
@@ -103,8 +103,23 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
         지하철_노선에_지하철역_순서_정렬됨(response, Arrays.asList(강남역, 양재역, 광교역));
     }
 
-    /*
+
     @DisplayName("새로운 역을 하행 종점으로 등록한다.")
+    @Test
+    void addNewStationAsLast() {
+        //given
+        //강남----------양재-----정자
+        지하철_노선에_지하철역_등록_요청(신분당선, 양재역, 정자역, 5);
+
+        //when
+        //강남----------양재-----정자-----광교
+        지하철_노선에_지하철역_등록_요청(신분당선, 정자역, 광교역, 5);
+
+        //then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        지하철_노선에_지하철역_순서_정렬됨(response, Arrays.asList(강남역, 양재역, 정자역, 광교역));
+    }
+    /*
     @DisplayName("역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없음.")
     @DisplayName("상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음.")
     @DisplayName("상행역과 하행역 둘 중 하나도 포함되어있지 않으면 추가할 수 없음")
