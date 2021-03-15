7bb9aaef0ac22ca2a52892b2e39d188dde8ebd76
bbbnam <bbbnam@naver.com>
2021-03-15 20:59:42 +0900

    feat : LineService -> Line으로 비즈니스 로직 이동

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -53,6 +53,81 @@ public class Line extends BaseEntity {
         return sections;
     }
 
+    public List<Station> getStations() {
+        if (sections.isEmpty()) {
+            return Arrays.asList();
+        }
+
+        List<Station> stations = new ArrayList<>();
+        Station downStation = findUpStation();
+        stations.add(downStation);
+
+        while (downStation != null) {
+            Station finalDownStation = downStation;
+            Optional<Section> nextLineStation = sections.stream()
+                    .filter(it -> it.getUpStation() == finalDownStation)
+                    .findFirst();
+            if (!nextLineStation.isPresent()) {
+                break;
+            }
+            downStation = nextLineStation.get().getDownStation();
+            stations.add(downStation);
+        }
+
+        return stations;
+    }
+
+    private Station findUpStation() {
+        Station downStation = sections.get(0).getUpStation();
+        while (downStation != null) {
+            Station finalDownStation = downStation;
+            Optional<Section> nextLineStation = sections.stream()
+                    .filter(it -> it.getDownStation() == finalDownStation)
+                    .findFirst();
+            if (!nextLineStation.isPresent()) {
+                break;
+            }
+            downStation = nextLineStation.get().getUpStation();
+        }
+
+        return downStation;
+    }
+
+    public void addSection(Station upStation, Station downStation, int distance) {
+        if (getStations().size() == 0) {
+            sections.add(new Section(this, upStation, downStation, distance));
+            return;
+        }
+
+        boolean isNotValidUpStation = getStations().get(getStations().size() - 1) != upStation;
+        if (isNotValidUpStation) {
+            throw new RuntimeException("상행역은 하행 종점역이어야 합니다.");
+        }
+
+        boolean isDownStationExisted = getStations().stream().anyMatch(it -> it == downStation);
+        if (isDownStationExisted) {
+            throw new RuntimeException("하행역이 이미 등록되어 있습니다.");
+        }
+
+        sections.add(new Section(this, upStation, downStation, distance));
+    }
+
+    public void removeSection(Long stationId) {
+        if (sections.size() <= 1) {
+            throw new RuntimeException();
+        }
+
+        boolean isNotValidUpStation = getStations().get(getStations().size() - 1).getId() != stationId;
+        if (isNotValidUpStation) {
+            throw new RuntimeException("하행 종점역만 삭제가 가능합니다.");
+        }
+
+        sections.stream()
+                .filter(it -> it.getDownStation().getId() == stationId)
+                .findFirst()
+                .ifPresent(it -> sections.remove(it));
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
