261db5dcd5d6008b71275496968902a1d9e68c28
Dong Hwan <bkuekany@gmail.com>
2021-03-16 21:26:07 +0900

    feat : 인수조건 추가

--- src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
@@ -20,11 +20,23 @@ import static nextstep.subway.line.acceptance.LineSteps.*;
 import static nextstep.subway.station.StationSteps.지하철역_등록되어_있음;
 import static org.assertj.core.api.Assertions.assertThat;
 
+
+/**
+ * 구간 추가 기능 변경 인수테스트
+ * 1. 역 사이에 새로운 역을 등록할 수 있다.음
+ *  - 기존 역 사이의 길이보다 크거나 같으면 등록 할 수 없음
+ *  - 상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음
+ *  - 상행역과 하행역 둘 중 하나도 포함되어 있지 않으면 추가할 수 없음
+ * 2. 새로운 역을 상행 종점으로 등록 할 수 있다.
+ * 3. 새로운 역을 하행 종점으로 등록 할 수 있다.
+ */
+
 @DisplayName("지하철 노선에 역 등록 관련 기능")
 public class LineSectionAcceptanceTest extends AcceptanceTest {
     private LineResponse 신분당선;
     private StationResponse 강남역;
     private StationResponse 양재역;
+    private StationResponse 판교역;
     private StationResponse 정자역;
     private StationResponse 광교역;
 
@@ -34,6 +46,7 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
 
         강남역 = 지하철역_등록되어_있음("강남역").as(StationResponse.class);
         양재역 = 지하철역_등록되어_있음("양재역").as(StationResponse.class);
+        판교역 = 지하철역_등록되어_있음("판교역").as(StationResponse.class);
         정자역 = 지하철역_등록되어_있음("정자역").as(StationResponse.class);
         광교역 = 지하철역_등록되어_있음("광교역").as(StationResponse.class);
 
@@ -57,6 +70,26 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
         지하철_노선에_지하철역_등록됨(response);
         지하철_노선에_지하철역_순서_정렬됨(response, Arrays.asList(강남역, 양재역, 정자역));
+    }
+
+    @DisplayName("역 사이에 새로운 역을 등록할 수 있다.")
+    @Test
+    void addLineSectionBetweenStation() {
+    }
+
+    @DisplayName("기존 역 사이의 길이보다 크거나 같으면 등록 할 수 없음")
+    @Test
+    void cannotAddWhenDistanceBiggerThanRegistered() {
+    }
+
+    @DisplayName("상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음")
+    @Test
+    void cannotAddWhenUpStationAndDownStationBothRegistered(){
+    }
+
+    @DisplayName("상행역과 하행역 둘 중 하나도 포함되어 있지 않으면 추가할 수 없음")
+    @Test
+    void cannotAddContainsEither(){
     }
 
     @DisplayName("지하철 노선에 이미 포함된 역을 구간으로 등록한다.")
