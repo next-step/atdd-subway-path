2f1a50ce4a73f13bb5de97e249be0be242180706
Kaeum <oror1024@gmail.com>
2021-03-17 23:04:06 +0900

    feat: PathFinder 단위테스트 통과하도록 기능구현

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -1,18 +1,40 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.path.exception.SourceEqualsWithTargetException;
+import nextstep.subway.path.exception.StationNotExistsException;
+import nextstep.subway.path.exception.StationsNotConnectedException;
 import nextstep.subway.station.domain.Station;
+import org.apache.commons.lang3.ObjectUtils;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 
 import java.util.List;
+import java.util.Optional;
 
 public class PathFinder {
-    private WeightedMultigraph<Station, Integer> graph;
+    private WeightedMultigraph<Station, DefaultWeightedEdge> graph;
 
-    public PathFinder(WeightedMultigraph<Station, Integer> graph) {
+    public PathFinder(WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
         this.graph = graph;
     }
 
-    public List<Station> getShortestPath(Station 강남역, Station 교대역) {
-        return null;
+    public GraphPath getShortestPath(Station source, Station target) {
+        Optional<GraphPath> path;
+
+        if(source.equals(target)) {
+            throw new SourceEqualsWithTargetException();
+        }
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+
+        try {
+            path = Optional.ofNullable(dijkstraShortestPath.getPath(source, target));
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new StationNotExistsException();
+        }
+
+        return path.orElseThrow(StationsNotConnectedException::new);
     }
 }
--- src/test/java/nextstep/subway/path/domain/PathFinderTest.java
+++ src/test/java/nextstep/subway/path/domain/PathFinderTest.java
@@ -1,10 +1,13 @@
 package nextstep.subway.path.domain;
 
 import nextstep.subway.path.exception.SourceEqualsWithTargetException;
+import nextstep.subway.path.exception.StationNotExistsException;
 import nextstep.subway.path.exception.StationsNotConnectedException;
 import nextstep.subway.station.domain.Station;
+import org.jgrapht.GraphPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,42 +19,47 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PathFinderTest {
     private PathFinder pathFinder;
+    private WeightedMultigraph<Station, DefaultWeightedEdge> graph;
+    private Station 강남역;
+    private Station 역삼역;
+    private Station 교대역;
+    private Station 사당역;
+
+    @BeforeEach
+    void setUp() {
+        graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        강남역 = new Station("강남역");
+        역삼역 = new Station("역삼역");
+        교대역 = new Station("교대역");
+        사당역 = new Station("사당역");
+
+        graph.addVertex(강남역);
+        graph.addVertex(역삼역);
+        graph.addVertex(교대역);
+        graph.addVertex(사당역);
+        graph.setEdgeWeight(graph.addEdge(강남역, 역삼역), 3);
+        graph.setEdgeWeight(graph.addEdge(역삼역, 교대역), 7);
+        graph.setEdgeWeight(graph.addEdge(강남역, 교대역), 100);
+    }
 
     @DisplayName("최단 경로를 조회한다.")
     @Test
     void getShortestPathTest() {
         // given
-        WeightedMultigraph<Station, Integer> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
-        Station 강남역 = new Station("강남역");
-        Station 역삼역 = new Station("역삼역");
-        Station 교대역 = new Station("교대역");
-
-        graph.setEdgeWeight(graph.addEdge(강남역, 역삼역), 3);
-        graph.setEdgeWeight(graph.addEdge(역삼역, 교대역), 7);
-        graph.setEdgeWeight(graph.addEdge(강남역, 교대역), 100);
-
         pathFinder = new PathFinder(graph);
 
         // when
-        List<Station> stations = pathFinder.getShortestPath(강남역, 교대역);
+        GraphPath graphPath = pathFinder.getShortestPath(강남역, 교대역);
 
         // then
-        assertThat(stations).isEqualTo(Arrays.asList(강남역, 역삼역, 교대역));
+        assertThat(graphPath.getVertexList()).isEqualTo(Arrays.asList(강남역, 역삼역, 교대역));
+        assertThat(graphPath.getWeight()).isEqualTo(10);
     }
 
     @DisplayName("출발역과 도착역이 같은 경우")
     @Test
     void getShortestPathTest_WhenSourceEqualsWithTarget() {
         // given
-        WeightedMultigraph<Station, Integer> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
-        Station 강남역 = new Station("강남역");
-        Station 역삼역 = new Station("역삼역");
-        Station 교대역 = new Station("교대역");
-
-        graph.setEdgeWeight(graph.addEdge(강남역, 역삼역), 3);
-        graph.setEdgeWeight(graph.addEdge(역삼역, 교대역), 7);
-        graph.setEdgeWeight(graph.addEdge(강남역, 교대역), 100);
-
         pathFinder = new PathFinder(graph);
 
         // when
@@ -65,16 +73,6 @@ public class PathFinderTest {
     @Test
     void getShortestPathTest_SourceAndTargetNotConnected() {
         // given
-        WeightedMultigraph<Station, Integer> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
-        Station 강남역 = new Station("강남역");
-        Station 역삼역 = new Station("역삼역");
-        Station 교대역 = new Station("교대역");
-        Station 사당역 = new Station("사당역");
-
-        graph.setEdgeWeight(graph.addEdge(강남역, 역삼역), 3);
-        graph.setEdgeWeight(graph.addEdge(역삼역, 교대역), 7);
-        graph.setEdgeWeight(graph.addEdge(강남역, 교대역), 100);
-
         pathFinder = new PathFinder(graph);
 
         // when
@@ -88,21 +86,12 @@ public class PathFinderTest {
     @Test
     void getShortestPathTest_WhenStationNotExists() {
         // given
-        WeightedMultigraph<Station, Integer> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
-        Station 강남역 = new Station("강남역");
-        Station 역삼역 = new Station("역삼역");
-        Station 교대역 = new Station("교대역");
-
-        graph.setEdgeWeight(graph.addEdge(강남역, 역삼역), 3);
-        graph.setEdgeWeight(graph.addEdge(역삼역, 교대역), 7);
-        graph.setEdgeWeight(graph.addEdge(강남역, 교대역), 100);
-
         pathFinder = new PathFinder(graph);
 
         // when
         // then
         assertThatThrownBy( () -> {
             pathFinder.getShortestPath(강남역, new Station("남부터미널역"));
-        }).isInstanceOf(StationsNotConnectedException.class);
+        }).isInstanceOf(StationNotExistsException.class);
     }
 }
