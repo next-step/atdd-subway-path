c31c8b3dc79128ec914e393a5d6f77708cf10fc0
perelandrax <luvyum4e@gmail.com>
2021-03-19 02:06:11 +0900

    feat: PathService, PathFinder 기능 구현

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,20 +1,51 @@
 package nextstep.subway.path.application;
 
+import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.application.StationService;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.dto.StationResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class PathService {
 
-    public PathService() {
+    private LineService lineService;
+    private StationService stationService;
 
+    public PathService(LineService lineService, StationService stationService) {
+        this.lineService = lineService;
+        this.stationService = stationService;
     }
 
     public PathResponse findShortestPath(Long sourceId, Long targetId) {
-        return new PathResponse(Collections.emptyList(), 0);
+        Station source = stationService.findById(sourceId);
+        Station target = stationService.findById(targetId);
+
+        List<Section> sections = lineService.findLines().stream()
+                .map(it -> it.getSections())
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        PathFinder pathFinder = PathFinder.create(sections);
+        pathFinder.calculateShortestPath(source, target);
+
+        return createPathResponse(pathFinder.getShortestPath(), pathFinder.getShortestPathDistance());
+    }
+
+    private PathResponse createPathResponse(List<Station> stations, int distance) {
+        List<StationResponse> stationReponses = stations.stream()
+                .map(it -> StationResponse.of(it))
+                .collect(Collectors.toList());
+
+        return new PathResponse(stationReponses, distance);
     }
 }
---
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -0,0 +1,56 @@
+package nextstep.subway.path.domain;
+
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.station.domain.Station;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PathFinder {
+
+    private final WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+    private final DijkstraShortestPath path = new DijkstraShortestPath(graph);
+    private GraphPath graphPath;
+
+    public static final PathFinder create(List<Section> sections) {
+        return new PathFinder(getStations(sections), sections);
+    }
+
+    private static List<Station> getStations(List<Section> sections) {
+        return sections.stream()
+                .map(it -> it.getStations())
+                .flatMap(Collection::stream)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private PathFinder(List<Station> stations, List<Section> sections) {
+        stations.forEach(this::addVertex);
+        sections.forEach(this::setEdgeWeight);
+    }
+
+    private void addVertex(Station station) {
+        graph.addVertex(station);
+    }
+
+    private void setEdgeWeight(Section section) {
+        graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
+    }
+
+    public void calculateShortestPath(Station source, Station target) {
+        graphPath = path.getPath(source, target);
+    }
+
+    public List<Station> getShortestPath() {
+        return graphPath.getVertexList();
+    }
+
+    public int getShortestPathDistance() {
+        return (int) graphPath.getWeight();
+    }
+}
