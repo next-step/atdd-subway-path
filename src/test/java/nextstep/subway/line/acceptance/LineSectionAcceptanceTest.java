2d7aaeeb4df560d6961fb86fecb939891b01eef2
Dong Hwan <bkuekany@gmail.com>
2021-03-19 01:12:18 +0900

    test : [2단계] 인수테스트 작성

--- src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
@@ -106,6 +106,28 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
         지하철_노선에_지하철역_등록_실패됨(response);
     }
 
+
+    @DisplayName("변경된 요구사항 : 지하철 노선에 등록된 지하철을 제외한다.")
+    @Test
+    void removeLineSection_NewVersion(){
+        //Given
+        지하철_노선에_지하철역_등록_요청(신분당선, 강남역, 양재역, 6);
+        지하철_노선에_지하철역_등록_요청(신분당선, 양재역, 정자역, 3);
+
+        //When
+        ExtractableResponse<Response> removeResponse = 지하철_노선에_지하철역_제외_요청(신분당선, 양재역);
+
+        //Then
+        지하철_노선에_지하철역_제외됨(removeResponse);
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        지하철_노선에_지하철역_순서_정렬됨(response, Arrays.asList(강남역, 정자역));
+    }
+
+    @DisplayName("등록되지 않는 노선을 제거한다.")
+    @Test
+    void removeLineSectionNotExistStation(){
+    }
+
     @DisplayName("지하철 노선에 등록된 지하철역을 제외한다.")
     @Test
     void removeLineSection() {
