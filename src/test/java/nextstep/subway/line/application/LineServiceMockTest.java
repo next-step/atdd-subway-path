174efcc9e9aafc777306687841361c2ebefc1ca5
bbbnam <bbbnam@naver.com>
2021-03-19 01:01:36 +0900

    test : lineService Mock 방식 테스트 작성

--- src/test/java/nextstep/subway/line/application/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/line/application/LineServiceMockTest.java
@@ -1,8 +1,9 @@
 package nextstep.subway.line.application;
 
+import com.google.common.collect.Lists;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
-import nextstep.subway.line.dto.SectionRequest;
+import nextstep.subway.line.dto.*;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 
@@ -14,6 +15,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,15 +41,104 @@ public class LineServiceMockTest {
     void setUp() {
         lineService = new LineService(lineRepository, stationService);
 
-        강남역 = new Station("강남역");
-        ReflectionTestUtils.setField(강남역, "id", 1L);
-        역삼역 = new Station("역삼역");
-        ReflectionTestUtils.setField(역삼역, "id", 2L);
-        삼성역 = new Station("삼성역");
-        ReflectionTestUtils.setField(삼성역, "id", 3L);
+        강남역 = initStation(강남역,"강남역", 1L);
+        역삼역 = initStation(역삼역,"역삼역", 2L);
+        삼성역 = initStation(삼성역,"삼성역", 3L);
+
         이호선 = new Line("2호선", "green", 강남역, 역삼역, 10);
         ReflectionTestUtils.setField(이호선, "id", 1L);
     }
+
+    @Test
+    void saveLine() {
+        // given
+        when(stationService.findStationById(강남역.getId())).thenReturn(강남역);
+        when(stationService.findStationById(역삼역.getId())).thenReturn(역삼역);
+        when(lineRepository.save(any())).thenReturn(이호선);
+
+        // when
+        LineResponse 저장된_이호선 = lineService.saveLine(
+                new LineRequest(이호선.getName(), 이호선.getColor(), 강남역.getId(), 역삼역.getId(), 10)
+        );
+
+        // then
+        assertThat(저장된_이호선).isNotNull();
+        assertThat(저장된_이호선.getName()).isEqualTo(이호선.getName());
+    }
+
+    @Test
+    void findLines() {
+        // given
+        when(lineRepository.findAll()).thenReturn(Lists.newArrayList(new Line()));
+
+        // when
+        List<Line> lines = lineService.findLines();
+
+        // then
+        assertThat(lines).hasSize(1);
+    }
+
+    @Test
+    void findLineResponses() {
+        // given
+        when(lineRepository.findAll()).thenReturn(Lists.newArrayList(new Line()));
+
+        // when
+        List<LineResponse> responses = lineService.findLineResponses();
+
+        // then
+        assertThat(responses).hasSize(1);
+    }
+
+    @Test
+    void findLineById() {
+        // given
+        when(lineRepository.findById(any())).thenReturn(Optional.ofNullable(이호선));
+
+        // when
+        Line 조회된_이호선 = lineService.findLineById(이호선.getId());
+
+        // then
+        assertThat(조회된_이호선).isNotNull();
+        assertThat(조회된_이호선.getName()).isEqualTo("2호선");
+    }
+
+    @Test
+    void findLineResponseById() {
+        // given
+        when(lineRepository.findById(any())).thenReturn(Optional.ofNullable(이호선));
+
+        // when
+        LineResponse 이호선_리스폰스 = lineService.findLineResponseById(이호선.getId());
+
+        // then
+        assertThat(이호선_리스폰스).isNotNull();
+        assertThat(이호선_리스폰스.getName()).isEqualTo("2호선");
+    }
+
+    @Test
+    void updateLine() {
+        // given
+        when(lineRepository.findById(any())).thenReturn(Optional.ofNullable(이호선));
+
+        // when
+        lineService.updateLine(이호선.getId(),  new LineRequest("3호선", "주황색", 강남역.getId(), 역삼역.getId(), 10));
+
+        // then
+        assertThat(이호선.getName()).isEqualTo("3호선");
+        assertThat(이호선.getColor()).isEqualTo("주황색");
+    }
+
+    /*@Test
+    void deleteLineById() {
+        // given ?
+
+        // when
+        lineService.deleteLineById(이호선.getId());
+
+        // then
+        assertThat(이호선).isNull();
+    }*/
 
     @Test
     void addSection() {
@@ -65,5 +157,40 @@ public class LineServiceMockTest {
         Line 구간생성된_이호선 = lineService.findLineById(이호선.getId());
         assertThat(구간생성된_이호선).isNotNull();
         assertThat(구간생성된_이호선.getSections().getSections()).hasSize(2);
+    }
+
+    @Test
+    void removeSection() {
+        // given
+        이호선.addSection(역삼역, 삼성역, 20);
+        when(lineRepository.findById(any())).thenReturn(Optional.of(이호선));
+        when(stationService.findStationById(any())).thenReturn(강남역);
+
+        // when
+        lineService.removeSection(이호선.getId(), 강남역.getId());
+
+        // then
+        assertThat(이호선.getSections().getStations().contains(강남역)).isFalse();
+    }
+
+    @Test
+    void findShortestPath() {
+        // given
+        when(lineRepository.findAll()).thenReturn(Arrays.asList(이호선));
+        when(stationService.findStationById(강남역.getId())).thenReturn(강남역);
+        when(stationService.findStationById(역삼역.getId())).thenReturn(역삼역);
+
+        // when
+        PathResponse shortestPath = lineService.findShortestPath(new PathRequest(강남역.getId(), 역삼역.getId()));
+
+        // then
+        assertThat(shortestPath).isNotNull();
+        assertThat(shortestPath.getDistance()).isEqualTo(10);
+    }
+
+    private Station initStation(Station station, String stationName, Long id) {
+        station = new Station(stationName);
+        ReflectionTestUtils.setField(station, "id", id);
+        return station;
     }
 }
