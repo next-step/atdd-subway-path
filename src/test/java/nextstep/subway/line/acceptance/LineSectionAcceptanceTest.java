22bee3c73338ab92456024944cbd965dba8f736b
Dong Hwan <bkuekany@gmail.com>
2021-03-18 23:12:36 +0900

    docs : 불필요 주석 제거 및 시각화된 @DisplayName 내용 주석으로 변경

--- src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
@@ -22,17 +22,6 @@ import static nextstep.subway.line.acceptance.LineSteps.*;
 import static nextstep.subway.station.StationSteps.지하철역_등록되어_있음;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-/**
- * 구간 추가 기능 변경 인수테스트
- * 1. 역 사이에 새로운 역을 등록할 수 있다.음
- *  - 기존 역 사이의 길이보다 크거나 같으면 등록 할 수 없음
- *  - 상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음
- *  - 상행역과 하행역 둘 중 하나도 포함되어 있지 않으면 추가할 수 없음
- * 2. 새로운 역을 상행 종점으로 등록 할 수 있다.
- * 3. 새로운 역을 하행 종점으로 등록 할 수 있다.
- */
-
 @DisplayName("지하철 노선에 역 등록 관련 기능")
 public class LineSectionAcceptanceTest extends AcceptanceTest {
     private LineResponse 신분당선;
