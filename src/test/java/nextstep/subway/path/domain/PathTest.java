0a02500bdb10093ee2df85fb7ce0f5e419e7f79e
임한결 <dlagksruf19@naver.com>
2021-03-18 11:34:57 +0900

    test : PathTest (지하철 최단 경로 조회 기능 테스트)

---
+++ src/test/java/nextstep/subway/path/domain/PathTest.java
@@ -0,0 +1,117 @@
+package nextstep.subway.path.domain;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@DisplayName("경로 Domain 단위 테스트")
+class PathTest {
+
+    private Station savedStationGangNam;
+    private Station savedStationYangJae;
+    private Station savedStationGyoDae;
+    private Station savedStationNambuTerminal;
+
+    private Line line2;
+    private Line line3;
+    private Line lineNewBunDang;
+
+    private Path path;
+
+    private List<Line> allLines;
+    private List<Section> allSections;
+
+    @BeforeEach
+    void setUp() {
+        savedStationGangNam = new Station(1L, "강남역");
+        savedStationYangJae = new Station(2L, "양재역");
+        savedStationGyoDae = new Station(3L, "교대역");
+        savedStationNambuTerminal = new Station(4L, "남부터미널역");
+
+        line2 = new Line(1L, "2호선", "bg-green-600");
+        line2.addSection(savedStationGyoDae, savedStationGangNam, 10);
+
+        line3 = new Line(2L, "3호선", "bg-orange-600");
+        line3.addSection(savedStationGyoDae, savedStationNambuTerminal, 5);
+        line3.addSection(savedStationNambuTerminal, savedStationYangJae, 3);
+
+        lineNewBunDang = new Line(3L, "신분당선", "bg-red-600");
+        lineNewBunDang.addSection(savedStationGangNam, savedStationYangJae, 10);
+
+        allLines = new ArrayList<>();
+        allLines.add(line2);
+        allLines.add(line3);
+        allLines.add(lineNewBunDang);
+
+        allSections = allLines.stream()
+                .map(Line::getSections)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        path = new Path(allSections);
+    }
+
+    @Test
+    @DisplayName("지하철 최단 경로 조회")
+    void findShortestPathStation() {
+        // given
+        long source = savedStationGangNam.getId();
+        long target = savedStationNambuTerminal.getId();
+
+        // when
+        List<Station> shortestPath = path.findShortestPath(source, target);
+
+        // then
+        assertThat(shortestPath).hasSize(3);
+        assertThat(path.getDistance()).isEqualTo(15);
+    }
+
+    @Test
+    @DisplayName("출발역과 도착역이 같은 경우 예외 발생")
+    void notEqualsSourceAndTarget() {
+        // given
+        long source = savedStationGangNam.getId();
+        long target = savedStationGangNam.getId();
+
+        // when & then
+        assertThatExceptionOfType(SameStationPathSearchException.class)
+                .isThrownBy(() -> path.findShortestPath(source, target));
+    }
+
+    @Test
+    @DisplayName("출발역과 도착역이 연결되어 있지 않은 경우 예외 발생")
+    void notConnectedSourceAndTarget() {
+        // given
+        Station savedStationMyeongDong = new Station(3L, "명동역");
+
+        long source = savedStationGangNam.getId();
+        long target = savedStationMyeongDong.getId();
+
+        // when & then
+        assertThatExceptionOfType(NotConenctedPathException.class)
+                .isThrownBy(() -> path.findShortestPath(source, target));
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 출발역, 도착역을 조회할 경우 예외 발생")
+    void findNotExistSourceAndTarget() {
+        // given
+        long source = 100L;
+        long target = 101L;
+
+        // when & then
+        assertThatExceptionOfType(StationNonExistException.class)
+                .isThrownBy(() -> path.findShortestPath(source, target));
+    }
+}
