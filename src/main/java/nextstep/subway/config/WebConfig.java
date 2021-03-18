9aafb23a4ff7257ea47bb0bec94b3c8c322f1c53
boorownie <brown@woowahan.com>
2021-03-18 15:58:11 +0900

    feat(cors): add cors setting

---
+++ src/main/java/nextstep/subway/config/WebConfig.java
@@ -0,0 +1,13 @@
+package nextstep.subway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+    }
+}
