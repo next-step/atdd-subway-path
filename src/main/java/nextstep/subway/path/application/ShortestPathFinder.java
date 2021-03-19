67184a08deec3addda1b51dded9d308d152ae209
Jeongsu Yang <mycat83@gmail.com>
2021-03-19 22:14:32 +0900

    fix: ShortestPathFinder 수정
    - 멤버변수 접근제어자 변경
    - 메소드 리팩토링
    - WeightedMultigraph 제네릭 변경

--- src/main/java/nextstep/subway/path/application/ShortestPathFinder.java
+++ src/main/java/nextstep/subway/path/application/ShortestPathFinder.java
@@ -1,7 +1,6 @@
 package nextstep.subway.path.application;
 
 import nextstep.subway.line.dto.LineSectionResponse;
-import nextstep.subway.line.dto.SectionResponse;
 import nextstep.subway.path.exception.SeperatedStationsException;
 import nextstep.subway.station.dto.StationResponse;
 import org.jgrapht.GraphPath;
@@ -15,50 +14,52 @@ import java.util.stream.Collectors;
 
 public class ShortestPathFinder {
 
-    WeightedMultigraph<String, DefaultWeightedEdge> graph;
-    List<StationResponse> stationResponses;
+    private final WeightedMultigraph<Long, DefaultWeightedEdge> graph;
+    private final List<StationResponse> stationResponses;
+    private final List<LineSectionResponse> lineSectionResponses;
 
     public ShortestPathFinder(List<StationResponse> stationResponses, List<LineSectionResponse> lineSectionResponses) {
-        graph = new WeightedMultigraph(DefaultWeightedEdge.class);
         this.stationResponses = stationResponses;
-        setStations(stationResponses);
-        setSections(lineSectionResponses);
-    }
+        this.lineSectionResponses = lineSectionResponses;
 
-    public void setStations(List<StationResponse> stationResponses) {
-        for (StationResponse stationResponse : stationResponses) {
-            graph.addVertex(String.valueOf(stationResponse.getId()));
-        }
-    }
-
-    public void setSections(List<LineSectionResponse> lineSectionResponses) {
-        for (LineSectionResponse lineSectionResponse : lineSectionResponses) {
-            List<SectionResponse> sectionResponses = lineSectionResponse.getSections();
-            for (SectionResponse sectionResponse : sectionResponses) {
-                graph.setEdgeWeight(
-                        graph.addEdge(String.valueOf(sectionResponse.getUpStationId()), String.valueOf(sectionResponse.getDownStationId())),
-                        sectionResponse.getDistance()
-                );
-            }
-        }
+        graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        setStations();
+        setSections();
     }
 
     public List<StationResponse> getShortestPath(Long sourceStationId, Long targetStationId) {
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
-        GraphPath path = dijkstraShortestPath.getPath(String.valueOf(sourceStationId), String.valueOf(targetStationId));
+        GraphPath path = dijkstraShortestPath.getPath(sourceStationId, targetStationId);
 
         if (path == null) {
             throw new SeperatedStationsException();
         }
 
-        List<String> shortestPath = path.getVertexList();
+        List<Long> shortestPath = path.getVertexList();
         return shortestPath.stream()
-                .map(id -> stationResponses.stream().filter(it -> it.getId() == Long.parseLong(id)).findFirst().orElseThrow(RuntimeException::new))
+                .map(id -> stationResponses.stream().filter(it -> it.getId().equals(id)).findFirst().orElseThrow(RuntimeException::new))
                 .collect(Collectors.toList());
     }
 
     public int getShortestDistance(Long sourceStationId, Long targetStationId) {
-        List<GraphPath> paths = new KShortestPaths(graph, 100).getPaths(String.valueOf(sourceStationId), String.valueOf(targetStationId));
+        List<GraphPath> paths = new KShortestPaths(graph, 100).getPaths(sourceStationId, targetStationId);
         return (int) paths.get(0).getWeight();
+    }
+
+    private void setStations() {
+        stationResponses.forEach(it -> graph.addVertex(it.getId()));
+    }
+
+    private void setSections() {
+        lineSectionResponses.forEach(this::setSection);
+    }
+
+    private void setSection(LineSectionResponse lineSectionResponse) {
+        lineSectionResponse.getSections()
+                .forEach(it -> graph.setEdgeWeight(
+                        graph.addEdge(it.getUpStationId(), it.getDownStationId()),
+                        it.getDistance()
+                        )
+                );
     }
 }
