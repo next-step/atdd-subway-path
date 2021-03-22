02bccc59959866234f2056861bb07c5f7aa5218b
BlueCitron <sds901234@naver.com>
2021-03-23 00:08:43 +0900

    feat: LineTest / getStations 테스트 추가

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -1,15 +1,52 @@
 package nextstep.subway.line.domain;
 
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
+
+    Line 신분당선;
+    Station 강남역;
+    Station 양재역;
+    Station 판교역;
+    Section 강남_양재;
+    Section 양재_판교;
+
+    @BeforeEach
+    public void setUp() {
+        신분당선 = new Line("신분당선", "red");
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        판교역 = new Station("판교역");
+        강남_양재 = new Section(신분당선, 강남역, 양재역, 10);
+        양재_판교 = new Section(신분당선, 양재역, 판교역, 10);
+        ReflectionTestUtils.setField(신분당선, "sections", Arrays.asList(강남_양재, 양재_판교));
+        ReflectionTestUtils.setField(강남역, "id", 1L);
+        ReflectionTestUtils.setField(양재역, "id", 2L);
+        ReflectionTestUtils.setField(판교역, "id", 3L);
+    }
+
     @Test
     void getStations() {
+        // when
+        List<Station> stations = 신분당선.getStations();
+
+        // then
+        지하철_노선에_지하철역_순서_정렬됨(stations, Arrays.asList(강남역, 양재역, 판교역));
     }
 
     @Test
     void addSection() {
+
     }
 
     @DisplayName("목록 중간에 추가할 경우 에러 발생")
@@ -30,4 +67,17 @@ public class LineTest {
     @Test
     void removeSectionNotEndOfList() {
     }
+
+    public static void 지하철_노선에_지하철역_순서_정렬됨(List<Station> stations, List<Station> expectedStations) {
+        List<Long> stationIds = stations.stream()
+                .map(Station::getId)
+                .collect(Collectors.toList());
+
+        List<Long> expectedStationIds = expectedStations.stream()
+                .map(Station::getId)
+                .collect(Collectors.toList());
+
+        assertThat(stationIds).containsExactlyElementsOf(expectedStationIds);
+    }
+
 }
