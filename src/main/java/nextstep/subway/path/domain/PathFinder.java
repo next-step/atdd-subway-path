29ef316f9f4fb52fa67ed2e22d22b2dd3bc5b2f1
Dong Hwan <bkuekany@gmail.com>
2021-03-23 01:51:56 +0900

    chore : 패키지 경로 이동

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -1,21 +1,13 @@
 package nextstep.subway.path.domain;
 
-import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Lines;
 import nextstep.subway.path.domain.exception.CannotFindPathException;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.Stations;
 
-import java.util.List;
-
 public class PathFinder {
 
-    private Lines lines;
     private PathStrategy pathStrategy;
-
-    public PathFinder(List<Line> lines) {
-        this.lines = new Lines(lines);
-    }
 
     public PathFinder(PathStrategy pathStrategy) {
         this.pathStrategy = pathStrategy;
