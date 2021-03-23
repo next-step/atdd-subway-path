ac6ee202034decc37901151618d9a6440412c8bc
Dong Hwan <bkuekany@gmail.com>
2021-03-24 00:20:54 +0900

    refactor : 전략패턴으로 사용되는 경로조회 클래스 bean 등록

--- src/main/java/nextstep/subway/SubwayApplication.java
+++ src/main/java/nextstep/subway/SubwayApplication.java
@@ -1,7 +1,10 @@
 package nextstep.subway;
 
+import nextstep.subway.path.domain.DijkstraStrategy;
+import nextstep.subway.path.domain.PathStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -14,4 +17,8 @@ public class SubwayApplication {
         SpringApplication.run(SubwayApplication.class, args);
     }
 
+    @Bean
+    public PathStrategy pathStrategy(){
+        return new DijkstraStrategy();
+    }
 }
