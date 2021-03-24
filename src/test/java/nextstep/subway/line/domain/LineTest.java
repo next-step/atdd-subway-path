857658d4a5fd0dbf2741685489939f210a5987c9
BlueCitron <sds901234@naver.com>
2021-03-24 09:31:46 +0900

    feat: LineTest > addSection 테스트 추가

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -1,6 +1,7 @@
 package nextstep.subway.line.domain;
 
 import nextstep.subway.station.domain.Station;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,9 +28,6 @@ public class LineTest {
         강남역 = new Station("강남역");
         양재역 = new Station("양재역");
         판교역 = new Station("판교역");
-        강남_양재 = new Section(신분당선, 강남역, 양재역, 10);
-        양재_판교 = new Section(신분당선, 양재역, 판교역, 10);
-        ReflectionTestUtils.setField(신분당선, "sections", Arrays.asList(강남_양재, 양재_판교));
         ReflectionTestUtils.setField(강남역, "id", 1L);
         ReflectionTestUtils.setField(양재역, "id", 2L);
         ReflectionTestUtils.setField(판교역, "id", 3L);
@@ -37,6 +35,11 @@ public class LineTest {
 
     @Test
     void getStations() {
+        // given
+        강남_양재 = new Section(신분당선, 강남역, 양재역, 10);
+        양재_판교 = new Section(신분당선, 양재역, 판교역, 10);
+        ReflectionTestUtils.setField(신분당선, "sections", Arrays.asList(강남_양재, 양재_판교));
+
         // when
         List<Station> stations = 신분당선.getStations();
 
@@ -46,7 +49,12 @@ public class LineTest {
 
     @Test
     void addSection() {
+        // when
+        신분당선.addSection(강남역, 양재역, 100);
 
+        // then
+        Assertions.assertThat(신분당선.getSections().size()).isEqualTo(1);
+        지하철_노선에_지하철역_순서_정렬됨(신분당선.getStations(), Arrays.asList(강남역, 양재역));
     }
 
     @DisplayName("목록 중간에 추가할 경우 에러 발생")
