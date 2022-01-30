203b8f776f7855ca68666209e28f13aa8c4d28e4
Hero <icraft2170@gmail.com>
2022-01-31 01:40:06 +0900

    LineServiceTest 추가

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,17 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -23,11 +28,22 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Line _2호선 = new Line("2호선","green");
+        Station 합정역 = stationRepository.save(new Station("합정역"));
+        Station 신도림역 = stationRepository.save(new Station("신도림역"));
+        Station 홍대입구역 = stationRepository.save(new Station("홍대입구역"));
+        _2호선.getSections().add(new Section(_2호선, 합정역, 신도림역, 10));
+        lineRepository.save(_2호선);
+
+        SectionRequest sectionRequest = new SectionRequest(신도림역.getId(), 홍대입구역.getId(), 10);
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(_2호선.getId(), sectionRequest);
 
         // then
         // line.getSections 메서드를 통해 검증
+        List<Station> stations = _2호선.getStation();
+        assertThat(stations).containsExactly(합정역, 신도림역, 홍대입구역);
     }
 }
