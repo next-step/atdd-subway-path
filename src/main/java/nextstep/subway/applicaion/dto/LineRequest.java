fe92793b3849a3c3c1382b848bd8f5d46f70b828
apdkeb <apdkeb@naver.com>
2022-01-30 17:32:01 +0900

    test : LineService 단위테스트 시작
    2022. 01. 30. 일요일
    - 노선 저장과 구간 추가하기 테스트 생성
    
    Resolves : loop-study
    See also : kang-hyungu

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -26,4 +26,24 @@ public class LineRequest {
     public int getDistance() {
         return distance;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setUpStationId(Long upStationId) {
+        this.upStationId = upStationId;
+    }
+
+    public void setDownStationId(Long downStationId) {
+        this.downStationId = downStationId;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
 }
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,18 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.applicaion.dto.LineRequest;
+import nextstep.subway.applicaion.dto.LineResponse;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest
 @Transactional
@@ -15,19 +21,47 @@ public class LineServiceTest {
     private StationRepository stationRepository;
     @Autowired
     private LineRepository lineRepository;
-
     @Autowired
     private LineService lineService;
 
+    @DisplayName("노선을 저장하다.")
+    @Test
+    void saveLine() {
+        // given
+        LineRequest request = new LineRequest();
+        request.setName("2호선");
+        request.setColor("bg-green-600");
+
+        // when
+        LineResponse lineResponse = lineService.saveLine(request);
+
+        // then
+        assertAll(
+                () -> assertThat(lineResponse.getName()).isEqualTo(request.getName()),
+                () -> assertThat(lineResponse.getColor()).isEqualTo(request.getColor())
+        );
+    }
+
+    @DisplayName("구간을 추가하다.")
     @Test
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Line line = lineRepository.save(new Line("2호선", "bg-red-600"));
+        Station upStation = stationRepository.save(new Station("강남역"));
+        Station downStation = stationRepository.save(new Station("역삼역"));
+
+        SectionRequest request = new SectionRequest();
+        request.setUpStationId(upStation.getId());
+        request.setDownStationId(downStation.getId());
+        request.setDistance(5);
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(line.getId(), request);
 
         // then
         // line.getSections 메서드를 통해 검증
+        assertThat(line.getSections()).hasSize(1);
     }
 }
