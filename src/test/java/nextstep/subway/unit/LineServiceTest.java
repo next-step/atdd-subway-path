ca50dc042e31a13c9fa0875082443dcdfcaf242d
apdkeb <apdkeb@naver.com>
2022-01-31 15:53:11 +0900

    test : LineService 단위 테스트 추가
    2022. 01. 31. 월요일
    - LineService 테스트 추가
    
    Resolves : loop-study
    See also : kang-hyungu

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -11,12 +11,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest
 @Transactional
 public class LineServiceTest {
+    private static final int DEFAULT_DISTANCE = 5;
+    private static final String DEFAULT_LINE_COLOR = "bg-green-600";
+    private static final String FIRST_LINE_NAME = "1호선";
+    private static final String SECOND_LINE_NAME = "2호선";
+
     @Autowired
     private StationRepository stationRepository;
     @Autowired
@@ -28,9 +35,7 @@ public class LineServiceTest {
     @Test
     void saveLine() {
         // given
-        LineRequest request = new LineRequest();
-        request.setName("2호선");
-        request.setColor("bg-green-600");
+        LineRequest request = createLineRequest(FIRST_LINE_NAME);
 
         // when
         LineResponse lineResponse = lineService.saveLine(request);
@@ -42,6 +47,68 @@ public class LineServiceTest {
         );
     }
 
+    @DisplayName("모든 노선을 조회한다")
+    @Test
+    void showLines() {
+        // given
+        lineService.saveLine(createLineRequest(FIRST_LINE_NAME));
+        lineService.saveLine(createLineRequest(SECOND_LINE_NAME));
+
+        // when
+        List<LineResponse> lines = lineService.showLines();
+
+        // then
+        assertThat(lines).hasSize(2);
+    }
+
+    @DisplayName("단일 노선을 조회한다")
+    @Test
+    void findById() {
+        // given
+        LineResponse lineResponse = lineService.saveLine(createLineRequest(FIRST_LINE_NAME));
+
+        // when
+        LineResponse findLine = lineService.findById(lineResponse.getId());
+
+        // then
+        assertAll(
+                () -> assertThat(findLine.getId()).isEqualTo(lineResponse.getId()),
+                () -> assertThat(findLine.getName()).isEqualTo(lineResponse.getName()),
+                () -> assertThat(findLine.getColor()).isEqualTo(lineResponse.getColor())
+        );
+    }
+
+    @DisplayName("노선 정보를 수정한다")
+    @Test
+    void updateLine() {
+        // given
+        String updateName = "분당선";
+
+        LineResponse lineResponse = lineService.saveLine(createLineRequest(FIRST_LINE_NAME));
+        LineRequest updateLineRequest = createLineRequest(updateName);
+
+        // when
+        lineService.updateLine(lineResponse.getId(), updateLineRequest);
+        LineResponse updateLineResponse = lineService.findById(lineResponse.getId());
+
+        // then
+        assertThat(updateLineResponse.getName()).isEqualTo(updateName);
+    }
+
+    @DisplayName("선택한 노선을 삭제한다")
+    @Test
+    void deleteLine() {
+        // given
+        LineResponse lineResponse = lineService.saveLine(createLineRequest(FIRST_LINE_NAME));
+
+        // when
+        lineService.deleteLine(lineResponse.getId());
+        List<LineResponse> lineResponses = lineService.showLines();
+
+        // then
+        assertThat(lineResponses).hasSize(0);
+    }
+
     @DisplayName("구간을 추가하다.")
     @Test
     void addSection() {
@@ -51,10 +118,7 @@ public class LineServiceTest {
         Station upStation = stationRepository.save(new Station("강남역"));
         Station downStation = stationRepository.save(new Station("역삼역"));
 
-        SectionRequest request = new SectionRequest();
-        request.setUpStationId(upStation.getId());
-        request.setDownStationId(downStation.getId());
-        request.setDistance(5);
+        SectionRequest request = createSectionRequest(upStation, downStation);
 
         // when
         // lineService.addSection 호출
@@ -63,5 +127,28 @@ public class LineServiceTest {
         // then
         // line.getSections 메서드를 통해 검증
         assertThat(line.getSections()).hasSize(1);
+    }
+
+    private LineRequest createLineRequest(String name) {
+        return createLineRequest(name, DEFAULT_LINE_COLOR);
+    }
+
+    private LineRequest createLineRequest(String name, String color) {
+        LineRequest request = new LineRequest();
+        request.setName(name);
+        request.setColor(color);
+        return request;
+    }
+
+    private SectionRequest createSectionRequest(Station upStation, Station downStation) {
+        return createSectionRequest(upStation, downStation, DEFAULT_DISTANCE);
+    }
+
+    private SectionRequest createSectionRequest(Station upStation, Station downStation, int distance) {
+        SectionRequest request = new SectionRequest();
+        request.setUpStationId(upStation.getId());
+        request.setDownStationId(downStation.getId());
+        request.setDistance(distance);
+        return request;
     }
 }
