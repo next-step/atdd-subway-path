287b0eed8fdee833a6589ec76be2e503a0f700bd
soo <yunsoo0510@daum.net>
2022-01-31 22:16:25 +0900

    [Test] 새로운 역을 상행 종점으로 등록할 경우: 인수 테스트 작성

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -53,6 +53,23 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
+     * When 지하철 노선에 새로운 역을 상행 종점으로 등록 요청 하면
+     * Then 노선에 새로운 구간이 추가된다
+     */
+    @DisplayName("지하철 노선에 구간을 등록(상행 종점)")
+    @Test
+    void addLineSection2() {
+        // when
+        Long 신논현역 = 지하철역_생성_요청("신논현역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(신논현역, 강남역));
+
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(신논현역, 강남역, 양재역);
+    }
+
+    /**
      * Given 지하철 노선에 새로운 구간 추가를 요청 하고
      * When 지하철 노선의 마지막 구간 제거를 요청 하면
      * Then 노선에 구간이 제거된다
