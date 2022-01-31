d81528cd46261a926e207935f1d9999db7899acf
BAEKJungHo <designjava@naver.com>
2022-01-31 22:08:36 +0900

    [feature] 노선에 속해있는 역 목록 조회 - 실패하는 단위 테스트 작성

--- src/test/java/nextstep/subway/unit/domain/LineTest.java
+++ src/test/java/nextstep/subway/unit/domain/LineTest.java
@@ -1,5 +1,7 @@
 package nextstep.subway.unit.domain;
 
+import nextstep.subway.applicaion.dto.LineResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Sections;
@@ -7,6 +9,8 @@ import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,6 +26,7 @@ class LineTest {
 
     @BeforeEach
     void setFixtures() {
+        // given
         line = new Line("2호선", "bg-red-600");
         서초역 = new Station("서초역");
         교대역 = new Station("교대역");
@@ -68,6 +73,16 @@ class LineTest {
     @DisplayName("노선에 속해있는 역 목록 조회")
     @Test
     void getStations() {
+        // given
+        line.addSection(new Section(line, 역삼역, 삼성역, 5));
+        line.addSection(new Section(line, 서초역, 교대역, 5));
+        line.addSection(new Section(line, 교대역, 강남역, 3));
+
+        // when
+        LineResponse lineResponse = LineResponse.of(line);
+        List<StationResponse> stations = lineResponse.getStations();
+        StationResponse lastStation = stations.get(stations.size() - 1);
+        assertThat(lastStation.getName()).isEqualTo(삼성역);
     }
 
     @DisplayName("구간이 목록에서 마지막 역 삭제")
