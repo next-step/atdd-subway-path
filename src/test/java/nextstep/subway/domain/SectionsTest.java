c70dfda22898030a4144bbae45fc9a2d4e43230d
soo <yunsoo0510@daum.net>
2022-02-01 01:25:15 +0900

    [Test] 상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음: 단위 테스트 작성

--- src/test/java/nextstep/subway/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/domain/SectionsTest.java
@@ -3,12 +3,6 @@ package nextstep.subway.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.test.context.event.annotation.BeforeTestMethod;
-
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -25,13 +19,13 @@ class SectionsTest {
         강남역 = new Station("강남역");
         정자역 = new Station("정자역");
         distance = 10;
+        신분당선.addSection(강남역, 정자역, distance);
     }
 
     @DisplayName("새로운 역을 상행 종점으로 등록할 경우")
     @Test
     void addSection() {
         // given
-        신분당선.addSection(강남역, 정자역, distance);
         Station 신논현역 = new Station("신논현역");
 
         // when
@@ -45,7 +39,6 @@ class SectionsTest {
     @Test
     void addSection2() {
         // given
-        신분당선.addSection(강남역, 정자역, distance);
         Station 중간역 = new Station("중간역");
 
         // when
@@ -59,13 +52,20 @@ class SectionsTest {
     @Test
     void addSectionFail() {
         // given
-        신분당선.addSection(강남역, 정자역, distance);
         Station 중간역 = new Station("중간역");
 
         // when & then
         assertThatThrownBy(() -> 신분당선.addSection(강남역, 중간역, distance))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> 신분당선.addSection(중간역, 정자역, distance))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("역 사이에 새로운 역을 등록할 경우 실패 - 상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음")
+    @Test
+    void addSectionFail2() {
+        // when & then
+        assertThatThrownBy(() -> 신분당선.addSection(강남역, 정자역, 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
