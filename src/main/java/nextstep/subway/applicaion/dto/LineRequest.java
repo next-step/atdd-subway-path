ce48dd96886408a94a2d567cb2802a715e2c6389
00hongjun <00hongjun@gmail.com>
2022-02-01 18:06:15 +0900

    <test> LineService의 테스트 코드 추가

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -8,6 +8,19 @@ public class LineRequest {
     private Long downStationId;
     private int distance;
 
+    public static LineRequest of(String name, String color,
+                                 long upStationId, long downStationId,
+                                 int distance) {
+        LineRequest request = new LineRequest();
+        request.name = name;
+        request.color = color;
+        request.upStationId = upStationId;
+        request.downStationId = downStationId;
+        request.distance = distance;
+
+        return request;
+    }
+
     public String getName() {
         return name;
     }
--- src/main/java/nextstep/subway/applicaion/dto/UpdateLineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/UpdateLineRequest.java
@@ -5,6 +5,14 @@ public class UpdateLineRequest {
     private String color;
     private String name;
 
+    public static UpdateLineRequest of(String color, String name) {
+        UpdateLineRequest request = new UpdateLineRequest();
+        request.color = color;
+        request.name = name;
+
+        return request;
+    }
+
     public String getColor() {
         return color;
     }
