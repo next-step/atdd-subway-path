ee800ad582b42ce1b473db61c08db6ab7393a0c5
BAEKJungHo <designjava@naver.com>
2022-02-01 17:44:00 +0900

    [feature] 서비스 테스트 완료

--- src/test/java/nextstep/subway/unit/service/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/service/LineServiceMockTest.java
@@ -1,28 +1,66 @@
 package nextstep.subway.unit.service;
 
-import nextstep.subway.applicaion.StationService;
-import nextstep.subway.domain.LineRepository;
+import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class LineServiceMockTest {
+class LineServiceMockTest {
+
     @Mock
     private LineRepository lineRepository;
     @Mock
-    private StationService stationService;
+    private StationRepository stationRepository;
+    private LineService lineService;
+    private Station 교대역;
+    private Station 역삼역;
+    private Line 이호선;
+    private SectionRequest sectionRequest = new SectionRequest();
+
+    @BeforeEach
+    void setFixtures() {
+        // given
+        lineService = new LineService(lineRepository, stationRepository);
+
+        교대역 = new Station("교대역");
+        역삼역 = new Station("역삼역");
+        ReflectionTestUtils.setField(교대역, "id", 1L);
+        ReflectionTestUtils.setField(역삼역, "id", 2L);
+
+        이호선 = new Line("2호선", "bg-red-600");
+        ReflectionTestUtils.setField(이호선, "id", 1L);
+
+        ReflectionTestUtils.setField(sectionRequest, "downStationId", 역삼역.getId());
+        ReflectionTestUtils.setField(sectionRequest, "upStationId", 교대역.getId());
+        ReflectionTestUtils.setField(sectionRequest, "distance", 5);
+    }
 
     @Test
     void addSection() {
         // given
-        // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        when(stationRepository.findById(1L)).thenReturn(Optional.of(교대역));
+        when(stationRepository.findById(2L)).thenReturn(Optional.of(역삼역));
+        when(lineRepository.findById(1L)).thenReturn(Optional.of(이호선));
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(sectionRequest, 이호선.getId());
 
         // then
         // line.findLineById 메서드를 통해 검증
+        Line line = lineRepository.findById(이호선.getId()).get();
+        Sections sections = line.getSections();
+        assertThat(sections.getSections()).hasSize(1);
+        assertThat(sections.getDownStationEndPoint()).isEqualTo(역삼역);
     }
 }
--- src/test/java/nextstep/subway/unit/service/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/service/LineServiceTest.java
@@ -1,18 +1,23 @@
 package nextstep.subway.unit.service;
 
 import nextstep.subway.applicaion.LineService;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
-public class LineServiceTest {
+class LineServiceTest {
+
     @Autowired
     private StationRepository stationRepository;
+
     @Autowired
     private LineRepository lineRepository;
 
@@ -22,12 +27,22 @@ public class LineServiceTest {
     @Test
     void addSection() {
         // given
-        // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 교대역 = stationRepository.save(new Station("교대역"));
+        Station 역삼역 = stationRepository.save(new Station("역삼역"));
+
+        SectionRequest sectionRequest = new SectionRequest();
+        ReflectionTestUtils.setField(sectionRequest, "downStationId", 역삼역.getId());
+        ReflectionTestUtils.setField(sectionRequest, "upStationId", 교대역.getId());
+        ReflectionTestUtils.setField(sectionRequest, "distance", 5);
+
+        Line 이호선 = lineRepository.save(new Line("2호선", "bg-red-600"));
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(sectionRequest, 이호선.getId());
 
         // then
-        // line.getSections 메서드를 통해 검증
+        Sections sections = 이호선.getSections();
+        Station station = sections.getDownStationEndPoint();
+        assertThat(station).isEqualTo(역삼역);
     }
 }
