d9de03eb5eea3b2ee88b770f0e583d0f193e735e
YisooKim <gashe7@gmail.com>
2022-02-01 19:10:06 +0900

    Test:Add service test

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,16 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -19,15 +23,41 @@ public class LineServiceTest {
     @Autowired
     private LineService lineService;
 
+    private Station 사당역;
+    private Station 방배역;
+    private Line 이호선;
+    private Section 사당_방배_구간;
+
+
+    @BeforeEach
+    void setUp() {
+        사당역 = new Station("사당");
+        방배역 = new Station("방배");
+        이호선 = new Line("2호선", "green");
+        사당_방배_구간 = new Section(사당역, 방배역, 5);
+        이호선.addSection(사당_방배_구간);
+
+        stationRepository.save(사당역);
+        stationRepository.save(방배역);
+        lineRepository.save(이호선);
+    }
+
+    @DisplayName("요청한 구간이 마지막 구간과 연결되면 구간 등록 성공")
     @Test
     void addSection() {
         // given
-        // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 서초역 = new Station("서초");
+        stationRepository.save(서초역);
+
+        Long lineId = 이호선.getId();
+        SectionRequest sectionRequest = new SectionRequest(방배역.getId(), 서초역.getId(), 3);
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(lineId, sectionRequest);
 
         // then
-        // line.getSections 메서드를 통해 검증
+        Line line = lineRepository.findById(lineId).get();
+        assertThat(line.getStations()).contains(서초역);
     }
+
 }
