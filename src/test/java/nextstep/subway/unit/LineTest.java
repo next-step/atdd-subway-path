9476fcfe34c5ecb9a1e90163bbb443cd08f419a5
김우재 <kwj1270@naver.com>
2022-02-01 16:05:28 +0900

    test: LineTest, 구간 목록 마지막에 새로운 구간을 추가할 경우 테스트 추가

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,12 +1,39 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LineTest {
+
+    private Station upStation;
+    private Station downStation;
+    private Line line;
+
+    @BeforeEach
+    void setUp() {
+        upStation = new Station("upStation");
+        downStation = new Station("downStation");
+        line = new Line("color", "name", upStation, downStation, 10);
+    }
+
     @DisplayName("구간 목록 마지막에 새로운 구간을 추가할 경우")
     @Test
     void addSection() {
+        // given
+        final Station extraStation = new Station("extraStation");
+        final Section section = new Section(line, downStation, extraStation, 1);
+
+        // when
+        line.addSection(section);
+
+        // then
+        assertThat(line.getStations()).containsExactly(upStation, downStation, extraStation);
     }
 
     @DisplayName("노선에 속해있는 역 목록 조회")
