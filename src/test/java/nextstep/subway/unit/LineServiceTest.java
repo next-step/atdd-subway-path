50db50ac05d0cb1f286d58449a584707a77d41ea
monkeyDugi <53487385+monkeyDugi@users.noreply.github.com>
2022-02-01 21:18:16 +0900

    단위 테스트 실습 (#182)
    * feat : 단위 테스트 작성
    
    * refactor : 단위 테스트 연습 및 Line 로직 리팩토링

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,16 +1,30 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.LineResponse;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.applicaion.dto.StationResponse;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
 @Transactional
-public class LineServiceTest {
+class LineServiceTest {
+
     @Autowired
     private StationRepository stationRepository;
     @Autowired
@@ -19,15 +33,34 @@ public class LineServiceTest {
     @Autowired
     private LineService lineService;
 
+    private Station 가양역;
+    private Station 증미역;
+    private Line 구호선;
+
+    @BeforeEach
+    void setUp() {
+        가양역 = new Station("가양역");
+        증미역 = new Station("증미역");
+        구호선 = new Line("9호선", "갈색");
+    }
+
     @Test
     void addSection() {
         // given
-        // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station upStation = stationRepository.save(가양역);
+        Station downStation = stationRepository.save(증미역);
+        Line line = lineRepository.save(구호선);
+
+        SectionRequest sectionRequest = new SectionRequest(upStation.getId(), downStation.getId(), 10);
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(line.getId(), sectionRequest);
 
         // then
-        // line.getSections 메서드를 통해 검증
+        assertThat(line.getSections().size()).isEqualTo(1);
+        LineResponse response = lineService.findById(line.getId());
+        List<StationResponse> stations = response.getStations();
+        assertThat(stations.get(0).getId()).isEqualTo(upStation.getId());
+        assertThat(stations.get(1).getId()).isEqualTo(downStation.getId());
     }
 }
--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,21 +1,82 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 class LineTest {
     @DisplayName("구간 목록 마지막에 새로운 구간을 추가할 경우")
     @Test
     void addSection() {
+        // given
+        Station upStation = new Station("가양역");
+        Station downStation = new Station("증미역");
+        Line line = new Line("9호선", "금색");
+        int distance = 10;
+
+        Section section = new Section(line, upStation, downStation, distance);
+
+        // when
+        line.addSection(section);
+
+        // then
+        List<Section> sections = line.getSections();
+        assertThat(sections.size()).isEqualTo(1);
+        assertThat(sections.get(0).getUpStation()).isEqualTo(upStation);
+        assertThat(sections.get(0).getDownStation()).isEqualTo(downStation);
+        assertThat(sections.get(0).getDistance()).isEqualTo(distance);
     }
 
     @DisplayName("노선에 속해있는 역 목록 조회")
     @Test
     void getStations() {
+        // given
+        Station 가양역 = new Station("가양역");
+        Station 증미역 = new Station("증미역");
+        Station 등촌역 = new Station("등촌역");
+        Line line = new Line("9호선", "금색");
+        int distance1 = 10;
+        int distance2 = 20;
+
+        line.addSection(new Section(line, 가양역, 증미역, distance1));
+        line.addSection(new Section(line, 증미역, 등촌역, distance2));
+
+        // when
+        List<Station> stations = line.getStations();
+
+        // then
+        List<Station> exceptedStations = Arrays.asList(가양역, 증미역, 등촌역);
+        assertThat(stations).isEqualTo(exceptedStations);
     }
 
     @DisplayName("구간이 목록에서 마지막 역 삭제")
     @Test
     void removeSection() {
+        // given
+        Station 가양역 = new Station("가양역");
+        Station 증미역 = new Station("증미역");
+        Station 등촌역 = new Station("등촌역");
+        Line line = new Line("9호선", "금색");
+        int distance1 = 10;
+        int distance2 = 20;
+
+        line.addSection(new Section(line, 가양역, 증미역, distance1));
+        line.addSection(new Section(line, 증미역, 등촌역, distance2));
+
+        // when
+        line.removeSection(등촌역);
+
+        // then
+        List<Section> sections = line.getSections();
+        assertThat(sections.size()).isEqualTo(1);
+        assertThat(sections.get(0).getDownStation()).isEqualTo(증미역);
     }
 }
