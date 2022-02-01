91d1b55a773a3b8e5d6326d20e9f306496c3dc25
sang5c <skah321@gmail.com>
2022-02-01 19:50:50 +0900

    test: LineService 단위 테스트 작성

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,11 +1,22 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.LineResponse;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
@@ -18,11 +29,31 @@ public class LineServiceMockTest {
     void addSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        Station 군자역 = new Station("군자역");
+        Station 아차산역 = new Station("아차산역");
+        Station 광나루역 = new Station("광나루역");
+
+        long upStationId = 2L;
+        long downStationId = 3L;
+        int distance = 10;
+        long lineId = 1L;
+        SectionRequest request = new SectionRequest(upStationId, downStationId, distance);
+
+        Line _5호선 = new Line("5호선", "보라색");
+        _5호선.getSections().add(new Section(_5호선, 군자역, 아차산역, request.getDistance()));
+
+        LineService lineService = new LineService(lineRepository, stationService);
+        given(stationService.findById(upStationId)).willReturn(아차산역);
+        given(stationService.findById(downStationId)).willReturn(광나루역);
+        given(lineRepository.findById(lineId)).willReturn(Optional.of(_5호선));
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(lineId, request);
 
         // then
         // line.findLineById 메서드를 통해 검증
+        LineResponse response = lineService.findById(lineId);
+        assertThat(response.getStations()).hasSize(3);
     }
 }
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,16 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -23,11 +27,26 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 군자역 = stationRepository.save(new Station("군자역"));
+        Station 아차산역 = stationRepository.save(new Station("아차산역"));
+        Station 광나루역 = stationRepository.save(new Station("광나루역"));
+        Line _5호선 = new Line("5호선", "보라색");
+        _5호선.getSections().add(new Section(_5호선, 군자역, 아차산역, 10));
+        lineRepository.save(_5호선);
+
+        int distance = 10;
+        SectionRequest request = new SectionRequest(아차산역.getId(), 광나루역.getId(), distance);
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(_5호선.getId(), request);
 
         // then
         // line.getSections 메서드를 통해 검증
+        List<Section> sections = _5호선.getSections();
+        Section lastSection = sections.get(sections.size() - 1);
+        assertThat(lastSection.getUpStation()).isEqualTo(아차산역);
+        assertThat(lastSection.getDownStation()).isEqualTo(광나루역);
+        assertThat(lastSection.getDistance()).isEqualTo(distance);
     }
 }
