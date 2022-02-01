3a1858e69121b793b8114e9f27348004114ba9ff
monkeyDugi <lbd4946@gmail.com>
2022-02-01 21:45:29 +0900

    test : 인수 조건 작성

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -1,17 +1,17 @@
 package nextstep.subway.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static nextstep.subway.acceptance.LineSteps.*;
+import static nextstep.subway.acceptance.LineSteps.getLineId;
+import static nextstep.subway.acceptance.LineSteps.지하철_노선_목록_조회_요청;
+import static nextstep.subway.acceptance.LineSteps.지하철_노선_삭제_요청;
+import static nextstep.subway.acceptance.LineSteps.지하철_노선_생성_요청;
+import static nextstep.subway.acceptance.LineSteps.지하철_노선_수정_요청;
+import static nextstep.subway.acceptance.LineSteps.지하철_노선_조회_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관리 기능")
@@ -31,10 +31,12 @@ class LineAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 지하철 노선 생성을 요청 하고
-     * Given 새로운 지하철 노선 생성을 요청 하고
+     * Given 지하철 노선 생성을 요청 하고,
+     * Given 역 사이에 구간 추가 요청을 하고,
+     * Given 새로운 지하철 노선 생성을 요청 하고,
+     * Given 역 사이에 구간 추가 요청을 하고,
      * When 지하철 노선 목록 조회를 요청 하면
-     * Then 두 노선이 포함된 지하철 노선 목록을 응답받는다
+     * Then 두 노선이 포함된 지하철 노선 목록을 응답하고, 상행 종점역 부터 하행 종점역 순으로 역 정보를 응답한다.
      */
     @DisplayName("지하철 노선 목록 조회")
     @Test
@@ -52,9 +54,10 @@ class LineAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 지하철 노선 생성을 요청 하고
+     * Given 지하철 노선 생성을 요청 하고,
+     * Given 역 사이에 구간 추가 요청을 하고,
      * When 생성한 지하철 노선 조회를 요청 하면
-     * Then 생성한 지하철 노선을 응답받는다
+     * Then 생성한 지하철 노선의 역을 상행 종점역 부터 하행 종점역 순으로 역 정보를 응답한다.
      */
     @DisplayName("지하철 노선 조회")
     @Test
--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -36,12 +36,12 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * When 지하철 노선에 새로운 구간 추가를 요청 하면
-     * Then 노선에 새로운 구간이 추가된다
+     * When 지하철 노선의 하행 종점역에 새로운 구간 추가를 요청하면,
+     * Then 노선에 새로운 구간이 추가된다.
      */
-    @DisplayName("지하철 노선에 구간을 등록")
+    @DisplayName("지하철 노선의 하행 종점역에 구간을 추가")
     @Test
-    void addLineSection() {
+    void addLineDownEndStationSection() {
         // when
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
         지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
@@ -50,6 +50,33 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역, 정자역);
+    }
+
+    /**
+     * When 역 사이에 새로운 구간 추가를 요청하면,
+     * Then 사이에 새로운 구간이 추가된다.
+     */
+    @DisplayName("지하철역 사이에 새로운 구간 추가")
+    @Test
+    void addLineBetweenSection() {
+
+    }
+
+    /**
+     * When 기존 상행 종점역에 새로운 구간 추가를 요청하면,
+     * Then 상행 종점역에 새로운 구간이 추가된다.
+     */
+    @DisplayName("지하철 노선의 상행 종점역으로 새로운 구간 추가")
+    @Test
+    void addLineUpEndStationSection() {
+        // when
+//        Long 신논현역 = 지하철역_생성_요청("신논현").jsonPath().getLong("id");
+//        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(신논현역, 강남역));
+//
+//        // then
+//        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+//        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+//        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(신논현역, 강남역, 양재역);
     }
 
     /**
@@ -78,17 +105,17 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         lineCreateParams = new HashMap<>();
         lineCreateParams.put("name", "신분당선");
         lineCreateParams.put("color", "bg-red-600");
-        lineCreateParams.put("upStationId", upStationId + "");
-        lineCreateParams.put("downStationId", downStationId + "");
-        lineCreateParams.put("distance", 10 + "");
+        lineCreateParams.put("upStationId", String.valueOf(upStationId));
+        lineCreateParams.put("downStationId", String.valueOf(downStationId));
+        lineCreateParams.put("distance", String.valueOf(10));
         return lineCreateParams;
     }
 
     private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId) {
         Map<String, String> params = new HashMap<>();
-        params.put("upStationId", upStationId + "");
-        params.put("downStationId", downStationId + "");
-        params.put("distance", 6 + "");
+        params.put("upStationId", String.valueOf(upStationId));
+        params.put("downStationId", String.valueOf(downStationId));
+        params.put("distance", String.valueOf(6));
         return params;
     }
 }
