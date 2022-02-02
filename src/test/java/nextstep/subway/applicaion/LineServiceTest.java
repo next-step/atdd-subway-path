41e128f70ad7dd318e03102cbda39951f9f39e39
sanhee <68936@naver.com>
2022-02-02 16:48:03 +0900

    test: 모든 노선을 출력한다.

--- src/test/java/nextstep/subway/applicaion/LineServiceTest.java
+++ src/test/java/nextstep/subway/applicaion/LineServiceTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -28,7 +30,10 @@ class LineServiceTest {
     @BeforeEach
     void init() {
         StationService stationService = new StationService(stationRepository);
-        lineService = new LineService(lineRepository, stationService);
+        lineService = new LineService(
+                lineRepository,
+                stationService
+        );
     }
 
     @DisplayName("노선을 생성한다.")
@@ -42,7 +47,13 @@ class LineServiceTest {
         String 파란색 = "파란색";
 
         // when
-        LineResponse 저장된_노선 = 노선_생성_요청(동암역_ID, 부평역_ID, 일호선, 파란색, 10);
+        LineResponse 저장된_노선 = 노선_생성_요청(
+                동암역_ID,
+                부평역_ID,
+                일호선,
+                파란색,
+                10
+        );
 
         // then
         assertAll(
@@ -51,16 +62,67 @@ class LineServiceTest {
         );
     }
 
+    @Test
+    @DisplayName("모든 노선을 출력한다.")
+    void showLines() {
+        // given
+        Long 동암역_ID = 역_생성_요청("동암역");
+        Long 부평역_ID = 역_생성_요청("부평역");
+
+        String 일호선 = "1호선";
+        String 파란색 = "파란색";
+
+        노선_생성_요청(
+                동암역_ID,
+                부평역_ID,
+                일호선,
+                파란색,
+                10
+        );
+
+        Long 주안역_ID = 역_생성_요청("주안역");
+        Long 백운역_ID = 역_생성_요청("백운역");
+
+        String 이호선 = "2호선";
+        String 노란색 = "노란색";
+
+        노선_생성_요청(
+                주안역_ID,
+                백운역_ID,
+                이호선,
+                노란색,
+                10
+        );
+
+        List<LineResponse> 출력된_노선목록 = lineService.showLines();
+
+        assertAll(
+                () -> assertThat(출력된_노선목록.size()).isEqualTo(2),
+                () -> assertThat(출력된_노선목록.get(0)
+                                         .getName()).isEqualTo(일호선),
+                () -> assertThat(출력된_노선목록.get(1)
+                                         .getName()).isEqualTo(이호선)
+        );
+
+    }
+
     private LineResponse 노선_생성_요청(Long 상행역_ID, Long 하행역_ID, String 노선_이름, String 노선_색상, int 노선_거리) {
-        LineRequest 노선_생성_정보 = new LineRequest(노선_이름, 노선_색상, 상행역_ID, 하행역_ID, 노선_거리);
+        LineRequest 노선_생성_정보 = new LineRequest(
+                노선_이름,
+                노선_색상,
+                상행역_ID,
+                하행역_ID,
+                노선_거리
+        );
 
         LineResponse 저장된_노선 = lineService.saveLine(노선_생성_정보);
         return 저장된_노선;
     }
 
     private Long 역_생성_요청(String 동암역) {
-        return stationRepository.save(new Station(동암역))
-                                .getId();
+        return stationRepository
+                .save(new Station(동암역))
+                .getId();
     }
 
 }
