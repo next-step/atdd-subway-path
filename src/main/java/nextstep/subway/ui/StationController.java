ade9625e7a93916f8d7111e00ffc2164b922c802
00hongjun <00hongjun@gmail.com>
2022-02-02 20:49:04 +0900

    <refactor> end point 정리

--- src/main/java/nextstep/subway/ui/StationController.java
+++ src/main/java/nextstep/subway/ui/StationController.java
@@ -12,34 +12,34 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/stations")
 public class StationController {
 
     private final StationCommandService stationCommandService;
     private final StationQueryService stationQueryService;
 
-    public StationController(StationCommandService stationCommandService,
-                             StationQueryService stationQueryService) {
+    public StationController(StationCommandService stationCommandService, StationQueryService stationQueryService) {
         this.stationCommandService = stationCommandService;
         this.stationQueryService = stationQueryService;
     }
 
-    @PostMapping("/stations")
+    @PostMapping
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
         StationResponse station = stationCommandService.saveStation(stationRequest);
         return ResponseEntity.created(URI.create("/stations/" + station.getId())).body(station);
     }
 
-    @GetMapping(value = "/stations", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StationResponse>> showStations() {
         return ResponseEntity.ok().body(stationQueryService.findAllStations());
     }
 
-    @GetMapping(value = "/stations/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<StationResponse> showStation(@PathVariable Long id) {
         return ResponseEntity.ok().body(stationQueryService.showStationById(id));
     }
 
-    @DeleteMapping("/stations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteStation(@PathVariable Long id) {
         stationCommandService.deleteStationById(id);
         return ResponseEntity.noContent().build();
