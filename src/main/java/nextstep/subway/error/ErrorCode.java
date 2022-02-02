69b3ee4137910c13097ca98650c387ff1a60d29f
BAEKJungHo <designjava@naver.com>
2022-02-02 17:37:50 +0900

    [feedback] 미사용 errorCode 제거

--- src/main/java/nextstep/subway/error/ErrorCode.java
+++ src/main/java/nextstep/subway/error/ErrorCode.java
@@ -4,8 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
 
-    NOT_FOUND(HttpStatus.NOT_FOUND, "-1001", "Resource Not Found"),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "-10002", "Bad Request"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "-10003", "Internal Server Error"),
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "-10004", "Validation Error"),
     NOT_FOUND_LINE_ERROR(HttpStatus.BAD_REQUEST, "-10005", "노선을 찾을 수 없습니다."),
@@ -13,8 +11,6 @@ public enum ErrorCode {
     NOT_FOUND_STATION_ERROR(HttpStatus.BAD_REQUEST, "-10007", "지하철 역을 찾을 수 없습니다."),
     NOT_FOUND_SECTION_ERROR(HttpStatus.BAD_REQUEST, "-10008", "구간을 찾을 수 없습니다."),
     FIRST_SECTION_CREATE_ERROR(HttpStatus.BAD_REQUEST, "-10009", "노선 생성시 등록된 구간이 존재하여, 구간 등록이 불가능합니다."),
-    UP_STATION_MUST_SAME_LAST_REGISTERED_DOWN_STATION_ERROR(HttpStatus.BAD_REQUEST, "-10010", "새로운 구간의 상행역은 현재 등록되어있는 하행 종점역이어야 합니다."),
-    DOWN_STATION_MUST_NOT_CURRENTLY_REGISTERED_STATION_ERROR(HttpStatus.BAD_REQUEST, "-10011", "새로운 구간의 하행역은 현재 등록되어있는 역일 수 없습니다."),
     SECTION_MINIMUM_SIZE_ERROR(HttpStatus.BAD_REQUEST, "-10012", "구간은 2개이상 등록되어있는 경우 삭제 가능합니다."),
     ONLY_THE_LAST_SECTION_CAN_BE_DELETE_ERROR(HttpStatus.BAD_REQUEST, "-10013", "마지막 구간만 삭제 가능합니다."),
     INVALID_SECTION_DISTANCE_ERROR(HttpStatus.BAD_REQUEST, "-10014", "역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없습니다."),
