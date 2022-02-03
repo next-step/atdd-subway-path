9c5b57297e1c45dd925c3ab71f6e33293603452d
mintheon <mintheon@mintheon.com>
2022-02-03 12:54:12 +0900

    test: LineServiceMockTest 테스트 작성

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,11 +1,23 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.LineResponse;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
@@ -14,15 +26,43 @@ public class LineServiceMockTest {
     @Mock
     private StationService stationService;
 
+    final Line 이호선 = new Line("2호선", "green");
+    final Station 강남역 = new Station("강남역");
+    final Station 역삼역 = new Station("역삼역");
+
+    final int 거리 = 100;
+
+    @BeforeEach
+    public void setUp() {
+        ReflectionTestUtils.setField(이호선, "id", 1L);
+        ReflectionTestUtils.setField(강남역, "id", 1L);
+        ReflectionTestUtils.setField(역삼역, "id", 2L);
+    }
+
     @Test
     void addSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        when(lineRepository.findById(1L)).thenReturn(Optional.of(이호선));
+
+        when(stationService.findById(1L)).thenReturn(강남역);
+        when(stationService.findById(2L)).thenReturn(역삼역);
 
         // when
         // lineService.addSection 호출
+        LineService lineService = new LineService(lineRepository, stationService);
+
+        SectionRequest sectionRequest = new SectionRequest();
+        ReflectionTestUtils.setField(sectionRequest, "upStationId", 강남역.getId());
+        ReflectionTestUtils.setField(sectionRequest, "downStationId", 역삼역.getId());
+        ReflectionTestUtils.setField(sectionRequest, "distance", 거리);
+
+        lineService.addSection(1L, sectionRequest);
 
         // then
         // line.findLineById 메서드를 통해 검증
+        LineResponse lineResponse = lineService.findById(이호선.getId());
+
+        assertThat(lineResponse.getStations().size()).isEqualTo(2);
     }
 }
