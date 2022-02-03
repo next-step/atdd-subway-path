3ce0a1762d87ba7c5ba36c66949998400f8f9580
soo <yunsoo0510@daum.net>
2022-02-03 23:28:08 +0900

    [Test] 하행 종점역 제거: 인수테스트 작성

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -145,6 +145,25 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
+     * Given 지하철 노선에 새로운 구간 추가를 요청 하고
+     * When 지하철 노선의 마지막 구간 제거(하행 종점역)를 요청 하면
+     * Then 노선에 구간이 제거된다
+     */
+    @DisplayName("지하철 노선에 구간을 제거(하행 종점역)")
+    @Test
+    void removeLineSection2() {
+        // given
+        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역, 6));
+
+        // when
+        지하철_노선에_지하철_구간_제거_요청(신분당선, 정자역);
+
+        // then
+        지하철_노선에_구간_제거됨(신분당선, 강남역, 양재역);
+    }
+
+    /**
      * Given 지하철 노선에 구간이 1개일 때
      * When 지하철 노선의 구간 제거를 요청 하면
      * Then 노선에 구간 제거 실패한다.
