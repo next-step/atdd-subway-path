7cb173b13e5bdfdf9be5f4141164345c69395a61
mintheon <mintheon@mintheon.com>
2022-02-03 14:21:42 +0900

    refactor: 도메인 내부로직 변경으로 인한 서비스 테스트 코드 수정

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -26,43 +26,47 @@ public class LineServiceMockTest {
     @Mock
     private StationService stationService;
 
-    final Line 이호선 = new Line("2호선", "green");
-    final Station 강남역 = new Station("강남역");
-    final Station 역삼역 = new Station("역삼역");
+    final Station 강남역 = Station.of("강남역");
+    final Station 역삼역 = Station.of("역삼역");
+    final Station 합정역 = Station.of("합정역");
 
-    final int 거리 = 100;
+    final Line 이호선 = Line.of("2호선", "green", 강남역, 역삼역, 100);
 
     @BeforeEach
     public void setUp() {
-        ReflectionTestUtils.setField(이호선, "id", 1L);
         ReflectionTestUtils.setField(강남역, "id", 1L);
         ReflectionTestUtils.setField(역삼역, "id", 2L);
+        ReflectionTestUtils.setField(합정역, "id", 3L);
+
+        ReflectionTestUtils.setField(이호선, "id", 1L);
     }
 
     @Test
     void addSection() {
+        final int 거리 = 50;
+
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
         when(lineRepository.findById(1L)).thenReturn(Optional.of(이호선));
 
-        when(stationService.findById(1L)).thenReturn(강남역);
         when(stationService.findById(2L)).thenReturn(역삼역);
+        when(stationService.findById(3L)).thenReturn(합정역);
 
         // when
         // lineService.addSection 호출
         LineService lineService = new LineService(lineRepository, stationService);
 
         SectionRequest sectionRequest = new SectionRequest();
-        ReflectionTestUtils.setField(sectionRequest, "upStationId", 강남역.getId());
-        ReflectionTestUtils.setField(sectionRequest, "downStationId", 역삼역.getId());
+        ReflectionTestUtils.setField(sectionRequest, "upStationId", 역삼역.getId());
+        ReflectionTestUtils.setField(sectionRequest, "downStationId", 합정역.getId());
         ReflectionTestUtils.setField(sectionRequest, "distance", 거리);
 
-        lineService.addSection(1L, sectionRequest);
+        lineService.addSection(이호선.getId(), sectionRequest);
 
         // then
         // line.findLineById 메서드를 통해 검증
         LineResponse lineResponse = lineService.findById(이호선.getId());
 
-        assertThat(lineResponse.getStations().size()).isEqualTo(2);
+        assertThat(lineResponse.getStations().size()).isEqualTo(3);
     }
 }
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -29,16 +29,16 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
-        final int 거리 = 10;
 
-        final Station 강남역 = stationRepository.save(new Station("강남역"));
-        final Station 역삼역 = stationRepository.save(new Station("역삼역"));
-        final Line 이호선 = lineRepository.save(new Line("이호선", "green"));
+        final Station 강남역 = stationRepository.save(Station.of("강남역"));
+        final Station 역삼역 = stationRepository.save(Station.of("역삼역"));
+        final Station 합정역 = stationRepository.save(Station.of("합정역"));
+        final Line 이호선 = lineRepository.save(Line.of("이호선", "green", 강남역, 역삼역, 100));
 
         SectionRequest sectionRequest = new SectionRequest();
-        ReflectionTestUtils.setField(sectionRequest, "upStationId", 강남역.getId());
-        ReflectionTestUtils.setField(sectionRequest, "downStationId", 역삼역.getId());
-        ReflectionTestUtils.setField(sectionRequest, "distance", 거리);
+        ReflectionTestUtils.setField(sectionRequest, "upStationId", 역삼역.getId());
+        ReflectionTestUtils.setField(sectionRequest, "downStationId", 합정역.getId());
+        ReflectionTestUtils.setField(sectionRequest, "distance", 10);
 
         // when
         // lineService.addSection 호출
@@ -46,6 +46,6 @@ public class LineServiceTest {
 
         // then
         // line.getSections 메서드를 통해 검증
-        assertThat(이호선.getSections().size()).isEqualTo(1);
+        assertThat(이호선.getSections().getStations().size()).isEqualTo(3);
     }
 }
