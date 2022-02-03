9a9cbf278cd47286f6267a9fadb5ad8abbfa223c
skid901 <tlrudejr1993@gmail.com>
2022-02-04 01:28:00 +0900

    refactor: implements additional requirements of the add section method in the sections

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -4,6 +4,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import java.util.*;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -19,7 +20,6 @@ public class Sections {
     Sections(List<Section> sections) {
         this.sections = sections;
     }
-
 
     private Map<Station, Station> getStationMap() {
         return sections.stream()
@@ -44,11 +44,56 @@ public class Sections {
                 .findAny().orElseThrow(IllegalStateException::new);
     }
 
+    private Optional<Section> findSectionByStation(Station station, Function<Section, Station> sectionToStationMapper) {
+        return sections.stream().filter(it -> sectionToStationMapper.apply(it).equals(station)).findAny();
+    }
+
+    private Optional<Section> findSectionByUpStation(Station upStation) {
+        return findSectionByStation(upStation, Section::getUpStation);
+    }
+
+    private Optional<Section> findSectionByDownStation(Station downStation) {
+        return findSectionByStation(downStation, Section::getDownStation);
+    }
+
+    private void replaceSection(Section replacedSection, Station newSectionUpStation, Station newSectionDownStation, int remainedDistance) {
+        if (remainedDistance <= 0) {
+            throw new IllegalArgumentException("기존 구간 거리 이상 구간은 사이에 추가할 수 없습니다.");
+        }
+        sections.remove(replacedSection);
+        sections.add(new Section(replacedSection.getLine(), newSectionUpStation, newSectionDownStation, remainedDistance));
+    }
+
     public List<Section> get() {
         return sections;
     }
 
     public void add(Section section) {
+        Station addedUpStation = section.getUpStation();
+        Station addedDownStation = section.getDownStation();
+
+        if (sections.isEmpty() || getStartingStation().equals(addedDownStation) ^ getEndingStation().equals(addedUpStation)) {
+            sections.add(section);
+            return;
+        }
+
+        Optional<Section> upStationSection = findSectionByUpStation(addedUpStation);
+        Optional<Section> downStationSection = findSectionByDownStation(addedDownStation);
+
+        if (upStationSection.isEmpty() && downStationSection.isEmpty()) {
+            throw new IllegalArgumentException("기존 구간에 속하지 않는 역 만으로 구간의 상행, 하행 역을 지정할 수 없습니다.");
+        }
+        if (upStationSection.isPresent() && downStationSection.isPresent()) {
+            throw new IllegalArgumentException("기존 구간에 속하는 역 만으로 구간의 상행, 하행 역을 지정할 수 없습니다.");
+        }
+
+        upStationSection.ifPresent(it -> replaceSection(
+                it, section.getDownStation(), it.getDownStation(), it.getDistance() - section.getDistance()
+        ));
+        downStationSection.ifPresent(it -> replaceSection(
+                it, it.getUpStation(), section.getUpStation(), it.getDistance() - section.getDistance()
+        ));
+
         sections.add(section);
     }
 
