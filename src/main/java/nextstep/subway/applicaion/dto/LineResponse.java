5fae2687af62cb08a99b8739901b7ed4d12d3321
minjoon_lee <minjoon1995@naver.com>
2022-02-04 11:26:01 +0900

    refactoring : line response에서 stationresponse convert method 추가

--- src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -1,7 +1,6 @@
 package nextstep.subway.applicaion.dto;
 
 import nextstep.subway.domain.Line;
-import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
 
 import java.time.LocalDateTime;
@@ -9,31 +8,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LineResponse {
-    private final Long id;
-    private final String name;
-    private final String color;
-    private final List<StationResponse> stations;
-    private final LocalDateTime createdDate;
-    private final LocalDateTime modifiedDate;
-    private final List<SectionResponse> sections;
+    private Long id;
+    private String name;
+    private String color;
+    private List<StationResponse> stations;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
 
     public LineResponse(Line line) {
         this.id = line.getId();
         this.name = line.getName();
         this.color = line.getColor();
         this.stations = convertStationResponse(line.getStations());
-        this.sections = convertSectionResponse(line.getSections());
         this.createdDate = line.getCreatedDate();
         this.modifiedDate = line.getModifiedDate();
     }
-
-    private List<SectionResponse> convertSectionResponse(List<Section> sections) {
-        List<SectionResponse> sectionResponses = sections.stream()
-                .map(section -> new SectionResponse(section))
-                .collect(Collectors.toList());
-        return sectionResponses;
-    }
-
     public Long getId() {
         return id;
     }
@@ -52,10 +41,6 @@ public class LineResponse {
 
     private List<StationResponse> convertStationResponse(List<Station> stations) {
         return stations.stream().map(station -> new StationResponse(station)).collect(Collectors.toList());
-    }
-
-    public List<SectionResponse> getSections() {
-        return sections;
     }
 
     public LocalDateTime getCreatedDate() {
