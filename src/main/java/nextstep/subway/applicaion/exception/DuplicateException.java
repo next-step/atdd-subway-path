48ff957255cbfa2fd93a000a7a3d74a2849090f5
mintheon <mintheon@mintheon.com>
2022-02-05 02:06:14 +0900

    refactor: 범용적으로 사용 가능하도록 예외 메시지 수정

--- src/main/java/nextstep/subway/applicaion/exception/DuplicateException.java
+++ src/main/java/nextstep/subway/applicaion/exception/DuplicateException.java
@@ -1,7 +1,7 @@
 package nextstep.subway.applicaion.exception;
 
 public class DuplicateException extends RuntimeException {
-    final static String message = "중복된 이름으로 생성할 수 없습니다. - [%s]";
+    final static String message = "중복된 정보로 생성할 수 없습니다. - [%s]";
 
     public DuplicateException(String name) {
         super(String.format(message, name));
--- src/main/java/nextstep/subway/applicaion/exception/NotExistLineException.java
+++ src/main/java/nextstep/subway/applicaion/exception/NotExistLineException.java
@@ -1,13 +1,9 @@
 package nextstep.subway.applicaion.exception;
 
-public class NotExistException extends RuntimeException {
-    final static String message = "해당 정보는 등록되지 않았습니다. - [%s]";
+public class NotExistLineException extends RuntimeException {
+    final static String message = "해당 노선은 등록되지 않았습니다. - [%d]";
 
-    public NotExistException(Long id) {
-        super(String.format(message, id.toString()));
-    }
-
-    public NotExistException(String name) {
-        super(String.format(message, name));
+    public NotExistLineException(Long id) {
+        super(String.format(message, id));
     }
 }
