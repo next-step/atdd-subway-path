918f04344a4f99f871c0241965bcc3d66fa853ff
YisooKim <gashe7@gmail.com>
2022-02-04 20:53:30 +0900

    Feat:Specify error response

--- src/main/java/nextstep/subway/exception/ControllerExceptionHandler.java
+++ src/main/java/nextstep/subway/exception/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
-package nextstep.subway.ui;
+package nextstep.subway.exception;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,7 +14,7 @@ public class ControllerExceptionHandler {
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Void> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
