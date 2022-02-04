a5568c1f23f049cf61adbced7b52227023ace4e2
abcdsds <choiyurim2@naver.com>
2022-02-04 17:26:51 +0900

    refactor: fetchType lazy로 수정

--- src/main/java/nextstep/subway/domain/Section.java
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -11,15 +11,15 @@ public class Section {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "line_id")
     private Line line;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "up_station_id")
     private Station upStation;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "down_station_id")
     private Station downStation;
 
