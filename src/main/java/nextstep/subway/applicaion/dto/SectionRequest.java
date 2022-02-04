794523ef6b86d601c40ebb6012c8c87910089083
minjoon_lee <minjoon1995@naver.com>
2022-02-04 13:16:02 +0900

    feature : test 코드를 위한 생성자 추가

--- src/main/java/nextstep/subway/applicaion/dto/SectionRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/SectionRequest.java
@@ -23,4 +23,16 @@ public class SectionRequest {
     public int getDistance() {
         return distance;
     }
+
+    public SectionRequest(Long upStationId, Long downStationId, int distance) {
+        this.downStationId = downStationId;
+        this.upStationId = upStationId;
+        this.distance = distance;
+    }
+
+    public SectionRequest(Section section) {
+        this.downStationId = section.getDownStation().getId();
+        this.upStationId = section.getUpStation().getId();
+        this.distance = section.getDistance();
+    }
 }
--- src/main/java/nextstep/subway/applicaion/dto/StationRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationRequest.java
@@ -3,9 +3,6 @@ package nextstep.subway.applicaion.dto;
 public class StationRequest {
     private String name;
 
-    protected StationRequest() {
-    }
-
     public StationRequest(String name) {
         this.name = name;
     }
