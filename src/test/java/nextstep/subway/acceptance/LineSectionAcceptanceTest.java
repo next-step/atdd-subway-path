def4b4e1066a02bce89b75c2a32e615f48604bbe
kimuno89 <kimunoya@gmail.com>
2022-02-05 01:58:28 +0900

    test: 노선에 등록되어있지 않은 역을 제거하는 경우의 인수테스트 작성

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -12,7 +12,6 @@ import java.util.Map;
 
 import static nextstep.subway.acceptance.LineSteps.*;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 구간 관리 기능")
 class LineSectionAcceptanceTest extends AcceptanceTest {
@@ -136,7 +135,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
      * When 중간역을 제거하면
      * Then 구간이 삭제되고 구간이 재배치 된다.
      */
-    @DisplayName("중간역이 제거될 경우 재배치")
+    @DisplayName("중간역이 제거될 경우 구간을 재배치 한다")
     @Test
     void rearrangeSection() {
         // given
@@ -153,6 +152,28 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         인수테스트_요청_응답을_확인한다(response, HttpStatus.OK);
         지하철_노선에_지하철역이_포함됐는지_확인한다(response, 강남역, 양재역, 판교역);
     }
+
+    /**
+     * Given 지하철역과 노선 생성을 요청 하고
+     * Given 구간 추가를 요청하고
+     * When 구간에 포함되지 않은 역을 삭제하면
+     * Then 에러가 발생한다.
+     */
+    @DisplayName("구간에 포함되지 않은 역을 삭제하면 에러가 발생한다")
+    @Test
+    void notValidStationSectionDeleteRequest() {
+        // given
+        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+        Long 판교역 = 지하철역_생성_요청("판교역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역, 7));
+
+        // when
+        ExtractableResponse<Response> response =  지하철_노선에_지하철_구간_제거_요청(신분당선, 판교역);
+
+        // then
+        인수테스트_요청_응답을_확인한다(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 
     private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
         Map<String, String> lineCreateParams;
