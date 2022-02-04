d772a2e57b197a267037484e3f8f70f920faf523
minjoon_lee <minjoon1995@naver.com>
2022-02-04 13:16:14 +0900

    feature : stub test 코드 생성

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -5,13 +5,12 @@ import nextstep.subway.applicaion.StationService;
 import nextstep.subway.applicaion.dto.LineRequest;
 import nextstep.subway.applicaion.dto.SectionRequest;
 import nextstep.subway.applicaion.dto.StationRequest;
-import nextstep.subway.domain.Line;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.SectionRepository;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.domain.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,25 +26,29 @@ public class LineServiceTest {
     private StationService stationService;
     @Autowired
     private LineService lineService;
-    @Autowired
-    private SectionRepository sectionRepository;
 
     @Test
     void addSection() {
         // given
         지하철_역_저장();
-        LineService lineService = new LineService(lineRepository, stationService, sectionRepository);
+
+        LineService lineService = new LineService(lineRepository, stationService);
         lineService.saveLine(new LineRequest("신분당선", "red", 1L, 2L, 100));
+        Line line = lineRepository.findById(1L).get();
+        Station 미금역 = stationRepository.findById(3L).get();
+        Station 정자역 = stationRepository.findById(4L).get();
 
+        Section section = new Section(line,미금역,정자역,200);
 //        // when
-        lineService.addSection(1L, new SectionRequest(2L, 3L, 200));
-
+        lineService.addSection(1L, new SectionRequest(3L,4L,200));
+//
 //        // then
         Line 신분당선 = lineRepository.findById(1L).get();
-        assertThat(신분당선.getSections().get(0).getDownStation().getName()).isEqualTo("강남");
-        assertThat(신분당선.getSections().get(0).getUpStation().getName()).isEqualTo("양재");
-        assertThat(신분당선.getSections().get(1).getDownStation().getName()).isEqualTo("양재");
-        assertThat(신분당선.getSections().get(1).getUpStation().getName()).isEqualTo("미금");
+
+        assertThat(신분당선.getSections().getSections().get(0).getUpStation().getName()).isEqualTo("강남");
+        assertThat(신분당선.getSections().getSections().get(0).getDownStation().getName()).isEqualTo("양재");
+        assertThat(신분당선.getSections().getSections().get(1).getUpStation().getName()).isEqualTo("미금");
+        assertThat(신분당선.getSections().getSections().get(1).getDownStation().getName()).isEqualTo("정자");
     }
 
     public void 지하철_역_저장() {
