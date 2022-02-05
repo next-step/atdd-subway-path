faff5b33ace00d00f3167e25b1937d3dab1c1ca0
gisungchoi <chlrltjd159@naver.com>
2022-02-05 20:24:54 +0900

    feat : 예외 테스트 추가

--- src/main/java/nextstep/subway/domain/PathFinder.java
+++ src/main/java/nextstep/subway/domain/PathFinder.java
@@ -1,15 +1,18 @@
 package nextstep.subway.domain;
 
 import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.exception.BusinessException;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
 public class PathFinder {
 
     public static PathResponse findPath(List<Line> lines, Station from, Station to) {
+        pathValidCheck(from, to);
         WeightedMultigraph<Station, DefaultWeightedEdge> graph
                 = new WeightedMultigraph(DefaultWeightedEdge.class);
 
@@ -17,12 +20,26 @@ public class PathFinder {
             createGraph(line, graph);
         }
 
-        DijkstraShortestPath dijkstraShortestPath
-                = new DijkstraShortestPath(graph);
-        List<Station> shortestPath
-                = dijkstraShortestPath.getPath(from, to).getVertexList();
-        double pathWeight = dijkstraShortestPath.getPathWeight(from, to);
-        return PathResponse.of(shortestPath, (int) pathWeight);
+        return getPathResponse(from, to, graph);
+    }
+
+    private static void pathValidCheck(Station from, Station to) {
+        if (from.getId().equals(to.getId())) {
+            throw new BusinessException("출발지와 도착지가 같습니다", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private static PathResponse getPathResponse(Station from, Station to, WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+        try {
+            DijkstraShortestPath dijkstraShortestPath
+                    = new DijkstraShortestPath(graph);
+            List<Station> shortestPath
+                    = dijkstraShortestPath.getPath(from, to).getVertexList();
+            double pathWeight = dijkstraShortestPath.getPathWeight(from, to);
+            return PathResponse.of(shortestPath, (int) pathWeight);
+        } catch (Exception e) {
+            throw new BusinessException("연결되지 않음", HttpStatus.BAD_REQUEST);
+        }
     }
 
     private static void createGraph(Line line, WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -32,11 +32,7 @@ public class PathAcceptanceTest extends AcceptanceTest{
         양재역 = 지하철역생성("양재역").jsonPath().getLong(지하철_역_아이디_키);
         남부터미널역 = 지하철역생성("남부터미널역").jsonPath().getLong(지하철_역_아이디_키);
 
-        이호선 = 노선생성(노선파라미터생성("2호선", "green", 교대역, 강남역, 100)).jsonPath().getLong(노선_아이디_키);
-        신분당선  = 노선생성(노선파라미터생성("신분당선", "red", 강남역, 양재역, 10)).jsonPath().getLong(노선_아이디_키);
-        삼호선  = 노선생성(노선파라미터생성("3호선", "orange", 교대역, 남부터미널역, 10)).jsonPath().getLong(노선_아이디_키);
 
-        구간등록(삼호선,남부터미널역, 양재역, 50);
     }
 
     /**             100
@@ -57,17 +53,72 @@ public class PathAcceptanceTest extends AcceptanceTest{
     @Test
     void 최단_경로를_조회(){
         //given
-
+        이호선 = 노선생성(노선파라미터생성("2호선", "green", 교대역, 강남역, 100)).jsonPath().getLong(노선_아이디_키);
+        신분당선 = 노선생성(노선파라미터생성("신분당선", "red", 강남역, 양재역, 10)).jsonPath().getLong(노선_아이디_키);
+        삼호선 = 노선생성(노선파라미터생성("3호선", "orange", 교대역, 남부터미널역, 10)).jsonPath().getLong(노선_아이디_키);
+        구간등록(삼호선, 남부터미널역, 양재역, 50);
 
         //when
-        ExtractableResponse<Response> 최단_거리_응답 = 최단_경로_조회_요청(교대역,양재역);
+        ExtractableResponse<Response> 최단_거리_응답 = 최단_경로_조회_요청(교대역, 양재역);
 
         //then
-        단일_값_검사(최단_거리_응답,"distance","60");
-        리스트_값_검사(최단_거리_응답,"stations.id",Math.toIntExact(교대역),Math.toIntExact(양재역),Math.toIntExact(남부터미널역));
+        단일_값_검사(최단_거리_응답, "distance", "60");
+        리스트_값_검사(최단_거리_응답, "stations.id", Math.toIntExact(교대역), Math.toIntExact(양재역), Math.toIntExact(남부터미널역));
     }
 
-    private ExtractableResponse<Response> 최단_경로_조회_요청(Long 출발역,Long 도착역){
+
+    /**
+     * 교대역                 강남역
+     * |                        |
+     * 10 * *3호선*                   *신분당선*      10
+     * |                        |
+     * 남부터미널역                양재
+     *              50
+     */
+
+    /**
+     * Given 노선들을 등록한다
+     * When  연결되지 않은 역의 최단 경로 조회를 요청한다
+     * Then  경로 탐색에 실패한다.
+     */
+    @DisplayName("최단 경로를 조회 시 출발지와 목적지는 연결되어 있어야한다")
+    @Test
+    void 출발역과_도착역이_연결되지_않는_경우_실패() {
+        //given
+        신분당선 = 노선생성(노선파라미터생성("신분당선", "red", 강남역, 양재역, 10)).jsonPath().getLong(노선_아이디_키);
+        삼호선 = 노선생성(노선파라미터생성("3호선", "orange", 교대역, 남부터미널역, 10)).jsonPath().getLong(노선_아이디_키);
+
+        //when
+        ExtractableResponse<Response> 최단_거리_응답 = 최단_경로_조회_요청(남부터미널역, 양재역);
+
+        //then
+        예외_검사(최단_거리_응답, "연결되지 않음");
+    }
+
+    /**
+     * Given 노선들을 등록한다
+     * When  연결되지 않은 역의 최단 경로 조회를 요청한다
+     * Then  경로 탐색에 실패한다.
+     */
+    @DisplayName("출발역과 도착역이 같을 경우 실패")
+    @Test
+    void 출발역과_도착역이_같을_경우_실패() {
+        //given
+        이호선 = 노선생성(노선파라미터생성("2호선", "green", 교대역, 강남역, 100)).jsonPath().getLong(노선_아이디_키);
+        신분당선 = 노선생성(노선파라미터생성("신분당선", "red", 강남역, 양재역, 10)).jsonPath().getLong(노선_아이디_키);
+        삼호선 = 노선생성(노선파라미터생성("3호선", "orange", 교대역, 남부터미널역, 10)).jsonPath().getLong(노선_아이디_키);
+        구간등록(삼호선, 남부터미널역, 양재역, 50);
+
+        //when
+        ExtractableResponse<Response> 최단_거리_응답 = 최단_경로_조회_요청(남부터미널역, 남부터미널역);
+
+        //then
+        예외_검사(최단_거리_응답, "출발지와 도착지가 같습니다");
+    }
+
+    private ExtractableResponse<Response> 최단_경로_조회_요청(Long 출발역, Long 도착역) {
         return HttpRequestTestUtil.겟_요청("/paths?source=" + 출발역 + "&target=" + 도착역);
     }
+
+
 }
