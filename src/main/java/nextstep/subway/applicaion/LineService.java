3b46e79347e8de29edcbf2b80f954eb0e84d56da
dk-ko <dev.dkko@gmail.com>
2022-02-05 17:57:48 +0900

    feat: add Section

--- src/main/java/nextstep/subway/applicaion/LineService.java
+++ src/main/java/nextstep/subway/applicaion/LineService.java
@@ -31,7 +31,7 @@ public class LineService {
         if (request.getUpStationId() != null && request.getDownStationId() != null && request.getDistance() != 0) {
             Station upStation = stationService.findById(request.getUpStationId());
             Station downStation = stationService.findById(request.getDownStationId());
-            line.getSections().add(new Section(line, upStation, downStation, request.getDistance()));
+            line.addSection(new Section(line, upStation, downStation, request.getDistance()));
         }
         return createLineResponse(line);
     }
@@ -68,7 +68,7 @@ public class LineService {
         Station downStation = stationService.findById(sectionRequest.getDownStationId());
         Line line = lineRepository.findById(lineId).orElseThrow(IllegalArgumentException::new);
 
-        line.getSections().add(new Section(line, upStation, downStation, sectionRequest.getDistance()));
+        line.addSection(new Section(line, upStation, downStation, sectionRequest.getDistance()));
     }
 
     private LineResponse createLineResponse(Line line) {
--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,12 +1,23 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
     @DisplayName("구간 목록 마지막에 새로운 구간을 추가할 경우")
     @Test
     void addSection() {
+        Line line = new Line();
+
+        int num = 2;
+        for (int i = 0; i < num; i++) {
+            line.addSection(new Section());
+        }
+        assertThat(line.getSections().size()).isEqualTo(num);
     }
 
     @DisplayName("노선에 속해있는 역 목록 조회")
