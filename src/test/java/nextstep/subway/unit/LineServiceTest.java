176d1c37c9b63ec9a654561eacafa27dcb0cb4c7
원스톤 <onestone@socar.kr>
2022-02-05 14:10:36 +0900

    test: LineServiceTest#addSection - 테스트 추가

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,25 @@
 package nextstep.subway.unit;
 
+import lombok.val;
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import nextstep.subway.domain.object.Distance;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static nextstep.subway.unit.test.utils.Stations.삼성역;
+import static nextstep.subway.unit.test.utils.Stations.역_초기화;
+import static org.assertj.core.api.Assertions.assertThat;
+import static nextstep.subway.unit.test.utils.Lines.GTXA_연신내_서울역;
+import static nextstep.subway.unit.test.utils.Lines.노선_초기화;
+import static nextstep.subway.unit.test.utils.Stations.서울역;
 
 @SpringBootTest
 @Transactional
@@ -19,15 +32,30 @@ public class LineServiceTest {
     @Autowired
     private LineService lineService;
 
+    @BeforeEach
+    public void setUp() {
+        역_초기화();
+        노선_초기화();
+    }
+
     @Test
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station upStation = stationRepository.save(서울역);
+        Station downStation = stationRepository.save(삼성역);
+        Line line = lineRepository.save(GTXA_연신내_서울역);
+        SectionRequest sectionRequest = new SectionRequest(upStation.getId(), downStation.getId(), new Distance(10));
 
         // when
         // lineService.addSection 호출
+        lineService.saveSection(line.getId(), sectionRequest);
 
         // then
-        // line.getSections 메서드를 통해 검증
+        val stations = line.getAllStations();
+
+        assertThat(stations.size()).isEqualTo(3);
+        assertThat(stations.get(1)).isEqualTo(upStation);
+        assertThat(stations.get(2)).isEqualTo(downStation);
     }
 }
