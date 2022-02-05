e83edb203e00a007264d4855470d5221270f72e5
mandykr <hongseokii92@gmail.com>
2022-02-05 22:33:45 +0900

    test: Line 단위테스트 지하철역의 이름으로 검증하도록 수정

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -8,74 +8,104 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 class LineTest {
+    Station 신림역;
+    Station 봉천역;
+    Station 서울대입구역;
     Line line;
-    Station firstUpStation;
-    Station firstDownStation;
-    Section firstSection;
-
-    Station newStation;
-    Section newSection;
+    Section section;
+    int distance = 10;
 
     @BeforeEach
     void setUp() {
+        신림역 = new Station("신림역");
+        봉천역 = new Station("봉천역");
+        서울대입구역 = new Station("서울대입구역");
         line = new Line("2호선", "green");
-        firstUpStation = new Station("신림역");
-        firstDownStation = new Station("봉천역");
-        firstSection = new Section(line, firstUpStation, firstDownStation, 10);
-        newStation = new Station("서울대입구역");
-        newSection = new Section(line, firstDownStation, newStation, 10);
+        section = new Section(line, 신림역, 봉천역, distance);
 
-        setField(firstUpStation, "id", 1L);
-        setField(firstDownStation, "id", 2L);
-        setField(newStation, "id", 3L);
-        setField(firstSection, "id", 1L);
-        setField(newSection, "id", 2L);
-
-        line.getSections().add(firstSection);
+        line.getSections().getSections().add(section);
     }
-    @DisplayName("구간 목록 마지막에 새로운 구간을 추가할 경우")
+
+    @DisplayName("구간 목록 처음에 새로운 구간을 추가할 경우")
     @Test
-    void addSection() {
+    void addFirstSection() {
+        // given
+        Section newSection = new Section(line, 서울대입구역, 신림역, 10);
+
         // when
         line.addSection(newSection);
 
         // then
-        List<Long> ids = line.getSections().getSections().stream().map(Section::getId).collect(Collectors.toList());
-        assertThat(ids).containsExactly(firstSection.getId(), newSection.getId());
+        assertThat(line.getSections().getSections())
+                .extracting(s -> s.getUpStation().getName())
+                .containsOnly(서울대입구역.getName(), 신림역.getName());
+    }
 
+    @DisplayName("구간 목록 마지막에 새로운 구간을 추가할 경우")
+    @Test
+    void addLastSection() {
+        // given
+        Section newSection = new Section(line, 봉천역, 서울대입구역, 10);
+
+        // when
+        line.addSection(newSection);
+
+        // then
+        assertThat(line.getSections().getSections())
+                .extracting(s -> s.getDownStation().getName())
+                .containsOnly(봉천역.getName(), 서울대입구역.getName());
+    }
+
+    @DisplayName("구간 목록 중간에 새로운 구간을 추가할 경우")
+    @Test
+    void addMiddleSection() {
+        // given
+        Section targetSection = new Section(line, 봉천역, 서울대입구역, distance);
+        line.getSections().getSections().add(targetSection);
+        int newDistance = 7;
+        Station newStation = new Station("낙성대역");
+        Section newSection = new Section(line, 봉천역, newStation, newDistance);
+
+        // when
+        line.addSection(newSection);
+
+        // then
+        assertThat(line.getSections().getSections())
+                .extracting(s -> s.getUpStation().getName())
+                .containsOnly(신림역.getName(), 봉천역.getName(), newStation.getName());
+        assertThat(targetSection.getDistance()).isEqualTo(distance - newDistance);
     }
 
     @DisplayName("노선에 속해있는 역 목록 조회")
     @Test
     void getStations() {
         // given
-        line.addSection(newSection);
+        line.addSection(new Section(line, 봉천역, 서울대입구역, 10));
 
         // when
         List<Station> stations = line.getStations();
 
         // then
-        List<Long> ids = stations.stream().map(Station::getId).collect(Collectors.toList());
-        assertThat(ids).containsExactly(firstUpStation.getId(), firstDownStation.getId(), newStation.getId());
+        assertThat(stations).extracting(Station::getName)
+                .containsOnly(신림역.getName(), 봉천역.getName(), 서울대입구역.getName());
     }
 
-    @DisplayName("구간이 목록에서 마지막 역 삭제")
+    @DisplayName("구간의 목록에서 마지막 역 삭제")
     @Test
     void removeSection() {
         // given
-        line.addSection(newSection);
+        line.addSection(new Section(line, 봉천역, 서울대입구역, 10));
 
         // when
-        line.removeSection(newStation.getId());
+        line.removeSection(서울대입구역);
 
         // then
-        List<Long> ids = line.getSections().getSections().stream().map(Section::getId).collect(Collectors.toList());
-        assertThat(ids).containsExactly(firstSection.getId());
+        assertThat(line.getSections().getSections())
+                .extracting(s -> s.getDownStation().getName())
+                .containsOnly(봉천역.getName());
     }
 }
