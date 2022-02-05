dc27e8f2e98bfdab29942f4d8b2d0fb338e66e35
songsimo <chy.song89@gmail.com>
2022-02-05 22:50:31 +0900

    LineServiceTest 단위테스트 작성

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -7,6 +7,11 @@ public class LineRequest {
     private Long downStationId;
     private int distance;
 
+    public LineRequest(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
     public String getName() {
         return name;
     }
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,21 @@
 package nextstep.subway.unit;
 
-import nextstep.subway.applicaion.LineService;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.StationRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
 
 @SpringBootTest
 @Transactional
@@ -23,11 +32,28 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 교대역 = stationRepository.save(new Station("교대역"));
+        Station 강남역 = stationRepository.save(new Station("강남역"));
+        Station 역삼역 = stationRepository.save(new Station("역삼역"));
+        int distance = 10;
+
+        Line 이호선 = new Line("2호선", "bg-green-600");
+        이호선.getSections().add(new Section(이호선, 교대역, 강남역, distance));
+        lineRepository.save(이호선);
+        SectionRequest sectionRequest = new SectionRequest(강남역.getId(), 역삼역.getId(), distance);
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(이호선.getId(), sectionRequest);
 
         // then
         // line.getSections 메서드를 통해 검증
+        List<Section> sections = 이호선.getSections();
+        Section section = sections.get(sections.size() - 1);
+
+        assertThat(section.getUpStation()).isEqualTo(강남역);
+        assertThat(section.getDownStation()).isEqualTo(역삼역);
+        assertThat(section.getDownStation()).isEqualTo(distance);
+
     }
 }
