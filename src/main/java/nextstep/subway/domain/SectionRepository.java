eb609e07671f1bba129aa98344c85018afe7b713
minjoon_lee <minjoon1995@naver.com>
2022-02-06 11:47:18 +0900

    fix : upstation과 downstation 위치 변경 및 dto에서 model mapper 기능 담당

--- src/main/java/nextstep/subway/domain/SectionRepository.java
+++ src/main/java/nextstep/subway/domain/SectionRepository.java
@@ -2,5 +2,5 @@ package nextstep.subway.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SectionRepository extends JpaRepository<Section, Long> {
+public interface SectionRepository extends JpaRepository<Section,Long> {
 }
