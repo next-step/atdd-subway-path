872845f7e4f021c6392428cd19279e8947e833ba
minjoon_lee <minjoon1995@naver.com>
2022-02-06 12:42:26 +0900

    add : test 코드추가

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -20,8 +20,6 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
 
     private Long 강남역;
     private Long 양재역;
-    private Long 신도림역;
-    private Long 문래역;
 
     /**
      * Given 지하철역과 노선 생성을 요청 하고
@@ -31,116 +29,43 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         super.setUp();
         강남역 = 지하철역_생성_요청("강남역").jsonPath().getLong("id");
         양재역 = 지하철역_생성_요청("양재역").jsonPath().getLong("id");
-        신도림역 = 지하철역_생성_요청("신도림역").jsonPath().getLong("id");
-        문래역 = 지하철역_생성_요청("문래역").jsonPath().getLong("id");
-
-        Map<String, String> lineCreateParams = createLineCreateParams(강남역, 양재역, 10);
+        Map<String, String> lineCreateParams = createLineCreateParams(강남역, 양재역,10);
         신분당선 = 지하철_노선_생성_요청(lineCreateParams).jsonPath().getLong("id");
     }
 
     /**
-     * When 지하철 노선에 강남->양재 구간에 강남->정자 구간 추가를 요청 하면
-     * Then
-     *  1. 200 status code 가 반환된다.
-     *  2. 강남->정자, 정자->양재 새로운 구간이 추가된다
+     * When 지하철 노선에 A->C 구간에 B->C 구간 추가를 요청 하면
+     * Then A->B, B->C 새로운 구간이 추가된다
      */
     @DisplayName("기존 구간의 왼쪽과 중간에 구간을 등록")
     @Test
-    void addLeftAndMiddleSection() {
+    void addMiddleSection() {
         // when
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 정자역, 5));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 정자역,5));
         // then
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getList("sections.downStation.name", String.class)).containsExactly("강남역", "정자역");
         assertThat(response.jsonPath().getList("sections.upStation.name", String.class)).containsExactly("정자역", "양재역");
+
     }
 
     /**
-     * When 지하철 노선에 강남->양재 구간에 정자->양재 구간 추가를 요청 하면
-     * Then
-     *  1. 200 status code 가 반환된다.
-     *  2. 강남->양재, 정자->양재 새로운 구간이 추가된다
-     */
-    @DisplayName("기존 구간의 중간과 오른쪽에 구간을 등록")
-    @Test
-    void addMiddleAndRightSection() {
-        // when
-        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(정자역, 양재역, 5));
-        // then
-        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.jsonPath().getList("sections.downStation.name", String.class)).containsExactly("강남역", "정자역");
-        assertThat(response.jsonPath().getList("sections.upStation.name", String.class)).containsExactly("정자역", "양재역");
-    }
-
-    /**
-     * When 지하철 노선에 강남역->양재역 구간에 양재역->정자역 구간 추가를 요청 하면
-     * Then
-     *  1. 200 status code 가 반환된다.
-     *  2. 강남역->양재역, 양재역->정자역 새로운 구간이 추가된다
+     * When 지하철 노선에 A->C 구간에 C->B 구간 추가를 요청 하면
+     * Then A->B, C->B 새로운 구간이 추가된다
      */
     @DisplayName("기존 구간의 오른쪽에 구간을 등록")
     @Test
     void addRightSection() {
         // when
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역, 5));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역,5));
         // then
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getList("sections.downStation.name", String.class)).containsExactly("강남역", "양재역");
         assertThat(response.jsonPath().getList("sections.upStation.name", String.class)).containsExactly("양재역", "정자역");
-    }
-
-    /**
-     * When 지하철 노선에 강남역->양재역 구간에 정자역->강남역 구간 추가를 요청 하면
-     * Then
-     *  1. 200 status code 가 반환된다.
-     *  2. 정자역->강남역, 강남역->양재역 새로운 구간이 추가된다
-     */
-    @DisplayName("기존 구간의 왼쪽에 구간을 등록")
-    @Test
-    void addLeftSection() {
-        // when
-        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(정자역, 강남역, 5));
-        // then
-        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.jsonPath().getList("sections.downStation.name", String.class)).containsExactly("정자역", "강남역");
-        assertThat(response.jsonPath().getList("sections.upStation.name", String.class)).containsExactly("강남역", "양재역");
-    }
-    /**
-     * When 지하철 노선에 강남역->양재역 구간에 신도림역->문래역 구간 추가를 요청 하면
-     * Then
-     *  1. 400 status code 가 반환된다.
-     *  2. 구간 생성이 실패한다.
-     */
-    @DisplayName("노선에 존재하지 않는 상행역과 하행역을 가지는 구간 생성 요청")
-    @Test
-    void NoMatchSectionException() {
-        // when
-        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(신도림역,문래역, 5));
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-    }
-
-    /**
-     * When 지하철 노선에 강남역->양재역 구간에 강남역->양재역 구간 추가를 요청 하면
-     * Then
-     *  1. 400 status code 가 반환된다.
-     *  2. 구간 생성이 실패한다.
-     */
-    @DisplayName("노선에 이미 존재하는 상행역과 하행역을 가지는 구간 생성 요청")
-    @Test
-    void AllMatchSectionException() {
-        // when
-        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역,양재역, 5));
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     /**
@@ -164,7 +89,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역);
     }
 
-    private Map<String, String> createLineCreateParams(Long downStationId, Long upStationId, int distance) {
+    private Map<String, String> createLineCreateParams(Long downStationId, Long upStationId,  int distance) {
         Map<String, String> lineCreateParams;
         lineCreateParams = new HashMap<>();
         lineCreateParams.put("name", "신분당선");
@@ -175,7 +100,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         return lineCreateParams;
     }
 
-    private Map<String, String> createSectionCreateParams(Long downStationId, Long upStationId, int distance) {
+    private Map<String, String> createSectionCreateParams(Long downStationId, Long upStationId,  int distance) {
         Map<String, String> params = new HashMap<>();
         params.put("downStationId", downStationId + "");
         params.put("upStationId", upStationId + "");
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -26,29 +26,25 @@ public class LineServiceTest {
     private StationService stationService;
     @Autowired
     private LineService lineService;
-
+    @Autowired
+    private SectionRepository sectionRepository;
     @Test
     void addSection() {
         // given
         지하철_역_저장();
-
-        LineService lineService = new LineService(lineRepository, stationService);
+        LineService lineService = new LineService(lineRepository, stationService,sectionRepository);
         lineService.saveLine(new LineRequest("신분당선", "red", 1L, 2L, 100));
-        Line line = lineRepository.findById(1L).get();
-        Station 미금역 = stationRepository.findById(3L).get();
-        Station 정자역 = stationRepository.findById(4L).get();
 
-        Section section = new Section(line,미금역,정자역,200);
 //        // when
-        lineService.addSection(1L, new SectionRequest(3L,4L,200));
-//
+        lineService.addSection(1L, new SectionRequest(2L,3L,200));
+
 //        // then
         Line 신분당선 = lineRepository.findById(1L).get();
 
-        assertThat(신분당선.getSections().getSections().get(0).getUpStation().getName()).isEqualTo("강남");
-        assertThat(신분당선.getSections().getSections().get(0).getDownStation().getName()).isEqualTo("양재");
-        assertThat(신분당선.getSections().getSections().get(1).getUpStation().getName()).isEqualTo("미금");
-        assertThat(신분당선.getSections().getSections().get(1).getDownStation().getName()).isEqualTo("정자");
+        assertThat(신분당선.getSections().get(0).getUpStation().getName()).isEqualTo("강남");
+        assertThat(신분당선.getSections().get(0).getDownStation().getName()).isEqualTo("양재");
+        assertThat(신분당선.getSections().get(1).getUpStation().getName()).isEqualTo("양재");
+        assertThat(신분당선.getSections().get(1).getDownStation().getName()).isEqualTo("미금");
     }
 
     public void 지하철_역_저장() {
