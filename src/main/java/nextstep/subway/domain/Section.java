ad1a83cab5f9ca09c7b86366b0b285acd5c589f7
minjoon_lee <minjoon1995@naver.com>
2022-02-06 12:40:36 +0900

    add : comparable 추가

--- src/main/java/nextstep/subway/domain/Section.java
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -3,7 +3,7 @@ package nextstep.subway.domain;
 import javax.persistence.*;
 
 @Entity
-public class Section {
+public class Section implements Comparable<Section> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,17 +26,26 @@ public class Section {
 
     }
 
-    public Section(Line line, Station upStation, Station downStation, int distance) {
+    public Section(Line line, Station downStation, Station upStation, int distance) {
         this.line = line;
+        this.downStation = downStation;
+        this.upStation = upStation;
+        this.distance = distance;
+    }
+
+    public Section(Station downStation, Station upStation, int distance) {
         this.upStation = upStation;
         this.downStation = downStation;
         this.distance = distance;
     }
-    public Section(Station upStation, Station downStation, int distance) {
+
+    public Section(Station downStation, Station upStation, int distance, Line line) {
         this.upStation = upStation;
         this.downStation = downStation;
         this.distance = distance;
+        this.line = line;
     }
+
     public Long getId() {
         return id;
     }
@@ -55,5 +64,26 @@ public class Section {
 
     public int getDistance() {
         return distance;
+    }
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "id=" + id +
+                ", line=" + line +
+                ", upStation=" + upStation +
+                ", downStation=" + downStation +
+                ", distance=" + distance +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Section o) {
+        if (o.getId() < id) {
+            return 1;
+        } else if (o.getId() > id) {
+            return -1;
+        }
+        return 0;
     }
 }
