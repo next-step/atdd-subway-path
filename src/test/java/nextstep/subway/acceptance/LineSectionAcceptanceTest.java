1a9790248c8fddb9d54a6dfc91395847a65b1ec7
povia <47127607+povia@users.noreply.github.com>
2022-02-06 18:21:02 +0900

    step1 test: 실패 케이스 추가

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -7,10 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static nextstep.subway.acceptance.LineSteps.*;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
@@ -80,6 +77,62 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
 
     }
 
+    /**
+     * Given 노선을 생성하고 해당 노선에 종점역을 추가한 후
+     * When 해당 노선의 전체 구간 중간에 구간 추가를 요청하면
+     * Then 구간 추가가 성공한다.
+     */
+    @DisplayName("노선 중간에 구간 추가(성공 케이스)")
+    @Test
+    void addMiddleStationsTest() {
+        // given
+        Long 정자역 = 지하철역_생성_요청_후_ID_반환("정자역");
+
+        // when
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 정자역, 3));
+
+        // then
+        List<Long> 지하철역_리스트 = createListOfStations(강남역, 양재역);
+        지하철_노선_조회_요청_후_역_검증(신분당선, 지하철역_리스트);
+    }
+
+    /**
+     * Given 노선을 생성하고 해당 노선에 종점역을 추가한 후
+     * When 해당 노선의 전체 구간 중간에 길이가 같거나 더 긴 구간 추가를 요청하면
+     * Then 구간 추가가 실패한다.
+     */
+    @DisplayName("노선 중간에 길이가 더 긴 구간 추가(실패 케이스)")
+    @Test
+    void addMiddleStationsFailedTest() {
+        // given
+        Long 정자역 = 지하철역_생성_요청_후_ID_반환("정자역");
+
+        // when
+        ExtractableResponse<Response> createResponse = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 정자역));
+
+        // then
+        assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    /**
+     * Given 노선을 생성하고 해당 노선에 종점역을 추가한 후
+     * When 해당 노선의 구간에 존재하지 않는 역들을 구간으로 추가할 경우
+     * Then 구간 추가가 실패한다.
+     */
+    @DisplayName("노선에 상 하행 역 존재하지 않는 구간 추가 실패")
+    @Test
+    void addSectionWithoutExistStationsTest() {
+        // given
+        Long 정자역 = 지하철역_생성_요청_후_ID_반환("정자역");
+        Long 미금역 = 지하철역_생성_요청_후_ID_반환("미금역");
+
+        // when
+        ExtractableResponse<Response> createResponse = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(정자역, 미금역, 3));
+
+        // then 구간 추가 실패해야 한다.
+        assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
     private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
         Map<String, String> lineCreateParams;
         lineCreateParams = new HashMap<>();
@@ -91,11 +144,19 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         return lineCreateParams;
     }
 
-    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId) {
+    public Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId) {
+        return createSectionCreateParams(upStationId, downStationId, 6);
+    }
+
+    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, int distance) {
         Map<String, String> params = new HashMap<>();
         params.put("upStationId", upStationId + "");
         params.put("downStationId", downStationId + "");
-        params.put("distance", 6 + "");
+        params.put("distance", distance + "");
         return params;
+    }
+
+    private List<Long> createListOfStations(Long ... stations){
+        return Arrays.asList(stations);
     }
 }
