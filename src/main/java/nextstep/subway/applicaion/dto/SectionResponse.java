1c907f48730ea9d2d24200966fdd47a555156932
minjoon_lee <minjoon1995@naver.com>
2022-02-06 12:43:24 +0900

    fix : intellij clean code

--- src/main/java/nextstep/subway/applicaion/dto/SectionResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/SectionResponse.java
@@ -3,10 +3,10 @@ package nextstep.subway.applicaion.dto;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
 
-public class SectionResponse implements Comparable<SectionResponse>{
-    private Long id;
-    private Station upStation;
-    private Station downStation;
+public class SectionResponse implements Comparable<SectionResponse> {
+    private final Long id;
+    private final Station upStation;
+    private final Station downStation;
 
     public SectionResponse(Section section) {
         this.id = section.getId();
--- src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
@@ -5,10 +5,10 @@ import nextstep.subway.domain.Station;
 import java.time.LocalDateTime;
 
 public class StationResponse {
-    private Long id;
-    private String name;
-    private LocalDateTime createdDate;
-    private LocalDateTime modifiedDate;
+    private final Long id;
+    private final String name;
+    private final LocalDateTime createdDate;
+    private final LocalDateTime modifiedDate;
 
     public StationResponse(Long id, String name, LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
