c78d3cfa05f5a4fb8a25f1f925773b9db59dec9a
minjoon_lee <minjoon1995@naver.com>
2022-02-06 18:06:07 +0900

    feature : test코드에 exception 관련 코드 추가

--- src/main/java/nextstep/subway/exception/IllegalSectionException.java
+++
@@ -1,9 +0,0 @@
-package nextstep.subway.exception;
-
-public class IllegalSectionException extends RuntimeException {
-    private static final String MESSAGE = "상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음";
-
-    public IllegalSectionException() {
-        super(MESSAGE);
-    }
-}
