55ebaa49ad8d35127098d036d70b8157e6421a65
minjoon_lee <minjoon1995@naver.com>
2022-02-06 11:47:18 +0900

    fix : upstation과 downstation 위치 변경 및 dto에서 model mapper 기능 담당

---
+++ src/main/java/nextstep/subway/exception/ExceptionHandler.java
@@ -0,0 +1,33 @@
+package nextstep.subway.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalDeleteSectionException.class)
+    public ResponseEntity<HttpStatus> checkIllegalDeleteSection() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalDistanceException.class)
+    public ResponseEntity<HttpStatus> checkIllegalAddSection() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalSectionException.class)
+    public ResponseEntity<HttpStatus> checkIllegalSectionException() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(NoMatchSectionException.class)
+    public ResponseEntity<HttpStatus> checkNoMatchException() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(NotMatchDeleteSectionException.class)
+    public ResponseEntity<HttpStatus> checkNotMatchDeleteSectionException() {
+        return ResponseEntity.badRequest().build();
+    }
+}
