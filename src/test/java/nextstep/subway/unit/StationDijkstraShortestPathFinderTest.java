787ec38fbbbadb6661965de09dbffe7be6741a89
kimuno89 <kimunoya@gmail.com>
2022-02-07 23:22:06 +0900

    test:  지하철역이 연결되지 않은 경우 도메인 테스트 추가

--- src/test/java/nextstep/subway/unit/StationDijkstraShortestPathFinderTest.java
+++ src/test/java/nextstep/subway/unit/StationDijkstraShortestPathFinderTest.java
@@ -18,6 +18,8 @@ public class StationDijkstraShortestPathFinderTest {
     private final static Station 강남역 = new Station(2L, "강남역");
     private final static Station 양재역 = new Station(3L, "양재역");
     private final static Station 남부터미널역 = new Station(4L, "남부터미널역");
+    private final static Station 충무로역 = new Station(5L, "충무로역");
+    private final static Station 명동역 = new Station(6L, "명동역");
 
     private StationDijkstraShortestStationPathFinder pathFinder;
 
@@ -27,16 +29,18 @@ public class StationDijkstraShortestPathFinderTest {
         Line 이호선 = new Line("2호선", "bg-green-600");
         Line 삼호선 = new Line("3호선", "bg-orange-600");
         Line 신분당선 = new Line("신분당선", "bg-pink-600");
-
+        Line 사호선 = new Line("4호선", "bg-blue-600");
 
         이호선.addSection(new Section(이호선, 교대역, 강남역, 10));
         신분당선.addSection(new Section(신분당선, 강남역, 양재역, 10));
         삼호선.addSection(new Section(삼호선, 교대역, 남부터미널역, 2));
         삼호선.addSection(new Section(삼호선, 남부터미널역, 양재역, 3));
+        사호선.addSection(new Section(사호선, 충무로역, 명동역, 3));
 
         lines.add(이호선);
         lines.add(삼호선);
         lines.add(신분당선);
+        lines.add(사호선);
 
         pathFinder = StationDijkstraShortestStationPathFinder.ofLines(new Lines(lines));
     }
@@ -50,4 +54,10 @@ public class StationDijkstraShortestPathFinderTest {
         assertThat(path.getWeight()).isEqualTo(5);
     }
 
+    @Test
+    @DisplayName("지하철 경로가 연결되지 않은 경우")
+    void notConnectedPath() {
+        GraphPath path = pathFinder.getPath(교대역.getId().toString(), 명동역.getId().toString());
+        assertThat(path).isEqualTo(null);
+    }
 }
