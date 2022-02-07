1770778d5a05b1ac8c80b86713a7124532ccbe71
abcdsds <choiyurim2@naver.com>
2022-02-07 20:15:22 +0900

    feat: 최단거리 찾기 기능 구현

---
+++ src/main/java/nextstep/subway/domain/PathFinder.java
@@ -0,0 +1,34 @@
+package nextstep.subway.domain;
+
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.alg.shortestpath.KShortestPaths;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+import java.util.List;
+
+public class PathFinder {
+    private final WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+
+    public PathFinder(List<Line> lines) {
+        lines.stream()
+                .map(line -> line.getSections())
+                .flatMap(sections -> sections.stream())
+                .forEach(this::setEdgeWeight);
+    }
+
+    public List<Station> getShortestPath(Station source, Station target) {
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        return dijkstraShortestPath.getPath(source, target).getVertexList();
+    }
+
+    private void setEdgeWeight(Section section) {
+        Station upStation = section.getUpStation();
+        Station downStation = section.getDownStation();
+
+        graph.addVertex(upStation);
+        graph.addVertex(downStation);
+        graph.setEdgeWeight(graph.addEdge(upStation, downStation), section.getDistance());
+    }
+}
