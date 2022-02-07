9f09b603b54c4d96e45d57cdd3bf84830870faf8
monkeyDugi <lbd4946@gmail.com>
2022-02-07 15:45:48 +0900

    test : given 리팩토링

--- src/test/java/nextstep/subway/unit/PathFinderTest.java
+++ src/test/java/nextstep/subway/unit/PathFinderTest.java
@@ -5,6 +5,7 @@ import nextstep.subway.domain.Line;
 import nextstep.subway.domain.PathFinder;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,28 +17,53 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PathFinderTest {
 
-    @DisplayName("최단 경로 조회")
-    @Test
-    void getShortsPath() {
-        // given
-        Station 교대역 = new Station("교대역");
-        Station 강남역 = new Station("강남역");
-        Station 양재역 = new Station("양재역");
-        Station 남부터미널역 = new Station("남부터미널역");
+    Station 교대역;
+    Station 강남역;
+    Station 양재역;
+    Station 남부터미널역;
 
-        Line 이호선 = new Line("2호선", "green");
-        Line 삼호선 = new Line("3호선", "orange");
-        Line 신분당선 = new Line("신분당선", "red");
+    Line 이호선;
+    Line 삼호선;
+    Line 신분당선;
 
-        Section 교대역_강남역_구간 = new Section(이호선, 교대역, 강남역, 10);
-        Section 교대역_남부터미널_구간 = new Section(삼호선, 교대역, 남부터미널역, 2);
-        Section 남부터미널_양재역_구간 = new Section(삼호선, 남부터미널역, 양재역, 3);
-        Section 강남역_양재역_구간 = new Section(신분당선, 강남역, 양재역, 10);
+    Section 교대역_강남역_구간;
+    Section 교대역_남부터미널_구간;
+    Section 남부터미널_양재역_구간;
+    Section 강남역_양재역_구간;
+
+    @BeforeEach
+    void setUp() {
+        교대역 = new Station("교대역");
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        남부터미널역 = new Station("남부터미널역");
+
+        이호선 = new Line("2호선", "green");
+        삼호선 = new Line("3호선", "orange");
+        신분당선 = new Line("신분당선", "red");
+
+        교대역_강남역_구간 = new Section(이호선, 교대역, 강남역, 10);
+        교대역_남부터미널_구간 = new Section(삼호선, 교대역, 남부터미널역, 2);
+        남부터미널_양재역_구간 = new Section(삼호선, 남부터미널역, 양재역, 3);
+        강남역_양재역_구간 = new Section(신분당선, 강남역, 양재역, 10);
 
         이호선.addSection(교대역_강남역_구간);
         삼호선.addSection(교대역_남부터미널_구간);
         삼호선.addSection(남부터미널_양재역_구간);
         신분당선.addSection(강남역_양재역_구간);
+        /**
+         * 교대역    --- *2호선(10m)* ---   강남역
+         * |                               |
+         * *3호선(2m)*                   *신분당선(10m)*
+         * |                               |
+         * 남부터미널역  --- *3호선(3m)* ---   양재
+         */
+    }
+
+    @DisplayName("최단 경로 조회")
+    @Test
+    void getShortsPath() {
+        // given
         List<Line> lines = Arrays.asList(이호선, 삼호선, 신분당선);
         PathFinder pathFinder = new PathFinder(lines);
 
