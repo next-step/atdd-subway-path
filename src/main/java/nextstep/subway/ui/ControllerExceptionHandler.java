abc47e52c02749df404f72934b019344efe7dc46
abcdsds <choiyurim2@naver.com>
2022-02-09 01:09:59 +0900

    refactor: exceptionhandler 수정

--- src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
+++ src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package nextstep.subway.ui;
 
 import javassist.NotFoundException;
+import nextstep.subway.exception.IllegalPathArgumentException;
 import nextstep.subway.exception.IllegalSectionArgumentException;
 import nextstep.subway.exception.LineNotFoundException;
 import nextstep.subway.exception.StationNotFoundException;
@@ -11,13 +12,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<Void> handleIllegalArgsException(DataIntegrityViolationException e) {
+    @ExceptionHandler({DataIntegrityViolationException.class, IllegalSectionArgumentException.class, IllegalPathArgumentException.class})
+    public ResponseEntity<Void> handleIllegalArgsException() {
         return ResponseEntity.badRequest().build();
     }
 
-    @ExceptionHandler(IllegalSectionArgumentException.class)
-    public ResponseEntity<Void> handleIllegalSectionArgsException(IllegalSectionArgumentException e) {
+    @ExceptionHandler(StationNotFoundException.class)
+    public ResponseEntity<Void> stationNotFoundException() {
         return ResponseEntity.badRequest().build();
     }
 }
