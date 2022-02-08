12ccdb2b1f10a49b1bd6c4abdbac67d4cd49f3fb
apdkeb <apdkeb@naver.com>
2022-02-08 19:38:47 +0900

    fest : 최단 경로 인수테스트 예외상황 추가
    2022. 02. 08. 화요일
    PathAcceptanceTest
    - 예외 상황 테스트 추가
    
    Resolves : loop-study
    See also : kang-hyungu

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -20,6 +20,8 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private Long 강남역;
     private Long 양재역;
     private Long 남부터미널역;
+    private Long 동인천역;
+
     private Long 이호선;
     private Long 신분당선;
     private Long 삼호선;
@@ -39,6 +41,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
         강남역 = 지하철역_식별번호_가져오기(지하철역_생성_요청("강남역"));
         양재역 = 지하철역_식별번호_가져오기(지하철역_생성_요청("양재역"));
         남부터미널역 = 지하철역_식별번호_가져오기(지하철역_생성_요청("남부터미널역"));
+        동인천역 = 지하철역_식별번호_가져오기(지하철역_생성_요청("동인천역"));
 
         이호선 = 노선_식별번호_추출(지하철_노선_생성_요청(createLineCreateParams("2호선", "green", 교대역, 강남역, 10)));
         신분당선 = 노선_식별번호_추출(지하철_노선_생성_요청(createLineCreateParams("신분당선", "red", 강남역, 양재역, 10)));
@@ -50,21 +53,45 @@ public class PathAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 삼호선의 구간(남부터미널역-양재역)에 새로운 구간(교대역-남부터미널역)을 추가
      * Then 출발지(교대역)과 도착역(양재역)의 최단 경로를 요청하면
      * When 최단 경로를 조회한다.
      */
     @DisplayName("최단 경로(교대역 - 양재역) 조회")
     @Test
-    void getShortPath() {
-        //
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(교대역, 남부터미널역, 3));
-
+    void shortPath() {
         // when
         ExtractableResponse<Response> response = 최단_경로_조회하기(교대역, 양재역);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    /**
+     * Then 출발지(교대역)과 도착역(교대역)의 최단 경로를 요청하면
+     * When 최단 경로 조회가 실패한다
+     */
+    @DisplayName("최단 경로(교대역 - 교대역) 조회")
+    @Test
+    void sourceEqualsTargetException() {
+        // when
+        ExtractableResponse<Response> response = 최단_경로_조회하기(교대역, 교대역);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    /**
+     * Then 구간이 연결이 안된 출발지(교대역)과 도착역(남부터미널역)으로 최단 거리 요청하면
+     * When 최단 경로 조회가 실패한다
+     */
+    @DisplayName("최단 경로(교대역 - 동인천역) 조회")
+    @Test
+    void notConnectException() {
+        // when
+        ExtractableResponse<Response> response = 최단_경로_조회하기(교대역, 동인천역);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     private Map<String, String> createLineCreateParams(String name, String color, Long upStationId, Long downStationId, int distance) {
