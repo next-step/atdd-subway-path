e105bad2eb11a3f664bad19f36f462badc9570de
abcdsds <choiyurim2@naver.com>
2022-02-09 01:09:35 +0900

    feat: 길찾기 예외상황 테스트 추가 및 exception 수정

--- src/test/java/nextstep/subway/unit/PathFinderTest.java
+++ src/test/java/nextstep/subway/unit/PathFinderTest.java
@@ -3,7 +3,9 @@ package nextstep.subway.unit;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.PathFinder;
 import nextstep.subway.domain.Station;
+import nextstep.subway.exception.IllegalPathArgumentException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,33 +17,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PathFinderTest {
 
-    @DisplayName("지하철역의 최단거리 경로를 찾는다.")
-    @Test
-    void find() {
-        // given
-        Station 서울역 = new Station("서울역");
-        Station 수원역 = new Station("수원역");
-        Station 곰역 = new Station("곰역");
-        Station 수유역 = new Station("수유역");
-        Station 판교역 = new Station("판교역");
-        Station 부산역 = new Station("부산역");
+    private Station 서울역;
+    private Station 수원역;
+    private Station 곰역;
+    private Station 수유역;
+    private Station 판교역;
+    private Station 부산역;
 
-        int 서울역_수원역_거리 = 10;
-        int 수원역_판교역_거리 = 5;
-        int 판교역_부산역_거리 = 1;
-        int 부산역_곰역_거리 = 1;
-        int 수원역_수유역_거리 = 4;
-        int 수유역_곰역_거리 = 4;
+    private int 서울역_수원역_거리 = 10;
+    private int 수원역_판교역_거리 = 5;
+    private int 판교역_부산역_거리 = 1;
+    private int 부산역_곰역_거리 = 1;
+    private int 수원역_수유역_거리 = 4;
+    private int 수유역_곰역_거리 = 4;
 
-        Line 간선 = new Line("간선", "blue");
-        간선.addSections(서울역, 수원역, 서울역_수원역_거리);
-        Line 신분당선 = new Line("신분당선", "blue");
+    private Line 분당선;
+    private Line 신분당선;
+
+    @BeforeEach
+    void setUp() {
+        서울역 = new Station("서울역");
+        수원역 = new Station("수원역");
+        곰역 = new Station("곰역");
+        수유역 = new Station("수유역");
+        판교역 = new Station("판교역");
+        부산역 = new Station("부산역");
+
+        신분당선 = new Line("신분당선", "blue");
         신분당선.addSections(수원역, 판교역, 수원역_판교역_거리);
         신분당선.addSections(판교역, 부산역, 판교역_부산역_거리);
         신분당선.addSections(부산역, 곰역, 부산역_곰역_거리);
-        Line 분당선 = new Line("분당선", "blue");
+
+        분당선 = new Line("분당선", "blue");
         분당선.addSections(수원역, 수유역, 수원역_수유역_거리);
         분당선.addSections(수유역, 곰역, 수유역_곰역_거리);
+    }
+
+    @DisplayName("지하철역의 최단거리 경로를 찾는다.")
+    @Test
+    void 최단거리_경로_찾기() {
+        // given
+        Line 간선 = new Line("간선", "blue");
+        간선.addSections(서울역, 수원역, 서울역_수원역_거리);
 
         // when
         List<Line> lines = Arrays.asList(간선, 신분당선, 분당선);
@@ -52,7 +69,6 @@ public class PathFinderTest {
         //then
         assertThat(shortestPath).containsExactly(서울역, 수원역, 판교역, 부산역, 곰역);
         int 최단경로_거리 = allDistance(서울역_수원역_거리, 수원역_판교역_거리, 판교역_부산역_거리, 부산역_곰역_거리);
-
         assertThat(shortestDistance).isEqualTo(최단경로_거리);
     }
 
@@ -60,77 +76,35 @@ public class PathFinderTest {
     @Test
     void 존재하지_않은_출발역이나_도착역을_조회_할_경우() {
         // given
-        Station 서울역 = new Station("서울역");
-        Station 수원역 = new Station("수원역");
-        Station 곰역 = new Station("곰역");
-        Station 수유역 = new Station("수유역");
-        Station 판교역 = new Station("판교역");
-        Station 부산역 = new Station("부산역");
-
         Station 구로역 = new Station("구로역");
-
-        int 서울역_수원역_거리 = 10;
-        int 수원역_판교역_거리 = 5;
-        int 판교역_부산역_거리 = 1;
-        int 부산역_곰역_거리 = 1;
-        int 수원역_수유역_거리 = 4;
-        int 수유역_곰역_거리 = 4;
-
         Line 간선 = new Line("간선", "blue");
         간선.addSections(서울역, 수원역, 서울역_수원역_거리);
-        Line 신분당선 = new Line("신분당선", "blue");
-        신분당선.addSections(수원역, 판교역, 수원역_판교역_거리);
-        신분당선.addSections(판교역, 부산역, 판교역_부산역_거리);
-        신분당선.addSections(부산역, 곰역, 부산역_곰역_거리);
-        Line 분당선 = new Line("분당선", "blue");
-        분당선.addSections(수원역, 수유역, 수원역_수유역_거리);
-        분당선.addSections(수유역, 곰역, 수유역_곰역_거리);
 
         // when
         List<Line> lines = Arrays.asList(간선, 신분당선, 분당선);
         PathFinder pathFinder = new PathFinder(lines);
 
         //then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> pathFinder.getShortestPath(서울역, 구로역));
-        Assertions.assertThrows(IllegalArgumentException.class, () -> pathFinder.getShortestDistance(서울역, 구로역));
+        Assertions.assertThrows(IllegalPathArgumentException.class, () -> pathFinder.getShortestPath(서울역, 구로역));
+        Assertions.assertThrows(IllegalPathArgumentException.class, () -> pathFinder.getShortestDistance(서울역, 구로역));
     }
 
     @DisplayName("출발역과 도착역이 연결이 되어 있지 않은 경우")
     @Test
     void 출발역과_도착역이_연결이_되어_있지_않은_경우() {
         // given
-        Station 서울역 = new Station("서울역");
-        Station 수원역 = new Station("수원역");
-        Station 곰역 = new Station("곰역");
-        Station 수유역 = new Station("수유역");
-        Station 판교역 = new Station("판교역");
-        Station 부산역 = new Station("부산역");
         Station 구로역 = new Station("구로역");
-
         int 서울역_구로역_거리 = 10;
-        int 수원역_판교역_거리 = 5;
-        int 판교역_부산역_거리 = 1;
-        int 부산역_곰역_거리 = 1;
-        int 수원역_수유역_거리 = 4;
-        int 수유역_곰역_거리 = 4;
-
         Line 간선 = new Line("간선", "blue");
         간선.addSections(서울역, 구로역, 서울역_구로역_거리);
-        Line 신분당선 = new Line("신분당선", "blue");
-        신분당선.addSections(수원역, 판교역, 수원역_판교역_거리);
-        신분당선.addSections(판교역, 부산역, 판교역_부산역_거리);
-        신분당선.addSections(부산역, 곰역, 부산역_곰역_거리);
-        Line 분당선 = new Line("분당선", "blue");
-        분당선.addSections(수원역, 수유역, 수원역_수유역_거리);
-        분당선.addSections(수유역, 곰역, 수유역_곰역_거리);
 
         // when
         List<Line> lines = Arrays.asList(간선, 신분당선, 분당선);
         PathFinder pathFinder = new PathFinder(lines);
 
         //then
-        Assertions.assertThrows(NullPointerException.class, () -> pathFinder.getShortestPath(서울역, 곰역));
-        Assertions.assertThrows(NullPointerException.class, () -> pathFinder.getShortestDistance(서울역, 곰역));
+        Assertions.assertThrows(IllegalPathArgumentException.class, () -> pathFinder.getShortestPath(서울역, 곰역));
+        Assertions.assertThrows(IllegalPathArgumentException.class, () -> pathFinder.getShortestDistance(서울역, 곰역));
     }
 
     private int allDistance(int... distances) {
