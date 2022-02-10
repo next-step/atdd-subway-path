d93bc174891af124defd1cfe59480f930f03a863
minjoon_lee <minjoon1995@naver.com>
2022-02-10 20:13:23 +0900

    refactorign : 안쓰는 필드 제거

--- src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
@@ -7,21 +7,16 @@ import java.time.LocalDateTime;
 public class StationResponse {
     private final Long id;
     private final String name;
-    private final LocalDateTime createdDate;
-    private final LocalDateTime modifiedDate;
 
-    public StationResponse(Long id, String name, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+
+    public StationResponse(Long id, String name) {
         this.id = id;
         this.name = name;
-        this.createdDate = createdDate;
-        this.modifiedDate = modifiedDate;
     }
 
     public StationResponse(Station station) {
         this.id = station.getId();
         this.name = station.getName();
-        this.createdDate = station.getCreatedDate();
-        this.modifiedDate = station.getModifiedDate();
     }
 
     public Long getId() {
@@ -32,11 +27,4 @@ public class StationResponse {
         return name;
     }
 
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public LocalDateTime getModifiedDate() {
-        return modifiedDate;
-    }
 }
