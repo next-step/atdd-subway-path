f404612f8c1b5632f0b865486019dd3248003ee7
minjoon_lee <minjoon1995@naver.com>
2022-02-10 22:34:56 +0900

    fix : response에 다시 create time , modify time 추가

--- src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
@@ -7,16 +7,21 @@ import java.time.LocalDateTime;
 public class StationResponse {
     private final Long id;
     private final String name;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
 
-
-    public StationResponse(Long id, String name) {
+    public StationResponse(Long id, String name, LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
     }
 
     public StationResponse(Station station) {
         this.id = station.getId();
         this.name = station.getName();
+        this.createdDate = station.getCreatedDate();
+        this.modifiedDate = station.getModifiedDate();
     }
 
     public Long getId() {
@@ -27,4 +32,16 @@ public class StationResponse {
         return name;
     }
 
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    @Override
+    public String toString() {
+        return "StationResponse{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", createdDate=" + createdDate +
+                '}';
+    }
 }
