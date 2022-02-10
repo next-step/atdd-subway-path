8293560d492969623048d705d975cca8b42559d0
minjoon_lee <minjoon1995@naver.com>
2022-02-10 20:12:21 +0900

    fix : req body로 사용하기 위해 기본 생성자 추가 및 final 제거

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -1,11 +1,18 @@
 package nextstep.subway.applicaion.dto;
 
+
+import nextstep.subway.domain.Line;
+
 public class LineRequest {
-    private final String name;
-    private final String color;
-    private final Long downStationId;
-    private final Long upStationId;
-    private final int distance;
+    private String name;
+    private String color;
+    private Long downStationId;
+    private Long upStationId;
+    private int distance;
+
+    protected LineRequest() {
+    }
+
 
     public LineRequest(String name, String color, Long downStationId, Long upStationId, int distance) {
         this.name = name;
