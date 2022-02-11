8d0db47cfe6a66dca026b6dc89105e6964f878d4
mintheon <mintheon@mintheon.com>
2022-02-11 22:20:35 +0900

    test: 지하철 노선 내 존재하지 않는 구간 삭제 테스트 추가

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -22,8 +22,9 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     private Long 강남역;
     private Long 양재역;
     private Long 정자역;
-    final private int 거리 = 50;
     private Long 합정역;
+
+    final private int 거리 = 50;
 
     /**
      * Given 지하철역과 노선 생성을 요청 하고
@@ -224,6 +225,25 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역);
+    }
+
+    /**
+     * Given 지하철 노선에 새로운 구간 추가를 요청 하고
+     * When 지하철 노선에 존재하지 않는 구간 제거를 요청 하면
+     * Then 노선의 구간 제거가 실패한다.
+     */
+    @DisplayName("지하철 노선의 존재하지 않는 구간을 제거")
+    @Test
+    void removeNotExistStation() {
+        // given
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역, 거리));
+
+        // when
+        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_제거_요청(신분당선, 합정역);
+
+        // then
+        지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
