315be42f607f923e3eee1a96d0efbbf5ebf95fd4
JoyDaheeCha <daheecha0612@gmail.com>
2022-02-13 03:14:59 +0900

    modify: LineService 내 모킹 메서드 추가

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,20 +1,25 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.SectionService;
 import nextstep.subway.applicaion.StationService;
 import nextstep.subway.applicaion.dto.LineResponse;
 import nextstep.subway.applicaion.dto.SectionRequest;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.internal.matchers.Any;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,15 +29,20 @@ class LineServiceMockTest {
     @Mock
     private StationService stationService;
 
+    @Mock
+    private SectionService sectionService;
+
     @Test
     void addSection() {
         // given
-        when(lineRepository.findById(1L)).thenReturn(Optional.of(new Line("2호선", "green")));
+        Line line = new Line("2호선", "green");
+        when(lineRepository.findById(1L)).thenReturn(Optional.of(line));
 
-        when(stationService.findById(1L)).thenReturn(new Station("강남역"));
-        when(stationService.findById(2L)).thenReturn(new Station("역삼역"));
+        Station 강남역 = new Station("강남역");
+        Station 역삼역 = new Station("역삼역");
+        when(sectionService.createSection(any(Line.class), any(SectionRequest.class))).thenReturn(new Section(line, 강남역, 역삼역, 3));
 
-        LineService lineService = new LineService(lineRepository, stationService);
+        LineService lineService = new LineService(lineRepository, stationService, sectionService);
 
         // when
         lineService.addSection(1L, new SectionRequest(1L,2L,3));
