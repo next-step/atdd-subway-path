c1fc24d79c30b3ad49d1be1935d2daedcf893e2f
JoyDaheeCha <daheecha0612@gmail.com>
2022-02-13 00:37:23 +0900

    test: LineServiceMockTest 추가

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Step 0 단위 테스트 실습
 - [ ] 테스트 클래스의 테스트 메서드 완성하기 
-    - [ ] LineServiceMockTest 
+    - [X] LineServiceMockTest 
     - [ ] LineServiceTest
 - [ ] Line Service 비즈니스 로직 리팩터링
     - [ ] 비즈니스 로직을 도메인 클래스(Line)으로 옮기기
--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,14 +1,24 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.LineResponse;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
-public class LineServiceMockTest {
+class LineServiceMockTest {
     @Mock
     private LineRepository lineRepository;
     @Mock
@@ -17,12 +27,19 @@ public class LineServiceMockTest {
     @Test
     void addSection() {
         // given
-        // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        when(lineRepository.findById(1L)).thenReturn(Optional.of(new Line("2호선", "green")));
+
+        when(stationService.findById(1L)).thenReturn(new Station("강남역"));
+        when(stationService.findById(2L)).thenReturn(new Station("역삼역"));
+
+        LineService lineService = new LineService(lineRepository, stationService);
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(1L, new SectionRequest(1L,2L,3));
 
         // then
-        // line.findLineById 메서드를 통해 검증
+        LineResponse lineResponse = lineService.findById(1L);
+        assertThat(lineResponse.getStations()).hasSize(2);
+
     }
 }
