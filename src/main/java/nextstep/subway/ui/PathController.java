c82f28201ee5d657ec1583fb65f778e317fcffca
minjoon_lee <minjoon1995@naver.com>
2022-02-13 15:30:45 +0900

    add : pathcontroller add

--- src/main/java/nextstep/subway/ui/PathController.java
+++ src/main/java/nextstep/subway/ui/PathController.java
@@ -1,5 +1,6 @@
 package nextstep.subway.ui;
 
+import nextstep.subway.applicaion.GraphService;
 import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.dto.PathResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PathController {
     @Autowired
-    private LineService lineService;
+    private GraphService graphService;
 
     @GetMapping("/paths")
     public ResponseEntity<PathResponse> path(@RequestParam Long source, @RequestParam Long target) {
-        return ResponseEntity.ok().body(lineService.getShortestPath(source, target));
+        return ResponseEntity.ok().body(graphService.getShortestPath(source, target));
     }
 }
