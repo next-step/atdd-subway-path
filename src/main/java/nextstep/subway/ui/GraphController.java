b783b15c027a5ace256d77754dfb0d975cb0f0d4
minjoon_lee <minjoon1995@naver.com>
2022-02-13 16:25:42 +0900

    refactoring : pathController 이름 변경

--- src/main/java/nextstep/subway/ui/GraphController.java
+++ src/main/java/nextstep/subway/ui/GraphController.java
@@ -1,7 +1,6 @@
 package nextstep.subway.ui;
 
 import nextstep.subway.applicaion.GraphService;
-import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.dto.PathResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PathController {
+public class GraphController {
     @Autowired
     private GraphService graphService;
 
