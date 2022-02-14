5ab9e66be19fd1c91c2ff2faa5d30b49c7f68297
povia <47127607+povia@users.noreply.github.com>
2022-02-15 00:19:56 +0900

    step3 test: 서비스 테스트 코드 추가

---
+++ src/main/java/nextstep/subway/applicaion/PathQueryService.java
@@ -0,0 +1,10 @@
+package nextstep.subway.applicaion;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class PathQueryService {
+  
+}
---
+++ src/test/java/nextstep/subway/unit/PathQueryServiceMockTest.java
@@ -0,0 +1,30 @@
+package nextstep.subway.unit;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PathQueryServiceMockTest {
+  @Test
+  @DisplayName("두 역 조회 시 성공")
+  void successfulTest() {
+
+  }
+
+  @Test
+  @DisplayName("연결되지 않은 두 역 조회 시 실패")
+  void failedNotLinkedStationsTest() {
+
+  }
+
+  @Test
+  @DisplayName("구간에 존재하지 않는 두 역 조회 시 실패")
+  void notRegisteredStationsTest() {
+
+  }
+
+  @Test
+  @DisplayName("동일한 역으로 조회 시 실패")
+  void sameStationsTest() {
+
+  }
+}
