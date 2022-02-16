18fd78de554a63abf9a2924c2e03ce712cb15da5
povia <47127607+povia@users.noreply.github.com>
2022-02-16 09:00:25 +0900

    step3 test: 테스트에 트랜잭셔널 추가

--- src/test/java/nextstep/subway/unit/PathServiceTest.java
+++ src/test/java/nextstep/subway/unit/PathServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Collectors;
 
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
+@Transactional
 class PathServiceTest {
 
   @Autowired
