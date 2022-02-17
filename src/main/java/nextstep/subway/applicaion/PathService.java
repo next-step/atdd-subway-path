593f245f9b66577f94d53cd2c68a007b45ef1341
soosue <yunsoo0510@daum.net>
2022-02-17 13:48:37 +0900

    [Feat] PathController 작성

---
+++ src/main/java/nextstep/subway/applicaion/PathService.java
@@ -0,0 +1,11 @@
+package nextstep.subway.applicaion;
+
+import nextstep.subway.applicaion.dto.PathResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PathService {
+    public PathResponse findPath(Long source, Long target) {
+        return null;
+    }
+}
