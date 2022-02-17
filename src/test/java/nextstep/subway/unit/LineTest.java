25ac58e7910a9e8c09838f7009920da103634e17
minyul <ggomjae@gmail.com>
2022-02-17 11:52:09 +0900

    refactor : BDD 패턴으로 수정

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -28,26 +28,23 @@ class LineTest {
     @DisplayName("구간 목록 마지막에 새로운 구간을 추가할 경우")
     @Test
     void addSection() {
+        // given
         final int newDistance = 5;
         final Station newDownStation = new Station("성균관대역");
+
+        // when
         line.addSection(downStation, newDownStation, newDistance);
 
+        // then
         assertThat(line.getSections().size()).isEqualTo(2);
     }
 
     @DisplayName("노선에 속해있는 역 목록 조회")
     @Test
     void getStations() {
-        // given
-        HashSet<Station> stations = new HashSet<>();
-        for (Section section : line.getSections()) {
-            stations.add(section.getUpStation());
-            stations.add(section.getDownStation());
-        }
 
         // then
-        assertThat(line.getSections().size()).isEqualTo(1);
-        assertThat(stations).containsExactly(upStation, downStation);
+        assertThat(line.getStations()).hasSize(2).containsExactly(upStation, downStation);
     }
 
     @DisplayName("구간이 목록에서 마지막 역 삭제")
