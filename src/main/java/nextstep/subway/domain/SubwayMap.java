3c190b203651cb4920e74528c65e84fadc803851
hiro032 <kiryuinh32@gmail.com>
2022-02-19 13:05:11 +0900

    refactor: 경로 조회에 필요한 인자 유효성 검증 로직 위치 변경
    PathService -> SubwayMap#validStations

--- src/main/java/nextstep/subway/domain/SubwayMap.java
+++ src/main/java/nextstep/subway/domain/SubwayMap.java
@@ -2,6 +2,7 @@ package nextstep.subway.domain;
 
 import java.util.List;
 import nextstep.subway.exception.SourceAndTargetNotConnectedException;
+import nextstep.subway.exception.SourceAndTargetSameException;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
@@ -23,12 +24,18 @@ public class SubwayMap {
 
 
     public List<Station> getShortestPath(Station source, Station target) {
+        validStations(source, target);
         try {
             return dijkstraShortestPath.getPath(source, target).getVertexList();
         } catch (NullPointerException e) {
             throw new SourceAndTargetNotConnectedException();
         }
+    }
 
+    private void validStations(Station source, Station target) {
+        if (source.equals(target)) {
+            throw new SourceAndTargetSameException();
+        }
     }
 
     public double getShortestDistance(Station source, Station target) {
