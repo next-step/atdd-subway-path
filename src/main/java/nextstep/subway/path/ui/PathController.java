91771fe9948b673b9c965977570690e781d3ea03
Pyro <rhwjddhks@gmail.com>
2022-02-22 19:29:34 +0900

    feat: PathController 구현

---
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -0,0 +1,25 @@
+package nextstep.subway.path.ui;
+
+import nextstep.subway.path.application.PathService;
+import nextstep.subway.path.dto.PathResponse;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/paths")
+public class PathController {
+    private final PathService pathService;
+
+    public PathController(PathService pathService) {
+        this.pathService = pathService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PathResponse> findShortestPath(@RequestParam("source") Long source, @RequestParam("target") Long target) {
+        return ResponseEntity.ok(pathService.findShortestPath(source, target));
+    }
+}
