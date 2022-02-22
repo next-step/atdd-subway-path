7a60f5d6fcc66ee3f955f57e756e1ceffbafb5be
sang5c <skah321@gmail.com>
2022-02-23 00:36:45 +0900

    feat: 존재하지 않는 역을 포함한 경로 조회시 에러 기능 구현

--- src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
+++ src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package nextstep.subway.ui;
 import nextstep.subway.exception.AddSectionFailException;
 import nextstep.subway.exception.FindPathFailException;
 import nextstep.subway.exception.RemoveSectionFailException;
+import nextstep.subway.exception.StationNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,7 +15,8 @@ public class ControllerExceptionHandler {
             DataIntegrityViolationException.class,
             AddSectionFailException.class,
             RemoveSectionFailException.class,
-            FindPathFailException.class
+            FindPathFailException.class,
+            StationNotFoundException.class
     })
     public ResponseEntity<Void> handleIllegalArgsException(Exception e) {
         return ResponseEntity.badRequest().build();
