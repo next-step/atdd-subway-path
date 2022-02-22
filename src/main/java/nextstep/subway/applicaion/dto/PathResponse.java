8c8306d581ee317d0a861b2764f8cfd637a35297
soo <yunsoo0510@daum.net>
2022-02-22 17:04:37 +0900

    [Feat] PathService.findPath() 구현

--- src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
@@ -1,4 +1,32 @@
 package nextstep.subway.applicaion.dto;
 
+import nextstep.subway.domain.Path;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PathResponse {
+    private List<StationResponse> stations;
+    private int distance;
+
+    public PathResponse(List<StationResponse> stations, int distance) {
+        this.stations = stations;
+        this.distance = distance;
+    }
+
+    public static PathResponse from(Path path) {
+        return new PathResponse(
+                path.stations().stream()
+                        .map(StationResponse::from)
+                        .collect(Collectors.toList()),
+                path.totalDistance());
+    }
+
+    public List<StationResponse> getStations() {
+        return stations;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
 }
