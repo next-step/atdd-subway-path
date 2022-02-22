8bfa0a91eac49a963a399d6ad68171d639581faa
sang5c <skah321@gmail.com>
2022-02-22 23:51:32 +0900

    refactor: 테스트 픽스처 세팅 분리

--- src/test/java/nextstep/subway/unit/PathFinderTest.java
+++ src/test/java/nextstep/subway/unit/PathFinderTest.java
@@ -1,6 +1,7 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.domain.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,25 +13,42 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("최단 경로 조회")
 public class PathFinderTest {
 
+    private Station 강남역;
+    private Station 군자역;
+    private Station 천호역;
+    private Line 신분당선;
+    private Line 구분당선;
+    private Line 오호선;
+    private List<Line> lines;
+    private int 강남_군자 = 5;
+    private int 강남_천호 = 10;
+    private int 군자_천호 = 3;
+
     /**
      * 강남 -> 천호 = 10
      * 강남 -> 군자 -> 천호 = 8 (최단거리)
      */
+    @BeforeEach
+    void setup() {
+        // given
+        강남역 = new Station(1L, "강남역");
+        군자역 = new Station(2L, "군자역");
+        천호역 = new Station(3L, "천호역");
+
+        신분당선 = new Line("신분당선", "빨간색");
+        신분당선.addSection(Section.of(신분당선, 강남역, 군자역, 강남_군자));
+        구분당선 = new Line("구분당선", "파란색");
+        구분당선.addSection(Section.of(구분당선, 강남역, 천호역, 강남_천호));
+        오호선 = new Line("오호선", "보라색");
+        오호선.addSection(Section.of(오호선, 군자역, 천호역, 군자_천호));
+
+        lines = Arrays.asList(신분당선, 구분당선, 오호선);
+    }
+
     @DisplayName("lines를 인자로 받아 최단 경로 역 목록과 거리의 합을 반환한다")
     @Test
     void shortestPath() {
         // given
-        Station 강남역 = new Station(1L, "강남역");
-        Station 군자역 = new Station(2L, "군자역");
-        Station 천호역 = new Station(3L, "천호역");
-        Line 신분당선 = new Line("신분당선", "빨간색");
-        신분당선.addSection(Section.of(신분당선, 강남역, 군자역, 5));
-        Line 구분당선 = new Line("구분당선", "파란색");
-        구분당선.addSection(Section.of(구분당선, 강남역, 천호역, 10));
-        Line 오호선 = new Line("오호선", "보라색");
-        오호선.addSection(Section.of(오호선, 군자역, 천호역, 3));
-        List<Line> lines = Arrays.asList(신분당선, 구분당선, 오호선);
-
         PathFinder pathFinder = new PathFinder(lines);
 
         // when
