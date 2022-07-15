e79635460f7eb9f7beda0fae623d4e4972d516e6
임명섭 <lms@elonsoft.co.kr>
2022-07-15 11:20:36 +0900

    test 지하철 노선에 등록된 지하철역 조회

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -5,6 +5,7 @@ import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static nextstep.subway.unit.LineTest.Stub.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 구간 관련")
@@ -14,8 +15,6 @@ class LineTest {
     @Test
     void addSection() {
         // given
-        Station 구로디지털단지역 = new Station("구로디지털단지역");
-        Station 신대방역 = new Station("신대방역");
         Line line = new Line("2호선", "green");
 
         // when
@@ -25,11 +24,27 @@ class LineTest {
         assertThat(line.getSections()).hasSize(1);
     }
 
+    @DisplayName("지하철 노선에 등록된 지하철역 조회")
     @Test
     void getStations() {
+        // given
+        Line line = new Line("2호선", "green");
+
+        // when
+        line.addSection(구로디지털단지역, 신대방역, 10);
+        line.addSection(신대방역, 신림역, 8);
+
+        // then
+        assertThat(line.getStations()).contains(구로디지털단지역, 신대방역, 신림역);
     }
 
     @Test
     void removeSection() {
+    }
+
+    protected static class Stub {
+        public static final Station 구로디지털단지역 = new Station("구로디지털단지역");
+        public static final Station 신대방역 = new Station("신대방역");
+        public static final Station 신림역 = new Station("신림역");
     }
 }
