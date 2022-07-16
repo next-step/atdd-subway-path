7d8e185a573c2f5c48cb3a1d7ab01a695817b765
MangKyu <whalsrb1226@gmail.com>
2022-07-16 18:00:56 +0900

    역 사이에 새로운 구간을 추가하는 단위 테스트 추가

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,6 +1,7 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 
@@ -8,9 +9,102 @@ import java.util.List;
 
 import static nextstep.subway.utils.StationTestSources.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LineTest {
+
+    @Test
+    void addSection_BetweenDownStation() {
+        // given
+        final Line line = new Line();
+        final Station station1 = station(1);
+        final Station station2 = station(2);
+        final Station station3 = station(3);
+        final Station station4 = station(4);
+
+        line.addSection(station1, station3, 10);
+        line.addSection(station3, station4, 10);
+
+        // when
+        line.addSection(station2, station3, 4);
+
+        // then
+        final List<Section> sections = line.getSections();
+        assertSections(station1, station2, 6, sections.get(0));
+        assertSections(station2, station3, 4, sections.get(1));
+    }
+
+    @Test
+    void addSection_BetweenDownStationOverLength() {
+        // given
+        final Line line = new Line();
+        final Station station1 = station(1);
+        final Station station2 = station(2);
+        final Station station3 = station(3);
+        final Station station4 = station(4);
+
+        line.addSection(station1, station3, 10);
+        line.addSection(station3, station4, 10);
+
+        // when
+        final IllegalArgumentException result = assertThrows(
+                IllegalArgumentException.class,
+                () -> line.addSection(station2, station3, 100));
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(line.getSections()).hasSize(2);
+    }
+
+    @Test
+    void addSection_BetweenUpStation() {
+        // given
+        final Line line = new Line();
+        final Station station1 = station(1);
+        final Station station2 = station(2);
+        final Station station3 = station(3);
+        final Station station4 = station(4);
+
+        line.addSection(station1, station2, 10);
+        line.addSection(station2, station4, 10);
+
+        // when
+        line.addSection(station2, station3, 4);
+
+        // then
+        final List<Section> sections = line.getSections();
+
+        assertSections(station2, station3, 4, sections.get(1));
+        assertSections(station3, station4, 6, sections.get(2));
+    }
+
+    @Test
+    void addSection_BetweenUpStationOverLength() {
+        // given
+        final Line line = new Line();
+        final Station station1 = station(1);
+        final Station station2 = station(2);
+        final Station station3 = station(3);
+        final Station station4 = station(4);
+
+        line.addSection(station1, station2, 10);
+        line.addSection(station2, station4, 10);
+
+        // when
+        final IllegalArgumentException result = assertThrows(
+                IllegalArgumentException.class,
+                () -> line.addSection(station2, station3, 100));
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(line.getSections()).hasSize(2);
+    }
+
+    private void assertSections(final Station upStation, final Station downStation, final int distance, final Section section) {
+        assertThat(section.getUpStation()).isEqualTo(upStation);
+        assertThat(section.getDownStation()).isEqualTo(downStation);
+        assertThat(section.getDistance()).isEqualTo(distance);
+    }
 
     @Test
     void addSection_FirstUpStation() {
@@ -24,7 +118,7 @@ class LineTest {
 
         // when
         line.addSection(station1, station2, 10);
-        
+
         // then
         assertThat(line.getStations()).containsExactly(station1, station2, station3);
     }
