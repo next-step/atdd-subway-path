bddccb248340cee32cda7555cf553c071c80dd54
jiwoo <zbqmgldjfh@gmail.com>
2022-07-16 10:22:11 +0900

    test(LineServiceMockTest): Stub을 활용한 LineService 테스트 작성 완료

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 
 ## TODO List 
 - [x] 구간 단위 테스트 작성 (LineTest)
-- [ ] Mock을 사용한 구간 서비스 단위 테스트 작성 (LineServiceMockTest)
+- [x] Stub을 사용한 구간 서비스 단위 테스트 작성 (LineServiceMockTest)
 - [ ] Mock 없이 구간 서비스 단위 테스트 (LineServiceTest)
 - [ ] 단위 테스트를 기반으로 비즈니스 로직을 리팩터링 하기
--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,16 +1,32 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
+
+    @InjectMocks
+    private LineService lineService;
+
     @Mock
     private LineRepository lineRepository;
+
     @Mock
     private StationService stationService;
 
@@ -18,11 +34,23 @@ public class LineServiceMockTest {
     void addSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        Long lineId = 3L;
+        Long upStationId = 1L;
+        Long downStationId = 2L;
+
+        given(stationService.findById(upStationId)).willReturn(new Station("강남역"));
+        given(stationService.findById(downStationId)).willReturn(new Station("건대입구역"));
+        given(lineRepository.findById(anyLong())).willReturn(Optional.of(new Line("2호선", "green")));
+
+        SectionRequest request = new SectionRequest(upStationId, downStationId, 10);
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(lineId, request);
 
         // then
         // line.findLineById 메서드를 통해 검증
+        Line findLine = lineRepository.findById(lineId).get();
+        then(findLine.getSections()).hasSize(1);
     }
 }
