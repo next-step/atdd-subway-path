4f8be7144493bdc9169758ff3078286a13bae735
jiwoo <zbqmgldjfh@gmail.com>
2022-07-16 10:33:21 +0900

    test(LineServiceTest): Stub없이 LineService 테스트 작성 완료

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,17 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.BDDAssertions.then;
 
 @SpringBootTest
 @Transactional
@@ -23,11 +28,19 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Line savedLine = lineRepository.save(new Line("2호선", "green"));
+        Station savedUpStation = stationRepository.save(new Station("강남역"));
+        Station savedDownStation = stationRepository.save(new Station("건대입구역"));
+        SectionRequest request = new SectionRequest(savedUpStation.getId(), savedDownStation.getId(), 10);
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(savedLine.getId(), request);
 
         // then
         // line.getSections 메서드를 통해 검증
+        Line findLine = lineRepository.findById(savedLine.getId()).get();
+        then(findLine.getSections()).hasSize(1);
+        then(findLine.getStations()).extracting("name").containsExactly("강남역", "건대입구역");
     }
 }
