284ea4f9d0f3c8ecf4a66a88317d552f1e6fb715
jiwoo <zbqmgldjfh@gmail.com>
2022-07-16 11:34:34 +0900

    test(LineServiceMockTest): Section을 삭제하는 테스트 추가

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -18,6 +18,12 @@ public class Line {
     public Line() {
     }
 
+    public Line(Long id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
     public Line(String name, String color) {
         this.name = name;
         this.color = color;
--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -5,7 +5,9 @@ import nextstep.subway.applicaion.StationService;
 import nextstep.subway.applicaion.dto.SectionRequest;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,7 +16,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
@@ -52,5 +56,45 @@ public class LineServiceMockTest {
         // line.findLineById 메서드를 통해 검증
         Line findLine = lineRepository.findById(lineId).get();
         then(findLine.getSections()).hasSize(1);
+    }
+
+    @DisplayName("구간이 하나 있는 line에 구간삭제 요청시 성공한다")
+    @Test
+    public void delete_section_test() {
+        // given
+        // 역과 라인을 생성하고, 라인에 구간을 등록시킨다
+        Station upStation = new Station(1L, "강남역");
+        Station downStation = new Station(2L, "건대입구역");
+        Line line = new Line(3L, "2호선", "green");
+        line.addSection(new Section(line, upStation, downStation, 10));
+
+        given(stationService.findById(anyLong())).willReturn(downStation);
+        given(lineRepository.findById(anyLong())).willReturn(Optional.of(line));
+
+        // when
+        // 라인에서 구간을 삭제하면
+        lineService.deleteSection(line.getId(), downStation.getId());
+
+        // then
+        // 라인의 구간은 비어있게 된다
+        Line findLine = lineRepository.findById(line.getId()).get();
+        then(findLine.getSections()).isEmpty();
+    }
+
+    @DisplayName("구간이 없는 line에 구간삭제 요청시 예외가 발생한다")
+    @Test
+    public void delete_section_when_no_section() {
+        // given
+        Station downStation = new Station(2L, "건대입구역");
+        Line line = new Line(3L, "2호선", "green");
+
+        given(stationService.findById(anyLong())).willReturn(downStation);
+        given(lineRepository.findById(anyLong())).willReturn(Optional.of(line));
+
+        // when
+        IndexOutOfBoundsException exception = assertThrows(IndexOutOfBoundsException.class, () -> lineService.deleteSection(line.getId(), downStation.getId()));
+
+        // then
+        assertThat(exception).isInstanceOf(IndexOutOfBoundsException.class);
     }
 }
