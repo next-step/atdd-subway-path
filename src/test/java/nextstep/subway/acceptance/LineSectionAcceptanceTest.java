8cd6be7662815da41678a5688c03479f8c07787b
MangKyu <whalsrb1226@gmail.com>
2022-07-16 17:05:45 +0900

    상행역과 하행역이 모두 등록되지 않은 구간을 새로운 노선으로 추가하는 인수 테스트 추가

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -72,7 +72,6 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(정자역, 강남역, 양재역);
     }
 
-
     /**
      * Given 상행역과 하행역이 모두 등록된 구간에 대해
      * When 지하철 노선에 새로운 구간 추가를 요청 하면
@@ -83,6 +82,26 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     void addLineSection_DuplicatedUpStationAndDownStation() {
         // when
         지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 양재역));
+
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역);
+    }
+
+    /**
+     * Given 상행역과 하행역이 모두 등록되지 않은 구간에 대해
+     * When 지하철 노선에 새로운 구간 추가를 요청 하면
+     * Then 노선에 새로운 구간이 추가되지 않는다
+     */
+    @DisplayName("지하철 노선에 상행역과 하행역이 모두 등록되어 있는 구간을 등록")
+    @Test
+    void addLineSection_NotRegisteredUpStationAndDownStation() {
+        // when
+        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+        Long 판교역 = 지하철역_생성_요청("판교역").jsonPath().getLong("id");
+
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(정자역, 판교역));
 
         // then
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
