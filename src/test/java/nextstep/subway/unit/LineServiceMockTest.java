39b867c1650f6cf80254a15e7514a114c77e72c2
jiwoo <zbqmgldjfh@gmail.com>
2022-07-16 12:28:14 +0900

    test(LineServiceMockTest): 하행구간역이 동일하지 않는 경우에 삭제를 요청하는 테스트 작성

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -113,6 +113,27 @@ public class LineServiceMockTest {
         then(exception).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("하행구간역이 동일하지 않는 경우에 삭제를 요청하면 예외가 발생한다")
+    @Test
+    public void delete_section_when_not_matched_down_station() {
+        // given
+        // 역과 라인을 생성하고, 라인에 구간을 등록시킨다
+        Station upStation = new Station(1L, "강남역");
+        Station downStation = new Station(2L, "건대입구역");
+        Station anotherStation = new Station(4L, "성수역");
+        Line line = new Line(3L, "2호선", "green");
+        line.addSection(new Section(line, upStation, downStation, 10));
+
+        given(stationService.findById(anyLong())).willReturn(anotherStation);
+        given(lineRepository.findById(anyLong())).willReturn(Optional.of(line));
+
+        // when
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> lineService.deleteSection(line.getId(), downStation.getId()));
+
+        // then
+        then(exception).isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("존재하는 라인의 색과 이름을 변경할 수 있다")
     @Test
     public void line_update_success_test() {
