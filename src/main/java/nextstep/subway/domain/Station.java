befddd2b114c75bfee4f175581d68f52e7a7d2e2
lsj8367 <lsj8367@naver.com>
2022-07-17 22:05:35 +0900

    :sparkles: equals, hashCode 추가

--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -1,5 +1,6 @@
 package nextstep.subway.domain;
 
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -26,4 +27,22 @@ public class Station {
     public String getName() {
         return name;
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Station station = (Station) o;
+        return Objects.equals(id, station.id) && Objects.equals(name, station.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
 }
