b8ef1c63e25441614affc19ecd0506e48c5154e7
임명섭 <lms@elonsoft.co.kr>
2022-07-17 17:15:26 +0900

    test 새로운 상행 종점역 추가 테스트

--- src/test/java/nextstep/subway/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/domain/SectionsTest.java
@@ -21,7 +21,22 @@ class SectionsTest {
         assertThat(sections.getStations()).contains(Stub.구로디지털단지역, Stub.신대방역, Stub.신림역);
     }
 
+    @DisplayName("새로운 상행 종점역 추가")
+    @Test
+    void addNewUpStation() {
+        // given
+        Sections sections = new Sections();
+        sections.add(new Section(Stub.구로디지털단지역, Stub.신대방역, 10));
+
+        // when
+        sections.add(new Section(Stub.대림역, Stub.구로디지털단지역, 4));
+
+        // then
+        assertThat(sections.getStations()).contains(Stub.대림역, Stub.구로디지털단지역, Stub.신대방역);
+    }
+
     private static class Stub {
+        public static final Station 대림역 = new Station("대림역");
         public static final Station 구로디지털단지역 = new Station("구로디지털단지역");
         public static final Station 신대방역 = new Station("신대방역");
         public static final Station 신림역 = new Station("신림역");
