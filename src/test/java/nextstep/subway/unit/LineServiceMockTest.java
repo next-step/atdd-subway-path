adeab2b0a44aca56287b8954b7cdeecd28283b3e
wlroh <wlroh.paul@gmail.com>
2022-07-17 15:23:32 +0900

    test: LineService addSection 메서드를 Mockito 기반으로 단위테스트 추가

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,11 +1,22 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
@@ -13,16 +24,29 @@ public class LineServiceMockTest {
     private LineRepository lineRepository;
     @Mock
     private StationService stationService;
+    @InjectMocks
+    private LineService lineService;
 
     @Test
     void addSection() {
         // given
-        // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        final long lineId = 1L;
+        final long upStationId = 1L;
+        final long downStationId = 2L;
+
+        Line line = new Line("2호선", "bg-green-600");
+        doReturn(Optional.of(line)).when(lineRepository).findById(lineId);
+
+        Station gangnam = new Station("강남역");
+        Station yeoksam = new Station("역삼역");
+        doReturn(gangnam).when(stationService).findById(upStationId);
+        doReturn(yeoksam).when(stationService).findById(downStationId);
 
         // when
-        // lineService.addSection 호출
+        SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, 10);
+        lineService.addSection(lineId, sectionRequest);
 
         // then
-        // line.findLineById 메서드를 통해 검증
+        assertThat(line.getSections()).containsOnly(new Section(line, gangnam, yeoksam, 10));
     }
 }
