a45b43b4c6e8f9460d3fa03b95b7b5a31dcb726e
Oh yong Soo <dhdydtn2019@gmail.com>
2022-07-18 01:55:13 +0900

    test: 지하철 구간 생성 테스트 추가 1. 지하철 구간 정상 생성 2. 지하철 구간 추가시 노선을 찾지 못하면 예외발생

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,8 +1,16 @@
 package nextstep.subway.unit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,15 +27,35 @@ public class LineServiceTest {
     @Autowired
     private LineService lineService;
 
+    @DisplayName("지하철 구간 생성")
     @Test
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 강남역 = stationRepository.save(new Station("강남역"));
+        Station 역삼역 = stationRepository.save(new Station("역삼역"));
+        Line 신분당선 = lineRepository.save(new Line("신분당선", "yellow"));
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(신분당선.getId(), new SectionRequest(강남역.getId(), 역삼역.getId(), 10));
 
         // then
         // line.getSections 메서드를 통해 검증
+        Section section = 신분당선.getSections().get(0);
+        assertThat(section.getLine()).isEqualTo(신분당선);
+        assertThat(section.getStations()).containsExactly(강남역,역삼역);
+    }
+
+    @DisplayName("지하철 구간 추가시 노선을 찾지 못하면 예외발생")
+    @Test
+    void addSectionException() {
+        //given
+        Station 강남역 = stationRepository.save(new Station("강남역"));
+        Station 역삼역 = stationRepository.save(new Station("역삼역"));
+
+        // when //then
+        assertThatThrownBy(() -> lineService.addSection(1L, new SectionRequest(강남역.getId(), 역삼역.getId(), 10)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
