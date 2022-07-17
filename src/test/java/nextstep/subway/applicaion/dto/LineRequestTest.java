2e4c30af64eac919c7a7e25a276735ba9b768bfe
giyeon95 <giyeon15@gmail.com>
2022-07-18 01:04:48 +0900

    test: 단위 테스트 검증 방식 @ParameterizedTest 로 변경
    ```
    단위 테스트 1개가 여러 번 검증하고 있는것 같아요.
    이런 케이스는 @ParameerizedTest를 활용해보시는건 어떨까요?
    
    _Originally posted by @starkim06 in https://github.com/next-step/atdd-subway-path/pull/291#discussion_r922755791_
    ```

--- src/test/java/nextstep/subway/applicaion/dto/LineRequestTest.java
+++ src/test/java/nextstep/subway/applicaion/dto/LineRequestTest.java
@@ -1,34 +1,22 @@
 package nextstep.subway.applicaion.dto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineRequestTest {
 
-    @Test
-    @DisplayName("upStationId, downStationId 값이 존재하고 distance 값이 0이 아니면 Section을 만들도록 True를 반환한다.")
-    void isCreateSectionTest() {
+    @ParameterizedTest(name = "upStationId, downStationId 값이 존재하고 distance 값이 0이 아니면 Section을 만들도록 True를 반환한다.")
+    @CsvSource({"1,2,10"})
+    void isCreateSectionTest(Long upStationId, Long downStationId, Integer distance) {
         assertThat(new LineRequest("4호선", "blue").isCreateSection()).isFalse();
-        assertThat(new LineRequest("4호선", "blue", 1L, 2L, 10).isCreateSection()).isTrue();
+        assertThat(new LineRequest("4호선", "blue", upStationId, downStationId, distance).isCreateSection()).isTrue();
     }
 
-    @Test
-    @DisplayName("upStationId가 없으면 Section을 만들지 않도록 false를 반환한다.")
-    void isCreateSectionWithoutUpStationIdTest() {
-        assertThat(new LineRequest("4호선", "blue", null, 2L, 10).isCreateSection()).isFalse();
-    }
-
-    @Test
-    @DisplayName("downStationId가 없으면 Section을 만들지 않도록 false를 반환한다.")
-    void isCreateSectionWithoutDownStationIdTest() {
-        assertThat(new LineRequest("4호선", "blue", 1L, null, 10).isCreateSection()).isFalse();
-    }
-
-    @Test
-    @DisplayName("distance가 0이면 Section을 만들지 않도록 false를 반환한다.")
-    void isCreateSectionWithoutDistanceZeroTest() {
-        assertThat(new LineRequest("4호선", "blue", 1L, 2L, 0).isCreateSection()).isFalse();
+    @ParameterizedTest(name = "upStationId 또는 downStationId가 null 이거나 distance 가 0 이면 false를 반환한다.")
+    @CsvSource({",2,10", "1,,10", "1,2,0",})
+    void isCreateSectionWithoutUpStationIdTest(Long upStationId, Long downStationId, Integer distance) {
+        assertThat(new LineRequest("4호선", "blue", upStationId, downStationId, distance).isCreateSection()).isFalse();
     }
 }
