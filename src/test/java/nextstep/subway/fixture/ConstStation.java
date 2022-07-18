7345111c74948d0c071be810b97d2562aa42b9af
DahamLeee <dlek1011@gmail.com>
2022-07-19 00:13:30 +0900

    feat: const station

---
+++ src/test/java/nextstep/subway/fixture/ConstStation.java
@@ -0,0 +1,11 @@
+package nextstep.subway.fixture;
+
+import nextstep.subway.domain.Station;
+
+public final class ConstStation {
+
+    public static final Station 강남역 = new Station("강남역");
+    public static final Station 신논현역 = new Station("신논현역");
+    public static final Station 정자역 = new Station("정자역");
+    public static final Station 판교역 = new Station("판교역");
+}
