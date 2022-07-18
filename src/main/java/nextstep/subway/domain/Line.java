146242010db1d76b8e4c36811f9925e091268836
Oh yong Soo <dhdydtn2019@gmail.com>
2022-07-18 11:51:57 +0900

    refactor: line setter 제거

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -26,16 +26,8 @@ public class Line {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getColor() {
