e0ac68e42fc8227000465f0d9382f70f3876d894
DahamLeee <dlek1011@gmail.com>
2022-07-19 00:43:51 +0900

    test: getStations unit test

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -2,6 +2,7 @@ package nextstep.subway.unit;
 
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -34,6 +35,20 @@ class LineTest {
     @DisplayName("지하철 노선에 존재하는 모든 역 조회")
     @Test
     void getStations() {
+        Line 신분당선 = Line.of("신분당선", "bg-red-600");
+
+        Section 강남_신논현 = Section.of(강남역, 신논현역, 10);
+        Section 신논현_정자 = Section.of(신논현역, 정자역, 5);
+
+        신분당선.addSection(강남_신논현);
+        신분당선.addSection(신논현_정자);
+
+        List<Station> stations = 신분당선.allStations();
+
+        assertAll(
+                () -> assertThat(stations).hasSize(3),
+                () -> assertThat(stations).containsExactly(강남역, 신논현역, 정자역)
+        );
     }
 
     @DisplayName("지하철 노선에 특정 구간 제거")
