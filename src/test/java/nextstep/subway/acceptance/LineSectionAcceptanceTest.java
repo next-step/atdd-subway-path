11ba3905f5c2c2b107b2401d2aae6a3f25f9793e
jiwoo <zbqmgldjfh@gmail.com>
2022-07-18 13:40:04 +0900

    test(LineSectionAcceptanceTest): 하행종점역 으로 구간을 추가하는 인수테스트 작성

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -47,7 +47,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
      */
     @DisplayName("신규 구간의 하행역이 기존 구간의 상행 종점역과 동일할 경우 상행종점역 으로 구간 추가하기 ")
     @Test
-    public void add_first_up_section_at_line() {
+    public void add_up_station_at_line_front() {
         // when
         지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(신규역, 강남역));
 
@@ -56,6 +56,27 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         assertAll(
                 () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
                 () -> assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(신규역, 강남역, 양재역)
+        );
+    }
+
+    /**
+     * When 기존 구간의 하행 종점역과 동일한 상행역을 가지고
+     * When 구간 생성 요청 하면
+     * Then 구간 생성이 성공하고
+     * Then 역 목록을 응답 받는다
+     */
+    @DisplayName("신규 구간의 상행역이 기존 구간의 하행역과 동일할 경우 하행종점역 으로 구간 추가하기")
+    @Test
+    public void add_down_station_at_line_back() {
+        // when
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 신규역));
+
+        // then
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역, 신규역)
         );
     }
 
