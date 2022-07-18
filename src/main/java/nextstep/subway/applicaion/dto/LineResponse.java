9fa8498f365560f166dbd6ee3db4d77f535c9b47
DahamLeee <dlek1011@gmail.com>
2022-07-19 00:44:37 +0900

    refactor: use static factory method

--- src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -1,6 +1,11 @@
 package nextstep.subway.applicaion.dto;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
+
 import java.util.List;
+
+import static java.util.stream.Collectors.*;
 
 public class LineResponse {
     private Long id;
@@ -8,11 +13,17 @@ public class LineResponse {
     private String color;
     private List<StationResponse> stations;
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
+    private LineResponse(Long id, String name, String color, List<Station> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
-        this.stations = stations;
+        this.stations = stations.stream()
+                .map(station -> StationResponse.of(station.getId(), station.getName()))
+                .collect(toList());
+    }
+
+    public static LineResponse from(Line line, List<Station> stations) {
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations);
     }
 
     public Long getId() {
