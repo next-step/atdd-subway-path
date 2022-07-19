102395e701b40a5d015e1a3c2d108a460145f040
DahamLeee <dlek1011@gmail.com>
2022-07-19 14:57:51 +0900

    feat: add static factory method

--- src/main/java/nextstep/subway/applicaion/dto/SectionRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/SectionRequest.java
@@ -5,6 +5,18 @@ public class SectionRequest {
     private Long downStationId;
     private int distance;
 
+    private SectionRequest() { }
+
+    private SectionRequest(Long upStationId, Long downStationId, int distance) {
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public static SectionRequest of(Long upStationId, Long downStationId, int distance) {
+        return new SectionRequest(upStationId, downStationId, distance);
+    }
+
     public Long getUpStationId() {
         return upStationId;
     }
