f3c27bda6489a653b1059cf30863ad6742105cb4
imcool2551 <imcool2551@gmail.com>
2022-07-19 22:31:21 +0900

    test: 구간 삭제에대한 도메인 단위 테스트 작성

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -2,16 +2,17 @@ package nextstep.subway.unit;
 
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
+import nextstep.subway.domain.exception.CannotDeleteSectionException;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class LineTest {
 
     @Test
-    void addSection() {
+    void 구간_추가() {
         // given
         long upStationId = 1L;
         long downStationId = 2L;
@@ -28,5 +29,35 @@ class LineTest {
         Section addedSection = sections.get(0);
         assertThat(addedSection.getUpStationId()).isEqualTo(upStationId);
         assertThat(addedSection.getDownStationId()).isEqualTo(downStationId);
+    }
+
+    @Test
+    void 구간_삭제() {
+        // given
+        long upStationId = 1L;
+        long downStationId = 2L;
+
+        Line line = new Line("신분당선", "red");
+        line.addSection(upStationId, downStationId, 6);
+
+        // when
+        line.removeSection(downStationId);
+
+        // then
+        assertThat(line.getSections()).isEmpty();
+    }
+
+    @Test
+    void 구간_삭제_종점이_아니면_예외() {
+        // given
+        long upStationId = 1L;
+        long downStationId = 2L;
+
+        Line line = new Line("신분당선", "red");
+        line.addSection(upStationId, downStationId, 6);
+
+        // when + then
+        assertThatThrownBy(() -> line.removeSection(upStationId))
+                .isInstanceOf(CannotDeleteSectionException.class);
     }
 }
