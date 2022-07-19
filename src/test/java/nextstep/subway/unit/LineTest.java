01e571a70cbf802f46dcedf64ce1d52048c55e76
waterdragon <tndyd5390@naver.com>
2022-07-19 22:23:46 +0900

    test: LineTest 노선 단위 테스트 작성

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,17 +1,55 @@
 package nextstep.subway.unit;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+
 class LineTest {
-    @Test
-    void addSection() {
-    }
+	@Test
+	void addSection() {
+		//given
+		Line line = new Line("분당선", "yellow");
+		Section section = new Section(line, new Station("서현역"), new Station("이매역"), 10);
 
-    @Test
-    void getStations() {
-    }
+		//when
+		line.addSection(section);
 
-    @Test
-    void removeSection() {
-    }
+		//then
+		assertThat(line.getSections()).containsExactly(section);
+	}
+
+	@Test
+	void getStations() {
+		//given
+		Line line = new Line("분당선", "yellow");
+		Station upStation = new Station("서현역");
+		Station downStation = new Station("이매역");
+		line.addSection(new Section(line, upStation, downStation, 10));
+
+		//when
+		List<Station> stationList = line.getStations();
+
+		//then
+		assertThat(stationList).containsExactly(upStation, downStation);
+	}
+
+	@Test
+	void removeSection() {
+		//given
+		Line line = new Line("분당선", "yellow");
+		Section section = new Section(line, new Station("서현역"), new Station("이매역"), 10);
+		line.addSection(section);
+
+		//when
+		line.removeSection(section);
+
+		//then
+		assertThat(line.getSections()).isEmpty();
+	}
 }
