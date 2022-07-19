31c1bf5ed40918990a99e8c42a6e27f9795d0c2d
MRSTK90 <dkgell13@gmail.com>
2022-07-20 01:14:05 +0900

    new : 테스트 코드 작성

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,11 +1,21 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
@@ -18,11 +28,20 @@ public class LineServiceMockTest {
     void addSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        Line line = new Line("5호선","purple");
+        when(stationService.findById(1L)).thenReturn(new Station("마곡역"));
+        when(stationService.findById(2L)).thenReturn(new Station("발산역"));
+        when(lineRepository.findById(1L)).thenReturn(Optional.of(line));
+        LineService lineService = new LineService(lineRepository, stationService);
 
         // when
         // lineService.addSection 호출
+        SectionRequest sectionRequest = new SectionRequest(1L, 2L, 10);
+        lineService.addSection(1L,sectionRequest);
+
 
         // then
         // line.findLineById 메서드를 통해 검증
+        assertThat(line.getSections()).hasSize(1);
     }
 }
--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,17 +1,64 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
     @Test
     void addSection() {
+        //Given
+        Station magok = new Station(1L, "마곡역");
+        Station balsan = new Station(2L, "발산역");
+        Line line = new Line("5호선", "purple");
+
+        //When
+        Section section = new Section(line, magok, balsan, 10);
+        line.addSection(section);
+
+        //Then
+        assertThat(line.getSections()).hasSize(1);
+        assertThat(line.getSections()).containsExactly(section);
     }
 
     @Test
     void getStations() {
+        //Given
+        Station magok = new Station(1L, "마곡역");
+        Station balsan = new Station(2L, "발산역");
+        Line line = new Line("5호선", "purple");
+        Section section = new Section(line, magok, balsan, 10);
+        line.addSection(section);
+
+        //When
+        List<Station> stations = line.getStations();
+
+        //Then
+        assertThat(stations).contains(magok, balsan);
     }
 
     @Test
     void removeSection() {
+        //Given
+        Station songjeong = new Station(1L, "송정역");
+        Station magok = new Station(2L, "마곡역");
+        Station balsan = new Station(3L, "발산역");
+        Line line = new Line("5호선", "purple");
+        Section section1 = new Section(line, songjeong, magok, 7);
+        Section section2 = new Section(line, magok, balsan, 10);
+        line.addSection(section1);
+        line.addSection(section2);
+
+        //When
+        line.removeSection(line.getSections().size()-1);
+
+        //Then
+        assertThat(line.getSections()).hasSize(1);
+
     }
 }
