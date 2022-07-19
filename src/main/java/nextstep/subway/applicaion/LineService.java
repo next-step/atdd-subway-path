c179525c53049baa339824c2a43080b714dfee8c
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-19 23:33:07 +0900

    Refact : 인수 테스트 코드 수정

--- src/main/java/nextstep/subway/applicaion/LineService.java
+++ src/main/java/nextstep/subway/applicaion/LineService.java
@@ -106,12 +106,6 @@ public class LineService {
     @Transactional
     public void deleteSection(Long lineId, Long stationId) {
         Line line = lineRepository.findById(lineId).orElseThrow(IllegalArgumentException::new);
-        Station station = stationService.findById(stationId);
-
-        if (!line.getSections().get(line.getSections().size() - 1).getDownStation().equals(station)) {
-            throw new IllegalArgumentException();
-        }
-
-        line.getSections().remove(line.getSections().size() - 1);
+        line.deleteSection(stationId);
     }
 }
