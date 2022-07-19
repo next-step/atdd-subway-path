3a248f1163e6a2384eae8e7c104cd9b3331fa3ed
jay.w <jay.w@kakaocorp.com>
2022-07-19 16:57:41 +0900

    fix: StationRequest 기본 생성자 추가
    - No Argument Constructor 가 없어 인수 테스트 실패 발생

--- src/main/java/nextstep/subway/applicaion/dto/StationRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationRequest.java
@@ -3,6 +3,8 @@ package nextstep.subway.applicaion.dto;
 public class StationRequest {
     private String name;
 
+    public StationRequest() {}
+
     public StationRequest(String name) {
         this.name = name;
     }
