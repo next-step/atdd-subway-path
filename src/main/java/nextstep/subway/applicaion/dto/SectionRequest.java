411fc3665838152d16b1bc822b49b45fa1e8b832
waterdragon <tndyd5390@naver.com>
2022-07-19 22:35:26 +0900

    feat: LineServiceMockTest 구현에 필요한 생성자 구현

--- src/main/java/nextstep/subway/applicaion/dto/SectionRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/SectionRequest.java
@@ -1,19 +1,28 @@
 package nextstep.subway.applicaion.dto;
 
 public class SectionRequest {
-    private Long upStationId;
-    private Long downStationId;
-    private int distance;
+	private Long upStationId;
+	private Long downStationId;
+	private int distance;
 
-    public Long getUpStationId() {
-        return upStationId;
-    }
+	private SectionRequest() {
+	}
 
-    public Long getDownStationId() {
-        return downStationId;
-    }
+	public SectionRequest(Long upStationId, Long downStationId, int distance) {
+		this.upStationId = upStationId;
+		this.downStationId = downStationId;
+		this.distance = distance;
+	}
 
-    public int getDistance() {
-        return distance;
-    }
+	public Long getUpStationId() {
+		return upStationId;
+	}
+
+	public Long getDownStationId() {
+		return downStationId;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
 }
