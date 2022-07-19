85fc768bf38d5f7956f908c5e5e235656208b2df
waterdragon <tndyd5390@naver.com>
2022-07-19 22:24:14 +0900

    feat: 노선 구간추가, 구간삭제, 역조회 기능 구현

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -1,53 +1,76 @@
 package nextstep.subway.domain;
 
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Line {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String color;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String color;
 
-    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
-    private List<Section> sections = new ArrayList<>();
+	@OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+	private List<Section> sections = new ArrayList<>();
 
-    public Line() {
-    }
+	public Line() {
+	}
 
-    public Line(String name, String color) {
-        this.name = name;
-        this.color = color;
-    }
+	public Line(String name, String color) {
+		this.name = name;
+		this.color = color;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getColor() {
-        return color;
-    }
+	public String getColor() {
+		return color;
+	}
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+	public void setColor(String color) {
+		this.color = color;
+	}
 
-    public List<Section> getSections() {
-        return sections;
-    }
+	public List<Section> getSections() {
+		return sections;
+	}
+
+	public void addSection(Section section) {
+		this.sections.add(section);
+	}
+
+	public List<Station> getStations() {
+		return this.sections.stream()
+			.map(Section::getStationList)
+			.flatMap(List::stream)
+			.distinct()
+			.collect(Collectors.toList());
+	}
+
+	public void removeSection(Section section) {
+		this.sections.remove(section);
+	}
 }
