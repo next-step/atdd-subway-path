471b359496a186c9d3e055c1180d231782d3b572
임명섭 <lms@elonsoft.co.kr>
2022-07-19 20:13:25 +0900

    refactor 발생하지 않는 예외 핸들링 처리 삭제

--- src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
+++ src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
@@ -2,7 +2,6 @@ package nextstep.subway.ui;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,12 +10,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ControllerExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(ControllerExceptionHandler.class);
-
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<Void> handleIllegalArgsException(DataIntegrityViolationException exception) {
-        log.warn(exception.getMessage());
-        return ResponseEntity.badRequest().build();
-    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException exception) {
