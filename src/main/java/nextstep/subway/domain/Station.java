86be285b0ed41d4aba0d01cc01207a3008106311
MangKyu <whalsrb1226@gmail.com>
2022-07-20 20:31:08 +0900

    리뷰 반영(Station EqualsAndHashCode 추가)

--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -40,4 +40,18 @@ public class Station {
                 ", name='" + name + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Station station = (Station) o;
+        return Objects.equals(id, station.id) && Objects.equals(name, station.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
 }
