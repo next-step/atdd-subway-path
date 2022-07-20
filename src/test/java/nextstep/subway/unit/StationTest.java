2db05a5a0f47461d9704470123d37b5f962629bd
Shin seongsu <tlstjdtn321@naver.com>
2022-07-21 01:16:46 +0900

    test: Station 유닛 테스트

---
+++ src/test/java/nextstep/subway/unit/StationTest.java
@@ -0,0 +1,20 @@
+package nextstep.subway.unit;
+
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("지하철역 테스트")
+public class StationTest {
+
+    @Test
+    @DisplayName("역을 생성한다.")
+    void createStation() {
+        Station 강남역 = new Station("강남역");
+
+        assertThat(강남역.getName()).isEqualTo("강남역");
+    }
+
+}
