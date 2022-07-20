47f13466aa7c0a102325963e3d127401ad915d89
giyeon95 <giyeon15@gmail.com>
2022-07-21 00:29:54 +0900

    test: 예외케이스에 대한 인수테스트 작성
    ```
    예외케이스에 대한 인수테스트도 하나 작성해보시면 어떨까요?
    
    _Originally posted by @starkim06 in https://github.com/next-step/atdd-subway-path/pull/311#discussion_r925431677_
    
    ```

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -5,6 +5,8 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.http.HttpStatus;
 
 import java.util.HashMap;
@@ -88,6 +90,27 @@ class SectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
+     * When 지하철 노선 구간 중간에 길이가 기존 구간보다 같거나 긴 역을 추가하면
+     * Then 노선에 새로운 구간이 추가가 실패한다.
+     */
+    @ValueSource(ints = {10, 11})
+    @ParameterizedTest(name = "[{argumentsWithNames}] 지하철 노선의 중간 구간이 추가된다.")
+    void addLineBetweenSectionFail(int distance) {
+
+        // when
+        Long 역삼역 = 지하철역_생성_요청("역삼역").jsonPath().getLong("id");
+        ExtractableResponse<Response> createResopnse = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 역삼역, distance));
+
+        // then
+        assertThat(createResopnse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역);
+    }
+
+
+    /**
      * Given 지하철 노선에 새로운 구간 추가를 요청 하고
      * When 지하철 노선의 마지막 구간 제거를 요청 하면
      * Then 노선에 구간이 제거된다
@@ -120,10 +143,14 @@ class SectionAcceptanceTest extends AcceptanceTest {
     }
 
     private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId) {
+        return createSectionCreateParams(upStationId, downStationId, 6);
+    }
+
+    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, int distance) {
         Map<String, String> params = new HashMap<>();
         params.put("upStationId", upStationId + "");
         params.put("downStationId", downStationId + "");
-        params.put("distance", 6 + "");
+        params.put("distance", distance + "");
         return params;
     }
 
