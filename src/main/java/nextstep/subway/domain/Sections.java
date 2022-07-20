c97a28e860fc384df04e13c494f1a158ed935c27
DahamLeee <dlek1011@gmail.com>
2022-07-20 16:17:53 +0900

    feat: addSection requirements

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -1,10 +1,13 @@
 package nextstep.subway.domain;
 
+import nextstep.subway.exception.AddSectionException;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Collections.*;
 import static java.util.stream.Collectors.*;
@@ -16,11 +19,34 @@ public class Sections {
     private List<Section> value = new ArrayList<>();
 
     public void addSection(Section section) {
+        if (value.isEmpty()) {
+            this.value.add(section);
+            return;
+        }
+
+        matchLastStationAndNewUpStation(section.getUpStation());
+        duplicateStation(section.getDownStation());
+
         this.value.add(section);
     }
 
-    public List<Section> getValue() {
-        return unmodifiableList(value);
+    private void matchLastStationAndNewUpStation(Station upStation) {
+        Station lastStation = lastSection().getDownStation();
+
+        if (!lastStation.equals(upStation)) {
+            throw new AddSectionException("기존 노선의 종점역과 신규 노선의 상행역이 일치하지 않습니다.");
+        }
+    }
+
+    private void duplicateStation(Station downStation) {
+        Optional<Station> findStation = value.stream()
+                .map(Section::getUpStation)
+                .filter(upStation -> upStation.equals(downStation))
+                .findAny();
+
+        if (findStation.isPresent()) {
+            throw new AddSectionException("신규 구간의 하행역이 기존 노션의 역에 이미 등록되어 있습니다.");
+        }
     }
 
     public List<Station> allStations() {
@@ -47,5 +73,9 @@ public class Sections {
 
     private Section lastSection() {
         return value.get(value.size() - 1);
+    }
+
+    public List<Section> getValue() {
+        return unmodifiableList(value);
     }
 }
---
+++ src/main/java/nextstep/subway/exception/AddSectionException.java
@@ -0,0 +1,8 @@
+package nextstep.subway.exception;
+
+public class AddSectionException extends RuntimeException {
+
+    public AddSectionException(String message) {
+        super(message);
+    }
+}
