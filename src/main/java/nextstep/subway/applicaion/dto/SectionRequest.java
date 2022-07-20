04060c66d9579f85f887cf51f203dc47304f2479
epicarts <0505zxc@gmail.com>
2022-07-20 11:46:07 +0900

    feat: SectionRequest dto에 NoArgsConstructor 애노테이션 추가

--- src/main/java/nextstep/subway/applicaion/dto/SectionRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/SectionRequest.java
@@ -2,9 +2,11 @@ package nextstep.subway.applicaion.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SectionRequest {
     private Long upStationId;
     private Long downStationId;
