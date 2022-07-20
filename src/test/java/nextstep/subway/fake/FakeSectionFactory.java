14db281020fa8ec1843ada0225e102af7b645194
uram <parkuram12@gmail.com>
2022-07-20 21:12:17 +0900

    feat: 테스트 픽처를 위한 FakeFactory class

--- src/test/java/nextstep/subway/fake/FakeSectionFactory.java
+++
@@ -1,4 +0,0 @@
-package nextstep.subway.fake;
-
-public class FakeSectionFactory {
-}
--- src/test/java/nextstep/subway/fake/FakeStationFactory.java
+++ src/test/java/nextstep/subway/fake/FakeStationFactory.java
@@ -3,8 +3,19 @@ package nextstep.subway.fake;
 import nextstep.subway.domain.Station;
 
 public class FakeStationFactory {
-    public static Station 강남역 = new Station(1L, "강남역");
-    public static Station 선릉역 = new Station(2L, "선릉역");
-    public static Station 영통역 = new Station(1L, "영통역");
-    public static Station 구의역 = new Station(1L, "구의역");
+    public static Station 강남역() {
+      return new Station(1L, "강남역");
+    }
+
+    public static Station 선릉역() {
+        return new Station(2L, "선릉역");
+    }
+
+    public static Station 영통역() {
+        return new Station(3L, "영통역");
+    }
+
+    public static Station 구의역() {
+        return new Station(4L, "구의역");
+    }
 }
