4aacc3df70d2168cabd6e815e4446672a23f47fd
uram <parkuram12@gmail.com>
2022-07-20 13:23:17 +0900

    test: lineService 테스트 작성

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,18 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.applicaion.dto.LineRequest;
+import nextstep.subway.applicaion.dto.LineResponse;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -15,7 +21,6 @@ public class LineServiceTest {
     private StationRepository stationRepository;
     @Autowired
     private LineRepository lineRepository;
-
     @Autowired
     private LineService lineService;
 
@@ -23,11 +28,65 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 강남역 = stationRepository.save(new Station("강남역"));
+        Station 교대역 = stationRepository.save(new Station("교대역"));
+        Line 분당선 = lineRepository.save(new Line("분당선", "green"));
+        Section 강남역_교대역_구간 = new Section(분당선, 강남역, 교대역, 10);
 
         // when
         // lineService.addSection 호출
+        SectionRequest sectionRequest = new SectionRequest(강남역.getId(), 교대역.getId(), 10);
+        lineService.addSection(분당선.getId(), sectionRequest);
 
         // then
         // line.getSections 메서드를 통해 검증
+        Line line = lineRepository.findById(분당선.getId()).orElseThrow(IllegalArgumentException::new);
+        Sections sections = line.getSections();
+        assertThat(sections.getList()).containsAnyOf(강남역_교대역_구간);
     }
+
+    @Test
+    void 라인_등록_검증() {
+        //given
+        LineRequest lineRequest = new LineRequest("2호선", "green");
+
+        //when
+        lineService.saveLine(lineRequest);
+
+        //then
+        List<Line> findAllLines = lineRepository.findAll();
+        assertThat(findAllLines).hasSize(1);
+    }
+
+    @Test
+    void 라인_수정_검증() {
+        //given
+        LineRequest saveRequest = new LineRequest("분당선", "yellow");
+        LineResponse lineResponse = lineService.saveLine(saveRequest);
+
+        //when
+        LineRequest updateRequest = new LineRequest("신분당선", "red");
+        lineService.updateLine(lineResponse.getId(), updateRequest);
+
+        //then
+        LineResponse findLineResponse = lineService.findById(lineResponse.getId());
+        assertThat(findLineResponse.getName()).isEqualTo("신분당선");
+        assertThat(findLineResponse.getColor()).isEqualTo("red");
+    }
+
+    @Test
+    void 라인_삭제_검증() {
+        //given
+        LineRequest saveRequest = new LineRequest("2호선", "green");
+        LineResponse saveResponse = lineService.saveLine(saveRequest);
+
+        //when
+        lineService.deleteLine(saveResponse.getId());
+
+        //then
+        List<LineResponse> lineResponses = lineService.showLines();
+        assertThat(lineResponses).hasSize(0);
+    }
+
+
 }
