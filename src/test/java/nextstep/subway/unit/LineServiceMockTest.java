aded60c1a391ced77a9d1580875e4238087236fd
sangnyyy <dltkdals2202@gmail.com>
2022-07-21 17:22:37 +0900

    test : Mockito 를 사용해서 LineService 구간 추가 테스트 추가

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,28 +1,46 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
-public class LineServiceMockTest {
+class LineServiceMockTest {
     @Mock
     private LineRepository lineRepository;
     @Mock
     private StationService stationService;
 
+    @DisplayName("구간을 추가할 수 있다.")
     @Test
     void addSection() {
         // given
-        // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        final var upStation = new Station("선릉역");
+        final var downStation = new Station("삼성역");
+        final var line = new Line("2호선", "bg-green-600");
+
+        when(stationService.findById(1L)).thenReturn(upStation);
+        when(stationService.findById(2L)).thenReturn(downStation);
+        when(lineRepository.findById(1L)).thenReturn(Optional.of(line));
 
         // when
-        // lineService.addSection 호출
+        final var lineService = new LineService(lineRepository, stationService);
+        lineService.addSection(1L, new SectionRequest(1L, 2L, 10));
 
         // then
-        // line.findLineById 메서드를 통해 검증
+        assertThat(line.findAllStations()).containsExactly(upStation, downStation);
     }
 }
