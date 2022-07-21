dbedfb4fb177cdf240df12bded116252f043e206
MRSTK90 <dkgell13@gmail.com>
2022-07-22 01:48:14 +0900

    fix : 변경된 스펙에 따른 기존 인수테스트 내용 살짝 변경

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -36,21 +36,41 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * When 지하철 노선에 새로운 구간 추가를 요청 하면
+     * When 지하철 노선에서 기존 구간의 하행을 기준으로 새로운 구간 추가를 요청 하면
      * Then 노선에 새로운 구간이 추가된다
      */
-    @DisplayName("지하철 노선에 구간을 등록")
+    @DisplayName("지하철 노선에서 기존 구간의 하행을 기준으로 구간을 등록")
     @Test
-    void addLineSection() {
+    void addLineSectionBaseOnDownStation() {
         // when
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
         지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+        Long 정자다음역 = 지하철역_생성_요청("정자다음역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(정자역, 정자다음역));
 
         // then
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역, 정자역);
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역, 정자역, 정자다음역);
     }
+
+    /**
+     * When 지하철 노선에서 기존 구간의 상행을 기준으로 새로운 구간 추가를 요청 하면
+     * Then 노선에 새로운 구간이 추가된다
+     */
+    @DisplayName("지하철 노선에서 기존 구간의 상행을 기준으로 구간을 등록")
+    @Test
+    void AddLineSectionBaseOnUpstation() {
+        //when
+        Long 강남양재사이역 = 지하철역_생성_요청("강남양재사이역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 강남양재사이역));
+
+        //Then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 강남양재사이역, 양재역);
+    }
+
 
     /**
      * Given 지하철 노선에 새로운 구간 추가를 요청 하고
