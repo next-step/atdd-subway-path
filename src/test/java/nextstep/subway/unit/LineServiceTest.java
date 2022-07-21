f7f5617e72eb05f762baf08247ecf93eb6e6bb1e
sangnyyy <dltkdals2202@gmail.com>
2022-07-21 17:33:24 +0900

    test : 실제로 동작하는 LineService 를 통해 구간 추가 테스트 작성

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,33 +1,52 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Transactional
-public class LineServiceTest {
+class LineServiceTest {
+
     @Autowired
     private StationRepository stationRepository;
+
     @Autowired
     private LineRepository lineRepository;
 
     @Autowired
     private LineService lineService;
 
+    @DisplayName("구간을 추가할 수 있다.")
     @Test
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        final var upStation = new Station("선릉역");
+        final var downStation = new Station("삼성역");
+        final var line = new Line("2호선", "bg-green-600");
+
+        stationRepository.saveAll(List.of(upStation, downStation));
+        lineRepository.save(line);
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(1L, new SectionRequest(1L, 2L, 10));
 
         // then
         // line.getSections 메서드를 통해 검증
+        assertThat(line.getSections()).hasSize(1);
     }
 }
