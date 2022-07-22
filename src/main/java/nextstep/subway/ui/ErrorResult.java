0eb54a3e50aaa86334da7a0b7e07ba6a23e3e25e
DahamLeee <dlek1011@gmail.com>
2022-07-22 11:13:52 +0900

    feat: Add Exception Handling

---
+++ src/main/java/nextstep/subway/ui/ErrorResult.java
@@ -0,0 +1,14 @@
+package nextstep.subway.ui;
+
+public class ErrorResult {
+
+    private String message;
+
+    public ErrorResult(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
