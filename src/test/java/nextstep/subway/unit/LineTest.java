78e267ce936e83bcb80974618c2d25c58bc2ebbb
younlll <youn.lee96@gmail.com>
2022-07-22 23:13:12 +0900

    test: 변경된 구간 추가 제약 사항에 따른 단위테스트 수정

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
 
 class LineTest {
 
-    @DisplayName("새로운 구간을 정상적으로 추가했다")
+    @DisplayName("노선의 하행 종점역에 새로운 구간을 정상적으로 추가했습니다.")
     @Test
     void addSection() {
         // given
@@ -79,21 +79,6 @@ class LineTest {
 
         // when & then
         assertThatThrownBy(() -> line.addSection(new Section(line, upStation, newStation, 6)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("신규 구간을 추가하는 경우 신규역이 이전에 등록된 역 목록에 있다면 오류가 발생한다")
-    @Test
-    void addSectionMatchedPreviouslyStationException() {
-        // given
-        Station upStation = new Station("신논현역");
-        Station downStation = new Station("언주역");
-        Station newStation = new Station("신논현역");
-        Line line = new Line("9호선", "bg-brown-600");
-        line.addSection(new Section(line, upStation, downStation, 5));
-
-        // when & then
-        assertThatThrownBy(() -> line.addSection(new Section(line, downStation, newStation, 6)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
