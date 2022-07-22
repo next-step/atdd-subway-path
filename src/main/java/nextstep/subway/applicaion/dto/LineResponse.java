4964ae2c1c48f298e6decca692843822911c7ec4
chkim <cndghks0213@gmail.com>
2022-07-23 00:38:37 +0900

    feat: 응답을 생성하는 정적 팩터리 메서드 추가

--- src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -1,6 +1,13 @@
 package nextstep.subway.applicaion.dto;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class LineResponse {
     private Long id;
@@ -13,6 +20,31 @@ public class LineResponse {
         this.name = name;
         this.color = color;
         this.stations = stations;
+    }
+
+    public static LineResponse from(Line line) {
+        return new LineResponse(
+                line.getId(),
+                line.getName(),
+                line.getColor(),
+                createStationResponses(line)
+        );
+    }
+
+    private static List<StationResponse> createStationResponses(Line line) {
+        if (line.getSections().isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<Station> stations = line.getSections().stream()
+                .map(Section::getDownStation)
+                .collect(Collectors.toList());
+
+        stations.add(0, line.getSections().get(0).getUpStation());
+
+        return stations.stream()
+                .map(StationResponse::from)
+                .collect(Collectors.toList());
     }
 
     public Long getId() {
@@ -29,6 +61,23 @@ public class LineResponse {
 
     public List<StationResponse> getStations() {
         return stations;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LineResponse response = (LineResponse) o;
+        return Objects.equals(getId(), response.getId()) && Objects.equals(getName(), response.getName()) && Objects.equals(getColor(), response.getColor()) && Objects.equals(getStations(), response.getStations());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getColor(), getStations());
     }
 }
 
--- src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
@@ -1,5 +1,9 @@
 package nextstep.subway.applicaion.dto;
 
+import nextstep.subway.domain.Station;
+
+import java.util.Objects;
+
 public class StationResponse {
     private Long id;
     private String name;
@@ -12,11 +16,28 @@ public class StationResponse {
         this.name = name;
     }
 
+    public static StationResponse from(Station station) {
+        return new StationResponse(station.getId(), station.getName());
+    }
+
     public Long getId() {
         return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StationResponse that = (StationResponse) o;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getName(), that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
     }
 }
