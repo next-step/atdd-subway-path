2651ce29673fe7957ea1681a49e65b90ead30dde
jay.w <jay.w@kakaocorp.com>
2022-07-23 15:24:26 +0900

    test: Line 유닛 테스트 (마지막, 첫 구간 추가)

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,24 +12,40 @@ class LineTest {
 
     private Station 광교역 = new Station("광교역");
     private Station 광교중앙역 = new Station("광교중앙역");
-    private Station 상현역 = new Station("상현역");
 
-    @DisplayName("노선 추가")
+    private Line sut;
+
+    // Given: 구간 하나를 포함한 노선 추가
+    @BeforeEach
+    void setUp() {
+        sut = new Line();
+        sut.addSection(광교역, 광교중앙역, 10);
+    }
+
+    @DisplayName("마지막 구간 추가")
     @Test
-    void addSection() {
-        var sut = new Line();
-        var distance = 10;
+    void addLastSection() {
+        var 상현역 = new Station("상현역");
 
-        sut.addSection(광교역, 광교중앙역, distance);
+        sut.addSection(광교중앙역, 상현역, 10);
 
-        assertThat(sut.getStations()).containsExactly(광교역, 광교중앙역);
+        assertThat(sut.getStations()).containsExactly(광교역, 광교중앙역, 상현역);
+    }
+
+    @DisplayName("첫 구간 추가")
+    @Test
+    void addFirstSection() {
+        var 호매실역 = new Station("호매실역");
+
+        sut.addSection(호매실역, 광교역, 10);
+
+        assertThat(sut.getStations()).containsExactly(호매실역, 광교역, 광교중앙역);
     }
 
     @DisplayName("노선 내 역 조회")
     @Test
     void getStations() {
-        var sut = new Line();
-        sut.addSection(광교역, 광교중앙역, 10);
+        var 상현역 = new Station("상현역");
         sut.addSection(광교중앙역, 상현역, 10);
 
         var stations = sut.getStations();
@@ -39,8 +56,7 @@ class LineTest {
     @DisplayName("노선 내 구간 삭제")
     @Test
     void removeSection() {
-        var sut = new Line();
-        sut.addSection(광교역, 광교중앙역, 10);
+        var 상현역 = new Station("상현역");
         sut.addSection(광교중앙역, 상현역, 10);
 
         sut.removeSection(상현역);
