578e7edad1d4c9189fedadeae1fe19fc1fa6f63c
flamme <flamme1004@gmail.com>
2022-07-23 20:57:09 +0900

    test: 인수 조건을 정의하고 도출하라

---
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -0,0 +1,62 @@
+package nextstep.subway.acceptance;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static nextstep.subway.acceptance.LineSectionAcceptanceTest.createSectionCreateParams;
+import static nextstep.subway.acceptance.LineSteps.지하철_노선_생성_요청;
+import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철_구간_생성_요청;
+import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
+
+public class PathAcceptanceTest extends AcceptanceTest {
+
+    private Long 교대역;
+    private Long 강남역;
+    private Long 양재역;
+    private Long 남부터미널역;
+    private Long 이호선;
+    private Long 신분당선;
+    private Long 삼호선;
+
+    /**
+     * 교대역    --- *2호선* ---   강남역
+     * |                        |
+     * *3호선*                   *신분당선*
+     * |                        |
+     * 남부터미널역  --- *3호선* ---   양재
+     */
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        교대역 = 지하철역_생성_요청("교대역").jsonPath().getLong("id");
+        강남역 = 지하철역_생성_요청("강남역").jsonPath().getLong("id");
+        양재역 = 지하철역_생성_요청("양재역").jsonPath().getLong("id");
+        남부터미널역 = 지하철역_생성_요청("남부터미널역").jsonPath().getLong("id");
+
+        이호선 = 지하철_노선_생성_요청("2호선", "green", 교대역, 강남역, 10).jsonPath().getLong("id");
+        신분당선 = 지하철_노선_생성_요청("신분당선", "red", 강남역, 양재역, 10).jsonPath().getLong("id");
+        삼호선 = 지하철_노선_생성_요청("3호선", "orange", 교대역, 남부터미널역, 2).jsonPath().getLong("id");
+
+        지하철_노선에_지하철_구간_생성_요청(삼호선, createSectionCreateParams(남부터미널역, 양재역, 3));
+    }
+
+    /**
+     * When 경로를 조회하면
+     * Then 최단 경로 역 목록과 구간의 거리를 찾을 수 있다.
+     */
+
+    /**
+     * When 출발역과 도착역이 같은 경우
+     * Then 최단 경로 조회에 실패한다.
+     */
+
+    /**
+     * When 출발역과 도착역이 연결되어 있지 않은 경우
+     * Then 최단 경로 조회에 실패한다.
+     */
+
+    /**
+     * When 존재하지 않은 출발역이나 도착역을 조회 할 경우
+     * Then 최단 경로 조회에 실패한다.
+     */
+}
