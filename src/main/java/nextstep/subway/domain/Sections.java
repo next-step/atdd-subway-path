09f6984fa330c93245138c3ead401e151a0b2218
waterdragon <tndyd5390@naver.com>
2022-07-23 15:22:17 +0900

    feat: 기존 구간의 역을 기준으로 새로운 구간 추가 기능 구현

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -3,6 +3,7 @@ package nextstep.subway.domain;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -14,12 +15,49 @@ public class Sections {
 	@OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
 	private List<Section> sectionList = new ArrayList<>();
 
+	protected Sections(){
+
+	}
+
+	private Sections(List<Section> sectionList) {
+		this.sectionList = sectionList;
+	}
+
+	public static Sections from(List<Section> sectionList){
+		return new Sections(sectionList);
+	}
+
 	public List<Section> getSectionList() {
 		return this.sectionList;
 	}
 
-	public void addSection(Section section) {
-		this.sectionList.add(section);
+	public void addSection(Section newSection) {
+		if(sectionList.isEmpty()){
+			sectionList.add(newSection);
+			return;
+		}
+
+		Optional<Section> sameUpStationSection = getSameUpStationSection(newSection);
+
+		if(sameUpStationSection.isPresent()){
+			addSectionUpStationBased(newSection, sameUpStationSection.get());
+			return;
+		}
+
+	}
+
+	private void addSectionUpStationBased(Section newSection, Section section) {
+		sectionList.remove(section);
+		sectionList.add(new Section(section.getLine(), section.getUpStation(), newSection.getDownStation(),
+			newSection.getDistance()));
+		sectionList.add(new Section(section.getLine(), newSection.getDownStation(), section.getDownStation(),
+			section.getDistance() - newSection.getDistance()));
+	}
+
+	public Optional<Section> getSameUpStationSection(Section newSection){
+		return this.sectionList.stream()
+			.filter(section -> section.getUpStation().getName().equals(newSection.getUpStation().getName()))
+			.findFirst();
 	}
 
 	public List<Station> getStations() {
@@ -40,4 +78,6 @@ public class Sections {
 	public Section getLastSection(){
 		return sectionList.get(sectionList.size() - 1);
 	}
+
+
 }
