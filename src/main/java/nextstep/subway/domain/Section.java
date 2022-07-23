d3cc7e4682ba4e7d59f201cb61f46ae353db663e
Shin seongsu <tlstjdtn321@naver.com>
2022-07-24 04:45:59 +0900

    feat: 상행선, 하행선 구간 넣을 수 있는 기능 추가

--- src/main/java/nextstep/subway/domain/Section.java
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import nextstep.subway.exception.SectionException;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -42,4 +43,31 @@ public class Section {
     public List<Station> getStations() {
         return List.of(upStation, downStation);
     }
+
+    public boolean isEqualsUpStationAndDownStation(Section section) {
+        return upStation.equals(section.getUpStation()) && downStation.equals(section.getDownStation());
+    }
+
+    private void isOverLength(int insertDistance) {
+        if(distance <= insertDistance) {
+            throw new SectionException("역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없습니다.");
+        }
+    }
+
+    private void minuseDistance(int distance) {
+        this.distance -= distance;
+    }
+
+    public void changeDownStationAndDistance(Station station, int distance) {
+        isOverLength(distance);
+        minuseDistance(distance);
+        this.upStation = station;
+    }
+
+    public void changeUpStationAndDistance(Station station, int distance) {
+        isOverLength(distance);
+        minuseDistance(distance);
+        this.downStation = station;
+    }
+
 }
--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -1,12 +1,15 @@
 package nextstep.subway.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import lombok.Getter;
+import nextstep.subway.exception.SectionException;
 
 @Getter
 @Embeddable
@@ -18,15 +21,89 @@ public class Sections {
     private List<Section> sections = new ArrayList<>();
 
     public void addSection(Section section) {
+        isEmptySections(section);
+
         sections.add(section);
     }
 
+    private void isEmptySections(Section section) {
+        if(!sections.isEmpty()) {
+            isExistSection(section);
+            isExitsUpStationOrDownStation(section);
+
+            insertUpStationBetween(section);
+            insertDownStationBetween(section);
+        }
+    }
+
+    private void insertUpStationBetween(Section section) {
+        if (sections.get(0).getUpStation().equals(section.getUpStation())) {
+            sections.get(0).changeDownStationAndDistance(section.getDownStation(), section.getDistance());
+        }
+    }
+
+    private void insertDownStationBetween(Section section) {
+        if (sections.get(getLastIndexSections()).getDownStation().equals(section.getDownStation())) {
+            sections.get(getLastIndexSections()).changeUpStationAndDistance(section.getUpStation(), section.getDistance());
+        }
+    }
+
+    private void isExistSection(Section addSection) {
+        sections.stream()
+            .filter(section -> addSection.isEqualsUpStationAndDownStation(section))
+            .findFirst()
+            .ifPresent(section -> {
+                throw new SectionException("상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없습니다.");
+            });
+    }
+
+    private void isExitsUpStationOrDownStation(Section section) {
+        Section upSection = sections.get(0);
+        Section downSection = sections.get(getLastIndexSections());
+
+        if(!upSection.getUpStation().equals(section.getUpStation())
+            && !upSection.getUpStation().equals(section.getDownStation())
+            && !downSection.getDownStation().equals(section.getUpStation())
+            && !downSection.getDownStation().equals(section.getDownStation())) {
+            throw new SectionException("상행역과 하행역 둘 중 하나도 포함되어 있지 않으면 추가할 수 없습니다.");
+        }
+    }
+
     public List<Station> getStations() {
+        if (sections.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<Station> stations = new ArrayList<>();
+        Station downStation = findFirstUpStation();
+        stations.add(downStation);
+
+        while (sections.size() + 1 != stations.size()) {
+            downStation = findNextLineStation(downStation).getDownStation();
+            stations.add(downStation);
+        }
+
+        return Collections.unmodifiableList(stations);
+    }
+
+    private Station findFirstUpStation() {
         return sections.stream()
-            .map(Section::getStations)
-            .flatMap(List::stream)
-            .distinct()
-            .collect(Collectors.toList());
+            .map(Section::getUpStation)
+            .filter(this::isStartStation)
+            .findFirst()
+            .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private Section findNextLineStation(Station finalDownStation) {
+        return sections.stream()
+            .filter(it -> finalDownStation.equals(it.getUpStation()))
+            .findFirst()
+            .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private boolean isStartStation(Station station) {
+        return sections.stream()
+            .noneMatch(currentStation -> station.equals(currentStation.getDownStation()));
     }
 
     public void removeStation(Station station) {
