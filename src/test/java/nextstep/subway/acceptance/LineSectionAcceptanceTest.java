80a3b711ebff2ccb2e58cdc98d32370b1594089b
wlroh <wlroh.paul@gmail.com>
2022-07-23 21:14:40 +0900

    test: 하행 종점에 새로운 구간 등록 인수 테스트 추가

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -87,6 +87,23 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
+     * When 노선의 하행 종점역에 새로운 구간을 추가하면
+     * Then 노선의 하행 종점역에 새로운 구간을 추가하면
+     */
+    @DisplayName("하행 종점에 새로운 구간을 등록하면 구간이 추가된다.")
+    @Test
+    void addLineSection_down() {
+        // when
+        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역, 정자역);
+    }
+
+    /**
      * Given 지하철 노선에 새로운 구간 추가를 요청 하고
      * When 지하철 노선의 마지막 구간 제거를 요청 하면
      * Then 노선에 구간이 제거된다
