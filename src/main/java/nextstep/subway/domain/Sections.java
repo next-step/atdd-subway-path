e52feddf00fcd33b3e0f041018fe67de8cbbd7c5
uram <parkuram12@gmail.com>
2022-07-23 19:33:37 +0900

    fix: Sections 구간이 하나만 등록되어있어도 제거되는 문제 수정

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -9,7 +9,10 @@ import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Embeddable
@@ -30,8 +33,11 @@ public class Sections {
         if (values.isEmpty()) {
             return;
         }
-        if (hasSection(section)) {
+        if (existAllStation(section)) {
             throw new IllegalArgumentException("상행역과 하행역이 모두 등록 되어있는 구간은 등록할 수 없습니다.");
+        }
+        if (!hasStation(section)) {
+            throw new IllegalArgumentException("상행역이나 하행역 중 하나는 등록된 구간에 포함되어야 합니다.");
         }
     }
 
@@ -53,7 +59,7 @@ public class Sections {
     }
 
     private void ensureCanRemove(Station station) {
-        if (values.size() < MIN_SIZE) {
+        if (values.size() <= MIN_SIZE) {
             throw new IllegalArgumentException("두개 이상의 구간이 등록되어야 제거가 가능합니다.");
         }
         if (!downStationTerminus().equals(station)) {
@@ -61,8 +67,25 @@ public class Sections {
         }
     }
 
-    private boolean hasSection(Section section) {
-        return values.stream().anyMatch(s -> s.equals(section));
+    private boolean hasStation(Section section) {
+        return findAllStation().stream()
+                .anyMatch(
+                        s1 -> section.getStations()
+                                     .stream()
+                                     .anyMatch(s1::equals)
+                );
+    }
+
+   private boolean existAllStation(Section section) {
+        return new HashSet<>(findAllStation()).containsAll(section.getStations());
+   }
+
+    public List<Station> findAllStation() {
+        return values.stream()
+                     .map(Section::getStations)
+                     .flatMap(Collection::stream)
+                     .distinct()
+                     .collect(Collectors.toList());
     }
 
 }
