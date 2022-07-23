ca558b79ed33a1e45be6afb494213147e6939dd0
giyeon95 <giyeon15@gmail.com>
2022-07-23 23:47:59 +0900

    test: 경로 조회 기능 실패 인수테스트 작성

---
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -0,0 +1,21 @@
+package nextstep.subway.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+public class PathSteps {
+
+    public static ExtractableResponse<Response> 지하철_경로_최단_거리_조회_요청(Long source, Long target) {
+        return RestAssured.given().log().all()
+                .params(Map.of(
+                        "source", source,
+                        "target", target
+                ))
+                .when().get("/paths")
+                .then().log().all()
+                .extract();
+    }
+}
