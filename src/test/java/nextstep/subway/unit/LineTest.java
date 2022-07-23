8a26852ebb6a8e011cb672d6870472fb51d30775
exemeedys <exemeedys@gmail.com>
2022-07-23 18:10:26 +0900

    구간 단위 테스트

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,17 +1,111 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LineTest {
+
+    @DisplayName("노선에 구간 등록하기")
     @Test
     void addSection() {
+
+        //given
+        final String 강남역_이름 = "강남역_이름";
+        final Station 강남역 = new Station(강남역_이름);
+
+        final String 시청역_이름 = "시청역_이름";
+        final Station 시청역 = new Station(시청역_이름);
+
+        final String 신분당선_이름 = "신분당선_이름";
+        final String red = "red";
+        final Line 신분당선 = new Line(신분당선_이름, red);
+
+        final int distance = 10;
+
+        //when
+        신분당선.getSections().add(new Section(신분당선, 강남역, 시청역, distance));
+
+        //then
+        assertThat(신분당선.getSections().size()).isEqualTo(1);
     }
 
+    @DisplayName("노선의 모든 지하철역 조회")
     @Test
     void getStations() {
+
+        //given
+        final String 강남역_이름 = "강남역_이름";
+        final Station 강남역 = new Station(강남역_이름);
+
+        final String 시청역_이름 = "시청역_이름";
+        final Station 시청역 = new Station(시청역_이름);
+
+        final String 구로디지털단지역_이름 = "구로디지털단지역_이름";
+        final Station 구로디지털단지역 = new Station(구로디지털단지역_이름);
+
+        final String 신분당선_이름 = "신분당선_이름";
+        final String red = "red";
+        final Line 신분당선 = new Line(신분당선_이름, red);
+
+        final int 첫번째구간_거리 = 10;
+        final int 두번째구간_거리 = 5;
+
+        Section 첫번째구간 = new Section(신분당선, 강남역, 시청역, 첫번째구간_거리);
+        Section 두번째구간 = new Section(신분당선, 시청역, 구로디지털단지역, 두번째구간_거리);
+
+        신분당선.getSections().add(첫번째구간);
+        신분당선.getSections().add(두번째구간);
+
+        //when
+        List<Station> 지하철역리스트 = 신분당선.getSections().stream()
+                .map(Section::getDownStation)
+                .collect(Collectors.toList());
+        지하철역리스트.add(0, 신분당선.getSections().get(0).getUpStation());
+
+        //then
+        assertThat(지하철역리스트).containsExactly(강남역, 시청역, 구로디지털단지역);
     }
 
+    @DisplayName("노선의 구간 제거하기")
     @Test
     void removeSection() {
+
+        //given
+        final String 강남역_이름 = "강남역_이름";
+        final Station 강남역 = new Station(강남역_이름);
+
+        final String 시청역_이름 = "시청역_이름";
+        final Station 시청역 = new Station(시청역_이름);
+
+        final String 구로디지털단지역_이름 = "구로디지털단지역_이름";
+        final Station 구로디지털단지역 = new Station(구로디지털단지역_이름);
+
+        final String 신분당선_이름 = "신분당선_이름";
+        final String red = "red";
+        final Line 신분당선 = new Line(신분당선_이름, red);
+
+        final int 첫번째구간_거리 = 10;
+        final int 두번째구간_거리 = 5;
+
+        Section 첫번째구간 = new Section(신분당선, 강남역, 시청역, 첫번째구간_거리);
+        Section 두번째구간 = new Section(신분당선, 시청역, 구로디지털단지역, 두번째구간_거리);
+
+        신분당선.getSections().add(첫번째구간);
+        신분당선.getSections().add(두번째구간);
+
+        //when
+        신분당선.getSections().remove(신분당선.getSections().size() - 1);
+
+        //then
+        assertThat(신분당선.getSections().size()).isEqualTo(1);
     }
 }
