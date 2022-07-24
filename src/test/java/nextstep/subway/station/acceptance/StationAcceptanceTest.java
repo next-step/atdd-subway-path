52bcce7e204e6c36943ff8d6aba3e4169d0eaf6e
imcool2551 <imcool2551@gmail.com>
2022-07-24 22:02:50 +0900

    test: StationAcceptanceTest
    요구사항에는 명시되어 있지 않지만 구간에 속한 지하철역을 검사없이 제거할 경우 애플리케이션 상태가 이상해질 것 같아서 ATDD 사이클로 해당 기능을 추가했습니다.

--- src/test/java/nextstep/subway/station/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/acceptance/StationAcceptanceTest.java
@@ -1,13 +1,19 @@
 package nextstep.subway.station.acceptance;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static nextstep.subway.station.acceptance.StationSteps.*;
+import static nextstep.subway.steps.LineSectionSteps.지하철_노선_생성_요청;
+import static nextstep.subway.steps.LineSectionSteps.지하철_노선에_지하철_구간_생성_요청;
+import static nextstep.subway.steps.StationSteps.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
@@ -64,6 +70,33 @@ class StationAcceptanceTest extends AcceptanceTest {
         지하철역이_존재하지_않는다("강남역");
     }
 
+    /**
+     * Given 지하철역을 생성하고
+     * When 지하철역이 속한 구간이 존재하면
+     * Then 지하철역을 제거할 수 없다
+     */
+    @DisplayName("지하철역이 속한 구간이 존재하면 지하철역을 제거할 수 없다")
+    @Test
+    void deleteStation_Exception() {
+        // given
+        Long 신분당선 = 지하철_노선_생성_요청("신분당선", "red").jsonPath().getLong("id");
+        Long 강남역 = 지하철역_생성_요청("강남역").jsonPath().getLong("id");
+        Long 교대역 = 지하철역_생성_요청("교대역").jsonPath().getLong("id");
+
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 교대역));
+
+        // when + then
+        지하철역_제거에_실패한다(강남역);
+    }
+
+    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId) {
+        Map<String, String> params = new HashMap<>();
+        params.put("upStationId", upStationId + "");
+        params.put("downStationId", downStationId + "");
+        params.put("distance", 6 + "");
+        return params;
+    }
+
     private void 지하철역이_존재한다(String... names) {
         List<String> stationNames = 지하철역_목록_조회_요청().jsonPath().getList("name", String.class);
 
@@ -74,5 +107,11 @@ class StationAcceptanceTest extends AcceptanceTest {
         List<String> stationNames = 지하철역_목록_조회_요청().jsonPath().getList("name", String.class);
 
         assertThat(stationNames).doesNotContain(names);
+    }
+
+    private void 지하철역_제거에_실패한다(Long stationId) {
+        ExtractableResponse<Response> response = 지하철역_삭제_요청(stationId);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
