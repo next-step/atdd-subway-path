a58799f91cfc5260ee6dc5442b782984431bf372
imcool2551 <imcool2551@gmail.com>
2022-07-24 21:56:03 +0900

    feat: Section, Sections
    구간 추가 요구사항을 구현했습니다

--- src/main/java/nextstep/subway/line/domain/Section.java
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -1,10 +1,12 @@
 package nextstep.subway.line.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import javax.persistence.*;
 
 @Getter
+@EqualsAndHashCode
 @Entity
 public class Section {
     @Id
@@ -31,15 +33,50 @@ public class Section {
         this.distance = distance;
     }
 
-    public boolean isDownStation(Long stationId) {
-        return this.downStationId.equals(stationId);
+    public Section subtract(Section anotherSection) {
+        int subtractedDistance = this.distance - anotherSection.distance;
+        assert subtractedDistance > 0;
+
+        if (startsTogether(anotherSection)) {
+            return new Section(line, anotherSection.getDownStationId(), downStationId, subtractedDistance);
+        }
+
+        if (endsTogether(anotherSection)) {
+            return new Section(line, upStationId, anotherSection.getUpStationId(), subtractedDistance);
+        }
+
+        throw new IllegalArgumentException("상행역이나 하행역이 겹치는 구간끼리만 뺄 수 있습니다.");
     }
 
-    public boolean includes(Section section) {
-        return downStationId.equals(section.downStationId) || upStationId.equals(section.upStationId);
+    public boolean startsOrEndsTogether(Section anotherSection) {
+        return startsTogether(anotherSection) || endsTogether(anotherSection);
     }
 
-    public boolean isLonger(Section section) {
-        return distance > section.distance;
+    private boolean startsTogether(Section anotherSection) {
+        return upStationId.equals(anotherSection.upStationId);
+    }
+
+    private boolean endsTogether(Section anotherSection) {
+        return downStationId.equals(anotherSection.downStationId);
+    }
+
+    public boolean isShorterThanOrEqualWith(Section anotherSection) {
+        return distance <= anotherSection.distance;
+    }
+
+    public boolean matchUpStation(Long stationId) {
+        return upStationId.equals(stationId);
+    }
+
+    public boolean matchDownStation(Long stationId) {
+        return downStationId.equals(stationId);
+    }
+
+    public boolean isBefore(Section anotherSection) {
+        return downStationId.equals(anotherSection.upStationId);
+    }
+
+    public boolean isAfter(Section anotherSection) {
+        return upStationId.equals(anotherSection.downStationId);
     }
 }
--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -7,8 +7,8 @@ import nextstep.subway.line.domain.exception.CannotDeleteSectionException;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static javax.persistence.CascadeType.ALL;
@@ -20,19 +20,49 @@ public class Sections {
     @OneToMany(mappedBy = "line", cascade = ALL, orphanRemoval = true)
     private List<Section> sections = new ArrayList<>();
 
-    public void add(Section section) {
-        Optional<Section> includingSectionOpt = sections.stream()
-                .filter(s -> s.includes(section))
-                .findAny();
-
-        if (includingSectionOpt.isPresent()) {
-            Section includingSection = includingSectionOpt.get();
-            if (!includingSection.isLonger(section)) {
-                throw new CannotAddSectionException("기존 구간 사이에 추가할 구간의 길이가 기존 구간의 길이보다 크거나 같을 수 없습니다.");
-            }
+    public void add(Section addedSection) {
+        if (sections.isEmpty()) {
+            sections.add(addedSection);
+            return;
         }
 
-        sections.add(section);
+        validateAddedSection(addedSection);
+
+        if (firstSection().isAfter(addedSection) || lastSection().isBefore(addedSection)) {
+            sections.add(addedSection);
+            return;
+        }
+
+        Section includingSection = sections.stream()
+                .filter(s -> s.startsOrEndsTogether(addedSection))
+                .findAny()
+                .orElseThrow(() -> new CannotAddSectionException("추가할 수 없는 구간입니다."));
+
+        sections.remove(includingSection);
+        sections.add(addedSection);
+        sections.add(includingSection.subtract(addedSection));
+    }
+
+    private void validateAddedSection(Section addedSection) {
+        List<Long> stationIds = stationIds();
+
+        boolean containsUpStation = stationIds.contains(addedSection.getUpStationId());
+        boolean containsDownStation = stationIds.contains(addedSection.getDownStationId());
+
+        if (containsUpStation && containsDownStation) {
+            throw new CannotAddSectionException("상행역과 하행역이 이미 노선에 모두 등록되어 있으면 구간을 추가할 수 없습니다.");
+        }
+
+        if (!containsUpStation && !containsDownStation) {
+            throw new CannotAddSectionException("상행역과 하행역 둘 중 하나도 포함되어있지 않으면 구간을 추가할 수 없습니다.");
+        }
+
+        sections.stream()
+                .filter(s -> s.startsOrEndsTogether(addedSection) && s.isShorterThanOrEqualWith(addedSection))
+                .findAny()
+                .ifPresent(s -> {
+                    throw new CannotAddSectionException("기존 구간 사이에 추가할 구간의 길이가 기존 구간의 길이보다 크거나 같을 수 없습니다.");
+                });
     }
 
     public void removeSection(Long stationId) {
@@ -41,26 +71,64 @@ public class Sections {
         }
 
         Section lastSection = lastSection();
-        if (!lastSection.isDownStation(stationId)) {
+        if (!lastSection.matchDownStation(stationId)) {
             throw new CannotDeleteSectionException("노선의 종점만 삭제할 수 있습니다.");
         }
         sections.remove(lastSection);
     }
 
-    private Section lastSection() {
-        return sections.get(sections.size() - 1);
-    }
-
     public List<Long> stationIds() {
-        List<Long> stationIds = sections.stream()
-                .map(Section::getDownStationId)
-                .collect(Collectors.toList());
+        if (sections.isEmpty()) {
+            return Collections.emptyList();
+        }
 
-        stationIds.add(0, firstSection().getUpStationId());
+        List<Long> stationIds = new ArrayList<>();
+        Section iter = firstSection();
+
+        while (!iter.equals(lastSection())) {
+            stationIds.add(iter.getUpStationId());
+            iter = findByUpStationId(iter.getDownStationId());
+        }
+
+        stationIds.add(iter.getUpStationId());
+        stationIds.add(iter.getDownStationId());
         return stationIds;
     }
 
+    private Section findByUpStationId(Long stationId) {
+        return sections.stream()
+                .filter(s -> s.matchUpStation(stationId))
+                .findAny()
+                .orElseThrow();
+    }
+
     private Section firstSection() {
-        return sections.get(0);
+        return sections.stream()
+                .filter(this::isFirstSection)
+                .findAny()
+                .orElseThrow();
+    }
+
+    private Section lastSection() {
+        return sections.stream()
+                .filter(this::isLastSection)
+                .findAny()
+                .orElseThrow();
+    }
+
+    private boolean isFirstSection(Section section) {
+        List<Long> downStationIds = sections.stream()
+                .map(Section::getDownStationId)
+                .collect(Collectors.toList());
+
+        return !downStationIds.contains(section.getUpStationId());
+    }
+
+    private boolean isLastSection(Section section) {
+        List<Long> upStationIds = sections.stream()
+                .map(Section::getUpStationId)
+                .collect(Collectors.toList());
+
+        return !upStationIds.contains(section.getDownStationId());
     }
 }
