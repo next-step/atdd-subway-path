b3e3336f02145104eea6907a35af63bdcb86a321
rladowl92 <rladowl92@naver.com>
2022-07-24 16:14:02 +0900

    feat(section): 구간 추가 변경사항 반영

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -3,6 +3,7 @@ package nextstep.subway.domain;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
@@ -17,7 +18,56 @@ public class Sections {
 	private final List<Section> sections = new ArrayList<>();
 
 	public void add(Section section) {
-		this.sections.add(section);
+		validate(section);
+
+		if(isLastSection(section)) {
+			this.sections.add(section);
+			return;
+		}
+
+		if(isFirstSection(section)) {
+			this.sections.add(0, section);
+			return;
+		}
+
+		if(isMiddleSection(section)) {
+			int index = IntStream.range(0, sections.size())
+					.filter(i-> sections.get(i).getDownStation().equals(section.getUpStation()))
+					.findFirst()
+					.orElse(0);
+			if(this.sections.get(index).getDistance() <= section.getDistance()) {
+				throw new IllegalArgumentException();
+			}
+			this.sections.add(index, section);
+		}
+	}
+
+	private void validate(Section section) {
+		if(this.sections.isEmpty()){
+			return;
+		}
+
+		// 상행과 하행이 모두 새로운 역이면 에러
+		if(!getStations().contains(section.getUpStation()) && !getStations().contains(section.getDownStation())) {
+			throw new IllegalArgumentException();
+		}
+
+		// 상행과 하행이 모두 등록되어 있으면 에러
+		if(getStations().contains(section.getUpStation()) && getStations().contains(section.getDownStation())) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private boolean isLastSection(Section section) {
+		return this.sections.isEmpty() || lastStation() == section.getUpStation();
+	}
+
+	private boolean isFirstSection(Section section) {
+		return firstStation() == section.getDownStation();
+	}
+
+	private boolean isMiddleSection(Section section) {
+		return getStations().contains(section.getUpStation());
 	}
 
 	public List<Station> getStations() {
@@ -41,5 +91,13 @@ public class Sections {
 
 	public Boolean isEmpty() {
 		return this.sections.isEmpty();
+	}
+
+	private Station firstStation() {
+		return this.sections.get(0).getUpStation();
+	}
+
+	private Station lastStation() {
+		return this.sections.get(this.sections.size() - 1).getDownStation();
 	}
 }
--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,11 +1,13 @@
 package nextstep.subway.unit;
 
+import static nextstep.subway.acceptance.LineSteps.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import nextstep.subway.domain.Line;
@@ -30,13 +32,6 @@ class LineTest {
 		신분당선.addSection(section);
 	}
 
-    @DisplayName("구간 추가")
-	@Test
-	void addSection() {
-		// add section -> setUp()
-		assertThat(신분당선.getStations()).containsExactly(강남역, 양재역);
-	}
-
     @DisplayName("라인에 해당하는 지하철역 조회")
 	@Test
 	void getStations() {
@@ -45,6 +40,109 @@ class LineTest {
 		System.out.println(stations.toString());
 		assertThat(stations).hasSize(2);
 		assertThat(stations).containsExactly(강남역, 양재역);
+	}
+
+	@DisplayName("구간 추가")
+	@Nested
+	class addSection {
+		/**
+		 * Given 새로운 역을 생성하고
+		 * When 새로운 역을 하행 종점으로 구간 추가를 요청 하면
+		 * Then 노선에 새로운 역이 하행 종점으로 등록된다.
+		 */
+		@DisplayName("새로운 역을 하행 종점으로 등록할 경우")
+		@Test
+		void addSection_성공1() {
+			// given
+			Station 정자역 = new Station("정자역");
+
+			// when
+			신분당선.addSection(new Section(신분당선, 양재역, 정자역, 10));
+
+			// then
+			assertThat(신분당선.getStations()).containsExactly(강남역, 양재역, 정자역);
+		}
+
+		/**
+		 * Given 새로운 역을 생성하고
+		 * When 새로운 역을 상행 종점으로 구간 추가를 요청 하면
+		 * Then 노선에 새로운 역이 상행 종점으로 등록된다.
+		 */
+		@DisplayName("새로운 역을 상행 종점으로 등록할 경우")
+		@Test
+		void addSection_성공2() {
+			// given
+			Station 정자역 = new Station("정자역");
+
+			// when
+			신분당선.addSection(new Section(신분당선, 정자역, 강남역, 10));
+
+			// then
+			assertThat(신분당선.getStations()).containsExactly(정자역, 강남역, 양재역);
+		}
+
+		/**
+		 * Given 새로운 역을 생성하고
+		 * When 새로운 역을 중간역으로 구간 추가를 요청 하면
+		 * Then 노선 중간에 새로운 역이 등록된다.
+		 */
+		@DisplayName("역 사이에 새로운 역을 등록할 경우")
+		@Test
+		void addSection_성공3() {
+			// given
+			Station 정자역 = new Station("정자역");
+
+			// when
+			신분당선.addSection(new Section(신분당선, 강남역, 정자역, 7));
+
+			// then
+			assertThat(신분당선.getStations()).containsExactly(강남역, 정자역, 양재역);
+		}
+
+		/**
+		 * When 기존 역을 새로운 구간으로 추가하면
+		 * Then 에러가 발생한다.
+		 */
+		@DisplayName("예외 1) 상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음")
+		@Test
+		void addSection_실패1() {
+			// when AND then
+			assertThatThrownBy(()-> 신분당선.addSection(new Section(신분당선, 강남역, 양재역, 10)))
+				.isInstanceOf(IllegalArgumentException.class);
+		}
+
+		/**
+		 * Given 새로운 역을 2개 생성하고
+		 * When 새로운 역으로만 구성된 구간을 추가하면
+		 * Then 에러가 발생한다.
+		 */
+		@DisplayName("예외 2) 상행역과 하행역 둘 중 하나도 포함되어있지 않으면 추가할 수 없음")
+		@Test
+		void addSection_실패2() {
+			// given
+			Station 정자역 = new Station("정자역");
+			Station 판교역 = new Station("판교역");
+
+			// when AND then
+			assertThatThrownBy(()-> 신분당선.addSection(new Section(신분당선, 정자역, 판교역, 10)))
+				.isInstanceOf(IllegalArgumentException.class);
+		}
+
+		/**
+		 * Given 새로운 역을 생성하고
+		 * When 기존 구간보다 큰 구간을 사이에 등록하면
+		 * Then 에러가 발생한다.
+		 */
+		@DisplayName("예외 3) 역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없음")
+		@Test
+		void addSection_실패3() {
+			// given
+			Station 정자역 = new Station("정자역");
+
+			// when AND then
+			assertThatThrownBy(()-> 신분당선.addSection(new Section(신분당선, 강남역, 정자역, 20)))
+				.isInstanceOf(IllegalArgumentException.class);
+		}
 	}
 
     @DisplayName("구간 제거")
