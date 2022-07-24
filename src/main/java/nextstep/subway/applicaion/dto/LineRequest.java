67032b029d3cc32db7400d806ebb6a9d18dba369
choiseon <com1368@naver.com>
2022-07-24 19:24:55 +0900

    test : LineServiceTest 테스트 코드 구현

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -1,11 +1,24 @@
 package nextstep.subway.applicaion.dto;
 
+import lombok.Builder;
+
 public class LineRequest {
     private String name;
     private String color;
     private Long upStationId;
     private Long downStationId;
     private int distance;
+
+    public LineRequest() {}
+
+    @Builder
+    public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
 
     public String getName() {
         return name;
