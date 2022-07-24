208206037cf0aba83afd44f7fce1390b5ff1b011
김민수 <minsu@sk.com>
2022-07-24 12:24:16 +0900

    refactor : 중복소스 분리

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -133,10 +133,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 새로운역_정자역, 7));
 
         // then
-        assertAll(
-                () -> assertThat(response.jsonPath().getString("message")).contains("역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없음"),
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
-        );
+        BADREQUEST_실패케이스_검증("역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없음", response);
     }
 
     @DisplayName("실패케이스2 - 상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음")
@@ -148,10 +145,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 양재역, DEFAULT_SECTION_DISTANCE));
 
         // then
-        assertAll(
-                () -> assertThat(response.jsonPath().getString("message")).contains("상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음"),
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
-        );
+        BADREQUEST_실패케이스_검증("상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음", response);
     }
 
     @DisplayName("실패케이스3 - 상행역과 하행역 둘 중 하나도 포함되어있지 않으면 추가할 수 없음")
@@ -164,10 +158,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(상행역, 하행역, DEFAULT_SECTION_DISTANCE));
 
         // then
-        assertAll(
-                () -> assertThat(response.jsonPath().getString("message")).contains("상행역과 하행역 둘 중 하나도 포함되어있지 않으면 추가할 수 없음"),
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
-        );
+        BADREQUEST_실패케이스_검증("상행역과 하행역 둘 중 하나도 포함되어있지 않으면 추가할 수 없음", response);
     }
 
     /**
