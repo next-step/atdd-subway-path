aacb58bff460d544282010eeab6eb32c224a8d9f
DahamLeee <dlek1011@gmail.com>
2022-07-24 21:57:53 +0900

    refactor: combine controller advice

--- src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
+++ src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package nextstep.subway.ui;
 
+import nextstep.subway.exception.AddSectionException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,5 +11,10 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Void> handleIllegalArgsException(DataIntegrityViolationException e) {
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(AddSectionException.class)
+    public ResponseEntity<ErrorResult> addSectionException(AddSectionException e) {
+        return ResponseEntity.badRequest().body(new ErrorResult(e.getMessage()));
     }
 }
