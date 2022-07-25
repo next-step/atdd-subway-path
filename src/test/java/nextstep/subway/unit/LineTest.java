6b8fa20d41cf45a0776d577102a6e37479575539
asdfg5415 <asdfg5415@gmail.com>
2022-07-25 17:56:39 +0900

    test: LineTest-addSection

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,10 +1,22 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LineTest {
+
     @Test
     void addSection() {
+        Line line = new Line();
+        Station upStation = new Station("강남역");
+        Station downStation = new Station("역삼역");
+
+        line.addSection(upStation, downStation, 10);
+
+        assertThat(line.getSections()).isNotEmpty();
     }
 
     @Test
