b6eef093d83b3d2f71e4dc2e18b7531ad2e2458c
DahamLeee <dlek1011@gmail.com>
2022-07-25 10:35:25 +0900

    test: Service Layer Unit Test without Mock

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -45,10 +45,12 @@ public class LineServiceTest {
         // then
         // line.getSections 메서드를 통해 검증
         List<Section> 신분당선_구간 = 신분당선.getSections();
+        List<Station> 신분당선_역 = 신분당선.allStations();
+
         assertAll(
                 () -> assertThat(신분당선_구간).hasSize(1),
-                () -> assertThat(신분당선_구간.get(0)).extracting("upStation").isEqualTo(ConstStation.강남역),
-                () -> assertThat(신분당선_구간.get(0)).extracting("downStation").isEqualTo(ConstStation.신논현역)
+                () -> assertThat(신분당선_역).hasSize(2),
+                () -> assertThat(신분당선_역).extracting("id").containsExactly(강남역, 신논현역)
         );
     }
 
@@ -79,6 +81,34 @@ public class LineServiceTest {
                 () -> assertThat(신분당선_역).hasSize(5),
                 () -> assertThat(신분당선_역).extracting("name")
                         .containsExactly("판교역", "강남역", "정자역", "신논현역", "이매역")
+        );
+    }
+
+    @Test
+    @DisplayName("중간 구간 제거")
+    void removeMiddleSection() {
+        Long 강남역 = stationRepository.save(ConstStation.강남역).getId();
+        Long 신논현역 = stationRepository.save(ConstStation.신논현역).getId();
+        Long 정자역 = stationRepository.save(ConstStation.정자역).getId();
+        Line 신분당선 = lineRepository.save(Line.of(NEW_BUN_DANG, BG_RED_600));
+
+        lineService.addSection(신분당선.getId(), SectionRequest.of(강남역, 신논현역, 10));
+        lineService.addSection(신분당선.getId(), SectionRequest.of(신논현역, 정자역, 5));
+
+        // when
+        // removeSection 호출
+        lineService.deleteSection(신분당선.getId(), 신논현역);
+
+        // then
+        // line.getSections, line.allStation 메서드를 통해 검증
+        List<Section> sections = 신분당선.getSections();
+        List<Station> stations = 신분당선.allStations();
+
+        assertAll(
+                () -> assertThat(sections).hasSize(1),
+                () -> assertThat(sections).extracting("distance").containsExactly(15),
+                () -> assertThat(stations).hasSize(2),
+                () -> assertThat(stations).extracting("id").containsExactly(강남역, 정자역)
         );
     }
 }
