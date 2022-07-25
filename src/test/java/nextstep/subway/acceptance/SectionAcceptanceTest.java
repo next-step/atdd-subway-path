790a16237627f7d0d8d91cf9a27fe60dc482d20e
DahamLeee <dlek1011@gmail.com>
2022-07-25 10:15:33 +0900

    test: [ATDD] remove middle section acceptance test

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -138,19 +138,22 @@ class SectionAcceptanceTest extends AcceptanceTest {
     /**
      * Given 지하철 노선에 새로운 구간 추가를 요청하고
      * When 지하철 노선의 중간 구간 제거를 요청 하면
-     * Then 노선에 구간이 제거되고
-     * Then 노선의 길이는 합산된다
+     * Then 노선에 구간이 제거된다
      */
     @DisplayName("지하철 노선에 중간 구간을 제거")
     @Test
     void removeLineMiddleSection() {
         // given
+        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
 
         // when
+        지하철_노선에_지하철_구간_제거_요청(신분당선, 양재역);
 
         // then
-
-        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 정자역);
     }
 
     /**
