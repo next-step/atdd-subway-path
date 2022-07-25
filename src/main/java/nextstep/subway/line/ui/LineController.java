dc251071993509016ef2370dbb2834e540ac6166
imcool2551 <imcool2551@gmail.com>
2022-07-25 23:48:36 +0900

    fix: 잘못된 메서드명 변경

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -40,7 +40,7 @@ public class LineController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> updateLine(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
         lineService.deleteLine(id);
         return ResponseEntity.noContent().build();
     }
