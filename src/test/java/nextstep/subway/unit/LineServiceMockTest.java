2398dcfb61e70b3c876e6ea78d1bc221288600ac
asdfg5415 <asdfg5415@gmail.com>
2022-07-26 01:32:57 +0900

    test: LineServiceMockTest-addSection

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,11 +1,21 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
@@ -13,16 +23,28 @@ public class LineServiceMockTest {
     private LineRepository lineRepository;
     @Mock
     private StationService stationService;
+    @InjectMocks
+    private LineService lineService;
 
     @Test
     void addSection() {
         // given
-        // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        doReturn(new Station("강남역"))
+                .when(stationService)
+                .findById(1L);
+        doReturn(new Station("역삼역"))
+                .when(stationService)
+                .findById(2L);
+
+        Line line = new Line("2호선", "빨간색");
+        doReturn(Optional.of(line))
+                .when(lineRepository)
+                .findById(1L);
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(1L, new SectionRequest(1L, 2L, 10));
 
         // then
-        // line.findLineById 메서드를 통해 검증
+        assertThat(line.getSections()).isNotEmpty();
     }
 }
