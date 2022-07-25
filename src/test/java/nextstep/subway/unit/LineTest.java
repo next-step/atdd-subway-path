65be2d4f6c5223c79d6b2ce4438208ee3bce145d
asdfg5415 <asdfg5415@gmail.com>
2022-07-25 18:08:01 +0900

    test: LineTest-getStations

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -4,6 +4,8 @@ import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
@@ -21,6 +23,14 @@ class LineTest {
 
     @Test
     void getStations() {
+        Line line = new Line();
+        Station upStation = new Station("강남역");
+        Station downStation = new Station("역삼역");
+        line.addSection(upStation, downStation, 10);
+
+        List<Station> stations = line.getStations();
+
+        assertThat(stations).containsExactly(upStation, downStation);
     }
 
     @Test
