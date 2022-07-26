f96424e4890cb8c69acfcea52a8515f1bc78ea06
charlie.k <charlie.k@kakaocorp.com>
2022-07-26 09:00:04 +0900

    test: LineServiceTest 작성 완료

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 지하철 노선도 미션
+
 [ATDD 강의](https://edu.nextstep.camp/c/R89PYi5H) 실습을 위한 지하철 노선도 애플리케이션
 
 ## 🚀 1단계 - 단위 테스트 작성
@@ -6,8 +7,9 @@
 ### 요구사항
 
 ### 기능 요구사항
+
 - [x] 지하철 구간 관련 단위 테스트를 완성하세요.
     - [x] 구간 단위 테스트 (LineTest)
     - [x] 구간 서비스 단위 테스트 with Mock (LineServiceMockTest)
-    - [ ] 구간 서비스 단위 테스트 without Mock (LineServiceTest)
+    - [x] 구간 서비스 단위 테스트 without Mock (LineServiceTest)
     - [ ] 단위 테스트를 기반으로 비즈니스 로직을 리팩터링 하세요.
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,22 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.LineResponse;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.applicaion.dto.StationResponse;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 @Transactional
@@ -19,15 +29,70 @@ public class LineServiceTest {
     @Autowired
     private LineService lineService;
 
+    @DisplayName("지하철 구간을 등록합니다.")
     @Test
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 서울대입구역 = createStation("서울대입구역");
+        Station 낙성대역 = createStation("낙성대역");
+        Line 이호선 = createLine("이호선", "br-red-600");
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(이호선.getId(), new SectionRequest(서울대입구역.getId(), 낙성대역.getId(), 10));
 
         // then
         // line.getSections 메서드를 통해 검증
+        LineResponse 이호선_결과값 = lineService.findById(이호선.getId());
+        LineResponse 이호선_예상값 = createLineResponse(이호선, List.of(서울대입구역, 낙성대역));
+        Assertions.assertThat(이호선_결과값)
+                .isEqualTo(이호선_예상값);
+    }
+
+    @DisplayName("지하철 구간을 삭제합니다.")
+    @Test
+    void deleteSection() {
+        // Given
+        Station 서울대입구역 = createStation("서울대입구역");
+        Station 낙성대역 = createStation("낙성대역");
+        Station 사당역 = createStation("사당역");
+        Line 이호선 = createLine("이호선", "br-red-600");
+
+        lineService.addSection(이호선.getId(), new SectionRequest(서울대입구역.getId(), 낙성대역.getId(), 10));
+        lineService.addSection(이호선.getId(), new SectionRequest(낙성대역.getId(), 사당역.getId(), 10));
+
+        // When
+        lineService.deleteSection(이호선.getId(), 사당역.getId());
+
+        // Then
+        LineResponse 이호선_결과값 = lineService.findById(이호선.getId());
+        LineResponse 이호선_예상값 = createLineResponse(이호선, List.of(서울대입구역, 낙성대역));
+        Assertions.assertThat(이호선_결과값)
+                .isEqualTo(이호선_예상값);
+    }
+
+    private LineResponse createLineResponse(Line line, List<Station> stations) {
+        LineResponse lineResponse = new LineResponse(line.getId(), line.getName(), line.getColor(), createStationResponses(stations));
+        return lineResponse;
+    }
+
+    private List<StationResponse> createStationResponses(List<Station> stations) {
+        List<StationResponse> collect = stations.stream()
+                .map(it -> new StationResponse(it.getId(), it.getName()))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    private Station createStation(String stationName) {
+        Station station = new Station(stationName);
+        stationRepository.save(station);
+        return station;
+    }
+
+    private Line createLine(String name, String color) {
+        Line line = new Line(name, color);
+        lineRepository.save(line);
+        return line;
     }
 }
