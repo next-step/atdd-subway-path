a42b7b4906cb3565d11693da64c244605dbb76d1
chkim <cndghks0213@gmail.com>
2022-07-26 21:01:20 +0900

    feat: 커스텀 예외 메시지 추가

--- src/main/java/nextstep/subway/exception/NonExistentStationException.java
+++ src/main/java/nextstep/subway/exception/NonExistentStationException.java
@@ -1,4 +1,7 @@
 package nextstep.subway.exception;
 
 public class NonExistentStationException extends RuntimeException {
+    public NonExistentStationException() {
+        super("존재하지 않는 지하철역입니다.");
+    }
 }
