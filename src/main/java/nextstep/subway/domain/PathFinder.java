0cb14df5a83d1dc63599cd26914617eb325ceea8
DahamLeee <dlek1011@gmail.com>
2022-07-26 15:37:24 +0900

    feat: [CREATE] path finder class

--- src/main/java/nextstep/subway/domain/PathFinder.java
+++ src/main/java/nextstep/subway/domain/PathFinder.java
@@ -1,22 +1,44 @@
 package nextstep.subway.domain;
 
+import org.jgrapht.WeightedGraph;
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+import java.util.Collection;
+import java.util.List;
+
 public class PathFinder {
 
-    private Long source;
-    private Long target;
+    private WeightedGraph<Station, DefaultWeightedEdge> graph;
+    private ShortestPathAlgorithm<Station, DefaultWeightedEdge> pathAlgorithm;
 
-    private PathFinder() { }
-
-    private PathFinder(Long source, Long target) {
-        if (source.equals(target)) {
-            throw new IllegalArgumentException("출발역과 도착역이 같을 수는 없습니다.");
-        }
-
-        this.source = source;
-        this.target = target;
+    private PathFinder(WeightedGraph<Station, DefaultWeightedEdge> graph, ShortestPathAlgorithm<Station, DefaultWeightedEdge> pathAlgorithm, List<Line> lines) {
+        this.graph = graph;
+        this.pathAlgorithm = pathAlgorithm;
+        initGraph(lines);
     }
 
-    public static PathFinder of(Long source, Long target) {
-        return new PathFinder(source, target);
+    public static PathFinder of(WeightedGraph<Station, DefaultWeightedEdge> graph, ShortestPathAlgorithm<Station, DefaultWeightedEdge> pathAlgorithm, List<Line> lines) {
+        return new PathFinder(graph, pathAlgorithm, lines);
+    }
+
+    private void initGraph(List<Line> lines) {
+        addVertexToGraph(lines);
+        addEdgeToGraph(lines);
+    }
+
+    private void addVertexToGraph(List<Line> lines) {
+        lines.stream()
+                .map(Line::allStations)
+                .flatMap(Collection::stream)
+                .distinct()
+                .forEach(graph::addVertex);
+    }
+
+    private void addEdgeToGraph(List<Line> lines) {
+        lines.stream()
+                .map(Line::getSections)
+                .flatMap(Collection::stream)
+                .forEach(section -> graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance()));
     }
 }
