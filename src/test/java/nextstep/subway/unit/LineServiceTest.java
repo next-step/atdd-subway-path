c70d91f072f1f62ff502e70bc2b57112d141abd7
beadoer1 <beadoer1@gmail.com>
2022-07-26 21:43:20 +0900

    test: LineServiceTest addSection() 테스트 작성

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,14 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -23,11 +25,16 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station upStation = stationRepository.save(new Station("강남역"));
+        Station downStation = stationRepository.save(new Station("양재역"));
+        Line line = lineRepository.save(new Line("신분당선", "빨강"));
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(line.getId(), new SectionRequest(upStation.getId(), downStation.getId(), 10));
 
         // then
         // line.getSections 메서드를 통해 검증
+        assertThat(line.getSections()).contains(new Section(line, upStation, downStation, 10));
     }
 }
