fd7c95671023767e096211c4184b9deec2ae4703
younlll <youn.lee96@gmail.com>
2022-07-27 06:44:02 +0900

    feat: 변경된 삭제 요구사항 구현

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -2,6 +2,7 @@ package nextstep.subway.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
@@ -25,7 +26,7 @@ public class Sections {
         validateDuplicationSection(section);
         validateNotMatchedExistingSections(section);
 
-        if (isMatchedUpStation(section)) {
+        if (isMatchedSection(s -> s.isMatchedUpStation(section.getUpStation()))) {
             Section existingUpStation = getExistingUpStation(section);
             validateDistanceOfSection(existingUpStation.getDistance(), section.getDistance());
             existingUpStation.updateExistingUpStationAndDowStationSection(section);
@@ -37,10 +38,13 @@ public class Sections {
     private void validateNotMatchedExistingSections(Section section) {
         List<Station> stations = getStations();
         stations.stream()
-                .filter(station -> station.isMatched(section.getUpStation()) || station.isMatched(section.getDownStation()))
+                .filter(station -> station.isMatched(section.getUpStation()) || station.isMatched(
+                        section.getDownStation()))
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException(String.format("기존 구간과 등록할 역이 하나 이상 일치하지 않습니다.(상행역:[%s] 하행역:[%s])", section.getUpStation().getName(),
-                        section.getDownStation().getName())));
+                .orElseThrow(() -> new IllegalArgumentException(
+                        String.format("기존 구간과 등록할 역이 하나 이상 일치하지 않습니다.(상행역:[%s] 하행역:[%s])",
+                                section.getUpStation().getName(),
+                                section.getDownStation().getName())));
     }
 
     private void validateDuplicationSection(Section section) {
@@ -54,7 +58,8 @@ public class Sections {
     private void validateDistanceOfSection(int existingDistance, int newDistance) {
         if (existingDistance <= newDistance) {
             throw new IllegalArgumentException(
-                    String.format("기존 구간의 길이는 [%d]로 새로 등록할 구간의 길이는 기존 구간의 길이보다 작아야합니다.(신규 구간 길이:[%d])", existingDistance,
+                    String.format("기존 구간의 길이는 [%d]로 새로 등록할 구간의 길이는 기존 구간의 길이보다 작아야합니다.(신규 구간 길이:[%d])",
+                            existingDistance,
                             newDistance));
         }
     }
@@ -66,24 +71,10 @@ public class Sections {
                 .orElseThrow(IllegalArgumentException::new);
     }
 
-    private boolean isMatchedUpStation(Section section) {
-        return sections.stream()
-                .anyMatch(s -> s.getUpStation().isMatched(section.getUpStation()));
-    }
-
     private boolean isDuplicateSection(Section section) {
         return sections.stream()
                 .anyMatch(s -> s.getUpStation().isMatched(section.getUpStation()) && s.getDownStation()
                         .isMatched(section.getDownStation()));
-    }
-
-    private Section getDownEndStation() {
-        int downEndStationIndex = getDownEndStationIndex();
-        return sections.get(downEndStationIndex);
-    }
-
-    private int getDownEndStationIndex() {
-        return sections.size() - 1;
     }
 
     public List<Section> getSections() {
@@ -130,29 +121,80 @@ public class Sections {
 
     private Section getFirstSection() {
         return sections.stream()
-                .filter(section -> isUpStation(section.getUpStation()))
+                .filter(section -> isNoneMatchedSection(s -> s.isMatchedDownStation(section.getUpStation())))
                 .findFirst()
                 .orElseThrow(IllegalArgumentException::new);
     }
 
-    private boolean isUpStation(Station station) {
-        return sections.stream()
-                .noneMatch(section -> section.getDownStation().isMatched(station));
-    }
-
     public void deleteSection(Station station) {
-        Section downEndStation = getDownEndStation();
         if (sections.size() <= 1) {
             throw new IllegalArgumentException("지하철 노선이 상행 종점역과 하행 종점역만 있는 하나의 구간인 경우, 역을 삭제할 수 없습니다.");
         }
 
-        if (!downEndStation.isMatchedStationId(station)) {
-            throw new IllegalArgumentException("지하철 노선의 하행 종점역만 삭제 할 수 있습니다.");
-        }
-        removeDownEndSection();
+        removeStation(station);
     }
 
-    private void removeDownEndSection() {
-        sections.remove(getDownEndStationIndex());
+    private void removeStation(Station station) {
+        if (!isMatchedSection(s -> s.getUpStation().isMatched(station) || s.getDownStation().isMatched(station))) {
+            throw new IllegalArgumentException(String.format("해당 구간에 삭제를 요청한 %s이 존재하지 않습니다.", station.getName()));
+        }
+
+        if (isUpEndStation(station)) {
+            Section deleteSection = getUpStationInSections(station);
+            this.sections.remove(deleteSection);
+            return;
+        }
+
+        if (isDownEndStation(station)) {
+            Section deleteSection = getDownStationInSections(station);
+            this.sections.remove(deleteSection);
+            return;
+        }
+
+        Section deleteSection = getSection(s -> s.isMatchedUpStation(station));
+        Section modifiedSection = getSection(s -> s.isMatchedDownStation(station));
+        modifiedSection.updateMiddleSection(deleteSection);
+        this.sections.remove(deleteSection);
+    }
+
+    private Section getSection(Predicate<Section> condition) {
+        return sections.stream()
+                .filter(condition)
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private Section getDownStationInSections(Station station) {
+        return sections.stream()
+                .filter(s -> s.getDownStation().isMatched(station))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private boolean isMatchedSection(Predicate<Section> condition) {
+        return sections.stream()
+                .anyMatch(condition);
+    }
+
+    private boolean isNoneMatchedSection(Predicate<Section> condition) {
+        return sections.stream()
+                .noneMatch(condition);
+    }
+
+    private Section getUpStationInSections(Station station) {
+        return sections.stream()
+                .filter(s -> s.getUpStation().isMatched(station))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private boolean isUpEndStation(Station station) {
+        return isMatchedSection(s -> s.isMatchedUpStation(station)) && !isMatchedSection(
+                s -> s.isMatchedDownStation(station));
+    }
+
+    private boolean isDownEndStation(Station station) {
+        return isMatchedSection(s -> s.isMatchedDownStation(station)) && !isMatchedSection(
+                s -> s.isMatchedUpStation(station));
     }
 }
