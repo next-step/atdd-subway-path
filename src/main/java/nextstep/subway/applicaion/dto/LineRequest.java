c843c9d098f5f0ce1932fce3c39d9475c75df004
JangHyeonJun2 <epfvkdlxj@naver.com>
2022-07-26 21:42:18 +0900

    create: create addSection method in LineTest

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -27,3 +27,4 @@ public class LineRequest {
         return distance;
     }
 }
+
--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,11 +1,24 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 class LineTest {
     @Test
     void addSection() {
+        Station 당곡역_상행 = new Station("당곡역");
+        Station 보라매역_하행 = new Station("보라매역");
 
+        Line 신림선 = new Line("신림선", "blue-bg-300");
+        신림선.addSection(당곡역_상행, 보라매역_하행, 10);
+
+        assertThat(신림선.getSections())
+                .filteredOn(section -> section.equals(new Section(신림선, 당곡역_상행, 보라매역_하행, 10)));
     }
 
     @Test
