2ead8b98b534523b102fe3fe10e670bd44fd74d5
MoonHKLee <mf311110@gmail.com>
2022-07-26 23:19:34 +0900

    fix : 테스트코드 위반 로직 변경. 입력값 valid check - 첫 구간 생성 예외처리. - 상행종점, 하행종점 추가 로직 예외처리.

--- src/main/java/nextstep/subway/domain/LineSection.java
+++ src/main/java/nextstep/subway/domain/LineSection.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
+import nextstep.subway.applicaion.dto.SectionRequest;
 
 @Embeddable
 public class LineSection {
@@ -65,22 +66,44 @@ public class LineSection {
     }
 
     private void addMiddleSection(Section section) {
-        Section target = findSection(section);
-        Station middleStation = section.getDownStation();
-        Station endStation = target.getDownStation();
-        target.changeDownStation(middleStation);
-        section.changeUpStation(middleStation);
-        section.changeDownStation(endStation);
-        int index = findSectionIndex(section);
-        sections.add(index+1,section);
+        Section target = findSectionByUpStationId(section.getUpStation().getId());
+        if (target==null) {
+            target = findSectionByDownStationId(section.getDownStation().getId());
+            addMiddleDownSection(section,target);
+            return;
+        }
+        addMiddleUpSection(section, target);
     }
 
-    private Section findSection(Section section) {
+    private void addMiddleUpSection(Section section, Section target) {
+        Station middleStation = section.getDownStation();
+        target.changeUpStation(middleStation);
+        int index = findSectionIndex(section);
+        sections.add(index, section);
+    }
+
+    private void addMiddleDownSection(Section section, Section target) {
+        Station middleStation = section.getUpStation();
+        target.changeDownStation(middleStation);
+        int index = findSectionIndex(section);
+        sections.add(index+1, section);
+    }
+
+    private Section findSectionByUpStationId(Long upStationId) {
         List<Section> foundSections = sections.stream()
-            .filter(v -> v.getUpStation().equals(section.getUpStation()))
+            .filter(v -> v.getUpStation().getId().equals(upStationId))
             .collect(Collectors.toList());
         if (foundSections.isEmpty()) {
-            throw new IllegalArgumentException();
+            return null;
+        }
+        return foundSections.get(0);
+    }
+    private Section findSectionByDownStationId(Long downStationId) {
+        List<Section> foundSections = sections.stream()
+            .filter(v -> v.getDownStation().getId().equals(downStationId))
+            .collect(Collectors.toList());
+        if (foundSections.isEmpty()) {
+            return null;
         }
         return foundSections.get(0);
     }
@@ -117,9 +140,36 @@ public class LineSection {
     public boolean isEmpty() {
         return sections.isEmpty();
     }
-    public void checkArgument(Station station) {
+    public void checkDeleteArgument(Station station) {
         if (!sections.get(sections.size() - 1).getDownStation().equals(station)) {
             throw new IllegalArgumentException();
+        }
+    }
+    public void checkAddArgument(SectionRequest sectionRequest) {
+        if (sections.isEmpty()) {
+            return;
+        }
+        if (findSectionByUpStationId(sectionRequest.getDownStationId())!=null) {
+            return;
+        }
+        if (findSectionByDownStationId(sectionRequest.getUpStationId()) != null) {
+            return;
+        }
+        Section target = findSectionByUpStationId(sectionRequest.getUpStationId());
+        if (target == null) {
+            target = findSectionByDownStationId(sectionRequest.getDownStationId());
+        }
+        if (target==null) {
+            throw new IllegalArgumentException("연결되는 구간의 역이 없습니다. upStationId="+sectionRequest.getUpStationId()
+                +", downStationId="+sectionRequest.getDownStationId());
+        }
+        if (target.getDistance()<=sectionRequest.getDistance()) {
+            throw new IllegalArgumentException("기존 구간보다 작은 길이의 구간을 입력해주세요. distance="+sectionRequest.getDistance());
+        }
+        if (target.getUpStation().getId().equals(sectionRequest.getUpStationId()) &&
+                target.getDownStation().getId().equals(sectionRequest.getDownStationId())) {
+            throw new IllegalArgumentException("동일한 역의 구간이 존재합니다. upStationId="+sectionRequest.getUpStationId()
+                +", downStationId="+sectionRequest.getDownStationId());
         }
     }
 }
