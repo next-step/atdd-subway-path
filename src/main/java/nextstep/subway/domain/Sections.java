9da6ecdd53402742b189899bfc251d24eecf4e97
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-26 22:31:56 +0900

    Refact : Sections 리팩토링

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -5,7 +5,6 @@ import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import javax.persistence.Transient;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -35,25 +34,25 @@ public class Sections {
             findSection = findSection(upStation);
             upStation = findSection.getDownStation();
             list.add(findSection);
-        } while (!findSection.getDownStation().equals(downTerminal));
+        } while (!isMatchStation(findSection.getDownStation(), downTerminal));
 
         return new ArrayList<>(list);
     }
 
     private Section findSection(Station upStation) {
-        return sections.stream().filter(section -> section.getUpStation()
-            .equals(upStation)).findFirst().orElseThrow(() -> new IllegalStateException("해당 구간을 찾을 수 없습니다."));
+        return sections.stream().filter(section -> isMatchStation(section.getUpStation(), upStation)).findFirst().orElseThrow(() -> new IllegalStateException("해당 구간을 찾을 수 없습니다."));
     }
 
     public boolean addSection(Section section) {
-        if (section.getDownStation().equals(upTerminal())) {
+        if (isMatchStation(section.getDownStation(), upTerminal())) {
             return addUpSection(section);
         }
-        if (section.getUpStation().equals(downTerminal())) {
+        if (isMatchStation(section.getUpStation(), downTerminal())) {
             return addDownSection(section);
         }
         return addMiddleSection(section);
     }
+
 
     public void deleteSection(Long stationId) {
         if (sections.size() < 2) {
@@ -68,11 +67,11 @@ public class Sections {
     }
 
     public boolean addUpSection(Section section) {
-        if (isMatchSection(section.getUpStation())) {
+        if (isMatchStation(section.getUpStation())) {
             throw new IllegalArgumentException("이미 존재하는 역입니다.");
         }
 
-        if (!section.getDownStation().equals(upTerminal())) {
+        if (!isMatchStation(section.getDownStation(), upTerminal())) {
             throw new IllegalArgumentException("등록하려는 구간의 하행역이 상행 종점역과 맞지 않습니다.");
         }
 
@@ -80,11 +79,11 @@ public class Sections {
     }
 
     public boolean addDownSection(Section section) {
-        if (isMatchSection(section.getDownStation())) {
+        if (isMatchStation(section.getDownStation())) {
             throw new IllegalArgumentException("이미 존재하는 역입니다.");
         }
 
-        if (!section.getUpStation().equals(downTerminal())) {
+        if (!isMatchStation(section.getUpStation(), downTerminal())) {
             throw new IllegalArgumentException("등록하려는 구간의 상행역이 하행 종점역과 일치하지 않습니다.");
         }
 
@@ -93,8 +92,8 @@ public class Sections {
 
 
     public boolean addMiddleSection(Section section) {
-        boolean matchDownSection = isMatchSection(section.getDownStation());
-        boolean matchUpSection = isMatchSection(section.getUpStation());
+        boolean matchDownSection = isMatchStation(section.getDownStation());
+        boolean matchUpSection = isMatchStation(section.getUpStation());
 
         if (matchDownSection == matchUpSection) {
             throw new IllegalArgumentException("이미 존재하는 역입니다.");
@@ -114,11 +113,11 @@ public class Sections {
     }
 
     private Section findMatchUpSection(Station station) {
-        return sections.stream().filter(section -> section.getUpStation().equals(station)).findFirst().orElseThrow(IllegalArgumentException::new);
+        return sections.stream().filter(section -> isMatchStation(section.getUpStation(), station)).findFirst().orElseThrow(IllegalArgumentException::new);
     }
 
     private Section findMatchDownSection(Station station) {
-        return sections.stream().filter(section -> section.getDownStation().equals(station)).findFirst().orElseThrow(IllegalArgumentException::new);
+        return sections.stream().filter(section -> isMatchStation(section.getDownStation(), station)).findFirst().orElseThrow(IllegalArgumentException::new);
     }
 
     private Station upTerminal() {
@@ -155,9 +154,13 @@ public class Sections {
         return downStations.get(ZERO);
     }
 
-    private boolean isMatchSection(Station station) {
+    private boolean isMatchStation(Station station) {
         return sections.stream()
-            .anyMatch(findSection -> findSection.getUpStation().equals(station)
-                || findSection.getDownStation().equals(station));
+            .anyMatch(findSection -> isMatchStation(findSection.getUpStation(), station)
+                || isMatchStation(findSection.getDownStation(), station));
+    }
+
+    private boolean isMatchStation(Station station, Station o) {
+        return station.equals(o);
     }
 }
