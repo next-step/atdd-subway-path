4ce3920bb026fa155d7fbd9d804ecc95ad33c7c6
jay.w <jay.w@kakaocorp.com>
2022-07-27 22:19:06 +0900

    test: 인수 테스트 작성 (지하철 구간제거 요구사항 변경 대응)
    - 노선의 첫 번째 역 제거
    - 노선의 마지막 역 제거
    - 노선의 중간 역 제거
    - 노선의 마지막 구간 제거 실패 (상, 하행역)

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -16,6 +16,8 @@ import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.http.HttpStatus;
 
 @DisplayName("지하철 구간 관리 기능")
@@ -97,12 +99,50 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
 
     /**
      * Given 지하철 노선에 새로운 구간 추가를 요청 하고
-     * When 지하철 노선의 마지막 구간 제거를 요청 하면
+     * When 노선의 중간역 제거요청을 하면
+     * Then 중간역이 제거된다
+     */
+    @DisplayName("노선의 중간 역 제거")
+    @Test
+    void removeLineSectionInMiddle() {
+        // given
+        var 양재시민의숲역 = 지하철역_생성_요청("양재시민의숲역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 양재시민의숲역));
+
+        // when
+        지하철_노선에_지하철_구간_제거_요청(신분당선, 양재역);
+
+        // then
+        구간이_올바른_순서대로_존재하는지_검증(신분당선, List.of(강남역, 양재시민의숲역));
+    }
+
+    /**
+     * Given 지하철 노선에 새로운 구간 추가를 요청 하고
+     * When 지하철 노선의 첫 번째 역 제거를 요청 하면
      * Then 노선에 구간이 제거된다
      */
-    @DisplayName("지하철 노선에 구간을 제거")
+    @DisplayName("노선의 첫 번째 역 제거")
     @Test
-    void removeLineSection() {
+    void removeFirstLineSection() {
+        // given
+        Long 신사역 = 지하철역_생성_요청("신사역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(신사역, 강남역));
+
+        // when
+        지하철_노선에_지하철_구간_제거_요청(신분당선, 신사역);
+
+        // then
+        구간이_올바른_순서대로_존재하는지_검증(신분당선, List.of(강남역, 양재역));
+    }
+
+    /**
+     * Given 지하철 노선에 새로운 구간 추가를 요청 하고
+     * When 지하철 노선의 마지막 역 제거를 요청 하면
+     * Then 노선에 구간이 제거된다
+     */
+    @DisplayName("노선의 마지막 역 제거")
+    @Test
+    void removeLastLineSection() {
         // given
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
         지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
@@ -111,9 +151,35 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         지하철_노선에_지하철_구간_제거_요청(신분당선, 정자역);
 
         // then
-        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역);
+        구간이_올바른_순서대로_존재하는지_검증(신분당선, List.of(강남역, 양재역));
+    }
+
+    /**
+     * When 노선에 마지막 남은 구간의 상행역을 제거 요청하면
+     * Then 구간 제거에 실패한다
+     */
+    @DisplayName("노선의 마지막 구간 제거 실패 (상행역)")
+    @Test
+    void removeLastRemainLineSectionWithUpStationFails() {
+        // when
+        var response = 지하철_노선에_지하철_구간_제거_요청(신분당선, 강남역);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    /**
+     * When 노선에 마지막 남은 구간의 하행역을 제거 요청하면
+     * Then 구간 제거에 실패한다
+     */
+    @DisplayName("노선의 마지막 구간 제거 실패 (하행역)")
+    @Test
+    void removeLastRemainLineSectionWithDownStationFails() {
+        // when
+        var response = 지하철_노선에_지하철_구간_제거_요청(신분당선, 양재역);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
