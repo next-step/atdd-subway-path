ac9fa70b4b6b8f551b8dfa10fdd8cf225100d886
김민수 <minsu@sk.com>
2022-07-27 16:36:22 +0900

    test : 인수테스트 정의

--- src/test/java/nextstep/subway/acceptance/SectionSteps.java
+++ src/test/java/nextstep/subway/acceptance/SectionSteps.java
@@ -4,6 +4,9 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SectionSteps {
 
     public static ExtractableResponse<Response> 지하철_노선의_구간목록_조회_요청(Long lineId) {
@@ -11,5 +14,13 @@ public class SectionSteps {
                 .given().log().all()
                 .when().get("/lines/sections/{lineId}", lineId)
                 .then().log().all().extract();
+    }
+
+    public static Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, int distance) {
+        Map<String, String> params = new HashMap<>();
+        params.put("upStationId", upStationId + "");
+        params.put("downStationId", downStationId + "");
+        params.put("distance", distance + "");
+        return params;
     }
 }
