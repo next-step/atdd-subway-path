4ef6f3fcff42aeff1debe2f1a74abf94732522d8
iiaii <iiaii@woowahan.com>
2022-07-27 19:08:55 +0900

    feat: 테스트 용 텍스쳐 추가 및 httpStatus 코드 검증, json 결과 컨버터 유틸 클래스 추가

--- src/test/java/nextstep/subway/utils/GivenUtils.java
+++ src/test/java/nextstep/subway/utils/GivenUtils.java
@@ -1,13 +1,19 @@
 package nextstep.subway.utils;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import lombok.RequiredArgsConstructor;
 import nextstep.subway.applicaion.dto.LineCreationRequest;
 import nextstep.subway.applicaion.dto.LineModificationRequest;
 import nextstep.subway.applicaion.dto.SectionRegistrationRequest;
+import nextstep.subway.client.LineClient;
+import nextstep.subway.client.StationClient;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -29,6 +35,12 @@ public class GivenUtils {
     public static final String 역삼역_이름 = "역삼역";
     public static final String 양재역_이름 = "양재역";
     public static final String 선릉역_이름 = "선릉역";
+    public static final List<String> 이호선역_이름들 = List.of(강남역_이름, 역삼역_이름);
+    public static final List<String> 신분당선역_이름들 = List.of(강남역_이름, 양재역_이름);
+
+    private final StationClient stationClient;
+    private final LineClient lineClient;
+    private final JsonResponseConverter responseConverter;
 
     public static Station 강남역() {
         return new Station(FIRST_ID, 강남역_이름);
@@ -66,7 +78,7 @@ public class GivenUtils {
         return new Section(이호선(), 역삼역(), 선릉역(), FIVE);
     }
 
-    public static LineCreationRequest 이호선_생성_요청() {
+    public static LineCreationRequest 이호선_객체_생성_요청() {
         return new LineCreationRequest(
                 이호선_이름,
                 GREEN,
@@ -86,6 +98,46 @@ public class GivenUtils {
 
     public static LineModificationRequest 분당선으로_수정_요청() {
         return new LineModificationRequest(분당선_이름, YELLOW);
+    }
+
+    public ExtractableResponse<Response> 강남역_생성() {
+        return stationClient.createStation(강남역_이름);
+    }
+
+    public nextstep.subway.client.dto.LineCreationRequest 이호선_생성_요청() {
+        List<Long> stationIds = responseConverter.convertToIds(stationClient.createStations(이호선역_이름들));
+        return new nextstep.subway.client.dto.LineCreationRequest(
+                이호선_이름,
+                GREEN,
+                stationIds.get(0),
+                stationIds.get(1),
+                TEN
+        );
+    }
+
+    public nextstep.subway.client.dto.LineCreationRequest 신분당선_생성_요청() {
+        List<Long> stationIds = responseConverter.convertToIds(stationClient.createStations(신분당선역_이름들));
+        return new nextstep.subway.client.dto.LineCreationRequest(
+                신분당선_이름,
+                RED,
+                stationIds.get(0),
+                stationIds.get(1),
+                FIVE
+        );
+    }
+
+    public ExtractableResponse<Response> 이호선_생성() {
+        return lineClient.createLine(이호선_생성_요청());
+    }
+
+    public nextstep.subway.client.dto.SectionRegistrationRequest 역삼_선릉_구간_생성_요청() {
+        List<Long> stationIds = responseConverter.convertToIds(stationClient.fetchStations());
+        Long downStationId = responseConverter.convertToId(stationClient.createStation(선릉역_이름));
+        return new nextstep.subway.client.dto.SectionRegistrationRequest(
+                stationIds.get(stationIds.size() - 1),
+                downStationId,
+                TEN
+        );
     }
 
 }
