677290c0d5302046ff773faa2c38caa3f6a97b2c
sangnyyy <dltkdals2202@gmail.com>
2022-07-27 09:47:49 +0900

    test : 새로운 역을 상행 종점으로 등록할 경우 추가

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -54,7 +54,23 @@ class SectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 지하철 노선에 구간을 등록하고
+     * When 새로운 역을 상행 종점으로 등록하면
+     * Then 노선에 새로운 구간이 추가된다
+     */
+    @DisplayName("지하철 노선에 새로운 역을 상행 종점으로 추가")
+    @Test
+    void addUpStationSection() {
+        // when
+        Long 신논현역 = 지하철역_생성_요청("신논현역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(신논현역, 강남역));
+
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(신논현역, 강남역, 양재역);
+    }
+
+    /**
      * When 지하철 노선에 역 사이에 새로운 역을 등록하면
      * Then 노선에 새로운 구간이 추가된다
      */
