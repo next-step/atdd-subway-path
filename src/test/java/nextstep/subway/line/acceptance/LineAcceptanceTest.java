2edd60abe669f9a9f2ad6333f86c61c3ebd219cc
imcool2551 <imcool2551@gmail.com>
2022-07-27 15:13:14 +0900

    refactor: given/when/then 주석 대신 DisplayName을 통해 테스트의 의도 설명

--- src/test/java/nextstep/subway/line/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineAcceptanceTest.java
@@ -15,11 +15,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관리 기능")
 class LineAcceptanceTest extends AcceptanceTest {
-    /**
-     * When 지하철 노선을 생성하면
-     * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
-     */
-    @DisplayName("지하철 노선 생성")
+
+    @DisplayName("지하철 노선 생성을 생성하면 노선 목록 조회 시 생성한 노선을 찾을 수 있다.")
     @Test
     void createLine() {
         // when
@@ -30,12 +27,7 @@ class LineAcceptanceTest extends AcceptanceTest {
         노선목록에_노선들이_존재한다("2호선");
     }
 
-    /**
-     * Given 2개의 지하철 노선을 생성하고
-     * When 지하철 노선 목록을 조회하면
-     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
-     */
-    @DisplayName("지하철 노선 목록 조회")
+    @DisplayName("지하철 노선들을 생성하면 목록을 조회했을 때 모두 조회할 수 있다.")
     @Test
     void getLines() {
         // given
@@ -46,12 +38,7 @@ class LineAcceptanceTest extends AcceptanceTest {
         노선목록에_노선들이_존재한다("2호선", "3호선");
     }
 
-    /**
-     * Given 지하철 노선을 생성하고
-     * When 생성한 지하철 노선을 조회하면
-     * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
-     */
-    @DisplayName("지하철 노선 조회")
+    @DisplayName("지하철 노선을 조회하면 노선의 정보를 응답받을 수 있다.")
     @Test
     void getLine() {
         // given
@@ -61,12 +48,7 @@ class LineAcceptanceTest extends AcceptanceTest {
         노선_정보가_일치한다(_2호선, "2호선", "green");
     }
 
-    /**
-     * Given 지하철 노선을 생성하고
-     * When 생성한 지하철 노선을 수정하면
-     * Then 해당 지하철 노선 정보는 수정된다
-     */
-    @DisplayName("지하철 노선 수정")
+    @DisplayName("지하철 노선 정보를 수정할 수 있다.")
     @Test
     void updateLine() {
         // given
@@ -79,12 +61,7 @@ class LineAcceptanceTest extends AcceptanceTest {
         노선_정보가_일치한다(_2호선, "신2호선", "lightgreen");
     }
 
-    /**
-     * Given 지하철 노선을 생성하고
-     * When 생성한 지하철 노선을 삭제하면
-     * Then 해당 지하철 노선 정보는 삭제된다
-     */
-    @DisplayName("지하철 노선 삭제")
+    @DisplayName("지하철 노선을 삭제할 수 있다.")
     @Test
     void deleteLine() {
         // given
--- src/test/java/nextstep/subway/station/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/acceptance/StationAcceptanceTest.java
@@ -19,12 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("지하철역 관련 기능")
 class StationAcceptanceTest extends AcceptanceTest {
 
-    /**
-     * When 지하철역을 생성하면
-     * Then 지하철역이 생성된다
-     * Then 지하철역 목록 조회 시 생성한 역을 찾을 수 있다
-     */
-    @DisplayName("지하철역을 생성한다.")
+    @DisplayName("지하철역을 생성하면 지하철역 목록 조회 시 포함되어 있다.")
     @Test
     void createStation() {
         // when
@@ -36,12 +31,7 @@ class StationAcceptanceTest extends AcceptanceTest {
         지하철역이_존재한다("강남역");
     }
 
-    /**
-     * Given 2개의 지하철역을 생성하고
-     * When 지하철역 목록을 조회하면
-     * Then 2개의 지하철역을 응답 받는다
-     */
-    @DisplayName("지하철역을 조회한다.")
+    @DisplayName("지하철역을 조회하면 존재하는 모든 지하철역을 응답 받는다.")
     @Test
     void getStations() {
         // given
@@ -52,12 +42,7 @@ class StationAcceptanceTest extends AcceptanceTest {
         지하철역이_존재한다("강남역", "역삼역");
     }
 
-    /**
-     * Given 지하철역을 생성하고
-     * When 그 지하철역을 삭제하면
-     * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
-     */
-    @DisplayName("지하철역을 제거한다.")
+    @DisplayName("지하철역을 제거하면 목록 조회 시 조회되지 않는다.")
     @Test
     void deleteStation() {
         // given
@@ -70,12 +55,7 @@ class StationAcceptanceTest extends AcceptanceTest {
         지하철역이_존재하지_않는다("강남역");
     }
 
-    /**
-     * Given 지하철역을 생성하고
-     * When 지하철역이 속한 구간이 존재하면
-     * Then 지하철역을 제거할 수 없다
-     */
-    @DisplayName("지하철역이 속한 구간이 존재하면 지하철역을 제거할 수 없다")
+    @DisplayName("지하철역이 속한 노선이 존재하면 지하철역을 제거할 수 없다")
     @Test
     void deleteStation_Exception() {
         // given
