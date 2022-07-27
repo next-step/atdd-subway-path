240c1646f054b0d655de14b61c46557096e17687
rladowl92 <rladowl92@naver.com>
2022-07-28 01:33:55 +0900

    refactor(section): 구간 위치에 따른 action 정의

---
+++ src/main/java/nextstep/subway/domain/SectionAddPosition.java
@@ -0,0 +1,72 @@
+package nextstep.subway.domain;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public enum SectionAddPosition {
+	FIRST((List<Section> sections, Section section) -> sections.add(0, section)),
+	LAST(List::add),
+	MIDDLE((List<Section> sections, Section section) -> {
+		int index = IntStream.range(0, sections.size())
+			.filter(i-> sections.get(i).getDownStation().equals(section.getUpStation()))
+			.findFirst()
+			.orElse(0);
+		if(sections.get(index).getDistance() <= section.getDistance()) {
+			throw new IllegalArgumentException();
+		}
+		sections.add(index, section);
+	});
+
+	private final AddStationFunction action;
+
+	SectionAddPosition(AddStationFunction action) {
+		this.action = action;
+	}
+
+	public static SectionAddPosition from(Sections sections, Section section) {
+		validate(sections, section);
+
+		if(isLastSection(sections, section)) {
+			return LAST;
+		}
+		if(isFirstSection(sections, section)) {
+			return FIRST;
+		}
+		return MIDDLE;
+	}
+
+	private static void validate(Sections sections, Section section) {
+		if(sections.isEmpty()){
+			return;
+		}
+
+		// 상행과 하행이 모두 새로운 역이면 에러
+		if(!sections.getStations().contains(section.getUpStation()) && !sections.getStations().contains(section.getDownStation())) {
+			throw new IllegalArgumentException();
+		}
+
+		// 상행과 하행이 모두 등록되어 있으면 에러
+		if(sections.getStations().contains(section.getUpStation()) && sections.getStations().contains(section.getDownStation())) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private static boolean isLastSection(Sections sections, Section section) {
+		return sections.isEmpty() || sections.lastStation() == section.getUpStation();
+	}
+
+	private static boolean isFirstSection(Sections sections, Section section) {
+		return sections.firstStation() == section.getDownStation();
+	}
+
+	public void add(List<Section> sections, Section section) {
+		this.action.add(sections, section);
+	}
+
+	@FunctionalInterface
+	public interface AddStationFunction {
+		void add(List<Section> sections, Section section);
+	}
+
+
+}
