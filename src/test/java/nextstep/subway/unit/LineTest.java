865fe150b6c00022dba2b3df575b4b6262fb95cb
hjeom <nara1030@naver.com>
2022-07-31 01:13:39 +0900

    test] 구간 생성 단위테스트 생성

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,10 +1,36 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LineTest {
+    private static final Long ONE_ID = 1L;
+    private static final Long TWO_ID = 2L;
+    private static final String LINE_ONE_NM = "일호선";
+    private static final String LINE_ONE_COLOR = "blue";
+    private static final String UP_STATION_NM_= "신도림";
+    private static final String DOWN_STATION_NM = "가산디지털단지";
+    private static final int DISTANCE_DEFAULT = 10;
+
+    private final Line 일호선 = new Line(ONE_ID, LINE_ONE_NM, LINE_ONE_COLOR);
+    private final Station 신도림 = new Station(ONE_ID, UP_STATION_NM_);
+    private final Station 가산디지털단지 = new Station(TWO_ID, DOWN_STATION_NM);
+
     @Test
+    @DisplayName("구간을 생성한다.")
     void addSection() {
+        // when
+        일호선.addSection(신도림, 가산디지털단지, DISTANCE_DEFAULT);
+
+        // then
+        assertThat(일호선.getSections()).filteredOn(
+                section -> section.equals(new Section(일호선, 신도림, 가산디지털단지, DISTANCE_DEFAULT))
+        );
     }
 
     @Test
