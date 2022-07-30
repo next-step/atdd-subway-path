c31ee2d7bb2786766ddcdfbd7e38075b99c1f32e
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-30 17:01:33 +0900

    Test : 인수 테스트 케이스 작성

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -1,6 +1,7 @@
 package nextstep.subway.acceptance;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
@@ -39,5 +40,51 @@ public class PathAcceptanceTest extends AcceptanceTest {
         삼호선 = 지하철_노선_생성_요청("3호선", "orange", 교대역, 남부터미널역, 2).jsonPath().getLong("id");
 
         지하철_노선에_지하철_구간_생성_요청(삼호선, createSectionCreateParams(남부터미널역, 양재역, 3));
+    }
+
+    /**
+     * When 교대역에서 강남역의 최단 거리를 조회할 때,
+     * Then 최단 거리는 10입니다.
+     */
+    @Test
+    void test1() {
+
+    }
+
+    /**
+     * When 교대역에서 양재역까지 최단 경로는 조회할 때
+     * Then 최단 경로는교대역 - 남부터미널역 - 양재역이고
+     * Then 거리는 12 입니다.
+     */
+    @Test
+    void test2() {
+
+    }
+
+    /**
+     * When 출발역과 도착역을 같게 조회하면
+     * Then 예외가 발생합니다.
+     */
+    @Test
+    void test3() {
+
+    }
+
+    /**
+     * When 출발역과 도착역이 연결이 되어 있지 않은 경우를 조회하면
+     * Then 예외가 발생합니다.
+     */
+    @Test
+    void test4() {
+
+    }
+
+    /**
+     * When 존재하지 않은 출발역이나 도착역을 조회 할 경우
+     * Then 예외가 발생합니다.
+     */
+    @Test
+    void test5() {
+
     }
 }
