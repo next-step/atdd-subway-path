0ea80a011af0c4690cc2466b0294a52566526804
lsj8367 <lsj8367@naver.com>
2022-07-30 20:30:39 +0900

    :sparkles: 예외 검증 테스트 추가

--- src/test/java/nextstep/subway/applicaion/PathServiceTest.java
+++ src/test/java/nextstep/subway/applicaion/PathServiceTest.java
@@ -2,11 +2,20 @@ package nextstep.subway.applicaion;
 
 import static nextstep.subway.unit.LineUnitSteps.노선_구간_추가;
 import static nextstep.subway.unit.StationUnitSteps.역_추가;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import nextstep.subway.applicaion.dto.LineResponse;
 import nextstep.subway.applicaion.dto.LineSaveRequest;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import nextstep.subway.exception.SameStationException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,19 +35,59 @@ class PathServiceTest {
     @Autowired
     private PathService pathService;
 
-    @Test
-    @DisplayName("최단거리의 경로와 거리를 구한다.")
-    void shortestPath() {
-        final long 교대역 = 역_생성("교대역");
-        final long 강남역 = 역_생성("강남역");
-        final long 남부터미널역 = 역_생성("남부터미널역");
-        final long 양재역 = 역_생성("양재역");
+    long 교대역;
+    long 강남역;
+    long 남부터미널역;
+    long 양재역;
+
+    @BeforeEach
+    void setUp() {
+        교대역 = 역_생성("교대역");
+        강남역 = 역_생성("강남역");
+        남부터미널역 = 역_생성("남부터미널역");
+        양재역 = 역_생성("양재역");
 
         노선_생성("이호선", "green", 교대역, 강남역, 10);
         노선_생성("삼호선", "orange", 교대역, 남부터미널역, 3);
         노선_생성("신분당선", "red", 강남역, 양재역, 5);
+    }
 
-        pathService.getShortestPath(강남역, 남부터미널역);
+
+    @Test
+    @DisplayName("최단거리의 경로와 거리를 구한다.")
+    void shortestPath() {
+        //when
+        final PathResponse shortestPath = pathService.getShortestPath(강남역, 남부터미널역);
+
+        //then
+        최단거리_경로_거리_검증(shortestPath);
+    }
+
+    @Test
+    @DisplayName("출발역과 도착역이 같으면 예외")
+    void sameStationsException() {
+        assertThatExceptionOfType(SameStationException.class)
+            .isThrownBy(() -> pathService.getShortestPath(강남역, 강남역))
+            .withMessage("출발역과 도착역이 동일합니다.");
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 역을 조회했을 시 예외")
+    void notFoundException() {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> pathService.getShortestPath(Long.MAX_VALUE, 강남역));
+    }
+
+    private void 최단거리_경로_거리_검증(final PathResponse shortestPath) {
+        assertAll(
+            () -> assertThat(shortestPath.getStations()).hasSize(3)
+                .containsExactly(
+                    new StationResponse(강남역, "강남역"),
+                    new StationResponse(교대역, "교대역"),
+                    new StationResponse(남부터미널역, "남부터미널역")
+                ),
+            () -> assertThat(shortestPath.getDistance()).isEqualTo(13)
+        );
     }
 
     private LineResponse 노선_생성(final String name, final String color, final long upStationId, final long downStationId
