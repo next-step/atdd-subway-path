d65155a5e2c30860f1bd531b8918c21eb8e48fd2
lsj8367 <lsj8367@naver.com>
2022-07-30 18:39:34 +0900

    :recycle: json 응답 list 변경

--- src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
@@ -1,19 +1,18 @@
 package nextstep.subway.applicaion.dto;
 
 import java.util.List;
-import nextstep.subway.domain.Station;
 
 public class PathResponse {
 
-    private final List<Station> stations;
+    private final List<StationResponse> stations;
     private final int distance;
 
-    public PathResponse(final List<Station> stations, final int distance) {
+    public PathResponse(final List<StationResponse> stations, final int distance) {
         this.stations = stations;
         this.distance = distance;
     }
 
-    public List<Station> getStations() {
+    public List<StationResponse> getStations() {
         return stations;
     }
 
