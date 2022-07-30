2132b13f754af7352cbf465128b5d84d1c28dc8d
uram <parkuram12@gmail.com>
2022-07-30 16:08:17 +0900

    test: 최단 경로 조회 인수테스트 실패 케이스 작성

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -5,14 +5,13 @@ import io.restassured.response.Response;
 import nextstep.subway.applicaion.dto.request.PathRequest;
 import nextstep.subway.applicaion.dto.request.SectionRequest;
 import nextstep.subway.domain.Station;
+import nextstep.subway.enums.SubwayErrorMessage;
 import nextstep.subway.fake.FakeStationFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,12 +19,16 @@ public class PathAcceptanceTest extends AcceptanceTest{
 
     private long 분당선_ID;
     private long 이호선_ID;
+    private long 경의중앙선_ID;
     private Station 강남역;
     private Station 선릉역;
     private Station 왕십리역;
+    private Station 영통역;
+    private Station 역삼역;
     SectionRequest 강남역_선릉역_거리2;
     SectionRequest 선릉역_왕십리역_거리10;
     SectionRequest 선릉역_왕십리역_거리5;
+    SectionRequest 영통역_역삼역_거리5;
 
     @Override
     @BeforeEach
@@ -33,12 +36,18 @@ public class PathAcceptanceTest extends AcceptanceTest{
         super.setUp();
         분당선_ID = LineSteps.지하철_노선_생성_요청("분당선", "yellow").jsonPath().getLong("id");
         이호선_ID = LineSteps.지하철_노선_생성_요청("2호선", "green").jsonPath().getLong("id");
+        이호선_ID = LineSteps.지하철_노선_생성_요청("경의중앙선", "sky").jsonPath().getLong("id");
+
         강남역 = StationSteps.지하철역_생성_요청("강남역").as(Station.class);
         선릉역 = StationSteps.지하철역_생성_요청("선릉역").as(Station.class);
         왕십리역 = StationSteps.지하철역_생성_요청("왕십리역").as(Station.class);
+        영통역 = StationSteps.지하철역_생성_요청("영통역").as(Station.class);
+        역삼역 = StationSteps.지하철역_생성_요청("역삼역").as(Station.class);
+
         강남역_선릉역_거리2 = createSectionDto(강남역.getId(), 선릉역.getId(), 2);
         선릉역_왕십리역_거리10 =  createSectionDto(선릉역.getId(), 왕십리역.getId(), 10);
         선릉역_왕십리역_거리5 = createSectionDto(선릉역.getId(), 왕십리역.getId(), 5);
+        영통역_역삼역_거리5 = createSectionDto(영통역.getId(), 역삼역.getId(), 5);
     }
 
     /* given
@@ -62,6 +71,64 @@ public class PathAcceptanceTest extends AcceptanceTest{
 
         //then
         지하철_최단거리_조회_검증(지하철_최단경로_조회_응답);
+    }
+
+    /* given
+     * 2호선에 선릉역_왕십리역_거리10, 강남역_선릉역_거리2 구간을 등록하고
+     * 분당선에 선릉역_왕십리역_거리5 구간을 등록한다.
+     * when
+     * 강남역에서 강남역까지의 최단 경로를 조회한다.
+     * then
+     * 목적지와 출발지가 동일하기 떄문에 400 상태코드를 응답받고 Exception 메세지를 응답 받는다.
+     */
+    @Test
+    void 출발역과_목적지가_동일할_경우() {
+        //given
+        LineSteps.지하철_노선에_지하철_구간_생성_요청(이호선_ID, 강남역_선릉역_거리2);
+        LineSteps.지하철_노선에_지하철_구간_생성_요청(이호선_ID, 선릉역_왕십리역_거리10);
+        LineSteps.지하철_노선에_지하철_구간_생성_요청(분당선_ID, 영통역_역삼역_거리5);
+
+        //when
+        PathRequest pathRequest = new PathRequest(강남역.getId(), 강남역.getId());
+        ExtractableResponse<Response> 지하철_최단경로_조회_응답 = PathSteps.지하철_최단경로_조회(pathRequest);
+
+        //then
+        출발지와와_목적지가_동일할_경우_검증(지하철_최단경로_조회_응답);
+    }
+
+    /* given
+     * 2호선에 선릉역_왕십리역_거리10, 강남역_선릉역_거리2 구간을 등록하고
+     * 경의중앙선에 영통역_역삼역_거리5 구간을 등록한다.
+     * when
+     * 강남역에서 영통역까지의 최단 경로를 조회한다.
+     * then
+     * 목적지와 출발지가 연결되지 않았기 떄문에 400 상태코드를 응답받고 Exception 메세지를 응답 받는다.
+     */
+    @Test
+    void 경로가_이어지지_않았을_경우() {
+        //given
+        LineSteps.지하철_노선에_지하철_구간_생성_요청(이호선_ID, 강남역_선릉역_거리2);
+        LineSteps.지하철_노선에_지하철_구간_생성_요청(이호선_ID, 선릉역_왕십리역_거리10);
+        LineSteps.지하철_노선에_지하철_구간_생성_요청(경의중앙선_ID, 영통역_역삼역_거리5);
+
+        //when
+        PathRequest pathRequest = new PathRequest(강남역.getId(), 영통역.getId());
+        ExtractableResponse<Response> 지하철_최단경로_조회_응답 = PathSteps.지하철_최단경로_조회(pathRequest);
+
+        //then
+        경로가_이어지지_않았을_경우_검증(지하철_최단경로_조회_응답);
+    }
+
+    private void 출발지와와_목적지가_동일할_경우_검증(ExtractableResponse<Response> 지하철_최단경로_조회_응답) {
+        assertThat(지하철_최단경로_조회_응답.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(지하철_최단경로_조회_응답.jsonPath().getString("message"))
+                .isEqualTo(SubwayErrorMessage.SAME_SOURCE_AND_DESTINATION.getMessage());
+    }
+
+    private void 경로가_이어지지_않았을_경우_검증(ExtractableResponse<Response> 지하철_최단경로_조회_응답) {
+        assertThat(지하철_최단경로_조회_응답.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(지하철_최단경로_조회_응답.jsonPath().getString("message"))
+                .isEqualTo(SubwayErrorMessage.UNCONNECTED_PATH.getMessage());
     }
 
     private void 지하철_최단거리_조회_검증(ExtractableResponse<Response> 지하철_최단경로_조회_응답) {
--- src/test/java/nextstep/subway/unit/PathServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/PathServiceMockTest.java
@@ -8,6 +8,7 @@ import nextstep.subway.applicaion.dto.SectionDto;
 import nextstep.subway.applicaion.dto.StationDto;
 import nextstep.subway.applicaion.dto.request.PathRequest;
 import nextstep.subway.domain.*;
+import nextstep.subway.enums.SubwayErrorMessage;
 import nextstep.subway.fake.FakeLineFactory;
 import nextstep.subway.fake.FakeStationFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +22,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,18 +40,22 @@ public class PathServiceMockTest {
     StationDto 강남역;
     StationDto 선릉역;
     StationDto 왕십리역;
+    StationDto 역삼역;
     SectionDto 강남_선릉_10;
     SectionDto 선릉_왕십리_10;
     SectionDto 선릉_왕십리_5;
+    SectionDto 역삼_왕십리_5;
 
     @BeforeEach
     void setUp() {
         강남역 = new StationDto(1L, "강남역");
         선릉역 = new StationDto(2L, "선릉역");
         왕십리역 = new StationDto(7L, "왕십리역");
+        역삼역 = new StationDto(6L, "역삼역");
         강남_선릉_10 = new SectionDto(1L, FakeLineFactory.분당선(), FakeStationFactory.강남역(), FakeStationFactory.선릉역(),2);
         선릉_왕십리_10 = new SectionDto(2L, FakeLineFactory.분당선(), FakeStationFactory.선릉역(),  FakeStationFactory.왕십리역(), 10);
         선릉_왕십리_5 = new SectionDto(3L, FakeLineFactory.신분당선(), FakeStationFactory.선릉역(), FakeStationFactory.왕십리역(), 5);
+        역삼_왕십리_5 = new SectionDto(4L, FakeLineFactory.경의중앙선(), FakeStationFactory.역삼역(), FakeStationFactory.왕십리역(), 5);
     }
 
     @Test
@@ -67,6 +73,49 @@ public class PathServiceMockTest {
 
         //then
         최단경로와_최단거리_조회_검증(pathDto);
+    }
+
+    @Test
+    void 경로가_연결되지_않았을_경우() {
+        //given
+        //필요한 것 = 전체 지하철역, 전체 구간
+        when(stationService.findById(FakeStationFactory.강남역().getId())).thenReturn(FakeStationFactory.강남역());
+        when(stationService.findById(FakeStationFactory.역삼역().getId())).thenReturn(FakeStationFactory.역삼역());
+        when(stationService.findAllStations()).thenReturn(List.of(강남역, 역삼역, 선릉역,왕십리역));
+        when(sectionService.findAll()).thenReturn(List.of(강남_선릉_10, 역삼_왕십리_5));
+
+        //when
+        PathRequest pathRequest = new PathRequest(FakeStationFactory.강남역().getId(), FakeStationFactory.역삼역().getId());
+
+        //then
+        경로가_이어져있지_않을_경우_검증(pathRequest);
+    }
+
+    @Test
+    void 목적지와_출발지가_동일할_경우() {
+        //given
+        //필요한 것 = 전체 지하철역, 전체 구간
+        when(stationService.findById(FakeStationFactory.강남역().getId())).thenReturn(FakeStationFactory.강남역());
+        when(stationService.findAllStations()).thenReturn(List.of(강남역, 선릉역, 왕십리역));
+        when(sectionService.findAll()).thenReturn(List.of(강남_선릉_10, 선릉_왕십리_10, 선릉_왕십리_5));
+
+        //when
+        PathRequest pathRequest = new PathRequest(FakeStationFactory.강남역().getId(), FakeStationFactory.강남역().getId());
+
+        //then
+        출발지와_목적지가_동일한_경우_검증(pathRequest);
+    }
+
+    private void 출발지와_목적지가_동일한_경우_검증(PathRequest pathRequest) {
+        assertThatThrownBy(() -> pathService.findShortPath(pathRequest))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(SubwayErrorMessage.SAME_SOURCE_AND_DESTINATION.getMessage());
+    }
+
+    private void 경로가_이어져있지_않을_경우_검증(PathRequest pathRequest) {
+        assertThatThrownBy(() -> pathService.findShortPath(pathRequest))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(SubwayErrorMessage.UNCONNECTED_PATH.getMessage());
     }
 
     private void 최단경로와_최단거리_조회_검증(PathDto pathDto) {
