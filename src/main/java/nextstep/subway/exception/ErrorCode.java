5d1a13dcbafe49ce96b60bcbc76c9330278a60d9
kookiedncream <kim76744@naver.com>
2022-07-30 11:43:42 +0900

    refactor: ErrorCode의 ErrorMessage변경

--- src/main/java/nextstep/subway/exception/ErrorCode.java
+++ src/main/java/nextstep/subway/exception/ErrorCode.java
@@ -15,9 +15,9 @@ public enum ErrorCode {
 	CANNOT_REMOVE_SECTION(HttpStatus.BAD_REQUEST, "등록되지 않은 역으로 삭제할 수 없습니다."),
 	CANNOT_REMOVE_LAST_SECTION(HttpStatus.BAD_REQUEST, "구간이 하나인 노선의 구간을 삭제할 수 없습니다."),
 
-	CANNOT_FIND_PATH_WITH_SAME_STATION(HttpStatus.BAD_REQUEST, "출발역과 종점역이 같으면 구간을 조회할 수 없습니다."),
-	CANNOT_FIND_PATH_WITH_DISCONNECTED_STATIONS(HttpStatus.BAD_REQUEST, "출발역과 종점역이 이어져있지 않으면 조회할 수 없습니다."),
-	CANNOT_FIND_PATH_WITH_NOT_REGISTERED_STATION(HttpStatus.BAD_REQUEST, "출발역과 종점역중 어느 하나라도 등록되어 있지 않으면 경로를 조회할 수 없습니다.");
+	CANNOT_FIND_PATH_WITH_SAME_STATION(HttpStatus.BAD_REQUEST, "출발역과 종점역이 같으면 경로를 조회할 수 없습니다."),
+	CANNOT_FIND_PATH_WITH_DISCONNECTED_STATIONS(HttpStatus.BAD_REQUEST, "출발역과 종점역이 이어져 있지 않으면 경로를 조회할 수 없습니다."),
+	CANNOT_FIND_PATH_WITH_NOT_REGISTERED_STATION(HttpStatus.BAD_REQUEST, "출발역과 종점역 중 어느 하나라도 등록되어 있지 않으면 경로를 조회할 수 없습니다.");
 
 	private HttpStatus status;
 	private String message;
