8debff7dd4316c8981d3a65e9a8c82430d82108f
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-30 18:15:59 +0900

    Feat : PathController 기능 구현

--- src/main/java/nextstep/subway/ui/PathController.java
+++ src/main/java/nextstep/subway/ui/PathController.java
@@ -1,18 +1,26 @@
 package nextstep.subway.ui;
 
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathRequest;
 import nextstep.subway.applicaion.dto.PathResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 public class PathController {
+
+    private final PathService pathService;
+
+    public PathController(PathService pathService) {
+        this.pathService = pathService;
+    }
 
     @GetMapping(
         value = "/paths",
         consumes = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<PathResponse> getPath(@RequestParam String source, @RequestParam String target) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<PathResponse> getPath(@ModelAttribute PathRequest pathRequest) {
+        return ResponseEntity.ok(pathService.findPath(pathRequest));
     }
 }
