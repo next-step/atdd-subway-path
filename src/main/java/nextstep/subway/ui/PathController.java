eb020d29b81c6fc7aaec14dae12eb7059287e853
lsj8367 <lsj8367@naver.com>
2022-07-30 20:37:52 +0900

    :sparkles: 경로 조회 기능 구현

---
+++ src/main/java/nextstep/subway/ui/PathController.java
@@ -0,0 +1,24 @@
+package nextstep.subway.ui;
+
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PathController {
+
+    private final PathService pathService;
+
+    public PathController(final PathService pathService) {
+        this.pathService = pathService;
+    }
+
+    @GetMapping("/paths")
+    public ResponseEntity<PathResponse> getPaths(@RequestParam final Long source, @RequestParam final Long target) {
+        return ResponseEntity.ok(pathService.getShortestPath(source, target));
+    }
+
+}
