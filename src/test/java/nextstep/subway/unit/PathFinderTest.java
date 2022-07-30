63483bbf80c783471d24d503af583477e4792735
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-30 19:27:14 +0900

    Refact : PathFinderTest 테스트 리팩토링

--- src/test/java/nextstep/subway/unit/PathFinderTest.java
+++ src/test/java/nextstep/subway/unit/PathFinderTest.java
@@ -7,6 +7,7 @@ import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -36,16 +37,20 @@ class PathFinderTest {
     private static final Section 남부터미널역_양재역 = new Section(남부터미널역, 양재역, 2);
     private static final Section 양재역_잠실역 = new Section(양재역, 잠실역, 1);
     private static final Section 떨어져있는_구간 = new Section(어딘가, 있는역, 999);
-    private static final Line 신분당선 = new Line(1L, "신분당선", "red", 강남역_역삼역_구간);
-    private static final Line 분당선 = new Line(2L, "분당선", "red", 강남역_남부터미널역_구간);
-    private static final Line 외딴선 = new Line(2L, "외딴선", "black", 떨어져있는_구간);
-    private static final List<Line> 노선_리스트 = Arrays.asList(신분당선, 분당선, 외딴선);
+    private Line 신분당선;
+    private Line 분당선;
+    private Line 외딴선;
+    private List<Line> 노선_리스트;
 
     @BeforeEach
     void setUp() {
+        신분당선 = new Line(1L, "신분당선", "red", 강남역_역삼역_구간);
+        분당선 = new Line(2L, "분당선", "red", 강남역_남부터미널역_구간);
+        외딴선 = new Line(2L, "외딴선", "black", 떨어져있는_구간);
         신분당선.getSections().addSection(역삼역_잠실역_구간);
         분당선.getSections().addSection(남부터미널역_양재역);
         분당선.getSections().addSection(양재역_잠실역);
+        노선_리스트 = Arrays.asList(신분당선, 분당선, 외딴선);
     }
 
     @Test
@@ -81,35 +86,35 @@ class PathFinderTest {
     }
 
     @Test
-    @DisplayName("출발역과 도착역을 같으면 예외가 발생합니다.")
+    @DisplayName("출발역과 도착역을 같으면 DataIntegrityViolationException 예외가 발생합니다.")
     void test6() {
         Station sourceId = 강남역;
         Station targetId = 강남역;
 
         assertThatThrownBy(
             () -> new PathFinder(sourceId, targetId, 노선_리스트)
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(DataIntegrityViolationException.class);
     }
 
     @Test
-    @DisplayName("출발역과 도착역이 연결이 되어 있지 않은 경우를 조회하면 예외가 발생합니다.")
+    @DisplayName("출발역과 도착역이 연결이 되어 있지 않은 경우를 조회하면 DataIntegrityViolationException 예외가 발생합니다.")
     void test7() {
         Station sourceId = 강남역;
         Station targetId = 어딘가;
 
         assertThatThrownBy(
             () -> new PathFinder(sourceId, targetId, 노선_리스트)
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(DataIntegrityViolationException.class);
     }
 
     @Test
-    @DisplayName("존재하지 않은 출발역이나 도착역을 조회 할 경우 예외가 발생합니다.")
+    @DisplayName("존재하지 않은 출발역이나 도착역을 조회 할 경우 DataIntegrityViolationException 예외가 발생합니다.")
     void test8() {
         Station sourceId = 강남역;
         Station 존재하지_않는역 = new Station(928L, "존재하지 않는역");
 
         assertThatThrownBy(
             () -> new PathFinder(sourceId, 존재하지_않는역, 노선_리스트)
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(DataIntegrityViolationException.class);
     }
 }
