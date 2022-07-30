36b23f913cab1cdce0322a488544c96595635358
kookiedncream <kim76744@naver.com>
2022-07-30 10:25:16 +0900

    fix: 오류수정
    service를 통해 값을 반환하도록 함

--- src/main/java/nextstep/subway/ui/PathController.java
+++ src/main/java/nextstep/subway/ui/PathController.java
@@ -1,26 +1,23 @@
 package nextstep.subway.ui;
 
+import lombok.RequiredArgsConstructor;
+import nextstep.subway.applicaion.PathService;
 import nextstep.subway.applicaion.dto.PathResponse;
-import nextstep.subway.domain.Station;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/paths")
+@RequiredArgsConstructor
 public class PathController {
+
+	private final PathService pathService;
 
 	@GetMapping
 	public ResponseEntity<PathResponse> findShortestPath(@RequestParam Long source, @RequestParam Long target) {
-
-		return ResponseEntity.ok().body(new PathResponse(List.of(new Station("종합운동장역"),
-				new Station("잠실역"),
-				new Station("천호역")),
-				20));
+		return ResponseEntity.ok().body(pathService.findPath(source, target));
 	}
-
 }
