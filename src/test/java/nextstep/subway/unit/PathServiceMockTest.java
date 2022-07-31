ba901da9435cddd8bbe0b4e2cd36b9ab823e3185
uram <parkuram12@gmail.com>
2022-07-31 19:09:39 +0900

    test: PathService 단위 테스트 스터빙 대상 클래스 변경

--- src/test/java/nextstep/subway/unit/PathServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/PathServiceMockTest.java
@@ -1,13 +1,14 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.PathService;
-import nextstep.subway.applicaion.SectionService;
-import nextstep.subway.applicaion.StationService;
-import nextstep.subway.applicaion.dto.PathDto;
-import nextstep.subway.applicaion.dto.SectionDto;
-import nextstep.subway.applicaion.dto.StationDto;
 import nextstep.subway.applicaion.dto.request.PathRequest;
-import nextstep.subway.domain.*;
+import nextstep.subway.applicaion.dto.response.PathResponse;
+import nextstep.subway.applicaion.dto.response.StationResponse;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
 import nextstep.subway.enums.SubwayErrorMessage;
 import nextstep.subway.fake.FakeLineFactory;
 import nextstep.subway.fake.FakeStationFactory;
@@ -19,6 +20,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,63 +31,85 @@ import static org.mockito.Mockito.when;
 public class PathServiceMockTest {
 
     @Mock
-    SectionService sectionService;
+    LineRepository lineRepository;
 
     @Mock
-    StationService stationService;
+    StationRepository stationRepository;
 
     @InjectMocks
     PathService pathService;
 
-    StationDto 강남역;
-    StationDto 선릉역;
-    StationDto 왕십리역;
-    StationDto 역삼역;
-    SectionDto 강남_선릉_10;
-    SectionDto 선릉_왕십리_10;
-    SectionDto 선릉_왕십리_5;
-    SectionDto 역삼_왕십리_5;
+    Station 강남역;
+    Station 선릉역;
+    Station 왕십리역;
+    Station 역삼역;
+    Station 신촌역;
+    Station 영통역;
+    Section 강남_선릉_10;
+    Section 선릉_왕십리_10;
+    Section 선릉_왕십리_5;
+    Section 왕십리_역삼_5;
+    Section 신촌_영통_5;
+    Line 신분당선;
+    Line 분당선;
+    Line 경의중앙선;
 
     @BeforeEach
     void setUp() {
-        강남역 = new StationDto(1L, "강남역");
-        선릉역 = new StationDto(2L, "선릉역");
-        왕십리역 = new StationDto(7L, "왕십리역");
-        역삼역 = new StationDto(6L, "역삼역");
-        강남_선릉_10 = new SectionDto(1L, FakeLineFactory.분당선(), FakeStationFactory.강남역(), FakeStationFactory.선릉역(),2);
-        선릉_왕십리_10 = new SectionDto(2L, FakeLineFactory.분당선(), FakeStationFactory.선릉역(),  FakeStationFactory.왕십리역(), 10);
-        선릉_왕십리_5 = new SectionDto(3L, FakeLineFactory.신분당선(), FakeStationFactory.선릉역(), FakeStationFactory.왕십리역(), 5);
-        역삼_왕십리_5 = new SectionDto(4L, FakeLineFactory.경의중앙선(), FakeStationFactory.역삼역(), FakeStationFactory.왕십리역(), 5);
+        강남역 = FakeStationFactory.강남역();
+        선릉역 = FakeStationFactory.선릉역();
+        왕십리역 = FakeStationFactory.왕십리역();
+        역삼역 =  FakeStationFactory.역삼역();
+        신촌역 =  FakeStationFactory.신촌역();
+        영통역 =  FakeStationFactory.영통역();
+
+        신분당선 = FakeLineFactory.신분당선();
+        분당선 = FakeLineFactory.분당선();
+        경의중앙선 = FakeLineFactory.경의중앙선();
+
+        강남_선릉_10 = new Section(1L, 분당선, 강남역, 선릉역,2);
+        선릉_왕십리_10 = new Section(2L, 분당선, 선릉역,  왕십리역, 10);
+        선릉_왕십리_5 = new Section(3L, 신분당선, 선릉역, 왕십리역, 5);
+        왕십리_역삼_5 = new Section(4L, 경의중앙선, 왕십리역, 역삼역, 5);
+        신촌_영통_5 = new Section(5L, 경의중앙선, 신촌역, 영통역, 5);
+
+
+        신분당선.addSection(강남_선릉_10);
+        신분당선.addSection(선릉_왕십리_10);
+        분당선.addSection(선릉_왕십리_5);
+        분당선.addSection(왕십리_역삼_5);
+        경의중앙선.addSection(신촌_영통_5);
     }
 
     @Test
     void 최단_경로_조회() {
         //given
         //필요한 것 = 전체 지하철역, 전체 구간
-        when(stationService.findById(FakeStationFactory.강남역().getId())).thenReturn(FakeStationFactory.강남역());
-        when(stationService.findById(FakeStationFactory.왕십리역().getId())).thenReturn(FakeStationFactory.왕십리역());
-        when(stationService.findAllStations()).thenReturn(List.of(강남역, 선릉역, 왕십리역));
-        when(sectionService.findAll()).thenReturn(List.of(강남_선릉_10, 선릉_왕십리_10, 선릉_왕십리_5));
+        when(stationRepository.findById(강남역.getId())).thenReturn(Optional.of(강남역));
+        when(stationRepository.findById(왕십리역.getId())).thenReturn(Optional.of(왕십리역));
+        when(stationRepository.findAll()).thenReturn(List.of(강남역, 선릉역, 왕십리역, 역삼역));
+        when(lineRepository.findAll()).thenReturn(List.of(신분당선, 분당선));
+
 
         //when
-        PathRequest pathRequest = new PathRequest(FakeStationFactory.강남역().getId(), FakeStationFactory.왕십리역().getId());
-        PathDto pathDto = pathService.findShortPath(pathRequest);
+        PathRequest pathRequest = new PathRequest(강남역.getId(), 왕십리역.getId());
+        PathResponse pathResponse = pathService.findShortPath(pathRequest);
 
         //then
-        최단경로와_최단거리_조회_검증(pathDto);
+        최단경로와_최단거리_조회_검증(pathResponse);
     }
 
     @Test
     void 경로가_연결되지_않았을_경우() {
         //given
         //필요한 것 = 전체 지하철역, 전체 구간
-        when(stationService.findById(FakeStationFactory.강남역().getId())).thenReturn(FakeStationFactory.강남역());
-        when(stationService.findById(FakeStationFactory.역삼역().getId())).thenReturn(FakeStationFactory.역삼역());
-        when(stationService.findAllStations()).thenReturn(List.of(강남역, 역삼역, 선릉역,왕십리역));
-        when(sectionService.findAll()).thenReturn(List.of(강남_선릉_10, 역삼_왕십리_5));
+        when(stationRepository.findById(강남역.getId())).thenReturn(Optional.of(강남역));
+        when(stationRepository.findById(영통역.getId())).thenReturn(Optional.of(영통역));
+        when(stationRepository.findAll()).thenReturn(List.of(강남역, 선릉역, 왕십리역, 신촌역, 영통역, 역삼역));
+        when(lineRepository.findAll()).thenReturn(List.of(신분당선, 분당선, 경의중앙선));
 
         //when
-        PathRequest pathRequest = new PathRequest(FakeStationFactory.강남역().getId(), FakeStationFactory.역삼역().getId());
+        PathRequest pathRequest = new PathRequest(강남역.getId(), 영통역.getId());
 
         //then
         경로가_이어져있지_않을_경우_검증(pathRequest);
@@ -95,9 +119,9 @@ public class PathServiceMockTest {
     void 목적지와_출발지가_동일할_경우() {
         //given
         //필요한 것 = 전체 지하철역, 전체 구간
-        when(stationService.findById(FakeStationFactory.강남역().getId())).thenReturn(FakeStationFactory.강남역());
-        when(stationService.findAllStations()).thenReturn(List.of(강남역, 선릉역, 왕십리역));
-        when(sectionService.findAll()).thenReturn(List.of(강남_선릉_10, 선릉_왕십리_10, 선릉_왕십리_5));
+        when(stationRepository.findById(강남역.getId())).thenReturn(Optional.of(강남역));
+        when(stationRepository.findAll()).thenReturn(List.of(강남역, 선릉역, 왕십리역));
+        when(lineRepository.findAll()).thenReturn(List.of(신분당선));
 
         //when
         PathRequest pathRequest = new PathRequest(FakeStationFactory.강남역().getId(), FakeStationFactory.강남역().getId());
@@ -118,12 +142,12 @@ public class PathServiceMockTest {
                 .hasMessage(SubwayErrorMessage.UNCONNECTED_PATH.getMessage());
     }
 
-    private void 최단경로와_최단거리_조회_검증(PathDto pathDto) {
-        assertThat(pathDto.getShortestPath()
+    private void 최단경로와_최단거리_조회_검증(PathResponse response) {
+        assertThat(response.getStations()
                           .stream()
-                          .map(Station::getName)
+                          .map(StationResponse::getName)
                           .collect(Collectors.toList()))
                 .containsExactly("강남역", "선릉역", "왕십리역");
-        assertThat(pathDto.getShortestDistance()).isEqualTo(7);
+        assertThat(response.getDistance()).isEqualTo(7);
     }
 }
