dd5bfbebe514270b70296a970da3e727ed80cbc6
hjeom <nara1030@naver.com>
2022-07-31 10:50:25 +0900

    test] 구간 삭제 단위테스트 생성

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -13,15 +13,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LineTest {
     private static final Long ONE_ID = 1L;
     private static final Long TWO_ID = 2L;
+    private static final Long THREE_ID = 3L;
     private static final String LINE_ONE_NM = "일호선";
     private static final String LINE_ONE_COLOR = "blue";
     private static final String UP_STATION_NM_= "신도림";
     private static final String DOWN_STATION_NM = "가산디지털단지";
+    private static final String ADDITIONAL_STATION_NM = "독산";
     private static final int DISTANCE_DEFAULT = 10;
 
     private final Line 일호선 = new Line(ONE_ID, LINE_ONE_NM, LINE_ONE_COLOR);
     private final Station 신도림 = new Station(ONE_ID, UP_STATION_NM_);
     private final Station 가산디지털단지 = new Station(TWO_ID, DOWN_STATION_NM);
+    private final Station 독산 = new Station(THREE_ID, ADDITIONAL_STATION_NM);
 
     @Test
     @DisplayName("구간을 생성한다.")
@@ -49,6 +52,17 @@ class LineTest {
     }
 
     @Test
+    @DisplayName("구간을 삭제한다.")
     void removeSection() {
+        // given
+        일호선.addSection(신도림, 가산디지털단지, DISTANCE_DEFAULT);
+        일호선.addSection(가산디지털단지, 독산, DISTANCE_DEFAULT);
+
+        // when
+        Station 일호선_종착역 = 일호선.getLastStation();
+        일호선.removeSection(일호선_종착역.getId());
+
+        // then
+        assertThat(일호선.getSections()).hasSize(1);
     }
 }
