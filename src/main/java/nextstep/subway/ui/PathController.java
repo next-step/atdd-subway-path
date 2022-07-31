c77cdb64bff2cac0097a357395ebd18c8c48db6d
sangnyyy <dltkdals2202@gmail.com>
2022-07-31 21:37:16 +0900

    feat : PathResponse 추가 및 인수 테스트 통과할 수 있도록 fake 값들 주입

--- src/main/java/nextstep/subway/ui/PathController.java
+++ src/main/java/nextstep/subway/ui/PathController.java
@@ -1,16 +1,28 @@
 package nextstep.subway.ui;
 
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/paths")
 public class PathController {
 
     @GetMapping
-    public ResponseEntity<String> showPath() {
-        return ResponseEntity.ok("path");
+    public ResponseEntity<PathResponse> showPath() {
+        return ResponseEntity.ok().body(new PathResponse(fakeStations(), 5));
+    }
+
+    private List<StationResponse> fakeStations() {
+        return List.of(
+                new StationResponse(1L, "교대역"),
+                new StationResponse(4L, "남부터미널역"),
+                new StationResponse(3L, "양재역")
+        );
     }
 }
