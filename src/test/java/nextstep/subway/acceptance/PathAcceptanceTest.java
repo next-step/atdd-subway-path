db1968c1fe9a46db2397b08e127e14c3026d0391
sangnyyy <dltkdals2202@gmail.com>
2022-07-31 21:27:23 +0900

    test : 경로 조회 인수 테스트 추가

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -1,7 +1,13 @@
 package nextstep.subway.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +15,7 @@ import java.util.Map;
 import static nextstep.subway.acceptance.LineSteps.지하철_노선_생성_요청;
 import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철_구간_생성_요청;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayName("지하철 경로 검색")
@@ -53,4 +60,36 @@ public class PathAcceptanceTest extends AcceptanceTest {
         params.put("distance", distance + "");
         return params;
     }
+
+    /**
+     * Given 출발역과 도착역이 주어지고
+     * When 경로를 조회하면
+     * Then 조회한 경로의 역과 최단 구간 거리를 확인할 수 있다
+     */
+    @DisplayName("경로를 조회")
+    @Test
+    void findPath() {
+        //given
+        Long 출발역 = 교대역;
+        Long 도착역 = 양재역;
+
+        //when
+        ExtractableResponse<Response> response = 지하철_경로조회_요청(출발역, 도착역);
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
+        assertThat(response.jsonPath().get("distance").equals(5));
+
+    }
+
+    private ExtractableResponse<Response> 지하철_경로조회_요청(Long sourceId, Long targetId) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/paths?source=${sourceId}&target=${targetId}", sourceId, targetId)
+                .then().log().all().extract();
+    }
+
+
 }
