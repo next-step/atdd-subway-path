76a520189468188f90ef3d59f20dfe4193eeee9e
imcool2551 <imcool2551@gmail.com>
2022-08-01 15:51:25 +0900

    refactor: PathService
    모든 역정보를 가져오지 않고 출발역과 도착역 정보만 가져오도록 리팩토링했습니다.

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -2,19 +2,17 @@ package nextstep.subway.path.application;
 
 import lombok.RequiredArgsConstructor;
 import nextstep.subway.line.application.SectionService;
+import nextstep.subway.line.domain.Section;
 import nextstep.subway.path.application.dto.PathResponse;
-import nextstep.subway.path.domain.Edge;
 import nextstep.subway.path.domain.Graph;
 import nextstep.subway.path.domain.Path;
 import nextstep.subway.station.applicaion.StationService;
 import nextstep.subway.station.applicaion.dto.response.StationResponse;
+import nextstep.subway.station.domain.Station;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static java.util.Comparator.comparing;
 
 @Service
 @RequiredArgsConstructor
@@ -25,25 +23,12 @@ public class PathService {
     private final SectionService sectionService;
 
     public PathResponse findPath(Long source, Long target) {
-        List<StationResponse> stations = stationService.findAllStations();
+        Station sourceStation = stationService.findById(source);
+        Station targetStation = stationService.findById(target);
+        List<Section> sections = sectionService.findAllSections();
 
-        List<Long> stationIds = stations.stream()
-                .map(StationResponse::getId)
-                .collect(Collectors.toList());
-
-        List<Edge> sections = sectionService.findAllSections()
-                .stream()
-                .map(it -> new Edge(it.getUpStationId(), it.getDownStationId(), it.getDistance()))
-                .collect(Collectors.toList());
-
-        Path shortestPath = new Graph(stationIds, sections).findShortestPath(source, target);
-        List<Long> pathStationIds = shortestPath.getVertexes();
-
-        List<StationResponse> pathStationsInOrder = stations.stream()
-                .filter(s -> pathStationIds.contains(s.getId()))
-                .sorted(comparing(s -> pathStationIds.indexOf(s.getId())))
-                .collect(Collectors.toList());
-
-        return new PathResponse(pathStationsInOrder, shortestPath.getDistance());
+        Path shortestPath = new Graph(sections).findShortestPath(sourceStation.getId(), targetStation.getId());
+        List<StationResponse> pathStations = stationService.findByIds(shortestPath.getVertexes());
+        return new PathResponse(pathStations, shortestPath.getDistance());
     }
 }
