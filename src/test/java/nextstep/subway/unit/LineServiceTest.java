ae393fa12c2c645ae2ef384e53cbc880e3c088a7
ashley <ashley@dunamu.com>
2022-08-03 00:31:38 +0900

    [modify] 코드리뷰 반영

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,18 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.LineRequest;
 import nextstep.subway.applicaion.dto.SectionRequest;
-import nextstep.subway.domain.*;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,17 +34,19 @@ public class LineServiceTest {
         Station 양재역 = stationRepository.save(new Station("양재역"));
         Station 양재시민의숲역 = stationRepository.save(new Station("양재시민의숲역"));
         Station 청계산입구역 = stationRepository.save(new Station("청계산입구역"));
-        Line 분당선 = lineRepository.save(new Line("분당선", "red"));
-        분당선.addSection(new Section(분당선, 양재역, 양재시민의숲역, 10));
+
+        LineRequest lineRequest = new LineRequest("분당선", "red", 양재역.getId(), 양재시민의숲역.getId(), 10);
+        Long 분당선Id = lineService.saveLine(lineRequest).getId();
         SectionRequest sectionRequest =  new SectionRequest(양재시민의숲역.getId(), 청계산입구역.getId(), 10);
 
         // when
         // lineService.addSection 호출
-        lineService.addSection(분당선.getId(), sectionRequest);
+        lineService.addSection(분당선Id, sectionRequest);
 
         // then
         // line.getSections 메서드를 통해 검증
+        Line 분당선 = lineRepository.getById(분당선Id);
         assertThat(분당선.getSections()).hasSize(2);
-        assertThat(분당선.getStations()).contains(양재역, 양재시민의숲역, 청계산입구역);
+        assertThat(분당선.getStations()).isEqualTo(List.of(양재역, 양재시민의숲역, 청계산입구역));
     }
 }
