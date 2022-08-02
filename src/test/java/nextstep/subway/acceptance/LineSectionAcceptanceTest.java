16dab5a1f18c2dcb444b0c07eca5801f6c963e32
sion <aabbcc030421@gmail.com>
2022-08-02 23:07:46 +0900

    refactor: 예외 발생 중복 메서드 추출

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -107,7 +107,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
                         지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 삼성역, 100));
 
                 // then
-                assertThat(구간_생성.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+                예외_발생(구간_생성);
             }
 
             /**
@@ -122,7 +122,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
                         지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 양재역));
 
                 // then
-                assertThat(구간_생성.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+                예외_발생(구간_생성);
             }
 
             /**
@@ -141,7 +141,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
                         지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(삼성역, 판교역));
 
                 // then
-                assertThat(구간_생성.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+                예외_발생(구간_생성);
             }
         }
 
@@ -199,7 +199,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
                 ExtractableResponse<Response> 구간_제거 = 지하철_노선에_지하철_구간_제거_요청(신분당선, 강남역);
 
                 // then
-                assertThat(구간_제거.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+                예외_발생(구간_제거);
             }
 
             /**
@@ -213,7 +213,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
                 ExtractableResponse<Response> 구간_제거 = 지하철_노선에_지하철_구간_제거_요청(신분당선, 삼성역);
 
                 // then
-                assertThat(구간_제거.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+                예외_발생(구간_제거);
             }
 
             /**
@@ -230,31 +230,14 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
                 ExtractableResponse<Response> 구간_제거 = 지하철_노선에_지하철_구간_제거_요청(신분당선, 판교역);
 
                 // then
-                assertThat(구간_제거.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+                예외_발생(구간_제거);
             }
         }
 
     }
 
-    /**
-     * Given 지하철 노선에 새로운 구간 추가를 요청 하고
-     * When 지하철 노선의 마지막 구간 제거를 요청 하면
-     * Then 노선에 구간이 제거된다
-     */
-    @DisplayName("지하철 노선에 구간을 제거")
-    @Test
-    void removeLineSection() {
-        // given
-        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
-
-        // when
-        지하철_노선에_지하철_구간_제거_요청(신분당선, 정자역);
-
-        // then
-        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역);
+    private void 예외_발생(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
