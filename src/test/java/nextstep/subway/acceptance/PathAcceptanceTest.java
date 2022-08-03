e2a566778fd2ebd7c98cffb2a42f0646a6c4f285
jwee0330 <jwee0330@gmail.com>
2022-08-03 16:52:21 +0900

    refactor: PathAcceptanceFixture 를 활용하여 중복 제거

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -3,19 +3,27 @@ package nextstep.subway.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-import static nextstep.subway.acceptance.LineSectionAcceptanceTest.createSectionParams;
-import static nextstep.subway.acceptance.LineSteps.지하철_노선_생성_요청;
-import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철_구간_생성_요청;
-import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 경로 조회 기능")
 public class PathAcceptanceTest extends AcceptanceTest {
+
+    @Autowired
+    private PathAcceptanceFixture pathAcceptanceFixture;
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        pathAcceptanceFixture.지하철_노선이_구성된다();
+    }
 
     /**
      * when 출발역 ID와 도착역 ID로 지하철 경로조회 요청을 하면
@@ -24,32 +32,13 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @DisplayName("최단 경로 경로 조회")
     @Test
     void findShortestPath() {
-        var 칠호선 = 지하철_노선_생성_요청("7호선", "olive").jsonPath().getLong("id");
-        var 구호선 = 지하철_노선_생성_요청("9호선", "gold").jsonPath().getLong("id");
-        var 신분당선 = 지하철_노선_생성_요청("신분당선", "red").jsonPath().getLong("id");
-
-        var 고속터미널역 = 지하철역_생성_요청("고속터미널역").jsonPath().getLong("id");
-        var 반포역 = 지하철역_생성_요청("반포역").jsonPath().getLong("id");
-        var 논현역 = 지하철역_생성_요청("논현역").jsonPath().getLong("id");
-        var 사평역 = 지하철역_생성_요청("사평역").jsonPath().getLong("id");
-        var 신논현역 = 지하철역_생성_요청("신논현역").jsonPath().getLong("id");
-
-        final int 고속터미널_반포_거리 = 2;
-        final int 반포_논현_거리 = 2;
-        final int 논현_신논현_거리 = 3;
-        final int 고속터미널_신논현_거리 = 10;
-        지하철_노선에_지하철_구간_생성_요청(칠호선, createSectionParams(고속터미널역, 반포역, 고속터미널_반포_거리));
-        지하철_노선에_지하철_구간_생성_요청(칠호선, createSectionParams(반포역, 논현역, 반포_논현_거리));
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionParams(논현역, 신논현역, 논현_신논현_거리));
-        지하철_노선에_지하철_구간_생성_요청(구호선, createSectionParams(고속터미널역, 신논현역, 고속터미널_신논현_거리));
-
-        var response = 최단_경로_조회_요청(고속터미널역, 신논현역);
+        var response = 최단_경로_조회_요청(pathAcceptanceFixture.고속터미널역, pathAcceptanceFixture.신논현역);
 
         final List<String> stationNames = response.jsonPath().getList("stations.name", String.class);
         assertThat(stationNames).containsSequence("고속터미널역", "반포역", "논현역", "신논현역");
 
         final Long distance = response.jsonPath().getLong("distance");
-        assertThat(distance).isEqualTo(고속터미널_반포_거리 + 반포_논현_거리 + 논현_신논현_거리);
+        assertThat(distance).isEqualTo(pathAcceptanceFixture.고속터미널_반포_거리 + pathAcceptanceFixture.반포_논현_거리 + pathAcceptanceFixture.논현_신논현_거리);
     }
 
     /**
@@ -59,32 +48,12 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @DisplayName("최단 경로 조회 실패")
     @Test
     void failFindShortestPath() {
-        // given
-        var 칠호선 = 지하철_노선_생성_요청("7호선", "olive").jsonPath().getLong("id");
-        var 구호선 = 지하철_노선_생성_요청("9호선", "gold").jsonPath().getLong("id");
-        var 신분당선 = 지하철_노선_생성_요청("신분당선", "red").jsonPath().getLong("id");
-
-        var 고속터미널역 = 지하철역_생성_요청("고속터미널역").jsonPath().getLong("id");
-        var 반포역 = 지하철역_생성_요청("반포역").jsonPath().getLong("id");
-        var 논현역 = 지하철역_생성_요청("논현역").jsonPath().getLong("id");
-        var 사평역 = 지하철역_생성_요청("사평역").jsonPath().getLong("id");
-        var 신논현역 = 지하철역_생성_요청("신논현역").jsonPath().getLong("id");
-
-        final int 고속터미널_반포_거리 = 2;
-        final int 반포_논현_거리 = 2;
-        final int 논현_신논현_거리 = 3;
-        final int 고속터미널_신논현_거리 = 10;
-        지하철_노선에_지하철_구간_생성_요청(칠호선, createSectionParams(고속터미널역, 반포역, 고속터미널_반포_거리));
-        지하철_노선에_지하철_구간_생성_요청(칠호선, createSectionParams(반포역, 논현역, 반포_논현_거리));
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionParams(논현역, 신논현역, 논현_신논현_거리));
-        지하철_노선에_지하철_구간_생성_요청(구호선, createSectionParams(고속터미널역, 신논현역, 고속터미널_신논현_거리));
-
         // when & then
-        var 존재하지않는_출발역 = 최단_경로_조회_요청(신논현역 + 1, 신논현역);
+        var 존재하지않는_출발역 = 최단_경로_조회_요청(pathAcceptanceFixture.신논현역 + 1, pathAcceptanceFixture.신논현역);
         assertThat(존재하지않는_출발역.statusCode()).isEqualTo(500);
 
         // when & // then
-        var 존재하지않는_도착역 = 최단_경로_조회_요청(신논현역, 신논현역 + 1);
+        var 존재하지않는_도착역 = 최단_경로_조회_요청(pathAcceptanceFixture.신논현역, pathAcceptanceFixture.신논현역 + 1);
         assertThat(존재하지않는_도착역.statusCode()).isEqualTo(500);
     }
 
@@ -94,6 +63,5 @@ public class PathAcceptanceTest extends AcceptanceTest {
                 .when().get(String.format("/paths?source=%d&target=%d", source, target))
                 .then().log().all().extract();
     }
-
 
 }
