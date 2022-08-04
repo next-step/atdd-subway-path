4cb5e6975cb0433cf5f718ac029764f4010dd6b0
csw <csw@elonsoft.co.kr>
2022-08-04 12:29:40 +0900

    - 역 목록 조회 단위테스트 작성 - 반복되는 코드 메서드로 분리

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -54,4 +54,15 @@ public class Line {
     public void addSection(Section section) {
         sections.add(section);
     }
+
+    public List<Station> getStations() {
+        List<Station> stations = new ArrayList<>();
+
+        stations.add(sections.get(0).getUpStation());
+        for (Section section: sections) {
+            stations.add(section.getDownStation());
+        }
+
+        return stations;
+    }
 }
--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -5,28 +5,47 @@ import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
-    @Test
-    void addSection() {
-        Line line = new Line("1호선", "blue");
+
+    Line 일호선_생성() {
+        Line line = 일호선_생성();
 
         Station 구로역 = new Station("구로역");
         Station 신도림역 = new Station("신도림역");
 
         line.addSection(new Section(line, 구로역, 신도림역, 10));
 
+        return line;
+    }
+
+    @Test
+    void addSection() {
+        Line line = 일호선_생성();
+
         assertThat(line.getSections().size()).isEqualTo(1);
     }
 
     @Test
     void getStations() {
+        Line line = 일호선_생성();
+
+        List<Station> stations = line.getStations();
+
+        assertThat(stations.stream().map(Station::getName)
+                .collect(Collectors.toList()))
+                .contains("구로역", "신도림역");
     }
 
     @Test
     void removeSection() {
+        Line line = new Line("1호선", "blue");
+
+
+
     }
 }
