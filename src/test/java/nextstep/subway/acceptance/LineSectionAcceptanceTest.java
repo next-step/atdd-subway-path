f3eb934313ff10c234ed43c221a9f959eedaf6a2
MoonHKLee <mf311110@gmail.com>
2022-08-05 03:03:55 +0900

    test : 예외 인수 테스트 추가

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -59,7 +59,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
      */
     @DisplayName("지하철 노선에 하행종점역 구간을 제거")
     @Test
-    void removeLineSection() {
+    void removeLineSectionAtLast() {
         // given
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
         지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
@@ -80,7 +80,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
      */
     @DisplayName("지하철 노선에 상행종점역 구간을 제거")
     @Test
-    void removeLineSection2() {
+    void removeLineSectionAtFirst() {
         // given
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
         지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
@@ -101,7 +101,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
      */
     @DisplayName("지하철 노선에 중간 구간을 제거")
     @Test
-    void removeLineSection3() {
+    void removeLineSectionAtMiddle() {
         // given
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
         지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
@@ -113,6 +113,42 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 정자역);
+    }
+
+    /**
+     * When 지하철 노선의 제거를 요청 하면
+     * Then 예외가 발생한다.
+     */
+    @DisplayName("구간이 하나인 노선에서 마지막 구간을 제거")
+    @Test
+    void removeLineSectionExceptionOnlyOneSection() {
+        // when
+        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_제거_요청(신분당선, 양재역);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    /**
+     * Given 지하철 노선에 새로운 구간 추가를 요청 하고
+     * When 노선에 등록되어있지 않은 역의 제거를 요청 하면
+     * Then 예외가 발생한다.
+     */
+    @DisplayName("노선에 등록되지 않은 역을 제거")
+    @Test
+    void removeLineSectionExceptionNonRegister() {
+        // given
+        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+
+        Long 가양역 = 지하철역_생성_요청("가양역").jsonPath().getLong("id");
+
+        // when
+        지하철_노선에_지하철_구간_제거_요청(신분당선, 가양역);
+
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
     private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
