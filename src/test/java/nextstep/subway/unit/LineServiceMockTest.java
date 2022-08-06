f6b76116e0e65b3c6f86dbb33282b3e93aa341d4
csw <csw@elonsoft.co.kr>
2022-08-06 17:03:37 +0900

    - 라인서비스 mock 구간추가 테스트 데이터 추가

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -39,23 +39,29 @@ public class LineServiceMockTest {
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
         Station 구로역 = new Station("구로역");
         Station 신도림역 = new Station("신도림역");
+        Station 영등포역 = new Station("영등포역");
 
         Line 일호선 = new Line("1호선", "blue");
         일호선.addSection(new Section(일호선, 구로역, 신도림역, 10));
+        일호선.addSection(new Section(일호선, 신도림역, 영등포역, 15));
 
         when(stationService.findById(1L)).thenReturn(구로역);
         when(stationService.findById(2L)).thenReturn(신도림역);
+        when(stationService.findById(3L)).thenReturn(영등포역);
+
         when(lineRepository.findById(1L)).thenReturn(Optional.of(일호선));
 
         // when
         // lineService.addSection 호출
         lineService.addSection(1L, new SectionRequest(1L, 2L, 10));
+        lineService.addSection(1L, new SectionRequest(2L, 3L, 15));
 
         // then
         // line.findLineById 메서드를 통해 검증
         Line line = lineService.findLineById(1L);
 
         assertThat(line.getName()).isEqualTo("1호선");
+        assertThat(line.getStations().stream().map(Station::getName)).contains("구로역", "신도림역", "영등포역");
 
     }
 }
