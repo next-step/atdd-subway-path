1a8cbc2d77dae1db64378178956e9db1d8b81ea2
SangwookChoi <94peterchoi@gmail.com>
2022-08-07 18:06:30 +0900

    [fix7] 공통으로 사용할 데이터를 내부 스태틱 클래스로 분리, 테스트케이스 추가

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -5,7 +5,6 @@ import nextstep.subway.applicaion.StationService;
 import nextstep.subway.applicaion.dto.SectionRequest;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,8 +15,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static nextstep.subway.unit.LineServiceMockTest.SubwayInfo.구로역;
+import static nextstep.subway.unit.LineServiceMockTest.SubwayInfo.구로역_신도림역_거리;
+import static nextstep.subway.unit.LineServiceMockTest.SubwayInfo.신도림역;
+import static nextstep.subway.unit.LineServiceMockTest.SubwayInfo.신도림역_영등포역_거리;
+import static nextstep.subway.unit.LineServiceMockTest.SubwayInfo.영등포역;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+
 
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
@@ -38,31 +43,83 @@ public class LineServiceMockTest {
     void addSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
-        Station 구로역 = new Station("구로역");
-        Station 신도림역 = new Station("신도림역");
-        Station 영등포역 = new Station("영등포역");
+        when(stationService.findById(구로역.getId())).thenReturn(구로역);
+        when(stationService.findById(신도림역.getId())).thenReturn(신도림역);
+        when(stationService.findById(영등포역.getId())).thenReturn(영등포역);
 
-        Line 일호선 = new Line("1호선", "blue");
-        일호선.addSection(new Section(일호선, 구로역, 신도림역, 10));
-        일호선.addSection(new Section(일호선, 신도림역, 영등포역, 15));
-
-        when(stationService.findById(1L)).thenReturn(구로역);
-        when(stationService.findById(2L)).thenReturn(신도림역);
-        when(stationService.findById(3L)).thenReturn(영등포역);
-
-        when(lineRepository.findById(1L)).thenReturn(Optional.of(일호선));
+        Line 일호선 = new Line(1L, "1호선", "blue");
+        when(lineRepository.findById(일호선.getId())).thenReturn(Optional.of(일호선));
 
         // when
         // lineService.addSection 호출
-        lineService.addSection(1L, new SectionRequest(1L, 2L, 10));
-        lineService.addSection(1L, new SectionRequest(2L, 3L, 15));
+        lineService.addSection(일호선.getId(), new SectionRequest(구로역.getId(), 신도림역.getId(), 구로역_신도림역_거리));
+        lineService.addSection(일호선.getId(), new SectionRequest(신도림역.getId(), 영등포역.getId(), 신도림역_영등포역_거리));
 
         // then
         // line.findLineById 메서드를 통해 검증
-        Line line = lineService.findLineById(1L);
+        Line line = lineService.findLineById(일호선.getId());
+
+        assertThat(line.getName()).isEqualTo("1호선");
+        assertThat(line.getSections().count()).isEqualTo(2);
+
+    }
+
+    @DisplayName("지하철 노선에 등록된 역 목록 조회")
+    @Test
+    void getStations() {
+        // given
+        // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        Line 일호선 = new Line(1L, "1호선", "blue");
+
+        when(stationService.findById(구로역.getId())).thenReturn(구로역);
+        when(stationService.findById(신도림역.getId())).thenReturn(신도림역);
+        when(stationService.findById(영등포역.getId())).thenReturn(영등포역);
+
+        when(lineRepository.findById(일호선.getId())).thenReturn(Optional.of(일호선));
+
+        // when
+        // lineService.addSection 호출
+        lineService.addSection(일호선.getId(), new SectionRequest(구로역.getId(), 신도림역.getId(), 구로역_신도림역_거리));
+        lineService.addSection(일호선.getId(), new SectionRequest(신도림역.getId(), 영등포역.getId(), 신도림역_영등포역_거리));
+
+        // then
+        // line.findLineById 메서드를 통해 검증
+        Line line = lineService.findLineById(일호선.getId());
 
         assertThat(line.getName()).isEqualTo("1호선");
         assertThat(line.getStations().stream().map(Station::getName)).contains("구로역", "신도림역", "영등포역");
+    }
 
+    @DisplayName("지하철 노선에서 구간 제거")
+    @Test
+    void removeSection() {
+        // given
+        Line 일호선 = new Line(1L, "1호선", "blue");
+
+        when(stationService.findById(구로역.getId())).thenReturn(구로역);
+        when(stationService.findById(신도림역.getId())).thenReturn(신도림역);
+        when(stationService.findById(영등포역.getId())).thenReturn(영등포역);
+
+        when(lineRepository.findById(일호선.getId())).thenReturn(Optional.of(일호선));
+
+        // when
+        lineService.addSection(일호선.getId(), new SectionRequest(구로역.getId(), 신도림역.getId(), 구로역_신도림역_거리));
+        lineService.addSection(일호선.getId(), new SectionRequest(신도림역.getId(), 영등포역.getId(), 신도림역_영등포역_거리));
+        lineService.deleteSection(일호선.getId(), 영등포역.getId());
+
+        Line line = lineService.findLineById(일호선.getId());
+
+        // then
+        assertThat(line.getName()).isEqualTo("1호선");
+        assertThat(line.getSections().count()).isEqualTo(1);
+    }
+
+    public static class SubwayInfo {
+        public static Station 구로역 = new Station(1L, "구로역");
+        public static Station 신도림역 = new Station(2L, "신도림역");
+        public static Station 영등포역 = new Station(3L, "영등포역");
+
+        public static int 구로역_신도림역_거리 = 10;
+        public static int 신도림역_영등포역_거리 = 15;
     }
 }
