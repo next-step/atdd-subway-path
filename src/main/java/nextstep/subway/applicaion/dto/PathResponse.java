b9851ca55218846837d13eb374e93c091d0b61c9
chkim <cndghks0213@gmail.com>
2022-08-09 09:46:29 +0900

    test: 경로 응답 클래스 생성

---
+++ src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
@@ -0,0 +1,32 @@
+package nextstep.subway.applicaion.dto;
+
+import nextstep.subway.domain.Station;
+import org.jgrapht.GraphPath;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PathResponse {
+    private List<StationResponse> stations;
+    private int distance;
+
+    public PathResponse(List<StationResponse> stations, int distance) {
+        this.stations = stations;
+        this.distance = distance;
+    }
+
+    public static PathResponse from(GraphPath path) {
+        List<Station> stations = path.getVertexList();
+        return new PathResponse(stations.stream()
+                .map(StationResponse::from)
+                .collect(Collectors.toList()), (int) path.getWeight());
+    }
+
+    public List<StationResponse> getStations() {
+        return stations;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
