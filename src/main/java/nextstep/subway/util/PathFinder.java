22b9cbfb49f75dd0c801cf71bdc361a471a8fca2
chkim <cndghks0213@gmail.com>
2022-08-10 03:11:22 +0900

    refactor: 그래프에 지하철역을 등록하는 기능 개선

--- src/main/java/nextstep/subway/util/PathFinder.java
+++ src/main/java/nextstep/subway/util/PathFinder.java
@@ -8,15 +8,13 @@ import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
-import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-@Component
 public class PathFinder {
-
     public static PathResponse findPath(List<Line> lines, Station source, Station target) {
         if (source.equals(target)) {
             throw new IllegalArgumentException();
@@ -38,14 +36,15 @@ public class PathFinder {
     }
 
     private static void registerSections(WeightedMultigraph<Station, DefaultWeightedEdge> graph, List<Section> sections) {
-        for (Section section : sections) {
-            Station upStation = section.getUpStation();
-            Station downStation = section.getDownStation();
+        sections.stream()
+                .flatMap(section -> Stream.of(section.getUpStation(), section.getDownStation()))
+                .distinct()
+                .forEach(graph::addVertex);
 
-            graph.addVertex(upStation);
-            graph.addVertex(downStation);
-            graph.setEdgeWeight(graph.addEdge(upStation, downStation), section.getDistance());
-        }
+        sections.forEach(section -> graph.setEdgeWeight(
+                graph.addEdge(section.getUpStation(), section.getDownStation()),
+                section.getDistance())
+        );
     }
 
     private static List<Section> extractSections(List<Line> lines) {
