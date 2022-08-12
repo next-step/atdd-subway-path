7189d486bd94162ce4db1e27e109a77365192506
cafitac <cafitac99@gmail.com>
2022-08-13 00:29:55 +0900

    feat: Sections 에 대한 에러 코드를 추가합니다

--- src/main/java/nextstep/subway/error/exception/ErrorCode.java
+++ src/main/java/nextstep/subway/error/exception/ErrorCode.java
@@ -8,12 +8,17 @@ public enum ErrorCode {
     // Entity
     LINE_NOT_FOUND("노선 정보를 찾을 수 없습니다."),
 
-    // Subway
+    // Line
+
+    // Sections
+    PREVIOUS_SECTION_NOT_FOUND("이전 구간이 존재하지 않습니다."),
+    NEXT_SECTION_NOT_FOUND("다음 구간이 존재하지 않습니다."),
     SECTION_NOT_FOUND_ABOUT_UP_AND_DOWN_STATION("상행역 또는 하행역에 대한 구간이 존재하지 않습니다."),
     SECTION_ALREADY_EXISTS("이미 등록되어있는 구간입니다."),
     INVALID_SECTION_DISTANCE("기존의 구간의 길이보다 새로운 구간의 길이가 길거나 클 수 없습니다."),
     CANNOT_REMOVE_SECTION_IF_IS_NOT_DOWN_STATION("삭제하려는 역이 하행종점역이 아닙니다."),
     CANNOT_REMOVE_LAST_SECTION("노선의 마지막 구간은 삭제할 수 없습니다."),
+    SECTION_LIST_IS_EMPTY("구간 목록이 비어있습니다."),
 
     ;
     private final String message;
