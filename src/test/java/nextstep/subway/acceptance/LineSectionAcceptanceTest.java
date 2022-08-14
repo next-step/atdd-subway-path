af4a25f92765e7ff3912616b410bd60a6d390863
SangwookChoi <csw@elonsoft.co.kr>
2022-08-14 15:38:49 +0900

    [지하철 구간 제거 기능 개선 미션 시작] - 구간제거 관련 요구사항에 따른 인수조건 도출 및 인수 테스트 작성

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -209,6 +209,88 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역);
     }
 
+    /**
+     * Given A-B 구간이 존재하던 지하철 노선에 B-C 구간을 등록 후
+     * When 지하철 노선의 상행종점역과 하행종점역 사이에 위치한 역(B) 제거를 요청 하면
+     * Then 역이 삭제되고 남아있는 구간을 재배치한다 (A-C)
+     */
+    @DisplayName("지하철 노선의 기존 구간에서 중간역 제거")
+    @Test
+    void removeMiddleStationInSection() {
+        // given
+        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+
+        // when
+        지하철_노선에_지하철_구간_제거_요청(신분당선, 양재역);
+
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 정자역);
+    }
+
+
+    /**
+     * Given A-B 구간이 존재하던 지하철 노선에 B-C 구간을 등록 후
+     * When 하행종점역(C) 제거를 요청하면
+     * Then 기존 하행종점역이 삭제되고 다음으로 오던 역(B)이 하행종점역이 된다
+     */
+    @DisplayName("지하철 노선에서 하행종점역 제거")
+    @Test
+    void removeLastDownStation() {
+        // given
+        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+
+        // when
+        지하철_노선에_지하철_구간_제거_요청(신분당선, 정자역);
+
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역);
+    }
+
+    /**
+     * Given B-C 구간이 존재하던 지하철 노선에 A-B 구간을 등록 후
+     * When 상행종점역(A) 제거를 요청하면
+     * Then 기존 상행종점역이 삭제되고 다음으로 오던 역(B)이 종점이 된다
+     */
+    @DisplayName("지하철 노선에서 상행종점역 제거")
+    @Test
+    void removeLastUpStation() {
+        // given
+        Long 신논현역 = 지하철역_생성_요청("신논현역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(신논현역, 강남역));
+
+        // when
+        지하철_노선에_지하철_구간_제거_요청(신분당선, 신논현역);
+
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역);
+    }
+
+    /**
+     * Given 지하철역 생성 후
+     * When 노선에 등록되어 있지 않은 역을 삭제하려고 할 시
+     * Then 잘못된 요청으로 인해 예외가 발생한다
+     */
+    @DisplayName("지하철 노선에 등록되지 않은 역 제거")
+    @Test
+    void removeNonExistentStation() {
+        // given
+        Long 구로역 = 지하철역_생성_요청("구로역").jsonPath().getLong("id");
+
+        // when
+        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_제거_요청(신분당선, 구로역);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
     private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
         Map<String, String> lineCreateParams;
         lineCreateParams = new HashMap<>();
