701681a92802a12498923004c645c59db6d2e338
chosundeveloper <iddaumid@naver.com>
2022-08-24 16:36:34 +0900

    refactor : 클래스명 변경

--- src/test/java/nextstep/subway/unit/PathFinderTest.java
+++ src/test/java/nextstep/subway/unit/PathFinderTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class PathTest {
+class PathFinderTest {
 
     Line 신분당선;
     Station 신논현역;
