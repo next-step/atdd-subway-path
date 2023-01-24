56e872505094343a3d55ebef73b399d174f35e20
michol.lee <lovethefeel@gmail.com>
2023-01-24 11:32:16 +0900

    refactor(test): Mock을 사용하지 않는 응용 서비스 단위 테스트 작성

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,16 +1,21 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("호선의 대한 테스트")
 @SpringBootTest
 @Transactional
-public class LineServiceTest {
+class LineServiceTest {
     @Autowired
     private StationRepository stationRepository;
     @Autowired
@@ -19,15 +24,29 @@ public class LineServiceTest {
     @Autowired
     private LineService lineService;
 
+    @DisplayName("호선의 구간을 생성한다.")
     @Test
     void addSection() {
-        // given
-        // stationRepository와 lineRepository를 활용하여 초기값 셋팅
 
-        // when
-        // lineService.addSection 호출
+        final Station upStation = new Station("강남역");
+        final Station saveUpStation = stationRepository.save(upStation);
 
-        // then
-        // line.getSections 메서드를 통해 검증
+        final Station downStation = new Station("잠실역");
+        final Station saveDownStation = stationRepository.save(downStation);
+
+        final Line line = new Line(new LineInfo("2호선", "green"));
+        final Line saveLine = lineRepository.save(line);
+
+        final SectionRequest sectionRequest = new SectionRequest(saveUpStation.getId(), saveDownStation.getId(), 10);
+
+        lineService.addSection(saveLine.getId(), sectionRequest);
+
+        assertAll(
+                () -> assertThat(line.getSections()).hasSize(1),
+                () -> assertThat(line.getSections().get(0).getLine().getLineInfo()).isEqualTo(new LineInfo("2호선", "green")),
+                () -> assertThat(line.getSections().get(0).getUpStation().getName()).isEqualTo("강남역"),
+                () -> assertThat(line.getSections().get(0).getDownStation().getName()).isEqualTo("잠실역"),
+                () -> assertThat(line.getSections().get(0).getDistance()).isEqualTo(10)
+        );
     }
 }
