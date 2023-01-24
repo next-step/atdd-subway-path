af8156b3f2c5f476386392323230e5f285cf239d
michol.lee <lovethefeel@gmail.com>
2023-01-24 11:27:55 +0900

    refactor: 호선의 대한 정보는 객체로 분리

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -1,5 +1,7 @@
 package nextstep.subway.domain;
 
+import org.springframework.util.CollectionUtils;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,45 +11,64 @@ public class Line {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String color;
+
+    @Embedded
+    private LineInfo lineInfo;
 
     @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
     private List<Section> sections = new ArrayList<>();
 
-    public Line() {
+    public LineInfo getLineInfo() {
+        return lineInfo;
     }
 
-    public Line(String name, String color) {
-        this.name = name;
-        this.color = color;
+    protected Line() {
+    }
+
+    public Line(final LineInfo lineInfo) {
+        this.lineInfo = lineInfo;
     }
 
     public Long getId() {
         return id;
     }
 
+
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
     public List<Section> getSections() {
         return sections;
+    }
+
+    public void addSection(final Section section) {
+        this.sections.add(section);
+    }
+
+    public void removeSection(final Station downStation) {
+
+        validateSections();
+        final int index = this.sections.size() - 1;
+        validateMatchStation(downStation, index);
+
+        this.sections.remove(index);
+    }
+
+    private void validateMatchStation(final Station downStation, final int index) {
+        final Section section = this.sections.get(index);
+        if (!section.getDownStation().equals(downStation)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateSections() {
+        if (CollectionUtils.isEmpty(this.sections)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void updateLine(final LineInfo lineInfo) {
+        this.lineInfo = lineInfo;
     }
 }
