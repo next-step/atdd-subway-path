c0be6bf5a681c74ad711f2d67384c8c005671e1b
seondongpyo <seondongpyo@gmail.com>
2023-02-02 15:49:12 +0900

    test: 지하철 노선 목록을 조회한다

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -14,6 +14,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -28,19 +30,23 @@ class LineServiceMockTest {
 
     private LineService lineService;
 
+    private Line 분당선;
+    private Station 수서역;
+    private Station 복정역;
+
     @BeforeEach
     void setup() {
         lineService = new LineService(lineRepository, stationService);
+
+        분당선 = new Line(1L, "분당선", "yellow");
+        수서역 = new Station(1L, "수서역");
+        복정역 = new Station(2L, "복정역");
     }
 
     @DisplayName("새로운 지하철 구간을 등록한다.")
     @Test
     void addSection() {
         // given
-        Line 분당선 = new Line(1L, "분당선", "yellow");
-        Station 수서역 = new Station(1L, "수서역");
-        Station 복정역 = new Station(2L, "복정역");
-
         when(lineRepository.findById(분당선.getId())).thenReturn(Optional.of(분당선));
         when(stationService.findById(수서역.getId())).thenReturn(수서역);
         when(stationService.findById(복정역.getId())).thenReturn(복정역);
@@ -51,5 +57,18 @@ class LineServiceMockTest {
         // then
         LineResponse response = lineService.findById(분당선.getId());
         assertThat(response.getStations()).hasSize(2);
+    }
+
+    @DisplayName("지하철 노선 목록을 조회한다.")
+    @Test
+    void showLines() {
+        // given
+        when(lineRepository.findAll()).thenReturn(Collections.singletonList(분당선));
+
+        // when
+        List<LineResponse> response = lineService.showLines();
+
+        // then
+        assertThat(response).hasSize(1);
     }
 }
