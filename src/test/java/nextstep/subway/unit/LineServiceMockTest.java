532a94e0886faa372ab81724efc5bfa71e756d6c
seondongpyo <seondongpyo@gmail.com>
2023-02-02 15:52:02 +0900

    test: 새로운 지하철 노선을 등록한다

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -2,6 +2,7 @@ package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.LineRequest;
 import nextstep.subway.applicaion.dto.LineResponse;
 import nextstep.subway.applicaion.dto.SectionRequest;
 import nextstep.subway.domain.Line;
@@ -19,6 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,16 +33,22 @@ class LineServiceMockTest {
     private LineService lineService;
 
     private Line 분당선;
+    private Line 신분당선;
     private Station 수서역;
     private Station 복정역;
+    private Station 판교역;
+    private Station 광교역;
 
     @BeforeEach
     void setup() {
         lineService = new LineService(lineRepository, stationService);
 
         분당선 = new Line(1L, "분당선", "yellow");
+        신분당선 = new Line(2L, "신분당선", "red");
         수서역 = new Station(1L, "수서역");
         복정역 = new Station(2L, "복정역");
+        판교역 = new Station(3L, "판교역");
+        광교역 = new Station(4L, "광교역");
     }
 
     @DisplayName("새로운 지하철 구간을 등록한다.")
@@ -57,6 +65,21 @@ class LineServiceMockTest {
         // then
         LineResponse response = lineService.findById(분당선.getId());
         assertThat(response.getStations()).hasSize(2);
+    }
+
+    @DisplayName("새로운 지하철 노선을 등록한다.")
+    @Test
+    void saveLine() {
+        // given
+        when(lineRepository.save(any(Line.class))).thenReturn(신분당선);
+        when(stationService.findById(판교역.getId())).thenReturn(판교역);
+        when(stationService.findById(광교역.getId())).thenReturn(광교역);
+
+        // when
+        lineService.saveLine(new LineRequest(신분당선.getName(), 신분당선.getColor(), 판교역.getId(), 광교역.getId(), 20));
+
+        // then
+        assertThat(신분당선.getStations()).hasSize(2);
     }
 
     @DisplayName("지하철 노선 목록을 조회한다.")
