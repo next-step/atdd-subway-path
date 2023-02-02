2d083d5c92d2670b1aa43c94ed27b48efa92c4ab
seondongpyo <seondongpyo@gmail.com>
2023-02-02 16:48:16 +0900

    test: 새로운 지하철 구간을 추가한다

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,33 +1,56 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Transactional
-public class LineServiceTest {
+class LineServiceTest {
+
     @Autowired
     private StationRepository stationRepository;
+
     @Autowired
     private LineRepository lineRepository;
 
     @Autowired
     private LineService lineService;
 
+    private Line 분당선;
+    private Station 수서역;
+    private Station 복정역;
+
+    @BeforeEach
+    void setup() {
+        분당선 = new Line( "분당선", "yellow");
+        수서역 = new Station( "수서역");
+        복정역 = new Station( "복정역");
+    }
+
+    @DisplayName("새로운 지하철 구간을 추가한다.")
     @Test
     void addSection() {
         // given
-        // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        stationRepository.save(수서역);
+        stationRepository.save(복정역);
+        lineRepository.save(분당선);
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(분당선.getId(), new SectionRequest(수서역.getId(), 복정역.getId(), 5));
 
         // then
-        // line.getSections 메서드를 통해 검증
+        assertThat(분당선.getSections()).hasSize(1);
     }
 }
