2e0e3d4bc04d7e8a64235ad1284145a3ea0e9047
michol.lee <lovethefeel@gmail.com>
2023-02-05 00:18:25 +0900

    refactor: 역 응답 객체 변환 로직 추가

--- src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
@@ -1,5 +1,13 @@
 package nextstep.subway.applicaion.dto;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StationResponse {
     private Long id;
     private String name;
@@ -7,9 +15,32 @@ public class StationResponse {
     public StationResponse() {
     }
 
-    public StationResponse(Long id, String name) {
+    public StationResponse(final Long id, final String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public static StationResponse createStationResponse(final Station station) {
+        return new StationResponse(
+                station.getId(),
+                station.getName()
+        );
+    }
+
+    public static List<StationResponse> createStationResponses(final Line line) {
+        if (line.getSections().getSections().isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<Station> stations = line.getSections().getSections().stream()
+                .map(Section::getDownStation)
+                .collect(Collectors.toList());
+
+        stations.add(0, line.getSections().getSections().get(0).getUpStation());
+
+        return stations.stream()
+                .map(StationResponse::createStationResponse)
+                .collect(Collectors.toList());
     }
 
     public Long getId() {
