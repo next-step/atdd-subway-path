1aaec4539175198e88eae748e2c0ff2635902ee1
michol.lee <lovethefeel@gmail.com>
2023-02-05 00:15:50 +0900

    refactor: 노선 요청/응답 생성자 및 정적 팩토리 메소드 생성 리팩토링

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -1,11 +1,30 @@
 package nextstep.subway.applicaion.dto;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
+
+import javax.validation.constraints.NotBlank;
+
 public class LineRequest {
+    @NotBlank(message = "노선의 이름은 필수값입니다.")
     private String name;
+    @NotBlank(message = "노선의 색깔은 필수값입니다.")
     private String color;
     private Long upStationId;
     private Long downStationId;
     private int distance;
+
+    public Line toEntity() {
+        return new Line(this.name, this.color);
+    }
+
+    public Line toEntityAddStation(final Station upStation, final Station downStation) {
+        return new Line(this.name, this.color, upStation, downStation, this.distance);
+    }
+
+    public boolean canAddSection() {
+        return this.upStationId != null && this.downStationId != null && this.distance != 0;
+    }
 
     public String getName() {
         return name;
--- src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -1,18 +1,35 @@
 package nextstep.subway.applicaion.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LineResponse {
     private Long id;
     private String name;
     private String color;
-    private List<StationResponse> stations;
+    @JsonProperty("stations")
+    private List<StationResponse> stationResponses;
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
+    public LineResponse(final Long id, final String name, final String color, final List<StationResponse> stationResponses) {
         this.id = id;
         this.name = name;
         this.color = color;
-        this.stations = stations;
+        this.stationResponses = stationResponses;
+    }
+
+    public static LineResponse createResponse(final Line saveLine) {
+        final List<StationResponse> stationResponses = convertToStationResponse(saveLine.convertToStation());
+        return new LineResponse(saveLine.getId(), saveLine.getName(), saveLine.getColor(), stationResponses);
+    }
+
+    private static List<StationResponse> convertToStationResponse(final List<Station> stations) {
+        return stations.stream()
+                .map(StationResponse::createStationResponse)
+                .collect(Collectors.toList());
     }
 
     public Long getId() {
@@ -27,8 +44,8 @@ public class LineResponse {
         return color;
     }
 
-    public List<StationResponse> getStations() {
-        return stations;
+    public List<StationResponse> getStationResponses() {
+        return stationResponses;
     }
 }
 
