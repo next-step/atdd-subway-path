8d94b4237a9952364fdced8b5dfc5a514bc5ea1e
bactoria(황준오) <bactoria.dev@kakaocorp.com>
2023-02-04 22:00:12 +0900

    test:: step0 테스트 추가

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,28 +1,48 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
+
 public class LineServiceMockTest {
     @Mock
     private LineRepository lineRepository;
     @Mock
     private StationService stationService;
 
+    @InjectMocks
+    private LineService lineService;
+
     @Test
     void addSection() {
         // given
-        // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        Long lineId = 1L;
+        Station 강남역 = new Station("강남역");
+        Station 양재역 = new Station("양재역");
+
+        when(lineRepository.findById(lineId)).thenReturn(Optional.of(new Line("신분당선", "빨강")));
+        when(stationService.findById(1L)).thenReturn(강남역);
+        when(stationService.findById(2L)).thenReturn(양재역);
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(lineId, new SectionRequest(1L, 2L, 10));
 
         // then
-        // line.findLineById 메서드를 통해 검증
+        assertThat(lineService.findById(1L).getStations()).hasSize(2);
     }
 }
--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,17 +1,69 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 class LineTest {
-    @Test
-    void addSection() {
+
+    private Line 신분당선;
+    private Station 강남역;
+    private Station 양재역;
+    private Station 양재시민의숲역;
+
+    @BeforeEach
+    void setUp() {
+        신분당선 = new Line();
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        양재시민의숲역 = new Station("양재시민의숲역");
     }
 
     @Test
-    void getStations() {
+    void 노선에_구간을_추가한다() {
+        // when
+        신분당선.addSection(강남역, 양재역, 10);
+
+        // then
+        assertAll(() -> {
+                    assertThat(신분당선.getSections()).hasSize(1);
+                    assertThat(신분당선.getSections().get(0).getUpStation()).isEqualTo(강남역);
+                    assertThat(신분당선.getSections().get(0).getDownStation()).isEqualTo(양재역);
+                }
+        );
     }
 
     @Test
-    void removeSection() {
+    void 노선에서_역을_조회한다() {
+        // given
+        신분당선.addSection(강남역, 양재역, 10);
+        신분당선.addSection(양재역, 양재시민의숲역, 10);
+
+        // when
+        var result = 신분당선.getStations();
+
+        // then
+        assertThat(result).containsExactly(강남역, 양재역, 양재시민의숲역);
+    }
+
+    @Test
+    void 노선에서_구간을_삭제한다() {
+        // given
+        신분당선.addSection(강남역, 양재역, 10);
+        신분당선.addSection(양재역, 양재시민의숲역, 10);
+
+        // when
+        신분당선.removeSection(양재시민의숲역);
+
+        // then
+        assertAll(() -> {
+                    assertThat(신분당선.getSections()).hasSize(1);
+                    assertThat(신분당선.getStations()).containsExactly(강남역, 양재역);
+                }
+        );
     }
 }
