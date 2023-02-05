aacd9d2369728e97c6d581553684b24b2d4d832a
michol.lee <lovethefeel@gmail.com>
2023-02-05 22:54:58 +0900

    refactor: 코드리뷰 반영 - 사용하지 않는 메소드 제거

--- src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationResponse.java
@@ -1,18 +1,12 @@
 package nextstep.subway.applicaion.dto;
 
-import nextstep.subway.domain.Line;
-import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class StationResponse {
     private Long id;
     private String name;
 
-    public StationResponse() {
+    private StationResponse() {
     }
 
     public StationResponse(final Long id, final String name) {
@@ -25,22 +19,6 @@ public class StationResponse {
                 station.getId(),
                 station.getName()
         );
-    }
-
-    public static List<StationResponse> createStationResponses(final Line line) {
-        if (line.getSections().getSections().isEmpty()) {
-            return Collections.emptyList();
-        }
-
-        List<Station> stations = line.getSections().getSections().stream()
-                .map(Section::getDownStation)
-                .collect(Collectors.toList());
-
-        stations.add(0, line.getSections().getSections().get(0).getUpStation());
-
-        return stations.stream()
-                .map(StationResponse::createStationResponse)
-                .collect(Collectors.toList());
     }
 
     public Long getId() {
