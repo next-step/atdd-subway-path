cad6e7aa73ede78f1817f2e7daf8e9b6385b48d2
hiblue02 <hiblue02@gmail.com>
2023-02-07 00:08:30 +0900

    refactor(LineServiceMockTest): Mock 테스트 리팩터링

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -30,13 +30,10 @@ class LineServiceMockTest {
     void addSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
-        Long 이호선 = 1L;
-        Long 삼성역 = 1L;
-        Long 역삼역 = 2L;
+        Line 이호선 = createLineWithMock(new Line(1L, "2호선", "green"));
+        Station 삼성역 = createStationWithMock(new Station(1L, "삼성역"));
+        Station 역삼역 = createStationWithMock(new Station(2L, "역삼역"));
 
-        when(lineRepository.findById(이호선)).thenReturn(Optional.of(new Line("2호선", "green")));
-        when(stationService.findById(삼성역)).thenReturn(new Station("삼성역"));
-        when(stationService.findById(역삼역)).thenReturn(new Station("역삼역"));
         LineService lineService = new LineService(lineRepository, stationService);
 
         // when
@@ -45,27 +42,24 @@ class LineServiceMockTest {
 
         // then
         // line.findLineById 메서드를 통해 검증
-        Line line = lineRepository.findById(이호선).orElseThrow();
-        assertAll(() -> assertThat(line.getSections().size()).isEqualTo(1)
-                , () -> assertThat(line.getAllStations().stream().map(Station::getName))
+        assertAll(() -> assertThat(이호선.getSections().size()).isEqualTo(1)
+                , () -> assertThat(이호선.getAllStations().stream().map(Station::getName))
                         .containsOnly("삼성역", "역삼역")
         );
     }
+
 
     @Test
     @DisplayName("구간 삭제")
     void deleteSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
-        Long 이호선 = 1L;
-        Long 삼성역 = 1L;
-        Long 선릉역 = 2L;
-        Long 역삼역 = 3L;
+        Line 이호선 = createLineWithMock(new Line(1L, "2호선", "green"));
 
-        when(lineRepository.findById(이호선)).thenReturn(Optional.of(new Line("2호선", "green")));
-        when(stationService.findById(삼성역)).thenReturn(new Station("삼성역"));
-        when(stationService.findById(선릉역)).thenReturn(new Station("선릉역"));
-        when(stationService.findById(역삼역)).thenReturn(new Station("역삼역"));
+        Station 삼성역 = createStationWithMock(new Station(1L, "삼성역"));
+        Station 선릉역 = createStationWithMock(new Station(2L, "선릉역"));
+        Station 역삼역 = createStationWithMock(new Station(3L, "역삼역"));
+
         LineService lineService = new LineService(lineRepository, stationService);
 
         addSection(lineService, 이호선, 삼성역, 선릉역);
@@ -73,19 +67,28 @@ class LineServiceMockTest {
 
         // when
         // lineService.deleteSection 호출
-        lineService.deleteSection(이호선, 역삼역);
+        lineService.deleteSection(이호선.getId(), 역삼역.getId());
 
         // then
         // line.getSections 메서드를 통해 검증
-        Line line = lineRepository.findById(이호선).orElseThrow();
-        assertAll(() -> assertThat(line.getSections().size()).isEqualTo(1)
-                , () -> assertThat(line.getAllStations().stream().map(Station::getName))
+        assertAll(() -> assertThat(이호선.getSections().size()).isEqualTo(1)
+                , () -> assertThat(이호선.getAllStations().stream().map(Station::getName))
                         .containsOnly("삼성역", "선릉역")
         );
     }
 
-    private static void addSection(LineService lineService, Long lineId, Long upStationId, Long downStationId) {
-        SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, 10);
-        lineService.addSection(lineId, sectionRequest);
+    private static void addSection(LineService lineService, Line line, Station upStation, Station downStation) {
+        SectionRequest sectionRequest = new SectionRequest(upStation.getId(), downStation.getId(), 10);
+        lineService.addSection(line.getId(), sectionRequest);
+    }
+
+    private Station createStationWithMock(Station station) {
+        when(stationService.findById(station.getId())).thenReturn(station);
+        return station;
+    }
+
+    private Line createLineWithMock(Line line) {
+        when(lineRepository.findById(line.getId())).thenReturn(Optional.of(line));
+        return line;
     }
 }
