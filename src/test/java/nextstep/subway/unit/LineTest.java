463a671cd0bfcc0c2e373d524f6dc5b1a98063f2
cafitac <cafitac99@gmail.com>
2023-02-07 21:41:13 +0900

    test: Line 도메인에 대한 단위 테스트 코드를 작성합니다

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -5,37 +5,61 @@ import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
 
     @Test
     void addSection() {
-//        final Station 강남역 = new Station("강남역");
-//        final Station 역삼역 = new Station("역삼역");
-//        final Station 선릉역 = new Station("선릉역");
-//
-//        final Line 이호선 = new Line("2호선", "bg-green-600");
-//        final Section 강남역_역삼역_구간 = new Section(이호선, 강남역, 역삼역, 10);
-//        final Section 역삼역_선릉역_구간 = new Section(이호선, 역삼역, 선릉역, 10);
-//        이호선.getSections().add(강남역_역삼역_구간);
-//        이호선.getSections().add(역삼역_선릉역_구간);
-//
-//        assertThat(이호선.getSections()).containsAnyOf(강남역_역삼역_구간, 역삼역_선릉역_구간);
+        // given
+        final Station 강남역 = new Station("강남역");
+        final Station 역삼역 = new Station("역삼역");
+        final Station 선릉역 = new Station("선릉역");
+        final Line 이호선 = new Line("2호선", "bg-green-600");
+
+        // when
+        이호선.addSection(강남역, 역삼역, 10);
+        이호선.addSection(역삼역, 선릉역, 10);
+
+
+        // then
+        final Section 강남역_역삼역_구간 = new Section(이호선, 강남역, 역삼역, 10);
+        final Section 역삼역_선릉역_구간 = new Section(이호선, 역삼역, 선릉역, 10);
+        assertThat(이호선.getSections()).containsAnyOf(강남역_역삼역_구간, 역삼역_선릉역_구간);
     }
 
     @Test
     void getStations() {
-//        final Station 강남역 = new Station("강남역");
-//        final Station 역삼역 = new Station("역삼역");
-//
-//        final Line 이호선 = new Line("2호선", "bg-green-600");
-//        final Section 강남역_역삼역_구간 = new Section(이호선, 강남역, 역삼역, 10);
-//
-//        이호선.
+        // given
+        final Station 강남역 = new Station("강남역");
+        final Station 역삼역 = new Station("역삼역");
+        final Line 이호선 = new Line("2호선", "bg-green-600");
+        이호선.addSection(강남역, 역삼역, 10);
+
+        // when
+        final List<Station> stations = 이호선.getStations();
+
+        // then
+        assertThat(stations).containsAnyOf(강남역, 역삼역);
     }
 
     @Test
     void removeSection() {
+        // given
+        final Station 강남역 = new Station("강남역");
+        final Station 역삼역 = new Station("역삼역");
+        final Station 선릉역 = new Station("선릉역");
+        final Line 이호선 = new Line("2호선", "bg-green-600");
+        이호선.addSection(강남역, 역삼역, 10);
+        이호선.addSection(역삼역, 선릉역, 10);
+
+        // when
+        이호선.removeSection(선릉역);
+
+        // then
+        final List<Station> stations = 이호선.getStations();
+        assertThat(stations).containsOnly(강남역, 역삼역);
     }
 }
