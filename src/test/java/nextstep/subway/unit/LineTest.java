e0403f17f4b355c605eb8bddfb379e93815a4626
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-02-08 02:56:23 +0900

    test(Line): 구간 제거 단위 테스트

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -2,6 +2,7 @@ package nextstep.subway.unit;
 
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,14 +26,32 @@ class LineTest {
 
         // Then
         sections = line.getSections();
-        assertThat(sections).hasSize( beforeSize + 1);
+        assertThat(sections).hasSize(beforeSize + 1);
     }
 
     @Test
     void getStations() {
     }
 
+    @DisplayName("지하철노선의 구간을 제거한다.")
     @Test
     void removeSection() {
+        // Given
+        Station 강남역 = new Station("강남역");
+        Station 삼성역 = new Station("삼성역");
+
+        Line line = new Line("2호선", "green");
+        List<Section> sections = line.getSections();
+
+        sections.add(new Section(line, 강남역, 삼성역, 10));
+        int beforeSize = sections.size();
+        int indexOfTailSection = sections.size() - 1;
+
+        // When
+        sections.remove(indexOfTailSection);
+
+        // Then
+        sections = line.getSections();
+        assertThat(sections).hasSize(beforeSize - 1);
     }
 }
