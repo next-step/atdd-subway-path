1fff3dbe86327cf5b3b2d4d55fb8fb88c01c12d9
cafitac <cafitac99@gmail.com>
2023-02-07 21:42:45 +0900

    feat: Line 도메인에서 section 추가, stations 목록 조회, section 삭제에 대한 기능을 작성합니다

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Entity
 public class Line {
@@ -28,6 +29,19 @@ public class Line {
         this.id = id;
         this.name = name;
         this.color = color;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Line line = (Line) o;
+        return Objects.equals(name, line.name) && Objects.equals(color, line.color) && Objects.equals(sections, line.sections);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color, sections);
     }
 
     public Long getId() {
@@ -62,16 +76,19 @@ public class Line {
         this.sections = sections;
     }
 
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        final Line line = (Line) o;
-        return Objects.equals(name, line.name) && Objects.equals(color, line.color) && Objects.equals(sections, line.sections);
+    public void addSection(final Station upStation, final Station downStation, final int distance) {
+        this.getSections().add(new Section(this, upStation, downStation, distance));
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, color, sections);
+    public List<Station> getStations() {
+        List<Station> stations = this.getSections().stream()
+                .map(Section::getDownStation)
+                .collect(Collectors.toList());
+        stations.add(0, this.getSections().get(0).getUpStation());
+        return stations;
+    }
+
+    public void removeSection(final Station station) {
+        this.getSections().remove(this.getSections().size() - 1);
     }
 }
