9917d21f0af1be2ca67519e2f35b392152046be3
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-02-08 03:22:22 +0900

    refactor: 공통 셋업 추출

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.unit;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,38 +16,43 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 @DisplayName("지하철 노선 단위 테스트")
 class LineTest {
 
+    private Line 이호선;
+    private List<Section> sections;
+    private Station 강남역;
+    private Station 삼성역;
+
+    @BeforeEach
+    void setUp() {
+        // Given
+        강남역 = new Station("강남역");
+        삼성역 = new Station("삼성역");
+
+        이호선 = new Line("2호선", "green");
+        sections = 이호선.getSections();
+
+        sections.add(new Section(이호선, 강남역, 삼성역, 10));
+    }
+
     @DisplayName("지하철노선의 구간을 추가한다.")
     @Test
     void addSection() {
-        // Given
-        Line line = new Line();
-        List<Section> sections = line.getSections();
         int beforeSize = sections.size();
 
         // When
         sections.add(new Section());
 
         // Then
-        sections = line.getSections();
+        sections = 이호선.getSections();
         assertThat(sections).hasSize(beforeSize + 1);
     }
 
     @DisplayName("지하철노선의 역을 조회한다.")
     @Test
     void getStations() {
-        // Given
-        Station 강남역 = new Station("강남역");
-        Station 삼성역 = new Station("삼성역");
-
-        Line line = new Line("2호선", "green");
-        List<Section> sections = line.getSections();
-
-        sections.add(new Section(line, 강남역, 삼성역, 10));
-
         // When
         List<Station> stations = new ArrayList<>();
-        
-        List<Section> foundSections = line.getSections();
+
+        List<Section> foundSections = 이호선.getSections();
         foundSections.forEach(section -> {
             stations.add(section.getUpStation());
             if (foundSections.indexOf(section) == foundSections.size() - 1) {
@@ -65,13 +71,6 @@ class LineTest {
     @Test
     void removeSection() {
         // Given
-        Station 강남역 = new Station("강남역");
-        Station 삼성역 = new Station("삼성역");
-
-        Line line = new Line("2호선", "green");
-        List<Section> sections = line.getSections();
-
-        sections.add(new Section(line, 강남역, 삼성역, 10));
         int beforeSize = sections.size();
         int indexOfTailSection = sections.size() - 1;
 
@@ -79,7 +78,7 @@ class LineTest {
         sections.remove(indexOfTailSection);
 
         // Then
-        sections = line.getSections();
+        sections = 이호선.getSections();
         assertThat(sections).hasSize(beforeSize - 1);
     }
 }
