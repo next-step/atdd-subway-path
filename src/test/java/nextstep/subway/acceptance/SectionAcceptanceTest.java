5773d6c7266f2aa5f7eb5d1ad788c6eca0e7ddb3
hiblue02 <hiblue02@gmail.com>
2023-02-07 23:41:55 +0900

    test(SectionAcceptanceTest) : 구간 추가 인수 테스트 작성

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -53,6 +53,53 @@ class SectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
+     * When 구간 사이의 구간을 추가한다.
+     * Then 노선에 새로운 구간이 추가된다.
+     */
+    @DisplayName("지하철 노선에 사이에 구간 등록")
+    @Test
+    void addLineSectionBetween(){
+        //when
+        Long 논현역 = 지하철역_생성_요청("논현역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 논현역,3L));
+        //then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 논현역, 양재역);
+        assertThat(response.jsonPath().getList("sections.distance", Long.class)).containsOnly(3l,3l);
+    }
+
+
+    /**
+     * When 길이가 더 긴 구간을 구간 사이에 추가한다.
+     * Then 오류가 발생한다.
+     */
+    @DisplayName("지하철 노선에 사이에 더 긴 구간 등록")
+    @Test
+    void addLineSectionBetweenOverDistance(){
+        //when
+        Long 논현역 = 지하철역_생성_요청("논현역").jsonPath().getLong("id");
+        //then
+        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 논현역, 10L));
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+    /**
+     * When 겹치는 역이 없는 구간을 추가한다.
+     * Then 오류가 발생한다..
+     */
+    @DisplayName("지하철 노선에 사이에 겹치지 않는 구간 등록")
+    @Test
+    void addLineSectionBetweenNotOverlapStation(){
+        //when
+        Long 논현역 = 지하철역_생성_요청("논현역").jsonPath().getLong("id");
+        Long 옥수역 = 지하철역_생성_요청("옥수역").jsonPath().getLong("id");
+
+        //then
+        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(옥수역, 논현역));
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    /**
      * Given 지하철 노선에 새로운 구간 추가를 요청 하고
      * When 지하철 노선의 마지막 구간 제거를 요청 하면
      * Then 노선에 구간이 제거된다
@@ -89,6 +136,14 @@ class SectionAcceptanceTest extends AcceptanceTest {
         params.put("upStationId", upStationId + "");
         params.put("downStationId", downStationId + "");
         params.put("distance", 6 + "");
+        return params;
+    }
+
+    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, Long distance) {
+        Map<String, String> params = new HashMap<>();
+        params.put("upStationId", upStationId + "");
+        params.put("downStationId", downStationId + "");
+        params.put("distance", distance + "");
         return params;
     }
 }
