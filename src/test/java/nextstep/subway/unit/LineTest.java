fb849fa2aa0212f22f8e3e9dea1b80a3729ecffb
yunhwan <pyh8618@gmail.com>
2023-02-08 00:38:57 +0900

    refactor : lineTest 중복 given 메서드 추출

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -3,44 +3,51 @@ package nextstep.subway.unit;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
-import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
+
+    private Line 신분당선;
+
+    private Station 강남역;
+    private Station 양재역;
+
+    private Section 강남_양재_구간;
+
+    private int distance = 10;
+
+    @BeforeEach
+    void setUp() {
+        신분당선 = new Line("신분당선", "bg-red-900");
+        distance = 10;
+
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        강남_양재_구간 = new Section(신분당선, 강남역, 양재역, distance);
+    }
+
     @Test
     void addSection() {
         // given
-        Line 신분당선 = new Line("신분당선", "bg-red-900");
-        int distance = 10;
-
-        Station 강남역 = new Station("강남역");
-        Station 양재역 = new Station("양재역");
-        Section section = new Section(신분당선, 강남역, 양재역, distance);
 
         // when
-        신분당선.addSection(section);
+        신분당선.addSection(강남_양재_구간);
 
         // then
-        assertThat(신분당선.getSections()).containsExactlyElementsOf(List.of(section));
+        assertThat(신분당선.getSections()).containsExactlyElementsOf(List.of(강남_양재_구간));
     }
 
     @Test
     void getStations() {
         // given
-        Line 신분당선 = new Line("신분당선", "bg-red-900");
-        int distance = 10;
-
-        Station 강남역 = new Station("강남역");
-        Station 양재역 = new Station("양재역");
-        Section section = new Section(신분당선, 강남역, 양재역, distance);
-        신분당선.getSections().add(section);
+        신분당선.getSections().add(강남_양재_구간);
 
         // when
         List<Station> 신분당선_지하철 = 신분당선.getStations();
@@ -53,13 +60,7 @@ class LineTest {
     @DisplayName("Section을 하나만 가지고 있는 Line 삭제 테스트")
     void removeSectionLineHasOneSection() {
         // given
-        Line 신분당선 = new Line("신분당선", "bg-red-900");
-        int distance = 10;
-
-        Station 강남역 = new Station("강남역");
-        Station 양재역 = new Station("양재역");
-        Section section = new Section(신분당선, 강남역, 양재역, distance);
-        신분당선.getSections().add(section);
+        신분당선.getSections().add(강남_양재_구간);
 
         // when
         신분당선.removeSection(양재역);
@@ -73,14 +74,9 @@ class LineTest {
     @DisplayName("Section을 하나이상 가지고 있는 Line 삭제 테스트")
     void removeSection() {
         // given
-        Line 신분당선 = new Line("신분당선", "bg-red-900");
-        int distance = 10;
-
-        Station 강남역 = new Station("강남역");
-        Station 양재역 = new Station("양재역");
         Station 양재시민의숲역 = new Station("양재시민의숲역");
-        Section 강남_양재_구간 = new Section(신분당선, 강남역, 양재역, distance);
         Section 양재_양재시민의숲_구간 = new Section(신분당선, 양재역, 양재시민의숲역, distance);
+        
         신분당선.getSections().add(강남_양재_구간);
         신분당선.getSections().add(양재_양재시민의숲_구간);
 
