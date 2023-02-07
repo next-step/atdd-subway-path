ce94046831aef47eece43b8ba6eefd42be5a9837
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-02-08 03:17:56 +0900

    test(Line): 노선 역 조회 단위 테스트

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -6,9 +6,11 @@ import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("지하철 노선 단위 테스트")
 class LineTest {
@@ -29,8 +31,34 @@ class LineTest {
         assertThat(sections).hasSize(beforeSize + 1);
     }
 
+    @DisplayName("지하철노선의 역을 조회한다.")
     @Test
     void getStations() {
+        // Given
+        Station 강남역 = new Station("강남역");
+        Station 삼성역 = new Station("삼성역");
+
+        Line line = new Line("2호선", "green");
+        List<Section> sections = line.getSections();
+
+        sections.add(new Section(line, 강남역, 삼성역, 10));
+
+        // When
+        List<Station> stations = new ArrayList<>();
+        
+        List<Section> foundSections = line.getSections();
+        foundSections.forEach(section -> {
+            stations.add(section.getUpStation());
+            if (foundSections.indexOf(section) == foundSections.size() - 1) {
+                stations.add(section.getDownStation());
+            }
+        });
+
+        // Then
+        assertAll(
+                () -> assertThat(stations).hasSize(2),
+                () -> assertThat(stations).containsExactly(강남역, 삼성역)
+        );
     }
 
     @DisplayName("지하철노선의 구간을 제거한다.")
