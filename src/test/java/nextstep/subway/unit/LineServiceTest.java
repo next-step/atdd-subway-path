c30df7f5a53f006219782d8130f1e0549390e2c3
yunhwan <pyh8618@gmail.com>
2023-02-09 01:11:44 +0900

    refactor : line생성 시 section추가하는 부분 도메인 로직 사용하도록 수정
    - line생성 테스트 코드 추가

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,15 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -19,15 +22,40 @@ public class LineServiceTest {
     @Autowired
     private LineService lineService;
 
+    private Line 신분당선;
+
+    private Station 강남역;
+    private Station 양재역;
+
+    private Section 강남_양재_구간;
+
+    private int distance = 10;
+
+    @BeforeEach
+    void setUp() {
+        신분당선 = new Line("신분당선", "bg-red-900");
+        distance = 10;
+
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        강남_양재_구간 = new Section(신분당선, 강남역, 양재역, distance);
+    }
+
     @Test
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        강남역 = stationRepository.save(강남역);
+        양재역 = stationRepository.save(양재역);
+        Line line = lineRepository.save(신분당선);
+        SectionRequest sectionRequest = new SectionRequest(강남역.getId(), 양재역.getId(), distance);
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(line.getId(), sectionRequest);
 
         // then
         // line.getSections 메서드를 통해 검증
+        신분당선 = lineRepository.findById(line.getId()).orElseThrow();
+        assertThat(line.getSections()).containsExactlyElementsOf(신분당선.getSections());
     }
 }
