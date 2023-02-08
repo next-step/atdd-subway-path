84b41af250214163e077b045ca198f73d81f49e0
Sangmin <sm9171@nate.com>
2023-02-09 01:07:37 +0900

    feat: 단위 테스트 작성

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -2,7 +2,10 @@ package nextstep.subway.domain;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Entity
 public class Line {
@@ -49,5 +52,33 @@ public class Line {
 
     public List<Section> getSections() {
         return sections;
+    }
+
+    public void addSection(Station upStation, Station downStation, int distance) {
+        this.sections.add(new Section(this, upStation, downStation, distance));
+    }
+
+    public List<Station> getStations() {
+        return this.sections.stream()
+                .map(s -> List.of(s.getUpStation(), s.getDownStation()))
+                .flatMap(Collection::stream)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public void removeSection(Station downEndStation) {
+        if (!this.getSections().get(this.getSections().size() - 1).getDownStation().equals(downEndStation)) {
+            throw new IllegalArgumentException();
+        }
+        this.getSections().remove(this.getSections().size() - 1);
+    }
+
+    public void updateLine(String name, String color) {
+        if (name != null) {
+            this.name = name;
+        }
+        if (color != null) {
+            this.color = color;
+        }
     }
 }
--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,12 +1,22 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.anyLong;
+import static org.mockito.BDDMockito.given;
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
     @Mock
@@ -14,15 +24,23 @@ public class LineServiceMockTest {
     @Mock
     private StationService stationService;
 
+    @InjectMocks
+    private LineService lineService;
+
     @Test
     void addSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        given(stationService.findById(anyLong())).willReturn(new Station("상행역"));
+        given(stationService.findById(anyLong())).willReturn(new Station("하행역"));
+        given(lineRepository.findById(anyLong())).willReturn(Optional.of(new Line("1호선", "파란색")));
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(1L, new SectionRequest(1L, 2L, 10));
 
         // then
-        // line.findLineById 메서드를 통해 검증
+        // lineService.findLineById 메서드를 통해 검증
+        assertThat(lineService.findLineById(1L).getSections()).isNotEmpty();
     }
 }
