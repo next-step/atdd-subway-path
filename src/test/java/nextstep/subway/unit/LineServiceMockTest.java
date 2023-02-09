10ae45a46f6d06c41710a5e827b0e471d6592354
jeongyolee <jeongyolee@ebay.com>
2023-02-09 21:39:45 +0900

    test : add LineService Unit Test with Mockito

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -2,6 +2,8 @@ package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.LineRequest;
+import nextstep.subway.applicaion.dto.LineResponse;
 import nextstep.subway.applicaion.dto.SectionRequest;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
@@ -18,10 +20,10 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class LineServiceMockTest {
     @Mock
     private LineRepository lineRepository;
@@ -56,5 +58,56 @@ public class LineServiceMockTest {
             assertThat(line.getSections()).hasSize(1);
             assertThat(line.getStations()).containsExactlyElementsOf(List.of(강남역, 역삼역));
         });
+    }
+
+    @Test
+    void saveLine() {
+        //given
+        LineRequest lineRequest = new LineRequest("이호선", "green", 강남역.getId(), 역삼역.getId(), 10);
+        when(lineRepository.save(any(Line.class))).thenReturn(이호선);
+        when(stationService.findById(강남역.getId())).thenReturn(강남역);
+        when(stationService.findById(역삼역.getId())).thenReturn(역삼역);
+
+        //when
+        LineResponse response = lineService.saveLine(lineRequest);
+
+        //then
+        verify(lineRepository, times(1)).save(any());
+        assertThat(response).isNotNull();
+        assertThat(response.getId()).isEqualTo(이호선.getId());
+    }
+
+    @Test
+    void findById() {
+        //given
+        when(lineRepository.findById(이호선.getId())).thenReturn(Optional.of(이호선));
+
+        //when
+        LineResponse response = lineService.findById(이호선.getId());
+
+        //then
+        assertThat(response).isNotNull();
+        assertThat(response.getId()).isEqualTo(이호선.getId());
+    }
+
+    @Test
+    void showLine() {
+        //given
+        when(lineRepository.findAll()).thenReturn(List.of(이호선));
+
+        //when
+        List<LineResponse> lines = lineService.showLines();
+
+        //then
+        assertThat(lines).hasSize(1);
+    }
+
+    @Test
+    void deleteLine() {
+        //when
+        lineService.deleteLine(이호선.getId());
+
+        //then
+        verify(lineRepository, times(1)).deleteById(이호선.getId());
     }
 }
