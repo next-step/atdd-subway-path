f068aea128ee3d5e479d9ce1f6351cb1bffd19b1
Yunsik-Choi <choiys0311@gmail.com>
2023-02-09 19:53:22 +0900

    ⚡feat : 노선에 구간 추가하는 기능 구현

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,10 +1,37 @@
 package nextstep.subway.unit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LineTest {
+
+    private int distance;
+    private Line line;
+    private Station upStation;
+    private Station downStation;
+
+    @BeforeEach
+    void setUp() {
+        this.distance = 10;
+        this.line = new Line("2호선", "bg-red-500");
+        this.upStation = new Station("강남역");
+        this.downStation = new Station("역삼역");
+    }
+
+    @DisplayName("노선에 구간을 추가한다.")
     @Test
     void addSection() {
+        Section section = new Section(line, upStation, downStation, distance);
+
+        line.addSection(section);
+
+        assertThat(line.getSections()).containsExactly(section);
     }
 
     @Test
