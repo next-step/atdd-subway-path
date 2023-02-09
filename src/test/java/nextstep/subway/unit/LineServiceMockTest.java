0e9a7657d1109492e0515557042d07c5ca5478ee
Yunsik-Choi <choiys0311@gmail.com>
2023-02-09 20:11:01 +0900

    ♻️ refactor : 노선에 구간 추가 로직 리팩터링

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,9 +1,24 @@
 package nextstep.subway.unit;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -14,15 +29,50 @@ public class LineServiceMockTest {
     @Mock
     private StationService stationService;
 
+    @InjectMocks
+    private LineService lineService;
+
+    private long lineId;
+    private long upStationId;
+    private long downStationId;
+    private int distance;
+    private Line line;
+    private Station upStation;
+    private Station downStation;
+
+    @BeforeEach
+    void setUp() {
+        this.lineId = 1L;
+        this.upStationId = 1L;
+        this.downStationId = 2L;
+        this.distance = 10;
+        this.line = new Line("2호선", "bg-red-500");
+        this.upStation = new Station("강남역");
+        this.downStation = new Station("역삼역");
+    }
+
+    @DisplayName("노선에 구간을 추가한다.")
     @Test
     void addSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        when(stationService.findById(upStationId)).thenReturn(upStation);
+        when(stationService.findById(downStationId)).thenReturn(downStation);
+        when(lineRepository.findById(lineId)).thenReturn(Optional.of(line));
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(lineId, new SectionRequest(upStationId, downStationId, distance));
 
         // then
         // lineService.findLineById 메서드를 통해 검증
+        List<Section> sections = lineService.findLineById(lineId).getSections();
+        Section section = sections.get(0);
+        assertAll(
+                () -> assertThat(sections).hasSize(1),
+                () -> assertThat(section.getUpStation()).isEqualTo(upStation),
+                () -> assertThat(section.getDownStation()).isEqualTo(downStation),
+                () -> assertThat(section.getDistance()).isEqualTo(distance)
+        );
     }
 }
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,8 +1,17 @@
 package nextstep.subway.unit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,15 +28,40 @@ public class LineServiceTest {
     @Autowired
     private LineService lineService;
 
+    private int distance;
+    private Line line;
+    private Station upStation;
+    private Station downStation;
+
+    @BeforeEach
+    void setUp() {
+        this.distance = 10;
+        this.line = new Line("2호선", "bg-red-500");
+        this.upStation = new Station("강남역");
+        this.downStation = new Station("역삼역");
+    }
+
+    @DisplayName("노선에 구간을 추가한다.")
     @Test
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        stationRepository.save(upStation);
+        stationRepository.save(downStation);
+        lineRepository.save(line);
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(line.getId(), new SectionRequest(upStation.getId(), downStation.getId(), distance));
 
         // then
         // line.getSections 메서드를 통해 검증
+        Section section = line.getSections().get(0);
+        assertAll(
+                () -> assertThat(line.getSections()).hasSize(1),
+                () -> assertThat(section.getUpStation()).isEqualTo(upStation),
+                () -> assertThat(section.getDownStation()).isEqualTo(downStation),
+                () -> assertThat(section.getDistance()).isEqualTo(distance)
+        );
     }
 }
