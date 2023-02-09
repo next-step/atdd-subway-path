782ab1663e60ac4234a735b5d24ea3f4ebe905f5
jeongyolee <jeongyolee@ebay.com>
2023-02-09 21:56:46 +0900

    test : add LineService Unit Test without Mockito

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,6 +1,8 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.LineRequest;
+import nextstep.subway.applicaion.dto.LineResponse;
 import nextstep.subway.applicaion.dto.SectionRequest;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
@@ -46,5 +48,92 @@ public class LineServiceTest {
             assertThat(이호선.getSections()).hasSize(1);
             assertThat(이호선.getStations()).containsExactlyElementsOf(List.of(강남역, 역삼역));
         });
+    }
+
+    @Test
+    void saveLine() {
+        //given
+        Station 강남역 = stationRepository.save(new Station( "강남역"));
+        Station 역삼역 = stationRepository.save(new Station( "역삼역"));
+        LineRequest lineRequest = new LineRequest("이호선", "green", 강남역.getId(), 역삼역.getId(), 10);
+
+        //when
+        LineResponse response = lineService.saveLine(lineRequest);
+
+        //then
+        assertThat(response).isNotNull();
+        assertThat(response.getName()).isEqualTo(lineRequest.getName());
+    }
+
+    @Test
+    void findById() {
+        //given
+        Line 이호선 = lineRepository.save(new Line("이호선", "green"));
+
+        //when
+        LineResponse response = lineService.findById(이호선.getId());
+
+        //then
+        assertThat(response).isNotNull();
+        assertThat(response.getId()).isEqualTo(이호선.getId());
+    }
+
+    @Test
+    void showLine() {
+        //given
+        Line 이호선 = lineRepository.save(new Line("이호선", "green"));
+        Line 삼호선 = lineRepository.save(new Line("삼호선", "blue"));
+
+        //when
+        List<LineResponse> lines = lineService.showLines();
+
+        //then
+        assertThat(lines).hasSize(2);
+    }
+
+    //updateLine
+    @Test
+    void updateLine() {
+        //given
+        Line 이호선 = lineRepository.save(new Line("이호선", "green"));
+        LineRequest lineRequest = new LineRequest("칠호선", "brown");
+
+        //when
+        lineService.updateLine(이호선.getId(), lineRequest);
+
+        //then
+        Line line = lineRepository.findById(이호선.getId()).get();
+        assertThat(line.getName()).isEqualTo(lineRequest.getName());
+        assertThat(line.getColor()).isEqualTo(lineRequest.getColor());
+    }
+
+    //deleteLine
+    @Test
+    void deleteLine() {
+        //given
+        Line 이호선 = lineRepository.save(new Line("이호선", "green"));
+
+        //when
+        lineService.deleteLine(이호선.getId());
+
+        //then
+        assertThat(lineRepository.findById(이호선.getId())).isEmpty();
+    }
+
+    //deleteSection
+    @Test
+    void deleteSection() {
+        //given
+        Station 강남역 = stationRepository.save(new Station( "강남역"));
+        Station 역삼역 = stationRepository.save(new Station( "역삼역"));
+        LineRequest lineRequest = new LineRequest("이호선", "green", 강남역.getId(), 역삼역.getId(), 10);
+        LineResponse 이호선 = lineService.saveLine(lineRequest);
+
+        //when
+        lineService.deleteSection(이호선.getId(), 역삼역.getId());
+
+        //then
+        LineResponse response = lineService.findById(이호선.getId());
+        assertThat(response.getStations()).isEmpty();
     }
 }
