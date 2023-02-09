3dca2f85e113180674cb5c583422f20478cbe0c4
cafitac <cafitac99@gmail.com>
2023-02-09 23:37:57 +0900

    test: SectionsTest 의 코드를 수정합니다

--- src/test/java/nextstep/subway/unit/SectionsTest.java
+++ src/test/java/nextstep/subway/unit/SectionsTest.java
@@ -1,6 +1,7 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Sections;
 import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,9 +17,11 @@ public class SectionsTest {
     private Station 강남역;
     private Station 역삼역;
     private Station 선릉역;
+    private Sections sections;
 
     @BeforeEach
     void setUp() {
+        sections = new Sections();
         이호선 = new Line(1L, "2호선", "bg-green-color");
         강남역 = new Station(1L, "강남역");
         역삼역 = new Station(1L, "역삼역");
@@ -28,17 +31,17 @@ public class SectionsTest {
     @Test
     void addSectionBetweenStations() {
         // when
-        이호선.addSection(강남역, 역삼역, 10);
-        이호선.addSection(강남역, 선릉역, 9);
+        sections.add(이호선, 강남역, 역삼역, 10);
+        sections.add(이호선, 강남역, 선릉역, 9);
 
         // then
-        assertThat(이호선.getStations()).containsExactly(강남역, 선릉역, 역삼역);
-        assertThat(이호선.getSections().stream()
+        assertThat(sections.getStations()).containsExactly(강남역, 선릉역, 역삼역);
+        assertThat(sections.getSections().stream()
                 .filter(section -> section.getUpStation().equals(강남역))
                 .filter(section -> section.getDownStation().equals(선릉역))
                 .findFirst().get()
                 .getDistance()).isEqualTo(9);
-        assertThat(이호선.getSections().stream()
+        assertThat(sections.getSections().stream()
                 .filter(section -> section.getUpStation().equals(선릉역))
                 .filter(section -> section.getDownStation().equals(역삼역))
                 .findFirst().get()
@@ -48,47 +51,47 @@ public class SectionsTest {
     @Test
     void addSectionWithLastUpStation() {
         // when
-        이호선.addSection(강남역, 역삼역, 10);
-        이호선.addSection(선릉역, 강남역, 10);
+        sections.add(이호선, 강남역, 역삼역, 10);
+        sections.add(이호선, 선릉역, 강남역, 10);
 
         // then
-        assertThat(이호선.getStations()).containsExactly(선릉역, 강남역, 역삼역);
+        assertThat(sections.getStations()).containsExactly(선릉역, 강남역, 역삼역);
     }
 
     @Test
     void addSectionWithLastDownStation() {
         // when
-        이호선.addSection(강남역, 역삼역, 10);
-        이호선.addSection(역삼역, 선릉역, 10);
+        sections.add(이호선, 강남역, 역삼역, 10);
+        sections.add(이호선, 선릉역, 강남역, 10);
 
         // then
-        assertThat(이호선.getStations()).containsExactly(강남역, 역삼역, 선릉역);
+        assertThat(sections.getStations()).containsExactly(강남역, 역삼역, 선릉역);
     }
 
     @Test
     void cannotAddSectionBetweenStationsWithInvalidDistance() {
         // given
-        이호선.addSection(강남역, 역삼역, 10);
+        sections.add(이호선, 강남역, 역삼역, 10);
 
         // when, then
         assertThatThrownBy(() -> {
-            이호선.addSection(강남역, 선릉역, 10);
+            sections.add(이호선, 강남역, 선릉역, 10);
         }).isInstanceOf(IllegalArgumentException.class);
 
         assertThatThrownBy(() -> {
-            이호선.addSection(강남역, 선릉역, 11);
+            sections.add(이호선, 강남역, 선릉역, 11);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void cannotAddSectionUpStationAndDownStationIsAlreadyExists() {
         // given
-        이호선.addSection(강남역, 역삼역, 10);
-        이호선.addSection(역삼역, 선릉역, 10);
+        sections.add(이호선, 강남역, 역삼역, 10);
+        sections.add(이호선, 역삼역, 선릉역, 10);
 
         // when, then
         assertThatThrownBy(() -> {
-            이호선.addSection(강남역, 선릉역, 3);
+            sections.add(이호선, 강남역, 선릉역, 3);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -97,22 +100,22 @@ public class SectionsTest {
         // given
         final Station 당산역 = new Station("당산역");
         final Station 선유도역 = new Station("선유도역");
-        이호선.addSection(강남역, 역삼역, 10);
-        이호선.addSection(역삼역, 선릉역, 10);
+        sections.add(이호선, 강남역, 역삼역, 10);
+        sections.add(이호선, 역삼역, 선릉역, 10);
 
         // when, then
         assertThatThrownBy(() -> {
-            이호선.addSection(당산역, 선유도역, 7);
+            sections.add(이호선, 당산역, 선유도역, 7);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void getStations() {
         // given
-        이호선.addSection(강남역, 역삼역, 10);
+        sections.add(이호선, 강남역, 역삼역, 10);
 
         // when
-        final List<Station> stations = 이호선.getStations();
+        final List<Station> stations = sections.getStations();
 
         // then
         assertThat(stations).containsAnyOf(강남역, 역삼역);
@@ -121,14 +124,14 @@ public class SectionsTest {
     @Test
     void removeSection() {
         // given
-        이호선.addSection(강남역, 역삼역, 10);
-        이호선.addSection(역삼역, 선릉역, 10);
+        sections.add(이호선, 강남역, 역삼역, 10);
+        sections.add(이호선, 역삼역, 선릉역, 10);
 
         // when
-        이호선.removeLastSection(선릉역);
+        sections.remove(선릉역);
 
         // then
-        final List<Station> stations = 이호선.getStations();
+        final List<Station> stations = sections.getStations();
         assertThat(stations).containsOnly(강남역, 역삼역);
     }
 }
