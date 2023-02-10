1c484539e28e4d53fcffe2e46c4e177d2963424d
ess234 <ess234@naver.com>
2023-02-10 23:45:01 +0900

    feat: 1단계 - 지하철 구간 추가 기능 개선
    - 구간 추가 개선 기능 구현
    - 도메인 단위 테스트 구현

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -53,7 +53,51 @@ public class Line {
     }
 
     public void addSection(Section section) {
+        isValidationSection(section);
+
         this.getSections().add(section);
+    }
+
+    public void addSection(String addTypeCd, Section section) {
+        isValidationSection(section);
+
+        if ("1".equals(addTypeCd)) {
+            Section standardSection = this.getSections().stream().filter(a -> section.getDownStation().getName().equals(a.getUpStation().getName())).findFirst().orElseThrow(IllegalArgumentException::new);
+
+            this.getSections().add(this.getSections().indexOf(standardSection), section);
+            return;
+        }
+
+        if ("2".equals(addTypeCd)) {
+            Section standardSection = this.getSections().stream().filter(a -> section.getUpStation().getName().equals(a.getUpStation().getName())).findFirst().orElseThrow(IllegalArgumentException::new);
+
+            isValidationSectionDistance(standardSection, section);
+
+            int standardIndex = this.getSections().indexOf(standardSection);
+
+            this.getSections().set(standardIndex, new Section(this, standardSection.getUpStation(), section.getDownStation(), section.getDistance()));
+            this.getSections().add(standardIndex+1, new Section(this, section.getDownStation(), standardSection.getDownStation(), standardSection.getDistance()-section.getDistance()));
+
+            return;
+        }
+    }
+
+    private void isValidationSectionDistance(Section standardSection, Section section) {
+        if (standardSection.getDistance() <= section.getDistance()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isValidationSection(Section section) {
+        if ((this.getSections().size() > 0) && isExistUpOrDownStation(section)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isExistUpOrDownStation (Section section) {
+        int existStationCnt = this.getStations().stream().filter(a -> a.getName().equals(section.getUpStation().getName())
+                || a.getName().equals(section.getDownStation().getName())).collect(Collectors.toList()).size();
+        return existStationCnt != 1;
     }
 
     public void removeSection(Station station) {
@@ -65,6 +109,10 @@ public class Line {
     }
 
     public List<Station> getStations() {
+        if (this.getSections().size() <= 0) {
+            return new ArrayList<>();
+        }
+
         List<Station> stations = this.getSections().stream()
                 .map(Section::getDownStation)
                 .collect(Collectors.toList());
--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -8,15 +8,74 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LineTest {
     @Test
     void addSection() {
         Line line = new Line("지하철노선", "bg-red-600");
-        Section section = new Section(line, new Station("지하철역1"), new Station("지하철역2"), 5);
+        Section section = new Section(line, new Station("지하철역1"), new Station("지하철역3"), 5);
         line.addSection(section);
 
         assertThat(line.getSections().contains(section)).isEqualTo(true);
+    }
+
+    @Test
+    void addSection_front() {
+        Line line = new Line("지하철노선", "bg-red-600");
+        Section oldSection = new Section(line, new Station("지하철역1"), new Station("지하철역3"), 5);
+        line.addSection(oldSection);
+
+        Section newSection = new Section(line, new Station("지하철역0"), new Station("지하철역1"), 5);
+        line.addSection("1", newSection);
+
+        assertThat(line.getStations().stream().map(Station::getName)).containsExactly("지하철역0", "지하철역1", "지하철역3");
+    }
+
+    @Test
+    void addSection_middle() {
+        Line line = new Line("지하철노선", "bg-red-600");
+        Section oldSection = new Section(line, new Station("지하철역1"), new Station("지하철역3"), 7);
+        line.addSection(oldSection);
+
+        Section newSection = new Section(line, new Station("지하철역1"), new Station("지하철역2"), 5);
+        line.addSection("2", newSection);
+
+        assertThat(line.getStations().stream().map(Station::getName)).containsExactly("지하철역1", "지하철역2", "지하철역3");
+        assertThat(line.getSections().stream().map(Section::getDistance)).containsExactly(5, 2);
+    }
+
+    @Test
+    void addSection_middle_Exception1() {
+        Line line = new Line("지하철노선", "bg-red-600");
+        Section oldSection = new Section(line, new Station("지하철역1"), new Station("지하철역3"), 7);
+        line.addSection(oldSection);
+
+        Section newSection = new Section(line, new Station("지하철역1"), new Station("지하철역2"), 7);
+
+        assertThrows(IllegalArgumentException.class, () -> line.addSection("2", newSection));
+    }
+
+    @Test
+    void addSection_middle_Exception2() {
+        Line line = new Line("지하철노선", "bg-red-600");
+        Section oldSection = new Section(line, new Station("지하철역1"), new Station("지하철역3"), 7);
+        line.addSection(oldSection);
+
+        Section newSection = new Section(line, new Station("지하철역1"), new Station("지하철역3"), 7);
+
+        assertThrows(IllegalArgumentException.class, () -> line.addSection(newSection));
+    }
+
+    @Test
+    void addSection_middle_Exception3() {
+        Line line = new Line("지하철노선", "bg-red-600");
+        Section oldSection = new Section(line, new Station("지하철역1"), new Station("지하철역3"), 7);
+        line.addSection(oldSection);
+
+        Section newSection = new Section(line, new Station("지하철역4"), new Station("지하철역5"), 7);
+
+        assertThrows(IllegalArgumentException.class, () -> line.addSection(newSection));
     }
 
     @Test
