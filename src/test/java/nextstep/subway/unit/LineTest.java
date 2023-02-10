ef8d6618aacd1aceb1b5ce85710945e6e11229e1
최현웅 <76509935+choi1204@users.noreply.github.com>
2023-02-10 22:45:21 +0900

    test : section추가 테스트 추가

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,10 +1,24 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.StationService;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class LineTest {
+
+    @Autowired
+    StationService stationService;
     @Test
+    @DisplayName("section을 추가할 수 있다.")
     void addSection() {
+        Line line = new Line("test1", "red");
+        Station firstStation = new Station();
+        Station secondStation = new Station();
+        Section section = new Section(line, firstStation, secondStation, 10);
     }
 
     @Test
