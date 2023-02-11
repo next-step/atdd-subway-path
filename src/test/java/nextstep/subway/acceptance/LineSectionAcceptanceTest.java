bdd0d7931f2c5f1c676e80d110e244b33ef8d83f
seondongpyo <seondongpyo@gmail.com>
2023-02-11 09:18:49 +0900

    refactor: 새로 추가하려는 구간의 길이가 기존 구간의 길이와 같은 경우에 대한 테스트 추가

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -1,19 +1,20 @@
 package nextstep.subway.acceptance;
 
-import static nextstep.subway.acceptance.LineSteps.*;
-import static nextstep.subway.acceptance.StationSteps.*;
-import static org.assertj.core.api.Assertions.*;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.http.HttpStatus;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import static nextstep.subway.acceptance.LineSteps.*;
+import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 구간 관리 기능")
 class LineSectionAcceptanceTest extends AcceptanceTest {
@@ -47,7 +48,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     void addLineSection() {
         // when
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createDefaultSectionCreateParams(양재역, 정자역));
 
         // then
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
@@ -65,7 +66,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     void removeLineSection() {
         // given
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createDefaultSectionCreateParams(양재역, 정자역));
 
         // when
         지하철_노선에_지하철_구간_제거_요청(신분당선, 정자역);
@@ -87,7 +88,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     @Test
     void addIntermediateSectionExistingUpStation() {
         // when
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 신규역));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createDefaultSectionCreateParams(강남역, 신규역));
 
         // then
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
@@ -106,7 +107,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     @Test
     void addIntermediateSectionExistingDownStation() {
         // when
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(신규역, 양재역));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createDefaultSectionCreateParams(신규역, 양재역));
 
         // then
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
@@ -122,7 +123,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     @Test
     void bothStationsExistInLine() {
         // when
-        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 양재역));
+        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createDefaultSectionCreateParams(강남역, 양재역));
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
@@ -141,7 +142,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         Long 복정역 = 지하철역_생성_요청("복정역").jsonPath().getLong("id");
         
         // when
-        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(수서역, 복정역));
+        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createDefaultSectionCreateParams(수서역, 복정역));
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
@@ -153,14 +154,16 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
      * Then 새로운 구간이 추가되지 않는다.
      */
     @DisplayName("새로 추가하려는 구간의 길이는 기존 구간의 길이보다 작아야 한다.")
-    @Test
-    void invalidSectionDistance() {
+    @ValueSource(ints = {4, 6})
+    @ParameterizedTest
+    void invalidSectionDistance(int distance) {
         // given
         Long 에러역 = 지하철역_생성_요청("에러역").jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(신규역, 양재역));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createDefaultSectionCreateParams(신규역, 양재역));
 
         // when
-        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(에러역, 신규역));
+        ExtractableResponse<Response> response =
+            지하철_노선에_지하철_구간_생성_요청(신분당선, createCustomSectionCreateParams(에러역, 신규역, distance));
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
@@ -177,7 +180,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     @Test
     void addSectionWithFirstUpStation() {
         // when
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(신규역, 강남역));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createDefaultSectionCreateParams(신규역, 강남역));
 
         // then
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
@@ -196,7 +199,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     @Test
     void addSectionWithLastDownStation() {
         // when
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 신규역));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createDefaultSectionCreateParams(양재역, 신규역));
 
         // then
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
@@ -215,11 +218,15 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         return lineCreateParams;
     }
 
-    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId) {
+    private Map<String, String> createCustomSectionCreateParams(Long upStationId, Long downStationId, int distance) {
         Map<String, String> params = new HashMap<>();
         params.put("upStationId", upStationId + "");
         params.put("downStationId", downStationId + "");
-        params.put("distance", 6 + "");
+        params.put("distance", distance + "");
         return params;
+    }
+
+    private Map<String, String> createDefaultSectionCreateParams(Long upStationId, Long downStationId) {
+        return createCustomSectionCreateParams(upStationId, downStationId, 6);
     }
 }
