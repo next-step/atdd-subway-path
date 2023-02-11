5481b92564993144b92ed7008f34cde03d060384
최현웅 <76509935+choi1204@users.noreply.github.com>
2023-02-11 22:40:05 +0900

    test : 구간사이에 새로운 구간 추가 테스트추가

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -53,6 +53,22 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
+     * Given 신분당선을 생성하고
+     * When 강남역 - 양재역 구간 사이에 새로운 구간(강남역 - 청계산역)을 등록하면
+     * Then 강남역 - 청계신역 - 양재역 구간이 생성된다.
+     */
+    @DisplayName("지하철 노선 추가")
+    @Test
+    void addStation() {
+        Long 청계산역 = 지하철역_생성_요청("청계산역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 청계산역));
+
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 청계산역, 양재역);
+    }
+
+    /**
      * Given 지하철 노선에 새로운 구간 추가를 요청 하고
      * When 지하철 노선의 마지막 구간 제거를 요청 하면
      * Then 노선에 구간이 제거된다
