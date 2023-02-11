8058bd18e7c872d5e1a8dd75bf02dbb4c4540102
정천우 <codehouse95@gmail.com>
2023-02-12 01:06:21 +0900

    test: LineTest 중복 제거

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,15 +15,25 @@ import nextstep.subway.domain.Station;
 
 class LineTest {
 
+    private Line line;
+    private Station upStation;
+    private Station downStation;
+    private Section newSection;
+
+    @BeforeEach
+    void setup() {
+        line = new Line("4호선", "#00A5DE");
+
+        upStation = new Station("사당");
+        downStation = new Station("금정");
+
+        newSection = new Section(line, upStation, downStation, 10);
+        line.addSection(newSection);
+    }
+
     @DisplayName("노선에 구간을 추가할 수 있다")
     @Test
     void addSection() {
-        Line line = new Line("4호선", "#00A5DE");
-
-        Station upStation = new Station("사당");
-        Station downStation = new Station("금정");
-        Section newSection = new Section(line, upStation, downStation, 10);
-
         Section addedSection = line.addSection(newSection);
         assertThat(addedSection).isEqualTo(newSection);
     }
@@ -30,14 +41,6 @@ class LineTest {
     @DisplayName("노선의 모든 역을 조회할 수 있다")
     @Test
     void getStations() {
-        Line line = new Line("4호선", "#00A5DE");
-
-        Station upStation = new Station("사당");
-        Station downStation = new Station("금정");
-        Section newSection = new Section(line, upStation, downStation, 10);
-
-        line.addSection(newSection);
-
         List<Station> stations = line.getStations();
         assertAll(
             () -> assertThat(stations.size()).isEqualTo(2),
@@ -48,16 +51,8 @@ class LineTest {
     @DisplayName("노선의 마지막 구간을 삭제할 수 있다")
     @Test
     void removeSection() {
-        Line line = new Line("4호선", "#00A5DE");
-
-        Station upStation = new Station("사당");
-        Station downStation = new Station("금정");
         Station newDownStation = new Station("오이도");
-
-        Section newSection = new Section(line, upStation, downStation, 10);
         Section newSection2 = new Section(line, downStation, newDownStation, 13);
-
-        line.addSection(newSection);
         line.addSection(newSection2);
 
         line.removeSection(newDownStation);
@@ -71,14 +66,6 @@ class LineTest {
     @DisplayName("노선의 구간이 1개라면 삭제할 수 없다")
     @Test
     void removeOneSection() {
-        Line line = new Line("4호선", "#00A5DE");
-
-        Station upStation = new Station("사당");
-        Station downStation = new Station("금정");
-
-        Section newSection = new Section(line, upStation, downStation, 10);
-        line.addSection(newSection);
-
         assertThatThrownBy(() -> line.removeSection(downStation)).isInstanceOf(IllegalArgumentException.class);
     }
 }
