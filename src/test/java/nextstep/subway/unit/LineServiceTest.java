2882ca201c4c417328c270e26a03d75bc1cf2fd0
bada.kang <kangbada0728@gmail.com>
2023-02-11 19:39:53 +0900

    update: LineServiceTest.addSection 추가

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,15 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -23,11 +26,25 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        stationRepository.save(new Station("up"));
+        stationRepository.save(new Station("down"));
+        lineRepository.save(new Line("name", "color"));
+        SectionRequest sectionRequest = new SectionRequest(1L, 2L, 20);
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(1L, sectionRequest);
 
         // then
         // line.getSections 메서드를 통해 검증
+        Line line = lineRepository.findById(1L).get();
+        assertThat(line.getSections())
+                .extracting(Section::getUpStation)
+                .extracting(Station::getId)
+                .containsExactly(1L);
+        assertThat(line.getSections())
+                .extracting(Section::getDownStation)
+                .extracting(Station::getId)
+                .containsExactly(2L);
     }
 }
