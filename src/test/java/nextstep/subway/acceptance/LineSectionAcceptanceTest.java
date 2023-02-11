d382ba4e5994a90ac823aac2dd69fa199f2f613e
dongho.geum <dongho.geum@myrealtrip.com>
2023-02-11 22:33:35 +0900

    test: 노선의 기존 구간에 새로운 역을 등록할 경우 기존 구간 길이보다 크거나 같게 구간 생성 요청 시 추가가 안된다

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -90,6 +90,21 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
             assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
         }
 
+        /**
+         * When 노선의 기존 구간에 새로운 역을 등록할 경우 기존 구간 길이보다 크거나 같게 구간 생성 요청 시
+         * Then 기존 구간에 추가가 안된다
+         */
+        @DisplayName("노선의 기존 구간에 새로운 역을 등록할 경우 기존 구간 길이보다 크거나 같게 구간 생성 요청 시 추가가 안된다")
+        @Test
+        void 노선의_기존_구간에_새로운_역을_등록할_경우_기존_구간_길이보다_크거나_같게_구간_생성_요청_시_추가가_안된다() {
+            // when
+            Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+            ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 정자역, 10L));
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
+
         private void 노선에_새로운_구간이_추가되며_길이가_재_정의_된다(Long 정자역, ExtractableResponse<Response> lineResponse) {
             assertThat(lineResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
             List<SectionResponse> sections = lineResponse.jsonPath().getList("sections", SectionResponse.class);
