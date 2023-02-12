3913ec2c60919a21331f6ec6234df3b325de132b
bada.kang <kangbada0728@gmail.com>
2023-02-12 15:12:20 +0900

    fix: Staion의 동일성, 동등성을 비교할 때 ID만을 사용하도록 변경

--- src/main/java/nextstep/subway/domain/station/Station.java
+++ src/main/java/nextstep/subway/domain/station/Station.java
@@ -32,11 +32,11 @@ public class Station {
         if (this == o) return true;
         if (!(o instanceof Station)) return false;
         Station station = (Station) o;
-        return Objects.equals(getId(), station.getId()) && Objects.equals(getName(), station.getName());
+        return Objects.equals(getId(), station.getId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getName());
+        return Objects.hash(getId());
     }
 }
