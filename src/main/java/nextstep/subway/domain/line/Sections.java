1bbc38ab41f7dd30f68bb89da887406dd225c08a
bada.kang <kangbada0728@gmail.com>
2023-02-12 15:52:20 +0900

    add: 지하철 구간 추가 기능 개선

--- src/main/java/nextstep/subway/domain/line/Sections.java
+++ src/main/java/nextstep/subway/domain/line/Sections.java
@@ -67,16 +67,80 @@ public class Sections {
         return list;
     }
 
-    public void addSection(Section section) {
-        sections.add(section);
+    public void addSection(Section newSection) {
+        if (sections.isEmpty()) {
+            sections.add(newSection);
+            return;
+        }
+        boolean isUpStationExist = sections.stream()
+                .anyMatch(s -> s.getUpStation().equals(newSection.getUpStation())
+                        || s.getDownStation().equals(newSection.getUpStation()));
+        boolean isDownStationExist = sections.stream()
+                .anyMatch(s -> s.getUpStation().equals(newSection.getDownStation())
+                        || s.getDownStation().equals(newSection.getDownStation()));
+        if (isUpStationExist && isDownStationExist) {
+            throw new IllegalArgumentException("노선에 존재하던 지하철 역 끼리는 구간을 만들 수 없습니다.");
+        }
+        if (!isUpStationExist && !isDownStationExist) {
+            throw new IllegalArgumentException("새로운 구간의 지하철 역들 중 하나는 기존 지하철 노선에 포함되어 있어야 합니다.");
+        }
+
+        if (isUpStationExist) {
+            addUpAndDownSection(newSection);
+            return;
+        }
+        addDownAndUpSection(newSection);
+    }
+
+    private void addUpAndDownSection(Section newSection) {
+        Optional<Section> maybeExistSection = sections.stream()
+                .filter(s -> s.getUpStation().equals(newSection.getUpStation()))
+                .findFirst();
+        if (maybeExistSection.isPresent()) {
+            addUpAndDownSectionBetween(newSection, maybeExistSection.get());
+            return;
+        }
+        sections.add(newSection);
+    }
+
+    private void addUpAndDownSectionBetween(Section newSection, Section section) {
+        if (newSection.getDistance() >= section.getDistance()) {
+            throw new IllegalArgumentException("기존 지하철 구간의 길이보다 추가된 구간의 길이가 더 깁니다.");
+        }
+        sections.remove(section);
+        sections.add(newSection);
+        sections.add(new Section(section.getLine(), newSection.getDownStation(), section.getDownStation(), section.getDistance() - newSection.getDistance()));
+    }
+
+    private void addDownAndUpSection(Section newSection) {
+        Optional<Section> maybeExistSection = sections.stream()
+                .filter(s -> s.getDownStation().equals(newSection.getDownStation()))
+                .findFirst();
+        if (maybeExistSection.isPresent()) {
+            addDownAndUpSectionBetween(newSection, maybeExistSection.get());
+            return;
+        }
+        sections.add(newSection);
+    }
+
+    private void addDownAndUpSectionBetween(Section newSection, Section section) {
+        if (newSection.getDistance() >= section.getDistance()) {
+            throw new IllegalArgumentException("기존 지하철 구간의 길이보다 추가된 구간의 길이가 더 깁니다.");
+        }
+        sections.remove(section);
+        sections.add(new Section(section.getLine(), section.getUpStation(), newSection.getUpStation(), section.getDistance() - newSection.getDistance()));
+        sections.add(newSection);
     }
 
     public void removeSection(Station station) {
-        Section section = sections.get(sections.size() - 1);
-        Station lastStation = section.getDownStation();
+        List<Station> stations = getStations();
+        Station lastStation = stations.get(stations.size() - 1);
         if (!lastStation.equals(station)) {
             throw new IllegalArgumentException();
         }
-        sections.remove(sections.size() - 1);
+        Section lastSection = sections.stream().filter(s -> s.getDownStation().equals(lastStation))
+                .findFirst()
+                .orElseThrow(IllegalAccessError::new);
+        sections.remove(lastSection);
     }
 }
