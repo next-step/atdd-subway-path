9c324e32f32b22e16be75ed943b5001a364d7c52
kimjio <jiokim222@gmail.com>
2023-02-12 12:12:42 +0900

    test(LineTest): 구간 추가, 목록 조회 단위 테스트

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,17 +1,101 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LineTest {
-    @Test
-    void addSection() {
+
+    private Station 강남역;
+    private Station 삼성역;
+    private Station 잠실역;
+    private Station 판교역;
+
+    @BeforeEach
+    void SetUp() {
+        강남역 = new Station("강남역");
+        삼성역 = new Station("삼성역");
+        잠실역 = new Station("잠실역");
+        판교역 = new Station("판교역");
     }
 
     @Test
+    @DisplayName("성공: 구간 추가 a-b & b-c")
+    void add_Section() {
+        Line line = new Line("이호선", "green");
+        line.addSection(강남역, 잠실역, 5);
+
+        assertAll(
+            ()->assertThat(line.getSections()).hasSize(1),
+            ()->assertThat(line.getStations())
+                    .extracting(Station::getName)
+                    .containsExactlyInAnyOrder("강남역", "잠실역")
+        );
+    }
+
+    @Test
+    @DisplayName("실패: 구간 추가 a-b & c-d")
+    void fail_add_Section() {
+        Line line = new Line("이호선", "green");
+        line.addSection(강남역, 잠실역, 5);
+
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            line.addSection(삼성역, 판교역, 3);
+        });
+    }
+
+    @Test
+    @DisplayName("실패: 중복된 구간 추가")
+    void fail_add_same_Section() {
+        Line line = new Line("이호선", "green");
+        line.addSection(강남역, 잠실역, 5);
+
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            line.addSection(강남역, 잠실역, 3);
+        });
+    }
+
+    @Test
+    @DisplayName("성공: 지하철역 목록 조회")
     void getStations() {
+        // 노선 추가
+        Line line = new Line("이호선", "green", 강남역, 잠실역, 5);
+
+        assertSoftly(softly -> {
+            softly.assertThat(line.getSections()).hasSize(1);
+            softly.assertThat(line.getStations())
+                    .extracting(Station::getName)
+                    .containsExactlyInAnyOrder("강남역", "잠실역");
+        });
     }
 
     @Test
+    @DisplayName("성공: 구간 삭제")
     void removeSection() {
+        // 노선 추가
+        Line line = new Line("이호선", "green");
+        // 1번째 구간추가
+        line.addSection(강남역, 삼성역, 5);
+
+        // 2번째 구간추가
+        line.addSection(삼성역, 잠실역, 10);
+
+        // 마지막구간 삭제
+        line.deleteSection(잠실역);
+
+        assertAll(
+                () -> assertThat(line.getSections()).hasSize(1),
+                () -> assertThat(line.getStations())
+                        .extracting(Station::getName)
+                        .doesNotContain("잠실역")
+        );
     }
 }
