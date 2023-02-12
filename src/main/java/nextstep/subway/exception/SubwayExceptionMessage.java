61f7c9aada659bd3d96110277cf43c770da08434
hiblue02 <hiblue02@gmail.com>
2023-02-12 23:30:06 +0900

    refactor(SubwayExceptionMessage): 쓰지 않는 메세지 삭제

--- src/main/java/nextstep/subway/exception/SubwayExceptionMessage.java
+++ src/main/java/nextstep/subway/exception/SubwayExceptionMessage.java
@@ -3,11 +3,9 @@ package nextstep.subway.exception;
 public enum SubwayExceptionMessage {
 
     SECTION_CANNOT_ADD("구간을 추가할 수 없습니다."),
-    SECTION_ALREADY_ADDED("이미 등록괸 구간입니다."),
     SECTION_LONGER("추가하려는 구간의 길이가 더 길어 추가할 수 없습니다."),
-    LINE_IS_EMPTY("노선이 비었습니다."),
-    STATION_NOT_CONTAINED("노선에 없는 역입니다."),
-    STATION_CANNOT_REMOVE("역을 삭제할 수 없습니다.");
+    STATION_CANNOT_REMOVE("역을 삭제할 수 없습니다.")
+    ;
 
 
     private final String message;
