c00cdf2dcd668c3ebe406ebed00cabdaafe22086
joelyou-nsuslab <joel.you@nsuslab.com>
2023-02-13 02:40:43 +0900

    feature: 구간추가 테스트케이스 추가
    * 인수조건 정의
    * 테스트코드 작성

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -12,6 +12,7 @@ import java.util.Map;
 
 import static nextstep.subway.acceptance.LineSteps.*;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
+import static nextstep.subway.acceptance.StationSteps.지하철역_생성_한다;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 구간 관리 기능")
@@ -53,6 +54,67 @@ class SectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
+     * When 기존 구간에 새로운 구간을 추가하면
+     * Then 새로운 구간이 추가되고, 기존 구간의 길이에서 새로운 구간의 길이를 빼면, 새롭게 추가된 역 까지의 거리이다.
+     */
+    @DisplayName("기존 구간에 새로운 구간을 추가")
+    @Test
+    void addLineSection_BetweenExistingSection() {
+        // when
+        Long 신사역 = 지하철역_생성_한다("신사역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 신사역, 6L));
+
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 신사역, 양재역);
+        assertThat(response.jsonPath().getList("sections.distance", Long.class)).containsOnly(6L, 4L);
+    }
+
+    /**
+     * When 기존 구간에 추가할 때 길이가 같거나 더 큰 새로운 구간을 추가하면
+     * Then 오류가 발생한다.
+     */
+    @DisplayName("기존 구간에 길이가 더 긴 새로운 구간 추가")
+    @Test
+    void addLineSection_BetweenExistingSection_WithLongerDistance() {
+        // when
+        Long 신사역 = 지하철역_생성_한다("신사역").jsonPath().getLong("id");
+        var response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 신사역, 11L));
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    /**
+     * When 기존 구간에 추가할 때 상하행역 모두 이미 등록이 되어있으면
+     * Then 오류가 발생한다.
+     */
+    @DisplayName("기존 구간에 이미 등록되어 있는 상하행역에 대한 구간 추가")
+    @Test
+    void addLineSection_AlreadyExistsUpAndDownStation() {
+        // when
+        var response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 양재역, 10L));
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    /**
+     * When 기존 구간에 추가할 때 상하행역 모두 포함되어 있지 않으면
+     * Then 오류가 발생한다.
+     */
+    @DisplayName("기존 구간에 모두 포함되지 않는 상하행역에 대한 구간 추가")
+    @Test
+    void addLineSection_NotIncludedUpAndDownStation() {
+        // when
+        var response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 양재역, 10L));
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    /**
      * Given 지하철 노선에 새로운 구간 추가를 요청 하고
      * When 지하철 노선의 마지막 구간 제거를 요청 하면
      * Then 노선에 구간이 제거된다
@@ -89,6 +151,14 @@ class SectionAcceptanceTest extends AcceptanceTest {
         params.put("upStationId", upStationId + "");
         params.put("downStationId", downStationId + "");
         params.put("distance", 6 + "");
+        return params;
+    }
+
+    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, Long distance) {
+        Map<String, String> params = new HashMap<>();
+        params.put("upStationId", upStationId + "");
+        params.put("downStationId", downStationId + "");
+        params.put("distance", distance + "");
         return params;
     }
 }
