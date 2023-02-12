32dfb1eac49e1664fe5ef2788450968634f9cfaf
kimjio <jiokim222@gmail.com>
2023-02-12 12:13:09 +0900

    test(LineServiceTest): 구간 추가

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,23 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
@@ -15,19 +26,25 @@ public class LineServiceTest {
     private StationRepository stationRepository;
     @Autowired
     private LineRepository lineRepository;
-
     @Autowired
     private LineService lineService;
 
     @Test
+    @DisplayName("성공: 구간추가")
     void addSection() {
         // given
-        // stationRepository와 lineRepository를 활용하여 초기값 셋팅
-
+        Station upStation = stationRepository.save(new Station("강남역"));
+        Station downStation = stationRepository.save(new Station("삼성역"));
+        Line line = lineRepository.save(new Line("이호선", "green"));
+        SectionRequest sectionRequest = new SectionRequest(upStation.getId(), downStation.getId(), 5);
         // when
-        // lineService.addSection 호출
-
+        lineService.addSection(line.getId(), sectionRequest);
         // then
-        // line.getSections 메서드를 통해 검증
+        assertSoftly(softly -> {
+            softly.assertThat(lineService.findLineById(line.getId()).getSections()).hasSize(1);
+            softly.assertThat(lineService.findLineById(line.getId()).getStations())
+                    .extracting(Station::getName)
+                    .containsExactlyInAnyOrder("삼성역","강남역");
+        });
     }
 }
