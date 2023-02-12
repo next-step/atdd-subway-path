1a255fa6659ad74c430163422738b73930ab8a83
kimjio <jiokim222@gmail.com>
2023-02-12 12:10:58 +0900

    refactor(Line): 구간 추가

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -12,15 +12,28 @@ public class Line {
     private String name;
     private String color;
 
-    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
-    private List<Section> sections = new ArrayList<>();
+    @Embedded
+    private Sections sections = new Sections();
 
     public Line() {
+    }
+
+    public Line(String name, String color, Station upStation, Station downStation, int distance) {
+        this(name, color);
+        this.addSection(upStation, downStation, distance);
     }
 
     public Line(String name, String color) {
         this.name = name;
         this.color = color;
+    }
+
+    public void addSection(Station upStation, Station downStation, int distance) {
+        sections.add(new Section(this, upStation, downStation, distance));
+    }
+
+    public void deleteSection(Station station) {
+        sections.delete(station);
     }
 
     public Long getId() {
@@ -48,6 +61,10 @@ public class Line {
     }
 
     public List<Section> getSections() {
-        return sections;
+        return sections.getValues();
+    }
+
+    public List<Station> getStations() {
+        return sections.getStations();
     }
 }
