5e2613fbe4b19d0e7d12fd8759c6c909c9ea95bd
kimjio <jiokim222@gmail.com>
2023-02-12 12:16:01 +0900

    test(SectionAcceptanceTest): 구간 추가 인수 테스트

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -5,6 +5,8 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.http.HttpStatus;
 
 import java.util.HashMap;
@@ -53,6 +55,39 @@ class SectionAcceptanceTest extends AcceptanceTest {
     }
 
     /**
+     * Given 지하철에 구간의 길이10인 새로운 구간 추가한다.
+     * When 10보다 큰 길이를 가진 신규 구간 추가한다.
+     * Then 에러 처리한다.
+     */
+    @DisplayName("실패: 기존 구간 사이에 신규 구간 추가시 기존구간 길이와 같거나 큰 구간 추가")
+    @ParameterizedTest(name = "[{index}] ==> Distance: {0}")
+    @ValueSource(ints = {10, 11, 12})
+    void fail_addLineSection(int distance) {
+        // when
+        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+        ExtractableResponse<Response> response =
+                지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 정자역, distance));
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    /**
+     * Given 지하철 노선에 새로운 구간 추가를 요청 한다.
+     * When 같은 역을 가진 구간을 추가한다.
+     * Then 에러 처리한다.
+     */
+    @DisplayName("실패: 기존구간과 같은 구간 추가")
+    @Test
+    void fail_addSameLineSection() {
+        // when
+        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+        ExtractableResponse<Response> response =
+                지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 양재역, 4));
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    /**
      * Given 지하철 노선에 새로운 구간 추가를 요청 하고
      * When 지하철 노선의 마지막 구간 제거를 요청 하면
      * Then 노선에 구간이 제거된다
@@ -89,6 +124,14 @@ class SectionAcceptanceTest extends AcceptanceTest {
         params.put("upStationId", upStationId + "");
         params.put("downStationId", downStationId + "");
         params.put("distance", 6 + "");
+        return params;
+    }
+
+    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, int distance) {
+        Map<String, String> params = new HashMap<>();
+        params.put("upStationId", upStationId + "");
+        params.put("downStationId", downStationId + "");
+        params.put("distance", distance + "");
         return params;
     }
 }
