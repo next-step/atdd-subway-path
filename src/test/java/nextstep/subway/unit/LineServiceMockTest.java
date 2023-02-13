1e65c57f520ce61ddd5cdaa64ebf4eaecde6da99
wu2ee <gulb1602@gmail.com>
2023-02-13 23:00:20 +0900

    test : LineServiceMockTest 작성
    - 테스트 더블 (stubbing) 이용

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,11 +1,23 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
@@ -13,16 +25,31 @@ public class LineServiceMockTest {
     private LineRepository lineRepository;
     @Mock
     private StationService stationService;
+    private LineService lineService;
+    private Station 강남역;
+    private Station 역삼역;
+    private Line 이호선;
 
+    @BeforeEach
+    void setUp() {
+        lineService = new LineService(lineRepository, stationService);
+        강남역 = new Station(1L, "강남역");
+        역삼역 = new Station(2L, "역삼역");
+        이호선 = new Line(1L, "2호선", "green");
+    }
+
+    @DisplayName("지하철 노선에 구간을 등록한다.")
     @Test
     void addSection() {
         // given
-        // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        when(lineRepository.findById(이호선.getId())).thenReturn(Optional.of(이호선));
+        when(stationService.findById(강남역.getId())).thenReturn(강남역);
+        when(stationService.findById(역삼역.getId())).thenReturn(역삼역);
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(이호선.getId(), new SectionRequest(강남역.getId(), 역삼역.getId(), 3));
 
         // then
-        // line.findLineById 메서드를 통해 검증
+        assertThat(이호선.getSections()).containsExactly(new Section(이호선, 강남역, 역삼역, 3));
     }
 }
