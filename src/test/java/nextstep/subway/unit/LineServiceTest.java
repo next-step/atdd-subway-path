35efc201cabe21e4a505f1510abba00f52885e28
yunhwan <pyh8618@gmail.com>
2023-02-14 00:21:16 +0900

    refactor : 노선서비스 테스트 중복검증메서드 리팩터링

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -7,6 +7,7 @@ import nextstep.subway.applicaion.dto.SectionRequest;
 import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.*;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,8 +24,10 @@ import static org.assertj.core.api.Assertions.fail;
 @SpringBootTest
 @Transactional
 public class LineServiceTest {
+
     @Autowired
     private StationRepository stationRepository;
+
     @Autowired
     private LineRepository lineRepository;
 
@@ -43,6 +46,7 @@ public class LineServiceTest {
 
     private int distance = 10;
     private int halfDistance = (int) (distance / 2);
+
     @BeforeEach
     void setUp() {
         신분당선 = new Line("신분당선", "bg-red-900");
@@ -50,6 +54,9 @@ public class LineServiceTest {
 
         강남역 = new Station("강남역");
         양재역 = new Station("양재역");
+        강남역 = stationRepository.save(강남역);
+        양재역 = stationRepository.save(양재역);
+
         강남_양재_구간 = new Section(신분당선, 강남역, 양재역, distance);
     }
 
@@ -58,8 +65,6 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
-        강남역 = stationRepository.save(강남역);
-        양재역 = stationRepository.save(양재역);
         Line line = lineRepository.save(신분당선);
         SectionRequest sectionRequest = new SectionRequest(강남역.getId(), 양재역.getId(), distance);
 
@@ -77,8 +82,6 @@ public class LineServiceTest {
     void addSectionFirst() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
-        강남역 = stationRepository.save(강남역);
-        양재역 = stationRepository.save(양재역);
         Station 신논현역 = stationRepository.save(new Station("신논현역"));
         Line line = lineRepository.save(신분당선);
         SectionRequest sectionRequest1 = new SectionRequest(강남역.getId(), 양재역.getId(), distance);
@@ -91,7 +94,7 @@ public class LineServiceTest {
 
         // then
         신분당선 = lineRepository.findById(line.getId()).orElseThrow();
-        assertThat(신분당선.getStations()).containsExactly(신논현역, 강남역, 양재역);
+        노선의_지하철역_순서_검증(신분당선, List.of(신논현역, 강남역, 양재역));
     }
 
     @Test
@@ -99,22 +102,20 @@ public class LineServiceTest {
     void addSectionMiddle() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
-        강남역 = stationRepository.save(강남역);
-        양재역 = stationRepository.save(양재역);
         Station 신논현역 = stationRepository.save(new Station("신논현역"));
         Line line = lineRepository.save(신분당선);
-        SectionRequest sectionRequest1 = new SectionRequest(신논현역.getId(), 양재역.getId(), distance);
 
-        SectionRequest sectionRequest2 = new SectionRequest(신논현역.getId(), 강남역.getId(), halfDistance);
-        lineService.addSection(line.getId(), sectionRequest1);
+        SectionRequest 기존구간 = new SectionRequest(신논현역.getId(), 양재역.getId(), distance);
+        SectionRequest 새구간 = new SectionRequest(신논현역.getId(), 강남역.getId(), halfDistance);
+        lineService.addSection(line.getId(), 기존구간);
 
         // when
-        lineService.addSection(line.getId(), sectionRequest2);
+        lineService.addSection(line.getId(), 새구간);
         flushAndClearEntityManger();
 
         // then
         신분당선 = lineRepository.findById(line.getId()).orElseThrow();
-        assertThat(신분당선.getStations()).containsExactly(신논현역, 강남역, 양재역);
+        노선의_지하철역_순서_검증(신분당선, List.of(신논현역, 강남역, 양재역));
     }
 
 
@@ -123,21 +124,19 @@ public class LineServiceTest {
     void addSectionMiddle2() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
-        강남역 = stationRepository.save(강남역);
-        양재역 = stationRepository.save(양재역);
         Station 신논현역 = stationRepository.save(new Station("신논현역"));
         Line line = lineRepository.save(신분당선);
-        SectionRequest sectionRequest1 = new SectionRequest(신논현역.getId(), 양재역.getId(), distance);
-        SectionRequest sectionRequest2 = new SectionRequest(강남역.getId(), 양재역.getId(), halfDistance);
-        lineService.addSection(line.getId(), sectionRequest1);
+        SectionRequest 기존구간 = new SectionRequest(신논현역.getId(), 양재역.getId(), distance);
+        SectionRequest 새구간 = new SectionRequest(강남역.getId(), 양재역.getId(), halfDistance);
+        lineService.addSection(line.getId(), 기존구간);
 
         // when
-        lineService.addSection(line.getId(), sectionRequest2);
+        lineService.addSection(line.getId(), 새구간);
         flushAndClearEntityManger();
 
         // then
         신분당선 = lineRepository.findById(line.getId()).orElseThrow();
-        assertThat(신분당선.getStations()).containsExactly(신논현역, 강남역, 양재역);
+        노선의_지하철역_순서_검증(신분당선, List.of(신논현역, 강남역, 양재역));
     }
 
     @Test
@@ -149,36 +148,24 @@ public class LineServiceTest {
         lineService.updateLine(line.getId(), new LineRequest("새로운이름", "새로운컬러"));
 
         // then
-        Line 업데이트된노선 = lineRepository.findById(line.getId()).orElseThrow();
-        SoftAssertions.assertSoftly((sa) -> {
-            sa.assertThat(line.getName()).isEqualTo(업데이트된노선.getName());
-            sa.assertThat(line.getColor()).isEqualTo(업데이트된노선.getColor());
-        });
+        노선_수정_정보_검증(line);
     }
 
     @Test
     void saveLine() {
         // given
-        강남역 = stationRepository.save(강남역);
-        양재역 = stationRepository.save(양재역);
         LineRequest lineRequest = new LineRequest(신분당선.getName(), 신분당선.getColor(), 강남역.getId(), 양재역.getId(), distance);
 
         // when
         LineResponse lineResponse = lineService.saveLine(lineRequest);
 
         // then
-        SoftAssertions.assertSoftly((sa) -> {
-            sa.assertThat(lineResponse.getName()).isEqualTo(신분당선.getName());
-            sa.assertThat(lineResponse.getColor()).isEqualTo(신분당선.getColor());
-            sa.assertThat(lineResponse.getStations()).usingRecursiveComparison().isEqualTo(List.of(StationResponse.from(강남역), StationResponse.from(양재역)));
-        });
+        노선_저장_검증(lineResponse);
     }
 
     @Test
     void getSections() {
         // given
-        강남역 = stationRepository.save(강남역);
-        양재역 = stationRepository.save(양재역);
         Station 청계산역 = stationRepository.save(new Station("청계산역"));
         LineRequest lineRequest = new LineRequest(신분당선.getName(), 신분당선.getColor(), 강남역.getId(), 양재역.getId(), distance);
 
@@ -194,16 +181,12 @@ public class LineServiceTest {
             sa.assertThat(line.getName()).isEqualTo(신분당선.getName());
             sa.assertThat(line.getColor()).isEqualTo(신분당선.getColor());
         });
-        assertThat(line.getStations()).usingRecursiveComparison()
-                .isEqualTo(List.of(강남역, 양재역, 청계산역));
+        노선의_지하철역_순서_검증(line, List.of(강남역, 양재역, 청계산역) );
     }
-
 
     @Test
     void deleteSection() {
         // given
-        강남역 = stationRepository.save(강남역);
-        양재역 = stationRepository.save(양재역);
         Station 양재시민의숲역 = stationRepository.save(new Station("양재시민의숲역"));
         LineRequest lineRequest = new LineRequest(신분당선.getName(), 신분당선.getColor(), 강남역.getId(), 양재역.getId(), distance);
         LineResponse lineResponse = lineService.saveLine(lineRequest);
@@ -216,7 +199,28 @@ public class LineServiceTest {
 
         // then
         Line 업데이트된노선 = lineRepository.findById(lineResponse.getId()).orElseThrow();
-        assertThat(업데이트된노선.getStations()).usingRecursiveComparison().isEqualTo(List.of(StationResponse.from(강남역), StationResponse.from(양재역)));
+        노선의_지하철역_순서_검증(업데이트된노선, List.of(강남역, 양재역));
+    }
+
+    private void 노선_수정_정보_검증(Line line) {
+        Line 업데이트된노선 = lineRepository.findById(line.getId()).orElseThrow();
+        SoftAssertions.assertSoftly((sa) -> {
+            sa.assertThat(line.getName()).isEqualTo(업데이트된노선.getName());
+            sa.assertThat(line.getColor()).isEqualTo(업데이트된노선.getColor());
+        });
+    }
+
+    private void 노선_저장_검증(LineResponse lineResponse) {
+        Assertions.assertAll(
+                () -> assertThat(lineResponse.getName()).isEqualTo(신분당선.getName()),
+                () -> assertThat(lineResponse.getColor()).isEqualTo(신분당선.getColor()),
+                () -> assertThat(lineResponse.getStations()).usingRecursiveComparison().isEqualTo(List.of(StationResponse.from(강남역), StationResponse.from(양재역)))
+        );
+    }
+
+    private void 노선의_지하철역_순서_검증(Line line, List<Station> 검증역들) {
+        assertThat(line.getStations()).usingRecursiveComparison()
+                .isEqualTo(검증역들);
     }
 
     private void flushAndClearEntityManger() {
