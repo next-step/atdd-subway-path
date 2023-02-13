dc953f432ef24b51b69525095506c71207ca9221
wu2ee <gulb1602@gmail.com>
2023-02-13 22:21:45 +0900

    fix : fixture 이름 한글로 변경

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -12,45 +12,45 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LineTest {
     private Station 강남역;
     private Station 역삼역;
-    private Line line;
+    private Line 이호선;
 
     @BeforeEach
     void setUp() {
         강남역 = new Station("강남역");
         역삼역 = new Station("역삼역");
-        line = new Line("2호선", "green");
+        이호선 = new Line("2호선", "green");
     }
 
     @DisplayName("지하철 노선에 구간을 추가한다.")
     @Test
     void addSection() {
         // when
-        line.addSection(new Section(line, 강남역, 역삼역, 3));
+        이호선.addSection(new Section(이호선, 강남역, 역삼역, 3));
 
         // then
-        assertThat(line.getSections()).containsExactly(new Section(line, 강남역, 역삼역, 3));
+        assertThat(이호선.getSections()).containsExactly(new Section(이호선, 강남역, 역삼역, 3));
     }
 
     @DisplayName("지하철 노선에 등록된 역들을 조회한다.")
     @Test
     void getStations() {
         // given
-        line.addSection(new Section(line, 강남역, 역삼역, 3));
+        이호선.addSection(new Section(이호선, 강남역, 역삼역, 3));
 
         // when & then
-        assertThat(line.getStations()).containsExactly(강남역, 역삼역);
+        assertThat(이호선.getStations()).containsExactly(강남역, 역삼역);
     }
 
     @DisplayName("지하철 노선에 등록된 구간을 삭제한다.")
     @Test
     void removeSection() {
         // given
-        line.addSection(new Section(line, 강남역, 역삼역, 3));
+        이호선.addSection(new Section(이호선, 강남역, 역삼역, 3));
 
         // when
-        line.removeSection(역삼역);
+        이호선.removeSection(역삼역);
 
         // then
-        assertThat(line.getStations()).isEmpty();
+        assertThat(이호선.getStations()).isEmpty();
     }
 }
