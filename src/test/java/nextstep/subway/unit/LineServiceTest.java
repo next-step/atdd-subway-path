d96ba55f083c160c08cfec2de9b562241b69ec1d
jykim <wodusl1103@naver.com>
2023-02-14 01:12:51 +0900

    Test(line): Line 도메인, LineService 테스트코드 작성

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,7 +1,14 @@
 package nextstep.subway.unit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +29,20 @@ public class LineServiceTest {
     @Test
     void addSection() {
         // given
-        // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 강남구청역 = stationRepository.save(new Station("강남구청역"));
+        Station 압구정로데오역 = stationRepository.save(new Station("압구정로데오역"));
+        Line 수인분당선 = lineRepository.save(new Line("수인분당선", "bg-yellow-600"));
+        int distance = 10;
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(수인분당선.getId(),
+            new SectionRequest(강남구청역.getId(), 압구정로데오역.getId(), distance));
 
         // then
-        // line.getSections 메서드를 통해 검증
+        수인분당선 = lineRepository.findById(수인분당선.getId()).get();
+        List<Section> sections = 수인분당선.getSections();
+        assertThat(sections.get(0).getUpStation().getId()).isEqualTo(강남구청역.getId());
+        assertThat(sections.get(0).getDownStation().getId()).isEqualTo(압구정로데오역.getId());
+        assertThat(sections.get(0).getDistance()).isEqualTo(distance);
     }
 }
