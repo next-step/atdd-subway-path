46771ebce00b4397be59d434cd0eda72206cec7d
Choonghee Lee <choonghee.lee@hotmail.com>
2023-02-13 20:51:06 +0900

    test(LineService): 구간 등록 테스트

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,18 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -19,15 +25,18 @@ public class LineServiceTest {
     @Autowired
     private LineService lineService;
 
+    @DisplayName("지하철 노선에 새로운 구간 추가하기")
     @Test
     void addSection() {
         // given
-        // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 서울역 = stationRepository.save(new Station("서울역"));
+        Station 시청역 = stationRepository.save(new Station("시청역"));
+        Line line = lineRepository.save(new Line("1호선", "남색"));
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(line.getId(), new SectionRequest(서울역.getId(), 시청역.getId(), 10));
 
         // then
-        // line.getSections 메서드를 통해 검증
+        assertThat(line.getStations()).containsExactly(서울역, 시청역);
     }
 }
