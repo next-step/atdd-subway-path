c33bc842823280498f19ab3201a66589caf5487b
wu2ee <gulb1602@gmail.com>
2023-02-13 22:06:18 +0900

    test : BeforeEach 테스트 픽스쳐 생성

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -3,20 +3,27 @@ package nextstep.subway.unit;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
+    private Station 강남역;
+    private Station 역삼역;
+    private Line line;
+
+    @BeforeEach
+    void setUp() {
+        강남역 = new Station("강남역");
+        역삼역 = new Station("역삼역");
+        line = new Line("2호선", "green");
+    }
+
     @DisplayName("지하철 노선에 구간을 추가한다.")
     @Test
     void addSection() {
-        // given
-        Station 강남역 = new Station("강남역");
-        Station 역삼역 = new Station("역삼역");
-        Line line = new Line("2호선", "green");
-
         // when
         line.addSection(new Section(line, 강남역, 역삼역, 3));
 
@@ -28,9 +35,6 @@ class LineTest {
     @Test
     void getStations() {
         // given
-        Station 강남역 = new Station("강남역");
-        Station 역삼역 = new Station("역삼역");
-        Line line = new Line("2호선", "green");
         line.addSection(new Section(line, 강남역, 역삼역, 3));
 
         // when & then
@@ -41,9 +45,6 @@ class LineTest {
     @Test
     void removeSection() {
         // given
-        Station 강남역 = new Station("강남역");
-        Station 역삼역 = new Station("역삼역");
-        Line line = new Line("2호선", "green");
         line.addSection(new Section(line, 강남역, 역삼역, 3));
 
         // when
