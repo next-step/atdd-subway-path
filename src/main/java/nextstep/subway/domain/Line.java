c690c93d5abe152fa696d45b16d7557fbb7f47a8
bactoria(황준오) <bactoria.dev@kakaocorp.com>
2023-02-14 02:39:59 +0900

    feat:: [노선의 이름, 색상 변경] 구현 (review)
    코멘트: https://github.com/next-step/atdd-subway-path/pull/455#discussion_r1102738640

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -34,28 +34,21 @@ public class Line {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getColor() {
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
     public List<Section> getSections() {
         return sections.getSections();
+    }
+
+    public void modify(String name, String color) {
+        this.name = name;
+        this.color = color;
     }
 
     public void removeSection(Station station) {
