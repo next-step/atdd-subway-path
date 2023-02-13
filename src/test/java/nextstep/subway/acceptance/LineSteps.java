1a93881244e22e124212a4a06ed8c3602f4f091b
bada.kang <kangbada0728@gmail.com>
2023-02-13 21:44:01 +0900

    update: LineStep에서 지하철 노선 생성 요청 추가

--- src/test/java/nextstep/subway/acceptance/LineSteps.java
+++ src/test/java/nextstep/subway/acceptance/LineSteps.java
@@ -51,6 +51,22 @@ public class LineSteps {
                 .then().log().all().extract();
     }
 
+    public static ExtractableResponse<Response> 지하철_노선_생성_요청(String name, String color, Long upStationId, Long downStationId, int distance) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+        params.put("upStationId", upStationId + "");
+        params.put("downStationId", downStationId + "");
+        params.put("distance", distance + "");
+
+        return RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all().extract();
+    }
+
     public static ExtractableResponse<Response> 지하철_노선에_지하철_구간_생성_요청(Long lineId, Map<String, String> params) {
         return RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
