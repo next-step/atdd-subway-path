46754d88a48c4d1d522d9e85a233965e208acc5f
bada.kang <kangbada0728@gmail.com>
2023-02-13 20:16:11 +0900

    refactor: Distance를 일급 객체로 변경

---
+++ src/main/java/nextstep/subway/domain/section/Distance.java
@@ -0,0 +1,20 @@
+package nextstep.subway.domain.section;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Getter
+@Embeddable
+@NoArgsConstructor
+public class Distance {
+    private int distance;
+
+    public Distance(int distance) {
+        if (distance < 0) {
+            throw new IllegalArgumentException("거리는 1 이상이어야 합니다.");
+        }
+        this.distance = distance;
+    }
+}
