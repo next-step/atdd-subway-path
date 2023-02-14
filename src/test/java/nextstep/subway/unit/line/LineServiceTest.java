48b4bc7369ab5d949be571bbc912ce79fc0a1dac
정천우 <codehouse95@gmail.com>
2023-02-14 12:55:01 +0900

    test: line 유닛테스트 테스트 수정

--- src/test/java/nextstep/subway/unit/line/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/line/LineServiceTest.java
@@ -1,14 +1,15 @@
 package nextstep.subway.unit.line;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
+import nextstep.subway.line.LineCreateRequest;
+import nextstep.subway.line.LineResponse;
 import nextstep.subway.line.LineService;
-import nextstep.subway.section.SectionRequest;
-import nextstep.subway.line.Line;
+import nextstep.subway.line.LineUpdateRequest;
 import nextstep.subway.line.LineRepository;
-import nextstep.subway.section.Section;
 import nextstep.subway.station.Station;
 import nextstep.subway.station.StationRepository;
 
@@ -23,38 +24,78 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class LineServiceTest {
 
-	@Autowired private StationRepository stationRepository;
 	@Autowired private LineRepository lineRepository;
+	@Autowired private StationRepository stationRepository;
 
 	@Autowired private LineService lineService;
 
-	private Line line;
-	private Station upStation;
-	private Station downStation;
+	private Long line4;
+	private Station 사당역;
+	private Station 금정역;
+
+	public LineServiceTest() {
+	}
 
 	@BeforeEach
 	void setup() {
-		upStation = new Station("사당");
-		downStation = new Station("금정");
-		stationRepository.save(upStation);
-		stationRepository.save(downStation);
+		사당역 = new Station("사당역");
+		금정역 = new Station("금정역");
+		stationRepository.save(사당역);
+		stationRepository.save(금정역);
 
-		line = new Line("4호선", "#00A5DE");
-		lineRepository.save(line);
+		line4 = lineService.saveLine(new LineCreateRequest("4호선", "#00A5DE", 사당역.getId(), 금정역.getId(), 10)).getId();
 	}
 
-	@DisplayName("지하철 노선에 구간을 등록할 수 있다")
+	@DisplayName("지하철 노선을 등록할 수 있다")
 	@Test
-	void addSection() {
-		// given stationRepository와 lineRepository를 활용하여 초기값 셋팅
-
+	void saveLineTest() {
 		// when lineService.addSection 호출
-		lineService.addSection(line.getId(), new SectionRequest(downStation.getId(), upStation.getId(), 10));
+		Long saveLineId = lineService.saveLine(new LineCreateRequest("TEST_LINE", "BLACK", 사당역.getId(), 금정역.getId(), 10)).getId();
+
+		// then line.findLineById 메서드를 통해 검증
+		LineResponse lineResponse = lineService.findLineById(saveLineId);
+		assertAll(
+			() -> assertThat(lineResponse.getId()).isEqualTo(saveLineId),
+			() -> assertThat(lineResponse.getName()).isEqualTo("TEST_LINE"),
+			() -> assertThat(lineResponse.getColor()).isEqualTo("BLACK"),
+			() -> assertThat(lineResponse.getStations().size()).isEqualTo(2)
+		);
+	}
+
+	@DisplayName("등록된 지하철 노선 목록을 조회할 수 있다")
+	@Test
+	void showLinesTest() {
+		// given
+		lineService.saveLine(new LineCreateRequest("2호선", "#00A5DE", 사당역.getId(), 금정역.getId(), 10)).getId();
+
+		// when
+		List<LineResponse> lineResponses = lineService.showLines();
 
 		// then
-		// line.getSections 메서드를 통해 검증
-		List<Section> sections = line.getSections();
-		assertThat(sections.get(0).getUpStation()).isEqualTo(upStation);
-		assertThat(sections.get(0).getDownStation()).isEqualTo(downStation);
+		assertThat(lineResponses.size()).isEqualTo(2);
+	}
+
+	@DisplayName("등록된 지하철 노선의 정보를 수정할 수 있다")
+	@Test
+	void updateLineByIdTest() {
+		// when
+		lineService.updateLineById(line4, new LineUpdateRequest("2호선", "#00A5DE"));
+
+		// then line.findLineById 메서드를 통해 검증
+		LineResponse lineResponse = lineService.findLineById(line4);
+		assertAll(
+			() -> assertThat(lineResponse.getName()).isEqualTo("2호선"),
+			() -> assertThat(lineResponse.getColor()).isEqualTo("#00A5DE")
+		);
+	}
+
+	@DisplayName("등록된 지하철 노선을 삭제할 수 있다")
+	@Test
+	void deleteLineByIdTest() {
+		// when
+		lineService.deleteLineById(line4);
+
+		// then
+		assertThrows(NullPointerException.class, () -> lineService.findLineById(line4));
 	}
 }
--- src/test/java/nextstep/subway/unit/line/LineTest.java
+++ src/test/java/nextstep/subway/unit/line/LineTest.java
@@ -1,71 +1,25 @@
 package nextstep.subway.unit.line;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import nextstep.subway.line.Line;
-import nextstep.subway.section.Section;
-import nextstep.subway.station.Station;
 
 class LineTest {
 
-    private Line line;
-    private Station upStation;
-    private Station downStation;
-    private Section newSection;
-
-    @BeforeEach
-    void setup() {
-        line = new Line("4호선", "#00A5DE");
-
-        upStation = new Station("사당");
-        downStation = new Station("금정");
-
-        newSection = new Section(line, upStation, downStation, 10);
-        line.addSection(newSection);
-    }
-
-    @DisplayName("노선에 구간을 추가할 수 있다")
+    @DisplayName("노선의 정보를 수정할 수 있다")
     @Test
-    void addSection() {
-        Section addedSection = line.addSection(newSection);
-        assertThat(addedSection).isEqualTo(newSection);
-    }
+    void updateLineTest() {
+		// given
+		Line line4 = new Line("4호선", "#00A5DE");
 
-    @DisplayName("노선의 모든 역을 조회할 수 있다")
-    @Test
-    void getStations() {
-        List<Station> stations = line.getStations();
-        assertAll(
-            () -> assertThat(stations.size()).isEqualTo(2),
-            () -> assertThat(stations).containsOnly(upStation, downStation)
-        );
-    }
+		// when
+		line4.updateLine("00호선", "BLACK");
 
-    @DisplayName("노선의 마지막 구간을 삭제할 수 있다")
-    @Test
-    void removeSection() {
-        Station newDownStation = new Station("오이도");
-        Section newSection2 = new Section(line, downStation, newDownStation, 13);
-        line.addSection(newSection2);
-
-        line.removeSection(newDownStation);
-        List<Station> stations = line.getStations();
-        assertAll(
-            () -> assertThat(stations.size()).isEqualTo(2),
-            () -> assertThat(stations).containsOnly(upStation, downStation)
-        );
-    }
-
-    @DisplayName("노선의 구간이 1개라면 삭제할 수 없다")
-    @Test
-    void removeOneSection() {
-        assertThatThrownBy(() -> line.removeSection(downStation)).isInstanceOf(IllegalArgumentException.class);
+		// then
+        assertThat(line4.getName()).isEqualTo("00호선");
+        assertThat(line4.getColor()).isEqualTo("BLACK");
     }
 }
