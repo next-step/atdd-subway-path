6ec6cdf5ecf607cc193ca93df380033e1f15b0a7
wu2ee <gulb1602@gmail.com>
2023-02-14 23:04:21 +0900

    test : Line 구간 등록 요구사항 단위 테스트 추가

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -25,10 +25,64 @@ class LineTest {
     @Test
     void addSection() {
         // when
-        신분당선.addSection(강남역, 판교역, 3);
+        신분당선.addSection(강남역, 판교역, 10);
 
         // then
         assertThat(신분당선.getSections()).containsExactly(new Section(신분당선, 강남역, 판교역, 10));
+    }
+
+    @DisplayName("지하철 노선의 역 사이에 새로운 역을 등록")
+    @Test
+    void addLineSectionWithinSection() {
+        // given
+        신분당선.addSection(강남역, 판교역, 10);
+        Station 양재역 = new Station("양재역");
+        Station 청계산입구역 = new Station("청계산입구역");
+
+        // when
+        신분당선.addSection(강남역, 양재역, 4);
+        신분당선.addSection(청계산입구역, 판교역, 3);
+
+        // then
+        assertThat(신분당선.getSections()).containsExactlyInAnyOrder(
+                new Section(신분당선, 강남역, 양재역, 4),
+                new Section(신분당선, 양재역, 청계산입구역, 3),
+                new Section(신분당선, 청계산입구역, 판교역, 3)
+        );
+    }
+
+    @DisplayName("지하철 노선의 새로운 상행 종점역 구간을 등록")
+    @Test
+    void addLineSectionBeforeFirstStation() {
+        // given
+        신분당선.addSection(강남역, 판교역, 10);
+        Station 신논현역 = new Station("신논현역");
+
+        // when
+        신분당선.addSection(신논현역, 강남역, 2);
+
+        // then
+        assertThat(신분당선.getSections()).containsExactlyInAnyOrder(
+                new Section(신분당선, 강남역, 판교역, 10),
+                new Section(신분당선, 신논현역, 강남역, 2)
+        );
+    }
+
+    @DisplayName("지하철 노선의 새로운 하행 종점역 구간을 등록")
+    @Test
+    void addLineSectionAfterFinalStation() {
+        // given
+        신분당선.addSection(강남역, 판교역, 10);
+        Station 정자역 = new Station("정자역");
+
+        // when
+        신분당선.addSection(판교역, 정자역, 2);
+
+        // then
+        assertThat(신분당선.getSections()).containsExactlyInAnyOrder(
+                new Section(신분당선, 강남역, 판교역, 10),
+                new Section(신분당선, 판교역, 정자역, 2)
+        );
     }
 
     @DisplayName("지하철 노선에 등록된 역들을 조회한다.")
