e940b39e3b1f33427a5580959c36d0920f3f0eb2
Yunsik-Choi <choiys0311@gmail.com>
2023-02-14 14:19:37 +0900

    ⛑️ test : 경로 조회 관련 기능 인수 조건 정의

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -6,6 +6,7 @@ import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("경로 조회 관련 기능")
 public class PathAcceptanceTest extends AcceptanceTest {
@@ -38,5 +39,116 @@ public class PathAcceptanceTest extends AcceptanceTest {
         삼호선 = 지하철_노선_생성_요청("3호선", "orange", 교대역, 남부터미널역, 2).jsonPath().getLong("id");
 
         지하철_노선에_지하철_구간_생성_요청(삼호선, 남부터미널역, 양재역, 3);
+    }
+
+    /**
+     * When 경로 조회를 요청하면
+     * Then 출발역으로부터 도착역까지의 최단 경로에 있는 역 목록과
+     * And 조회한 경로 구간의 거리를 반환한다.
+     */
+    @DisplayName("경로 조회를 요청하면 출발역으로부터 도착역까지의 최단 경로에 있는 역 목록과 조회한 경로 구간의 거리를 반환한다.")
+    @Test
+    void findShortestPath() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    /**
+     * Given 경로 조회를 요청했을 때
+     * Then 출발역과 도착역이 일치하면
+     * And 에러 처리한다.
+     */
+    @DisplayName("경로 조회를 요청했을 때 출발역과 도착역이 일치하면 에러 처리한다.")
+    @Test
+    void findShortestPathIsSourceAndTargetEqual() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    /**
+     * Given 경로 조회를 요청했을 때
+     * Then 출발역과 도착역이 연결되어 있지 않으면
+     * And 에러 처리한다.
+     */
+    @DisplayName("경로 조회를 요청했을 때 출발역과 도착역이 연결되어 있지 않으면 에러 처리한다.")
+    @Test
+    void findShortestPathIsSourceAndTargetNotConnection() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("출발역 관련 에러")
+    class SourceExceptionTest {
+        /**
+         * Given 경로 조회를 요청했을 때
+         * Then 존재하지 않은 출발역으로 조회할 경우
+         * And 에러 처리한다.
+         */
+        @DisplayName("경로 조회를 요청했을 때 존재하지 않은 출발역으로 조회할 경우 에러 처리한다.")
+        @Test
+        void findShortestPathIsSourceNotExist() {
+            // given
+
+            // when
+
+            // then
+        }
+
+        /**
+         * Given 경로 조회를 요청했을 때
+         * Then 출발역을 상행역으로 하는 구간이 없는 경우
+         * And 에러 처리한다.
+         */
+        @DisplayName("경로 조회를 요청했을 때 존재하지 않은 출발역으로 조회할 경우 에러 처리한다.")
+        @Test
+        void findShortestPathIsSourceNotExistSection() {
+            // given
+
+            // when
+
+            // then
+        }
+    }
+
+    @DisplayName("도착역 관련 에러")
+    class TargetExceptionTest {
+        /**
+         * Given 경로 조회를 요청했을 때
+         * Then 존재하지 않은 도착역으로 조회할 경우
+         * And 에러 처리한다.
+         */
+        @DisplayName("경로 조회를 요청했을 때 존재하지 않은 도착역으로 조회할 경우 에러 처리한다.")
+        @Test
+        void findShortestPathIsTargetNotExist() {
+            // given
+
+            // when
+
+            // then
+        }
+
+        /**
+         * Given 경로 조회를 요청했을 때
+         * Then 도착역을 하행역으로 하는 구간이 없는 경우
+         * And 에러 처리한다.
+         */
+        @DisplayName("경로 조회를 요청했을 때 존재하지 않은 출발역으로 조회할 경우 에러 처리한다.")
+        @Test
+        void findShortestPathIsTargetNotExistSection() {
+            // given
+
+            // when
+
+            // then
+        }
     }
 }
