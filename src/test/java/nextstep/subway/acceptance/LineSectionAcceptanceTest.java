559b9b594b8a9b226e23d3f31ec5a967e3e3f8a3
wu2ee <gulb1602@gmail.com>
2023-02-14 22:29:22 +0900

    test : 상행 종점 역 구간과 하행 종점 역 구간 등록 인수 테스트 추가

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -29,9 +29,9 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         super.setUp();
 
         강남역 = 지하철역_생성_요청("강남역").jsonPath().getLong("id");
-        판교역 = 지하철역_생성_요청("양재역").jsonPath().getLong("id");
+        판교역 = 지하철역_생성_요청("판교역").jsonPath().getLong("id");
 
-        Map<String, String> lineCreateParams = createLineCreateParams(강남역, 판교역);
+        Map<String, String> lineCreateParams = createLineCreateParams(강남역, 판교역, 10);
         신분당선 = 지하철_노선_생성_요청(lineCreateParams).jsonPath().getLong("id");
     }
 
@@ -44,7 +44,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     void addLineSection() {
         // when
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(판교역, 정자역));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(판교역, 정자역, 3));
 
         // then
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
@@ -65,13 +65,53 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         Long 청계산입구역 = 지하철역_생성_요청("청계산입구역").jsonPath().getLong("id");
 
         // when
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 양재역));
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(청계산입구역, 판교역));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(강남역, 양재역, 4));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(청계산입구역, 판교역, 3));
 
         // then
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역, 청계산입구역, 판교역);
+    }
+
+    /**
+     * Given 새로운 구간을 위한 역을 추가하고
+     * When 지하철 노선에 새로운 구간 추가를 요청하면 (새로운 역을 상행 종점역에 등록하는 경우)
+     * Then 노선에 새로운 구간이 추가된다.
+     */
+    @DisplayName("지하철 노선의 새로운 상행 종점역 구간을 등록")
+    @Test
+    void addLineSectionBeforeFirstStation() {
+        // given
+        Long 신논현역 = 지하철역_생성_요청("신논현역").jsonPath().getLong("id");
+
+        // when
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(신논현역, 강남역, 2));
+
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(신논현역, 강남역, 판교역);
+    }
+
+    /**
+     * Given 새로운 구간을 위한 역을 추가하고
+     * When 지하철 노선에 새로운 구간 추가를 요청하면 (새로운 역을 하행 종점역에 등록하는 경우)
+     * Then 노선에 새로운 구간이 추가된다.
+     */
+    @DisplayName("지하철 노선의 새로운 하행 종점역 구간을 등록")
+    @Test
+    void addLineSectionAfterFinalStation() {
+        // given
+        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+
+        // when
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(판교역, 정자역, 2));
+
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 판교역, 정자역);
     }
 
     /**
@@ -84,7 +124,7 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     void removeLineSection() {
         // given
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(판교역, 정자역));
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(판교역, 정자역, 3));
 
         // when
         지하철_노선에_지하철_구간_제거_요청(신분당선, 정자역);
@@ -95,22 +135,22 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 판교역);
     }
 
-    private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
+    private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId, int distance) {
         Map<String, String> lineCreateParams;
         lineCreateParams = new HashMap<>();
         lineCreateParams.put("name", "신분당선");
         lineCreateParams.put("color", "bg-red-600");
         lineCreateParams.put("upStationId", upStationId + "");
         lineCreateParams.put("downStationId", downStationId + "");
-        lineCreateParams.put("distance", 10 + "");
+        lineCreateParams.put("distance", distance + "");
         return lineCreateParams;
     }
 
-    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId) {
+    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, int distance) {
         Map<String, String> params = new HashMap<>();
         params.put("upStationId", upStationId + "");
         params.put("downStationId", downStationId + "");
-        params.put("distance", 6 + "");
+        params.put("distance", distance + "");
         return params;
     }
 }
