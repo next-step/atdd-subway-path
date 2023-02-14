c70988e35d377eeb86c5e30ae9304f97c7f1670d
kimjio <jiokim222@gmail.com>
2023-02-15 00:49:33 +0900

    refactor(Sections): 변수명 변경

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -7,7 +7,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,60 +20,60 @@ public class Sections {
 
 
     @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
-    private List<Section> values = new ArrayList<>();
+    private List<Section> sections = new ArrayList<>();
 
 
     public void add(Section section) {
-        if (values.isEmpty()) {
-            values.add(section);
-            return;
-        }
 
         validateForAdding(section);
 
-        if(section.getDownStation().equals(findFirstStation())){
-            values.add(section);
+        if (canAddToFirstOrEndPosition(section)) {
+            sections.add(section);
             return;
         }
 
-        if(section.getUpStation().equals(findEndStation())){
-            values.add(section);
-            return;
-        }
-
-        values.stream()
+        sections.stream()
                 .filter(origin -> origin.hasOneMatchedStation(section))
                 .findFirst()
                 .ifPresent(origin -> {
-                    if(!section.hasShorterDistanceThan(origin)){
+                    if (!section.hasShorterDistanceThan(origin)) {
                         throw new DataIntegrityViolationException(CAN_UPDATE_IF_DISTANCE_SHORTER);
                     }
-                    values.add(origin.divideBy(section));
-                    values.add(section);
-                    values.remove(origin);
+                    sections.add(origin.divideBy(section));
+                    sections.add(section);
+                    sections.remove(origin);
                 });
     }
 
-    private void validateForAdding(Section section) {
+    private boolean canAddToFirstOrEndPosition(Section section) {
+        return sections.isEmpty()
+                || section.getDownStation().equals(findFirstStation())
+                || section.getUpStation().equals(findEndStation());
+    }
 
-        values.stream()
+    private void validateForAdding(Section section) {
+        if (sections.isEmpty()) {
+            return;
+        }
+
+        sections.stream()
                 .filter(origin -> origin.hasBothMatchedStation(section))
                 .findAny()
                 .ifPresent(i -> {
                     throw new DataIntegrityViolationException(SECTION_ALREADY_EXISTS);
                 });
 
-        values.stream()
+        sections.stream()
                 .filter(origin -> origin.hasOneMatchedStation(section))
                 .findAny()
                 .orElseThrow(() -> new DataIntegrityViolationException(MATCHED_SECTION_NOT_EXISTS));
     }
 
     public void delete(Station station) {
-        if (!values.get(values.size() - 1).getDownStation().equals(station)) {
+        if (!sections.get(sections.size() - 1).getDownStation().equals(station)) {
             throw new IllegalArgumentException();
         }
-        values.remove(values.size() - 1);
+        sections.remove(sections.size() - 1);
     }
 
     public List<Station> getStations() {
@@ -83,7 +82,7 @@ public class Sections {
         Station firstStation = findFirstStation();
         orderedStations.add(firstStation);
 
-        while (hasSectionContainingStation(firstStation)){
+        while (hasSectionContainingStation(firstStation)) {
             Section section = findSectionContainingStation(firstStation);
             orderedStations.add(section.getDownStation());
             firstStation = section.getDownStation();
@@ -92,22 +91,22 @@ public class Sections {
         return Collections.unmodifiableList(orderedStations);
     }
 
-    private Section findSectionContainingStation(Station station){
-        return values.stream()
-                .filter(i->i.getUpStation().equals(station))
+    private Section findSectionContainingStation(Station station) {
+        return sections.stream()
+                .filter(i -> i.getUpStation().equals(station))
                 .findFirst()
                 .orElse(new Section());
     }
 
     private boolean hasSectionContainingStation(Station firstStation) {
-        return values.stream().anyMatch(i->i.getUpStation().equals(firstStation));
+        return sections.stream().anyMatch(i -> i.getUpStation().equals(firstStation));
     }
 
     private Station findFirstStation() {
         List<Station> upStations = findUpStations();
         List<Station> downStations = findDownStations();
         return upStations.stream()
-                .filter(upStation->!downStations.contains(upStation))
+                .filter(upStation -> !downStations.contains(upStation))
                 .findAny()
                 .orElse(new Station());
     }
@@ -116,28 +115,24 @@ public class Sections {
         List<Station> upStations = findUpStations();
         List<Station> downStations = findDownStations();
         return downStations.stream()
-                .filter(downStation->!upStations.contains(downStation))
+                .filter(downStation -> !upStations.contains(downStation))
                 .findAny()
                 .orElse(new Station());
     }
 
-
-
     private List<Station> findUpStations() {
-        return values.stream()
+        return sections.stream()
                 .map(Section::getUpStation)
                 .collect(Collectors.toList());
     }
 
     private List<Station> findDownStations() {
-        return values.stream()
+        return sections.stream()
                 .map(Section::getDownStation)
                 .collect(Collectors.toList());
     }
 
     public List<Section> getValues() {
-        return Collections.unmodifiableList(this.values);
+        return Collections.unmodifiableList(this.sections);
     }
 }
-
-
