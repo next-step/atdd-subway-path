55de1ab5221485663f8f5ec7d049ada44fffe458
TSTP <86605720+seokjin0404@users.noreply.github.com>
2023-02-14 10:53:28 +0900

    test: Line Test 수정

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -4,6 +4,8 @@ import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
 import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -11,40 +13,42 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
+
+    private Station 강남역;
+    private Station 정자역;
+    private Station 광교역;
+
+    private Line 이호선;
+
+    @BeforeEach
+    void setUp() {
+        강남역 = new Station("강남역");
+        정자역 = new Station("정자역");
+        광교역 = new Station("광교역");
+        이호선 = new Line("2호선", "green", 강남역, 정자역, 10);
+    }
+    @DisplayName("지하철 구간 추가")
     @Test
     void addSection() {
-        Station 강남역 = new Station("강남역");
-        Station 정자역 = new Station("정자역");
-        Station 광교역 = new Station("광교역");
-
-        Line line = new Line("2호선", "green", 강남역, 정자역, 10);
-        line.addSection(정자역, 광교역, 15);
-        List<Section> sections = line.getSections();
+        이호선.addSection(정자역, 광교역, 15);
+        List<Section> sections = 이호선.getSections();
         assertThat(sections.get(1).getDownStation()).isEqualTo(광교역);
     }
 
+    @DisplayName("지하철역 목록 생성")
     @Test
     void getStations() {
-        Station 강남역 = new Station("강남역");
-        Station 정자역 = new Station("정자역");
-        Station 광교역 = new Station("광교역");
-
-        Line line = new Line("2호선", "green", 강남역, 정자역, 10);
-        line.addSection(정자역, 광교역, 15);
-        List<Station> stations = line.getStations();
+        이호선.addSection(정자역, 광교역, 15);
+        List<Station> stations = 이호선.getStations();
         assertThat(stations).containsExactly(Arrays.array(강남역, 정자역, 광교역));
     }
 
+    @DisplayName("지하철 구간 삭제")
     @Test
     void removeSection() {
-        Station 강남역 = new Station("강남역");
-        Station 정자역 = new Station("정자역");
-        Station 광교역 = new Station("광교역");
-
-        Line line = new Line("2호선", "green", 강남역, 정자역, 10);
-        line.addSection(정자역, 광교역, 15);
-        line.removeSection();
-        List<Station> stations = line.getStations();
+        이호선.addSection(정자역, 광교역, 15);
+        이호선.removeSection();
+        List<Station> stations = 이호선.getStations();
         assertThat(stations).doesNotContain(광교역);
     }
 }
