fb25339d939da9045001e0c12587517bde377b9b
bada.kang <kangbada0728@gmail.com>
2023-02-14 22:24:50 +0900

    update: Distance가 1 이상이도록 조건 수정

--- src/main/java/nextstep/subway/domain/section/Distance.java
+++ src/main/java/nextstep/subway/domain/section/Distance.java
@@ -12,7 +12,7 @@ public class Distance {
     private int distance;
 
     public Distance(int distance) {
-        if (distance < 0) {
+        if (distance <= 0) {
             throw new IllegalArgumentException("거리는 1 이상이어야 합니다.");
         }
         this.distance = distance;
