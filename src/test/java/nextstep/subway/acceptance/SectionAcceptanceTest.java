751e390ab5738048868d54f1b96a5d238b7c9880
dhlee3994 <dhlee3994@gmail.com>
2023-02-15 13:22:17 +0900

    Test: 새로운 구간을 노선의 중간에 추가하는 기능 테스트

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -11,13 +11,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static nextstep.subway.acceptance.LineSteps.지하철_노선_생성_요청;
-import static nextstep.subway.acceptance.LineSteps.지하철_노선_조회_요청;
-import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철_구간_생성_요청;
-import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철_구간_제거_요청;
+import static nextstep.subway.acceptance.LineSteps.*;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
-import static nextstep.subway.utils.AssertionUtils.목록은_다음을_순서대로_포함한다;
-import static nextstep.subway.utils.AssertionUtils.응답코드_200을_반환한다;
+import static nextstep.subway.domain.exception.line.LineErrorMessage.*;
+import static nextstep.subway.utils.AssertionUtils.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 구간 관리 기능")
@@ -81,61 +78,94 @@ class SectionAcceptanceTest extends AcceptanceTest {
         목록은_다음을_순서대로_포함한다(조회된_역_목록, 신논현역, 강남역, 양재역);
     }
 
-
-
     /**
-     * given:
-     * when :
-     * then :
+     * given: 지하철 노선 중간의 역을 상행역으로 갖는 구간을 추가하고
+     * when : 지하철 노선 목록을 조회하면
+     * then : 역이 새로운 순서대로 조회된다.
      */
     @DisplayName("지하철 노선의 중간에 노선의 역을 상행역으로 갖는 구간을 등록한다")
     @Test
     void addMiddleSectionFromUpStation() {
+        // given
+        Long 신강남역 = 지하철역_생성_요청("신강남역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createLineCreateParams(강남역, 신강남역, 6));
 
+        // when
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+
+        // then
+        응답코드_200을_반환한다(response);
+
+        final List<Long> 조회된_역_목록 = response.jsonPath().getList("stations.id", Long.class);
+        목록은_다음을_순서대로_포함한다(조회된_역_목록, 강남역, 신강남역, 양재역);
     }
 
     /**
-     * given:
-     * when :
-     * then :
+     * given: 지하철 노선 중간의 역을 하행역으로 갖는 구간을 추가하고
+     * when : 지하철 노선 목록을 조회하면
+     * then : 역이 새로운 순서대로 조회된다.
      */
     @DisplayName("지하철 노선의 중간에 노선의 역을 하행역으로 갖는 구간을 등록한다.")
     @Test
     void addMiddleSectionFromDownStation() {
+        // given
+        Long 신강남역 = 지하철역_생성_요청("신강남역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createLineCreateParams(신강남역, 양재역, 6));
 
+        // when
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+
+        // then
+        응답코드_200을_반환한다(response);
+
+        final List<Long> 조회된_역_목록 = response.jsonPath().getList("stations.id", Long.class);
+        목록은_다음을_순서대로_포함한다(조회된_역_목록, 강남역, 신강남역, 양재역);
     }
 
     /**
-     * given:
-     * when :
-     * then :
+     * when : 지하철 노선의 중간 구간에 같거나 더 긴 길이를 갖는 새로운 구간을 추가하면
+     * then : 오류가 발생한다.
      */
     @DisplayName("새로운 역 사이의 길이가 기존 역사이의 길이보다 같거나 큰 경우 등록할 수 없다")
     @Test
     void sectionCannotRegisteredWhenNewSectionIsLongerOrEqual() {
+        // when
+        Long 신강남역 = 지하철역_생성_요청("신강남역").jsonPath().getLong("id");
+        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createLineCreateParams(신강남역, 양재역));
 
+        // then
+        응답코드_400을_반환한다(response);
+        에러메시지는_다음과_같다(response, INVALID_DISTANCE.getMessage());
     }
 
     /**
-     * given:
-     * when :
-     * then :
+     * when : 상행역과 하행역이 모두 노선에 등록되어 있는 구간을 등록하면
+     * then : 오류가 발생한다.
      */
     @DisplayName("상행역과 하행역이 모두 노선에 등록되어있는 경우 등록할 수 없다.")
     @Test
     void sectionCannotRegisteredWhenBothAreRegistered() {
+        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createLineCreateParams(강남역, 양재역, 5));
 
+        // then
+        응답코드_400을_반환한다(response);
+        에러메시지는_다음과_같다(response, LINE_HAS_BOTH_STATION.getMessage());
     }
 
     /**
-     * given:
-     * when :
-     * then :
+     * when : 상행역과 하행역이 모두 노선에 등록되어있지 않은 구간을 등록하면
+     * then : 오류가 발생한다.
      */
     @DisplayName("상행역과 하행역 모두 노선에 등록되어있지 않는 경우 등록할 수 없다.")
     @Test
     void sectionCannotRegisteredWhenBothAreNotRegistered() {
+        Long 서울대입구역 = 지하철역_생성_요청("서울대입구역").jsonPath().getLong("id");
+        Long 봉천역 = 지하철역_생성_요청("봉천역").jsonPath().getLong("id");
+        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_생성_요청(신분당선, createLineCreateParams(서울대입구역, 봉천역));
 
+        // then
+        응답코드_400을_반환한다(response);
+        에러메시지는_다음과_같다(response, NEW_SECTION_COULD_HAVE_ANY_REGISTERED_STATION.getMessage());
     }
 
     /**
@@ -167,6 +197,17 @@ class SectionAcceptanceTest extends AcceptanceTest {
         lineCreateParams.put("upStationId", upStationId + "");
         lineCreateParams.put("downStationId", downStationId + "");
         lineCreateParams.put("distance", 10 + "");
+        return lineCreateParams;
+    }
+
+    private Map<String, String> createLineCreateParams(final Long upStationId, final Long downStationId, final int distance) {
+        Map<String, String> lineCreateParams;
+        lineCreateParams = new HashMap<>();
+        lineCreateParams.put("name", "신분당선");
+        lineCreateParams.put("color", "bg-red-600");
+        lineCreateParams.put("upStationId", upStationId + "");
+        lineCreateParams.put("downStationId", downStationId + "");
+        lineCreateParams.put("distance", distance + "");
         return lineCreateParams;
     }
 
--- src/test/java/nextstep/subway/utils/AssertionUtils.java
+++ src/test/java/nextstep/subway/utils/AssertionUtils.java
@@ -2,10 +2,12 @@ package nextstep.subway.utils;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.domain.exception.ErrorResponse;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
 
 public class AssertionUtils {
@@ -15,10 +17,19 @@ public class AssertionUtils {
         assertThat(response.statusCode()).isEqualTo(OK.value());
     }
 
+    public static void 응답코드_400을_반환한다(final ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(BAD_REQUEST.value());
+    }
+
 
     // list
-
     public static <T> void 목록은_다음을_순서대로_포함한다(final List<T> list, final T... obj) {
         assertThat(list).containsExactly(obj);
+    }
+
+    // error
+    public static void 에러메시지는_다음과_같다(ExtractableResponse<Response> response, String message) {
+        ErrorResponse errorResponse = response.as(ErrorResponse.class);
+        assertThat(errorResponse.getMessage()).isEqualTo(message);
     }
 }
