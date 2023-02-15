cc4e835693479b4b443eaec5cc737c1ef2ceb10a
michol.lee <lovethefeel@gmail.com>
2023-02-15 10:11:12 +0900

    refactor(test): 경로찾기 라이브러리 테스트

--- src/test/java/nextstep/study/JgraphtTest.java
+++ src/test/java/nextstep/study/JgraphtTest.java
@@ -5,12 +5,12 @@ import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.alg.shortestpath.KShortestPaths;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 
 class JgraphtTest {
     @Test
@@ -29,7 +29,29 @@ class JgraphtTest {
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
         List<String> shortestPath = dijkstraShortestPath.getPath(source, target).getVertexList();
 
-        assertThat(shortestPath.size()).isEqualTo(3);
+        assertThat(shortestPath).hasSize(3);
+    }
+
+    @DisplayName("최적의 경로가 없는 경우에는 null을 반환한다.")
+    @Test
+    void error_getDijkstraShortestPath() {
+        String source = "v4";
+        String target = "v1";
+
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        graph.addVertex("v4");
+        graph.addVertex("v5");
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+        graph.setEdgeWeight(graph.addEdge("v4", "v5"), 5);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+
+        assertThat(dijkstraShortestPath.getPath(source, target)).isNull();
     }
 
     @Test
