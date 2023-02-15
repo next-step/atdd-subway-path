b44fc1c4e7275af7e40c643dc3990929a6f66d31
Yunsik-Choi <choiys0311@gmail.com>
2023-02-15 20:29:59 +0900

    ♻️ refactor : LineRepository findAll fetch join으로 수정

--- src/main/java/nextstep/subway/domain/LineRepository.java
+++ src/main/java/nextstep/subway/domain/LineRepository.java
@@ -3,8 +3,9 @@ package nextstep.subway.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
-    @Override
+    @Query("SELECT l FROM Line l left join fetch l.sections.sections")
     List<Line> findAll();
 }
