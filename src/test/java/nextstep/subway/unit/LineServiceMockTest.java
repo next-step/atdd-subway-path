0380c76ecd344c199b418ecc7058f2110eab2f3b
Jaeyoon <wodbs4338@naver.com>
2023-02-15 22:39:14 +0900

    실습 - LineServiceMockTest, LineServiceTest 작성

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,11 +1,24 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.LineResponse;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.applicaion.dto.StationResponse;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
@@ -18,11 +31,34 @@ public class LineServiceMockTest {
     void addSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        Long lineId = 10L;
+        SectionRequest request = setUpStubs(lineId);
 
         // when
         // lineService.addSection 호출
+        LineService lineService = new LineService(lineRepository, stationService);
+        lineService.addSection(lineId, request);
 
         // then
         // lineService.findLineById 메서드를 통해 검증
+        LineResponse response = lineService.findById(lineId);
+        List<Long> stationIds = response.getStations().stream().map(StationResponse::getId).collect(Collectors.toList());
+        assertThat(stationIds).contains(request.getDownStationId(), request.getUpStationId());
     }
+
+    private SectionRequest setUpStubs(Long lineId) {
+        Line line = new Line("신분당선", "red");
+        Station upStation = new Station("양재역");
+        Station downStation = new Station("판교역");
+        SectionRequest request = new SectionRequest(1L, 2L, 10);
+
+        when(lineRepository.findById(lineId)).thenReturn(Optional.of(line));
+        when(stationService.findById(request.getUpStationId())).thenReturn(upStation);
+        when(stationService.findById(request.getDownStationId())).thenReturn(downStation);
+        when(stationService.createStationResponse(upStation)).thenReturn(new StationResponse(request.getUpStationId(), upStation.getName()));
+        when(stationService.createStationResponse(downStation)).thenReturn(new StationResponse(request.getDownStationId(), downStation.getName()));
+
+        return request;
+    }
+
 }
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,20 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -23,11 +31,24 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Line line = lineRepository.save(new Line("신분당선", "red"));
+        Station upStation = stationRepository.save(new Station("양재역"));
+        Station downStation = stationRepository.save(new Station("판교역"));
 
         // when
         // lineService.addSection 호출
+        SectionRequest request = new SectionRequest(upStation.getId(), downStation.getId(), 10);
+        lineService.addSection(line.getId(), request);
 
         // then
         // line.getSections 메서드를 통해 검증
+        List<Section> sections = line.getSections();
+        assertThat(sections.size()).isEqualTo(1);
+
+        Section section = sections.get(0);
+        assertThat(section.getLine().getId()).isEqualTo(line.getId());
+        assertThat(section.getUpStation().getId()).isEqualTo(upStation.getId());
+        assertThat(section.getDownStation().getId()).isEqualTo(downStation.getId());
     }
+
 }
