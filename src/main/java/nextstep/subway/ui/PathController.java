8e5100b69b84bfc00ad58f06409e44783f3f2b4a
michol.lee <lovethefeel@gmail.com>
2023-02-15 10:00:40 +0900

    feat: 경로찾기 컨트롤러 생성

---
+++ src/main/java/nextstep/subway/ui/PathController.java
@@ -0,0 +1,27 @@
+package nextstep.subway.ui;
+
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/paths")
+public class PathController {
+
+    private final PathService pathService;
+
+    public PathController(final PathService pathService) {
+        this.pathService = pathService;
+    }
+
+    @GetMapping
+    public ResponseEntity<PathResponse> showRoutes(@RequestParam(name = "source") final Long source
+            , @RequestParam(name = "target") final Long target) {
+        final PathResponse pathResponse = pathService.showRoutes(source, target);
+        return ResponseEntity.ok().body(pathResponse);
+    }
+}
