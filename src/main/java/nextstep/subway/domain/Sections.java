b0018a11755b77e63fd60b64279860170b0b03ce
sm9171 <sm9171@nate.com>
2023-02-15 11:18:33 +0900

    refactor(Sections): 피드백 요청으로 인한 수정 * 사용하지 않은 메서드 제거 * null을 Optional로 활용

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -16,109 +16,99 @@ import static nextstep.subway.ui.error.exception.ErrorCode.*;
 
 @Embeddable
 public class Sections {
-    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
-    private List<Section> sections = new ArrayList<>();
+	@OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+	private List<Section> sections = new ArrayList<>();
 
-    public List<Section> getAllSections() {
-        return sections;
-    }
+	public List<Section> getAllSections() {
+		return sections;
+	}
 
-    public void addSection(Line line, Station upStation, Station downStation, int distance) {
-        if (sections.isEmpty()) {
-            sections.add(new Section(line, upStation, downStation, distance));
-            return;
-        }
+	public void addSection(Line line, Station upStation, Station downStation, int distance) {
+		if (sections.isEmpty()) {
+			sections.add(new Section(line, upStation, downStation, distance));
+			return;
+		}
 
-        for (Section section : line.getSections()) {
-            if (section.getDownStation().equals(downStation) || section.getUpStation().equals(downStation)) {
-                throw new InvalidValueException(ALREADY_REGISTERED_STAION);
-            }
-        }
+		for (Section section : line.getSections()) {
+			if (section.getDownStation().equals(downStation) || section.getUpStation().equals(downStation)) {
+				throw new InvalidValueException(ALREADY_REGISTERED_STAION);
+			}
+		}
 
-        Section findedSection = sections.stream().filter(s -> s.getUpStation().equals(upStation))
-                .findFirst()
-                .orElse(null);
+		sections.stream().filter(s -> s.getUpStation().equals(upStation))
+				.findFirst()
+				.ifPresentOrElse(section -> {
+							splitSection(line, downStation, distance, section);
+						}, () -> {
+							sections.add(new Section(line, upStation, downStation, distance));
+						}
+				);
+	}
 
-        if (findedSection != null) {
-            splitSection(line, downStation, distance, findedSection);
-            return;
-        }
+	public void remove(Line line, Station station) {
 
-        sections.add(new Section(line, upStation, downStation, distance));
-    }
+		if (sections.size() <= 1) {
+			throw new BusinessException(SECTION_NOT_DELETE_THEN_ONE);
+		}
 
-    public void remove(Line line, Station station) {
+		Station targetStation = getAllStations().stream()
+				.filter(station::equals)
+				.findFirst()
+				.orElseThrow(() -> new EntityNotFoundException(STATION_NOT_EXISTS));
 
-        if (sections.size() <= 1) {
-            throw new BusinessException(SECTION_NOT_DELETE_THEN_ONE);
-        }
+		List<Section> targetSections = line.getSections().stream()
+				.filter(section -> section.getUpStation().equals(targetStation) || section.getDownStation().equals(targetStation))
+				.collect(Collectors.toList());
 
-        Station targetStation = getAllStations().stream()
-                .filter(station::equals)
-                .findFirst()
-                .orElseThrow(() -> new EntityNotFoundException(STATION_NOT_EXISTS));
+		if (targetSections.size() > 1) {
+			Section firstSection = targetSections.get(0);
+			Section secondSection = targetSections.get(1);
+			this.sections.add(new Section(line, firstSection.getUpStation(), secondSection.getDownStation(), firstSection.getDistance() + secondSection.getDistance()));
+			this.sections.removeIf(section -> section.equals(targetSections.get(0)) || section.equals(targetSections.get(1)));
+			return;
+		}
 
-        List<Section> targetSections = line.getSections().stream()
-                .filter(section -> section.getUpStation().equals(targetStation) || section.getDownStation().equals(targetStation))
-                .collect(Collectors.toList());
+		this.sections.removeIf(section -> section.equals(targetSections.get(0)));
+	}
 
-        if (targetSections.size() > 1) {
-            Section firstSection = targetSections.get(0);
-            Section secondSection = targetSections.get(1);
-            this.sections.add(new Section(line, firstSection.getUpStation(), secondSection.getDownStation(), firstSection.getDistance() + secondSection.getDistance()));
-            this.sections.removeIf(section -> section.equals(targetSections.get(0)) || section.equals(targetSections.get(1)));
-            return;
-        }
+	public List<Station> getStations() {
+		return this.sections.stream()
+				.map(s -> List.of(s.getUpStation(), s.getDownStation()))
+				.flatMap(Collection::stream)
+				.distinct()
+				.collect(Collectors.toList());
+	}
 
-        this.sections.removeIf(section -> section.equals(targetSections.get(0)));
-    }
+	private void splitSection(Line line, Station downStation, int distance, Section oldSection) {
+		Station oldUpstation = oldSection.getUpStation();
+		Station oldDownStation = oldSection.getDownStation();
+		int oldDistance = oldSection.getDistance();
 
-    public int size() {
-        return sections.size();
-    }
-
-    public boolean isEmpty() {
-        return sections.isEmpty();
-    }
-
-    public List<Station> getStations() {
-        return this.sections.stream()
-                .map(s -> List.of(s.getUpStation(), s.getDownStation()))
-                .flatMap(Collection::stream)
-                .distinct()
-                .collect(Collectors.toList());
-    }
-
-    private void splitSection(Line line, Station downStation, int distance, Section oldSection) {
-        Station oldUpstation = oldSection.getUpStation();
-        Station oldDownStation = oldSection.getDownStation();
-        int oldDistance = oldSection.getDistance();
-
-        if (distance >= oldDistance) {
-            throw new InvalidValueException(SECTION_NOT_LONGER_THEN_EXISTING_SECTION);
-        }
+		if (distance >= oldDistance) {
+			throw new InvalidValueException(SECTION_NOT_LONGER_THEN_EXISTING_SECTION);
+		}
 
 
-        sections.remove(oldSection);
+		sections.remove(oldSection);
 
-        sections.addAll(List.of(
-                new Section(line, downStation, oldDownStation, oldDistance - distance),
-                new Section(line, oldUpstation, downStation, distance)
-        ));
-    }
+		sections.addAll(List.of(
+				new Section(line, downStation, oldDownStation, oldDistance - distance),
+				new Section(line, oldUpstation, downStation, distance)
+		));
+	}
 
-    private List<Station> getAllStations() {
-        List<Station> stations = new ArrayList<>();
+	private List<Station> getAllStations() {
+		List<Station> stations = new ArrayList<>();
 
-        if (this.sections.isEmpty()) {
-            return stations;
-        }
+		if (this.sections.isEmpty()) {
+			return stations;
+		}
 
-        stations.add(this.sections.get(0).getUpStation());
-        for (Section section : this.sections) {
-            stations.add(section.getDownStation());
-        }
+		stations.add(this.sections.get(0).getUpStation());
+		for (Section section : this.sections) {
+			stations.add(section.getDownStation());
+		}
 
-        return stations;
-    }
+		return stations;
+	}
 }
