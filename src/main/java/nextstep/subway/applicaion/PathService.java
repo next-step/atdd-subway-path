6bcd927093addf7e4cc2dd6047d295bc144cc5bb
Yunsik-Choi <choiys0311@gmail.com>
2023-02-16 13:06:40 +0900

    ⚡feat : Path Mapper 구현

--- src/main/java/nextstep/subway/applicaion/PathService.java
+++ src/main/java/nextstep/subway/applicaion/PathService.java
@@ -1,8 +1,6 @@
 package nextstep.subway.applicaion;
 
-import java.util.List;
 import nextstep.subway.applicaion.dto.PathResponse;
-import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.Station;
 import nextstep.subway.domain.dto.PathDto;
 import org.springframework.stereotype.Service;
@@ -13,32 +11,26 @@ import org.springframework.transaction.annotation.Transactional;
 public class PathService {
     private final StationService stationService;
     private final PathFinderService pathFinderService;
-    private final StationMapper stationMapper;
+    private final PathMapper pathMapper;
 
     public PathService(
             final StationService stationService,
             final PathFinderService pathFinderService,
-            final StationMapper stationMapper
+            final PathMapper pathMapper
     ) {
         this.stationService = stationService;
         this.pathFinderService = pathFinderService;
-        this.stationMapper = stationMapper;
+        this.pathMapper = pathMapper;
     }
 
     public PathResponse findPathBy(final long source, final long target) {
         Station sourceStation = stationService.findById(source);
         Station targetStation = stationService.findById(target);
-        return createPathResponse(findPath(sourceStation, targetStation));
+        return pathMapper.toResponseFrom(findPath(sourceStation, targetStation));
     }
 
     private PathDto findPath(final Station sourceStation, final Station targetStation) {
         pathFinderService.initGraph();
         return pathFinderService.find(sourceStation, targetStation);
-    }
-
-    private PathResponse createPathResponse(final PathDto pathDto) {
-        List<StationResponse> stations = stationMapper.toResponseFrom(pathDto.getNodes());
-        double distance = pathDto.getWeight();
-        return new PathResponse(stations, distance);
     }
 }
