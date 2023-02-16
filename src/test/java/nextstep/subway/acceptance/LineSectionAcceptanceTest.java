0b993d75befb710b474032058deec68afec4aa19
dongho.geum <dongho.geum@myrealtrip.com>
2023-02-16 17:59:17 +0900

    test: 지하철 노선에 어느 구간이든 제거 할 수 있다 인수 테스트 작성

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -171,25 +171,52 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         }
     }
 
-    /**
-     * Given 지하철 노선에 새로운 구간 추가를 요청 하고
-     * When 지하철 노선의 마지막 구간 제거를 요청 하면
-     * Then 노선에 구간이 제거된다
-     */
-    @DisplayName("지하철 노선에 구간을 제거")
-    @Test
-    void removeLineSection() {
-        // given
-        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+    @Nested
+    @DisplayName("구간 삭제")
+    class 지하철_노선에_구간_삭제 {
 
-        // when
-        지하철_노선에_지하철_구간_제거_요청(신분당선, 정자역);
+        /**
+         * Given 지하철 노선에 새로운 구간 추가를 요청 하고
+         * When 지하철 노선의 마지막 구간 제거를 요청 하면
+         * Then 노선에 구간이 제거된다
+         */
+        @DisplayName("지하철 노선에 구간을 제거")
+        @Test
+        void removeLineSection() {
+            // given
+            Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+            지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
 
-        // then
-        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역);
+            // when
+            지하철_노선에_지하철_구간_제거_요청(신분당선, 정자역);
+
+            // then
+            ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역);
+        }
+
+        /**
+         * Given 지하철 노선에 새로운 구간 추가를 요청 하고
+         * When 지하철 노선의 어느 구간이든 제거를 요청 하면
+         * Then 노선에 구간이 제거된다
+         */
+        @DisplayName("지하철 노선에 어느 구간이든 제거 할 수 있다")
+        @Test
+        void 지하철_노선에_어느_구간이든_제거_할_수_있다() {
+            // given
+            Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+            지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+
+            // when
+            지하철_노선에_지하철_구간_제거_요청(신분당선, 양재역);
+
+            // then
+            ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 정자역);
+        }
+
     }
 
     private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
