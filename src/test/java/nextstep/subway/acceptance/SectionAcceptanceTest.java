d9957de7b4d36e1092ed083ed283f086d698217f
Jaeyoon <wodbs4338@naver.com>
2023-02-16 21:35:42 +0900

    step1 - 지하철 구간 추가 기능 개선 관련 인수 시나리오 정의

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -39,9 +39,9 @@ class SectionAcceptanceTest extends AcceptanceTest {
      * When 지하철 노선에 새로운 구간 추가를 요청 하면
      * Then 노선에 새로운 구간이 추가된다
      */
-    @DisplayName("지하철 노선에 구간을 등록")
+    @DisplayName("지하철 구간 등록 - 정상1 : 지하철 노선에 구간을 등록")
     @Test
-    void addLineSection() {
+    void addLineSection_ValidCase1() {
         // when
         Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
         지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
@@ -51,6 +51,80 @@ class SectionAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역, 정자역);
     }
+
+    /**
+     * Given 지하철 노선에 구간을 생성한 뒤
+     * When 생성된 구간의 상행역 또는 하행역을 기준으로 그 사이에 새로운 구간을 추가하면
+     *   - 단, 새로운 구간의 길이는 기존 구간의 길이보다 작아야한다.
+     * Then 새로운 두 개의 구간이 생성된다. (상행역 - 추가된역 & 추가된역 - 하행역)
+     *   - 노선의 총 길이 = 기존 구간의 길이
+     */
+    @DisplayName("지하철 구간 등록 - 정상2 : 기존 구간 사이에 새로운 구간 추가")
+    @Test
+    void addLineSection_ValidCase2() {
+
+    }
+
+    /**
+     * Given 지하철 노선에 구간을 생성한 뒤
+     * When 새로운 역을 상행 종점으로 등록할 경우
+     * Then 새로운 두 개의 구간이 생성된다.
+     *   - 추가된 역이 해당 노선의 상행 종점역이 된다.
+     *   - 노선의 총 길이 = 기존 구간 길이 + 새로운 구간 길이
+     */
+    @DisplayName("지하철 구간 등록 - 정상3 : 새로운 역이 상행 종점인 구간 추가")
+    @Test
+    void addLineSection_ValidCase3() {
+
+    }
+
+    /**
+     * Given 지하철 노선에 구간을 생성한 뒤
+     * When 새로운 역을 하행 종점으로 등록할 경우
+     * Then 새로운 두 개의 구간이 생성된다.
+     *   - 추가된 역이 해당 노선의 하행 종점역이 된다.
+     *   - 노선의 총 길이 = 기존 구간 길이 + 새로운 구간 길이
+     */
+    @DisplayName("지하철 구간 등록 - 정상4 : 새로운 역이 하행 종점인 구간 추가")
+    @Test
+    void addLineSection_ValidCase4() {
+
+    }
+
+    /**
+     * Given 지하철 노선에 구간을 생성한 뒤
+     * When 생성된 구간의 상행역 또는 하행역을 기준으로 그 사이에 새로운 구간을 추가하면
+     *    - 단, 새로운 구간의 길이는 기존 구간의 길이보다 크거나 같다.
+     * Then 구간이 추가되지 않는다.
+     */
+    @DisplayName("지하철 구간 등록 - 예외1 : 기존 구간 사이에 새로운 구간을 추가하려는데, 새로운 구간의 길이가 기존 구간의 길이 이상인 경우")
+    @Test
+    void addLineSection_InvalidCase1() {
+
+    }
+
+    /**
+     * Given 지하철 노선에 구간을 생성한 뒤
+     * When 상,하행역 모두 이미 노선에 존재하는 새로운 구간을 추가하려는 경우
+     * Then 구간이 추가되지 않는다.
+     */
+    @DisplayName("지하철 구간 등록 - 예외2: 새로운 구간의 상,하행역 모두 이미 노선에 등록된 경우")
+    @Test
+    void addLineSection_InvalidCase2() {
+
+    }
+
+    /**
+     * Given 지하철 노선에 구간을 생성한 뒤
+     * When 상,하행역 모두 노선에 존재하지 않는 새로운 구간을 추가하려는 경우
+     * Then 구간이 추가되지 않는다.
+     */
+    @DisplayName("지하철 구간 등록 - 예외3: 새로운 구간의 상,하행역 모두 노선에 포함되지 않은 경우")
+    @Test
+    void addLineSection_InvalidCase3() {
+
+    }
+
 
     /**
      * Given 지하철 노선에 새로운 구간 추가를 요청 하고
