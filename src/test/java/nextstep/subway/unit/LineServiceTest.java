0ab7a2d2e6d60f0ac7d82c751f6bd7f8cb4438fa
ji-one <rlawldnjs8@gmail.com>
2023-02-17 17:24:47 +0900

    test: 구간 서비스 단위 테스트 without Mock 작성

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,8 +1,16 @@
 package nextstep.subway.unit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @Transactional
 public class LineServiceTest {
+
     @Autowired
     private StationRepository stationRepository;
     @Autowired
@@ -20,14 +29,26 @@ public class LineServiceTest {
     private LineService lineService;
 
     @Test
+    @DisplayName("지하철 구간을 추가한다.")
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 미금역 = stationRepository.save(new Station("미금역"));
+        Station 판교역 = stationRepository.save(new Station("판교역"));
+        Line 신분당선 = lineRepository.save(new Line("신분당선", "red"));
 
         // when
         // lineService.addSection 호출
+        SectionRequest sectionRequest = new SectionRequest(미금역.getId(), 판교역.getId(), 10);
+        lineService.addSection(신분당선.getId(), sectionRequest);
 
         // then
         // line.getSections 메서드를 통해 검증
+        List<Section> sections = 신분당선.getSections();
+        assertThat(sections).hasSize(1);
+
+        Section addedSection = sections.get(0);
+        assertThat(addedSection.getUpStation()).isEqualTo(미금역);
+        assertThat(addedSection.getDownStation()).isEqualTo(판교역);
     }
 }
