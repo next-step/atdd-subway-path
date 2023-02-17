5949d3b1b65149af97e64570e281582ff212b4d1
최현웅 <121544753+hyunwoongchoi-29cm@users.noreply.github.com>
2023-02-17 23:04:12 +0900

    test : 지하철 경로 조회 인수테스트 수정

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -23,23 +23,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("경로조회 기능 ")
 public class PathAcceptanceTest extends AcceptanceTest {
 
-    private Long 신분당선;
-
+    private Long 교대역;
     private Long 강남역;
     private Long 양재역;
-    private Long 양재시민의숲역;
+    private Long 남부터미널역;
+    private Long 이호선;
+    private Long 신분당선;
+    private Long 삼호선;
+
+    /**
+     * 교대역    --- *2호선* ---   강남역
+     * |                        |
+     * *3호선*                   *신분당선*
+     * |                        |
+     * 남부터미널역  --- *3호선* ---   양재
+     */
     @BeforeEach
-    void setup() {
+    public void setUp() {
         super.setUp();
+
+        교대역 = 지하철역_생성_요청("교대역").jsonPath().getLong("id");
         강남역 = 지하철역_생성_요청("강남역").jsonPath().getLong("id");
         양재역 = 지하철역_생성_요청("양재역").jsonPath().getLong("id");
-        양재시민의숲역 = 지하철역_생성_요청("양재시민의숲역").jsonPath().get("id");
+        남부터미널역 = 지하철역_생성_요청("남부터미널역").jsonPath().getLong("id");
 
-        Map<String, String> lineCreateParams = createLineCreateParams(강남역, 양재역);
-        신분당선 = 지하철_노선_생성_요청(lineCreateParams).jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 양재시민의숲역));
+        이호선 = 지하철_노선_생성_요청("2호선", "green", 교대역, 강남역, 10);
+        신분당선 = 지하철_노선_생성_요청("신분당선", "red", 강남역, 양재역, 10);
+        삼호선 = 지하철_노선_생성_요청("3호선", "orange", 교대역, 남부터미널역, 2);
+
+        지하철_노선에_지하철_구간_생성_요청(삼호선, createSectionCreateParams(남부터미널역, 양재역, 3));
     }
-
     /**
      * Given 강남역 - 양재역 - 양재 시민의 숲역이 주어질때
      * When 강남역에서 - 양재시민의 숲역 경로를 조회하면
@@ -48,33 +61,23 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @DisplayName("출발역과 도착역을 통해 경로를 조회할 수 있다.")
     @Test
     void pathTest() {
-        ExtractableResponse<Response> response = 지하철역_경로_조회_요청();
+        ExtractableResponse<Response> response = 지하철역_경로_조회_요청(교대역, 양재역);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.contentType()).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
 
-        List<String> names = response.jsonPath().getList("stations.name", String.class);
+        List<Long> ids = response.jsonPath().getList("stations.id", Long.class);
 
-        assertThat(names).containsExactly("강남역", "양재역", "양재시민의숲역");
-        assertThat(response.jsonPath().getInt("distance")).isEqualTo(16);
+        assertThat(ids).containsExactly(교대역, 강남역, 양재역);
+        assertThat(response.jsonPath().getInt("distance")).isEqualTo(20);
     }
 
-    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId) {
+    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, int distance) {
         Map<String, String> params = new HashMap<>();
         params.put("upStationId", upStationId + "");
         params.put("downStationId", downStationId + "");
-        params.put("distance", 6 + "");
+        params.put("distance", distance + "");
         return params;
     }
 
-    private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
-        Map<String, String> lineCreateParams;
-        lineCreateParams = new HashMap<>();
-        lineCreateParams.put("name", "신분당선");
-        lineCreateParams.put("color", "bg-red-600");
-        lineCreateParams.put("upStationId", upStationId + "");
-        lineCreateParams.put("downStationId", downStationId + "");
-        lineCreateParams.put("distance", 10 + "");
-        return lineCreateParams;
-    }
 }
--- src/test/java/nextstep/subway/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -7,10 +7,10 @@ import org.springframework.http.MediaType;
 
 public class PathSteps {
 
-    public static ExtractableResponse<Response> 지하철역_경로_조회_요청() {
+    public static ExtractableResponse<Response> 지하철역_경로_조회_요청(Long source, Long target) {
         return RestAssured.given().log().all()
-                .queryParam("source", 16)
-                .queryParam("target", 3)
+                .queryParam("source", source)
+                .queryParam("target", target)
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().get("/paths")
