62c83c5d98ae8acd5aa83e059f71fc46fc214f84
dhlee3994 <dhlee3994@gmail.com>
2023-02-19 02:54:10 +0900

    Test: 최단 경로 조회 기능 인수 테스트 작성

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -1,14 +1,13 @@
 package nextstep.subway.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.applicaion.dto.LineRequest;
+import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,11 +16,13 @@ import java.util.stream.Collectors;
 
 import static nextstep.subway.acceptance.LineSteps.지하철_노선_생성_요청;
 import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철_구간_생성_요청;
+import static nextstep.subway.acceptance.PathSteps.지하철_최단경로_조회;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static nextstep.subway.utils.AssertionUtils.목록은_다음을_순서대로_포함한다;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-@DisplayName("경로 조회 관련 기능")
+@DisplayName("최단 경로 조회 관련 기능")
 public class PathAcceptanceTest extends AcceptanceTest {
 
     private Long 교대역;
@@ -33,11 +34,11 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private Long 삼호선;
 
     /**
-     * 교대역    --- *2호선* ---   강남역
+     * 교대역 --- *2호선(10)* --- 강남역
      * |                        |
-     * *3호선*                   *신분당선*
+     * *3호선(2)*             *신분당선(10)*
      * |                        |
-     * 남부터미널역  --- *3호선* ---   양재
+     * 남부터미널역 --- *3호선(3)* -양재
      */
     @BeforeEach
     public void setUp() {
@@ -56,39 +57,30 @@ public class PathAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * given: 출발역과 도착역으로
+     * given: 출발역과 도착역으로(교대역 -> 양재역)
      * when : 최단 경로 조회를 요청하면
      * then : 최단 경로의 역 목록과 해당 경로의 총 거리를 얻을 수 있다.
      */
     @DisplayName("최단 경로 조회")
     @Test
     void path() {
-        int sourceId = 1;
-        int targetId = 3;
+        final ExtractableResponse<Response> response = 지하철_최단경로_조회(교대역, 양재역);
 
-        final ExtractableResponse<Response> response =
-                RestAssured
-                        .given()
-                            .accept(MediaType.APPLICATION_JSON_VALUE)
-                            .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when()
-                            .get("/paths?source={sourceId}&target={targetId}", sourceId, targetId)
-                        .then()
-                        .extract();
+        final PathResponse path = response.as(PathResponse.class);
+        final List<Long> 지하철_경로의_역_아이디_목록 = 지하철_경로에서_역_아이디를_순서대로_뽑는다(path);
 
-        final int distance = response.jsonPath().getInt("distance");
-        assertThat(distance).isEqualTo(5);
-
-        final List<StationResponse> stations = response.jsonPath().getList("stations", StationResponse.class);
-        final List<String> stationNames = stations.stream()
-                .map(StationResponse::getName)
-                .collect(Collectors.toList());
-
-        목록은_다음을_순서대로_포함한다(stationNames, "교대역", "남부터미널역", "양재역");
+        assertAll(
+                () -> assertThat(path.getDistance()).isEqualTo(5),
+                () -> 목록은_다음을_순서대로_포함한다(지하철_경로의_역_아이디_목록, 교대역, 남부터미널역, 양재역)
+        );
     }
 
-
-
+    private static List<Long> 지하철_경로에서_역_아이디를_순서대로_뽑는다(final PathResponse path) {
+        return path.getStations()
+                .stream()
+                .map(StationResponse::getId)
+                .collect(Collectors.toList());
+    }
 
 
     private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, int distance) {
