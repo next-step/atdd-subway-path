d90e25967cd7438efc25b13e17c3fa20885ded81
JohnPrk <uo3641493@gmail.com>
2023-02-18 19:56:39 +0900

    refactor : Line 객체 단위 테스트 추가(메서드를 기준으로), 테스트의 편리성을 위해 LineRequest 생성자 생성

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -1,5 +1,11 @@
 package nextstep.subway.applicaion.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class LineRequest {
     private String name;
     private String color;
@@ -7,23 +13,12 @@ public class LineRequest {
     private Long downStationId;
     private int distance;
 
-    public String getName() {
-        return name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public Long getUpStationId() {
-        return upStationId;
-    }
-
-    public Long getDownStationId() {
-        return downStationId;
-    }
-
-    public int getDistance() {
-        return distance;
+    @Builder
+    public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
     }
 }
--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,77 +1,99 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.dto.LineRequest;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 class LineTest {
 
-    private Line 일호선;
-    private Station 서울역;
-    private Station 용산역;
+    private static final String LINE_ONE = "1호선";
+    private static final String LINE_TWO = "2호선";
+    private static final String BACKGROUND_COLOR_BLUE = "bg-color-blue";
+    private static final String BACKGROUND_COLOR_RED = "bg-color-red";
+    private static final String SEOUL_STATION = "서울역";
+    private static final String YONGSAN_STATION = "용산역";
+    private static final String HONGDAE_STATION = "홍대역";
+    private static final int DISTANCE_FIVE = 5;
+    private Line lineOne;
+    private Station seoulStation;
+    private Station yongSanStation;
 
     @BeforeEach
     void setUp() {
-        일호선 = new Line("1호선", "bg-color-blue");
-        서울역 = new Station("서울역");
-        용산역 = new Station("용산역");
+        lineOne = new Line(LINE_ONE, BACKGROUND_COLOR_BLUE);
+        seoulStation = new Station(SEOUL_STATION);
+        yongSanStation = new Station(YONGSAN_STATION);
     }
 
-    /**
-     * given 생성된 노선과 지하철 역을 구간을 추가하고
-     * when 생성된 노선에 구간을 추가하면
-     * then 구간에 노선의 정보가 포함된다.
-     */
+    @DisplayName("노선에 구간을 추가 하면 노선의 포함 여부를 알 수 있다")
     @Test
     void addSection() {
         //given
-        Section 일구간 = new Section(일호선, 서울역, 용산역, 5);
+        Station hongDaeStation = new Station(HONGDAE_STATION);
+        Section sectionOne = new Section(lineOne, seoulStation, yongSanStation, DISTANCE_FIVE);
+        Section sectionTwo = new Section(lineOne, yongSanStation, hongDaeStation, DISTANCE_FIVE);
 
         //when
-        일호선.addSection(서울역, 용산역, 5);
+        lineOne.addSection(sectionOne.getUpStation(), sectionOne.getDownStation(), sectionOne.getDistance());
+        lineOne.addSection(sectionTwo.getUpStation(), sectionTwo.getDownStation(), sectionTwo.getDistance());
 
         //then
-        Assertions.assertThat(일호선.getSections().getSections().contains(일구간)).isTrue();
+        Assertions.assertThat(lineOne.getSections().getSections().contains(sectionOne)).isTrue();
+        Assertions.assertThat(lineOne.getSections().getSections().contains(sectionTwo)).isTrue();
     }
 
-    /**
-     * given 생성된 노선에 구간을 추가하고
-     * when 노선을 조회하면
-     * then 구간에 포함된 지하철 역을 확인할 수 있다.
-     */
-    @Test
-    void getStations() {
-        //given
-        Section 일구간 = new Section(일호선, 서울역, 용산역, 5);
-        일호선.addSection(서울역, 용산역, 5);
-
-        //when
-        List<Station> stations = 일호선.getStations();
-
-        //then
-        Assertions.assertThat(stations).containsExactly(서울역, 용산역);
-    }
-
-    /**
-     * given 생성된 노선에 구간을 추가하고
-     * when 노선에 포함된 구간을 삭제하면
-     * then 노선에 포함된 구간은 존재하지 않는다.
-     */
+    @DisplayName("노선에 존재 하는 구간을 삭제 하면 노선의 포함 여부를 알 수 없다")
     @Test
     void removeSection() {
         //given
-        Section 일구간 = new Section(일호선, 서울역, 용산역, 5);
-        일호선.addSection(서울역, 용산역, 5);
+        Section sectionOne = new Section(lineOne, seoulStation, yongSanStation, DISTANCE_FIVE);
+        lineOne.addSection(sectionOne.getUpStation(), sectionOne.getDownStation(), sectionOne.getDistance());
 
         //when
-        일호선.removeSection(용산역);
+        lineOne.removeSection(yongSanStation);
 
         //then
-        Assertions.assertThat(일호선.getStations()).isEmpty();
+        Assertions.assertThat(lineOne.getSections().getSections()).doesNotContain(sectionOne);
+        Assertions.assertThat(lineOne.getStations()).isEmpty();
+    }
+
+    @DisplayName("노선의 이름이나 색깔을 변경할 수 있다")
+    @Test
+    void updateNameOrColor() {
+        //given
+        String lineTwo = LINE_TWO;
+        String red = BACKGROUND_COLOR_RED;
+        LineRequest lineRequest = LineRequest.builder()
+                .name(lineTwo)
+                .color(red)
+                .build();
+
+        //when
+        lineOne.updateNameOrColor(lineRequest);
+
+        //then
+        Assertions.assertThat(lineOne.getName()).isEqualTo(lineTwo);
+        Assertions.assertThat(lineOne.getColor()).isEqualTo(red);
+    }
+
+    @DisplayName("노선에 구간들을 추가하고 노선을 조회하면 구간에 포함된 지하철역을 알 수 있다")
+    @Test
+    void getStations() {
+        //given
+        Section sectionOne = new Section(lineOne, seoulStation, yongSanStation, DISTANCE_FIVE);
+        lineOne.addSection(sectionOne.getUpStation(), sectionOne.getDownStation(), sectionOne.getDistance());
+
+        //when
+        List<Station> stations = lineOne.getStations();
+
+        //then
+        Assertions.assertThat(stations).containsExactly(sectionOne.getUpStation(), sectionOne.getDownStation());
     }
 }
