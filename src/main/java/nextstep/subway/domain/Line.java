62301ee3d48f5df9ce1712e3a62b5f76b6c22f68
ji-one <rlawldnjs8@gmail.com>
2023-02-19 03:00:15 +0900

    feat: 구간 추가 기능 개선

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -1,27 +1,24 @@
 package nextstep.subway.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import javax.persistence.CascadeType;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import nextstep.subway.applicaion.dto.LineRequest;
 
 @Entity
 public class Line {
 
-    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST,
-        CascadeType.MERGE}, orphanRemoval = true)
-    private final List<Section> sections = new ArrayList<>();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Embedded
+    private final Sections sections = new Sections();
+
     private String name;
     private String color;
 
@@ -58,35 +55,19 @@ public class Line {
     }
 
     public List<Section> getSections() {
-        return sections;
+        return sections.getSections();
     }
 
     public void addSection(Section section) {
-        this.sections.add(section);
+        sections.addSection(section);
     }
 
     public void deleteSection(Station station) {
-        if (sections.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-
-        int lastSectionIndex = sections.size() - 1;
-        Station lastSectionDownStation = sections.get(lastSectionIndex).getDownStation();
-
-        if (!lastSectionDownStation.equals(station)) {
-            throw new IllegalArgumentException();
-        }
-
-        sections.remove(lastSectionIndex);
+        sections.deleteSection(station);
     }
 
     public List<Station> getStations() {
-        return sections.stream()
-            .flatMap(section ->
-                Stream.of(section.getUpStation(), section.getDownStation())
-            )
-            .distinct()
-            .collect(Collectors.toList());
+        return sections.getStations();
     }
 
     public void update(LineRequest lineRequest) {
