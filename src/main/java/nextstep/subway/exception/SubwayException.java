d83063b83af9d02c4a95deae27199e4ea77cb9b5
ji-one <rlawldnjs8@gmail.com>
2023-02-19 02:58:30 +0900

    feat: Exception 추가

---
+++ src/main/java/nextstep/subway/exception/SubwayException.java
@@ -0,0 +1,29 @@
+package nextstep.subway.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class SubwayException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public SubwayException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.httpStatus = errorCode.getHttpStatus();
+        this.message = errorCode.getMessage();
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
+++ src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package nextstep.subway.ui;
 
+import nextstep.subway.exception.ErrorResponse;
+import nextstep.subway.exception.SubwayException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,8 +9,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
+
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Void> handleIllegalArgsException(DataIntegrityViolationException e) {
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(SubwayException.class)
+    public ResponseEntity<ErrorResponse> handleSubwayException(SubwayException e){
+        return ResponseEntity.status(e.getHttpStatus())
+            .body(new ErrorResponse(e.getMessage()));
     }
 }
