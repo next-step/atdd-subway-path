79b67dfd1d90853044b1f95992a1f3bf5e9ec62c
heoseungyeon <dia0312@naver.com>
2023-02-18 16:11:01 +0900

    test: Line - addSection 테스트 작성

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,17 +1,36 @@
 package nextstep.subway.unit;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
+
 class LineTest {
-    @Test
-    void addSection() {
-    }
+	@DisplayName("구간 추가")
+	@Test
+	void addSection() {
+		// Given
+		Station 강남역 = new Station("강남역");
+		Station 선릉역 = new Station("선릉역");
+		Line 이호선 = new Line("2호선", "green");
 
-    @Test
-    void getStations() {
-    }
+		// When
+		이호선.addSection(강남역, 선릉역, 10);
 
-    @Test
-    void removeSection() {
-    }
+		// Then
+		assertThat(이호선.getSections()).isNotEmpty();
+	}
+
+	@DisplayName("구간 조회")
+	@Test
+	void getStations() {
+	}
+
+	@DisplayName("구간 삭제")
+	@Test
+	void removeSection() {
+	}
 }
