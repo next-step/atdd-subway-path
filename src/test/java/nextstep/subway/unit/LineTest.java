f70bd4d725e37f92091f259748c73ee3581c02bc
heoseungyeon <dia0312@naver.com>
2023-02-19 21:13:02 +0900

    test: 지하철 구간 추가 기능 개선 단위 테스트 작성

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -9,7 +9,58 @@ import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Station;
 
 class LineTest {
-	@DisplayName("구간 추가")
+	@DisplayName("구간 추가 - 새로운 역이 상행 종점역과 하행 종점역 사이로 하는 구간 생성")
+	@Test
+	void addSectionBetweenUpStationAndDwonStation() {
+		// Given
+		Station 강남역 = new Station("강남역");
+		Station 선릉역 = new Station("선릉역");
+		Station 역삼역 = new Station("역삼역");
+		Line 이호선 = new Line("2호선", "green");
+
+		// When
+		이호선.addSection(강남역, 선릉역, 10);
+		이호선.addSection(강남역, 역삼역, 9);
+
+		// Then
+		assertThat(이호선.getStations()).containsOnly(강남역, 선릉역, 역삼역);
+	}
+
+	@DisplayName("구간 추가 - 새로운 역이 상행 종점역으로 하는 구간 생성")
+	@Test
+	void addSectionToUpStation() {
+		// Given
+		Station 강남역 = new Station("강남역");
+		Station 선릉역 = new Station("선릉역");
+		Station 교대역 = new Station("교대역");
+		Line 이호선 = new Line("2호선", "green");
+
+		// When
+		이호선.addSection(강남역, 선릉역, 10);
+		이호선.addSection(교대역, 강남역, 10);
+
+		// Then
+		assertThat(이호선.getStations()).containsOnly(강남역, 선릉역, 교대역);
+	}
+
+	@DisplayName("구간 추가 - 새로운 역이 하행 종점역으로 하는 구간 생성")
+	@Test
+	void addSectionToDownStation() {
+		// Given
+		Station 강남역 = new Station("강남역");
+		Station 선릉역 = new Station("선릉역");
+		Station 삼성역 = new Station("삼성역");
+		Line 이호선 = new Line("2호선", "green");
+
+		// When
+		이호선.addSection(강남역, 선릉역, 10);
+		이호선.addSection(선릉역, 삼성역, 10);
+
+		// Then
+		assertThat(이호선.getStations()).containsOnly(강남역, 선릉역, 삼성역);
+	}
+
+	@DisplayName("구간 추가 - 노선이 생성 되는 경우")
 	@Test
 	void addSection() {
 		// Given
@@ -45,14 +96,14 @@ class LineTest {
 		// Given
 		Station 강남역 = new Station("강남역");
 		Station 선릉역 = new Station("선릉역");
-		Station 역삼역 = new Station("역삼역");
+		Station 삼성역 = new Station("삼성역");
 
 		Line 이호선 = new Line("2호선", "green");
 		이호선.addSection(강남역, 선릉역, 10);
-		이호선.addSection(선릉역, 역삼역, 20);
+		이호선.addSection(선릉역, 삼성역, 20);
 
 		// When
-		이호선.removeSection(역삼역);
+		이호선.removeSection(삼성역);
 
 		// Then
 		assertThat(이호선.getStations()).containsOnly(강남역, 선릉역);
