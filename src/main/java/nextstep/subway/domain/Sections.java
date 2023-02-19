56ac6f7910bd10fd49b9acc13b6cf048dee7bdf4
ji-one <rlawldnjs8@gmail.com>
2023-02-19 17:14:15 +0900

    refactor: 지하철 구간 관리 기능 리팩토링

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -28,86 +28,138 @@ public class Sections {
     }
 
     private List<Section> sortSectionsByOrder(List<Section> sections) {
-        Station upFirstStation = sections.stream()
-            .map(Section::getUpStation)
-            .filter(upStation -> sections.stream()
-                .noneMatch(section -> section.getDownStation() == upStation))
-            .findFirst()
-            .orElse(null);
-
-        Section firstSection = sections.stream()
-            .filter(section -> section.getUpStation() == upFirstStation)
-            .findFirst()
-            .orElse(null);
+        Station upFirstStation = getUpFirstStation(sections);
+        Section firstSection = getSectionByUpStation(sections, upFirstStation);
 
         List<Section> sortedSections = new ArrayList<>();
         Section currentSection = firstSection;
         while (currentSection != null) {
             sortedSections.add(currentSection);
             Station nextUpStation = currentSection.getDownStation();
-            currentSection = sections.stream()
-                .filter(section -> section.getUpStation() == nextUpStation)
-                .findFirst()
-                .orElse(null);
+            currentSection = getSectionByUpStation(sections, nextUpStation);
         }
-
         return sortedSections;
     }
 
-    public void addSection(Section section) {
+    private Station getUpFirstStation(List<Section> sections) {
+        return sections
+            .stream()
+            .map(Section::getUpStation)
+            .filter(upStation -> sections
+                .stream()
+                .noneMatch(section -> upStation.equals(section.getDownStation())))
+            .findFirst()
+            .orElse(null);
+    }
+
+    private Section getSectionByUpStation(List<Section> sections, Station upStation) {
+        return sections
+            .stream()
+            .filter(section -> upStation.equals(section.getUpStation()))
+            .findFirst()
+            .orElse(null);
+    }
+
+    public void addSection(Section newSection) {
+        validateSectionAddition(newSection);
+
         if (sections.isEmpty()) {
-            sections.add(section);
+            sections.add(newSection);
             return;
         }
 
-        // 역 사이에 새로운 역을 등록할 경우
-        Section targetSection = sections.stream()
-            .filter(s -> section.getUpStation().equals(s.getUpStation()))
-            .findFirst()
-            .orElse(null);
+        if (canAddSectionBetween(newSection)) {
+            addSectionBetween(newSection);
+            return;
+        }
 
-        if (targetSection != null) {
-            if (section.getDistance() >= targetSection.getDistance()) {
-                throw new SubwayException(INVALID_DISTANCE);
-            }
-
-            if (targetSection.getDownStation() == section.getDownStation()) {
-                throw new SubwayException(DUPLICATED_SECTION);
-            }
-            sections.add(section);
-            sections.remove(targetSection);
-            Section newSection = new Section(
-                targetSection.getLine(),
-                section.getDownStation(),
-                targetSection.getDownStation(),
-                targetSection.getDistance() - section.getDistance());
-
+        if (canAddSectionAsEndpoint(newSection)) {
             sections.add(newSection);
         }
+    }
 
-        // 새로운 역을 상행 종점으로 등록할 경우
-        boolean match = sections.stream()
-            .anyMatch(s -> section.getDownStation().equals(s.getUpStation()));
-
-        if (match) {
-            sections.add(section);
-        }
-
-        // 새로운 역을 하행 종점으로 등록할 경우
-        boolean isMatch = sections.stream()
-            .anyMatch(s -> section.getUpStation().equals(s.getDownStation()));
-
-        if (isMatch) {
-            sections.add(section);
-
-        }
-
-        boolean isExist = getStations().stream()
-            .anyMatch(s -> s.equals(section.getUpStation()) || s.equals(section.getDownStation()));
-        if (!isExist) {
+    private void validateSectionAddition(Section section) {
+        boolean upStationNotInLine = !isStationInLine(section.getUpStation());
+        boolean downStationNotInLine = !isStationInLine(section.getDownStation());
+        if (upStationNotInLine && downStationNotInLine) {
             throw new SubwayException(INVALID_SECTION);
         }
     }
+
+    private boolean isStationInLine(Station station) {
+        return getStations().contains(station);
+    }
+
+    public List<Station> getStations() {
+        return sections
+            .stream()
+            .flatMap(section ->
+                Stream.of(section.getUpStation(), section.getDownStation()))
+            .distinct()
+            .collect(Collectors.toList());
+    }
+
+    private boolean canAddSectionBetween(Section newSection) {
+        return sections.stream()
+            .anyMatch(section ->
+                section.getUpStation().equals(newSection.getUpStation()));
+    }
+
+    private void addSectionBetween(Section section) {
+        Section baseSection = getBaseSection(section);
+        validateDistance(baseSection, section);
+        validateDuplicateSection(baseSection, section);
+        addSectionToBaseSection(baseSection, section);
+    }
+
+    private boolean canAddSectionAsEndpoint(Section newSection) {
+        return canAddSectionAsUpwardEndpoint(newSection)
+            || canAddSectionAsDownwardEndpoint(newSection);
+    }
+
+    private boolean canAddSectionAsUpwardEndpoint(Section newSection) {
+        return sections.stream()
+            .anyMatch(section ->
+                newSection.getDownStation().equals(section.getUpStation()));
+    }
+
+    private boolean canAddSectionAsDownwardEndpoint(Section newSection) {
+        return sections.stream()
+            .anyMatch(section ->
+                newSection.getUpStation().equals(section.getDownStation()));
+    }
+
+    private Section getBaseSection(Section section) {
+        return sections.stream()
+            .filter(s -> section.getUpStation().equals(s.getUpStation()))
+            .findFirst()
+            .orElseThrow(() -> new SubwayException(INVALID_SECTION));
+    }
+
+    private void validateDistance(Section baseSection, Section section) {
+        if (baseSection.getDistance() <= section.getDistance()) {
+            throw new SubwayException(INVALID_DISTANCE);
+        }
+    }
+
+    private void validateDuplicateSection(Section baseSection, Section section) {
+        if (baseSection.getDownStation() == section.getDownStation()) {
+            throw new SubwayException(DUPLICATED_SECTION);
+        }
+    }
+
+    private void addSectionToBaseSection(Section baseSection, Section section) {
+        Section newSection = new Section(
+            baseSection.getLine(),
+            section.getDownStation(),
+            baseSection.getDownStation(),
+            baseSection.getDistance() - section.getDistance());
+
+        sections.remove(baseSection);
+        sections.add(section);
+        sections.add(newSection);
+    }
+
 
     public void deleteSection(Station station) {
         if (sections.isEmpty()) {
@@ -116,21 +168,10 @@ public class Sections {
 
         int lastSectionIndex = sections.size() - 1;
         Station lastSectionDownStation = sections.get(lastSectionIndex).getDownStation();
-
         if (!lastSectionDownStation.equals(station)) {
             throw new IllegalArgumentException();
         }
 
         sections.remove(lastSectionIndex);
-    }
-
-    public List<Station> getStations() {
-        return sections
-            .stream()
-            .flatMap(section ->
-                Stream.of(section.getUpStation(), section.getDownStation())
-            )
-            .distinct()
-            .collect(Collectors.toList());
     }
 }
