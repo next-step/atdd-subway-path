1421feaf8ebd4e60f420c46a2bb7407ac38b7cc9
cyPark95 <cas15987@gmail.com>
2023-02-19 22:57:38 +0900

    refactor: 지하철 경로 조회 응답 값 변경(PathResponse -> ExtractableResponse<Response>)

--- src/test/java/nextstep/subway/acceptance/PathFinderAssertUtil.java
+++ src/test/java/nextstep/subway/acceptance/PathFinderAssertUtil.java
@@ -1,16 +1,19 @@
 package nextstep.subway.acceptance;
 
-import nextstep.subway.domain.dto.PathResponse;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class PathFinderAssertUtil {
 
-    public static void 지하철_경로_조회_검증(PathResponse response, int expectedDistance, Long... expectedStationIds) {
+    public static void 지하철_경로_조회_검증(ExtractableResponse<Response> response, int expectedDistance, Long... expectedStationIds) {
         assertAll(
-                () -> assertThat(response.getDistance()).isEqualTo(expectedDistance),
-                () -> assertThat(response.getStations()).extracting("id").containsExactly(expectedStationIds)
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(response.jsonPath().getDouble("distance")).isEqualTo(expectedDistance),
+                () -> assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(expectedStationIds)
         );
     }
 }
