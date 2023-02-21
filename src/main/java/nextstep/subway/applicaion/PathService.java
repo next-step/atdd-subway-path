69c9e41852d4ce2803ffa049ec9eaeb028a26eeb
jeongyolee <jeongyolee@ebay.com>
2023-02-21 20:20:12 +0900

    refac : 존재하지 않은 station validation 추가 -> 예외 책임 변경

--- src/main/java/nextstep/subway/applicaion/PathService.java
+++ src/main/java/nextstep/subway/applicaion/PathService.java
@@ -4,11 +4,13 @@ import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.DijkstraPathFinder;
 import nextstep.subway.domain.Line;
+import nextstep.subway.domain.PathFinder;
 import nextstep.subway.exception.CustomException;
 import org.jgrapht.GraphPath;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,15 +25,13 @@ public class PathService {
     }
 
     public PathResponse getShortPath(Long source, Long target) {
-        if(source.equals(target)) {
-            throw new CustomException(CustomException.SAME_STATION_CAN_NOT_SEARCH_PATH);
-        }
+        validStations(source, target);
 
         List<Line> lines = lineService.getLines();
-        DijkstraPathFinder pathFinder = new DijkstraPathFinder(lines);
+        PathFinder pathFinder = new DijkstraPathFinder(lines);
         // <-> KshortestPathFinder.class
 
-        GraphPath<Long, String> shotestGraph = pathFinder.getShortestPath(source, target);
+        GraphPath<Long, String> shotestGraph = pathFinder.getPath(source, target);
 
         List<StationResponse> stations = shotestGraph.getVertexList().stream()
                 .map(s -> stationService.findById(s))
@@ -39,5 +39,18 @@ public class PathService {
                 .collect(Collectors.toList());
 
         return PathResponse.of(stations, (int)shotestGraph.getWeight());
+    }
+
+    private void validStations(Long source, Long target) {
+        if(source.equals(target)) {
+            throw new CustomException(CustomException.SAME_STATION_CAN_NOT_SEARCH_PATH);
+        }
+
+        try {
+            stationService.findById(source);
+            stationService.findById(target);
+        } catch (IllegalArgumentException e) {
+            throw new CustomException(CustomException.PATH_MUST_CONTAIN_STATION);
+        }
     }
 }
