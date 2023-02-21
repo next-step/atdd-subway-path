e4ee39c8e305efa7243b4127a6690356253532f3
hoo <tbnsok40@gmail.com>
2023-02-21 15:24:59 +0900

    feature: Test 사전 작업 - 쿼리 생성 및 DatabaseCleanUp

---
+++ src/main/resources/sql/insert-line-and-section.sql
@@ -0,0 +1,11 @@
+INSERT INTO line values (1, '이호선', 'bg-color-green');
+
+Insert into station values (1, '교대역');
+Insert into station values (2, '강남역');
+Insert into station values (3, '역삼역');
+Insert into station values (4, '선릉역');
+Insert into station values (5, '삼성역');
+
+Insert into section values (100, 1, 2, 4, 10);
+
+
