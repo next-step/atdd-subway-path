1449cc5e7c0e21ad9c05ff16f73dc77604dcb350
kimjio <jiokim222@gmail.com>
2023-02-22 01:36:49 +0900

    test(LineSectionAcceptanceTest): 구간 삭제 인수 테스트 작성

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -13,6 +13,7 @@ import java.util.Map;
 import static nextstep.subway.acceptance.LineSteps.*;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("지하철 구간 관리 기능")
 class LineSectionAcceptanceTest extends AcceptanceTest {
@@ -82,7 +83,16 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     @DisplayName("성공: 지하철 노선에 시작 구간을 제거")
     @Test
     void remove_from_first_LineSection() {
-
+        // given
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+        // when
+        지하철_노선에_지하철_구간_제거_요청(신분당선, 강남역);
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(양재역, 정자역)
+        );
     }
 
     /**
@@ -90,10 +100,19 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
      * When 구간 사이의 역을 삭제 요청을 하면
      * Then 해당 구간이 삭제된다.
      */
-    @DisplayName("성공: 중간역 제거 요청")
+    @DisplayName("성공: 지하철 노선에 중간역 제거 요청")
     @Test
     void remove_from_middle_LineSection() {
-
+        // given
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+        // when
+        지하철_노선에_지하철_구간_제거_요청(신분당선, 양재역);
+        // then
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 정자역)
+        );
     }
 
     /**
@@ -104,7 +123,12 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     @DisplayName("실패: 존재하지 않는 역 삭제 요청")
     @Test
     void fail_remove_LineSection2() {
-
+        // Given
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+        // when
+        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_제거_요청(신분당선, 잠실역);
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     /**
@@ -114,7 +138,10 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
     @DisplayName("실패: 구간이 하나인 노선의 구간 제거")
     @Test
     void fail_remove_LineSection() {
-
+        // when
+        ExtractableResponse<Response> response = 지하철_노선에_지하철_구간_제거_요청(신분당선, 강남역);
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
