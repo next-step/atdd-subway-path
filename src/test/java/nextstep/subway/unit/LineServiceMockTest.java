ade893444f48af6dfbb2d18614231cad30ad4043
kimjio <jiokim222@gmail.com>
2023-02-22 02:53:31 +0900

    test(LineServiceMockTest): 구간 삭제 mock 테스트

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -10,34 +10,34 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
+    Long 이호선;
+    Long 강남역;
+    Long 삼성역;
+    Long 잠실역;
     @Mock
     private LineRepository lineRepository;
     @Mock
     private StationService stationService;
-
+    @InjectMocks
     private LineService lineService;
 
     @BeforeEach
     void setUp() {
-        lineService = new LineService(lineRepository, stationService);
+        이호선 = 1L;
+        강남역 = 1L;
+        삼성역 = 2L;
+        잠실역 = 3L;
     }
 
     /**
@@ -48,25 +48,48 @@ public class LineServiceMockTest {
     @DisplayName("성공: 구간 추가")
     @Test
     void add_Section() {
-        Station upStation = new Station("삼성역");
-        Station downStation = new Station("강남역");
-        Line line = new Line("이호선", "green");
 
-        // given
-        given(stationService.findById(any())).willReturn(upStation, downStation);
-        given(lineRepository.findById(any())).willReturn(Optional.of(line));
+        given(stationService.findById(강남역)).willReturn(new Station("강남역"));
+        given(stationService.findById(삼성역)).willReturn(new Station("삼성역"));
+        given(lineRepository.findById(이호선)).willReturn(Optional.of(new Line("이호선", "green")));
 
-        SectionRequest sectionRequest = new SectionRequest(upStation.getId(), downStation.getId(), 3);
+        SectionRequest sectionRequest = new SectionRequest(강남역, 삼성역, 5);
 
         // when
-        lineService.addSection(1L, sectionRequest);
-
+        lineService.addSection(이호선, sectionRequest);
         // then
         assertSoftly(softly -> {
-            softly.assertThat(lineService.findLineById(line.getId()).getSections()).hasSize(1);
-            softly.assertThat(lineService.findLineById(line.getId()).getStations())
-                            .extracting(Station::getName)
-                            .containsExactlyInAnyOrder("삼성역", "강남역");
+            softly.assertThat(lineService.findLineById(이호선).getSections()).hasSize(1);
+            softly.assertThat(lineService.findLineById(이호선).getStations())
+                    .extracting(Station::getName)
+                    .containsExactlyInAnyOrder("강남역", "삼성역");
+        });
+    }
+
+    /**
+     * Given 지하철 노선에 새로운 구간 2개 추가를 요청한다.
+     * When 지하철 노선의 가운데 역 삭제 요청 하면
+     * Then 노선에 구간이 재배치 된다.
+     */
+    @DisplayName("성공: 구간사이 역 삭제")
+    @Test
+    void delete_Section() {
+        given(stationService.findById(강남역)).willReturn(new Station("강남역"));
+        given(stationService.findById(삼성역)).willReturn(new Station("삼성역"));
+        given(stationService.findById(잠실역)).willReturn(new Station("잠실역"));
+        given(lineRepository.findById(이호선)).willReturn(Optional.of(new Line("이호선", "green")));
+
+        // given
+        lineService.addSection(이호선, new SectionRequest(강남역, 삼성역, 5));
+        lineService.addSection(이호선, new SectionRequest(삼성역, 잠실역, 10));
+        // when
+        lineService.deleteSection(이호선, 삼성역);
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(lineService.findLineById(이호선).getSections()).hasSize(1);
+            softly.assertThat(lineService.findLineById(이호선).getStations())
+                    .extracting(Station::getName)
+                    .containsExactlyInAnyOrder("강남역", "잠실역");
         });
     }
 }
