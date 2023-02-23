b427e571e25058b78e2c4e6dfe3b272f11fff964
sangwon lee <231rollo@gmail.com>
2023-02-23 16:18:06 +0900

    2단계 - 지하철 구간 제거 기능 개선(#538)
    피드백 반영
    
    - Sections 실패 케이스 추가

--- src/test/java/nextstep/subway/unit/SectionsTest.java
+++ src/test/java/nextstep/subway/unit/SectionsTest.java
@@ -3,6 +3,8 @@ package nextstep.subway.unit;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Sections;
+import nextstep.subway.exception.AddSectionException;
+import nextstep.subway.exception.DeleteSectionException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,12 +12,13 @@ import java.util.List;
 
 import static nextstep.subway.unit.fixture.StationFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class SectionsTest {
 
     @Test
-    @DisplayName("지하철 구간 추가")
-    void addSection() {
+    @DisplayName("지하철 구간 추가 : 성공")
+    void addSection_success() {
         // given
         Line 신분당선 = new Line(1L, "신분당", "RED");
         Section 광교_상현 = new Section(신분당선, 광교, 상현, 10);
@@ -23,7 +26,6 @@ class SectionsTest {
         Sections sections = new Sections(List.of(광교_상현));
 
         // when
-        System.out.println(sections.getSections().get(0).getUpStation().getName());
         sections.addSection(광교_광교중앙);
 
         // then
@@ -31,8 +33,21 @@ class SectionsTest {
     }
 
     @Test
-    @DisplayName("지하철 목록 삭제")
-    void delete() {
+    @DisplayName("지하철 구간 추가 : 실패 - 구간 거리 초과")
+    void addSection_fail() {
+        // given
+        Line 신분당선 = new Line(1L, "신분당", "RED");
+        Section 광교_상현 = new Section(신분당선, 광교, 상현, 10);
+        Section 광교_광교중앙 = new Section(신분당선, 광교, 광교중앙, 15);
+        Sections sections = new Sections(List.of(광교_상현));
+
+        // when & then
+        assertThatThrownBy(() -> sections.addSection(광교_광교중앙)).isInstanceOf(AddSectionException.class);
+    }
+
+    @Test
+    @DisplayName("지하철 목록 삭제 : 성공")
+    void delete_success() {
         // given
         Line 신분당선 = new Line(1L, "신분당", "RED");
         Section 광교_광교중앙 = new Section(신분당선, 광교, 광교중앙, 10);
@@ -44,5 +59,18 @@ class SectionsTest {
 
         // then
         assertThat(sections.getStations()).containsExactly(광교, 상현);
+    }
+
+    @Test
+    @DisplayName("지하철 목록 삭제 : 실패 - 노선에 등록되지 않은 역")
+    void delete_fail() {
+        // given
+        Line 신분당선 = new Line(1L, "신분당", "RED");
+        Section 광교_광교중앙 = new Section(신분당선, 광교, 광교중앙, 10);
+        Section 광교중앙_상현 = new Section(신분당선, 광교, 상현, 5);
+        Sections sections = new Sections(List.of(광교_광교중앙, 광교중앙_상현));
+
+        // when & then
+        assertThatThrownBy(() -> sections.delete(성복)).isInstanceOf(DeleteSectionException.class);
     }
 }
