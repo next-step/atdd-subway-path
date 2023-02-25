396d9472a1981e457dd100aabd0195cfc32a9173
boradol2 <boradol0902@gmail.com>
2023-02-26 02:25:34 +0900

    test: 지하철 노선에서 역 삭제 테스트

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -45,6 +45,20 @@ class LineTest {
     }
 
     @Test
+    @DisplayName("지하철 노선에서 지하철 역을 삭제하면 구간 삭제된다.")
     void removeSection() {
+        //given
+        Station 강남역 = new Station("강남역");
+        Station 역삼역 = new Station("역삼역");
+        Station 삼성역 = new Station("삼성역");
+        Line line = new Line("2호선", "green");
+        line.addSection(강남역, 역삼역, 10);
+        line.addSection(역삼역, 삼성역, 5);
+
+        //when
+        line.removeSection(삼성역);
+
+        //then
+        assertThat(line.getAllStations()).containsExactly(강남역, 역삼역);
     }
 }
