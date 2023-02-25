417467381c213b734ecdd3dc6975746a667562fa
bactoria <bactoria@gmail.com>
2023-02-25 10:11:29 +0900

    test:: 다익스트라 최소구간 찾기 단위테스트 추가

---
+++ src/main/java/nextstep/subway/domain/PathFinder.java
@@ -0,0 +1,12 @@
+package nextstep.subway.domain;
+
+import nextstep.subway.ui.PathResponse;
+
+import java.util.List;
+
+public class PathFinder {
+    
+    public PathResponse findShortestPath(Station source, Station target) {
+        return null;
+    }
+}
