95b867b0c56e23590f8f516f30c74c0af28c0d27
bactoria <bactoria@gmail.com>
2023-02-26 11:27:43 +0900

    feat:: Path 구현

--- src/main/java/nextstep/subway/domain/Path.java
+++ src/main/java/nextstep/subway/domain/Path.java
@@ -4,15 +4,26 @@ import java.util.List;
 
 public class Path {
 
+    private List<Station> stations;
+    private Distance distance;
+
     public static Path create(List<Station> stations, int distance) {
-        return null;
+        if (stations.size() < 2) {
+            throw new IllegalArgumentException();
+        }
+        return new Path(stations, Distance.of(distance));
+    }
+
+    private Path(List<Station> stations, Distance distance) {
+        this.stations = stations;
+        this.distance = distance;
     }
 
     public List<Station> getStations() {
-        return null;
+        return stations;
     }
 
     public Distance getDistance() {
-        return null;
+        return distance;
     }
 }
