b56507b81f5f05f7b82853bb10367ea96f652614
bactoria <bactoria@gmail.com>
2023-02-26 11:15:19 +0900

    test:: Path 단위테스트 추가

---
+++ src/main/java/nextstep/subway/domain/Path.java
@@ -0,0 +1,18 @@
+package nextstep.subway.domain;
+
+import java.util.List;
+
+public class Path {
+
+    public static Path create(List<Station> stations, int distance) {
+        return null;
+    }
+
+    public List<Station> getStations() {
+        return null;
+    }
+
+    public Distance getDistance() {
+        return null;
+    }
+}
