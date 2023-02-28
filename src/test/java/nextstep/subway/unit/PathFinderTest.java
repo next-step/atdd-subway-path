bcae8bd7336603f0791f9a7460d0e28f1426c3e4
TSTP <86605720+seokjin0404@users.noreply.github.com>
2023-03-01 00:54:59 +0900

    test: PathFinder 단위테스트 작성

--- src/test/java/nextstep/subway/unit/PathFinderTest.java
+++ src/test/java/nextstep/subway/unit/PathFinderTest.java
@@ -1,5 +1,56 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Path;
+import nextstep.subway.domain.PathFinder;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PathFinderTest {
+
+    private Station 교대역;
+    private Station 강남역;
+    private Station 양재역;
+    private Station 남부터미널역;
+    private Line 이호선;
+    private Line 신분당선;
+    private Line 삼호선;
+
+    /**
+     * 교대역    --- *2호선* ---   강남역
+     * |                        |
+     * *3호선*                   *신분당선*
+     * |                        |
+     * 남부터미널역  --- *3호선* ---   양재
+     */
+    @BeforeEach
+    public void setUp() {
+
+        교대역 = new Station("교대역");
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        남부터미널역 = new Station("남부터미널역");
+
+        이호선 = new Line("2호선", "green", 교대역, 강남역, 10);
+        신분당선 = new Line("신분당선", "red", 강남역, 양재역, 10);
+        삼호선 = new Line("3호선", "orange", 교대역, 남부터미널역, 2);
+        삼호선.addSection(남부터미널역, 양재역, 3);
+    }
+
+    @DisplayName("")
+    @Test
+    void findPath() {
+        List<Line> lines = List.of(이호선, 신분당선, 삼호선);
+        PathFinder pathFinder = new PathFinder(lines);
+        Path path = pathFinder.findPath(교대역.getId(), 양재역.getId());
+        assertThat(path.getStations()).containsExactly(교대역, 남부터미널역, 양재역);
+        assertThat(path.getDistance()).isEqualTo(5);
+    }
 
 }
