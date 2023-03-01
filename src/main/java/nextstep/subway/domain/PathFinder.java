9c3495c04cb8551bcb106a6156e7148933b6f344
cafitac <cafitac99@gmail.com>
2023-03-01 21:57:11 +0900

    fix: 작동하지 않는 테스트 코드를 수정합니다

--- src/main/java/nextstep/subway/domain/PathFinder.java
+++ src/main/java/nextstep/subway/domain/PathFinder.java
@@ -19,39 +19,43 @@ public class PathFinder {
     private final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath;
 
     public PathFinder(final List<Line> lines) {
-        init(lines);
         graph = new WeightedMultigraph<Station, DefaultWeightedEdge>(DefaultWeightedEdge.class);
-        dijkstraShortestPath = new DijkstraShortestPath<Station, DefaultWeightedEdge>(graph);
+        dijkstraShortestPath = init(lines);
     }
 
     public List<Station> findShortestPath(final Station sourceStation, final Station targetStation) {
-        final GraphPath<Station, DefaultWeightedEdge> path = dijkstraShortestPath.getPath(sourceStation, targetStation);
-        validateBeforeFindPath(sourceStation, targetStation, path);
+        final GraphPath<Station, DefaultWeightedEdge> path = getPath(sourceStation, targetStation);
+        validatePathIsValid(path);
         return path.getVertexList();
     }
 
     public int findShortestPathDistance(final Station sourceStation, final Station targetStation) {
-        final GraphPath<Station, DefaultWeightedEdge> path = dijkstraShortestPath.getPath(sourceStation, targetStation);
-        validateBeforeFindPath(sourceStation, targetStation, path);
+        final GraphPath<Station, DefaultWeightedEdge> path = getPath(sourceStation, targetStation);
+        validatePathIsValid(path);
         return (int) path.getWeight();
     }
 
-    private void init(final List<Line> lines) {
+    private DijkstraShortestPath<Station, DefaultWeightedEdge> init(final List<Line> lines) {
         for (final Line line : lines) {
             final List<Station> stations = line.getStations();
             stations.forEach(graph::addVertex);
             final List<Section> sections = line.getSections();
             sections.forEach(section -> graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance()));
         }
+        return new DijkstraShortestPath<Station, DefaultWeightedEdge>(graph);
     }
 
-    private void validateBeforeFindPath(final Station source, final Station target, final GraphPath<Station, DefaultWeightedEdge> path) {
+    private GraphPath<Station, DefaultWeightedEdge> getPath(final Station source, final Station target) {
         if (source.equals(target)) {
             throw new BusinessException(ErrorCode.SOURCE_AND_TARGET_STATION_IS_SAME);
         }
         if (!graph.vertexSet().contains(source) | !graph.vertexSet().contains(target)) {
             throw new BusinessException(ErrorCode.STATION_NOT_FOUND);
         }
+        return dijkstraShortestPath.getPath(source, target);
+    }
+
+    private void validatePathIsValid(GraphPath<Station, DefaultWeightedEdge> path) {
         if (Objects.isNull(path)) {
             throw new BusinessException(ErrorCode.SOURCE_AND_TARGET_STATION_IS_NOT_CONNECTED);
         }
