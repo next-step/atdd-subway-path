81034e217af43b306098ace27a3427567fdcad9c
sjhello <sjhello235@gmail.com>
2023-03-01 21:18:56 +0900

    test: 구간에 포함되어있는 역 목록 기능 테스트

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -3,17 +3,30 @@ package nextstep.subway.unit;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
+    private Line 강남_2호선;
+    private Station 강남역;
+    private Station 역삼역;
+    private Station 삼성역;
+
+    @BeforeEach
+    void setUp() {
+        강남_2호선 = new Line("2호선", "green");
+        강남역 = new Station();
+        역삼역 = new Station();
+        삼성역 = new Station();
+    }
+
     @Test
     void addSection() {
         // given
-        Line 강남_2호선 = new Line("2호선", "green");
-        Station 강남역 = new Station();
-        Station 역삼역 = new Station();
         Section 강남_역삼_구간 = new Section(강남_2호선, 강남역, 역삼역, 10);
 
         // when
@@ -25,6 +38,17 @@ class LineTest {
 
     @Test
     void getStations() {
+        // given
+        Section 강남_역삼_구간 = new Section(강남_2호선, 강남역, 역삼역, 10);
+        Section 역삼_삼성_구간 = new Section(강남_2호선, 역삼역, 삼성역, 12);
+        강남_2호선.addSection(강남_역삼_구간);
+        강남_2호선.addSection(역삼_삼성_구간);
+
+        // when
+        List<Station> stations = 강남_2호선.getStations();
+
+        // then
+        assertThat(stations).containsExactly(강남역, 역삼역, 삼성역);
     }
 
     @Test
