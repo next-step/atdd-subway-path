46590c40e8b9cde1e5a913e0dc77b671755f2362
sjhello <sjhello235@gmail.com>
2023-03-01 21:00:26 +0900

    test: 구간 추가 기능 테스트

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,10 +1,26 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
     @Test
     void addSection() {
+        // given
+        Line 강남_2호선 = new Line("2호선", "green");
+        Station 강남역 = new Station();
+        Station 역삼역 = new Station();
+        Section 강남_역삼_구간 = new Section(강남_2호선, 강남역, 역삼역, 10);
+
+        // when
+        강남_2호선.addSection(강남_역삼_구간);
+
+        // then
+        assertThat(강남_2호선.getSections()).containsExactly(강남_역삼_구간);
     }
 
     @Test
