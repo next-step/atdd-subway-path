414ddfe7c8ca13dd4f964510b080b338324e791c
sjhello <sjhello235@gmail.com>
2023-03-01 22:19:06 +0900

    test: 노선 삭제 테스트

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -3,12 +3,15 @@ package nextstep.subway.unit;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
+import nextstep.subway.exception.SectionBadRequestException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LineTest {
     private Line 강남_2호선;
@@ -25,6 +28,7 @@ class LineTest {
     }
 
     @Test
+    @DisplayName("구간을 추가 테스트")
     void addSection() {
         // given
         Section 강남_역삼_구간 = new Section(강남_2호선, 강남역, 역삼역, 10);
@@ -37,6 +41,7 @@ class LineTest {
     }
 
     @Test
+    @DisplayName("노선의 역 목록 테스트")
     void getStations() {
         // given
         Section 강남_역삼_구간 = new Section(강남_2호선, 강남역, 역삼역, 10);
@@ -52,6 +57,33 @@ class LineTest {
     }
 
     @Test
-    void removeSection() {
+    @DisplayName("노선에 구간이 비어 있는 경우 구간을 삭제 할 수 없다")
+    void removeSectionFailBySectionsIsEmpty() {
+        assertThatThrownBy(() -> 강남_2호선.removeSection())
+                .isInstanceOf(SectionBadRequestException.class)
+                .hasMessage("구간이 존재하지 않습니다");
+    }
+
+    @Test
+    void 노선의_구간이_1개일_경우_구간을_삭제_할_수_없다() {
+        // given
+        강남_2호선.addSection(new Section(강남_2호선, 강남역, 역삼역, 10));
+
+        // then
+        assertThatThrownBy(() -> 강남_2호선.removeSection())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 노선의_마지막_구간을_삭제한다() {
+        // given
+        강남_2호선.addSection(new Section(강남_2호선, 강남역, 역삼역, 10));
+        강남_2호선.addSection(new Section(강남_2호선, 역삼역, 삼성역, 12));
+
+        // when
+        강남_2호선.removeSection();
+
+        // then
+        assertThat(강남_2호선.getSections().size()).isEqualTo(1);
     }
 }
