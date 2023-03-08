e2c9500fc9b3dd939a5e2179da71559bb4829a1c
wkwon <rnjsdntjr26@daum.net>
2023-03-09 00:21:45 +0900

    2주차 3단계: subwayMap 도메인 생성 (피드백 반영)

--- src/main/java/nextstep/subway/applicaion/PathService.java
+++ src/main/java/nextstep/subway/applicaion/PathService.java
@@ -2,10 +2,7 @@ package nextstep.subway.applicaion;
 
 import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
-import nextstep.subway.domain.Line;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.Section;
-import nextstep.subway.domain.Station;
+import nextstep.subway.domain.*;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
@@ -13,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PathService {
@@ -30,70 +28,22 @@ public class PathService {
             throw new IllegalArgumentException("출발역과 도착역이 같습니다.");
         }
 
+        List<Line> lines = lineRepository.findAll();
+        SubwayMap subwayMap = new SubwayMap(lines);
+
         Station sourceStation = stationService.findById(source);
         Station targetStation = stationService.findById(target);
 
-        DijkstraShortestPath<Long, DefaultWeightedEdge> map = getSubwayMap();
+        int distance = subwayMap.calculateDistance(sourceStation, targetStation);
+        List<Long> path = subwayMap.findPath(sourceStation, targetStation);
 
+        List<StationResponse> pathResult = path.stream()
+                .map(it -> StationResponse.from(stationService.findById(it)))
+                .collect(Collectors.toList());
 
-            int distance = calculateDistance(map, sourceStation.getId(), targetStation.getId());
-            List<StationResponse> path = findPath(map, sourceStation.getId(), targetStation.getId());
-
-
-
-        return new PathResponse(distance, path);
+        return new PathResponse(distance, pathResult);
     }
 
-    private DijkstraShortestPath getSubwayMap(){
-        List<Line> lines = lineRepository.findAll();
-        WeightedMultigraph<Long, DefaultWeightedEdge> graph =
-                new WeightedMultigraph(DefaultWeightedEdge.class);
 
 
-        addStationToMap(graph, lines);
-        addSectionToMap(graph, lines);
-
-        DijkstraShortestPath<Long, DefaultWeightedEdge> map = new DijkstraShortestPath(graph);
-
-        return map;
-    }
-
-    private WeightedMultigraph<Long, DefaultWeightedEdge> addStationToMap(WeightedMultigraph<Long, DefaultWeightedEdge> graph, List<Line> lines){
-        for(Line line : lines){
-            for(Station station : line.getSortedStations()){
-                graph.addVertex(station.getId());
-            }
-        }
-
-        return graph;
-    }
-
-    private WeightedMultigraph<Long, DefaultWeightedEdge> addSectionToMap(WeightedMultigraph<Long, DefaultWeightedEdge> graph, List<Line> lines){
-        for(Line line : lines){
-            for(Section section : line.getSections()){
-                graph.setEdgeWeight(graph.addEdge(section.getUpStation().getId(),section.getDownStation().getId()), section.getDistance());
-            }
-        }
-
-        return graph;
-    }
-
-    private List<StationResponse> findPath(DijkstraShortestPath map, Long source, Long target){
-        List<StationResponse> pathWithStationResponse = new ArrayList<>();
-        List<Long> pathWithStationId = map.getPath(source, target).getVertexList();
-
-        for(Long id : pathWithStationId){
-            pathWithStationResponse.add(stationService.findStation(id));
-        }
-
-        return pathWithStationResponse;
-    }
-
-    private int calculateDistance(DijkstraShortestPath map, Long source, Long target){
-        try {
-            return (int) map.getPathWeight(source, target);
-        } catch(Exception e) {
-            throw new IllegalArgumentException("연결되지 않은 역입니다.");
-        }
-    }
 }
