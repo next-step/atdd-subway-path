058a3d958c85d4d8178b29977021b79578073fcc
jisu <jisuuoo96@gmail.com>
2023-03-15 19:37:04 +0900

    test: mock 을 사용하지 않고 구간 서비스 단위 테스트 작성

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,16 +1,23 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Transactional
-public class LineServiceTest {
+class LineServiceTest {
+
     @Autowired
     private StationRepository stationRepository;
     @Autowired
@@ -23,11 +30,19 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Long 여주역_Id = stationRepository.save(new Station("여주역")).getId();
+        Long 부발역_Id = stationRepository.save(new Station("부발역")).getId();
+        Line 경강선 = lineRepository.save(new Line("경강선", "blue"));
 
         // when
         // lineService.addSection 호출
+        SectionRequest sectionRequest = new SectionRequest(여주역_Id, 부발역_Id, 10);
+        lineService.addSection(경강선.getId(), sectionRequest);
 
         // then
         // line.getSections 메서드를 통해 검증
+        assertThat(경강선.getSections()).hasSize(1);
+        assertThat(경강선.getSections().get(0).getUpStation().getName()).isEqualTo("여주역");
+        assertThat(경강선.getSections().get(0).getDownStation().getName()).isEqualTo("부발역");
     }
 }
