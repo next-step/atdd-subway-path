d1dc708c132754acf547c39b42dc37b8375f59c6
jisu <jisuuoo96@gmail.com>
2023-03-18 02:43:42 +0900

    feat: sectionResponse 클래스 추가

---
+++ src/main/java/nextstep/subway/applicaion/dto/SectionResponse.java
@@ -0,0 +1,11 @@
+package nextstep.subway.applicaion.dto;
+
+import java.util.List;
+
+public class SectionResponse {
+    private List<StationResponse> stations;
+    
+    public List<StationResponse> getStations() {
+        return stations;
+    }
+}
