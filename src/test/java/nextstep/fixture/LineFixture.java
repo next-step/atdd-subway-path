33410295d991741c71fa46a8be62c67f526dd293
sjhello <sjhello235@gmail.com>
2023-03-19 22:09:15 +0900

    test: Test Fixture 생성

---
+++ src/test/java/nextstep/fixture/LineFixture.java
@@ -0,0 +1,18 @@
+package nextstep.fixture;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LineFixture {
+
+    public static Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
+        Map<String, String> lineCreateParams;
+        lineCreateParams = new HashMap<>();
+        lineCreateParams.put("name", "신분당선");
+        lineCreateParams.put("color", "bg-red-600");
+        lineCreateParams.put("upStationId", upStationId + "");
+        lineCreateParams.put("downStationId", downStationId + "");
+        lineCreateParams.put("distance", 10 + "");
+        return lineCreateParams;
+    }
+}
---
+++ src/test/java/nextstep/fixture/SectionFixture.java
@@ -0,0 +1,15 @@
+package nextstep.fixture;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SectionFixture {
+
+    public static Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, int distance) {
+        Map<String, String> params = new HashMap<>();
+        params.put("upStationId", upStationId + "");
+        params.put("downStationId", downStationId + "");
+        params.put("distance", distance + "");
+        return params;
+    }
+}
