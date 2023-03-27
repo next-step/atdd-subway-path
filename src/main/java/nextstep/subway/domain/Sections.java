e1642347c00559a29b4bc49cef6683638bb74bb5
sjhello <sjhello235@gmail.com>
2023-03-28 01:02:52 +0900

    feat: 구간 제거 기능 개선
     - 노선 테스트 픽처 추가
     - 구간 제거 기능 개선
       - 상행 종점
       - 하행 종점
       - 중간역

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
+import javax.persistence.EntityNotFoundException;
 import javax.persistence.OneToMany;
 
 @Embeddable
@@ -127,23 +128,72 @@ public class Sections {
 	}
 
 	public void remove(Station station) {
+		validateRemove();
+
+		if (isLastStation(station)) {
+			sections.removeIf(section -> section.getDownStation().equals(station));
+			return;
+		}
+
+		if (isUpStation(station)) {
+			sections.removeIf(section -> section.getUpStation().equals(station));
+			return;
+		}
+		removeMiddleSection(station);
+	}
+
+	private void validateRemove() {
 		if (isEmpty()) {
 			throw new SectionBadRequestException("구간이 존재하지 않습니다.");
 		}
 		if (hasEnoughSize()) {
 			throw new SectionBadRequestException("현재 노선은 구간이 1개 입니다.");
 		}
-		if (isNotEndStation(station)) {
-			throw new SectionBadRequestException("노선에 등록된 하행 종점역만 제거 할 수 있습니다.");
-		}
-
-		int end = sections.size() - 1;
-		sections.remove(end);
 	}
 
-	public boolean isNotEndStation(Station station) {
-		Station endStation = getLastSection().getDownStation();
-		return !station.equals(endStation);
+	private void removeMiddleSection(Station station) {
+		Section section = findSectionByDownStation(station);
+		Section nextSection = findSectionByUpStation(station);
+		nextSection.modifyUpstation(section);
+
+		sections.remove(section);
+	}
+
+	private boolean isLastStation(Station station) {
+		List<Station> upStations = sections.stream()
+				.map(Section::getUpStation)
+				.collect(Collectors.toList());
+
+		Station downStation = sections.stream()
+				.map(Section::getDownStation)
+				.filter(s -> !upStations.contains(s))
+				.findFirst()
+				.orElseThrow(() -> new EntityNotFoundException("역이 존재 하지 않습니다."));
+
+		return downStation.equals(station);
+	}
+
+	private boolean isUpStation(Station station) {
+		Station upStation = sections.stream()
+				.map(Section::getUpStation)
+				.findFirst()
+				.orElseThrow(() -> new EntityNotFoundException("역이 존재 하지 않습니다."));
+
+		return upStation.equals(station);
+	}
+
+	private Section findSectionByUpStation(Station upStation) {
+		return sections.stream()
+				.filter(section -> section.getUpStation().equals(upStation))
+				.findFirst()
+				.orElseThrow(() -> new EntityNotFoundException("역이 존재 하지 않습니다."));
+	}
+
+	private Section findSectionByDownStation(Station downStation) {
+		return sections.stream()
+				.filter(section -> section.getDownStation().equals(downStation))
+				.findFirst()
+				.orElseThrow(() -> new EntityNotFoundException("역이 존재 하지 않습니다."));
 	}
 
 	public int getDistance() {
