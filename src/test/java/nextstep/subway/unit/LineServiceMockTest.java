a22410287f33331ead5655049911452ac3224495
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-07-08 10:40:04 +0900

    test: 구간 서비스 단위 테스트 with Mock 구현

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,26 +1,45 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.StationService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
-//    @Mock
-//    private LineRepository lineRepository;
-//    @Mock
-//    private StationService stationService;
+    @Mock
+    private LineRepository lineRepository;
+    @Mock
+    private StationService stationService;
 
     @Test
     void addSection() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        Long lineId = 1L;
+        when(stationService.findById(any())).thenReturn(new Station());
+        when(lineRepository.findById(lineId)).thenReturn(Optional.of(new Line()));
+        LineService lineService = new LineService(lineRepository, stationService);
 
         // when
         // lineService.addSection 호출
+        SectionRequest sectionRequest = new SectionRequest(1L, 2L, 10);
+        lineService.addSection(lineId, sectionRequest);
 
         // then
         // lineService.findLineById 메서드를 통해 검증
+        Assertions.assertThat(lineService.findById(lineId).getStations().size()).isEqualTo(2);
     }
 }
