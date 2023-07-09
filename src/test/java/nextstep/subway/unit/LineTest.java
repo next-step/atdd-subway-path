4d1d93e7e7dba5b4e77454085acc2624f2218368
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-07-09 11:23:59 +0900

    refactor: Line unit test 중복 코드 분리

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -3,63 +3,63 @@ package nextstep.subway.unit;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
+
+    private Station 마들역;
+    private Station 노원역;
+    private Station 중계역;
+
+    private Section 노원_마들;
+    private Section 중계_노원;
+
+    private Line 칠호선;
+
+    @BeforeEach
+    void setUp() {
+        칠호선 = new Line();
+
+        마들역 = new Station("마들역");
+        노원역 = new Station("노원역");
+        중계역 = new Station("중계역");
+
+        노원_마들 = new Section(칠호선, 노원역, 마들역, 10);
+        중계_노원 = new Section(칠호선, 중계역, 노원역, 10);
+    }
+
     @Test
     void addSection() {
-        // given
-        Station 마들역 = new Station("마들역");
-        Station 노원역 = new Station("노원역");
-        Line line = new Line();
-
-        Section 노원_마들 = new Section(line, 노원역, 마들역, 10);
-
         // when
-        line.addSection(노원_마들);
+        칠호선.addSection(노원_마들);
 
         // then
-        assertThat(line.getSections().contains(노원_마들)).isTrue();
+        assertThat(칠호선.getSections().contains(노원_마들)).isTrue();
     }
 
     @Test
     void getStations() {
-        // given
-        Station 마들역 = new Station("마들역");
-        Station 노원역 = new Station("노원역");
-        Station 중계역 = new Station("중계역");
-        Line line = new Line();
-
-        Section 노원_마들 = new Section(line, 노원역, 마들역, 10);
-        Section 중계_노원 = new Section(line, 중계역, 노원역, 10);
-
         // when
-        line.addSection(중계_노원);
-        line.addSection(노원_마들);
+        칠호선.addSection(중계_노원);
+        칠호선.addSection(노원_마들);
 
         // then
-        assertThat(line.getStations()).containsExactlyInAnyOrder(노원역, 마들역, 중계역);
+        assertThat(칠호선.getStations()).containsExactlyInAnyOrder(노원역, 마들역, 중계역);
     }
 
     @Test
     void removeSection() {
         // give
-        Station 마들역 = new Station("마들역");
-        Station 노원역 = new Station("노원역");
-        Station 중계역 = new Station("중계역");
-        Line line = new Line();
-
-        Section 노원_마들 = new Section(line, 노원역, 마들역, 10);
-        Section 중계_노원 = new Section(line, 중계역, 노원역, 10);
-        line.addSection(중계_노원);
-        line.addSection(노원_마들);
+        칠호선.addSection(중계_노원);
+        칠호선.addSection(노원_마들);
 
         // when
-        line.removeSection(노원_마들);
+        칠호선.removeSection(노원_마들);
 
         // then
-        assertThat(line.getSections().contains(노원_마들)).isFalse();
+        assertThat(칠호선.getSections().contains(노원_마들)).isFalse();
     }
 }
