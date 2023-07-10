d01f61b613eb49323cf3cc3759bcfadac814cdeb
Seungyong Park <42832854+psy3720@users.noreply.github.com>
2023-07-10 13:03:48 +0900

    단위 테스트 작성 (#574)
    * test: 뼈대코드 추가
    
    * test: 구간단위 테스트, 구간서비스 단위테스트 추가
    
    * test: 지하철역 생성자 수정

---
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -0,0 +1,69 @@
+package nextstep.subway.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Entity
+public class Line {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String color;
+
+    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    private List<Section> sections = new ArrayList<>();
+
+    public Line() {
+    }
+
+    public Line(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public List<Section> getSections() {
+        return sections;
+    }
+
+    public List<Station> getStations() {
+        return sections.stream()
+                .flatMap(section -> Stream.of(section.getUpStation(), section.getDownStation()))
+                .collect(Collectors.toList());
+    }
+
+    public void addSection(Section section) {
+        sections.add(section);
+    }
+
+    public void removeSection() {
+        sections.remove(sections.size() - 1);
+    }
+}
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,9 +1,15 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.applicaion.LineService;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -19,12 +25,29 @@ public class LineServiceTest {
     @Test
     void addSection() {
         // given
-        // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 지하철역 = stationRepository.save(new Station("지하철역"));
+        Station 새로운_지하철역 = stationRepository.save(new Station("새로운_지하철역"));
+        Line 신분당선 = lineRepository.save(new Line("신분당선", "bg-red-600"));
 
         // when
-        // lineService.addSection 호출
+        lineService.addSection(신분당선.getId(), new SectionRequest(지하철역.getId(), 새로운_지하철역.getId(), 10));
 
         // then
-        // line.getSections 메서드를 통해 검증
+        assertThat(신분당선.getSections().size()).isEqualTo(1);
+    }
+
+    @Test
+    void deleteSection() {
+        // given
+        Station 지하철역 = stationRepository.save(new Station("지하철역"));
+        Station 새로운_지하철역 = stationRepository.save(new Station("새로운_지하철역"));
+        Line 신분당선 = lineRepository.save(new Line("신분당선", "bg-red-600"));
+        lineService.addSection(신분당선.getId(), new SectionRequest(지하철역.getId(), 새로운_지하철역.getId(), 10));
+
+        // when
+        lineService.deleteSection(신분당선.getId(), 새로운_지하철역.getId());
+
+        // then
+        assertThat(신분당선.getSections().size()).isEqualTo(0);
     }
 }
