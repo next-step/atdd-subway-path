58874d1d76951a33d639db66be3b02c1bff2cec9
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-07-11 22:29:38 +0900

    test: 새로운 역을 하행 종점으로 등록할 경우 인수 테스트 구현

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -77,11 +77,13 @@ public class LineAcceptanceTest extends AbstractAcceptanceTest {
      */
     @Test
     void 새로운_역을_하행_종점으로_등록할_경우() {
-        //given
-
         //when
+        LineSteps.지하철_노선_구간_등록_요청(노선.getId(), new SectionRequest(하행종점역.getId(), 새로운역.getId(), 5));
 
         //then
+        List<String> stationNames = LineSteps.지하철_노선_조회_요청(노선.getId()).getStations()
+                .stream().map(StationResponse::getName).collect(Collectors.toList());
+        Assertions.assertThat(stationNames).containsExactly(상행종점역명, 하행종점역명, 새로운역명);
     }
 
     /**
