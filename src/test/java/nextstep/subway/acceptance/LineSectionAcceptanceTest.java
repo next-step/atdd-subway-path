68d6175dbb3a295fa089aa251120e1a165f1ec53
wade.song <wade.song@kakaocorp.com>
2023-07-13 23:06:08 +0900

    [step1] 인수테스트 목록 작성

--- src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineSectionAcceptanceTest.java
@@ -4,6 +4,7 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -35,22 +36,123 @@ class LineSectionAcceptanceTest extends AcceptanceTest {
         신분당선 = 지하철_노선_생성_요청(lineCreateParams).jsonPath().getLong("id");
     }
 
-    /**
-     * When 지하철 노선에 새로운 구간 추가를 요청 하면
-     * Then 노선에 새로운 구간이 추가된다
-     */
-    @DisplayName("지하철 노선에 구간을 등록")
-    @Test
-    void addLineSection() {
-        // when
-        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
-        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+    @Nested
+    @DisplayName("구간 등록 테스트")
+    public class SectionCreationTest {
 
-        // then
-        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역, 정자역);
+        /**
+         * Given 강남역-양재역 지하철 노선(거리 10)에
+         * When 양재역-정자역(거리 6) 구간 추가를 요청 하면
+         * Then 노선에 새로운 구간이 추가되고 (강남역-양재역-정자역)
+         * Then 강남역-양재역은 거리가 10, 양재역-정자역은 거리가 6이다.
+         */
+        @DisplayName("지하철 노선의 마지막에 구간을 추가")
+        @Test
+        void addLineSection_atTheEnd() {
+            // when
+            Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+            지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+
+            // then
+            ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역, 정자역);
+        }
+
+        /**
+         * Given 강남역-양재역 지하철 노선(거리 10)에
+         * When 강남역-정자역(거리 6) 구간 추가를 요청 하면
+         * Then 노선에 새로운 구간이 추가되고 (강남역-정자역-양재역)
+         * Then 강남역-정자역은 거리가 6, 양재역-정자역은 거리가 4이다.
+         */
+        @DisplayName("지하철 노선의 중간에 구간을 추가 (상행역 기준)")
+        @Test
+        void addLineSection_atTheMiddle_basedOnUpStation() {
+
+        }
+
+        /**
+         * Given 강남역-양재역 지하철 노선(거리 10)에
+         * When 정자역-양재역(거리 6) 구간 추가를 요청 하면
+         * Then 노선에 새로운 구간이 추가되고 (강남역-정자역-양재역)
+         * Then 강남역-정자역은 거리가 4, 양재역-정자역은 거리가 6이다.
+         */
+        @DisplayName("지하철 노선의 중간에 구간을 추가 (하행역 기준)")
+        @Test
+        void addLineSection_atTheMiddle_basedOnDownStation() {
+
+        }
+
+        /**
+         * Given 강남역-양재역 지하철 노선(거리 10)에
+         * When 정자역-강남역(거리 6) 구간 추가를 요청 하면
+         * Then 노선에 새로운 구간이 추가되고 (정자역-강남역-양재역)
+         * Then 정자역-강남역은 거리가 6, 강남역-양재역은 거리가 10이다.
+         */
+        @DisplayName("지하철 노선의 시작점에 구간을 추가")
+        @Test
+        void addLineSection_atTheStart() {
+
+        }
+
+        /**
+         * Given 강남역-양재역 지하철 노선에
+         * When 정자역-신사역 구간 추가를 요청 하면
+         * Then 오류가 발생한다.
+         */
+        @DisplayName("[오류] 노선에 존재하지 않는 역으로 이루어진 구간 추가")
+        @Test
+        void addLineSection_error_nonExistStation() {
+
+        }
+
+        /**
+         * Given 강남역-양재역 지하철 노선에
+         * When 강남역-양재역 구간 추가를 요청 하면
+         * Then 오류가 발생한다.
+         */
+        @DisplayName("[오류] 노선에 존재하는 역으로 이루어진 구간 추가")
+        @Test
+        void addLineSection_error_allExistStation() {
+
+        }
+
+        /**
+         * Given 강남역-양재역 지하철 노선(거리 10)에
+         * When 강남역-정자역(거리 11) 구간 추가를 요청 하면
+         * Then 오류가 발생한다.
+         */
+        @DisplayName("[오류] 지하철 노선의 중간에 거리가 긴 구간을 추가 (상행역 기준)")
+        @Test
+        void addLineSection_error_distanceTooLong_atTheMiddle_basedOnUpStation() {
+
+        }
+
+        /**
+         * Given 강남역-양재역 지하철 노선(거리 10)에
+         * When 정자역-양재역(거리 10) 구간 추가를 요청 하면
+         * Then 오류가 발생한다.
+         */
+        @DisplayName("[오류] 지하철 노선의 중간에 거리가 긴 구간을 추가 (하행역 기준)")
+        @Test
+        void addLineSection_error_distanceTooLong_atTheMiddle_basedOnDownStation() {
+
+        }
+
+        /**
+         * Given 강남역-양재역 지하철 노선(거리 10)에
+         * When 정자역-양재역(거리 0) 구간 추가를 요청 하면
+         * Then 오류가 발생한다.
+         */
+        @DisplayName("[오류] 지하철 노선의 중간에 거리가 0인 구간을 추가")
+        @Test
+        void addLineSection_error_zeroDistance_atTheMiddle_basedOnDownStation() {
+
+        }
+
     }
+
+
 
     /**
      * Given 지하철 노선에 새로운 구간 추가를 요청 하고
