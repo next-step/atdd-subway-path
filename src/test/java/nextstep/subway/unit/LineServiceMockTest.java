f09929e9679863a2d6289ba74ee25c8d196b1890
road-jin <rjswmtk@gmail.com>
2023-07-15 15:24:54 +0900

    test: 지하철 노선 서비스 테스트 코드 작성

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,26 +1,54 @@
 package nextstep.subway.unit;
 
+import static org.mockito.BDDMockito.*;
+import static nextstep.subway.utils.LineFixture.*;
+import static nextstep.subway.utils.StationFixture.*;
+
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import nextstep.subway.application.LineService;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
+import nextstep.subway.dto.SectionAddRequest;
+
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
+
+    @InjectMocks
+    private LineService lineService;
+
     @Mock
     private LineRepository lineRepository;
-    @Mock
-    private StationService stationService;
 
+    @Mock
+    private StationRepository stationRepository;
+
+    @DisplayName("지하철 노선에 구간을 추가한다.")
     @Test
     void addSection() {
         // given
-        // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        given(lineRepository.findById(any())).willReturn(Optional.of(new Line(1L, 신분당선_이름, 신분당선_색상)));
+        given(stationRepository.findById(1L)).willReturn(Optional.of(new Station(1L, 신사역)));
+        given(stationRepository.findById(2L)).willReturn(Optional.of(new Station(2L, 논현역)));
+        SectionAddRequest sectionAddRequest = new SectionAddRequest(1L, 2L, 10);
 
         // when
-        // lineService.addSection 호출
+        Line actual = lineService.addSection(1L, sectionAddRequest);
 
         // then
         // lineService.findLineById 메서드를 통해 검증
+        Assertions.assertThat(actual)
+            .usingRecursiveComparison()
+            .isEqualTo(lineService.findById(actual.getId()));
     }
 }
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,30 +1,54 @@
 package nextstep.subway.unit;
 
+import static nextstep.subway.utils.LineFixture.*;
+import static nextstep.subway.utils.StationFixture.*;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import nextstep.subway.application.LineService;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
+import nextstep.subway.dto.LineResponse;
+import nextstep.subway.dto.SectionAddRequest;
+
 @SpringBootTest
 @Transactional
 public class LineServiceTest {
+
     @Autowired
     private StationRepository stationRepository;
+
     @Autowired
     private LineRepository lineRepository;
 
     @Autowired
     private LineService lineService;
 
+    @DisplayName("지하철 노선에 구간을 추가한다.")
     @Test
     void addSection() {
         // given
-        // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station savedStation = stationRepository.save(new Station(1L, 신사역));
+        Station savedStation2 = stationRepository.save(new Station(2L, 논현역));
+        Line savedLine = lineRepository.save(new Line(1L, 신분당선_이름, 신분당선_색상));
+        SectionAddRequest sectionAddRequest = new SectionAddRequest(savedStation.getId(),
+            savedStation2.getId(), 10);
 
         // when
-        // lineService.addSection 호출
+        Line actual = lineService.addSection(savedLine.getId(), sectionAddRequest);
 
         // then
-        // line.getSections 메서드를 통해 검증
+        Assertions.assertThat(actual.getStations())
+            .usingRecursiveComparison()
+            .isEqualTo(List.of(savedStation, savedStation2));
     }
 }
