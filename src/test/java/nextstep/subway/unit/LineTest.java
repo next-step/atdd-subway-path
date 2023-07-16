f55cfaa0b1fae00bda207b6a3572c5de652d45bc
owen.qq <owen.qq@kakaocorp.com>
2023-07-16 23:05:53 +0900

    refactor: lineTest에 대한 코드리뷰 반영

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,5 +1,7 @@
 package nextstep.subway.unit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,104 +18,113 @@ import nextstep.subway.station.domain.Station;
 
 class LineTest {
 
-    private Station stationA;
-    private Station stationB;
-    private Line lineAB;
-    private Section firstSection;
+    @Nested
+    class Given_노선에_구간이1개있을때 {
 
-    @BeforeEach
-    void beforeEach() {
-        // given
-        stationA = createStation(1L, "stationA");
-        stationB = createStation(2L, "stationB");
+        private Station stationA;
+        private Station stationB;
+        private Line lineAB;
+        private Section firstSection;
 
-        lineAB = Line.builder()
-                     .id(10L)
-                     .name("lineAB")
-                     .color("yellow")
-                     .upStation(stationA)
-                     .downStation(stationB)
-                     .sections(new Sections(new ArrayList<>()))
-                     .build();
+        @BeforeEach
+        void beforeEach() {
+            // given
+            stationA = createStation(1L, "stationA");
+            stationB = createStation(2L, "stationB");
 
-        firstSection = Section.builder().line(lineAB).distance(10)
-                              .upStation(stationA)
-                              .downStation(stationB)
-                              .build();
-    }
+            lineAB = Line.builder()
+                         .id(10L)
+                         .name("lineAB")
+                         .color("yellow")
+                         .upStation(stationA)
+                         .downStation(stationB)
+                         .sections(new Sections(new ArrayList<>()))
+                         .build();
 
-    @Test
-    void addSection() {
-        // when
-        // then
-        Assertions.assertDoesNotThrow(() -> {
+            firstSection = Section.builder().line(lineAB).distance(10)
+                                  .upStation(stationA)
+                                  .downStation(stationB)
+                                  .build();
+        }
+
+        @Test
+        void addSection() {
+            // when
+            // then
+            Assertions.assertDoesNotThrow(() -> {
+                lineAB.addSection(firstSection);
+            });
+
+        }
+
+        @Test
+        void getStations() {
+            // when
             lineAB.addSection(firstSection);
-        });
 
-    }
+            // then
+            assertThat(lineAB.getSections().isEmpty())
+                                           .isFalse();
+        }
 
-    private Station createStation(Long id, String name) {
-        return new Station(id, name);
-    }
+        @Test
+        void removeSection() {
+            // when
+            lineAB.addSection(firstSection);
 
-    @Test
-    void getStations() {
-        // when
-        lineAB.addSection(firstSection);
-
-        // then
-        org.assertj.core.api.Assertions.assertThat(lineAB.getSections().isEmpty())
-                                       .isFalse();
-    }
-
-    @Test
-    void removeSection() {
-        // when
-        lineAB.addSection(firstSection);
-
-        // then
-        Assertions.assertThrows(InvalidSectionDeleteException.class, () -> {
-            lineAB.deleteSection(stationB.getId());
-        });
+            // then
+            Assertions.assertThrows(InvalidSectionDeleteException.class, () -> {
+                lineAB.deleteSection(stationB.getId());
+            });
+        }
     }
 
     @Nested
     class Given_노선에_구간이_2개이상일때 {
+        private Station stationA;
+        private Station stationB;
+        private Station stationC;
+        private Line lineAB;
+        private Section firstSection;
+
+        @BeforeEach
+        void beforeEach() {
+            // given
+            stationA = createStation(1L, "stationA");
+            stationB = createStation(2L, "stationB");
+            stationC = createStation(3L, "stationC");
+
+            firstSection = new Section(1L, lineAB, stationA, stationB, 10);
+
+            List<Section> sectionList = new ArrayList<>();
+            sectionList.add(firstSection);
+
+            Sections sections = new Sections(sectionList);
+
+            lineAB = Line.builder()
+                         .id(1L)
+                         .color("y")
+                         .upStation(stationA)
+                         .downStation(stationB)
+                         .sections(sections)
+                         .build();
+
+            Section givenAddSection = new Section(2L, lineAB, stationB, stationC,5);
+
+            lineAB.addSection(givenAddSection);
+        }
 
         @Nested
         class When_상행_종점역을_제거하면 {
+
             @Test
             void 상행_종점역이_변경된다() {
-                // given
-                Station stationA = createStation(1L, "stationA");
-                Station stationB = createStation(2L, "stationB");
-                Station stationC = createStation(3L, "stationC");
-
-                firstSection = new Section(1L, lineAB, stationA, stationB, 10);
-
-                List<Section> sectionList = new ArrayList<>();
-                sectionList.add(firstSection);
-
-                Sections sections = new Sections(sectionList);
-
-                lineAB = Line.builder()
-                             .id(1L)
-                             .color("y")
-                             .upStation(stationA)
-                             .downStation(stationB)
-                             .sections(sections)
-                             .build();
-
-                Section givenAddSection = new Section(2L, lineAB, stationB, stationC,5);
-
-                lineAB.addSection(givenAddSection);
-
                 // when
                 lineAB.deleteSection(stationA.getId());
 
                 // when
-                org.assertj.core.api.Assertions.assertThat(lineAB.equalUpStation(stationB.getId()));
-                org.assertj.core.api.Assertions.assertThat(lineAB.equalDownStation(stationC.getId()));
+                assertThat(lineAB.equalUpStation(stationB.getId()));
+                assertThat(lineAB.equalDownStation(stationC.getId()));
             }
         }
 
@@ -122,37 +133,17 @@ class LineTest {
 
             @Test
             void 하행_종점역이_변경된다() {
-                // given
-                Station stationA = createStation(1L, "stationA");
-                Station stationB = createStation(2L, "stationB");
-                Station stationC = createStation(3L, "stationC");
-
-                firstSection = new Section(1L, lineAB, stationA, stationB, 10);
-
-                List<Section> sectionList = new ArrayList<>();
-                sectionList.add(firstSection);
-
-                Sections sections = new Sections(sectionList);
-
-                lineAB = Line.builder()
-                             .id(1L)
-                             .color("y")
-                             .upStation(stationA)
-                             .downStation(stationB)
-                             .sections(sections)
-                             .build();
-
-                Section givenAddSection = new Section(2L, lineAB, stationB, stationC,5);
-
-                lineAB.addSection(givenAddSection);
-
                 // when
                 lineAB.deleteSection(stationC.getId());
 
                 // when
-                org.assertj.core.api.Assertions.assertThat(lineAB.equalUpStation(stationA.getId()));
-                org.assertj.core.api.Assertions.assertThat(lineAB.equalDownStation(stationB.getId()));
+                assertThat(lineAB.equalUpStation(stationA.getId())).isTrue();
+                assertThat(lineAB.equalDownStation(stationB.getId())).isTrue();
             }
         }
+    }
+
+    private Station createStation(Long id, String name) {
+        return new Station(id, name);
     }
 }
