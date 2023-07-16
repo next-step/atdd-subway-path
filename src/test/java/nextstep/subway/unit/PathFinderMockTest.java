69f82367a46fe23f411ef48294dc59e63d46f51d
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-07-16 14:36:48 +0900

    test: 출발역과 도착역이 같은 경우 단위 테스트 구현

--- src/test/java/nextstep/subway/unit/PathFinderMockTest.java
+++ src/test/java/nextstep/subway/unit/PathFinderMockTest.java
@@ -7,7 +7,9 @@ import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.SectionRepository;
 import nextstep.subway.domain.Station;
+import nextstep.subway.exception.PathException;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -25,26 +27,48 @@ public class PathFinderMockTest {
     @Mock
     private SectionRepository sectionRepository;
 
+    private String 출발역명 = "출발역";
+    private String 중간역명 = "중간역";
+    private String 도착역명 = "도착역";
+    private Station 출발역;
+    private Station 중간역;
+    private Station 도착역;
+    private Line 노선;
+    private Section 출발역_중간역;
+    private Section 중간역_도착역;
+    private Section 출발역_도착역;
+
+    private PathFinder pathFinder;
+
+    @BeforeEach
+    public void setUp() {
+        출발역 = new Station(1L, 출발역명);
+        중간역 = new Station(2L, 중간역명);
+        도착역 = new Station(3L, 도착역명);
+        노선 = new Line();
+        출발역_중간역 = new Section(노선, 출발역, 중간역, 5);
+        중간역_도착역 = new Section(노선, 중간역, 도착역, 10);
+        출발역_도착역 = new Section(노선, 출발역, 도착역, 16);
+
+        when(sectionRepository.findAll()).thenReturn(List.of(출발역_중간역, 중간역_도착역, 출발역_도착역));
+        pathFinder = new PathFinder(sectionRepository);
+    }
+
     @Test
     void find() {
-        //given
-        Station 출발역 = new Station(1L, "출발역");
-        Station 중간역 = new Station(2L, "중간역");
-        Station 도착역 = new Station(3L, "도착역");
-        Line 노선 = new Line();
-        Section 출발역_중간역 = new Section(노선, 출발역, 중간역, 5);
-        Section 중간역_도착역 = new Section(노선, 중간역, 도착역, 10);
-        Section 출발역_도착역 = new Section(노선, 출발역, 도착역, 16);
-
-        //when
-        when(sectionRepository.findAll()).thenReturn(List.of(출발역_중간역, 중간역_도착역, 출발역_도착역));
-        PathFinder pathFinder = new PathFinder(sectionRepository);
-
         //then
         PathResponse pathResponse = pathFinder.find(1L, 3L);
         List<String> stationNames = pathResponse.getStations().stream()
                 .map(StationResponse::getName).collect(Collectors.toList());
-        assertThat(stationNames).containsExactly("출발역", "중간역", "도착역");
+        assertThat(stationNames).containsExactly(출발역명, 중간역명, 도착역명);
         assertThat(pathResponse.getDistance()).isEqualTo(15);
+    }
+
+    @Test
+    void 출발역과_도착역이_같은_경우() {
+        //then
+        assertThatThrownBy(() -> pathFinder.find(1L, 1L))
+                .isInstanceOf(PathException.class)
+                .hasMessage("출발역과 도착역이 같습니다.");
     }
 }
