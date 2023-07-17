21555e90667153819aede31b7aad9e162c93c76a
psy3720 <qkrtmddyd12@naver.com>
2023-07-17 19:26:44 +0900

    test: 경로 조회 인수테스트 추가

--- src/test/java/nextstep/subway/acceptance/LineSteps.java
+++ src/test/java/nextstep/subway/acceptance/LineSteps.java
@@ -9,6 +9,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LineSteps {
+    public static long 지하철_노선_생성_요청(String name, String color, Long upStationId, Long downStationId
+            , int distance) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+        params.put("upStationId", String.valueOf(upStationId));
+        params.put("downStationId", String.valueOf(downStationId));
+        params.put("distance", String.valueOf(distance));
+
+        return RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all().extract().jsonPath().getLong("id");
+    }
+
     public static ExtractableResponse<Response> 지하철_노선_생성_요청(String name, String color) {
         Map<String, String> params = new HashMap<>();
         params.put("name", name);
