2f89d82bd05927495fad6054b15e6ba813be3371
Minsuk kim <alstjrdl951@gmail.com>
2023-07-18 16:37:34 +0900

    test: 구간 등록 도메인 단위 테스트

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -4,6 +4,7 @@ import nextstep.subway.line.entity.Line;
 import nextstep.subway.line.entity.Section;
 import nextstep.subway.station.entity.Station;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -15,6 +16,7 @@ class LineTest {
     Station 강남역;
     Station 역삼역;
     Station 선릉역;
+    Station 익명역;
     Line 이호선;
 
     @BeforeEach
@@ -22,16 +24,40 @@ class LineTest {
         강남역 = new Station(1L, "강남역");
         역삼역 = new Station(2L, "역삼역");
         선릉역 = new Station(3L, "선릉역");
+        익명역 = new Station(4L, "익명역");
         이호선 = new Line("이호선", "Green", 10, 강남역, 역삼역);
     }
 
+
+    @DisplayName("구간을 등록한다. 기존 구간 A-C에 신규 구간 C-D 추가")
     @Test
-    void addSection() {
+    void addSectionACCD() {
         // when
-        이호선.addSection(new Section(이호선, 역삼역, 선릉역, 20));
+        이호선.addSection(new Section(이호선, 역삼역, 선릉역, 10));
 
         // then
         assertThat(이호선.getStations()).contains(선릉역);
+    }
+
+    @DisplayName("구간을 등록한다. 기존 구간 A-C에 신규 구간 A-B 추가")
+    @Test
+    void addSectionACAB() {
+        // when
+        int smallerDistanceThanSectionAC = 1;
+        이호선.addSection(new Section(이호선, 강남역, 익명역, smallerDistanceThanSectionAC));
+
+        // then
+        assertThat(이호선.getStations()).contains(익명역);
+    }
+
+    @DisplayName("구간을 등록한다. 기존 구간 A-C에 신규 구간 B-A 추가")
+    @Test
+    void addSectionACBA() {
+        // when
+        이호선.addSection(new Section(이호선, 익명역, 강남역, 10));
+
+        // then
+        assertThat(이호선.getStations()).contains(익명역);
     }
 
     @Test
@@ -46,7 +72,7 @@ class LineTest {
     @Test
     void removeSection() {
         // given
-        이호선.addSection(new Section(이호선, 역삼역, 선릉역, 20));
+        이호선.addSection(new Section(이호선, 역삼역, 선릉역, 10));
 
         // when
         이호선.removeSection(선릉역);
