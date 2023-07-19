54b523058c6567d39604e1de27f718c94d8230a2
psy3720 <qkrtmddyd12@naver.com>
2023-07-19 23:48:45 +0900

    test: import 와일드 카드 수정, validation 로직 dto 클래스로 이동

--- src/main/java/nextstep/subway/applicaion/PathService.java
+++ src/main/java/nextstep/subway/applicaion/PathService.java
@@ -3,14 +3,16 @@ package nextstep.subway.applicaion;
 import nextstep.subway.applicaion.dto.PathRequest;
 import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
-import nextstep.subway.domain.*;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.StationRepository;
+import nextstep.subway.domain.PathFinder;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.exception.StationNotFoundException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,32 +23,20 @@ public class PathService {
     private final PathFinder pathFinder;
 
     public PathService(LineRepository lineRepository, StationRepository stationRepository, PathFinder pathFinder) {
-        this.lineRepository = lineRepository;
         this.stationRepository = stationRepository;
         this.pathFinder = pathFinder;
+        this.lineRepository = lineRepository;
     }
 
     public PathResponse findPath(PathRequest request) {
-        validatePathRequest(request);
-
-        List<Line> lines = lineRepository.findAll();
         Station source = stationRepository.findById(request.getSource()).orElseThrow(() -> new StationNotFoundException());
         Station target = stationRepository.findById(request.getTarget()).orElseThrow(() -> new StationNotFoundException());
+        List<Line> lines = lineRepository.findAll();
 
         List<Station> path = pathFinder.findPath(lines, source, target);
         int pathWeight = pathFinder.findPathWeight(lines, source, target);
 
         return createPathResponse(path, pathWeight);
-    }
-
-    private static void validatePathRequest(PathRequest request) {
-        if (Objects.isNull(request.getTarget()) || Objects.isNull(request.getSource())) {
-            throw new StationNotFoundException();
-        }
-
-        if (request.getTarget() == request.getSource()) {
-            throw new DataIntegrityViolationException("출발역과 도착역이 같습니다.");
-        }
     }
 
     private PathResponse createPathResponse(List<Station> path, int pathWeight) {
--- src/main/java/nextstep/subway/applicaion/dto/PathRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/PathRequest.java
@@ -1,10 +1,17 @@
 package nextstep.subway.applicaion.dto;
 
+import nextstep.subway.domain.exception.StationNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import java.util.Objects;
+
 public class PathRequest {
     private Long source;
     private Long target;
 
     public PathRequest(Long source, Long target) {
+        validatePathRequest(source, target);
+
         this.source = source;
         this.target = target;
     }
@@ -23,5 +30,15 @@ public class PathRequest {
 
     public void setTarget(Long target) {
         this.target = target;
+    }
+
+    private static void validatePathRequest(Long source, Long target) {
+        if (Objects.isNull(source) || Objects.isNull(target)) {
+            throw new StationNotFoundException();
+        }
+
+        if (source == target) {
+            throw new DataIntegrityViolationException("출발역과 도착역이 같습니다.");
+        }
     }
 }
