524ff4cc948606c943699b06ef75e1cf19646047
SeokyungKim <kskyung0624@gmail.com>
2023-07-20 15:19:30 +0900

    refactor : GlobalException의 Response Type을 BaseExceptionResponse로 만든다

--- src/main/java/nextstep/subway/common/exception/GlobalExceptionHandler.java
+++ src/main/java/nextstep/subway/common/exception/GlobalExceptionHandler.java
@@ -9,12 +9,12 @@ public class GlobalExceptionHandler {
 
     // BaseResponse 두고 거기다가 모든 값 null 이런 패턴을 사용해야 한다고 생각하나, 미션 단순화를 위해 이렇게 둠
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> handleResourceNotFoundException(final ResourceNotFoundException e) {
+    public ResponseEntity<BaseExceptionResponse> handleResourceNotFoundException(final ResourceNotFoundException e) {
         return ResponseEntity.ok().body(new BaseExceptionResponse(e.getMessage()));
     }
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<?> handleBusinessException(final BusinessException e) {
+    public ResponseEntity<BaseExceptionResponse> handleBusinessException(final BusinessException e) {
         return ResponseEntity.badRequest().body(new BaseExceptionResponse(e.getMessage()));
     }
 }
