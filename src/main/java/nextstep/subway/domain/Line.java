10e927da848d7bf3a10347be865a9e15c3371165
pawoo0211 <pawoo0211@naver.com>
2023-07-20 12:53:42 +0900

    [0단계] 구간 단위 테스트 작성

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -15,12 +15,17 @@ public class Line {
     @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
     private List<Section> sections = new ArrayList<>();
 
-    public Line() {
-    }
+    public Line() { }
 
     public Line(String name, String color) {
         this.name = name;
         this.color = color;
+    }
+
+    public Line(String name, String color, Station upStation, Station downStation, int distance) {
+        this.name = name;
+        this.color = color;
+        this.sections.add(new Section(this, upStation, downStation, distance));
     }
 
     public Long getId() {
@@ -49,5 +54,29 @@ public class Line {
 
     public List<Section> getSections() {
         return sections;
+    }
+
+    public void addSection(Section section) {
+        this.sections.add(section);
+    }
+
+    public void deleteSectionByUpStation(Station upStation) {
+        for (Section savedSection : this.sections) {
+            Station savedUpStation = savedSection.getUpStation();
+            if (savedUpStation.getName().equals(upStation.getName())) {
+                this.sections.remove(savedSection);
+                return;
+            }
+        }
+    }
+
+    public void deleteSectionByDownStation(Station downStation) {
+        for (Section savedSection : this.sections) {
+            Station savedUpStation = savedSection.getUpStation();
+            if (savedUpStation.getName().equals(downStation.getName())) {
+                this.sections.remove(savedSection);
+                return;
+            }
+        }
     }
 }
--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,17 +1,68 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("구간 단위 테스트")
+@ExtendWith(MockitoExtension.class)
 class LineTest {
+
+    private Station 강남역;
+    private Station 양재역;
+    private Line line;
+
+    @BeforeEach
+    void setUp() {
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        line = new Line("2호선", "green", 강남역, 양재역, 7);
+    }
+
+    @DisplayName("지하철 노선에 구간 추가")
     @Test
     void addSection() {
+        // given
+        Station 역삼역 = new Station("역삼역");
+        Station 구디역 = new Station("구디역");
+        Section section = new Section(line, 역삼역, 구디역, 3);
+
+        // when
+        line.addSection(section);
+
+        // then
+        Assertions.assertThat(line.getSections()).hasSize(2);
     }
 
+    @DisplayName("지하철 노선의 역 조회")
     @Test
     void getStations() {
+        // given
+        Section section = line.getSections().get(0);
+
+        // when
+        Station upStation = section.getUpStation();
+        Station downStation = section.getDownStation();
+
+        // then
+        assertThat(upStation).isEqualTo(강남역);
+        assertThat(downStation).isEqualTo(양재역);
     }
 
+    @DisplayName("지하철 구간 삭제")
     @Test
     void removeSection() {
+
+        // when
+        line.deleteSectionByUpStation(강남역);
+
+        // then
+        Assertions.assertThat(line.getSections()).hasSize(0);
     }
 }
