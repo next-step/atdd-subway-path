a56f9f19adb57647225902bc0bd668d574272823
김설영 <kimsy8979@mrblue.com>
2023-07-21 13:54:04 +0900

    test: 구간 제거 단위테스트 작성

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -19,15 +19,15 @@ import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("노선 단위 테스트")
 class LineTest {
-    private Station 강남역;
-    private Station 양재역;
-    private Line 신분당선;
+    private Station 노선_상행_역;
+    private Station 노선_하행_역;
+    private Line 노선;
 
     @BeforeEach
     void setUp() {
-        강남역 = new Station(1L, "강남역");
-        양재역 = new Station(2L, "양재역");
-        신분당선 = new Line("신분당선", "bg-red-600", new Section(강남역, 양재역, 10));
+        노선_상행_역 = new Station(1L, "노선_상행_역");
+        노선_하행_역 = new Station(2L, "노선_하행_역");
+        노선 = new Line("노선", "bg-red-600", new Section(노선_상행_역, 노선_하행_역, 10));
     }
 
     @DisplayName("구간 추가 기능 테스트")
@@ -41,20 +41,20 @@ class LineTest {
             void addSectionBetweenStations() {
                 // given
                 Station 가운데_추가될_역 = new Station(3L, "가운데_추가될_역");
-                Section newSection = new Section(강남역, 가운데_추가될_역, 4);
+                Section newSection = new Section(노선_상행_역, 가운데_추가될_역, 4);
 
                 // when
-                신분당선.registerSection(newSection);
+                노선.registerSection(newSection);
 
-                // then : 강남역-양재역 & 양재역-양재시민의숲역 구간이 있어야함 각각 길이는 4, 6
-                List<Section> sections = 신분당선.getSections();
+                // then : 노선_상행_역-노선_하행_역 & 노선_하행_역-양재시민의숲역 구간이 있어야함 각각 길이는 4, 6
+                List<Section> sections = 노선.getSections();
                 assertThat(sections).hasSize(2);
 
                 Section firstSection = sections.get(0);
-                assertSection(firstSection, "강남역", "가운데_추가될_역", 4);
+                assertSection(firstSection, "노선_상행_역", "가운데_추가될_역", 4);
 
                 Section secondSection = sections.get(1);
-                assertSection(secondSection, "가운데_추가될_역", "양재역", 6);
+                assertSection(secondSection, "가운데_추가될_역", "노선_하행_역", 6);
             }
 
             private void assertSection(Section target, String upStationName, String downStationName, int distance) {
@@ -68,23 +68,23 @@ class LineTest {
             void addSectionUpStation() {
                 // given
                 Station 상행종점에_추가될_역 = new Station(3L, "상행종점에_추가될_역");
-                Section newSection = new Section(상행종점에_추가될_역, 강남역, 5);
+                Section newSection = new Section(상행종점에_추가될_역, 노선_상행_역, 5);
 
                 // when
-                신분당선.registerSection(newSection);
+                노선.registerSection(newSection);
 
                 // then
-                Long 상행_종점 = 신분당선.getFirstStationId();
+                Long 상행_종점 = 노선.getFirstStationId();
                 assertThat(상행_종점).isEqualTo(상행종점에_추가될_역.getId());
 
-                List<Section> sections = 신분당선.getSections();
+                List<Section> sections = 노선.getSections();
                 assertThat(sections).hasSize(2);
 
                 Section firstSection = sections.get(0);
-                assertSection(firstSection, "상행종점에_추가될_역", "강남역", 5);
+                assertSection(firstSection, "상행종점에_추가될_역", "노선_상행_역", 5);
 
                 Section secondSection = sections.get(1);
-                assertSection(secondSection, "강남역", "양재역", 10);
+                assertSection(secondSection, "노선_상행_역", "노선_하행_역", 10);
             }
 
             @DisplayName("하행역에 새로운 구간 추가")
@@ -92,23 +92,23 @@ class LineTest {
             void addSectionDownStation() {
                 // given
                 Station 하행종점에_추가될_역 = new Station(3L, "하행종점에_추가될_역");
-                Section newSection = new Section(양재역, 하행종점에_추가될_역, 5);
+                Section newSection = new Section(노선_하행_역, 하행종점에_추가될_역, 5);
 
                 // when
-                신분당선.registerSection(newSection);
+                노선.registerSection(newSection);
 
                 // then
-                Long 하행_종점 = 신분당선.getLastStationId();
+                Long 하행_종점 = 노선.getLastStationId();
                 assertThat(하행_종점).isEqualTo(하행종점에_추가될_역.getId());
 
-                List<Section> sections = 신분당선.getSections();
+                List<Section> sections = 노선.getSections();
                 assertThat(sections).hasSize(2);
 
                 Section firstSection = sections.get(0);
-                assertSection(firstSection, "강남역", "양재역", 10);
+                assertSection(firstSection, "노선_상행_역", "노선_하행_역", 10);
 
                 Section secondSection = sections.get(1);
-                assertSection(secondSection, "양재역", "하행종점에_추가될_역", 5);
+                assertSection(secondSection, "노선_하행_역", "하행종점에_추가될_역", 5);
             }
         
         }
@@ -125,7 +125,7 @@ class LineTest {
                 Section newSection = new Section(여의도역, 샛강역, 12);
 
                 // when, then
-                assertThatThrownBy(() -> 신분당선.registerSection(newSection))
+                assertThatThrownBy(() -> 노선.registerSection(newSection))
                         .isInstanceOf(InvalidSectionRegistrationException.class);
             }
 
@@ -133,10 +133,10 @@ class LineTest {
             @Test
             void registerSectionFailByAlreadyRegisteredSection() {
                 // given
-                Section sectionForLine = new Section(강남역, 양재역, 10);
+                Section sectionForLine = new Section(노선_상행_역, 노선_하행_역, 10);
 
                 // when, then
-                assertThatThrownBy(() -> 신분당선.registerSection(sectionForLine))
+                assertThatThrownBy(() -> 노선.registerSection(sectionForLine))
                         .isInstanceOf(AlreadyRegisteredStationException.class);
             }
 
@@ -146,10 +146,10 @@ class LineTest {
             void registerSectionFailByDistance(int newSectionDistance) {
                 // given
                 Station 가운데_추가될_역 = new Station(3L, "가운데_추가될_역");
-                Section newSection = new Section(강남역, 가운데_추가될_역, newSectionDistance);
+                Section newSection = new Section(노선_상행_역, 가운데_추가될_역, newSectionDistance);
 
                 // when, then
-                assertThatThrownBy(() -> 신분당선.registerSection(newSection))
+                assertThatThrownBy(() -> 노선.registerSection(newSection))
                         .isInstanceOf(DistanceNotLongerThanExistingSectionException.class);
             }
         }
@@ -160,23 +160,94 @@ class LineTest {
     void getSections() {
         // given
         Station 상행종점에_추가될_역 = new Station(3L, "상행종점에_추가될_역");
-        Section newSection = new Section(상행종점에_추가될_역, 강남역, 5);
-        신분당선.registerSection(newSection);
+        Section newSection = new Section(상행종점에_추가될_역, 노선_상행_역, 5);
+        노선.registerSection(newSection);
 
         // when
-        List<Section> sections = 신분당선.getSections();
+        List<Section> sections = 노선.getSections();
 
         // then
         assertThat(sections.get(0).getUpStation()).isEqualTo(상행종점에_추가될_역);
-        assertThat(sections.get(1).getUpStation()).isEqualTo(강남역);
+        assertThat(sections.get(1).getUpStation()).isEqualTo(노선_상행_역);
     }
 
-    @Test
-    void getStations() {
+    @DisplayName("구간 삭제 기능 테스트")
+    @Nested
+    class RemoveSection {
+        @DisplayName("성공 케이스")
+        @Nested
+        class Success {
+            @DisplayName("상행 종점 역 제거")
+            @Test
+            void removeFirstStation() {
+                // given : 역1 -(거리)- 역2 ...
+                // 상행종점에_추가될_역 -(5)- 노선_상행_역 -(10)- 노선_하행_역
+                Station 상행종점에_추가될_역 = new Station(3L, "상행종점에_추가될_역");
+                Section newSection = new Section(상행종점에_추가될_역, 노선_상행_역, 5);
+                노선.registerSection(newSection);
 
-    }
+                // when
+                노선.removeSection(상행종점에_추가될_역);
 
-    @Test
-    void removeSection() {
+                // then
+                List<Section> sections = 노선.getSections();
+                assertThat(sections).hasSize(1);
+
+                Section remainSection = sections.get(0);
+                assertThat(remainSection.getUpStationName()).isEqualTo("노선_상행_역");
+                assertThat(remainSection.getDownStationName()).isEqualTo("노선_하행_역");
+                assertThat(remainSection.getDistance()).isEqualTo(10);
+            }
+
+            @DisplayName("하행 종점 역 제거")
+            @Test
+            void removeLastStation() {
+                // given : 역1 -(거리)- 역2 ...
+                // 상행종점에_추가될_역 -(5)- 노선_상행_역 -(10)- 노선_하행_역
+                Station 상행종점에_추가될_역 = new Station(3L, "상행종점에_추가될_역");
+                Section newSection = new Section(상행종점에_추가될_역, 노선_상행_역, 5);
+                노선.registerSection(newSection);
+
+                // when
+                노선.removeSection(노선_하행_역);
+
+                // then
+                List<Section> sections = 노선.getSections();
+                assertThat(sections).hasSize(1);
+
+                Section remainSection = sections.get(0);
+                assertThat(remainSection.getUpStationName()).isEqualTo("상행_종점에_추가될_역");
+                assertThat(remainSection.getDownStationName()).isEqualTo("노선_상행_역");
+                assertThat(remainSection.getDistance()).isEqualTo(5);
+            }
+
+            @DisplayName("가운데 역 제거")
+            @Test
+            void removeMidStation() {
+                // given : 역1 -(거리)- 역2 ...
+                // 상행종점에_추가될_역 -(5)- 노선_상행_역 -(10)- 노선_하행_역
+                Station 상행종점에_추가될_역 = new Station(3L, "상행종점에_추가될_역");
+                Section newSection = new Section(상행종점에_추가될_역, 노선_상행_역, 5);
+                노선.registerSection(newSection);
+
+                // when
+                노선.removeSection(노선_상행_역);
+
+                // then
+                List<Section> sections = 노선.getSections();
+                assertThat(sections).hasSize(1);
+
+                Section remainSection = sections.get(0);
+                assertThat(remainSection.getUpStationName()).isEqualTo("상행종점에_추가될_역");
+                assertThat(remainSection.getDownStationName()).isEqualTo("노선_하행_역");
+                assertThat(remainSection.getDistance()).isEqualTo(15);
+            }
+        }
+
+        @DisplayName("실패 케이스")
+        @Nested
+        class Fail {
+
+        }
     }
 }
