97c435f72e81d2472c88a1b4ed4eed4c7fd2c803
Minsuk kim <alstjrdl951@gmail.com>
2023-07-24 00:43:43 +0900

    test: 구간 제거 핸들러 단위 테스트 추가

--- src/test/java/nextstep/subway/unit/line/handler/SectionDeletionHandlerTest.java
+++ src/test/java/nextstep/subway/unit/line/handler/SectionDeletionHandlerTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SectionDeletionHandlerMappingTest {
+public class SectionDeletionHandlerTest {
 
     final int SECTION_DEFAULT_DISTANCE = 10;
 
@@ -21,9 +21,6 @@ public class SectionDeletionHandlerMappingTest {
     Station 익명역;
     Line 이호선;
 
-    SectionDeletionHandlerMapping handlerMapping = new SectionDeletionHandlerMapping();
-
-
     @BeforeEach
     void setUp() {
         강남역 = new Station(1L, "강남역");
@@ -31,40 +28,49 @@ public class SectionDeletionHandlerMappingTest {
         선릉역 = new Station(3L, "선릉역");
         익명역 = new Station(4L, "익명역");
         이호선 = new Line("이호선", "Green", SECTION_DEFAULT_DISTANCE, 강남역, 역삼역);
-        이호선.addSection(new SectionAdditionHandlerMapping(), new Section(이호선, 역삼역, 선릉역, SECTION_DEFAULT_DISTANCE));
-
     }
 
-    @DisplayName("상행 종착역이 속한 구간 제거하는 핸들러 반환")
+    /**
+     * Given
+     * When DeleteSectionAtTopHandler에 상행 종착역과 구간을 전달하며 구간 삭제 명령을 내리면
+     * Then 구간에서 상행 종착역을 찾을 수 없다
+     * */
+    @DisplayName("상행 종착역이 속한 구간 제거")
     @Test
     void returnDeleteSectionAtTopHandler() {
+        // given
+        이호선.addSection(new SectionAdditionHandlerMapping(), new Section(이호선, 역삼역, 선릉역, SECTION_DEFAULT_DISTANCE));
+
         // when
-        SectionDeletionHandler handler = handlerMapping.getHandler(이호선.getSections(), 강남역);
+        new DeleteSectionAtTopHandler().apply(이호선.getSections(), 강남역);
 
         // then
-        assertThat(handler).isInstanceOf(DeleteSectionAtTopHandler.class);
-        assertThat(handler.checkApplicable(이호선.getSections(), 강남역)).isTrue();
+        assertThat(이호선.getStations()).doesNotContain(강남역);
     }
 
-    @DisplayName("하행 종착역이 속한 구간 제거하는 핸들러 반환")
+    @DisplayName("하행 종착역이 속한 구간 제거")
     @Test
     void returnDeleteSectionAtLastHandler() {
+        // given
+        이호선.addSection(new SectionAdditionHandlerMapping(), new Section(이호선, 역삼역, 선릉역, SECTION_DEFAULT_DISTANCE));
+
         // when
-        SectionDeletionHandler handler = handlerMapping.getHandler(이호선.getSections(), 선릉역);
+        new DeleteSectionAtLastHandler().apply(이호선.getSections(), 선릉역);
 
         // then
-        assertThat(handler).isInstanceOf(DeleteSectionAtLastHandler.class);
-        assertThat(handler.checkApplicable(이호선.getSections(), 선릉역)).isTrue();
+        assertThat(이호선.getStations()).doesNotContain(선릉역);
     }
 
     @DisplayName("상행 종착역과 하행종착역이 아닌 중간 구간 제거하는 핸들러 반환")
     @Test
     void returnDeleteSectionAtMiddleHandler() {
+        // given
+        이호선.addSection(new SectionAdditionHandlerMapping(), new Section(이호선, 역삼역, 선릉역, SECTION_DEFAULT_DISTANCE));
+
         // when
-        SectionDeletionHandler handler = handlerMapping.getHandler(이호선.getSections(), 역삼역);
+        new DeleteSectionAtMiddleHandler().apply(이호선.getSections(), 선릉역);
 
         // then
-        assertThat(handler).isInstanceOf(DeleteSectionAtMiddleHandler.class);
-        assertThat(handler.checkApplicable(이호선.getSections(), 역삼역)).isTrue();
+        assertThat(이호선.getStations()).doesNotContain(선릉역);
     }
 }
