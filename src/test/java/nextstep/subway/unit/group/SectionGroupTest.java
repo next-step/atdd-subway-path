e566a1ea9bf0be210977558e8700bd5d4e7912eb
Hyeongmin Park <thalsal@naver.com>
2023-07-26 17:15:10 +0900

    test : 출발지와 목적지가 연결되어있지 않은경우 단위 테스트

--- src/test/java/nextstep/subway/unit/group/SectionGroupTest.java
+++ src/test/java/nextstep/subway/unit/group/SectionGroupTest.java
@@ -410,4 +410,31 @@ class SectionGroupTest {
         //then
         assertThat(distance).isEqualTo(15);
     }
+
+    @Test
+    @DisplayName("출발역에서 목적지까지 최적 경로 가중치 추출")
+    void getPathsWeight_notConnected() {
+
+        //given
+        Station 부평역 = StationFixture.of(1);
+        Station 구로역 = StationFixture.of(2);
+        Station 신도림역 = StationFixture.of(3);
+        Station 영등포구청역 = StationFixture.of(4);
+
+        List<Section> sections = List.of(
+            new Section(line, 부평역, 구로역, 5),
+            new Section(line, 신도림역, 영등포구청역, 10)
+        );
+
+        // 부평 - 5 - 구로 - 5 | 신도림 *** 10 *** 영등포구청
+
+        //given
+        SectionGroup sectionGroup = SectionGroup.of(sections);
+
+        //when - then
+        assertThatThrownBy(() -> sectionGroup.getPath(부평역, 영등포구청역))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageMatching("출발지와 목적지의 각 구간이 이어져있지 않습니다.");
+
+    }
 }
