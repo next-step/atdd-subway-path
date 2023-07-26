f3bea47d2b08dcd3e62ff687a0095df96d6b7d3c
Jivebreaddev <jivebreaddev@gmail.com>
2023-07-26 20:41:31 +0900

    test(LineTest): 구간 추가에 대한 유닛테스트 구현했습니다.

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -42,8 +42,9 @@ public class LineServiceTest {
         // when
         lineService.addSection(lineResponse.getId(), SectionRequest.builder().distance(10L).upStationId(2L).downStationId(3L).build());
         // then
-        Optional<Line> line = lineRepository.findById(lineResponse.getId());
-        assertThat(line.get().getSections().getStations().stream().map(station -> station.getName()).collect(
+        Line line = lineRepository.findById(lineResponse.getId()).orElseThrow(()-> new IllegalArgumentException("테스트에 노선 찾기 실패"));
+        assertThat(line.getSections().getStations().stream().map(station -> station.getName()).collect(
             Collectors.toList())).containsExactly("첫번째역","두번째역","세번째역");
+        // 여기 줄줄이 delimiter law 어겼음
     }
 }
