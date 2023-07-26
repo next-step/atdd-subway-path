9ae74a90002b411657095f6233c2c5123964c466
kim jisu <jisu3316@naver.com>
2023-07-26 20:28:24 +0900

    Revert "충돌해결"
    This reverts commit 46c7249c880c7fc24e90eb5a8087b792d29bf3a4.

--- src/test/java/nextstep/subway/unit/SectionServiceTest.java
+++ src/test/java/nextstep/subway/unit/SectionServiceTest.java
@@ -1,7 +1,5 @@
 package nextstep.subway.unit;
 
-import nextstep.subway.exception.ErrorCode;
-import nextstep.subway.exception.SubwayException;
 import nextstep.subway.line.entity.Line;
 import nextstep.subway.line.repository.LineRepository;
 import nextstep.subway.section.dto.SectionDto;
@@ -9,17 +7,15 @@ import nextstep.subway.section.entity.Section;
 import nextstep.subway.section.service.SectionService;
 import nextstep.subway.station.entity.Station;
 import nextstep.subway.station.repository.StationRepository;
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
@@ -42,116 +38,38 @@ public class SectionServiceTest {
         이수역 = stationRepository.save(이수역());
         사당역 = stationRepository.save(사당역());
         line = lineRepository.save(line(당고개역, 이수역));
+        sectionDto = sectionDto(이수역.getId(), 사당역.getId());
     }
 
-    @DisplayName("구간을 추가한다. - 역 사이에 새로운 역을 등록할 경우")
+    @DisplayName("구간을 추가한다.")
     @Test
     void addSection() {
-        // given
-        sectionDto = sectionDto(당고개역.getId(), 사당역.getId(), 3);
-
         // when
         sectionService.addSection(line.getId(), sectionDto);
 
         // then
-        assertThat(line.getStations()).hasSize(3)
-                .extracting("name")
-                .containsExactly(
-                        "당고개역", "사당역", "이수역"
+        assertThat(line.getSections()).hasSize(2)
+                .extracting("upStation.name", "downStation.name")
+                .containsExactlyInAnyOrder(
+                        Tuple.tuple("당고개역", "이수역"),
+                        Tuple.tuple("이수역", "사당역")
                 );
-    }
-
-    @DisplayName("구간을 추가한다. - 새로운 역을 상행 종점으로 등록할 경우")
-    @Test
-    void addSection2() {
-        // given
-        sectionDto = sectionDto(사당역.getId(), 당고개역.getId(), 3);
-
-        // when
-        sectionService.addSection(line.getId(), sectionDto);
-
-        // then
-        assertThat(line.getStations()).hasSize(3)
-                .extracting("name")
-                .containsExactly(
-                        "사당역", "당고개역", "이수역"
-                );
-    }
-
-    @DisplayName("구간을 추가한다. - 새로운역을 하행 종점으로 등록할 경우")
-    @Test
-    void addSection3() {
-        // given
-        sectionDto = sectionDto(이수역.getId(), 사당역.getId(), 3);
-
-        // when
-        sectionService.addSection(line.getId(), sectionDto);
-
-        // then
-        assertThat(line.getStations()).hasSize(3)
-                .extracting("name")
-                .containsExactly(
-                        "당고개역", "이수역", "사당역"
-                );
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {10, 11})
-    @DisplayName("역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같을때 예외 발생")
-    void addSectionThrowExceptionIsINVALID_DISTANCE(int distance) {
-        // given
-        sectionDto = sectionDto(당고개역.getId(), 사당역.getId(), distance);
-
-        // when then
-        assertThatThrownBy(() -> sectionService.addSection(line.getId(), sectionDto))
-                .isInstanceOf(SubwayException.class)
-                .hasMessageContaining(ErrorCode.INVALID_DISTANCE.getMessage());
-    }
-
-    @DisplayName("새로운 구간을 등록할 시 상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음")
-    @Test
-    void addSectionThrowExceptionIsALREADY_SECTION() {
-        // given
-        sectionDto = sectionDto(당고개역.getId(), 사당역.getId(), 3);
-        sectionService.addSection(line.getId(), sectionDto);
-
-        // when then
-        assertThatThrownBy(() -> sectionService.addSection(line.getId(), sectionDto))
-                .isInstanceOf(SubwayException.class)
-                .hasMessageContaining(ErrorCode.ALREADY_SECTION.getMessage());
-    }
-
-    @DisplayName("상행역과 하행역 둘 중 하나도 포함되어있지 않으면 추가할 수 없음")
-    @Test
-    void addSectionThrowExceptionIsCAN_NOT_BE_ADDED_SECTION() {
-        // given
-        sectionDto = sectionDto(당고개역.getId(), 사당역.getId(), 3);
-        sectionService.addSection(line.getId(), sectionDto);
-        Station 동작역 = stationRepository.save(동작역());
-        Station 이촌역 = stationRepository.save(이촌역());
-        sectionDto = sectionDto(동작역.getId(), 이촌역.getId(), 3);
-
-        // when  then
-        assertThatThrownBy(() -> sectionService.addSection(line.getId(), sectionDto))
-                .isInstanceOf(SubwayException.class)
-                .hasMessageContaining(ErrorCode.CAN_NOT_BE_ADDED_SECTION.getMessage());
     }
 
     @DisplayName("구간을 제거한다.")
     @Test
     void removeSection() {
         // given : 선행조건 기술
-        sectionDto = sectionDto(당고개역.getId(), 사당역.getId(), 3);
         sectionService.addSection(line.getId(), sectionDto);
 
         // when : 기능 수행
-        sectionService.removeSection(line.getId(), 이수역.getId());
+        sectionService.removeSection(line.getId(), 사당역.getId());
 
         // then : 결과 확인
-        assertThat(line.getStations()).hasSize(2)
-                .extracting("name")
-                .containsExactly(
-                        "당고개역", "사당역"
+        assertThat(line.getSections()).hasSize(1)
+                .extracting("upStation.name", "downStation.name")
+                .containsExactlyInAnyOrder(
+                        Tuple.tuple("당고개역", "이수역")
                 );
     }
 
@@ -178,14 +96,6 @@ public class SectionServiceTest {
         return new Station("사당역");
     }
 
-    private Station 동작역() {
-        return new Station("동작역");
-    }
-
-    private Station 이촌역() {
-        return new Station("이촌역");
-    }
-
     private Section section(Station upStation, Station downStation) {
         return Section.builder()
                 .upStation(upStation)
@@ -194,11 +104,11 @@ public class SectionServiceTest {
                 .build();
     }
 
-    private SectionDto sectionDto(Long upStationId, Long downStationId, int distance) {
+    private SectionDto sectionDto(Long upStationId, Long downStationId) {
         return SectionDto.builder()
                 .upStationId(upStationId)
                 .downStationId(downStationId)
-                .distance(distance)
+                .distance(10)
                 .build();
     }
 }
