9268991ec317e94cdff50564e84d6a51acc8f2ba
devk0ng <kongbean@kakao.com>
2023-07-27 22:05:40 +0900

    controller 구현

---
+++ src/main/java/nextstep/subway/ui/PathController.java
@@ -0,0 +1,27 @@
+package nextstep.subway.ui;
+
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.StationResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/paths")
+public class PathController {
+    private PathService pathService;
+
+    public PathController(PathService pathService) {
+        this.pathService = pathService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StationResponse>> getPath(@RequestParam("source") Long sourceId,
+                                                         @RequestParam("targetId") Long targetId) {
+        return ResponseEntity.ok().body(pathService.getPath(sourceId, targetId));
+    }
+}
