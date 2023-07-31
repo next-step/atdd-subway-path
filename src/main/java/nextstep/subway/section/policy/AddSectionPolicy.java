0ade11e79ae6972eedd964466508a436b778946c
George <oh980225@gmail.com>
2023-07-31 17:22:39 +0900

    feat: 구간 추가 정책 구현
    구간 추가시, 추가하려는 구간의 상행역, 하행역이 이미 모두 노선 내 구간에 등록되어 있는지 확인하는 로직과,
    모두 노선 내 없는지 확인하는 로직을 구현하고 모두 테스트를 통과하는지 확인하였습니다.

--- src/main/java/nextstep/subway/section/policy/AddSectionPolicy.java
+++ src/main/java/nextstep/subway/section/policy/AddSectionPolicy.java
@@ -9,14 +9,38 @@ import java.util.Optional;
 
 public class AddSectionPolicy {
     public static void validate(Sections sections, Section section) {
+        Optional<Station> upStation = sections.getAllStation().stream()
+                .filter(station -> Objects.equals(station, section.getUpStation()))
+                .findFirst();
+        Optional<Station> downStation = sections.getAllStation().stream()
+                .filter(station -> Objects.equals(station, section.getDownStation()))
+                .findFirst();
         Optional<Section> sectionEqualUpStation = sections.getSectionByUpStation(section.getUpStation());
         Optional<Section> sectionEqualDownStation = sections.getSectionByDownStation(section.getDownStation());
 
+        checkRegisteredStation(upStation, downStation);
+        checkNotRegisteredStation(upStation, downStation);
+        checkSectionDistance(section, sectionEqualUpStation, sectionEqualDownStation);
+    }
+
+    private static void checkSectionDistance(Section section, Optional<Section> sectionEqualUpStation, Optional<Section> sectionEqualDownStation) {
         if (
                 (sectionEqualUpStation.isPresent() && (sectionEqualUpStation.get().getDistance() <= section.getDistance()))
                 ||(sectionEqualDownStation.isPresent() && (sectionEqualDownStation.get().getDistance() <= section.getDistance()))
         ) {
             throw new RuntimeException("Section's distance too long");
+        }
+    }
+
+    private static void checkNotRegisteredStation(Optional<Station> upStation, Optional<Station> downStation) {
+        if (upStation.isEmpty() && downStation.isEmpty()) {
+            throw new RuntimeException("Section's stations not exist in sections");
+        }
+    }
+
+    private static void checkRegisteredStation(Optional<Station> upStation, Optional<Station> downStation) {
+        if (upStation.isPresent() && downStation.isPresent()) {
+            throw new RuntimeException("Section's stations already registered");
         }
     }
 }
