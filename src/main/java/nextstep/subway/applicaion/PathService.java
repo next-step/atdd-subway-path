f6ef53cc8ce7b290b44a03ea772a27ff54663e57
devk0ng <kongbean@kakao.com>
2023-07-31 18:01:50 +0900

    stream 로직 메소드로 추출

--- src/main/java/nextstep/subway/applicaion/PathService.java
+++ src/main/java/nextstep/subway/applicaion/PathService.java
@@ -3,12 +3,9 @@ package nextstep.subway.applicaion;
 import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.*;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
@@ -23,17 +20,23 @@ public class PathService {
         this.stationRepository = stationRepository;
     }
 
+    @Transactional(readOnly = true)
     public PathResponse getPath(Long sourceId, Long targetId) {
         List<Line> lines = lineRepository.findAll();
         List<Long> shortestPath = new Path(lines).getShortestPath(sourceId, targetId);
 
+        List<StationResponse> stationResponses = convertToStationResponses(shortestPath);
+
+        return new PathResponse(stationResponses);
+    }
+
+    private List<StationResponse> convertToStationResponses(List<Long> shortestPath) {
         List<StationResponse> stationResponses = shortestPath.stream()
                 .map(stationId -> {
                     Station station = stationRepository.findById(stationId).orElseThrow(NoSuchElementException::new);
 
                     return new StationResponse(station.getId(), station.getName());
                 }).collect(Collectors.toList());
-
-        return new PathResponse(stationResponses);
+        return stationResponses;
     }
 }
