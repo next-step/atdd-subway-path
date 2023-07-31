438357b3543e8bc0278153c9be2974345c3dfc45
SeokyungKim <kskyung0624@gmail.com>
2023-07-31 15:49:13 +0900

    test : findShortestRoute()에 대한 검증 테스트를 작성한다

--- src/test/java/nextstep/subway/section/RouteFinderTest.java
+++ src/test/java/nextstep/subway/section/RouteFinderTest.java
@@ -5,19 +5,35 @@ import static common.Constants.양재역;
 import static common.Constants.판교역;
 import static nextstep.subway.section.SectionBuilder.aSection;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import nextstep.subway.common.exception.BusinessException;
 import nextstep.subway.station.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RouteFinderTest {
 
-    @DisplayName("시작역에서 목적역까지의 최단거리를 검색해 해당 경로만 가지는 Sections를 반환한다")
+    @DisplayName("findShortestRoute() : 출발역과 도착역이 같으면 예외를 발생시킨다")
+    @Test
+    void RouteFinder_fail_sameSourceAndDestination() {
+        Section section = aSection()
+            .withStations(new Station(1L, 강남역), new Station(2L, 양재역))
+            .build();
+
+        RouteFinder routeFinder = RouteFinder.from(List.of(section));
+
+        assertThatThrownBy(
+            () -> routeFinder.findShortestRoute(new Station(1L, 강남역), new Station(1L, 강남역)))
+            .isInstanceOf(BusinessException.class)
+            .hasMessageContaining("출발역과 도착역은 같을 수 없습니다");
+    }
+
+    @DisplayName("findShortestRoute() : 시작역에서 목적역까지의 최단거리를 검색해 해당 경로만 가지는 Sections를 반환한다")
     @Test
     void findShortestRoute() {
         // given
-        // TODO: 안되면 역 객체 추출해서 해보기
         Section section1 = aSection()
             .withStations(new Station(1L, 강남역), new Station(2L, 양재역))
             .build();
@@ -30,11 +46,28 @@ class RouteFinderTest {
             .build();
 
         // when
-        RouteFinder routeFinder = new RouteFinder(List.of(section1, section2, section3), new Station(1L, 강남역), new Station(3L, 판교역));
+        RouteFinder routeFinder = RouteFinder.from(List.of(section1, section2, section3));
 
         // then
-        assertThat(routeFinder.totalDistance()).isEqualTo(20);
-        assertThat(routeFinder.findShortestRoute()).containsExactly(new Station(1L, 강남역), new Station(2L, 양재역), new Station(3L, 판교역));
+        assertThat(routeFinder.totalDistance(new Station(1L, 강남역), new Station(3L, 판교역)))
+            .isEqualTo(20);
+        assertThat(routeFinder.findShortestRoute(new Station(1L, 강남역), new Station(3L, 판교역)))
+            .containsExactly(
+                new Station(1L, 강남역), new Station(2L, 양재역), new Station(3L, 판교역));
     }
 
+    @DisplayName("findShortestRoute() : 출발역과 도착역이 연결되지 않았다면 예외를 발생시킨다")
+    @Test
+    void findShortestRoute_fail_unconnected() {
+        Section section1 = aSection()
+            .withStations(new Station(1L, 강남역), new Station(2L, 양재역))
+            .build();
+
+        RouteFinder routeFinder = RouteFinder.from(List.of(section1));
+
+        assertThatThrownBy(
+            () -> routeFinder.findShortestRoute(new Station(1L, 강남역), new Station(3L, 판교역)))
+            .isInstanceOf(BusinessException.class)
+            .hasMessageContaining("출발역과 도착역이 연결되지 않았습니다");
+    }
 }
