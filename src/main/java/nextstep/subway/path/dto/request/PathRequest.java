66bb65c2842d2c1157ca48618ac473ff7b7b52fc
dev-yoonjung <dear__yoon@naver.com>
2023-07-31 14:08:41 +0900

    feat: get shortest distance path

--- src/main/java/nextstep/subway/path/dto/request/PathRequest.java
+++ src/main/java/nextstep/subway/path/dto/request/PathRequest.java
@@ -1,4 +1,22 @@
 package nextstep.subway.path.dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
 public class PathRequest {
+
+    @NotNull(message = "출발역은 반드시 입력해야 합니다.")
+    private Long source;
+
+    @NotNull(message = "도착역은 반드시 입력해야 합니다.")
+    private Long target;
+
+    public boolean isSameSourceAndTarget() {
+        return source.equals(target);
+    }
+
 }
