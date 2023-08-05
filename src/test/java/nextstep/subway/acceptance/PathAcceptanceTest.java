34f7ea0c0d20d1f96f527defdb605acccfed08d5
pawoo0211 <pawoo0211@naver.com>
2023-08-05 11:04:42 +0900

    [3단계] 경로 조회 기능 인수 테스트 코드 작성

---
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -0,0 +1,94 @@
+package nextstep.subway.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static nextstep.subway.handler.LineHandler.지하철_노선_생성_요청;
+import static nextstep.subway.handler.LineHandler.지하철_노선에_지하철_구간_생성_요청;
+import static nextstep.subway.handler.StationHandler.지하철역_생성_요청;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PathAcceptanceTest extends AcceptanceTest {
+
+    private Long 신분당선;
+    private Long 강남역;
+    private Long 양재역;
+
+    /**
+     * Given 지하철역과 노선 생성을 요청 하고
+     */
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        강남역 = 지하철역_생성_요청("강남역").jsonPath().getLong("id");
+        양재역 = 지하철역_생성_요청("양재역").jsonPath().getLong("id");
+
+        Map<String, String> lineCreateParams = createLineCreateParams(강남역, 양재역);
+        신분당선 = 지하철_노선_생성_요청(lineCreateParams).jsonPath().getLong("id");
+    }
+
+
+    /**
+     * Given 2개의 지하철 역을 추가하고
+     * When 출발역과 도착역의 경로를 조회하면
+     * Then 두 역 사이의 경로에 있는 역 목록이 조회된다.
+     */
+    @DisplayName("출발역과 도착역 사이에 있는 역 목록을 조회한다.")
+    @Test
+    void findPath() {
+        // given
+        Long 정자역 = 지하철역_생성_요청("정자역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(양재역, 정자역));
+        Long 판교역 = 지하철역_생성_요청("판교역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(신분당선, createSectionCreateParams(정자역, 판교역));
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .queryParam("target", 3)
+                .when().log().all()
+                .get("/paths?source={source}", 1)
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역, 양재역, 정자역);
+    }
+
+    /**
+     * TODO
+     * - "LineHandler"로 추출
+     */
+    private Map<String, String> createLineCreateParams(Long upStationId, Long downStationId) {
+        Map<String, String> lineCreateParams;
+        lineCreateParams = new HashMap<>();
+        lineCreateParams.put("name", "신분당선");
+        lineCreateParams.put("color", "bg-red-600");
+        lineCreateParams.put("upStationId", upStationId + "");
+        lineCreateParams.put("downStationId", downStationId + "");
+        lineCreateParams.put("distance", 10 + "");
+        return lineCreateParams;
+    }
+
+    /**
+     * TODO
+     * - "SectionHandler"로 추출
+     */
+    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId) {
+        Map<String, String> params = new HashMap<>();
+        params.put("upStationId", upStationId + "");
+        params.put("downStationId", downStationId + "");
+        params.put("distance", 6 + "");
+        return params;
+    }
+}
