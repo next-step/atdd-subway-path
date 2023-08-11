e1c84ddbd431c8164ecc2fc0d08a87f1f6023d0c
toongri <jhkim03284@gmail.com>
2023-08-11 14:36:25 +0900

    경로 탐색 추상화
    PathSearcher 정의
    PathSearcher/search 함수 정의

---
+++ src/main/java/subway/domain/PathSearcher.java
@@ -0,0 +1,16 @@
+package subway.domain;
+
+/**
+ * 지하철 경로 탐색 인터페이스
+ */
+public interface PathSearcher {
+
+    /**
+     * 지하철 경로 탐색을 합니다.
+     * @param source 출발역
+     * @param target 도착역
+     * @return 경로 탐색 결과
+     */
+    PathSubway search(PathStation source, PathStation target);
+
+}
