2e47d3702bef33b49156080eed8c85dc4975913f
toongri <jhkim03284@gmail.com>
2023-09-03 17:47:37 +0900

    경로 최단거리 탐색 기능 테스트 수정
    PathShortestDistanceSearcherTest 리팩토링(경로구간 생성 함수 추가, setUp함수 추가)

--- src/test/java/subway/domain/PathShortestDistanceSearcherTest.java
+++ src/test/java/subway/domain/PathShortestDistanceSearcherTest.java
@@ -1,5 +1,6 @@
 package subway.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import subway.common.annotation.UnitTest;
@@ -17,6 +18,26 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 @DisplayName("노선 최단 거리 탐색에 대한 도메인 단위 테스트")
 class PathShortestDistanceSearcherTest {
 
+    PathStation 서울대입구역;
+    PathStation 강남역;
+    PathStation 성수역;
+    PathStation 건대입구역;
+    @BeforeEach
+    void setUp() {
+        //given
+        PathStation.Id 서울대입구역Id = PathStation.Id.of(1L);
+        서울대입구역 = PathStation.of(서울대입구역Id, "서울대입구역");
+
+        PathStation.Id 강남역Id = PathStation.Id.of(2L);
+        강남역 = PathStation.of(강남역Id, "강남역");
+
+        PathStation.Id 성수역Id = PathStation.Id.of(3L);
+        성수역 = PathStation.of(성수역Id, "성수역");
+
+        PathStation.Id 건대입구역Id = PathStation.Id.of(4L);
+        건대입구역 = PathStation.of(건대입구역Id, "건대입구역");
+    }
+
     /**
      * @given A, B, C, D 역이 존재하고
      * @given 탑승역이 A역, 하차역이 B역, 거리가 5인 구간이 존재하고
@@ -29,50 +50,18 @@ class PathShortestDistanceSearcherTest {
     @Test
     @DisplayName("두 경로 이상 중 총 거리가 더 적은 경로로 갈 수 있다.")
     void searchPathBetweenDifferentPaths() {
-        //given
-        PathStation.Id 서울대입구역Id = PathStation.Id.of(1L);
-        PathStation 서울대입구역 = PathStation.of(서울대입구역Id, "서울대입구역");
-
-        PathStation.Id 강남역Id = PathStation.Id.of(2L);
-        PathStation 강남역 = PathStation.of(강남역Id, "강남역");
-
-        PathStation.Id 성수역Id = PathStation.Id.of(3L);
-        PathStation 성수역 = PathStation.of(성수역Id, "성수역");
-
-        PathStation.Id 건대입구역Id = PathStation.Id.of(4L);
-        PathStation 건대입구역 = PathStation.of(건대입구역Id, "건대입구역");
 
         //given
-        PathSection.Id firstSectionId = PathSection.Id.of(1L);
-        PathSection firstSection = PathSection.of(
-                firstSectionId,
-                서울대입구역,
-                강남역,
-                Kilometer.of(5));
+        PathSection firstSection = createPathSection(1L, 서울대입구역, 강남역, Kilometer.of(5));
 
         //given
-        PathSection.Id secondSectionId = PathSection.Id.of(2L);
-        PathSection secondSection = PathSection.of(
-                secondSectionId,
-                강남역,
-                성수역,
-                Kilometer.of(12));
+        PathSection secondSection = createPathSection(2L, 강남역, 성수역, Kilometer.of(12));
 
         //given
-        PathSection.Id thirdSectionId = PathSection.Id.of(3L);
-        PathSection thirdSection = PathSection.of(
-                thirdSectionId,
-                서울대입구역,
-                건대입구역,
-                Kilometer.of(5));
+        PathSection thirdSection = createPathSection(3L, 서울대입구역, 건대입구역, Kilometer.of(5));
 
         //given
-        PathSection.Id fourthSectionId = PathSection.Id.of(4L);
-        PathSection fourthSection = PathSection.of(
-                fourthSectionId,
-                건대입구역,
-                성수역,
-                Kilometer.of(8));
+        PathSection fourthSection = createPathSection(4L, 건대입구역, 성수역, Kilometer.of(8));
 
         //when
         PathSearcher pathSearcher = PathShortestDistanceSearcher.from(List.of(firstSection, secondSection, thirdSection, fourthSection));
@@ -88,7 +77,6 @@ class PathShortestDistanceSearcherTest {
     }
 
     /**
-     * @given A, B, C, D 역이 존재하고
      * @given 탑승역이 A역, 하차역이 B역, 거리가 5인 구간이 존재하고
      * @given 탑승역이 B역, 하차역이 C역, 거리가 8인 구간이 존재하고
      * @given 탑승역이 C역, 하차역이 D역, 거리가 12인 구간이 존재한다고 하면
@@ -99,41 +87,13 @@ class PathShortestDistanceSearcherTest {
     @DisplayName("서로 다른 두 노선을 환승하여 최단 거리로 갈 수 있다.")
     void searchPathBetweenTwoLines() {
         //given
-        PathStation.Id 서울대입구역Id = PathStation.Id.of(1L);
-        PathStation 서울대입구역 = PathStation.of(서울대입구역Id, "서울대입구역");
-
-        PathStation.Id 강남역Id = PathStation.Id.of(2L);
-        PathStation 강남역 = PathStation.of(강남역Id, "강남역");
-
-        PathStation.Id 성수역Id = PathStation.Id.of(3L);
-        PathStation 성수역 = PathStation.of(성수역Id, "성수역");
-
-        PathStation.Id 건대입구역Id = PathStation.Id.of(4L);
-        PathStation 건대입구역 = PathStation.of(건대입구역Id, "건대입구역");
+        PathSection firstSection = createPathSection(1L, 서울대입구역, 강남역, Kilometer.of(5));
 
         //given
-        PathSection.Id firstSectionId = PathSection.Id.of(1L);
-        PathSection firstSection = PathSection.of(
-                firstSectionId,
-                서울대입구역,
-                강남역,
-                Kilometer.of(5));
+        PathSection secondSection = createPathSection(2L, 강남역, 성수역, Kilometer.of(8));
 
         //given
-        PathSection.Id secondSectionId = PathSection.Id.of(2L);
-        PathSection secondSection = PathSection.of(
-                secondSectionId,
-                강남역,
-                성수역,
-                Kilometer.of(8));
-
-        //given
-        PathSection.Id thirdSectionId = PathSection.Id.of(3L);
-        PathSection thirdSection = PathSection.of(
-                thirdSectionId,
-                성수역,
-                건대입구역,
-                Kilometer.of(12));
+        PathSection thirdSection = createPathSection(3L, 성수역, 건대입구역, Kilometer.of(12));
 
         //when
         PathSearcher pathSearcher = PathShortestDistanceSearcher.from(List.of(firstSection, secondSection, thirdSection));
@@ -149,7 +109,6 @@ class PathShortestDistanceSearcherTest {
     }
 
     /**
-     * @given A, B, C, D 역이 존재하고
      * @given 탑승역이 A역, 하차역이 B역, 거리가 5인 구간이 존재하고
      * @given 탑승역이 B역, 하차역이 C역, 거리가 12인 구간이 존재하고
      * @given 탑승역이 B역, 하차역이 C역, 거리가 8인 구간이 존재하고
@@ -161,49 +120,16 @@ class PathShortestDistanceSearcherTest {
     @DisplayName("중복되는 구간이 있다면 그 중 더 짧은 거리로 갈 수 있는 노선을 선택하여 최단 거리로 갈 수 있다.")
     public void searchPathBetweenDuplicatedSection() {
         //given
-        PathStation.Id 서울대입구역Id = PathStation.Id.of(1L);
-        PathStation 서울대입구역 = PathStation.of(서울대입구역Id, "서울대입구역");
-
-        PathStation.Id 강남역Id = PathStation.Id.of(2L);
-        PathStation 강남역 = PathStation.of(강남역Id, "강남역");
-
-        PathStation.Id 성수역Id = PathStation.Id.of(3L);
-        PathStation 성수역 = PathStation.of(성수역Id, "성수역");
-
-        PathStation.Id 건대입구역Id = PathStation.Id.of(4L);
-        PathStation 건대입구역 = PathStation.of(건대입구역Id, "건대입구역");
+        PathSection firstSection = createPathSection(1L, 서울대입구역, 강남역, Kilometer.of(5));
 
         //given
-        PathSection.Id firstSectionId = PathSection.Id.of(1L);
-        PathSection firstSection = PathSection.of(
-                firstSectionId,
-                서울대입구역,
-                강남역,
-                Kilometer.of(5));
+        PathSection secondSection = createPathSection(2L, 강남역, 건대입구역, Kilometer.of(12));
 
         //given
-        PathSection.Id secondSectionId = PathSection.Id.of(2L);
-        PathSection secondSection = PathSection.of(
-                secondSectionId,
-                강남역,
-                건대입구역,
-                Kilometer.of(12));
+        PathSection thirdSection = createPathSection(3L, 강남역, 건대입구역, Kilometer.of(8));
 
         //given
-        PathSection.Id thirdSectionId = PathSection.Id.of(3L);
-        PathSection thirdSection = PathSection.of(
-                thirdSectionId,
-                강남역,
-                건대입구역,
-                Kilometer.of(8));
-
-        //given
-        PathSection.Id fourthSectionId = PathSection.Id.of(4L);
-        PathSection fourthSection = PathSection.of(
-                fourthSectionId,
-                건대입구역,
-                성수역,
-                Kilometer.of(12));
+        PathSection fourthSection = createPathSection(4L, 건대입구역, 성수역, Kilometer.of(12));
 
         //when
         PathSearcher pathSearcher = PathShortestDistanceSearcher.from(List.of(firstSection, secondSection, thirdSection, fourthSection));
@@ -219,7 +145,6 @@ class PathShortestDistanceSearcherTest {
     }
 
     /**
-     * @given A, B, C역이 존재하고
      * @given 탑승역이 A역, 하차역이 B역인 구간이 존재하고
      * @given 탑승역이 B역, 하차역이 C역인 구간이 존재하고
      * @given 탑승역이 C역, 하차역이 A역인 구간이 존재한다고 하면
@@ -230,38 +155,13 @@ class PathShortestDistanceSearcherTest {
     @DisplayName("같은 역을 기준으로 경로탐색을 할 수 없다.")
     public void cantSearchPathBetweenSameStation() {
         //given
-        PathStation.Id 서울대입구역Id = PathStation.Id.of(1L);
-        PathStation 서울대입구역 = PathStation.of(서울대입구역Id, "서울대입구역");
-
-        PathStation.Id 강남역Id = PathStation.Id.of(2L);
-        PathStation 강남역 = PathStation.of(강남역Id, "강남역");
-
-        PathStation.Id 건대입구역Id = PathStation.Id.of(4L);
-        PathStation 건대입구역 = PathStation.of(건대입구역Id, "건대입구역");
+        PathSection firstSection = createPathSection(1L, 서울대입구역, 강남역, Kilometer.of(5));
 
         //given
-        PathSection.Id firstSectionId = PathSection.Id.of(1L);
-        PathSection firstSection = PathSection.of(
-                firstSectionId,
-                서울대입구역,
-                강남역,
-                Kilometer.of(5));
+        PathSection secondSection = createPathSection(2L, 강남역, 건대입구역, Kilometer.of(12));
 
         //given
-        PathSection.Id secondSectionId = PathSection.Id.of(2L);
-        PathSection secondSection = PathSection.of(
-                secondSectionId,
-                강남역,
-                건대입구역,
-                Kilometer.of(12));
-
-        //given
-        PathSection.Id thirdSectionId = PathSection.Id.of(3L);
-        PathSection thirdSection = PathSection.of(
-                thirdSectionId,
-                건대입구역,
-                서울대입구역,
-                Kilometer.of(8));
+        PathSection thirdSection = createPathSection(3L, 건대입구역, 서울대입구역, Kilometer.of(8));
 
         //when
         PathSearcher pathSearcher = PathShortestDistanceSearcher.from(List.of(firstSection, secondSection, thirdSection));
@@ -285,34 +185,10 @@ class PathShortestDistanceSearcherTest {
     @DisplayName("서로 이어지지 않은 두 역을 기준으로 경로탐색을 할 수 없다.")
     public void cantSearchPathBetweenNotConnectedStations() {
         //given
-
-        PathStation.Id 서울대입구역Id = PathStation.Id.of(1L);
-        PathStation 서울대입구역 = PathStation.of(서울대입구역Id, "서울대입구역");
-
-        PathStation.Id 강남역Id = PathStation.Id.of(2L);
-        PathStation 강남역 = PathStation.of(강남역Id, "강남역");
-
-        PathStation.Id 성수역Id = PathStation.Id.of(3L);
-        PathStation 성수역 = PathStation.of(성수역Id, "성수역");
-
-        PathStation.Id 건대입구역Id = PathStation.Id.of(4L);
-        PathStation 건대입구역 = PathStation.of(건대입구역Id, "건대입구역");
+        PathSection firstSection = createPathSection(1L, 서울대입구역, 강남역, Kilometer.of(5));
 
         //given
-        PathSection.Id firstSectionId = PathSection.Id.of(1L);
-        PathSection firstSection = PathSection.of(
-                firstSectionId,
-                서울대입구역,
-                강남역,
-                Kilometer.of(5));
-
-        //given
-        PathSection.Id secondSectionId = PathSection.Id.of(2L);
-        PathSection secondSection = PathSection.of(
-                secondSectionId,
-                성수역,
-                건대입구역,
-                Kilometer.of(12));
+        PathSection secondSection = createPathSection(2L, 성수역, 건대입구역, Kilometer.of(12));
 
         //when
         PathSearcher pathSearcher = PathShortestDistanceSearcher.from(List.of(firstSection, secondSection));
@@ -323,5 +199,14 @@ class PathShortestDistanceSearcherTest {
         assertThat(throwable)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("경로를 찾을 수 없습니다.");
+    }
+
+    private PathSection createPathSection(Long id, PathStation upStation, PathStation downStation, Kilometer distance) {
+        PathSection.Id domainId = PathSection.Id.of(id);
+        return PathSection.of(
+                domainId,
+                upStation,
+                downStation,
+                distance);
     }
 }
