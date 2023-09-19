0cd563bbe6fd6b1261c0f8937fac57c16c0d829c
hoon25 <cchoon95@hanmail.net>
2023-09-19 22:53:39 +0900

    test(path): 경로탐색 ATDD HappyCase구현

--- src/test/java/nextstep/subway/fixture/LineFixture.java
+++ src/test/java/nextstep/subway/fixture/LineFixture.java
@@ -7,9 +7,11 @@ public class LineFixture {
     public static final String 신분당선_이름 = "신분당선";
     public static final String 분당선_이름 = "분당선";
     public static final String 이호선_이름 = "이호선";
+    public static final String 삼호선_이름 = "삼호선";
     public static final String 신분당선_색 = "RED";
     public static final String 분당선_색 = "Yellow";
     public static final String 이호선_색 = "Green";
+    public static final String 삼호선_색 = "Orange";
     public static final Integer 거리_10 = 10;
     public static final Integer 거리_04 = 4;
 
