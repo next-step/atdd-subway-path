79484849209658408c544e40afe37fa748617432
jikimee64 <jikimee64@gmail.com>
2024-02-04 03:36:13 +0900

    test: 구간 서비스 단위 테스트 without Mock

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,12 +1,17 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.applicaion.dto.SectionRequest;
+import nextstep.subway.domain.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 @SpringBootTest
 @Transactional
@@ -23,11 +28,24 @@ public class LineServiceTest {
     void addSection() {
         // given
         // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+        Station 강남역 = stationRepository.save(new Station("강남역"));
+        Station 선릉역 = stationRepository.save(new Station("선릉역"));
+        Line 그린선 = lineRepository.save(new Line(1L, "2호선", "green"));
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(1L,
+                new SectionRequest(
+                        1L, 2L, 10
+                ));
 
         // then
-        // line.getSections 메서드를 통해 검증
+        // line.sections 메서드를 통해 검증
+        List<Section> sections = 그린선.sections();
+        assertThat(sections).hasSize(1)
+                .extracting("line.name", "upStation.name", "downStation.name", "distance")
+                .containsExactly(
+                        tuple("2호선", "강남역", "선릉역", 10)
+                );
     }
 }
