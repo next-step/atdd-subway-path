748477c93985e13360ac7e3b20da4dc68462a1da
jikimee64 <jikimee64@gmail.com>
2024-02-04 03:36:00 +0900

    fix: 런타임 에러 수정

--- src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
@@ -1,19 +1,30 @@
 package nextstep.subway.acceptance;
 
+import io.restassured.RestAssured;
 import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
+import static io.restassured.RestAssured.UNDEFINED_PORT;
+
 @ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
+
+    @LocalServerPort
+    private int port;
+
     @Autowired
     private DatabaseCleanup databaseCleanup;
 
     @BeforeEach
     public void setUp() {
+        if (RestAssured.port == UNDEFINED_PORT) {
+            RestAssured.port = port;
+        }
         databaseCleanup.execute();
     }
 }
