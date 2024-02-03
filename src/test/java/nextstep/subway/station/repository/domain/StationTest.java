d5ef2de1eb5ebd22133e5457b02fbc55dfdc6ab0
Jongmin Yoon <jongmin4943@gmail.com>
2024-02-03 21:15:50 +0900

    refactor: Station Fixture 생성을 위한 StationFactory 추가

--- src/test/java/nextstep/subway/station/repository/domain/StationTest.java
+++ src/test/java/nextstep/subway/station/repository/domain/StationTest.java
@@ -1,7 +1,6 @@
 package nextstep.subway.station.repository.domain;
 
-import nextstep.util.ReflectionUtils;
-import org.assertj.core.api.SoftAssertions;
+import nextstep.subway.common.fixture.StationFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,14 +14,13 @@ class StationTest {
 
     @BeforeEach
     void setUp() {
-        강남역 = new Station("강남역");
-        ReflectionUtils.injectIdField(강남역, 강남역_ID);
+        강남역 = StationFactory.createStation(강남역_ID, "강남역");
     }
 
     @Test
     @DisplayName("Station 클래스를 생성할 수 있다.")
     void createStationTest() {
-        assertSoftly(softly->{
+        assertSoftly(softly -> {
             softly.assertThat(강남역.getId()).isEqualTo(강남역_ID);
             softly.assertThat(강남역.getName()).isEqualTo("강남역");
         });
@@ -31,9 +29,7 @@ class StationTest {
     @Test
     @DisplayName("Station 클래스의 id 가 같다면 동등한 객체이다.")
     void stationEqualsTest() {
-        final Station 선릉역 = new Station("선릉역");
-
-        ReflectionUtils.injectIdField(선릉역, 강남역_ID);
+        final Station 선릉역 = StationFactory.createStation(강남역_ID, "선릉역");
 
         assertThat(강남역).isEqualTo(선릉역);
     }
@@ -41,9 +37,7 @@ class StationTest {
     @Test
     @DisplayName("Station 클래스의 id 가 다르다면 동등하지 않은 객체이다.")
     void stationNotEqualsTest() {
-        final Station 선릉역 = new Station("선릉역");
-
-        ReflectionUtils.injectIdField(선릉역, 강남역_ID + 1);
+        final Station 선릉역 = StationFactory.createStation(강남역_ID + 1L, "선릉역");
 
         assertThat(강남역.notEquals(선릉역)).isTrue();
     }
