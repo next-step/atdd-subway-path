501a85bd46922fdda1ea449391bae41476dfea85
Lee seung chan <dltmdcks702@gmail.com>
2024-02-06 06:29:50 +0900

    rfeat: add validation to entity

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -1,10 +1,18 @@
 package nextstep.subway.applicaion.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class LineRequest {
     private String name;
     private String color;
+    @NotNull
     private Long upStationId;
+    @NotNull
     private Long downStationId;
+
+
+    @Min(1)
     private int distance;
 
     public String getName() {
