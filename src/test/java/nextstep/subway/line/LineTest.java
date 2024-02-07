21f4bcdd9a3d4ad3c6d8ebd63fab6d99e0425ca3
g0rani <pjw0405@naver.com>
2024-02-07 19:36:42 +0900

    test: Line을 기준으로 끝점과 추가되는 구간의 끝점이 추가될때 중간에 구간 추가 test code 작성

--- src/test/java/nextstep/subway/line/LineTest.java
+++ src/test/java/nextstep/subway/line/LineTest.java
@@ -79,7 +79,7 @@ class LineTest {
     }
 
     @Test
-    @DisplayName("생성된 라인에 중간에 구간을 더할 수 있다")
+    @DisplayName("생성된 라인에 중간에 구간을 더할 수 있다 (시작점을 기준으로 일치 시")
     void addSection3() {
         Section newSection = new Section(
                 new Station(1L, StationFixture.강남역),
@@ -103,14 +103,38 @@ class LineTest {
     }
 
     @Test
-    @DisplayName("이미 추가된 구간은 추가 할 수 없다")
+    @DisplayName("생성된 라인에 중간에 구간을 더할 수 있다 (끝점을 기준으로 일치 시")
     void addSection4() {
+        Section newSection = new Section(
+                new Station(3L, StationFixture.교대역),
+                new Station(2L, StationFixture.선릉역),
+                5L);
+        line.addSection(newSection);
+
+        Sections actual = line.getSections();
+        Sections expected = Sections.from(
+                List.of(new Section(new Station(1L, StationFixture.강남역),
+                                new Station(3L, StationFixture.교대역),
+                                5L),
+                        new Section(new Station(3L, StationFixture.교대역),
+                                new Station(2L, StationFixture.선릉역),
+                                5L)));
+        assertThat(actual).isEqualTo(expected);
+
+        Long actualDistance = line.getDistance();
+        Long expectedDistance = 10L;
+        assertThat(actualDistance).isEqualTo(expectedDistance);
+    }
+
+    @Test
+    @DisplayName("이미 추가된 구간은 추가 할 수 없다")
+    void addSection5() {
         assertThrows(IllegalArgumentException.class, () -> line.addSection(createdSection));
     }
 
     @Test
     @DisplayName("중간에 더하는 구간의 길이는 전체 라인의 길이보다 작아야한다")
-    void addSection5() {
+    void addSection6() {
         Section newSection = new Section(
                 new Station(1L, StationFixture.강남역),
                 new Station(3L, StationFixture.교대역),
