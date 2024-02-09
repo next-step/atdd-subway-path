14c41be1fb2ffaea356d08eb011d8e625c072890
yu <yu@reverselab.co.kr>
2024-02-09 22:36:31 +0900

    테스트 코드 추가

---
+++ src/test/java/subway/line/LineServiceTest.java
@@ -0,0 +1,210 @@
+package subway.line;
+
+import static java.util.stream.Collectors.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import subway.dto.line.LineResponse;
+import subway.dto.line.LineUpdateRequest;
+import subway.dto.station.StationRequest;
+import subway.dto.station.StationResponse;
+import subway.fixture.line.LineEntityFixture;
+import subway.fixture.line.LineRequestFixture;
+import subway.fixture.section.SectionEntityFixture;
+import subway.station.Station;
+import subway.station.StationService;
+
+@Transactional
+@SpringBootTest
+class LineServiceTest {
+	private static final String MORE_THAN_20_CHARACTERS = "012345678901234567891";
+
+	@Autowired
+	private LineService lineService;
+
+	@Autowired
+	private StationService stationService;
+
+	@DisplayName("노선을 등록한다.")
+	@Test
+	void successLineSave() {
+		// when
+		Line 신분당선 = 신분당선_생성();
+
+		// then
+		Line expectedLine = lineService.findLineById(신분당선.getId());
+		assertThat(신분당선).usingRecursiveComparison().isEqualTo(expectedLine);
+	}
+
+	@DisplayName("노선의 이름을 변경한다.")
+	@Test
+	void successChangeLineName() {
+		// given
+		Line 신분당선 = 신분당선_생성();
+		LineUpdateRequest request = LineRequestFixture.updateBuilder().build();
+
+		// when
+		lineService.update(신분당선, request);
+
+		// then
+		assertThat(신분당선.getName()).isEqualTo(request.getName());
+	}
+
+	@DisplayName("노선의 이름은 20자를 넘길수 없다.")
+	@Test
+	void failChangeLineName() {
+		// given
+		Line 신분당선 = 신분당선_생성();
+		LineUpdateRequest request = LineRequestFixture
+			.updateBuilder()
+			.name(MORE_THAN_20_CHARACTERS)
+			.build();
+
+		// then
+		assertThatThrownBy(() -> lineService.update(신분당선, request))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("노선의 이름은 공백이 아니거나 20자 이하여야 합니다.");
+	}
+
+	@DisplayName("노선의 색깔을 변경한다.")
+	@Test
+	void successChangeLineColor() {
+		// given
+		Line 신분당선 = 신분당선_생성();
+		LineUpdateRequest request = LineRequestFixture.updateBuilder().build();
+
+		// when
+		lineService.update(신분당선, request);
+
+		// then
+		assertThat(신분당선.getColor()).isEqualTo(request.getColor());
+	}
+
+	@DisplayName("노선의 색깔은 20자를 넘길수 없다.")
+	@Test
+	void failChangeLineColor() {
+		// given
+		Line 신분당선 = 신분당선_생성();
+		LineUpdateRequest request = LineRequestFixture
+			.updateBuilder()
+			.color(MORE_THAN_20_CHARACTERS)
+			.build();
+
+		// then
+		assertThatThrownBy(() -> lineService.update(신분당선, request))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("노선의 색깔은 공백이 아니거나 20자 이하여야 합니다.");
+	}
+
+	@DisplayName("등록된 노선을 조회한다.")
+	@Test
+	void successFindById() {
+		// given
+		Line 신분당선 = 신분당선_생성();
+
+		// when
+		Line expectedLine = lineService.findLineById(신분당선.getId());
+
+		// then
+		assertThat(신분당선).usingRecursiveComparison().isEqualTo(expectedLine);
+	}
+
+	@DisplayName("노선의 구간을 추가한다.")
+	@Test
+	void successAddSection() {
+		// given
+		Line 신분당선 = 신분당선_생성();
+		Station 마지막_정류장 = 정류장_조회(신분당선.getSections().getFinalStation().getId());
+		Station 신논현역 = 정류장_생성("신논현역");
+
+		// when
+		Line actualLine = lineService.addSection(신분당선, 마지막_정류장, 신논현역, 10);
+
+		// then
+		Line expectedLine = lineService.findLineById(신분당선.getId());
+		assertThat(actualLine).usingRecursiveComparison().isEqualTo(expectedLine);
+	}
+
+	@DisplayName("노선의 구간을 삭제한다.")
+	@Test
+	void successDeleteSection() {
+		// given
+		Line 신분당선 = 신분당선_구간_추가();
+		Station 마지막_정류장 = 정류장_조회(신분당선.getSections().getFinalStation().getId());
+
+		// when
+		lineService.deleteSection(신분당선, 마지막_정류장);
+
+		// then
+		List<LineResponse> lines = lineService.lines();
+		assertThat(lines).hasSize(1);
+	}
+
+	@DisplayName("모든 노선을 조회한다.")
+	@Test
+	void successLines() {
+		// given
+		Line 신분당선 = 신분당선_생성();
+		Line 삼호선 = 삼호선_생성();
+
+		// when
+		List<LineResponse> actualLineResponse = lineService.lines();
+
+		// then
+		List<LineResponse> expectedLineResponse = Stream.of(신분당선, 삼호선)
+			.map(LineResponse::of)
+			.collect(toList());
+
+		assertThat(actualLineResponse).usingRecursiveComparison().isEqualTo(expectedLineResponse);
+	}
+
+	private Line 신분당선_생성() {
+		Line 신분당선 = LineEntityFixture.신분당선();
+		Station 강남역 = 정류장_생성("강남역");
+		Station 양재역 = 정류장_생성("양재역");
+		Integer distance = SectionEntityFixture.DISTANCE;
+
+		return lineService.save(신분당선, 강남역, 양재역, distance);
+	}
+
+	private Line 신분당선_구간_추가() {
+		Line 신분당선 = LineEntityFixture.신분당선();
+
+		Station 강남역 = 정류장_생성("강남역");
+		Station 양재역 = 정류장_생성("양재역");
+		Station 논현역 = 정류장_생성("논현역");
+
+		Integer distance = SectionEntityFixture.DISTANCE;
+
+		Line line = lineService.save(신분당선, 강남역, 양재역, distance);
+		line.addSection(양재역, 논현역, distance);
+
+		return line;
+	}
+
+	private Line 삼호선_생성() {
+		Line 삼호선 = LineEntityFixture.삼호선();
+		Station 불광역 = 정류장_생성("불광역");
+		Station 녹번역 = 정류장_생성("녹번역");
+		Integer distance = SectionEntityFixture.DISTANCE;
+
+		return lineService.save(삼호선, 불광역, 녹번역, distance);
+	}
+
+	private Station 정류장_조회(Long id) {
+		return stationService.findStationById(id);
+	}
+
+	private Station 정류장_생성(String name) {
+		StationResponse stationResponse = stationService.saveStation(new StationRequest(name));
+		return stationService.findStationById(stationResponse.getId());
+	}
+}
---
+++ src/test/java/subway/line/LineTest.java
@@ -0,0 +1,166 @@
+package subway.line;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import subway.fixture.line.LineEntityFixture;
+import subway.fixture.section.SectionEntityFixture;
+import subway.fixture.station.StationEntityFixture;
+import subway.station.Station;
+
+@DisplayName("Line 도메인 테스트")
+class LineTest {
+	@DisplayName("노선의 이름은 공백이 아니고 20자 이하이다.")
+	@NullAndEmptySource
+	@ValueSource(strings = {"21자입니다다다다다다다다다다다다다다다다", " "})
+	@ParameterizedTest
+	void failCreateLineName(String name) {
+		String color = "line-color";
+		assertThatThrownBy(() -> new Line(name, color))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("노선의 이름은 공백이 아니거나 20자 이하여야 합니다.");
+	}
+
+	@DisplayName("노선의 색깔은 공백이 아니고 20자 이하이다.")
+	@NullAndEmptySource
+	@ValueSource(strings = {"21자입니다다다다다다다다다다다다다다다다", " "})
+	@ParameterizedTest
+	void failCreateLineColor(String color) {
+		String name = "line-name";
+
+		assertThatThrownBy(() -> new Line(name, color))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("노선의 색깔은 공백이 아니거나 20자 이하여야 합니다.");
+	}
+
+	@DisplayName("노선의 이름은 공백 또는 20자 이하로 변경할 수 없다.")
+	@NullAndEmptySource
+	@ValueSource(strings = {"21자입니다다다다다다다다다다다다다다다다", " "})
+	@ParameterizedTest
+	void failChangeLineName(String name) {
+		Line 신분당선 = LineEntityFixture.신분당선();
+		assertThatThrownBy(() -> 신분당선.changeName(name))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("노선의 이름은 공백이 아니거나 20자 이하여야 합니다.");
+	}
+
+	@DisplayName("노선의 색깔은 공백 또는 20자 이하로 변경할 수 없다.")
+	@NullAndEmptySource
+	@ValueSource(strings = {"21자입니다다다다다다다다다다다다다다다다", " "})
+	@ParameterizedTest
+	void failChangeLineColor(String color) {
+		Line 신분당선 = LineEntityFixture.신분당선();
+		assertThatThrownBy(() -> 신분당선.changeColor(color))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("노선의 색깔은 공백이 아니거나 20자 이하여야 합니다.");
+	}
+
+	@DisplayName("노선의 이름은 공백이 아닌 20자 이하로 변경할 수 있다.")
+	@Test
+	void successChangeName() {
+		// given
+		String changedName = "변경된 노선";
+		Line 신분당선 = LineEntityFixture.신분당선();
+
+		// when
+		신분당선.changeName(changedName);
+
+		// then
+		assertThat(changedName).isEqualTo(신분당선.getName());
+	}
+
+	@DisplayName("노선의 색깔은 공백이 아닌 20자 이하로 변경할 수 있다.")
+	@Test
+	void successChangeColor() {
+		// given
+		String changedColor = "변경된 색깔";
+		Line 신분당선 = LineEntityFixture.신분당선();
+
+		// when
+		신분당선.changeColor(changedColor);
+
+		// then
+		assertThat(changedColor).isEqualTo(신분당선.getColor());
+	}
+
+	@DisplayName("노선에 이미 존재하는 정류장은 다시 추가할 수 없다.")
+	@Test
+	void failAddSection() {
+		// given
+		Line 신분당선_2구간_추가 = LineEntityFixture.신분당선_2구간_추가();
+		Station 양재역 = StationEntityFixture.양재역();
+		Station 논현역 = StationEntityFixture.논현역();
+		Integer distance = SectionEntityFixture.DISTANCE;
+
+		// when
+		// then
+		assertThatThrownBy(() -> 신분당선_2구간_추가.addSection(양재역, 논현역, distance))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("추가 할려는 정류장은 이미 해당 노선에 존재하는 정류장입니다.");
+	}
+
+	@DisplayName("노선에 추가할려는 정류장은 이전 구간의 종점이어야 한다.")
+	@Test
+	void failAddNotFinalStation() {
+		// given
+		Line 신분당선_1구간_추가 = LineEntityFixture.신분당선_1구간_추가();
+		Station 논현역 = StationEntityFixture.논현역();
+		Station 양재역 = StationEntityFixture.양재역();
+		Integer distance = SectionEntityFixture.DISTANCE;
+
+		// when
+		// then
+		assertThatThrownBy(() -> 신분당선_1구간_추가.addSection(논현역, 양재역, distance))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("해당 노선의 마지막 정류장이 아닙니다.");
+	}
+
+	@DisplayName("노선에 구간을 추가할 수 있다.")
+	@Test
+	void successAddSection() {
+		// given
+		Line 신분당선 = LineEntityFixture.신분당선();
+		Station 강남역 = StationEntityFixture.강남역();
+		Station 양재역 = StationEntityFixture.양재역();
+		Integer distance = SectionEntityFixture.DISTANCE;
+
+		// when
+		신분당선.addSection(강남역, 양재역, distance);
+
+		// then
+		assertThat(신분당선).usingRecursiveComparison().isEqualTo(LineEntityFixture.신분당선_1구간_추가());
+	}
+
+	@DisplayName("노선에 두개의 정류장만 존재하는 경우, 정류장을 삭제할 수 없다.")
+	@Test
+	void failRemoveSection() {
+		// given
+		Line 신분당선_1구간_추가 = LineEntityFixture.신분당선_1구간_추가();
+		Station 양재역 = StationEntityFixture.양재역();
+
+		// when
+		// then
+		assertThatThrownBy(() -> 신분당선_1구간_추가.removeFinalStation(양재역))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("해당 노선은 두개의 정류장만 존재 하므로, 삭제할 수 없습니다.");
+	}
+
+	@DisplayName("노선의 마지막 정류장을 삭제할 수 있다.")
+	@Test
+	void successRemoveSection() {
+		// given
+		Line 신분당선_2구간_추가 = LineEntityFixture.신분당선_2구간_추가();
+		Station 논현역 = StationEntityFixture.논현역();
+
+		// when
+		신분당선_2구간_추가.removeFinalStation(논현역);
+
+		// then
+		assertThat(신분당선_2구간_추가.getSections().getSortedStations()).hasSize(2);
+	}
+}
