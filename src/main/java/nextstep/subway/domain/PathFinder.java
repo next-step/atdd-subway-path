b7d21682ed6eec7770c58f3fa587fffb05498238
qwe5507 <apvmffkdls@gmail.com>
2024-02-10 17:32:26 +0900

    test: PathService Mockist 테스트 작성 (1) - 도메인에 대한 설계가 떨어지기 떄문에 서비스 작성 후 협력객체를 Mock으로 구성하여 테스트

---
+++ src/main/java/nextstep/subway/domain/PathFinder.java
@@ -0,0 +1,4 @@
+package nextstep.subway.domain;
+
+public class PathFinder {
+}
---
+++ src/test/java/nextstep/subway/unit/PathFinderTest.java
@@ -0,0 +1,15 @@
+package nextstep.subway.unit;
+
+import nextstep.subway.domain.PathFinder;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PathFinderTest {
+
+    @Test
+    void create() {
+        new PathFinder();
+    }
+
+}
