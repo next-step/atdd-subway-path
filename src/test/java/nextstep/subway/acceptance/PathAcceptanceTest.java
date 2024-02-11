5ab4514b64efa10230b7826c12bac1568010f1b9
Lee seung chan <dltmdcks702@gmail.com>
2024-02-11 19:55:46 +0900

    test: add get path request across two lines

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -72,6 +72,24 @@ public class PathAcceptanceTest extends AcceptanceTest {
         assertThat(distance).isEqualTo(남부터미널_양재_거리);
     }
 
+    /**
+     * Given 공통으로 1개의 역을 가진 2개 노선이 주어진다.
+     * When 두개 노선에 걸친 역의 경로를 조회하면,
+     * Then 경로가 조회 된다.
+     */
+    @Test
+    void 두_노선에_걸친_역_간의_경로를_찾을_수_있다() {
+        // when
+        final ExtractableResponse<Response> response = 지하철_경로_조회(교대역, 양재역);
+
+        // then
+        final List<Station> stations = response.jsonPath().getList("stations", Station.class);
+        final int distance = response.jsonPath().getInt("distance");
+
+        assertThat(stations.stream().mapToLong(Station::getId)).containsAll(List.of(교대역, 남부터미널역, 양재역));
+        assertThat(distance).isEqualTo(교대_남부터미널_거리 + 남부터미널_양재_거리);
+    }
+
     private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, int distance) {
         final Map<String, String> params = new HashMap<>();
 
