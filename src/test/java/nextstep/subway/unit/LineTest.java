c3239f33c4f273ea93453385ca49a7653380f695
jung0930 <dev.jung0930@gmail.com>
2024-02-11 20:06:06 +0900

    test : 구간 조회 단위 테스트 작성

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -10,18 +10,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LineTest {
 
-    Line 신분당선 = Line.of(Constant.신분당선, Constant.빨간색);
-    Station 신논현역 = Station.from(Constant.신논현역);
-    Station 강남역 = Station.from(Constant.강남역);
-    Station 양재역 = Station.from(Constant.양재역);
+    Line 신분당선;
+    Station 논현역;
+    Station 신논현역;
+    Station 강남역;
+    Station 양재역;
 
     @BeforeEach
     protected void setUp() {
+        신분당선 = Line.of(Constant.신분당선, Constant.빨간색);
+        논현역 = Station.from(Constant.논현역);
+        신논현역 = Station.from(Constant.신논현역);
+        강남역 = Station.from(Constant.강남역);
+        양재역 = Station.from(Constant.양재역);
     }
 
     @Test
@@ -42,11 +51,11 @@ class LineTest {
     void 노선의_하행_종점역이_아닌_상행역을_가진_지하철_구간을_등록하면_예외_발생() {
         // given
         Section 신논현역_강남역_구간 = Section.of(신논현역, 강남역, Constant.기본_역_간격);
-        Section 신논현역_양재역_구간 = Section.of(신논현역, 양재역, Constant.기본_역_간격);
+        Section 논현역_양재역_구간 = Section.of(논현역, 양재역, Constant.기본_역_간격);
         신분당선.addSection(신논현역_강남역_구간);
 
         // when & then
-        assertThatThrownBy(() -> 신분당선.addSection(신논현역_양재역_구간))
+        assertThatThrownBy(() -> 신분당선.addSection(논현역_양재역_구간))
                 .isInstanceOf(IsNotLastStationException.class);
     }
 
@@ -64,10 +73,26 @@ class LineTest {
     }
 
     @Test
-    void getStations() {
+    @DisplayName("등록한 역을 조회")
+    void 등록한_역을_조회() {
+        // given
+        Section 신논현역_강남역_구간 = Section.of(신논현역, 강남역, Constant.기본_역_간격);
+        Section 강남역_양재역_구간 = Section.of(강남역, 양재역, Constant.기본_역_간격);
+        신분당선.addSection(신논현역_강남역_구간);
+        신분당선.addSection(강남역_양재역_구간);
+
+        // when
+        List<Station> 신분당선_역_목록 = 신분당선.getStations();
+
+        // then
+        assertAll(
+                () -> assertThat(신분당선_역_목록).contains(신논현역, 강남역, 양재역),
+                () -> assertThat(신분당선_역_목록).doesNotContain(논현역)
+        );
     }
 
     @Test
     void removeSection() {
     }
+
 }
