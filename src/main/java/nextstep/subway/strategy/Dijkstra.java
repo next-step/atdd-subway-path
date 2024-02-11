778ed9fe99bea8488c4edff1773d6bdf1b5b3d22
jikimee64 <jikimee64@gmail.com>
2024-02-11 23:31:58 +0900

    feat: jgrapht 라이브러리 사용

--- src/main/java/nextstep/subway/strategy/Dijkstra.java
+++ src/main/java/nextstep/subway/strategy/Dijkstra.java
@@ -1,20 +1,75 @@
 package nextstep.subway.strategy;
 
+import nextstep.subway.domain.Section;
 import nextstep.subway.domain.ShortestPathType;
+import nextstep.subway.domain.Station;
+import nextstep.subway.exception.ApplicationException;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Objects;
 
 import static nextstep.subway.domain.ShortestPathType.DIJKSTRA;
 
 @Component
 public class Dijkstra implements ShortestPathStrategy {
 
-    @Override
-    public int weight() {
-        return 0;
+    private final DijkstraShortestPath dijkstraShortestPath;
+
+    public Dijkstra(List<Section> sections) {
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        addVertexes(sections, graph);
+        addEdges(sections, graph);
+        dijkstraShortestPath = new DijkstraShortestPath(graph);
+    }
+
+    private void addVertexes(List<Section> sections, WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+        for (Section section : sections) {
+            graph.addVertex(section.upStation());
+            graph.addVertex(section.downStation());
+        }
+    }
+
+    private void addEdges(List<Section> sections, WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+        for (Section section : sections) {
+            graph.setEdgeWeight(graph.addEdge(section.upStation(), section.downStation()), section.distance());
+        }
     }
 
     @Override
-    public boolean supported(ShortestPathType shortestPathType) {
+    public List<Station> findShortestStationPath(Station source, Station target) {
+        GraphPath shortestPath = getPath(source, target);
+        validateExistPath(shortestPath);
+        return shortestPath.getVertexList();
+    }
+
+    @Override
+    public int totalPathDistance(Station source, Station target) {
+        GraphPath shortestPath = getPath(source, target);
+        validateExistPath(shortestPath);
+        return (int) shortestPath.getWeight();
+    }
+
+    private GraphPath getPath(Station source, Station target) {
+        try {
+            return dijkstraShortestPath.getPath(source, target);
+        } catch (IllegalArgumentException e) {
+            throw new ApplicationException("노선에 존재하지 않는 지하철역입니다.");
+        }
+    }
+
+    private void validateExistPath(GraphPath<Station, DefaultWeightedEdge> shortestPath) {
+        if (Objects.isNull(shortestPath)) {
+            throw new ApplicationException("출발역과 도착역이 연결되어 있지 않습니다.");
+        }
+    }
+
+    @Override
+    public boolean support(ShortestPathType shortestPathType) {
         return shortestPathType == DIJKSTRA;
     }
 }
--- src/main/java/nextstep/subway/strategy/ShortestPathStrategy.java
+++ src/main/java/nextstep/subway/strategy/ShortestPathStrategy.java
@@ -1,8 +1,12 @@
 package nextstep.subway.strategy;
 
 import nextstep.subway.domain.ShortestPathType;
+import nextstep.subway.domain.Station;
+
+import java.util.List;
 
 public interface ShortestPathStrategy {
-    int weight();
-    boolean supported(ShortestPathType shortestPathType);
+    List<Station> findShortestStationPath(Station source, Station target);
+    int totalPathDistance(Station source, Station target);
+    boolean support(ShortestPathType shortestPathType);
 }
