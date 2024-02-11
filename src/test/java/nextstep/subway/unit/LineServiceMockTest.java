af4505dfc3c69bda6a9ebbc0d05e94a085983217
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-02-11 22:13:46 +0900

    refactor: Line Service에 대한 Mock 테스트 코드 리팩토링

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -5,8 +5,6 @@ import nextstep.config.fixtures.SectionFixture;
 import nextstep.config.fixtures.StationFixture;
 import nextstep.subway.application.LineService;
 import nextstep.subway.dto.SectionRequest;
-import nextstep.subway.dto.SectionResponse;
-import nextstep.subway.entity.Line;
 import nextstep.subway.entity.Station;
 import nextstep.subway.entity.repository.LineRepository;
 import nextstep.subway.entity.repository.StationRepository;
@@ -24,6 +22,15 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
 
+    Long 이호선_아이디;
+    Long 선릉역_번호;
+    Long 삼성역_번호;
+    Long 신천역_번호;
+
+    Station 선릉역;
+    Station 삼성역;
+    Station 신천역;
+
     LineService lineService;
 
     @Mock
@@ -34,8 +41,24 @@ public class LineServiceMockTest {
 
 
     @BeforeEach
-    void setUp() {
+    void 서비스_객체_생성() {
         lineService = new LineService(stationRepository, lineRepository);
+    }
+
+    @BeforeEach
+    void 스텁_설정() {
+        이호선_아이디 = 1L;
+        선릉역_번호 = 1L;
+        삼성역_번호 = 2L;
+        신천역_번호 = 3L;
+
+        선릉역 = StationFixture.선릉.updateId(선릉역_번호);
+        삼성역 = StationFixture.삼성.updateId(삼성역_번호);
+        신천역 = StationFixture.신천.updateId(신천역_번호);
+
+        when(lineRepository.findById(이호선_아이디)).thenReturn(Optional.of(LineFixture.이호선_생성()));
+        when(stationRepository.findById(선릉역_번호)).thenReturn(Optional.of(선릉역));
+        when(stationRepository.findById(삼성역_번호)).thenReturn(Optional.of(삼성역));
     }
 
     /**
@@ -46,17 +69,6 @@ public class LineServiceMockTest {
     @Test
     void 지하철_구간_추가() {
         // given
-        Long 이호선_아이디 = 1L;
-        when(lineRepository.findById(이호선_아이디)).thenReturn(Optional.of(LineFixture.이호선_생성()));
-
-        Long 선릉역_번호 = 1L;
-        Station 선릉역 = StationFixture.선릉.updateId(선릉역_번호);
-        when(stationRepository.findById(선릉역_번호)).thenReturn(Optional.of(선릉역));
-
-        Long 삼성역_번호 = 2L;
-        Station 삼성역 = StationFixture.삼성.updateId(삼성역_번호);
-        when(stationRepository.findById(삼성역_번호)).thenReturn(Optional.of(삼성역));
-
         SectionRequest 선릉_삼성_구간_요청 = SectionRequest.mergeForCreateLine(
                         이호선_아이디,
                         SectionFixture.지하철_구간(선릉역_번호, 삼성역_번호, 10));
@@ -70,35 +82,21 @@ public class LineServiceMockTest {
     }
 
     /**
-      * Given 지하철 노선이 생성되고
+      * Given 지하철 노선이 생성되고 구간을 추가한다
       * When  지하철 구간을 삭제하면
       * Then  지하철 노선에 구간이 삭제된다
       */
     @Test
     void 지하철_구간_삭제() {
         // given
-        Long 이호선_아이디 = 1L;
-        when(lineRepository.findById(이호선_아이디)).thenReturn(Optional.of(LineFixture.이호선_생성()));
-
-        Long 선릉역_번호 = 1L;
-        Station 선릉역 = StationFixture.선릉.updateId(선릉역_번호);
-        when(stationRepository.findById(선릉역_번호)).thenReturn(Optional.of(선릉역));
-
-        Long 삼성역_번호 = 2L;
-        Station 삼성역 = StationFixture.삼성.updateId(삼성역_번호);
-        when(stationRepository.findById(삼성역_번호)).thenReturn(Optional.of(삼성역));
-
         SectionRequest 선릉_삼성_구간_요청 = SectionRequest.mergeForCreateLine(
                 이호선_아이디,
                 SectionFixture.지하철_구간(선릉역_번호, 삼성역_번호, 10));
-
-        Long 신천역_번호 = 3L;
-        Station 신천역 = StationFixture.신천.updateId(신천역_번호);
-        when(stationRepository.findById(신천역_번호)).thenReturn(Optional.of(신천역));
-
         SectionRequest 삼성_신천_구간_요청 = SectionRequest.mergeForCreateLine(
                 이호선_아이디,
                 SectionFixture.지하철_구간(삼성역_번호, 신천역_번호, 10));
+
+        when(stationRepository.findById(신천역_번호)).thenReturn(Optional.of(신천역));
 
         lineService.addSection(선릉_삼성_구간_요청);
         lineService.addSection(삼성_신천_구간_요청);
