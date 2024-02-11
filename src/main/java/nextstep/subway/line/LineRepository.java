53e806b81d64dfca6a9da2827871b7126ab427a0
moonstal1506 <moonstal1506@gmail.com>
2024-02-11 21:41:42 +0900

    refactor: 불필요한 코드 삭제

--- src/main/java/nextstep/subway/line/LineRepository.java
+++ src/main/java/nextstep/subway/line/LineRepository.java
@@ -6,8 +6,4 @@ import java.util.List;
 import java.util.Optional;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
-
-    List<Line> findAll();
-
-    Optional<Line> findById(Long lineId);
 }
