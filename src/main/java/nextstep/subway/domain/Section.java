5ae5edce501e7053215c414fe94a33f4430fc6eb
Lee seung chan <dltmdcks702@gmail.com>
2024-02-12 11:17:35 +0900

    chore: delete dead code

--- src/main/java/nextstep/subway/domain/Section.java
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -53,19 +53,6 @@ public class Section {
         return distance;
     }
 
-    public void splitTheSectionWith(Section section) {
-        if (upStation == section.getUpStation()) {
-            upStation = section.getDownStation();
-            distance -= section.getDistance();
-            return;
-        }
-
-        if (downStation == section.getDownStation()) {
-            downStation = section.getUpStation();
-            distance -= section.getDistance();
-        }
-    }
-
     public void shorten(Section section) {
         upStation = section.downStation;
         distance -= section.distance;
