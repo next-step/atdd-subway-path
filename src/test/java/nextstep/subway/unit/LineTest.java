ecb45805c47769b1b41ac3173b5cf76a4c66d9ea
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-02-13 13:02:44 +0900

    refactor: 구간 추가 요구사항에 대한 도메인 관련 단위 테스트 리팩토링

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -29,149 +29,153 @@ class LineTest {
 
     @Nested
     class 지하철_구간_추가 {
+
         @Nested
         class 성공 {
 
-            /**
-             * Given 지하철 노선이 생성되고
-             * When  지하철 구간을 추가하면
-             * Then  지하철 노선에 구간이 추가된다.
-             */
-            @Test
-            void 지하철_구간_추가_성공() {
-                // given
-                Section 강남_양재_구간 = SectionFixture.강남_양재_구간;
+            @Nested
+            class 노선에_구간_없음 {
+                /**
+                 * Given 지하철 노선이 생성되고
+                 * When  지하철 구간을 추가하면
+                 * Then  지하철 노선에 구간이 추가된다.
+                 */
+                @Test
+                void 지하철_구간_추가_성공() {
+                    // given
+                    Section 강남_양재_구간 = SectionFixture.강남_양재_구간;
 
-                // when
-                이호선.addSection(강남_양재_구간);
+                    // when
+                    이호선.addSection(강남_양재_구간);
 
-                // then
-                assertThat(이호선.getAllSections()).containsAnyOf(강남_양재_구간);
+                    // then
+                    assertThat(이호선.getAllSections()).containsAnyOf(강남_양재_구간);
+                }
             }
 
             @Nested
-            class 노선_처음에_추가 {
+            class 노선에_구간이_있음 {
+                Section 삼성_선릉_구간;
+                Section 선릉_역삼_구간;
+                Section 역삼_강남_구간;
+                Section 강남_서초_구간;
 
-                /**
-                 * Given 지하철 노선이 생성되고, 구간을 추가한다.
-                 * When  지하철 구간을 노선 처음에 등록할 때
-                 * When  요청한 구간의 상행역이 기존 노선의 역으로 등록되어 있으면서
-                 * When  요청한 구간의 하행역이 기존 노선의 역으로 등록되어 있지 않을 경우
-                 * Then  지하철 구간 등록에 성공한다.
-                 */
-                @Test
-                void 추가_요청한_구간의_상행역이_기존_노선의_역으로_등록되어_있고_하행역은_등록되지_않은_역일_경우() {
-                    // given
-                    Section 삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
+                @BeforeEach
+                void 초기_노선에_구간_추가() {
+                    삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
+                    선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
+                    역삼_강남_구간 = SectionFixture.역삼_강남_구간;
+                    강남_서초_구간 = SectionFixture.강남_서초_구간;
+
                     이호선.addSection(삼성_선릉_구간);
-
-                    Section 선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
                     이호선.addSection(선릉_역삼_구간);
-
-                    Section 역삼_강남_구간 = SectionFixture.역삼_강남_구간;
                     이호선.addSection(역삼_강남_구간);
-
-                    Section 강남_서초_구간 = SectionFixture.강남_서초_구간;
                     이호선.addSection(강남_서초_구간);
-
-                    // when
-                    Section 삼성_사성_구간 = new Section(삼성, 사성, 5);
-                    Section 사성_선릉_구간 = new Section(사성, 선릉, 5);
-
-                    이호선.addSection(삼성_사성_구간);
-
-                    // then
-                    assertThat(이호선.getStations())
-                            .containsOnly(삼성, 사성, 선릉, 역삼, 강남, 서초);
-
-                    assertThat(이호선.getAllSections())
-                            .containsOnly(삼성_사성_구간, 사성_선릉_구간, 선릉_역삼_구간, 역삼_강남_구간, 강남_서초_구간);
                 }
 
-            }
+                @Nested
+                class 노선_처음에_추가 {
 
-            @Nested
-            class 노선_중간에_추가 {
-                /**
-                 * Given 지하철 노선이 생성되고, 구간을 추가한다.
-                 * When  지하철 구간을 노선 중간에 등록할 때
-                 * When  요청한 구간의 상행역이 기존 노선의 역으로 등록되어 있으면서
-                 * When  요청한 구간의 하행역이 기존 노선의 역으로 등록되어 있지 않을 경우
-                 * Then  지하철 구간 등록에 성공한다.
-                 */
-                @Test
-                void 추가_요청한_구간의_상행역이_기존_노선의_역으로_등록되어_있고_하행역은_등록되지_않은_역일_경우() {
-                    // given
-                    Section 삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
-                    이호선.addSection(삼성_선릉_구간);
+                    /**
+                     * Given 지하철 노선이 생성되고, 구간을 추가한다.
+                     * When  지하철 구간을 노선 처음에 등록할 때
+                     * When  요청한 구간의 상행역이 기존 노선의 역으로 등록되어 있으면서
+                     * When  요청한 구간의 하행역이 기존 노선의 역으로 등록되어 있지 않을 경우
+                     * Then  지하철 구간 등록에 성공한다.
+                     */
+                    @Test
+                    void 추가_요청한_구간의_상행역이_기존_노선의_역으로_등록되어_있고_하행역은_등록되지_않은_역일_경우() {
+                        // when
+                        Section 삼성_사성_구간 = new Section(삼성, 사성, 5);
+                        Section 사성_선릉_구간 = new Section(사성, 선릉, 5);
 
-                    Section 선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
-                    이호선.addSection(선릉_역삼_구간);
+                        이호선.addSection(삼성_사성_구간);
 
-                    Section 역삼_강남_구간 = SectionFixture.역삼_강남_구간;
-                    이호선.addSection(역삼_강남_구간);
+                        // then
+                        assertThat(이호선.getStations())
+                                .containsOnly(삼성, 사성, 선릉, 역삼, 강남, 서초);
 
-                    Section 강남_서초_구간 = SectionFixture.강남_서초_구간;
-                    이호선.addSection(강남_서초_구간);
+                        assertThat(이호선.getAllSections())
+                                .containsOnly(삼성_사성_구간, 사성_선릉_구간, 선릉_역삼_구간, 역삼_강남_구간, 강남_서초_구간);
+                    }
 
-                    // when
-                    Section 역삼_사성_구간 = new Section(역삼, 사성, 5);
-                    Section 사성_강남_구간 = new Section(사성, 강남, 5);
-
-                    이호선.addSection(역삼_사성_구간);
-
-                    // then
-                    assertThat(이호선.getStations())
-                            .containsOnly(삼성, 사성, 선릉, 역삼, 강남, 서초);
-
-                    assertThat(이호선.getAllSections())
-                            .containsOnly(삼성_선릉_구간, 선릉_역삼_구간, 역삼_사성_구간, 사성_강남_구간, 강남_서초_구간);
                 }
 
-            }
+                @Nested
+                class 노선_중간에_추가 {
+                    /**
+                     * Given 지하철 노선이 생성되고, 구간을 추가한다.
+                     * When  지하철 구간을 노선 중간에 등록할 때
+                     * When  요청한 구간의 상행역이 기존 노선의 역으로 등록되어 있으면서
+                     * When  요청한 구간의 하행역이 기존 노선의 역으로 등록되어 있지 않을 경우
+                     * Then  지하철 구간 등록에 성공한다.
+                     */
+                    @Test
+                    void 추가_요청한_구간의_상행역이_기존_노선의_역으로_등록되어_있고_하행역은_등록되지_않은_역일_경우() {
+                        // when
+                        Section 역삼_사성_구간 = new Section(역삼, 사성, 5);
+                        Section 사성_강남_구간 = new Section(사성, 강남, 5);
 
-            @Nested
-            class 노선_끝에_추가 {
-                /**
-                 * Given 지하철 노선이 생성되고, 구간을 추가한다.
-                 * When  지하철 구간을 노선 끝에 등록할 때
-                 * When  요청한 구간의 상행역이 기존 노선의 역으로 등록되어 있으면서
-                 * When  요청한 구간의 하행역이 기존 노선의 역으로 등록되어 있지 않을 경우
-                 * Then  지하철 구간 등록에 성공한다.
-                 */
-                @Test
-                void 추가_요청한_구간의_상행역이_기존_노선의_역으로_등록되어_있고_하행역은_등록되지_않은_역일_경우() {
-                    // given
-                    Section 삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
-                    이호선.addSection(삼성_선릉_구간);
+                        이호선.addSection(역삼_사성_구간);
 
-                    Section 선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
-                    이호선.addSection(선릉_역삼_구간);
+                        // then
+                        assertThat(이호선.getStations())
+                                .containsOnly(삼성, 사성, 선릉, 역삼, 강남, 서초);
 
-                    Section 역삼_강남_구간 = SectionFixture.역삼_강남_구간;
-                    이호선.addSection(역삼_강남_구간);
+                        assertThat(이호선.getAllSections())
+                                .containsOnly(삼성_선릉_구간, 선릉_역삼_구간, 역삼_사성_구간, 사성_강남_구간, 강남_서초_구간);
+                    }
 
-                    Section 강남_서초_구간 = SectionFixture.강남_서초_구간;
-                    이호선.addSection(강남_서초_구간);
-
-                    // when
-                    Section 서초_사성_구간 = new Section(서초, 사성, 5);
-
-                    이호선.addSection(서초_사성_구간);
-
-                    // then
-                    assertThat(이호선.getStations())
-                            .containsOnly(삼성, 사성, 선릉, 역삼, 강남, 서초);
-
-                    assertThat(이호선.getAllSections())
-                            .containsOnly(삼성_선릉_구간, 선릉_역삼_구간, 역삼_강남_구간, 강남_서초_구간, 서초_사성_구간);
                 }
 
+                @Nested
+                class 노선_끝에_추가 {
+                    /**
+                     * Given 지하철 노선이 생성되고, 구간을 추가한다.
+                     * When  지하철 구간을 노선 끝에 등록할 때
+                     * When  요청한 구간의 상행역이 기존 노선의 역으로 등록되어 있으면서
+                     * When  요청한 구간의 하행역이 기존 노선의 역으로 등록되어 있지 않을 경우
+                     * Then  지하철 구간 등록에 성공한다.
+                     */
+                    @Test
+                    void 추가_요청한_구간의_상행역이_기존_노선의_역으로_등록되어_있고_하행역은_등록되지_않은_역일_경우() {
+                        // when
+                        Section 서초_사성_구간 = new Section(서초, 사성, 5);
+
+                        이호선.addSection(서초_사성_구간);
+
+                        // then
+                        assertThat(이호선.getStations())
+                                .containsOnly(삼성, 사성, 선릉, 역삼, 강남, 서초);
+
+                        assertThat(이호선.getAllSections())
+                                .containsOnly(삼성_선릉_구간, 선릉_역삼_구간, 역삼_강남_구간, 강남_서초_구간, 서초_사성_구간);
+                    }
+
+                }
             }
         }
 
         @Nested
         class 실패 {
+
+            Section 삼성_선릉_구간;
+            Section 선릉_역삼_구간;
+            Section 역삼_강남_구간;
+            Section 강남_서초_구간;
+
+            @BeforeEach
+            void 초기_노선에_구간_추가() {
+                삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
+                선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
+                역삼_강남_구간 = SectionFixture.역삼_강남_구간;
+                강남_서초_구간 = SectionFixture.강남_서초_구간;
+
+                이호선.addSection(삼성_선릉_구간);
+                이호선.addSection(선릉_역삼_구간);
+                이호선.addSection(역삼_강남_구간);
+                이호선.addSection(강남_서초_구간);
+            }
 
             @Nested
             class 공통 {
@@ -183,19 +187,6 @@ class LineTest {
                  */
                 @Test
                 void 추가할_구간의_상행역이_기존_노선에_등록되지_않았을_경우() {
-                    // given
-                    Section 삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
-                    이호선.addSection(삼성_선릉_구간);
-
-                    Section 선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
-                    이호선.addSection(선릉_역삼_구간);
-
-                    Section 역삼_강남_구간 = SectionFixture.역삼_강남_구간;
-                    이호선.addSection(역삼_강남_구간);
-
-                    Section 강남_서초_구간 = SectionFixture.강남_서초_구간;
-                    이호선.addSection(강남_서초_구간);
-
                     // when, then
                     Section 양재_사성_구간 = new Section(양재, 사성, 5);
 
@@ -213,19 +204,6 @@ class LineTest {
                  */
                 @Test
                 void 상행역과_하행역이_동일한_구간일_경우() {
-                    // given
-                    Section 삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
-                    이호선.addSection(삼성_선릉_구간);
-
-                    Section 선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
-                    이호선.addSection(선릉_역삼_구간);
-
-                    Section 역삼_강남_구간 = SectionFixture.역삼_강남_구간;
-                    이호선.addSection(역삼_강남_구간);
-
-                    Section 강남_서초_구간 = SectionFixture.강남_서초_구간;
-                    이호선.addSection(강남_서초_구간);
-
                     // when, then
                     Section 역삼_역삼_구간 = new Section(역삼, 역삼, 5);
 
@@ -248,19 +226,6 @@ class LineTest {
                  */
                 @Test
                 void 추가할_구간의_하행역과_기존_모든_구간의_역_중_하나라도_같은_역이_존재할_경우() {
-                    // given
-                    Section 삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
-                    이호선.addSection(삼성_선릉_구간);
-
-                    Section 선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
-                    이호선.addSection(선릉_역삼_구간);
-
-                    Section 역삼_강남_구간 = SectionFixture.역삼_강남_구간;
-                    이호선.addSection(역삼_강남_구간);
-
-                    Section 강남_서초_구간 = SectionFixture.강남_서초_구간;
-                    이호선.addSection(강남_서초_구간);
-
                     // when, then
                     Section 삼성_강남_구간 = new Section(삼성, 강남, 5);
 
@@ -278,19 +243,6 @@ class LineTest {
                  */
                 @Test
                 void 추가할_구간의_하행역을_기준으로_직후_역과의_거리가_1보다_작은_경우() {
-                    // given
-                    Section 삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
-                    이호선.addSection(삼성_선릉_구간);
-
-                    Section 선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
-                    이호선.addSection(선릉_역삼_구간);
-
-                    Section 역삼_강남_구간 = SectionFixture.역삼_강남_구간;
-                    이호선.addSection(역삼_강남_구간);
-
-                    Section 강남_서초_구간 = SectionFixture.강남_서초_구간;
-                    이호선.addSection(강남_서초_구간);
-
                     // when, then
                     Section 삼성_사성_구간 = new Section(삼성, 사성, 10);
 
@@ -312,19 +264,6 @@ class LineTest {
                  */
                 @Test
                 void 추가할_구간의_하행역과_기존_모든_구간의_역_중_하나라도_같은_역이_존재할_경우() {
-                    // given
-                    Section 삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
-                    이호선.addSection(삼성_선릉_구간);
-
-                    Section 선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
-                    이호선.addSection(선릉_역삼_구간);
-
-                    Section 역삼_강남_구간 = SectionFixture.역삼_강남_구간;
-                    이호선.addSection(역삼_강남_구간);
-
-                    Section 강남_서초_구간 = SectionFixture.강남_서초_구간;
-                    이호선.addSection(강남_서초_구간);
-
                     // when, then
                     Section 역삼_서초_구간 = new Section(역삼, 서초, 5);
 
@@ -342,19 +281,6 @@ class LineTest {
                  */
                 @Test
                 void 추가할_구간의_하행역을_기준으로_직후_역과의_거리가_1보다_작은_경우() {
-                    // given
-                    Section 삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
-                    이호선.addSection(삼성_선릉_구간);
-
-                    Section 선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
-                    이호선.addSection(선릉_역삼_구간);
-
-                    Section 역삼_강남_구간 = SectionFixture.역삼_강남_구간;
-                    이호선.addSection(역삼_강남_구간);
-
-                    Section 강남_서초_구간 = SectionFixture.강남_서초_구간;
-                    이호선.addSection(강남_서초_구간);
-
                     // when, then
                     Section 역삼_사성_구간 = new Section(역삼, 사성, 10);
 
@@ -376,19 +302,6 @@ class LineTest {
                  */
                 @Test
                 void 추가할_구간의_하행역과_기존_모든_구간의_역_중_하나라도_같은_역이_존재할_경우() {
-                    // given
-                    Section 삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
-                    이호선.addSection(삼성_선릉_구간);
-
-                    Section 선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
-                    이호선.addSection(선릉_역삼_구간);
-
-                    Section 역삼_강남_구간 = SectionFixture.역삼_강남_구간;
-                    이호선.addSection(역삼_강남_구간);
-
-                    Section 강남_서초_구간 = SectionFixture.강남_서초_구간;
-                    이호선.addSection(강남_서초_구간);
-
                     // when, then
                     Section 서초_삼성_구간 = new Section(서초, 삼성, 5);
 
