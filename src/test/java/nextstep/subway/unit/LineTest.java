d589d200b0f4872d5cf621f6a5e808bfe9d9e457
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-02-13 10:31:40 +0900

    test: 구간 추가 요구사항에 대한 도메인 관련 단위 테스트 추가

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -5,15 +5,18 @@ import nextstep.config.fixtures.SectionFixture;
 import nextstep.config.fixtures.StationFixture;
 import nextstep.subway.entity.Line;
 import nextstep.subway.entity.Section;
+import nextstep.subway.entity.Sections;
 import nextstep.subway.entity.Station;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
 
 import static nextstep.config.fixtures.SectionFixture.*;
+import static nextstep.config.fixtures.StationFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
@@ -31,6 +34,7 @@ class LineTest {
     class 지하철_구간_추가 {
         @Nested
         class 성공 {
+
             /**
              * Given 지하철 노선이 생성되고
              * When  지하철 구간을 추가하면
@@ -46,6 +50,57 @@ class LineTest {
 
                 // then
                 assertThat(이호선.getAllSections()).containsAnyOf(강남_양재_구간);
+            }
+
+            @Nested
+            class 위치_노선_처음 {
+
+                /**
+                 * Given 지하철 노선이 생성되고, 구간을 추가한다.
+                 * When  지하철 구간을 노선 처음에 등록할 때
+                 * When  요청한 구간의 상행역이 기존 노선의 역으로 등록되어 있으면서
+                 * When  요청한 구간의 하행역이 기존 노선의 역으로 등록되어 있지 않을 경우
+                 * Then  지하철 구간 등록에 성공한다.
+                 */
+                @Test
+                void 추가_요청한_구간의_상행역이_기존_노선의_역으로_등록되어_있고_하행역은_등록되지_않은_역일_경우() {
+                    // given
+                    Section 삼성_선릉_구간 = SectionFixture.삼성_선릉_구간;
+                    이호선.addSection(삼성_선릉_구간);
+
+                    Section 선릉_역삼_구간 = SectionFixture.선릉_역삼_구간;
+                    이호선.addSection(선릉_역삼_구간);
+
+                    Section 역삼_강남_구간 = SectionFixture.역삼_강남_구간;
+                    이호선.addSection(역삼_강남_구간);
+
+                    Section 강남_서초_구간 = SectionFixture.강남_서초_구간;
+                    이호선.addSection(강남_서초_구간);
+
+                    // when
+                    Section 삼성_사성_구간 = new Section(삼성, 사성, 5);
+                    Section 사성_선릉_구간 = new Section(사성, 선릉, 5);
+
+                    이호선.addSection(삼성_사성_구간);
+
+                    // then
+                    assertThat(이호선.getStations())
+                            .containsOnly(삼성, 사성, 선릉, 역삼, 강남, 서초);
+
+                    assertThat(이호선.getAllSections())
+                            .containsOnly(삼성_사성_구간, 사성_선릉_구간, 선릉_역삼_구간, 역삼_강남_구간, 강남_서초_구간);
+                }
+
+            }
+
+            @Nested
+            class 위치_노선_중간 {
+
+            }
+
+            @Nested
+            class 위치_노선_끝 {
+
             }
         }
         @Nested
@@ -145,7 +200,7 @@ class LineTest {
                 이호선.deleteSection(StationFixture.역삼);
 
                 // then
-                assertThat(이호선.getStations()).containsAnyOf(StationFixture.삼성, StationFixture.선릉);
+                assertThat(이호선.getStations()).containsAnyOf(삼성, StationFixture.선릉);
             }
         }
         @Nested
@@ -159,7 +214,7 @@ class LineTest {
             void 기존_노선_구간_목록에_구간이_존재하지_않을_경우_삭제_실패() {
                 assertThatExceptionOfType(IllegalArgumentException.class)
                         .isThrownBy(() -> {
-                            이호선.deleteSection(StationFixture.삼성);
+                            이호선.deleteSection(삼성);
                         })
                         .withMessageMatching("해당 노선에 구간이 존재하지 않습니다.");
             }
@@ -177,7 +232,7 @@ class LineTest {
                 // when, then
                 assertThatExceptionOfType(IllegalArgumentException.class)
                         .isThrownBy(() -> {
-                            이호선.deleteSection(StationFixture.삼성);
+                            이호선.deleteSection(삼성);
                         })
                         .withMessageMatching("마지막 구간의 하행역과 동일하지 않습니다.");
             }
