cdeb17586bfeab2a82d2e17d2d3997641d6276b3
mincheolkk <kmc04080408@gmail.com>
2024-02-15 01:34:42 +0900

    test : 라인 서비스 테스트 구현

--- src/main/java/nextstep/subway/line/api/LineService.java
+++ src/main/java/nextstep/subway/line/api/LineService.java
@@ -6,7 +6,6 @@ import nextstep.subway.line.presentation.request.LineCreateRequest;
 import nextstep.subway.line.presentation.request.LineUpdateRequest;
 import nextstep.subway.line.repository.LineRepository;
 import nextstep.subway.section.SectionRepository;
-import nextstep.subway.section.api.SectionService;
 import nextstep.subway.section.domain.Section;
 import nextstep.subway.section.presentation.request.SectionCreateRequest;
 import nextstep.subway.station.Station;
@@ -24,13 +23,11 @@ public class LineService {
 
     private final LineRepository lineRepository;
     private final StationRepository stationRepository;
-    private final SectionService sectionService;
     private final SectionRepository sectionRepository;
 
-    public LineService(LineRepository lineRepository, StationRepository stationRepository, SectionService sectionService, SectionRepository sectionRepository) {
+    public LineService(LineRepository lineRepository, StationRepository stationRepository, SectionRepository sectionRepository) {
         this.lineRepository = lineRepository;
         this.stationRepository = stationRepository;
-        this.sectionService = sectionService;
         this.sectionRepository = sectionRepository;
     }
 
@@ -78,7 +75,7 @@ public class LineService {
     }
 
     public LineResponse getLine(Long lineId) {
-        Line line = getLineEntity(lineId);
+        Line line = findLineById(lineId);
 
         Station upStation = getStationEntity(line.getSections().getUpStationId());
         Station downStation = getStationEntity(line.getSections().getDownStationId());
@@ -90,17 +87,22 @@ public class LineService {
     }
 
     public void updateLine(Long lineId, LineUpdateRequest request) {
-        Line line = getLineEntity(lineId);
+        Line line = findLineById(lineId);
         line.updateName(request.getName());
         line.updateColor(request.getColor());
     }
 
     public void deleteLine(Long lineId) {
-        Line line = getLineEntity(lineId);
+        Line line = findLineById(lineId);
         lineRepository.delete(line);
     }
 
-    private Line getLineEntity(Long lineId) {
+    public void addSection(Line line, Section section) {
+        line.getSections().addSection(section);
+    }
+
+
+    public Line findLineById(Long lineId) {
         return lineRepository.findById(lineId).orElseThrow(
                 () -> new EntityNotFoundException("해당 엔티티를 찾을 수 없습니다.")
         );
