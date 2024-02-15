b02fba2d3000008fbdb1ccce9c0ed4c2de45dac8
seonje1 <154968849+seonje1@users.noreply.github.com>
2024-02-15 20:28:23 +0900

    Step2 - 구간 제거 추가 요구사항 (#755)
    * 인수테스트 시나리오 작성
    
    * 구간 제거 추가 요구사항 기능 구현
    
    - 종점역 제거 기능 구현
    - 중간역 제거시 구간 재배치 기능 구현
    - 단위 테스트 작성
    - 인수 테스트 시나리오 보충, 수정

--- src/test/java/nextstep/subway/acceptance/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/section/SectionAcceptanceTest.java
@@ -156,23 +156,72 @@ public class SectionAcceptanceTest {
 
     /**
      * Given 지하철 구간을 등록하고
-     * When 노선의 하행종점역이 아닌 구간을 삭제하면
-     * Then 예외가 발생한다
+     * When 구간의 위치에 상관없이 구간을 삭제하면
+     * Then 해당 구간이 삭제되고, 구간이 재배치 된다
      */
-    @DisplayName("노선의 하행종점역이 아닌 구간은 삭제할 수 없다")
+    @DisplayName("구간의 위치에 상관없이 수정이 가능하다")
     @Test
-    void deleteNotDownSection() {
+    void deleteMiddleSection() {
+        //given
+        SectionCreateRequest 용산건대입구역 = new SectionCreateRequest(용산역id, 건대입구역id, 5);
+        SectionApiRequester.generateSection(용산건대입구역, 이호선id);
+        SectionCreateRequest 건대입구성수역 = new SectionCreateRequest(건대입구역id, 성수역id, 5);
+        SectionApiRequester.generateSection(건대입구성수역, 이호선id);
+
+        //when
+        ExtractableResponse<Response> response = SectionApiRequester.deleteSection(이호선id, 건대입구역id);
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+        ExtractableResponse<Response> 이호선 = LineApiRequester.findLineApiCall(이호선id);
+        assertThat(getStationIds(이호선)).containsExactly(잠실역id, 용산역id, 성수역id);
+    }
+
+    /**
+     * Given 지하철 구간을 등록하고
+     * When 상행종점역이 제거될 경우
+     * Then 상행종점역의 다음 역이 상행종점역이 된다
+     */
+    @DisplayName("상행종점역이 제거될 경우 상행종점역의 다음 역이 상행종점역이 된다")
+    @Test
+    void deleteUpFinalStation() {
         //given
         SectionCreateRequest request = new SectionCreateRequest(용산역id, 건대입구역id, 5);
-
         SectionApiRequester.generateSection(request, 이호선id);
 
         //when
-        ExtractableResponse<Response> response = SectionApiRequester.deleteSection(이호선id, 용산역id);
+        ExtractableResponse<Response> response = SectionApiRequester.deleteSection(이호선id, 잠실역id);
 
         //then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-        assertThat(response.asPrettyString()).isEqualTo("노선의 하행종점역만 제거할 수 있습니다.");
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+        ExtractableResponse<Response> 이호선 = LineApiRequester.findLineApiCall(이호선id);
+        Long downFinalStationId = getStationIds(이호선).get(0);
+        assertThat(downFinalStationId).isEqualTo(용산역id);
+    }
+
+    /**
+     * Given 지하철 구간을 등록하고
+     * When 하행종점역이 제거될 경우
+     * Then 하행종점역의 전 역이 하행종점역이 된다
+     */
+    @DisplayName("하행행종점역이 제거될 경우 하행종점역의 전 역이 하행종점역이 된다")
+    @Test
+    void deleteDownFinalStation() {
+        //given
+        SectionCreateRequest request = new SectionCreateRequest(용산역id, 건대입구역id, 5);
+        SectionApiRequester.generateSection(request, 이호선id);
+
+        //when
+        ExtractableResponse<Response> response = SectionApiRequester.deleteSection(이호선id, 건대입구역id);
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+        ExtractableResponse<Response> 이호선 = LineApiRequester.findLineApiCall(이호선id);
+        Long downFinalStationId = getStationIds(이호선).get(getStationIds(이호선).size() - 1);
+        assertThat(downFinalStationId).isEqualTo(용산역id);
     }
 
     /**
