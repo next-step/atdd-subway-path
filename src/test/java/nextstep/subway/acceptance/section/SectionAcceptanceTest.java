32eb00a19c6571f8dfa342d8f53975ad9453c8c4
tomato <devmtn30@gmail.com>
2024-02-16 16:21:42 +0900

    step2 - 인수테스트 작성

--- src/test/java/nextstep/subway/acceptance/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/section/SectionAcceptanceTest.java
@@ -9,7 +9,10 @@ import static nextstep.subway.acceptance.station.StationAcceptanceTestHelper.지
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.acceptance.AcceptanceTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -81,5 +84,46 @@ public class SectionAcceptanceTest {
         // then
         assertStatusCode(response, BAD_REQUEST);
         assertThat(노선_하행ID조회(노선ID)).isEqualTo(신규하행ID);
+    }
+
+
+    @Test
+    @DisplayName("구간 제거시 노선 처음에 제거할 수 있다.")
+    void deleteFrontSectionAcceptanceTest() {
+        //given
+        String 신규하행ID = getStringIdFromResponse(지하철역_생성_요청(지하철_파라미터_생성(신규역)));
+        구간_등록_요청(구간_파라미터_생성(신규하행ID, 상행ID), 노선ID);
+
+        //when
+        var response = 구간_제거_요청(신규하행ID, 노선ID);
+
+        // then
+        assertStatusCode(response, NO_CONTENT);
+    }
+    @Test
+    @DisplayName("구간 제거시 노선 중간에 제거할 수 있다.")
+    void deleteMidSectionAcceptanceTest() {
+        //given
+        String 신규하행ID = getStringIdFromResponse(지하철역_생성_요청(지하철_파라미터_생성(신규역)));
+        구간_등록_요청(구간_파라미터_생성(상행ID, 신규하행ID), 노선ID);
+
+        //when
+        var response = 구간_제거_요청(신규하행ID, 노선ID);
+
+        // then
+        assertStatusCode(response, NO_CONTENT);
+    }
+    @Test
+    @DisplayName("구간 제거시 노선 마지막에 제거할 수 있다.")
+    void deleteLastSectionAcceptanceTest() {
+        //given
+        String 신규하행ID = getStringIdFromResponse(지하철역_생성_요청(지하철_파라미터_생성(신규역)));
+        구간_등록_요청(구간_파라미터_생성(하행ID, 신규하행ID), 노선ID);
+
+        //when
+        var response = 구간_제거_요청(신규하행ID, 노선ID);
+
+        // then
+        assertStatusCode(response, NO_CONTENT);
     }
 }
