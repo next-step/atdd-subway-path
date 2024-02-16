2cdfa7ece6a47a7c498009558a1d66050b7f302f
yu <yu@reverselab.co.kr>
2024-02-16 10:42:31 +0900

    불필요 메서드 삭제

--- src/main/java/subway/line/Sections.java
+++ src/main/java/subway/line/Sections.java
@@ -90,16 +90,6 @@ public class Sections {
 		}
 	}
 
-	private void checkFinalStation(Station station) {
-		if (sectionList.isEmpty()) {
-			throw new IllegalArgumentException("해당 노선은 설정된 구간이 없습니다.");
-		}
-
-		if (!getFinalStation().getId().equals(station.getId())) {
-			throw new IllegalArgumentException("해당 노선의 마지막 정류장이 아닙니다.");
-		}
-	}
-
 	public Station getFirstStation() {
 		if (sectionList.isEmpty()) {
 			return new Station("");
