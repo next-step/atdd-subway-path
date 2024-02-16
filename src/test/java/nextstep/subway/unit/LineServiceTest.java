38e6f237f83212352604e1d4b5c41c247989a3aa
ifi999 <kkwkjm4231@gmail.com>
2024-02-16 22:34:59 +0900

    test: 지하철 노선 정보 수정 실패 테스트 작성

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -24,8 +24,8 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 @Transactional
 @ActiveProfiles("test")
@@ -140,6 +140,54 @@ public class LineServiceTest {
         // then
         assertThat(신분당선.getName()).isEqualTo("2호선");
         assertThat(신분당선.getColor()).isEqualTo("bg-green-600");
+    }
+
+    @DisplayName("지하철 노선 정보 수정 시 존재하지 않는 역을 수정할 경우 오류가 발생한다.")
+    @Test
+    void 지하철_노선_정보_수정_시_존재하지_않는_역으로_수정_불가() {
+        // given
+        강남역 = 역_생성("강남역");
+        역삼역 = 역_생성("역삼역");
+        신분당선 = 노선_생성("신분당선", "bg-red-600", 강남역, 역삼역, 10);
+        final LineUpdateRequest request = new LineUpdateRequest("2호선", "bg-green-600");
+
+        // then
+        assertThatThrownBy(() -> lineService.updateSubwayLine(3L, request))
+                .isInstanceOf(EntityNotFoundException.class);
+    }
+
+    @DisplayName("지하철 노선 정보 수정 시 노선명이 null 혹은 공백일 때 수정할 경우 오류가 발생한다.")
+    @Test
+    void 지하철_노선_정보_수정_시_노선명이_올바르지_않으면_수정_불가() {
+        // given
+        강남역 = 역_생성("강남역");
+        역삼역 = 역_생성("역삼역");
+        신분당선 = 노선_생성("신분당선", "bg-red-600", 강남역, 역삼역, 10);
+        final LineUpdateRequest 노선명_null = new LineUpdateRequest(null, "bg-green-600");
+        final LineUpdateRequest 노선명_공백 = new LineUpdateRequest("", "bg-green-600");
+
+        // then
+        assertThatThrownBy(() -> lineService.updateSubwayLine(1L, 노선명_null))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> lineService.updateSubwayLine(1L, 노선명_공백))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("지하철 노선 정보 수정 시 색상값이 null 혹은 공백일 때 수정할 경우 오류가 발생한다.")
+    @Test
+    void 지하철_노선_정보_수정_시_색상값이_올바르지_않으면_수정_불가() {
+        // given
+        강남역 = 역_생성("강남역");
+        역삼역 = 역_생성("역삼역");
+        신분당선 = 노선_생성("신분당선", "bg-red-600", 강남역, 역삼역, 10);
+        final LineUpdateRequest 색상값_null = new LineUpdateRequest("2호선", null);
+        final LineUpdateRequest 색상값_공백 = new LineUpdateRequest("2호선", "");
+
+        // then
+        assertThatThrownBy(() -> lineService.updateSubwayLine(1L, 색상값_null))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> lineService.updateSubwayLine(1L, 색상값_공백))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     private Line 노선_생성(final String name, final String color, final Station upStation, final Station downStation, final int distance) {
