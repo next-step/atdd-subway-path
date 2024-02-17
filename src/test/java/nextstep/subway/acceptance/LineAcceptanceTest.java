ab5f66f56585fb6d2e1309fb07e4444d9bcaa44b
sukhyunmoon <sukhyunmoon@sk.com>
2024-02-17 17:31:12 +0900

    test : 리뷰반영(오타수정, 변수명 통일)

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -7,7 +7,6 @@ import io.restassured.response.ValidatableResponse;
 import nextstep.subway.fixture.LineTestFixture;
 import nextstep.subway.fixture.SectionTestFixture;
 import nextstep.subway.fixture.StationTestFixture;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,21 +16,21 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-@DisplayName("지하철 노선 인수 테스")
+@DisplayName("지하철 노선 인수 테스트")
 public class LineAcceptanceTest extends AcceptanceTest{
 
-    private long 강남역_아이디;
-    private long 역삼역_아이디;
-    private long 교대역_아이디;
-    private long 이호선_아이디;
+    private long 강남역;
+    private long 역삼역;
+    private long 교대역;
+    private long 이호선;
 
     @BeforeEach
     public void setUp() {
-        강남역_아이디 = StationTestFixture.createStationFromName("강남역").jsonPath().getLong("id");
-        역삼역_아이디 = StationTestFixture.createStationFromName("역삼역").jsonPath().getLong("id");
-        교대역_아이디 = StationTestFixture.createStationFromName("교대역").jsonPath().getLong("id");
-        이호선_아이디 = LineTestFixture.createLine("2호선", "green", 강남역_아이디, 역삼역_아이디).jsonPath().getLong("id");
-        SectionTestFixture.createSection(강남역_아이디, 역삼역_아이디, 10, 이호선_아이디);
+        강남역 = StationTestFixture.createStationFromName("강남역").jsonPath().getLong("id");
+        역삼역 = StationTestFixture.createStationFromName("역삼역").jsonPath().getLong("id");
+        교대역 = StationTestFixture.createStationFromName("교대역").jsonPath().getLong("id");
+        이호선 = LineTestFixture.createLine("2호선", "green", 강남역, 역삼역).jsonPath().getLong("id");
+        SectionTestFixture.createSection(강남역, 역삼역, 10, 이호선);
     }
 
     @DisplayName("지하철 노선 맨 앞에 역을 등록한다.")
@@ -41,15 +40,15 @@ public class LineAcceptanceTest extends AcceptanceTest{
         long newStartStationId = RestAssured.given().log().all()
                 .contentType("application/json")
                 .body(new HashMap<String, String>() {{
-                    put("stationId", String.valueOf(교대역_아이디));
-                    put("nextStationId", String.valueOf(강남역_아이디));
+                    put("stationId", String.valueOf(교대역));
+                    put("nextStationId", String.valueOf(강남역));
                     put("distance", String.valueOf(2));
                 }})
-                .when().post("/lines/{lineId}/stations", 이호선_아이디)
+                .when().post("/lines/{lineId}/stations", 이호선)
                 .then().log().all()
                 .extract().jsonPath().getLong("startStation.id");
 
-        assertThat(newStartStationId).isEqualTo(교대역_아이디);
+        assertThat(newStartStationId).isEqualTo(교대역);
     }
 
     @DisplayName("지하철 노선 중간에 역을 등록한다.")
@@ -59,11 +58,11 @@ public class LineAcceptanceTest extends AcceptanceTest{
         ExtractableResponse<Response> extract = RestAssured.given().log().all()
                 .contentType("application/json")
                 .body(new HashMap<String, String>() {{
-                    put("stationId", String.valueOf(교대역_아이디));
-                    put("nextStationId", String.valueOf(역삼역_아이디));
+                    put("stationId", String.valueOf(교대역));
+                    put("nextStationId", String.valueOf(역삼역));
                     put("distance", String.valueOf(2));
                 }})
-                .when().post("/lines/{lineId}/stations", 이호선_아이디)
+                .when().post("/lines/{lineId}/stations", 이호선)
                 .then().log().all().extract();
 
         List<Long> sections = extract.jsonPath().getList("sections.sections.id", Long.class);
@@ -75,10 +74,10 @@ public class LineAcceptanceTest extends AcceptanceTest{
     @Test
     void removeBetween() {
 
-        SectionTestFixture.createSection(교대역_아이디, 강남역_아이디, 2, 이호선_아이디);
+        SectionTestFixture.createSection(교대역, 강남역, 2, 이호선);
 
         ValidatableResponse validatableResponse = RestAssured.given().log().all()
-                .when().delete("/lines/{lineId}/stations?stationId={stationId}", 이호선_아이디, 강남역_아이디)
+                .when().delete("/lines/{lineId}/stations?stationId={stationId}", 이호선, 강남역)
                 .then().log().all()
                 .statusCode(204);
     }
@@ -86,10 +85,10 @@ public class LineAcceptanceTest extends AcceptanceTest{
     @DisplayName("지하철 노선의 출발역을 제거한다.")
     @Test
     void removeFirst() {
-        SectionTestFixture.createSection(교대역_아이디, 강남역_아이디, 2, 이호선_아이디);
+        SectionTestFixture.createSection(교대역, 강남역, 2, 이호선);
 
         RestAssured.given().log().all()
-                .when().delete("/lines/{lineId}/stations?stationId={stationId}", 이호선_아이디, 교대역_아이디)
+                .when().delete("/lines/{lineId}/stations?stationId={stationId}", 이호선, 교대역)
                 .then().log().all()
                 .statusCode(204);
     }
