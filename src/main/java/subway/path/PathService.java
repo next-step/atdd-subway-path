949a6c572db03a8d3796d7a3fb1bf7e2b6d9317e
yu <yu@reverselab.co.kr>
2024-02-17 14:39:41 +0900

    최단 경로 단위 테스트 초안 통과 시키기용 서비스 로직 작성

--- src/main/java/subway/path/PathService.java
+++ src/main/java/subway/path/PathService.java
@@ -1,9 +1,21 @@
 package subway.path;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import subway.dto.path.PathResponse;
+import subway.station.Station;
 
 @Transactional(readOnly = true)
 @Service
 public class PathService {
+	public PathResponse findShortestPath(Long sourceStationId, Long targetStationId) {
+		Station station1 = new Station(7L, "교대역");
+		Station station2 = new Station(8L, "남부터미널역");
+		Station station3 = new Station(2L, "양재역");
+		List<Station> result = List.of(station1, station2, station3);
+		return new PathResponse(result, 4);
+	}
 }
