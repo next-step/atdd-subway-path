4b0423841502fc83e54a9d38892b23865b15ee73
ian <xonic789@naver.com>
2024-02-18 16:44:37 +0900

    test: 지하철 노선 경로 조회 인수테스트 작성

---
+++ src/main/java/nextstep/subway/applicaion/PathService.java
@@ -0,0 +1,13 @@
+package nextstep.subway.applicaion;
+
+import nextstep.subway.applicaion.dto.PathRequest;
+import nextstep.subway.applicaion.dto.PathResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PathService {
+
+    public PathResponse findPath(PathRequest request) {
+        return null;
+    }
+}
