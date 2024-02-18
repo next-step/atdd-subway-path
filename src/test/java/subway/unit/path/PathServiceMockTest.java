eba8bab607c38b2c8327ecde26f0468354fb8cc1
yu <yu@reverselab.co.kr>
2024-02-18 17:34:22 +0900

    given fixture BeforeEach로 이동

--- src/test/java/subway/unit/path/PathServiceMockTest.java
+++ src/test/java/subway/unit/path/PathServiceMockTest.java
@@ -1,15 +1,16 @@
 package subway.unit.path;
 
+import static org.mockito.BDDMockito.*;
 import static subway.fixture.line.LineEntityFixture.*;
 import static subway.fixture.station.StationEntityFixture.*;
 
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -29,26 +30,38 @@ class PathServiceMockTest {
 	@Mock
 	private LineRepository lineRepository;
 
-	@DisplayName("출발역과 도착역의 최단 거리를 조회한다.")
-	@Test
-	void findShortestPath() {
-		// given
-		Station 교대역 = 교대역();
-		Station 강남역 = 강남역();
-		Station 남부터미널역 = 남부터미널역();
-		Station 양재역 = 양재역();
+	private Station 교대역;
+	private Station 강남역;
+	private Station 남부터미널역;
+	private Station 양재역;
 
-		Line 이호선 = 이호선();
+	private Line 이호선;
+	private Line 삼호선;
+	private Line 신분당선;
+
+	@BeforeEach
+	void setUp() {
+		교대역 = 교대역();
+		강남역 = 강남역();
+		남부터미널역 = 남부터미널역();
+		양재역 = 양재역();
+
+		이호선 = 이호선();
 		이호선.addSection(new Section(이호선, 교대역, 강남역, 10));
 
-		Line 삼호선 = 삼호선();
+		삼호선 = 삼호선();
 		삼호선.addSection(new Section(삼호선, 교대역, 남부터미널역, 2));
 		삼호선.addSection(new Section(삼호선, 남부터미널역, 양재역, 2));
 
-		Line 신분당선 = 신분당선();
+		신분당선 = 신분당선();
 		신분당선.addSection(new Section(신분당선, 강남역, 양재역, 10));
+	}
 
-		BDDMockito.given(lineRepository.findAll()).willReturn(List.of(이호선, 삼호선, 신분당선));
+	@DisplayName("출발역과 도착역의 최단 거리를 조회한다.")
+	@Test
+	void successFindShortestPath() {
+		// given
+		given(lineRepository.findAll()).willReturn(List.of(이호선, 삼호선, 신분당선));
 
 		// when
 		PathResponse pathResponse = pathService.findShortestPath(교대역, 양재역);
