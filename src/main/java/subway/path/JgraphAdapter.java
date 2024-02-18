cbedbc187dc99b66b8c4bee5819ebc53f7c54d16
yu <yu@reverselab.co.kr>
2024-02-18 16:38:35 +0900

    더티 코딩 개선 - Jgraph 다른 객체로 감싸서 로직 분리 하기

---
+++ src/main/java/subway/path/JgraphAdapter.java
@@ -0,0 +1,51 @@
+package subway.path;
+
+import java.util.List;
+
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+import subway.line.Line;
+import subway.line.Section;
+import subway.station.Station;
+
+public class JgraphAdapter {
+	private final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath;
+
+	private JgraphAdapter(List<Line> lines) {
+		this.dijkstraShortestPath = init(lines);
+	}
+
+	public static JgraphAdapter of(List<Line> lines) {
+		return new JgraphAdapter(lines);
+	}
+
+	private DijkstraShortestPath<Station, DefaultWeightedEdge> init(List<Line> lines) {
+		WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+		for (Line line : lines) {
+			List<Station> sortedSections = line.getSortedStations();
+			for (Station station : sortedSections) {
+				graph.addVertex(station);
+			}
+
+			List<Section> sections = line.getSortedSections();
+			for (Section section : sections) {
+				DefaultWeightedEdge weightedEdge = graph.addEdge(section.getUpStation(), section.getDownStation());
+				graph.setEdgeWeight(weightedEdge, section.getDistance());
+			}
+		}
+
+		return new DijkstraShortestPath<>(graph);
+	}
+
+	public List<Station> getPath(Station sourceStation, Station targetStation) {
+		return dijkstraShortestPath.getPath(sourceStation, targetStation).getVertexList();
+	}
+
+	public Integer getPathWeight(Station sourceStation, Station targetStation) {
+		double pathWeight = dijkstraShortestPath.getPathWeight(sourceStation, targetStation);
+		return (int)pathWeight;
+	}
+}
--- src/main/java/subway/path/PathService.java
+++ src/main/java/subway/path/PathService.java
@@ -2,16 +2,12 @@ package subway.path;
 
 import java.util.List;
 
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import subway.dto.path.PathResponse;
 import subway.line.Line;
 import subway.line.LineRepository;
-import subway.line.Section;
 import subway.station.Station;
 
 @Transactional(readOnly = true)
@@ -24,35 +20,12 @@ public class PathService {
 	}
 
 	public PathResponse findShortestPath(Station sourceStation, Station targetStation) {
-
-		// Station station1 = new Station(7L, "교대역");
-		// Station station2 = new Station(8L, "남부터미널역");
-		// Station station3 = new Station(2L, "양재역");
-		// List<Station> result = List.of(station1, station2, station3);
-
-		WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
 		List<Line> lines = lineRepository.findAll();
-		for (Line line : lines) {
-			List<Station> sortedSections = line.getSortedStations();
-			for (Station station : sortedSections) {
-				graph.addVertex(station);
-			}
+		JgraphAdapter jgraphAdapter = JgraphAdapter.of(lines);
 
-			List<Section> sections = line.getSortedSections();
-			for (Section section : sections) {
-				graph.setEdgeWeight(
-					graph.addEdge(
-						section.getUpStation(),
-						section.getDownStation()
-					),
-					section.getDistance()
-				);
-			}
-		}
-
-		DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
-		List<Station> vertexList = dijkstraShortestPath.getPath(sourceStation, targetStation).getVertexList();
-		Double pathWeight = dijkstraShortestPath.getPathWeight(sourceStation, targetStation);
-		return new PathResponse(vertexList, pathWeight.intValue());
+		return new PathResponse(
+			jgraphAdapter.getPath(sourceStation, targetStation),
+			jgraphAdapter.getPathWeight(sourceStation, targetStation)
+		);
 	}
 }
