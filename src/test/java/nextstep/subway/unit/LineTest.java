3270b088307cf03f16595d36234b1ca4dfa23913
moonstal1506 <moonstal1506@gmail.com>
2024-02-19 21:31:01 +0900

    test: 구간 삭제 도메인 단위테스트 추가

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -1,5 +1,6 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.exception.SubwayException;
 import nextstep.subway.line.Line;
 import nextstep.subway.section.Section;
 import nextstep.subway.station.Station;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LineTest {
 
@@ -70,9 +72,14 @@ class LineTest {
         assertThat(이호선.getOrderedStations()).containsExactly(강남역, 역삼역);
     }
 
-    @DisplayName("지하철 노선에 마지막 역을 삭제한다.")
+    /**
+     * Given 마지막 구간을 생성하고
+     * When 마지막 역을 삭제하면
+     * Then 노선 조회 시 등록한 역을 찾을 수 없다
+     */
+    @DisplayName("마지막 지하철역을 삭제하면 지하철역 목록 조회 시 생성한 역을 찾을 수 없다.")
     @Test
-    void removeEndSection() {
+    void deleteEndSection() {
         Line 이호선 = new Line("2호선", "green", 강남역, 역삼역, 10L);
         Section section = new Section(이호선, 역삼역, 선릉역, 10L);
         이호선.addSection(section);
@@ -82,9 +89,14 @@ class LineTest {
         assertThat(이호선.getOrderedStations()).containsExactly(강남역, 역삼역);
     }
 
-    @DisplayName("지하철 노선에 가운데 역을 삭제한다.")
+    /**
+     * Given 마지막 구간을 생성하고
+     * When 가운데 역을 삭제하면
+     * Then 노선 조회 시 등록한 역을 찾을 수 없다
+     */
+    @DisplayName("가운데 지하철역을 삭제하면 지하철역 목록 조회 시 생성한 역을 찾을 수 없다.")
     @Test
-    void removeMiddleSection() {
+    void deleteMiddleSection() {
         Line 이호선 = new Line("2호선", "green", 강남역, 역삼역, 10L);
         Section 역삼_선릉 = new Section(이호선, 역삼역, 선릉역, 10L);
         이호선.addSection(역삼_선릉);
@@ -97,5 +109,49 @@ class LineTest {
         assertThat(이호선.getOrderedStations()).containsExactly(강남역, 역삼역, 삼성역);
         assertThat(역삼_선릉.getDownStation()).isEqualTo(선릉_삼성.getDownStation());
         assertThat(역삼_선릉.getDistance()).isEqualTo(20L);
+    }
+
+    /**
+     * Given 마지막 구간을 생성하고
+     * When 상행 종점 역을 삭제하면
+     * Then 노선 조회 시 등록한 역을 찾을 수 없다
+     */
+    @DisplayName("상행 종점 역을 삭제하면 지하철역 목록 조회 시 생성한 역을 찾을 수 없다.")
+    @Test
+    void deleteStartSection() {
+        Line 이호선 = new Line("2호선", "green", 강남역, 역삼역, 10L);
+        Section section = new Section(이호선, 역삼역, 선릉역, 10L);
+        이호선.addSection(section);
+
+        이호선.removeSection(강남역);
+
+        assertThat(이호선.getOrderedStations()).containsExactly(역삼역, 선릉역);
+    }
+
+    /**
+     * When 구간이 1개인 경우 삭제하면
+     * Then 에러를 반환한다.
+     */
+    @DisplayName("구간이 1개인 경우 삭제하면 에러를 반환한다.")
+    @Test
+    void validateLastSection() {
+        Line 이호선 = new Line("2호선", "green", 강남역, 역삼역, 10L);
+
+        assertThrows(SubwayException.class, () -> 이호선.removeSection(강남역));
+    }
+
+    /**
+     * Given 구간을 생성하고
+     * When 존재하지 않는 역을 삭제하면
+     * Then 에러를 반환한다.
+     */
+    @DisplayName("존재하지 않는 역을 삭제하면 에러를 반환한다.")
+    @Test
+    void validatePresent() {
+        Line 이호선 = new Line("2호선", "green", 강남역, 역삼역, 10L);
+        Section section = new Section(이호선, 역삼역, 선릉역, 10L);
+        이호선.addSection(section);
+
+        assertThrows(SubwayException.class, () -> 이호선.removeSection(삼성역));
     }
 }
