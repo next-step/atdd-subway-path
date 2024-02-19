847fd561812baa21b9b4e85e99e6a0a480da8910
문희원 <munhw1408@gmail.com>
2024-02-19 22:29:10 +0900

    test: 구간 조회 인수테스트 작성

--- src/test/java/nextstep/subway/helper/fixture/LineFixture.java
+++ src/test/java/nextstep/subway/helper/fixture/LineFixture.java
@@ -5,14 +5,38 @@ import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Station;
 
 public class LineFixture {
+    public static Line 신분당선_엔티티(Station upStation, Station downStation) {
+        return Line.create("신분당선", "bg-red-600", upStation, downStation, 10);
+    }
+
     public static LineCreateRequestBody 신분당선_생성_바디(Long upStationId, Long downStationId) {
         return new LineCreateRequestBody(
                 "신분당선", "bg-red-600", upStationId, downStationId, 10
         );
     }
 
-    public static Line 신분당선_엔티티(Station upStation, Station downStation) {
-        return Line.create("신분당선", "bg-red-600", upStation, downStation, 10);
+    public static LineCreateRequestBody 구호선_생성_바디(Long upStationId, Long downStationId) {
+        return new LineCreateRequestBody(
+                "9호선", "bg-yellow-300", upStationId, downStationId, 12
+        );
+    }
+
+    public static LineCreateRequestBody 삼호선_생성_바디(Long upStationId, Long downStationId) {
+        return new LineCreateRequestBody(
+                "3호선", "bg-yellow-300", upStationId, downStationId, 10
+        );
+    }
+
+    public static LineCreateRequestBody 칠호선_생성_바디(Long upStationId, Long downStationId) {
+        return new LineCreateRequestBody(
+                "7호선", "bg-green-300", upStationId, downStationId, 9
+        );
+    }
+
+    public static LineCreateRequestBody 경의중앙선_생성_바디(Long upStationId, Long downStationId) {
+        return new LineCreateRequestBody(
+                "신분당선", "bg-blue-600", upStationId, downStationId, 14
+        );
     }
 
     public static LineCreateRequestBody 이호선_생성_바디(Long upStationId, Long downStationId) {
--- src/test/java/nextstep/subway/helper/fixture/StationFixture.java
+++ src/test/java/nextstep/subway/helper/fixture/StationFixture.java
@@ -12,6 +12,13 @@ public class StationFixture {
     public static Map<String, String> 논현역 = Map.of("name", "논현역");
     public static Map<String, String> 역삼역 = Map.of("name", "역삼역");
 
+    public static Map<String, String> 논현역_생성_바디 = Map.of("name", "논현역");
+    public static Map<String, String> 신사역_생성_바디 = Map.of("name", "신사역");
+    public static Map<String, String> 신논현역_생성_바디 = Map.of("name", "신논현역");
+    public static Map<String, String> 고속터미널역_생성_바디 = Map.of("name", "고속터미널역");
+    public static Map<String, String> 한남역_생성_바디 = Map.of("name", "한남역");
+    public static Map<String, String> 서빙고역_생성_바디 = Map.of("name", "서빙고역");
+
     public static Station 강남역_엔티티 = new Station(1L, "강남역");
     public static Station 역삼역_엔티티 = new Station(2L, "역삼역");
     public static Station 선릉역_엔티티 = new Station(3L, "선릉역");
