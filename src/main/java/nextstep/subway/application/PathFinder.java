68bc1939dc9066d70b043a28be9f78951bc8998c
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-02-19 23:13:15 +0900

    feat: 경로 조회 기능 구현을 위한 경로 조회 컴포넌트에 만족하는 기능 구현

--- src/main/java/nextstep/subway/application/PathFinder.java
+++ src/main/java/nextstep/subway/application/PathFinder.java
@@ -2,15 +2,39 @@ package nextstep.subway.application;
 
 import nextstep.subway.application.dto.PathResult;
 import nextstep.subway.entity.Line;
+import nextstep.subway.entity.Section;
 import nextstep.subway.entity.Station;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PathFinder {
     public PathResult findShortestPath(List<Line> lines, Station departureStation, Station arrivalStation) {
-        return new PathResult(new ArrayList<>(), 0);
+
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+        for(Line line : lines) {
+            for(Section section : line.getAllSections()) {
+                Station upStation = section.getUpStation();
+                Station downStation = section.getDownStation();
+
+                graph.addVertex(upStation);
+                graph.addVertex(downStation);
+                graph.setEdgeWeight(graph.addEdge(upStation, downStation), section.getDistance());
+            }
+        }
+
+        DijkstraShortestPath shortestPath = new DijkstraShortestPath<>(graph);
+        GraphPath path = shortestPath.getPath(departureStation, arrivalStation);
+
+        List vertexInOrder = path.getVertexList();
+        int distance = (int) path.getWeight();
+
+        return new PathResult(vertexInOrder, distance);
     }
 }
