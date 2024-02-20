780b43fb9099bc8094dca0c100bbb9a8940c50cb
woonys <wodns1324@gmail.com>
2024-02-21 02:15:56 +0900

    test: 인수 테스트 작성

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -1,6 +1,7 @@
 package nextstep.subway.acceptance;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import nextstep.subway.dto.SectionRequest;
@@ -26,8 +27,39 @@ public class PathAcceptanceTest extends BaseAcceptanceTest {
         지하철_노선에_지하철_구간_생성_요청(삼호선_ID, new SectionRequest(남부터미널역_ID, 양재역_ID, 3));
     }
 
+    @DisplayName("   given 출발역과 도착역이 주어질 때\n"
+                 + "   when 경로를 조회하면\n"
+                 + "   then 출발역과 도착역 사이 역 목록과\n"
+                 + "   and  출발역과 도착역 사이 거리를 조회할 수 있다.")
     @Test
-    void test() {
+    void 경로를_조회하면_출발역과_도착역_사이_역_목록과_거리를_조회할_수_있다() {
+
+    }
+
+    @DisplayName("given 출발역과 도착역이 주어질 때\n"
+                 + "    when 경로를 조회하는데\n"
+                 + "    and  출발역과 도착역이 동일하면\n"
+                 + "    then 예외를 반환한다.")
+    @Test
+    void 경로를_조회할_때_출발역과_도착역이_동일하면_예외를_반환한다() {
+
+    }
+
+    @DisplayName("given 출발역과 도착역이 주어질 때\n"
+                 + "    when 경로를 조회하는데\n"
+                 + "    and  출발역과 도착역이 연결되어 있지 않으면\n"
+                 + "    then 예외를 반환한다.")
+    @Test
+    void 경로를_조회할_때_출발역과_도착역이_연결되어있지_않으면_예외를_반환한다() {
+
+    }
+
+    @DisplayName("given 출발역과 도착역이 주어질 때\n"
+                 + "    when 경로를 조회하는데\n"
+                 + "    and  출발역 또는 도착역이 존재하지 않으면\n"
+                 + "    then 예외를 반환한다.")
+    @Test
+    void 경로를_조회할_때_출발역_또는_도착역이_존재하지_않으면_예외를_반환한다() {
 
     }
 }
