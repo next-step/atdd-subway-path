4be341b38c0b10a3cbed7d6a1a37bc7797f2f1b4
cjsrhd94 <cjsrhd94@gmail.com>
2024-02-28 17:33:14 +0900

    test: 구간 추가시 유효성 검사 단위 테스트 작성
    - 동일 지하철 구간 유효성 검사 테스트 작성
    - 기존 지하철 구간 사이에 신규 구간 생성시 유효성 검사 테스트 작성

--- src/test/java/subway/unit/LineTest.java
+++ src/test/java/subway/unit/LineTest.java
@@ -1,10 +1,17 @@
 package subway.unit;
 
+import io.restassured.RestAssured;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import subway.line.Line;
 import subway.section.Section;
+import subway.section.SectionRequest;
 import subway.station.Station;
+import util.RestAssuredUtil;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -22,6 +29,7 @@ public class LineTest {
     private final Section fourthSection = new Section(4L, seonyudo, sinmokdong, 20L, line);
     private final Section resultSection1 = new Section(5L, nationalAssembly, dangsan, 20L, line);
     private final Section resultSection2 = new Section(6L, dangsan, seonyudo, 20L, line);
+    private final Section errorSection = new Section(7L, yeouido, nationalAssembly, 40L, line);
 
     @BeforeEach
     void beforeEach(){
@@ -127,5 +135,33 @@ public class LineTest {
                 .isEqualTo(resultSection2.getUpStation().getName());
         assertThat(line.sections().sections().get(1).getDownStation().getName())
                 .isEqualTo(resultSection2.getDownStation().getName());
+    }
+
+    /**
+     * When 동일한 지하철 구간을 등록하면
+     * Then IllegalArgumentException이 발생한다.
+     */
+    @DisplayName("동일한 지하철 구간을 등록할 수 없다.")
+    @Test
+    void 동일_지하철_구간_유효성_검사() {
+        //then
+        line.addSection(firstSection);
+        Assertions.assertThatThrownBy(() -> line.addSection(firstSection))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("동일한 구간을 추가할 수 없습니다.");
+
+    }
+
+    /**
+     * When 기존 지하철 구간 중간에 신규 구간을 추가할 때, 기존 구간보다 거리가 멀면
+     * Then IllegalArgumentException이 발생한다.
+     */
+    @DisplayName("기존 지하철 구간 중간에 신규 구간을 추가할 때, 기존 구간보다 거리가 멀면 추가할 수 없다.")
+    @Test
+    void 기존_지하철_구간_사이에_신규_구간_생성시_거리_유효성_검사() {
+        line.addSection(firstSection);
+        Assertions.assertThatThrownBy(() -> line.addSection(errorSection))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("신규 구간은 기존 구간보다 짧아야 합니다.");
     }
 }
