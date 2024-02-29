22e34e9a8414ad3397352d41c866ff3159fae124
woonys <wodns1324@gmail.com>
2024-03-01 01:34:07 +0900

    refactor: Station 객체 equals 메소드 리팩토링 - id 기준으로 비교

--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -49,9 +49,9 @@ public class Station {
     @Override
     public boolean equals(Object o) {
         if (this == o) {return true;}
-        if (o == null || getClass() != o.getClass()) {return false;}
+        if (!(o instanceof Station)) {return false;}
         Station station = (Station) o;
-        return Objects.equals(name, station.name);
+        return Objects.equals(id, station.getId());
     }
 
     @Override
